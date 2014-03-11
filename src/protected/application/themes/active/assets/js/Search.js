594610414a18e0b70ee9cf848aec579b66275549
Leonardo Santagada <santagada@gmail.com>
2014-03-11 13:09:48 -0300

    filtering empty data from hash

--- src/protected/application/themes/active/assets/js/Search.js
+++ src/protected/application/themes/active/assets/js/Search.js
@@ -1,9 +1,40 @@
 (function(angular){
     "use strict";
-    var app = angular.module('search', ['ng-mapasculturais', 'SearchService', 'rison']);
 
-    app.controller('SearchController', ['$scope', '$rootScope', '$location', '$rison', '$window', function($scope, $rootScope, $location, $rison, $window){
-        $scope.data = {
+    var emptyFilter = function (input, levels) {
+        // return a filtered object without nulls
+        if(levels === undefined) levels = 4;
+        if(levels === 0) return angular.copy(input);
+        var output = {};
+        if(angular.isArray(input)) {
+            output = [];
+        }
+
+        var currVal;
+        angular.forEach(input, function(value, key){
+            if(typeof value === 'undefined' ||
+               value === null ||
+               value === '') {
+                return;
+            }
+
+            if(angular.isObject(value)) {
+                currVal = emptyFilter(value, levels - 1);
+            } else {
+                currVal = value;
+            }
+
+            if(angular.isArray(output)) {
+                this.push(currVal);
+            } else {
+                this[key] = currVal;
+            }
+        }, output);
+
+        return output;
+    };
+
+    var skeletonData = {
             global: {
                 // isVerified: true,
                 // isCombined: false,
@@ -48,37 +79,42 @@
             // }
         };
 
+    var app = angular.module('search', ['ng-mapasculturais', 'SearchService', 'rison']);
+
+    app.controller('SearchController', ['$scope', '$rootScope', '$location', '$rison', '$window', function($scope, $rootScope, $location, $rison, $window){
+        $scope.data = angular.copy(skeletonData);
+
         $scope.areas = MapasCulturais.taxonomyTerms.area.map(function(el, i){ return {id: i, name: el}; });
 
         $scope.$watch('data', function(newValue, oldValue){
-            console.log('calling watch for ', newValue);
             if(newValue === undefined) return;
-            console.log('zuado');
-            if($location.hash() !== $rison.stringify(newValue)){
+            var serialized = $rison.stringify(emptyFilter(newValue));
+
+            if($location.hash() !== serialized){
                 console.log('changing location.hash');
-                $location.hash($rison.stringify(newValue));
+                $location.hash(serialized);
             }
         }, true);
 
-        $rootScope.$on('$locationChangeSuccess', function(newValue, oldValue){
-            if($location.hash() !== $rison.stringify($scope.data)){
-                console.log('changing data object from ',$location.hash());
-                $scope.data = $rison.parse($location.hash());
+        $rootScope.$on('$locationChangeSuccess', function(){
+            var newValue = $location.hash();
+            if(newValue !== $rison.stringify(emptyFilter($scope.data))){
+                $scope.data = angular.extend(angular.copy(skeletonData), $rison.parse(newValue));
             }
         });
 
         $scope.getName = function(valores, id){
             try{
-                return valores.filter(function(e){if(e.id === id) return true })[0].name;
+                return valores.filter(function(e){if(e.id === id) return true;})[0].name;
             }catch(e){
                 return false;
             }
-        }
+        };
 
         angular.element(document).ready(function(){
             $window.leaflet.map.removeLayer($window.leaflet.marker);
             $window.leaflet.map.on('zoomend dragend', function(){
-                $scope.data.map = {
+                $scope.data.global.map = {
                     center : $window.leaflet.map.getCenter(),
                     zoom : $window.leaflet.map.getZoom()
                 };
