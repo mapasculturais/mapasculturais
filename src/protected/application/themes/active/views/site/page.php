0d8506b68b290fec41b429277bf1236c08a91ce8
Cátia <catia@hacklab.com.br>
2014-03-11 15:36:05 -0300

    removi espaços em branco e classe painel

--- src/protected/application/themes/active/views/site/page.php
+++ src/protected/application/themes/active/views/site/page.php
@@ -1,9 +1,9 @@
-<nav id="nav-da-pagina" class="barra-esquerda barra-lateral painel">
+<nav id="nav-da-pagina" class="barra-esquerda barra-lateral">
     <ul>
 		<li><a class="active" href="#">Página</a></li>
 		<li><a href="#">Página irmã</a></li>
 		<li><a href="#">Página irmã</a></li>
-		<li><a href="#">Página irmã</a></li>        
+		<li><a href="#">Página irmã</a></li>  
     </ul>
 </nav>
 <!--#nav-do-painel-->
