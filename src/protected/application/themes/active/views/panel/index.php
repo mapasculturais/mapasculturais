90308346857c17643041cd5f9d1ac72300b07dad
Cátia <catia@hacklab.com.br>
2014-03-11 20:34:27 -0300

    arrumei alguns estilos do menu do painel epáginas criei estilos novos para as estatísticas do painel alterei o html das estatísticas do painel, coloquei links para as listagens e para adicionar entidades. (issue #28) organizei a ordem do menu do painel igual ao menu principal.

--- src/protected/application/themes/active/views/panel/index.php
+++ src/protected/application/themes/active/views/panel/index.php
@@ -1,11 +1,50 @@
 <?php $this->part('panel/part-nav.php');?>
 <div class="main-content">
-    <section id="estatisticas-do-usuario">
-        <div class="clearfix">Agentes <span class="valor"><?php echo $count->agents;?></span></div>
-        <div class="clearfix">Eventos <span class="valor"><?php echo $count->events;?></span></div>
-        <div class="clearfix">Espaços <span class="valor"><?php echo $count->spaces;?></span></div>
-        <div class="clearfix">Projetos <span class="valor">00</span></div>
-        <div class="clearfix">Contratos <span class="valor">00</span></div>
+	<p id="boas-vindas-painel">
+		Olá, <strong>nome-de-usuário</strong>, bem-vindo ao painel do SPCultura!
+	</p>
+	<h2>Resumo</h2>
+    <section id="estatisticas-do-usuario" class="clearfix">
+    	<div class="box">
+        	<div class="clearfix">
+        		<span class="alignleft">Eventos</span>
+        		<div class="icone icon_calendar alignright"></div>
+        	</div>
+        	<div class="clearfix">        		
+        		<a class="valor hltip" href="<?php echo $app->createUrl('panel', 'events') ?>" title="Ver meus eventos"><?php echo $count->events;?></a>
+        		<a class="icone icon_plus alignright hltip" href="<?php echo $app->createUrl('event', 'create'); ?>" title="Adicionar eventos"></a>
+        	</div>
+        </div>
+        <div class="box">
+        	<div class="clearfix">
+        		<span class="alignleft">Agentes</span>
+        		<div class="icone icon_profile alignright"></div>
+        	</div>
+        	<div class="clearfix">
+        		<a class="valor hltip" href="<?php echo $app->createUrl('panel', 'agents') ?>" title="Ver meus agentes"><?php echo $count->agents;?></a>
+        		<a class="icone icon_plus alignright hltip" href="<?php echo $app->createUrl('agent', 'create'); ?>" title="Adicionar agentes"></a>
+        	</div>
+        </div>        
+        <div class="box">
+	        <div class="clearfix">
+	        	<span class="alignleft">Espaços</span>
+	        	<div class="icone icon_building alignright"></div>
+	        </div>
+	        <div class="clearfix">
+	        	<a class="valor hltip" href="<?php echo $app->createUrl('panel', 'spaces') ?>" title="Ver meus espaços"><?php echo $count->spaces;?></a>
+	        	<a class="icone icon_plus alignright hltip" href="<?php echo $app->createUrl('space', 'create'); ?>" title="Adicionar espaços"></a>
+	        </div>
+	    </div>
+        <div class="box">
+        	<div class="clearfix">
+        		<span class="alignleft">Projetos</span>
+        		<div class="icone icon_document_alt alignright"></div>
+        	</div>
+        	<div class="clearfix">
+        		<a class="valor hltip" href="<?php echo $app->createUrl('panel', 'projects') ?>" title="Ver meus projetos">00</a>
+        		<a class="icone icon_plus alignright hltip" href="<?php echo $app->createUrl('project', 'create'); ?>" title="Adicionar projetos"></a>
+        	</div>
+        </div>
     </section>
     <section id="atividades">
 		<header class="clearfix">
