cd4f1e49f5b9662f38225c9049a0bc732d8ddd37
Cátia <catia@hacklab.com.br>
2014-03-13 20:05:47 -0300

    resolvi parte da tarefa #36 coloquei as abas de ativos/lixeira em todas entidades coloquei o miolo dos loops em parts

--- src/protected/application/themes/active/views/panel/agents.php
+++ src/protected/application/themes/active/views/panel/agents.php
@@ -6,11 +6,8 @@ $this->part('panel/part-nav.php');
 		<h2 class="alignleft">Meus agentes</h2>
 		<a class="botao adicionar" href="<?php echo $app->createUrl('agent', 'create'); ?>">Adicionar novo agente</a>
 	</header>
-    <ul class="abas clearfix clear" style="display: block; ">
+    <ul class="abas clearfix clear">
         <li class="active"><a href="#ativos">Ativos</a></li>
-        <li><a href="#inativos">Inativos</a></li>
-        <li><a href="#relacionados">Relacionados</a></li>
-        <li><a href="#convidados">Convidados</a></li>
         <li><a href="#lixeira">Lixeira</a></li>
     </ul>
     <div id="ativos">
@@ -18,24 +15,11 @@ $this->part('panel/part-nav.php');
             <?php $this->part('panel/part-agent', array('entity' => $entity)); ?>
         <?php endforeach; ?>
     </div>
-    <div id="inativos">
-        <?php foreach($app->user->disabledAgents as $entity): ?>
-            <?php $this->part('panel/part-agent', array('entity' => $entity)); ?>
-        <?php endforeach; ?>
-    </div>
-    <div id="relacionados">
-        <?php foreach($app->user->relatedAgents as $entity): ?>
-            <?php $this->part('panel/part-agent', array('entity' => $entity)); ?>
-        <?php endforeach; ?>
-    </div>
-    <div id="convidados">
-        <?php foreach($app->user->invitedAgents as $entity): ?>
-            <?php $this->part('panel/part-agent', array('entity' => $entity)); ?>
-        <?php endforeach; ?>
-    </div>
+    <!-- #ativos-->
     <div id="lixeira">
         <?php foreach($app->user->trashedAgents as $entity): ?>
             <?php $this->part('panel/part-agent', array('entity' => $entity)); ?>
         <?php endforeach; ?>
     </div>
+    <!-- #lixeira-->
 </div>
---
+++ src/protected/application/themes/active/views/panel/part-space.php
@@ -0,0 +1,18 @@
+<article class="objeto clearfix">
+    <?php if($avatar = $entity->avatar): ?>
+        <div class="thumb" style="background-image: url(<?php echo $avatar->transform('avatarSmall')->url; ?>)"></div>
+    <?php else: ?>
+        <div class="thumb"></div>
+    <?php endif; ?>
+    <h1><a href="<?php echo $entity->singleUrl; ?>"><?php echo $entity->name; ?></a></h1>
+	<div class="objeto-meta">
+		<div><span class="label">Tipo:</span> <?php echo $entity->type->name?></div>
+		<div><span class="label">Área de atuação:</span> Cinema</div>
+		<div><span class="label">Local:</span> <?php echo $entity->endereco?></div>
+		<div><span class="label">Acessibilidade:</span> Sim</div>			
+	</div>
+    <div>
+        <a class="action" href="<?php echo $entity->editUrl; ?>">editar</a>
+        <a class="action" href="<?php echo $entity->deleteUrl; ?>">excluir</a>
+    </div>
+</article>
