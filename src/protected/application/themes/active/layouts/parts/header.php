60a802f96c1b513bb4d7de3e1923f3361f03022d
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-04-02 15:44:23 -0300

    User Panel menu with shortcuts and icons

--- src/protected/application/themes/active/layouts/parts/header.php
+++ src/protected/application/themes/active/layouts/parts/header.php
@@ -237,7 +237,14 @@
                             </a>
                             <ul class="submenu">
                                 <div class="setinha"></div>
-                                <li><a href="<?php echo $app->createUrl('panel') ?>">Painel</a></li>
+                                <li><a href="<?php echo $app->createUrl('panel');?>"><span class="icone icon_house"></span> Painel</a></li>
+                                    <ul class="third-level">
+                                        <li><a href="<?php echo $app->createUrl('panel', 'events') ?>"><span class="icone icon_calendar"></span> Meus Eventos</a></li>
+                                        <li><a href="<?php echo $app->createUrl('panel', 'agents') ?>"><span class="icone icon_profile"></span> Meus Agentes</a></li>
+                                        <li><a href="<?php echo $app->createUrl('panel', 'spaces') ?>"><span class="icone icon_building"></span> Meus Espaços</a></li>
+                                        <li><a href="<?php echo $app->createUrl('panel', 'projects') ?>"><span class="icone icon_document_alt"></span> Meus Projetos</a></li>
+                                        <li><a href="<?php echo $app->createUrl('panel', 'contracts') ?>"><span class="icone icon_currency_alt"></span> Meus Contratos</a></li>
+                                    </ul>
                                 <li><a href="#">Ajuda</a></li>
                                 <li><a href="<?php echo $app->createUrl('auth', 'logout') ?>">Sair</a></li>
                             </ul>
