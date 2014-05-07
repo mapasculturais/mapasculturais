849169d61836d1ab5b034c7e39ba0c0c9928b210
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-05-07 12:59:22 -0300

    [add] remove eventos e espaços antigos da prefeitura
    [mod] novo script para importar planilhas da prefeitura

--- src/protected/application/conf/db-updates.php
+++ src/protected/application/conf/db-updates.php
@@ -2,7 +2,32 @@
 use MapasCulturais\App;
 
 return array(
-    'importa programação virada cultural - estado' => function(){
+    'remove eventos e espacos antigos da virada' => function(){
+        $user_id = 85;
+        $user = $this->repo('User')->find($user_id);
+        foreach ($user->agents as $agent) {
+            $events = $this->repo('Event')->findBy(array('owner' => $agent));
+            foreach($events as $event){
+                $this->log->info('REMOVENDO EVENTO ' . $event->name);
+                $event->destroy();
+            }
+        }
+        foreach ($user->agents as $agent) {
+            $spaces = $this->repo('Space')->findBy(array('owner' => $agent));
+            foreach($spaces as $space){
+                $this->log->info('REMOVENDO ESPAÇO ' . $space->name);
+                if($occs = $this->repo('EventOccurrence')->findBy(array('space' => $space))){
+                    $this->log->info("EVENTOS: " . print_r(json_encode($occs), true));
+                    foreach ($occs as $occ) {
+                        $occ->delete();
+                    }
+                }
+                $space->destroy();
+            }
+        }
+    },
+
+    'programação virada cultural' => function(){
         $csv_to_array = function ($filename='', $delimiter=','){
             if(!file_exists($filename) || !is_readable($filename))
                 return FALSE;
@@ -26,148 +51,166 @@ return array(
         $area_definition = $this->getRegisteredTaxonomy('MapasCulturais\Entities\Event', 'linguagem');
 
         $mapeamento_areas = array(
+            'Teatro Infantil' => array('Teatro'),
+            'Teatro Adulto' => array('Teatro'),
             'Exposições' => array('Artes Visuais'),
             'Cinema e Vídeo' => array('Audiovisual', 'Cinema')
         );
 
-        $created_events = array();
+        $agentes = array(
+            array(433, 'smc', "SECRETARIA MUNICIPAL DE CULTURA"), 
+            array(434, 'estado', "SECRETARIA DE ESTADO DA CULTURA"),
+            array(432, 'sesc', "SESC")
+        );
 
-        $agent_id = 428;
+
         $project_id = 4;
-
-        $agent = App::i()->repo('Agent')->find($agent_id);
-        $agent_errado = App::i()->repo('Agent')->find(427);
+        $agent_errado_id = 427;
 
         $project = App::i()->repo('Project')->find($project_id);
 
-        if(!$space_errado = $this->repo('Space')->findOneBy(array('name' => 'ESPAÇO DOS EVENTOS ERRADOS'))){
-            $space_errado = new \MapasCulturais\Entities\Space;
-            $space_errado->owner = $agent_errado;
-            $space_errado->name = "ESPAÇO DOS EVENTOS ERRADOS";
-            $space_errado->type = 501;
-            $space_errado->terms['area'] = array('Outros');
-            $space_errado->location = array(1,1);
-            $space_errado->save();
-        }
+        foreach ($agentes as $cfg) {
+            $agent_id = $cfg[0];
+            $prefix = $cfg[1];
 
-        $spaces = $csv_to_array(realpath(__DIR__ . '/../../../../tmp/estado-espacos.csv'));
-        $events = $csv_to_array(realpath(__DIR__ . '/../../../../tmp/estado-eventos.csv'));
+            $this->log->info("\n\n================================ IMPORTANDO PROGRAMAÇÃO DA " . $cfg[2] . " =====================================");
 
+            $created_events = array();
 
-        $create_event = function($space, $event_data, $subtitle = '') use($mapeamento_areas, $created_events, $area_definition, $agent, $project){
-            foreach($event_data as $k => $v)
-                $event_data[$k] = trim($v);
+            $agent = App::i()->repo('Agent')->find($agent_id);
+            $agent_errado = App::i()->repo('Agent')->find($agent_errado_id);
 
-            if(!$event_data['Atração'])
-                return;
+            $espaco_errado_nome = 'ESPAÇO DOS EVENTOS ERRADOS - ' . $prefix;
 
-            $event = new \MapasCulturais\Entities\Event;
-            $event->owner = $agent;
-            $event->project = $project;
-            $event->subTitle = $subtitle;
-
-            $event->name = $event_data['Atração'];
-            $event->shortDescription = array_key_exists('Sinopse (até 350 caracteres)', $event_data) ?
-                    $event_data['Sinopse (até 350 caracteres)'] : '';
-
-            $event->classificacaoEtaria = 'Livre';
-
-            if(array_key_exists('Preço ou ingresso', $event_data))
-                $event->preco = $event_data['Preço ou ingresso'];
-
-            $term = $event_data['Área de Atuação'];
-
-            $event->terms['tag'] = array('Virada Cultural', $term);
-
-            if(in_array($term, $area_definition->restrictedTerms) || in_array(strtolower($term), $area_definition->restrictedTerms)){
-                $event->terms['area'] = array($term);
-            }elseif(array_key_exists($term, $mapeamento_areas)){
-                $event->terms['area'] = $mapeamento_areas[$term];
-            }else{
-                $event->terms['area'] = array('Outros');
+            if(!$space_errado = $this->repo('Space')->findOneBy(array('name' => $espaco_errado_nome))){
+                $space_errado = new \MapasCulturais\Entities\Space;
+                $space_errado->owner = $agent_errado;
+                $space_errado->name = $espaco_errado_nome;
+                $space_errado->type = 501;
+                $space_errado->terms['area'] = array('Outros');
+                $space_errado->location = array(1,1);
+                $space_errado->save();
             }
 
-            $this->log->info("      >> >> EVENTO $event->name CRIADO");
-            $event->save(true);
+            $spaces = $csv_to_array(realpath(__DIR__ . '/../../../../tmp/' . $prefix . '-espacos.csv'));
+            $events = $csv_to_array(realpath(__DIR__ . '/../../../../tmp/' . $prefix . '-eventos.csv'));
 
-            if(strtolower($event_data['Horário']) === '24h'){
-                $startsOn = '2014-05-17';
-                $startsAt = '18:00';
-                $duration = '24h00';
-            }else{
-                $startsOn =  $event_data['Data'] === '17/5' ? '2014-05-17' : '2014-05-18';
-                $startsAt = $event_data['Horário'];
-                $duration = '';
-            }
-            @list($hours, $minutes) = explode(':', $startsAt);
+            $create_event = function($space, $event_data, $subtitle = '') use($mapeamento_areas, $created_events, $area_definition, $agent, $project){
+                foreach($event_data as $k => $v)
+                    $event_data[$k] = trim($v);
 
-            $startsAt = str_pad($hours, 2, '0', STR_PAD_LEFT)  . ':' . str_pad($minutes, 2, '0', STR_PAD_LEFT);
+                if(!$event_data['Atração'])
+                    return;
 
-            $eoccurrence = new \MapasCulturais\Entities\EventOccurrence();
-            $eoccurrence->space = $space;
-            $eoccurrence->event = $event;
+                $event = new \MapasCulturais\Entities\Event;
+                $event->owner = $agent;
+                $event->project = $project;
+                $event->subTitle = $subtitle;
+
+                $event->name = $event_data['Atração'];
+                $event->shortDescription = array_key_exists('Sinopse (até 350 caracteres)', $event_data) ?
+                        $event_data['Sinopse (até 350 caracteres)'] : '';
+
+                $event->classificacaoEtaria = 'Livre';
+
+                if(array_key_exists('Preço ou ingresso', $event_data))
+                    $event->preco = $event_data['Preço ou ingresso'];
+
+                $term = $event_data['Área de Atuação'];
+
+                $event->terms['tag'] = array('Virada Cultural', $term);
+
+                if(in_array($term, $area_definition->restrictedTerms) || in_array(strtolower($term), $area_definition->restrictedTerms)){
+                    $event->terms['area'] = array($term);
+                }elseif(array_key_exists($term, $mapeamento_areas)){
+                    $event->terms['area'] = $mapeamento_areas[$term];
+                }else{
+                    $event->terms['area'] = array('Outros');
+                }
+
+                $this->log->info("      >> >> EVENTO $event->name CRIADO");
+                $event->save(true);
+
+                if(strtolower($event_data['Horário']) === '24h'){
+                    $startsOn = '2014-05-17';
+                    $startsAt = '18:00';
+                    $duration = '24h00';
+                }else{
+                    $startsOn =  $event_data['Data'] === '17/5' ? '2014-05-17' : '2014-05-18';
+                    $startsAt = $event_data['Horário'];
+                    $duration = '';
+                }
+                @list($hours, $minutes) = explode(':', $startsAt);
+
+                $startsAt = str_pad($hours, 2, '0', STR_PAD_LEFT)  . ':' . str_pad($minutes, 2, '0', STR_PAD_LEFT);
+
+                $eoccurrence = new \MapasCulturais\Entities\EventOccurrence();
+                $eoccurrence->space = $space;
+                $eoccurrence->event = $event;
 
 
-            $rule = '{
-                "spaceId":"' . $space->id . '",
-                "startsAt": "' . $startsAt . '",
-                "duration": "' . $duration . '",
-                "frequency": "daily",
-                "startsOn": "' . $startsOn . '",
-                "until": "' . $startsOn . '"
-            }';
+                $rule = '{
+                    "spaceId":"' . $space->id . '",
+                    "startsAt": "' . $startsAt . '",
+                    "duration": "' . $duration . '",
+                    "frequency": "daily",
+                    "startsOn": "' . $startsOn . '",
+                    "until": "' . $startsOn . '"
+                }';
 
-            $this->log->info(print_r($rule, true));
+                $this->log->info(print_r($rule, true));
 
-            $eoccurrence->rule = json_decode($rule);
-            $eoccurrence->save(true);
-        };
+                $eoccurrence->rule = json_decode($rule);
+                $eoccurrence->save(true);
+            };
 
 
-        foreach($spaces as $space_data){
-            $space = new \MapasCulturais\Entities\Space;
-            $space->owner = $agent;
+            foreach($spaces as $space_data){
+                $space = new \MapasCulturais\Entities\Space;
+                $space->owner = $agent;
 
-            $space->name = $space_data['Local/Palco'];
-            $space->type = 501;
-            $space->terms['area'] = array('Outros');
+                $space->name = $space_data['Palco'];
+                $space->type = 501;
+                $space->terms['area'] = array('Outros');
 
-            if($space_data['Endereço'])
-                $space->endereco = $space_data['Endereço'];
+                if($space_data['Endereço'])
+                    $space->endereco = $space_data['Endereço'];
 
-            if($space_data['Georreferenciamento']){
-                $loc = array(0,0);
+                if($space_data['Georreferenciamento']){
+                    $loc = array(0,0);
 
-                if(preg_match('#^[ ]*(-\d{2}\.\d+)[ ]*,[ ]*(-\d{2}\.\d+)[ ]*#', $space_data['Georreferenciamento'], $match1))
-                    $loc = array($match1[1], $match1[2]);
+                    if(preg_match('#^[ ]*(-\d{2}\.\d+)[ ]*,[ ]*(-\d{2}\.\d+)[ ]*#', $space_data['Georreferenciamento'], $match1))
+                        $loc = array($match1[1], $match1[2]);
 
-                else if(preg_match('#^[ ]*(-\d{2},\d+)[ ]*;[ ]*(-\d{2},\d+)[ ]*#', $space_data['Georreferenciamento'], $match2))
-                    $loc = array(str_replace(',','.',$match2[1]), str_replace(',','.',$match2[2]));
+                    else if(preg_match('#^[ ]*(-\d{2},\d+)[ ]*;[ ]*(-\d{2},\d+)[ ]*#', $space_data['Georreferenciamento'], $match2))
+                        $loc = array(str_replace(',','.',$match2[1]), str_replace(',','.',$match2[2]));
 
-                if(count($loc) == 2){
-                    $space->location = array((float) $loc[1], (float) $loc[0]);
+                    if(count($loc) == 2){
+                        $space->location = array((float) $loc[1], (float) $loc[0]);
+                    }
+                }
+                $this->log->info("VIRADA>> ESPAÇO $space->name CRIADO  ($space->location)");
+                $space->save(true);
+
+
+                foreach($events as $event_data){
+                    if(str_replace(' ','',strtolower($event_data['Palco'])) != str_replace(' ','',strtolower($space_data['Palco'])))
+                        continue;
+                    $created_events[] = md5(json_encode($event_data));
+                    $create_event($space, $event_data);
                 }
             }
-            $this->log->info("VIRADA>> ESPAÇO $space->name CRIADO  ($space->location)");
-            $space->save(true);
 
-
+            $this->log->info("EVENTOS NAO CRIADOS:::::");
             foreach($events as $event_data){
-                if(str_replace(' ','',strtolower($event_data['Palco'])) != str_replace(' ','',strtolower($space_data['Local/Palco'])))
-                    continue;
-                $created_events[] = md5(json_encode($event_data));
-                $create_event($space, $event_data);
+                $md5 = md5(json_encode($event_data));
+
+                if(!in_array($md5, $created_events)){
+                    $create_event($space_errado, $event_data, $event_data['Palco']);
+                }
             }
         }
 
-        $this->log->info("EVENTOS NAO CRIADOS:::::");
-        foreach($events as $event_data){
-            $md5 = md5(json_encode($event_data));
-
-            if(!in_array($md5, $created_events)){
-                $create_event($space_errado, $event_data, $event_data['Palco']);
-            }
-        }
         return true;
     },
 
