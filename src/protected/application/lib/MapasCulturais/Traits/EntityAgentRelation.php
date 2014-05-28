cd10e50364db53788130f70bf422b69fade017bf
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-05-28 19:59:38 -0300

    fix method createAgentRelation of trait EntityAgentRelation when has_control is true

--- src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
+++ src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
@@ -21,14 +21,26 @@ trait EntityAgentRelation {
         return preg_replace('#Entities\\\([^\\\]+)$#', 'Entities\\\AgentRelations\\\$1', $this->getClassName());
     }
 
-    function getAgentRelations(){
+    function getAgentRelations($has_control = null){
         if(!$this->id)
             return array();
 
         $relation_class = $this->getAgentRelationEntityClassName();
         if(!class_exists($relation_class))
             return array();
-
+        
+        $params = array(
+            'owner' => $this,
+            'in' => array(Agent::STATUS_ENABLED, Agent::STATUS_INVITED, Agent::STATUS_RELATED)
+        );
+        
+        $dql_has_control = '';
+        
+        if(is_bool($has_control)){
+            $params['has_control'] = $has_control;
+            $dql_has_control = "ar.hasControl = :has_control AND";
+        }
+        
         $dql = "
             SELECT
                 ar,
@@ -41,18 +53,15 @@ trait EntityAgentRelation {
             WHERE
                 ar.owner = :owner AND
                 ar.status > 0 AND
+                $dql_has_control
                 a.status IN (:in)
             ORDER BY a.name";
 
         $query = App::i()->em->createQuery($dql);
 
-        $query->setParameters(array(
-            'owner' => $this,
-            'in' => array(Agent::STATUS_ENABLED, Agent::STATUS_INVITED, Agent::STATUS_RELATED)
-        ));
+        $query->setParameters($params);
 
         $result = $query->getResult();
-
         return $result;
     }
 
@@ -96,14 +105,12 @@ trait EntityAgentRelation {
     }
 
     function getUsersWithControl(){
-        $result = array($this->ownerUser);
-        $relations = $this->getAgentRelations();
+        $result = array($this->getOwnerUser());
+        $relations = $this->getAgentRelations(true);
+        
         foreach($relations as $relation){
-            if($relation->hasControl && !in_array($relation->agent->user, $result)){
-                $result[] = $relation->agent->user;
-            }
+            $result[] = $relation->agent->user;
         }
-
         return $result;
     }
 
@@ -126,10 +133,11 @@ trait EntityAgentRelation {
         $relation->agent = $agent;
         $relation->owner = $this;
         $relation->group = $group;
-        $relation->save($flush);
-
+        
         if($has_control)
-            $this->addControlToRelatedAgent($agent);
+            $relation->hasControl = true;
+        
+        $relation->save($flush);
 
         return $relation;
     }
