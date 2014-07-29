5b9fb0074a672d99f30ec49cad4d6d883c726ea8
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-07-29 20:23:57 -0300

    fix file types description in editboxes

--- src/protected/application/themes/active/views/project/single.php
+++ src/protected/application/themes/active/views/project/single.php
@@ -196,8 +196,8 @@ $ids = array_map(function($e){
         
         <?php if($this->controller->action == 'edit'): ?>
             <a class="botao simples js-open-editbox" data-target="#editbox-upload-registration-form">Subir uma ficha de inscrição</a>
-            <div id="editbox-upload-registration-form" class="js-editbox mc-right">
-                <?php add_ajax_uploader ($entity, 'registrationForm', 'set-content', '.js-ficha-inscricao','<a href="{{url}}" class="botao principal"><span class="icone icon_download"></span>Baixar a Ficha de Inscrição</a><a class="icone icon_close hltip js-remove-item" data-href="{{deleteUrl}}" data-target=".js-ficha-inscricao>*" data-confirm-message="Rmover a ficha de inscrição?" title="Remover a ficha de inscrição"></a>'); ?>
+            <div id="editbox-upload-registration-form" class="js-editbox mc-right" title="Subir ficha de inscrição">
+                <?php add_ajax_uploader ($entity, 'registrationForm', 'set-content', '.js-ficha-inscricao','<a href="{{url}}" class="botao principal"><span class="icone icon_download"></span>Baixar a Ficha de Inscrição</a><a class="icone icon_close hltip js-remove-item" data-href="{{deleteUrl}}" data-target=".js-ficha-inscricao>*" data-confirm-message="Rmover a ficha de inscrição?" title="Remover a ficha de inscrição"></a>','',false,'.doc, .xls, .pdf'); ?>
             </div>
         <?php endif; ?>
         <?php if($app->auth->isUserAuthenticated() && $entity->isRegistrationOpen() && !is_editable()): ?>
