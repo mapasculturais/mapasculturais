b3fd0b857b9437c133207fa436524430ff2cfd0c
Felipe <hola@seufelipe.com.br>
2014-08-03 15:58:05 +0100

    adicionando icone de edicao no brasao da prefeitura (ref. #274)

--- src/protected/application/themes/active/layouts/parts/verified.php
+++ src/protected/application/themes/active/layouts/parts/verified.php
@@ -1,6 +1,6 @@
 <?php if(is_editable() && $entity->canUser('verify')): ?>
 <input id="is-verified-input" type="hidden" class="js-editable" data-edit="isVerified" data-value="<?php echo $entity->isVerified ? '1' : '0'?>"/>
-<div class="bloco">
+<div class="bloco  bloco--verified">
     <a href="#"
        class="oficial js-verified hltip <?php if($entity->isVerified) echo ' active'; ?>"
        data-verify-url="<?php echo $this->controller->createUrl('verify', array($entity->id)) ?>"
