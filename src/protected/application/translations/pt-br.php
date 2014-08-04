b204cb7e3864038119267b6318befd8f045a083f
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-08-04 10:28:13 -0300

    changed 'Inscritos' to 'Aprovados'; ref: #258

--- src/protected/application/translations/pt-br.php
+++ src/protected/application/translations/pt-br.php
@@ -4,7 +4,6 @@ return array(
         'Fake Authentication' => "Autenticação Fake",
         'Login with user' => 'Entrar com o usuário'
     ),
-//    'Fake Authentication' => "Autenticação Fake DAQUEUEUEUE",
     'Agent' => 'Agente',
     'Space' => 'Espaço',
     'Event' => 'Evento',
@@ -15,7 +14,7 @@ return array(
     '%s Entities found.' => '%s Entidades encontradas.',
     '%s Entity found.' => '%s Entidade encontrada.',
 
-    'project registration' => "Inscritos",
+    'project registration' => "Aprovados",
     'This agent is already registered in this project.' => 'Este agente já está inscrito neste projeto.',
     'the registration form is required' => 'A ficha de inscrição é requerida.',
 
