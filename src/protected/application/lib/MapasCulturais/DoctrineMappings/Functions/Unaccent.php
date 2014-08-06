4967dc9bafedbfa4eba0117a113067bd0fc7b338
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-08-06 19:26:59 -0300

    Mapped functions unaccent(), string_agg() and recurring_event_occurrence_for()  (refs #239)

---
+++ src/protected/application/lib/MapasCulturais/DoctrineMappings/Functions/Unaccent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MapasCulturais\DoctrineMappings\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Unaccent extends FunctionNode {
+
+    public $string;
+
+    public function parse(Parser $parser) {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->string = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker) {
+        return 'unaccent(' . $this->string->dispatch($sqlWalker) . ')';
+    }
+
+}
