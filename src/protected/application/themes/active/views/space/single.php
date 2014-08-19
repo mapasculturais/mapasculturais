ddf8214d8321fac0d32e32111f701b411606340e
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-08-19 20:20:59 -0300

    fix xeditable select of space public property (ref: #419)

--- src/protected/application/themes/active/views/space/single.php
+++ src/protected/application/themes/active/views/space/single.php
@@ -24,15 +24,15 @@ add_angular_entity_assets($entity);
     <div class="bloco">
         <h3 class="subtitulo">Status</h3>
         <?php if(is_editable()): ?>
-            <span class="js-editable" data-edit="public" data-type="select" data-source="[{value: 0, text: 'privado e requer autorização para criar eventos'},{value: 1, text:'público e qualquer pessoa pode criar eventos'}]">
+            <div id="editable-space-status" class="js-editable" data-edit="public" data-type="select" data-value="<?php echo $entity->public ? '1' : '0' ?>"  data-source="[{value: 0, text: 'privado e requer autorização para criar eventos'},{value: 1, text:'público e qualquer pessoa pode criar eventos'}]">
                 <?php if ($entity->public) : ?>
-                <div class="venue-status"><div class="icone icon_group"></div>público</div>
-                <p class="venue-status-definition">Qualquer pessoa pode criar eventos.</p>
-            <?php else: ?>
-                <div class="venue-status"><div class="icone icon_lock"></div>privado</div>
-                <p class="venue-status-definition">Requer autorização para criar eventos.</p>
-            <?php endif; ?>
-            </span>
+                    <div class="venue-status"><div class="icone icon_group"></div>público</div>
+                    <p class="venue-status-definition">Qualquer pessoa pode criar eventos.</p>
+                <?php else: ?>
+                    <div class="venue-status"><div class="icone icon_lock"></div>privado</div>
+                    <p class="venue-status-definition">Requer autorização para criar eventos.</p>
+                <?php endif; ?>
+            </div>
         <?php else: ?>
             <?php if ($entity->public) : ?>
                 <div class="venue-status"><div class="icone icon_group"></div>público</div>
