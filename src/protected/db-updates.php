4e2a97904d60c8ca6820205427021e9f7c4917e2
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-08-21 17:05:11 -0300

    fix an error when removing event occurrences caused by FKs

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -13,4 +13,22 @@ return array(
         $conn->executeQuery('ALTER TABLE agent ADD COLUMN parent_id INTEGER;');
         $conn->executeQuery('ALTER TABLE ONLY agent ADD CONSTRAINT agent_agent_fk FOREIGN KEY (parent_id) REFERENCES agent(id);');
     },
+            
+    'alter occurrence fk' => function() use($conn) {
+        $conn->executeQuery("
+            ALTER TABLE public.event_occurrence_cancellation
+                DROP CONSTRAINT event_occurrence_fk,
+                ADD CONSTRAINT event_occurrence_fk
+                   FOREIGN KEY (event_occurrence_id)
+                   REFERENCES event_occurrence(id)
+                   ON DELETE CASCADE");
+        
+        $conn->executeQuery("
+            ALTER TABLE public.event_occurrence_recurrence
+                DROP CONSTRAINT event_occurrence_fk,
+                ADD CONSTRAINT event_occurrence_fk
+                   FOREIGN KEY (event_occurrence_id)
+                   REFERENCES event_occurrence(id)
+                   ON DELETE CASCADE");
+    }
 );
