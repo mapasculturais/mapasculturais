ccecd5de8813a64c8a2936ccb032941610eef425
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-08-26 11:32:17 -0300

    improved api html outpu (ref: #447)

--- src/protected/application/lib/MapasCulturais/ApiOutputs/Html.php
+++ src/protected/application/lib/MapasCulturais/ApiOutputs/Html.php
@@ -6,22 +6,22 @@ class Html extends \MapasCulturais\ApiOutput{
     protected function getContentType() {
         return 'text/html';
     }
-
-    protected function _outputArray(array $data, $singular_object_name = 'Entity', $plural_object_name = 'Entities') {
+    
+    protected function printTable($data){
+        $data = json_decode(json_encode($data));
+        if(is_array($data))
+            $this->printArrayTable($data);
+        elseif(is_object($data))
+            $this->printOneItemTable($data);
+        else
+            return;
+    }
+    
+    protected function printArrayTable($data){
         $first = true;
-
+        
         ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title><?php echo sprintf(App::txts("%s $singular_object_name found.", "%s $plural_object_name found.", count($data)), count($data)) ?></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-
 <table border="1">
-    <caption><?php echo sprintf(App::txts("%s $singular_object_name found.", "%s $plural_object_name found.", count($data)), count($data)) ?></caption>
 <?php foreach($data as $item): ?>
     <?php if($first): $first=false;?>
     <thead>
@@ -35,7 +35,16 @@ class Html extends \MapasCulturais\ApiOutput{
     <?php endif; ?>
         <tr>
             <?php foreach($item as $k => $v): ?>
-            <td><?php echo $v ?></td>
+            <td><?php 
+            if(is_string($v) || is_numeric($v)){            
+                echo $v;
+            }elseif(is_array($v) || is_object($v)){
+                $this->printTable($v);
+                
+            }else{
+                var_dump($v);
+            }
+            ?></td>
             <?php endforeach; ?>
         </tr>
 <?php endforeach; ?>
@@ -43,7 +52,45 @@ class Html extends \MapasCulturais\ApiOutput{
     </tbody>
 <?php endif; ?>
 </table>
+    <?php
+    }
 
+    
+    protected function printOneItemTable($item){
+        ?>
+<table>
+    <?php foreach($item as $p => $v): ?>
+    <tr>
+        <th><?php echo $p ?></th>
+        <td><?php 
+            if(is_object($v) || is_array($v)) 
+                $this->printTable($v);
+            elseif(is_string($v) || is_numeric($v))
+                echo $v;
+            else
+                var_dump($v);
+            ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<?php
+    }
+    protected function _outputArray(array $data, $singular_object_name = 'Entity', $plural_object_name = 'Entities') {
+        ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo sprintf(App::txts("%s $singular_object_name found.", "%s $plural_object_name found.", count($data)), count($data)) ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            table table th {text-align: left; white-space: nowrap; }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo sprintf(App::txts("%s $singular_object_name found.", "%s $plural_object_name found.", count($data)), count($data)) ?></h1>
+        <?php $this->printTable($data) ?>
     </body>
 </html>
 
