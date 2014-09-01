4b648b73dc5b4316e741c7e7a5573762843d441b
Cátia Kitahara <catia@hacklab.com.br>
2014-09-01 17:54:20 -0300

    fix event search filter text

--- src/protected/application/themes/active/layouts/parts/busca-avancada.php
+++ src/protected/application/themes/active/layouts/parts/busca-avancada.php
@@ -42,7 +42,7 @@
             <!--#busca-->
             <div class="filtro">
                 <label class="show-label" for="data-de-inicio">Entre</label>
-                <input id="data-de-inicio" class="data" ng-model="data.event.from" ui-date="dateOptions" ui-date-format="yy-mm-dd" placeholder="00/00/0000" readonly="readonly" /> <label class="show-label">a</label>
+                <input id="data-de-inicio" class="data" ng-model="data.event.from" ui-date="dateOptions" ui-date-format="yy-mm-dd" placeholder="00/00/0000" readonly="readonly" /> <label class="show-label">e</label>
                 <input class="data" ng-model="data.event.to" ui-date="dateOptions" ui-date-format="yy-mm-dd" placeholder="00/00/0000" readonly="readonly" />
             </div>
             <!--.filtro-->
