cd889fc4721be48d508b07a01a44fde33cf67d54
Cátia Kitahara <catia@hacklab.com.br>
2014-09-02 10:41:12 -0300

    undo last commit from master

--- src/protected/application/themes/active/views/agent/single.php
+++ src/protected/application/themes/active/views/agent/single.php
@@ -21,30 +21,6 @@ add_angular_entity_assets($entity);
 <div class="barra-esquerda barra-lateral agente">
     <div class="setinha"></div>
     <?php $this->part('verified', array('entity' => $entity)); ?>
-    <div class="bloco">
-        <h3 class="subtitulo">Área de atuação</h3>
-        <?php if(is_editable()): ?>
-            <span id="term-area" class="js-editable-taxonomy" data-original-title="Área de Atuação" data-emptytext="Selecione pelo menos uma área" data-restrict="true" data-taxonomy="area"><?php echo implode('; ', $entity->terms['area'])?></span>
-        <?php else: ?>
-            <?php foreach($entity->terms['area'] as $i => $term): ;?>
-                <a class="tag tag-agente" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[area][]=<?php echo $term ?>"><?php echo $term ?></a>
-            <?php endforeach; ?>
-        <?php endif;?>
-    </div>
-    
-    <?php if(is_editable() || !empty($entity->terms['tag'])): ?>
-        <div class="bloco">
-            <h3 class="subtitulo">Tags</h3>
-            <?php if(is_editable()): ?>
-                <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
-            <?php else: ?>
-                <?php foreach($entity->terms['tag'] as $i => $term): ; ?>
-                    <a class="tag tag-agente" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
-                <?php endforeach; ?>
-            <?php endif;?>
-        </div>
-    <?php endif; ?>
-    
     <?php $this->part('redes-sociais', array('entity'=>$entity)); ?>
 </div>
 <article class="main-content agente">
@@ -84,10 +60,31 @@ add_angular_entity_assets($entity);
             <h2><span class="js-editable" data-edit="name" data-original-title="Nome de exibição" data-emptytext="Nome de exibição"><?php echo $entity->name; ?></span></h2>
             <div class="objeto-meta">
                 <div>
+                    <span class="label">Área de atuação: </span>
+                    <?php if(is_editable()): ?>
+                        <span id="term-area" class="js-editable-taxonomy" data-original-title="Área de Atuação" data-emptytext="Selecione pelo menos uma área" data-restrict="true" data-taxonomy="area"><?php echo implode('; ', $entity->terms['area'])?></span>
+                    <?php else: ?>
+                        <?php foreach($entity->terms['area'] as $i => $term): if($i) echo '; ';?>
+                            <a href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[area][]=<?php echo $term ?>"><?php echo $term ?></a>
+                        <?php endforeach; ?>
+                    <?php endif;?>
+                </div>
+                <div>
                     <span class="label">Tipo: </span>
                     <a href="#" class='js-editable-type' data-original-title="Tipo" data-emptytext="Selecione um tipo" data-entity='agent' data-value='<?php echo $entity->type ?>'><?php echo $entity->type->name; ?></a>
                 </div>
-                
+                <div>
+                    <?php if(is_editable() || !empty($entity->terms['tag'])): ?>
+                        <span class="label">Tags: </span>
+                        <?php if(is_editable()): ?>
+                            <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
+                        <?php else: ?>
+                            <?php foreach($entity->terms['tag'] as $i => $term): if($i) echo '; '; ?>
+                                <a href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
+                            <?php endforeach; ?>
+                        <?php endif;?>
+                    <?php endif; ?>
+                </div>
             </div>
             <!--.objeto-meta-->
         </div>
