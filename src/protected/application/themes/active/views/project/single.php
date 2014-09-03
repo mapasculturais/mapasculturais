c53d70bdc1ac715c2de89bd014f400382d144622
Cátia Kitahara <catia@hacklab.com.br>
2014-09-03 19:13:53 -0300

    move tags to the right sidebar move project type above the title and change markup

--- src/protected/application/themes/active/views/project/single.php
+++ src/protected/application/themes/active/views/project/single.php
@@ -37,6 +37,18 @@ add_entity_properties_metadata_to_js($entity);
 <div class="barra-esquerda barra-lateral projeto">
     <div class="setinha"></div>
     <?php $this->part('verified', array('entity' => $entity)); ?>
+    <?php if(is_editable() || !empty($entity->terms['tag'])): ?>
+        <div class="widget">
+            <h3>Tags</h3>
+            <?php if(is_editable()): ?>
+                <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
+            <?php else: ?>
+                <?php foreach($entity->terms['tag'] as $i => $term): ?>
+                    <a class="tag tag-project" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
+                <?php endforeach; ?>
+            <?php endif;?>
+        </div>
+    <?php endif;?>
     <?php $this->part('redes-sociais', array('entity'=>$entity)); ?>
 </div>
 
@@ -73,7 +85,12 @@ add_entity_properties_metadata_to_js($entity);
                 <?php endif; ?>
             </div>
             <!--.avatar-->
-
+            <div class="entity-type">
+                <div class="icone icon_document_alt"></div>
+                <a href="#" class='js-editable-type' data-original-title="Tipo" data-emptytext="Selecione um tipo" data-entity='project' data-value='<?php echo $entity->type ?>'>
+                    <?php echo $entity->type? $entity->type->name : ''; ?>
+                </a>
+            </div>
             <?php if(is_editable() && $entity->canUser('modifyParent')): ?>
             <span  class="js-search js-include-editable"
                    data-field-name='parentId'
@@ -94,27 +111,7 @@ add_entity_properties_metadata_to_js($entity);
             <?php elseif($entity->parent): ?>
                 <h4 class="entity-parent-title"><a href="<?php echo $entity->parent->singleUrl; ?>"><?php echo $entity->parent->name; ?></a></h4>
             <?php endif; ?>
-
             <h2><span class="js-editable" data-edit="name" data-original-title="Nome de exibição" data-emptytext="Nome de exibição"><?php echo $entity->name; ?></span></h2>
-            <div class="objeto-meta">
-                <div>
-                    <span class="label">Tipo: </span>
-                    <a href="#" class='js-editable-type' data-original-title="Tipo" data-emptytext="Selecione um tipo" data-entity='project' data-value='<?php echo $entity->type ?>'><?php echo $entity->type? $entity->type->name : ''; ?></a>
-                </div>
-                <div>
-                    <?php if(is_editable() || !empty($entity->terms['tag'])): ?>
-                        <span class="label">Tags: </span>
-                        <?php if(is_editable()): ?>
-                            <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
-                        <?php else: ?>
-                            <?php foreach($entity->terms['tag'] as $i => $term): if($i) echo '; '; ?>
-                                <a href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
-                            <?php endforeach; ?>
-                        <?php endif;?>
-                    <?php endif;?>
-                </div>
-            </div>
-            <!--.objeto-meta-->
         </div>
     </header>
 
