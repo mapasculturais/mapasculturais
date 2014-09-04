55e80ccb5f27712230faeec8e9af87c81e6bf8c7
Cátia Kitahara <catia@hacklab.com.br>
2014-09-03 21:42:59 -0300

    fix tag-agent with php function to echo entity

--- src/protected/application/themes/active/layouts/parts/widget-tags.php
+++ src/protected/application/themes/active/layouts/parts/widget-tags.php
@@ -5,7 +5,7 @@
             <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
         <?php else: ?>
             <?php foreach($entity->terms['tag'] as $i => $term): ?>
-                <a class="tag tag-agent" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
+                <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
             <?php endforeach; ?>
         <?php endif;?>
     </div>
