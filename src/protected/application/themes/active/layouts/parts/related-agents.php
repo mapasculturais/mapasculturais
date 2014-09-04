7d155fdbc5e696373b5af4eeff16d1271116e0c1
Cátia Kitahara <catia@hacklab.com.br>
2014-09-03 21:41:25 -0300

    delete textright class

--- src/protected/application/themes/active/layouts/parts/related-agents.php
+++ src/protected/application/themes/active/layouts/parts/related-agents.php
@@ -3,7 +3,7 @@ if($this->controller->action === 'create')
     return;
 ?>
 <div class="agentes-relacionados" ng-controller="RelatedAgentsController">
-    <div class="widget textright" ng-if="isEditable">
+    <div class="widget" ng-if="isEditable">
         <edit-box id="new-related-agent-group" position="left" title="Adicionar grupo de agentes" cancel-label="Cancelar" submit-label="Criar" on-cancel="closeNewGroupEditBox" on-submit="createGroup">
             <input type="text" ng-model="data.newGroupName" placeholder="Nome do grupo de agentes"/>
         </edit-box>
