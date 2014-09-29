e114a96d4de327718528d044b87b8c4597c83364
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-09-29 20:11:17 -0300

    themes in progress

---
+++ src/protected/application/themes/Base/Theme.php
@@ -0,0 +1,53 @@
+<?php
+namespace MapasCulturais\Themes\Base;
+use MapasCulturais\App;
+
+abstract class Theme{
+
+    protected $jsObject = array();
+
+    public function __construct() {
+        $app = App::i();
+        $this->jsObject['baseURL'] = $app->baseUrl;
+        $this->jsObject['assetURL'] = $app->assetUrl;
+    }
+
+    function isEditable(){
+        return (bool) preg_match('#^\w+/(create|edit)$#', App::i()->view->template);
+    }
+
+    function bodyBegin(){
+        App::i()->applyHook('mapasculturais.body:before');
+    }
+
+    function bodyEnd(){
+        App::i()->applyHook('mapasculturais.body:after');
+    }
+
+    function bodyProperties(){
+        $app = App::i();
+
+        $body_properties = array();
+
+        foreach ($app->view->bodyProperties as $key => $val)
+            $body_properties[] = "{$key}=\"$val\"";
+
+        $body_properties[] = 'class="' . implode(' ', $app->view->bodyClasses->getArrayCopy()) . '"';
+
+        echo implode(' ', $body_properties);;
+    }
+
+    function head(){
+        $app = App::i();
+
+        $app->applyHook('mapasculturais.head');
+
+        $app->printStyles('vendor');
+        $app->printStyles('fonts');
+        $app->printStyles('app');
+        $app->printScripts('vendor');
+        $app->printScripts('app');
+
+        $app->applyHook('mapasculturais.scripts');
+    }
+}
