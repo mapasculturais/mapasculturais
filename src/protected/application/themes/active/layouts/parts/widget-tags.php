efe3fae0fc6c7d474b0151f4c150c7dec69fcf16
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-09-30 13:23:16 -0300

    Fixes tag widget in singles for linking with search by keyword (refs #418)

--- src/protected/application/themes/active/layouts/parts/widget-tags.php
+++ src/protected/application/themes/active/layouts/parts/widget-tags.php
@@ -1,11 +1,19 @@
-<?php if(is_editable() || !empty($entity->terms['tag'])): ?>
+<?php
+$entityClass = get_class($entity);
+$entityName = strtolower(array_slice(explode('\\', $entityClass),-1)[0]);
+$tags = $entity->terms['tag'];
+?>
+<?php if(is_editable() || !empty($tags)): ?>
     <div class="widget">
         <h3>Tags</h3>
         <?php if(is_editable()): ?>
-            <span class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
+            <span id="term-area" class="js-editable-taxonomy" data-original-title="Tags" data-emptytext="Insira tags" data-restrict="true" data-taxonomy="tag"><?php echo implode('; ', $entity->terms['tag'])?></span>
         <?php else: ?>
-            <?php foreach($entity->terms['tag'] as $i => $term): ?>
-                <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search')?>#taxonomies[tags][]=<?php echo $term ?>"><?php echo $term ?></a>
+            <?php
+            foreach($tags as $i => $t): ?>
+                <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search') ?>##(<?php echo $entityName ?>:(keyword:'<?php echo $t ?>'),global:(enabled:(<?php echo $entityName ?>:!t),filterEntity:<?php echo $entityName ?>))">
+                    <?php echo $t ?>
+                </a>
             <?php endforeach; ?>
         <?php endif;?>
     </div>
