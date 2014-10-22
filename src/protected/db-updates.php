6f7eaaaaa238f0163b0cb6c35eed44e50cf4a423
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-10-22 21:40:00 -0200

    reorder db-updates

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -6,6 +6,89 @@ $em = $app->em;
 $conn = $em->getConnection();
 
 return array(
+
+
+    'create table geo_division' => function() use($conn){
+        echo 'creating geo_division_id_seq';
+        $conn->executeQuery("
+            CREATE SEQUENCE geo_division_id_seq
+                START WITH 1
+                INCREMENT BY 1
+                NO MINVALUE
+                NO MAXVALUE
+                CACHE 1;
+        ");
+
+        echo 'creating table geo_division';
+        $conn->executeQuery("
+            CREATE TABLE geo_division (
+                id integer DEFAULT nextval('geo_division_id_seq'::regclass) NOT NULL,
+                parent_id integer,
+                type character varying(32) NOT NULL,
+                cod character varying(32),
+                name character varying(128) NOT NULL,
+                geom geometry,
+                CONSTRAINT enforce_dims_geom CHECK ((st_ndims(geom) = 2)),
+                CONSTRAINT enforce_geotype_geom CHECK (((geometrytype(geom) = 'MULTIPOLYGON'::text) OR (geom IS NULL))),
+                CONSTRAINT enforce_srid_geom CHECK ((st_srid(geom) = 4326))
+            );
+        ");
+
+        echo 'creating geo_division primary key';
+        $conn->executeQuery("
+            ALTER TABLE ONLY geo_division
+                ADD CONSTRAINT geo_divisions_pkey PRIMARY KEY (id);
+        ");
+
+        echo 'creating index geo_division';
+        $conn->executeQuery("CREATE INDEX geo_divisions_geom_idx ON geo_division USING gist (geom);");
+
+    },
+
+    'migrate sp geo data to geo_division table - teste 1' => function () use($conn) {
+        foreach($conn->fetchAll("SELECT * FROM sp_regiao ORDER BY cod_reg_8 ASC") as $item){
+            $cod  = addslashes("zona-" . $item['cod_reg_8']);
+            $name = addslashes($item['nome']);
+            $geom = addslashes($item['the_geom']);
+
+            echo "\n zona $name\n";
+
+            $conn->executeQuery("
+                INSERT INTO geo_division
+                    ( type, cod, name, geom )
+                VALUES
+                    ( 'zona', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
+        }
+
+        foreach($conn->fetchAll("SELECT * FROM sp_subprefeitura") as $item){
+            $cod  = addslashes("subprefeitura-" . $item['cod_subpre']);
+            $name = addslashes($item['nome']);
+            $geom = addslashes($item['the_geom']);
+
+            echo "\n inserindo subprefeitura $name\n";
+
+            $conn->executeQuery("
+                INSERT INTO geo_division
+                    ( type, cod, name, geom )
+                VALUES
+                    ( 'subprefeitura', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
+        }
+
+
+        foreach($conn->fetchAll("SELECT * FROM sp_distrito") as $item){
+            $cod  = addslashes("distrito-" . $item['cod_distri']);
+            $name = addslashes($item['nome_distr']);
+            $geom = addslashes($item['the_geom']);
+
+            echo "\n inserindo distrito $name\n";
+            $conn->executeQuery("
+                INSERT INTO geo_division
+                    ( type, cod, name, geom )
+                VALUES
+                    ( 'distrito', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
+        }
+    },
+
     'alter table space add column public' => function() use ($conn){
         $conn->executeQuery('ALTER TABLE space ADD COLUMN public BOOLEAN NOT NULL DEFAULT false;');
     },
@@ -376,87 +459,6 @@ return array(
             \$BODY$;
             ");
 
-    },
-
-    'create table geo_division' => function() use($conn){
-        echo 'creating geo_division_id_seq';
-        $conn->executeQuery("
-            CREATE SEQUENCE geo_division_id_seq
-                START WITH 1
-                INCREMENT BY 1
-                NO MINVALUE
-                NO MAXVALUE
-                CACHE 1;
-        ");
-        
-        echo 'creating table geo_division';
-        $conn->executeQuery("
-            CREATE TABLE geo_division (
-                id integer DEFAULT nextval('geo_division_id_seq'::regclass) NOT NULL,
-                parent_id integer,
-                type character varying(32) NOT NULL,
-                cod character varying(32),
-                name character varying(128) NOT NULL,
-                geom geometry,
-                CONSTRAINT enforce_dims_geom CHECK ((st_ndims(geom) = 2)),
-                CONSTRAINT enforce_geotype_geom CHECK (((geometrytype(geom) = 'MULTIPOLYGON'::text) OR (geom IS NULL))),
-                CONSTRAINT enforce_srid_geom CHECK ((st_srid(geom) = 4326))
-            );
-        ");
-
-        echo 'creating geo_division primary key';
-        $conn->executeQuery("
-            ALTER TABLE ONLY geo_division
-                ADD CONSTRAINT geo_divisions_pkey PRIMARY KEY (id);
-        ");
-
-        echo 'creating index geo_division';
-        $conn->executeQuery("CREATE INDEX geo_divisions_geom_idx ON geo_division USING gist (geom);");
-
-    },
-
-    'migrate sp geo data to geo_division table - teste 1' => function () use($conn) {
-        foreach($conn->fetchAll("SELECT * FROM sp_regiao ORDER BY cod_reg_8 ASC") as $item){
-            $cod  = addslashes("zona-" . $item['cod_reg_8']);
-            $name = addslashes($item['nome']);
-            $geom = addslashes($item['the_geom']);
-            
-            echo "\n zona $name\n";
-            
-            $conn->executeQuery("
-                INSERT INTO geo_division 
-                    ( type, cod, name, geom ) 
-                VALUES 
-                    ( 'zona', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
-        }
-
-        foreach($conn->fetchAll("SELECT * FROM sp_subprefeitura") as $item){
-            $cod  = addslashes("subprefeitura-" . $item['cod_subpre']);
-            $name = addslashes($item['nome']);
-            $geom = addslashes($item['the_geom']);
-            
-            echo "\n inserindo subprefeitura $name\n";
-            
-            $conn->executeQuery("
-                INSERT INTO geo_division 
-                    ( type, cod, name, geom ) 
-                VALUES 
-                    ( 'subprefeitura', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
-        }  
-
-
-        foreach($conn->fetchAll("SELECT * FROM sp_distrito") as $item){
-            $cod  = addslashes("distrito-" . $item['cod_distri']);
-            $name = addslashes($item['nome_distr']);
-            $geom = addslashes($item['the_geom']);
-            
-            echo "\n inserindo distrito $name\n";
-            $conn->executeQuery("
-                INSERT INTO geo_division 
-                    ( type, cod, name, geom ) 
-                VALUES 
-                    ( 'distrito', :cod, :name, :geom );", array('cod' => $cod, 'name' => $name, 'geom' => $geom));
-        }
     }
 
 );
