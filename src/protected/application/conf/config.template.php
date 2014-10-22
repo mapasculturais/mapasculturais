1c7d0530684b94660c7e38a4b3d804ca28e21a87
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-10-22 14:13:43 -0200

    new config.template

--- src/protected/application/conf/config.template.php
+++ src/protected/application/conf/config.template.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -8,26 +7,57 @@ $config = include 'conf-base.php';
 // devel
 return array_merge($config,
     array(
+        'namespaces' => array_merge( $config['namespaces'], array('SpCultura' => THEMES_PATH . 'SpCultura') ),
+        'themes.active' => 'SpCultura',
+        'themes.assetManager' => new \MapasCulturais\AssetManagers\FileSystem(array(
+            'publishPath' => BASE_PATH . 'pub/',
+
+            'mergeScripts' => false,
+            'mergeStyles' => true,
+
+            'process.js' => 'uglifyjs {IN} -o {OUT} --source-map {OUT}.map --source-map-include-sources --source-map-url /pub/{FILENAME}.map -b -p 7',
+            'process.css' => 'uglifycss {IN} > {OUT}',
+            'publishFolderCommand' => 'ln -s -f {IN} {PUBLISH_PATH}'
+
+        )),
+        'base.assetUrl' => 'http://localhost:8000/pub/',
+
         'base.url' => 'http://localhost:8000/',
-        // development, staging, production
-        'app.mode' => 'development',
 
-        'doctrine.isDev' => true,
+        'app.mode' => 'development', // development, staging, production
 
-        // 'slim.middlewares' => array( new \MapasCulturais\Middlewares\ExecutionTime(true, false) ),
+        'doctrine.isDev' => false,
+        'slim.debug' => true,
+        'slim.middlewares' => array(
+            new MapasCulturais\Middlewares\ErrorHandler(function(){ return 'mapasculturais.log'; }),
+
+            new MapasCulturais\Middlewares\ExecutionTime(true, false)
+        ),
+
+        'app.geoDivisionsHierarchy' => array(
+            'zona' => 'Zona',
+            'subprefeitura' => 'Subprefeitura',
+            'distrito' => 'Distrito'
+        ),
+        'maps.center' => array(-23.54894, -46.63882), // são paulo
 
         // logs
+        'slim.log.writer' => new \MapasCulturais\Loggers\File(function(){ return 'mapasculturais.log'; }),
+        'app.log.path' => '/var/log/mapasculturais/',
+
         'slim.log.level' => \Slim\Log::DEBUG,
         'slim.log.enabled' => true,
 
-        'app.log.hook' => true,
+        'app.log.hook' => false,
         'app.log.query' => false,
         'app.log.requestData' => false,
-        'app.log.translations' => false,
-        'app.log.apiCache' => true,
+        'app.log.translations' => true,
+        'app.log.apiCache' => false,
 
         // cache
-        'app.useApiCache' => false,
+        'app.useAssetsUrlCache' => false,
+
+        'app.useApiCache' => true,
         'app.apiCache.lifetime' => 10 * 60,
 
         'app.useTranslationsCache' => true,
@@ -35,16 +65,22 @@ return array_merge($config,
         'app.useRegisterCache' => true,
         'app.registerCache.lifeTime' => 10 * 60,
 
-        'app.cache' => new \Doctrine\Common\Cache\ArrayCache(),
+        'plugins.enabled' => array('em-cartaz', 'agenda-singles'),
 
-        
+        /*
+        'auth.provider' => 'OpauthOpenId',
+        'auth.config' => array(
+            'login_url' => 'http://localhost:9000/',
+            'logout_url' => 'http://localhost:9000/accounts/logout/',
+            'salt' => 'LT_SECURITY_SALT_SECURITY_SALT_SECURITY_SALT_SECURITY_SALT_SECU',
+            'timeout' => '24 hours'
+        ),
+        // */
+
+        //*
         'auth.provider' => 'Fake',
         'auth.config' => array(),
+        // */
 
-
-        'plugins.enabled' => array(
-            'agenda-singles',
-            'endereco'
-        )
     )
 );
