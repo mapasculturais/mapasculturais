70ebec7f561eb13791ec8170c30e161d502e6680
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-11-06 19:15:33 -0200

    fix ORM mapping and trait usage

--- src/protected/application/lib/MapasCulturais/Entities/RegistrationFileConfiguration.php
+++ src/protected/application/lib/MapasCulturais/Entities/RegistrationFileConfiguration.php
@@ -4,6 +4,7 @@ namespace MapasCulturais\Entities;
 
 use Doctrine\ORM\Mapping as ORM;
 use MapasCulturais\App;
+use MapasCulturais\Traits;
 
 /**
  * RegistrationMeta
@@ -31,8 +32,6 @@ class RegistrationFileConfiguration extends \MapasCulturais\Entity {
     /**
      * @var \MapasCulturais\Entities\Project
      *
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
      * @ORM\OneToOne(targetEntity="MapasCulturais\Entities\Registration")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="project_id", referencedColumnName="id")
