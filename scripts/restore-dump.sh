5f8739f375f3d50048e2f52bd948b795b0986f49
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2014-11-07 13:14:42 -0200

    ask for confirmation in restore-dump.sh

--- scripts/restore-dump.sh
+++ scripts/restore-dump.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-sudo -u postgres dropdb mapasculturais
-sudo -u postgres psql < ../db/dump.sql
+echo "
+==========================================================================================
+=================================== ATENÇÃO - CUIDADO ====================================
+==========================================================================================
+ESTA OPERAÇÃO APAGARAÁ O BANCO DE DADOS ATUAL E RECUPERARÁ A VERSÃO DO ARQUIVO db/dump.sql
+
+
+PARA CONTINUAR ESCREVA 'sim': "
+read confirm
+
+if [[ $confirm == 'sim' ]]; then
+	sudo -u postgres dropdb mapasculturais
+	sudo -u postgres psql < ../db/dump.sql
+fi;
+
