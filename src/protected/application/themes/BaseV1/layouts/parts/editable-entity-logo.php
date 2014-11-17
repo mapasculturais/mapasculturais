a31718529b66a015c537241a37cc70c152ef6720
Ariadne Melo <ariadne@hacklab.com.br>
2014-11-17 18:03:19 -0200

    create editable-entity-logo part

---
+++ src/protected/application/themes/BaseV1/layouts/parts/editable-entity-logo.php
@@ -0,0 +1,1 @@
+<h1 id="logo-peq"><a href="<?php echo $app->getBaseUrl() ?>">Mapas Culturais</a></h1>
---
+++ src/protected/application/themes/SpCultura/layouts/parts/editable-entity-logo.php
@@ -0,0 +1,1 @@
+<h1 id="logo-spcultura-peq"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-spcultura.png'); ?>" /></a></h1>
