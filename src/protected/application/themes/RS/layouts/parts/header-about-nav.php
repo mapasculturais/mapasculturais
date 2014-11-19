35fd39704b89cb4efc059d1e003013e095326cac
Ariadne Melo <ariadne@hacklab.com.br>
2014-11-19 15:50:32 -0200

    customize theme header, colors variables and font variables

---
+++ src/protected/application/themes/RS/layouts/parts/header-about-nav.php
@@ -0,0 +1,7 @@
+<nav id="about-nav" class="alignright clearfix">
+    <ul id="menu-secundario">
+        <li><a href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>">Sobre o Cultura RS</a></li>
+        <li><a href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>">Como usar</a></li>
+    </ul>
+    <h1 id="logo-smc"><a href="http://www.prefeitura.sp.gov.br" target="_blank"><img src="<?php $this->asset('img/logo-governors.png'); ?>" /></a></h1>
+</nav>
