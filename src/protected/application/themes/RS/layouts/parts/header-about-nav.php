b87afdaf28a485b6a40cb4a9a06d466800333c7a
Ariadne Melo <ariadne@hacklab.com.br>
2014-11-20 16:14:32 -0200

    fix governo do rs link url on header

--- src/protected/application/themes/RS/layouts/parts/header-about-nav.php
+++ src/protected/application/themes/RS/layouts/parts/header-about-nav.php
@@ -3,5 +3,5 @@
         <li><a href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>">Sobre o Cultura RS</a></li>
         <li><a href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>">Como usar</a></li>
     </ul>
-    <h1 id="logo-smc"><a href="http://www.prefeitura.sp.gov.br" target="_blank"><img src="<?php $this->asset('img/logo-governors.png'); ?>" /></a></h1>
+    <h1 id="logo-smc"><a href="http://www.rs.gov.br/" target="_blank"><img src="<?php $this->asset('img/logo-governors.png'); ?>" /></a></h1>
 </nav>
