95f3e17ebb548eed6018664e57542fd2fc9b943f
Ariadne Melo <ariadne@hacklab.com.br>
2014-11-20 16:42:40 -0200

    fix class name from editable entity logo

--- src/protected/application/themes/RS/layouts/parts/editable-entity-logo.php
+++ src/protected/application/themes/RS/layouts/parts/editable-entity-logo.php
@@ -1,1 +1,1 @@
-<h1 id="logo-peq"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-culturars.png'); ?>" /></a></h1>
+<h1 id="small-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-culturars.png'); ?>" /></a></h1>
