697f2640c1cd1ee92cd4f74bb168e89b0952abeb
Cátia Kitahara <catia@hacklab.com.br>
2014-11-24 15:55:46 -0200

    delete inline style and refactor attachments html

--- src/protected/application/themes/BaseV1/views/project/single.php
+++ src/protected/application/themes/BaseV1/views/project/single.php
@@ -199,74 +199,49 @@ $this->includeAngularEntityAssets($entity);
                 <?php endforeach; ?>
             </div>
             <!-- #registration-agent-relations -->
-            <style>
-                .attachment-list-item .enviar:before{
-                    display: inline-block;
-                    width: 1rem;
-                    height: 1rem;
-                    font-size: 1rem;
-                    font-family: 'ElegantIcons';
-                    vertical-align: top;
-                    text-transform: none;
-                    font-weight: normal;
-                    font-style: none;
-
-                    content: "\e091";
-                }
-
-            </style>
             <div id="registration-attachments" class="registration-fieldset">
-
                 <h4>5. Anexos</h4>
                 <p class="registration-help">Você pode pedir para os proponentes enviarem anexos para se inscrever no seu projeto. Para cada anexo, você pode fornecer um modelo, que o proponente poderá baixar, preencher, e fazer o upload novamente.</p>
-                <!-- da parte downloads.php -->
 
                 <div ng-controller="RegistrationFileConfigurationsController">
                     <?php if($this->controller->action == 'create'): ?>
-                        <p>Para adicionar anexos, primeiro é preciso salvar o projeto.</p>
+                        <p class="allert warning">Antes de adicionar anexos é preciso salvar o projeto.</p>
                     <?php else: ?>
-                        <p><a class="botao adicionar" title="" ng-click="editbox.open('editbox-registration-files', $event)">Adicionar Anexo</a></p>
+                        <p><a class="botao adicionar" title="" ng-click="editbox.open('editbox-registration-files', $event)">adicionar anexo</a></p>
                     <?php endif; ?>
-
+                    <!-- edit-box to add attachment -->
                     <edit-box id="editbox-registration-files" position="bottom" title="Adicionar Anexo" cancel-label="Cancelar" submit-label="Criar" close-on-cancel='true' on-cancel="closeNewFileConfigurationEditBox" on-submit="createFileConfiguration" spinner-condition="data.uploadSpinner">
                         <input type="text" ng-model="data.newFileConfiguration.title" placeholder="Nome do anexo"/>
                         <textarea ng-model="data.newFileConfiguration.description" placeholder="Descrição do anexo"/></textarea>
                         <p><label><input type="checkbox" ng-model="data.newFileConfiguration.required">  É obrigatório o envio deste anexo para se inscrever neste projeto</label></p>
                     </edit-box>
-
+                    <!-- added attachments list -->
                     <ul class="attachment-list">
                         <li ng-repeat="fileConfiguration in data.fileConfigurations" on-repeat-done="init-ajax-uploaders" id="registration-file-{{fileConfiguration.id}}" class="attachment-list-item">
-                            <div class="label">{{fileConfiguration.title}}</div>
-                            <div>
+                            <div class="mc-editable js-open-editbox" ng-click="openFileConfigurationEditBox(fileConfiguration.id, $index, $event);">
+                                <div class="label">{{fileConfiguration.title}}</div>
                                 <span class="attachment-description">{{fileConfiguration.description}} - {{fileConfiguration.required ? 'Obrigatório' : 'Opcional'}}</span>
-                                <a class="editar js-open-editbox hltip" ng-click="openFileConfigurationEditBox(fileConfiguration.id, $index, $event);" title="editar"></a>
                             </div>
+                            <!-- edit-box to edit attachment -->
                             <edit-box id="editbox-registration-files-{{fileConfiguration.id}}" position="bottom" title="Editar Anexo" cancel-label="Cancelar" submit-label="Salvar" close-on-cancel='true' on-cancel="cancelFileConfigurationEditBox" on-submit="editFileConfiguration" index="{{$index}}" spinner-condition="data.uploadSpinner">
                                 <input type="text" ng-model="fileConfiguration.title" placeholder="Nome do anexo"/>
                                 <textarea ng-model="fileConfiguration.description" placeholder="Descrição do anexo"/></textarea>
                                 <p><label><input type="checkbox" ng-model="fileConfiguration.required">  É obrigatório o envio deste anexo para se inscrever neste projeto</label></p>
                             </edit-box>
-
-                            <div class="file-{{fileConfiguration.template.id}}">
-                                <div class="label">Modelo</div>
-                                <div ng-if="fileConfiguration.template">
-                                    <a ng-href="{{fileConfiguration.template.url}}" target="_blank">{{fileConfiguration.template.name}}</a>
-                                    <a class="js-open-editbox editar hltip" ng-click="openFileConfigurationTemplateEditBox(fileConfiguration.id, $index, $event);" title="editar modelo"></a>
-                                    <a class="excluir hltip" ng-click="removeFileConfigurationTemplate(fileConfiguration.id, $index)" hltitle="excluir modelo"></a>
-                                </div>
-
-                                <a ng-if="!fileConfiguration.template" class="botao enviar hltip" title="enviar modelo">enviar</a>
+                            <!-- attachment upload -->
+                            <div class="file-{{fileConfiguration.template.id}}" ng-if="fileConfiguration.template">
+                                <span class="js-open-editbox mc-editable attachment-title" ng-click="openFileConfigurationTemplateEditBox(fileConfiguration.id, $index, $event);">{{fileConfiguration.template.name}}</span>
+                                <a class="excluir hltip" ng-click="removeFileConfigurationTemplate(fileConfiguration.id, $index)" hltitle="excluir modelo"></a>
                             </div>
 
-                            <edit-box id="editbox-registration-files-template-{{fileConfiguration.id}}" position="bottom" title="Editar Anexo" cancel-label="Cancelar" close-on-cancel='true' spinner-condition="data.uploadSpinner">
-                                <p class="file-{{fileConfiguration.template.id}}" ng-if="fileConfiguration.template" >
-                                    <a href="{{fileConfiguration.template.url}}">Modelo - {{fileConfiguration.template.name}}</a>
+                            <edit-box id="editbox-registration-files-template-{{fileConfiguration.id}}" position="bottom" title="Enviar modelo" cancel-label="Cancelar" close-on-cancel='true' spinner-condition="data.uploadSpinner">
+                                <p ng-if="fileConfiguration.template">
+                                    <a class="file-{{fileConfiguration.template.id}} attachment-template"  href="{{fileConfiguration.template.url}}" target="_blank">{{fileConfiguration.template.name}}</a>
                                 </p>
                                 <form class="js-ajax-upload" method="post" data-group="{{uploadFileGroup}}" action="{{getUploadUrl(fileConfiguration.id)}}" enctype="multipart/form-data">
                                     <div class="alert danger escondido"></div>
                                     <p class="form-help">Tamanho máximo do arquivo: {{maxUploadSizeFormatted}}</p>
                                     <input type="file" name="{{uploadFileGroup}}" />
-                                    <input type="submit" value="Enviar Modelo">
 
                                     <div class="js-ajax-upload-progress">
                                         <div class="progress">
@@ -274,10 +249,12 @@ $this->includeAngularEntityAssets($entity);
                                             <div class="percent">0%</div >
                                         </div>
                                     </div>
+                                    <input type="submit" value="enviar">
                                 </form>
 
                             </edit-box>
                             <div class="btn-group">
+                                <a class="botao enviar hltip" title="enviar modelo" ng-if="!fileConfiguration.template" ng-click="openFileConfigurationTemplateEditBox(fileConfiguration.id, $index, $event);" >enviar modelo</a>
                                 <a data-href="{{fileConfiguration.deleteUrl}}" ng-click="removeFileConfiguration(fileConfiguration.id, $index)" class="botao excluir hltip" title="excluir anexo">excluir</a>
                             </div>
                         </li>
