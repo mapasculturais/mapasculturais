98f10d8280c869c581a2c9337c355d9dcf6a0861
Gutierri Barboza <eugutierri@gmail.com>
2014-12-04 17:56:42 -0200

    Changes in the variable text

--- src/protected/application/themes/SaoJose/Theme.php
+++ src/protected/application/themes/SaoJose/Theme.php
@@ -13,9 +13,11 @@ class Theme extends BaseV1\Theme{
             'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
             'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
         
-            'home: abbreviation' => "SMC",
+            'home: abbreviation' => "FCCR",
             'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Lugares da Cultura. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Lugares da Cultura é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
             
+            'home: colabore' => "Colabore com o Lugares da Cultura",
+
             'search: verified results' => 'Resultados da SMC'
         );
     }
