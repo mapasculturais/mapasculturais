6567521519a0623ce416dbd3c8b6aca7f65d14a8
Gutierri Barboza <eugutierri@gmail.com>
2014-12-04 18:46:48 -0200

    add logo and edition of the texts home

--- src/protected/application/themes/SaoJose/Theme.php
+++ src/protected/application/themes/SaoJose/Theme.php
@@ -7,7 +7,7 @@ class Theme extends BaseV1\Theme{
 
     protected static function _getTexts(){
         return array(
-            'home: welcome' => "O Mapas Culturais é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
+            'home: welcome' => "O Lugares da Cultura é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
             'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
             'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural paulistana. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
             'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
@@ -18,7 +18,7 @@ class Theme extends BaseV1\Theme{
             
             'home: colabore' => "Colabore com o Lugares da Cultura",
 
-            'search: verified results' => 'Resultados da SMC'
+            'search: verified results' => 'Resultados da FCCR'
         );
     }
 
