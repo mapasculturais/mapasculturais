18be244026153e31b6996c631a73e4ab3382805d
Cátia Kitahara <catia@hacklab.com.br>
2014-12-12 22:22:16 -0200

    start main header responsive styles rename classes in English refactor css

--- src/protected/application/themes/BaseV1/layouts/parts/header-main-nav.php
+++ src/protected/application/themes/BaseV1/layouts/parts/header-main-nav.php
@@ -1,83 +1,35 @@
-<nav id="main-nav" class="alignright clearfix">
-    <ul class="menu abas-objetos clearfix">
-        <li id="aba-eventos" ng-class="{'active':data.global.filterEntity === 'event'}" ng-click="tabClick('event')">
+<nav id="main-nav" class="clearfix">
+    <ul class="menu entities-menu clearfix">
+        <li id="entities-menu-event" ng-class="{'active':data.global.filterEntity === 'event'}" ng-click="tabClick('event')">
             <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(event:!t),filterEntity:event))'; ?>">
                 <div class="icone icon_calendar"></div>
-                <div>Eventos</div>
+                <div class="entity-item-label">Eventos</div>
             </a>
         </li>
-        <li id="aba-espacos" ng-class="{'active':data.global.filterEntity === 'space'}" ng-click="tabClick('space')">
+        <li id="entities-menu-space" ng-class="{'active':data.global.filterEntity === 'space'}" ng-click="tabClick('space')">
             <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(space:!t),filterEntity:space))'; ?>">
                 <div class="icone icon_building"></div>
-                <div>Espaços</div>
+                <div class="entity-item-label">Espaços</div>
             </a>
         </li>
-        <li id="aba-agentes" ng-class="{'active':data.global.filterEntity === 'agent'}" ng-click="tabClick('agent')">
+        <li id="entities-menu-agent" ng-class="{'active':data.global.filterEntity === 'agent'}" ng-click="tabClick('agent')">
             <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(agent:!t),filterEntity:agent))'; ?>">
                 <div class="icone icon_profile"></div>
-                <div>Agentes</div>
+                <div class="entity-item-label">Agentes</div>
             </a>
         </li>
-        <li id="aba-projetos"  ng-class="{'active':data.global.filterEntity === 'project'}" ng-click="tabClick('project')">
+        <li id="entities-menu-project"  ng-class="{'active':data.global.filterEntity === 'project'}" ng-click="tabClick('project')">
             <a href="<?php if ($this->controller->action !== 'search') echo $app->createUrl('busca') . '##(global:(enabled:(project:!t),filterEntity:project,viewMode:list))'; ?>">
                 <div class="icone icon_document_alt"></div>
-                <div>Projetos</div>
+                <div class="entity-item-label">Projetos</div>
             </a>
         </li>
     </ul>
     <!--.menu.abas-objetos-->
-    <ul class="menu logado clearfix">
+    <ul class="menu session clearfix">
         <?php if ($app->auth->isUserAuthenticated()): ?>
-
-            <li class="notificacoes" ng-controller="NotificationController" ng-hide="data.length == 0">
-
-                <a>
-                    <div class="icone icon_comment"></div>
-                    <div>Notificações</div>
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <div class="setinha"></div>
-                        <div class="clearfix">
-                            <h6 class="alignleft">Notificações</h6>
-                            <a href="#" style="display:none" class="staging-hidden hltip icone icon_check_alt" title="Marcar todas como lidas"></a>
-                        </div>
-                        <ul>
-                            <li ng-repeat="notification in data" on-last-repeat="adjustScroll();">
-                                <p class="notificacao clearfix">
-                                    <span ng-bind-html="notification.message"></span>
-                                    <br>
-
-                                    <a ng-if="notification.request.permissionTo.approve" class="btn btn-small btn-success" ng-click="approve(notification.id)">aceitar</a>
-
-                                    <span ng-if="notification.request.permissionTo.reject">
-                                        <span ng-if="notification.request.requesterUser.id === MapasCulturais.userId">
-                                            <a class="btn btn-small btn-default" ng-click="reject(notification.id)">cancelar</a>
-                                            <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
-                                        </span>
-                                        <span ng-if="notification.request.requesterUser.id !== MapasCulturais.userId">
-                                            <a class="btn btn-small btn-danger" ng-click="reject(notification.id)">rejeitar</a>
-                                        </span>
-                                    </span>
-
-                                    <span ng-if="!notification.isRequest">
-                                        <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
-                                    </span>
-
-                                </p>
-                            </li>
-                        </ul>
-                        <a href="<?php echo $app->createUrl('panel'); ?>">
-                            Ver todas atividades
-                        </a>
-                    </li>
-                </ul>
-                <!--.submenu-->
-            </li>
-            <!--.notificacoes-->
-
-            <li class="usuario">
-                <a href="<?php echo $app->createUrl('panel'); ?>">
+            <li class="user">
+                <a href="#" ng-click="userSubmenu = !userSubmenu">
                     <div class="avatar">
                         <?php if ($app->user->profile->avatar): ?>
                             <img src="<?php echo $app->user->profile->avatar->transform('avatarSmall')->url; ?>" />
@@ -86,8 +38,7 @@
                         <?php endif; ?>
                     </div>
                 </a>
-                <ul class="submenu">
-                    <div class="setinha"></div>
+                <ul class="submenu" ng-show="userSubmenu">
                     <li>
                         <a href="<?php echo $app->createUrl('panel'); ?>">Painel</a>
                     </li>
@@ -118,8 +69,53 @@
                 </ul>
             </li>
             <!--.usuario-->
+            <li class="notifications" ng-controller="NotificationController" ng-hide="data.length == 0">
+
+                <a ng-click="notificationsSubmenu = !notificationsSubmenu">
+                    <div class="icone icon_comment"></div>
+                    <div>Notificações</div>
+                </a>
+                <ul class="submenu" ng-show="notificationsSubmenu">
+                    <li>
+                        <div class="clearfix">
+                            <h6 class="alignleft">Notificações</h6>
+                            <a href="#" style="display:none" class="staging-hidden hltip icone icon_check_alt" title="Marcar todas como lidas"></a>
+                        </div>
+                        <ul>
+                            <li ng-repeat="notification in data" on-last-repeat="adjustScroll();">
+                                <p class="notification clearfix">
+                                    <span ng-bind-html="notification.message"></span>
+                                    <br>
+
+                                    <a ng-if="notification.request.permissionTo.approve" class="btn btn-small btn-success" ng-click="approve(notification.id)">aceitar</a>
+
+                                    <span ng-if="notification.request.permissionTo.reject">
+                                        <span ng-if="notification.request.requesterUser.id === MapasCulturais.userId">
+                                            <a class="btn btn-small btn-default" ng-click="reject(notification.id)">cancelar</a>
+                                            <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
+                                        </span>
+                                        <span ng-if="notification.request.requesterUser.id !== MapasCulturais.userId">
+                                            <a class="btn btn-small btn-danger" ng-click="reject(notification.id)">rejeitar</a>
+                                        </span>
+                                    </span>
+
+                                    <span ng-if="!notification.isRequest">
+                                        <a class="btn btn-small btn-success" ng-click="delete(notification.id)">ok</a>
+                                    </span>
+
+                                </p>
+                            </li>
+                        </ul>
+                        <a href="<?php echo $app->createUrl('panel'); ?>">
+                            Ver todas atividades
+                        </a>
+                    </li>
+                </ul>
+                <!--.submenu-->
+            </li>
+            <!--.notificacoes-->
         <?php else: ?>
-            <li class="entrar">
+            <li class="login">
                 <a href="<?php echo $app->createUrl('panel') ?>">
                     <div class="icone icon_lock"></div>
                     <div>Entrar</div>
