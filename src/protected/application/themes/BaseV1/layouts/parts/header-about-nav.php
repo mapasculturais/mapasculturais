9a8330da89675b37d67df1637c69a30539e37529
Cátia Kitahara <catia@hacklab.com.br>
2014-12-12 22:20:53 -0200

    replace links with icons and refactor html

--- src/protected/application/themes/BaseV1/layouts/parts/header-about-nav.php
+++ src/protected/application/themes/BaseV1/layouts/parts/header-about-nav.php
@@ -1,7 +1,7 @@
 <nav id="about-nav" class="alignright clearfix">
-    <ul id="menu-secundario">
-        <li><a href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>">Sobre o Mapas Culturais</a></li>
-        <li><a href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>">Como usar</a></li>
-    </ul>
     <h1 id="logo-smc"><a href="#"><img src="<?php $this->asset('img/marca-da-org.png'); ?>" /></a></h1>
+    <ul id="menu-secundario">
+        <li><a class="icone icon_info hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre os Mapas Culturais"></a></li>
+        <li><a class="icone icon_question_alt hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></li>
+    </ul>
 </nav>
--- src/protected/application/themes/SpCultura/layouts/parts/header-about-nav.php
+++ src/protected/application/themes/SpCultura/layouts/parts/header-about-nav.php
@@ -1,7 +1,7 @@
-<nav id="about-nav" class="alignright clearfix">
-    <ul id="menu-secundario">
-        <li><a href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>">Sobre o SP Cultura</a></li>
-        <li><a href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>">Como usar</a></li>
-    </ul>
+<nav id="about-nav" class="clearfix">
     <h1 id="logo-smc"><a href="http://www.prefeitura.sp.gov.br" target="_blank"><img src="<?php $this->asset('img/logo-prefeitura.png'); ?>" /></a></h1>
+    <ul id="menu-secundario">
+        <li><a class="icone icon_info hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre o SP Cultura"></a></li>
+        <li><a class="icone icon_question_alt hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></li>
+    </ul>
 </nav>
