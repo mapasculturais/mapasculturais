20242dfbb7eb49157f778fdae8f74efa5117537d
Cátia Kitahara <catia@hacklab.com.br>
2014-12-17 17:36:10 -0200

    more adjustments to print styles

--- src/protected/application/themes/BaseV1/views/registration/single.php
+++ src/protected/application/themes/BaseV1/views/registration/single.php
@@ -57,9 +57,9 @@ $this->includeAngularEntityAssets($entity);
             <!-- selecionar categoria -->
             <h4><?php echo $project->registrationCategTitle ?></h4>
             <!-- <p class="registration-help"><?php echo $project->registrationCategDescription ?></p> -->
-            <p>
-                <span class='js-editable-registrationCategory' data-original-title="Opção" data-emptytext="Selecione uma opção" data-value="<?php echo htmlentities($entity->category) ?>"><?php echo $entity->category ?></span>
-            </p>
+            <div>
+                <?php echo $entity->category ?>
+            </div>
         </div>
     <?php endif; ?>
     <div class="registration-fieldset">
