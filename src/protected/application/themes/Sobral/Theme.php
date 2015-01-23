08502f98c0aa587309b33c9650ddff7e559b7157
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-01-23 18:30:03 -0200

    page about of theme Sobral

--- src/protected/application/themes/Sobral/Theme.php
+++ src/protected/application/themes/Sobral/Theme.php
@@ -6,7 +6,14 @@ use MapasCulturais\App;
 class Theme extends BaseV1\Theme{
 
     protected static function _getTexts(){
+        $self = App::i()->view;
+        $url_search_agents = $self->searchAgentsUrl;
+        $url_search_spaces = $self->searchSpacesUrl;
+        $url_search_events = $self->searchEventsUrl;
+        $url_search_projects = $self->searchProjectsUrl;
+        
         return array(
+            'site: name' => 'Sobral Cultura',
             'site: in the region' => 'na cidade de Sobral',
             'site: of the region' => 'da cidade de Sobral',
             'site: owner' => 'Secretaria de Cultura e Turismo de Sobral',
@@ -14,7 +21,14 @@ class Theme extends BaseV1\Theme{
 
 //            'home: abbreviation' => "MC",
 //            'home: colabore' => "Colabore com o Mapas Culturais",
-//            'home: welcome' => "O Mapas Culturais é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
+            'home: welcome' => "Sobral Cultura é a plataforma livre, gratuita e colaborativa de
+mapeamento da Secretaria Municipal da Cultura e do Turismo de Sobral sobre o cenário
+cultural sobralense. Ficou mais fácil se programar para conhecer as opções culturais que a
+cidade oferece: shows musicais, espetáculos teatrais, exposições, museus, bens patrimoniais,
+artesanato, entre outros.  Além de conferir a agenda de eventos, você também pode colaborar
+na gestão da cultura da cidade: basta criar seu perfil de <a href=\"$url_search_agents\" >agente cultural</a>. A partir deste
+cadastro, fica mais fácil participar dos editais de fomento à cultura da Prefeitura e também
+divulgar seus <a href=\"{$url_search_events}\">eventos</a>, <a href=\"{$url_search_spaces}\">espaços</a> ou <a href=\"$url_search_projects\">projetos</a>.",
 //            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
 //            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural paulistana. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
 //            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
