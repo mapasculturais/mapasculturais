ab0caf503991b46b3d73545eddb93fa8bc55afee
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-01-27 21:56:18 -0200

    Registrations: Removes link from anchor if user can't publish results (#672)

--- src/protected/application/themes/BaseV1/views/project/single.php
+++ src/protected/application/themes/BaseV1/views/project/single.php
@@ -491,7 +491,11 @@ $this->includeAngularEntityAssets($entity);
                 </div>
                 <?php else: ?>
                 <div class="clearfix">
-                    <a id="btn-publish-results" class="btn btn-primary <?php if(!$entity->canUser('publishRegistrations')) echo 'disabled hltip'; ?>" <?php if(!$entity->canUser('publishRegistrations')) echo 'title="Você só pode publicar a lista de aprovados após o término do período de inscrições."'; ?> href="<?php echo $app->createUrl('project', 'publish', [$entity->id]) ?>">Publicar resultados</a>
+                    <?php if($entity->canUser('publishRegistrations')): ?>
+                        <a id="btn-publish-results" class="btn btn-primary" href="<?php echo $app->createUrl('project', 'publish', [$entity->id]) ?>">Publicar resultados</a>
+                    <?php else: ?>
+                        <a id="btn-publish-results" class="btn btn-primary disabled hltip" title="Você só pode publicar a lista de aprovados após o término do período de inscrições.">Publicar resultados</a>
+                    <?php endif; ?>
                 </div>
                 <?php endif; ?>
             <?php endif; ?>
