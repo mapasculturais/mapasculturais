6625b003c4ef9c22355c1da0d571099623b2fa49
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-01-28 18:28:20 -0200

    Replaces word "paulistana" from BaseV1 an Sobral themes texts and now uses the owner abbreviation for Sobral

--- src/protected/application/themes/Sobral/Theme.php
+++ src/protected/application/themes/Sobral/Theme.php
@@ -11,7 +11,7 @@ class Theme extends BaseV1\Theme{
         $url_search_spaces = $self->searchSpacesUrl;
         $url_search_events = $self->searchEventsUrl;
         $url_search_projects = $self->searchProjectsUrl;
-        
+
         return array(
             'site: name' => 'Sobral Cultura',
             'site: in the region' => 'na cidade de Sobral',
@@ -19,7 +19,7 @@ class Theme extends BaseV1\Theme{
             'site: owner' => 'Secretaria de Cultura e Turismo de Sobral',
             'site: by the site owner' => 'pela Secretaria de Cultura e Turismo de Sobral',
 
-//            'home: abbreviation' => "MC",
+            'home: abbreviation' => "PMS",
 //            'home: colabore' => "Colabore com o Mapas Culturais",
             'home: welcome' => "Sobral Cultura é a plataforma livre, gratuita e colaborativa de
 mapeamento da Secretaria Municipal da Cultura e do Turismo de Sobral sobre o cenário
@@ -30,7 +30,7 @@ na gestão da cultura da cidade: basta criar seu perfil de <a href=\"$url_searc
 cadastro, fica mais fácil participar dos editais de fomento à cultura da Prefeitura e também
 divulgar seus <a href=\"{$url_search_events}\">eventos</a>, <a href=\"{$url_search_spaces}\">espaços</a> ou <a href=\"$url_search_projects\">projetos</a>.",
 //            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
-//            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural paulistana. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
+            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural sobralense. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
 //            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
 //            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
 //            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
