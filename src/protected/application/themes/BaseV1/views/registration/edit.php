f2a9eac9d069aa3afbec9b0d90ecff9e9c431981
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-02-03 16:19:46 -0200

    Shows full date in registration message informing it's not open anymore

--- src/protected/application/themes/BaseV1/views/registration/edit.php
+++ src/protected/application/themes/BaseV1/views/registration/edit.php
@@ -144,8 +144,13 @@ $this->includeAngularEntityAssets($entity);
         <?php else: ?>
             <p class="registration-help">
                 <strong>
-                As inscrições encerraram-se em <?php echo $entity->project->registrationTo->format('d/m/Y'); ?>
-                às <?php echo $entity->project->registrationTo->format('H:i'); ?>.
+                    <?php // gets full date in the format "26 de {January} de 2015 às 17:00" and uses App translation to replace english month name inside curly brackets to the equivalent in portuguese. It avoids requiring the operating system to have portuguese locale as used in this example: http://pt.stackoverflow.com/a/21642
+                    $date = strftime("%d de {%B} de %G às %H:%M", $entity->project->registrationTo->getTimestamp());
+                    $full_date = preg_replace_callback("/{(.*?)}/", function($matches) use ($app) {
+                        return strtolower($app::txt(str_replace(['{', '}'], ['',''], $matches[0]))); //removes curly brackets from the matched pattern and convert its content to lowercase
+                    }, $date);
+                    ?>
+                    As inscrições encerraram-se em <?php echo $full_date; ?>.
                 </strong>
             </p>
         <?php endif; ?>
