058ec96a5b05295bf6e1ab69a75bd14adf7dca00
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-04-22 14:27:46 -0300

    fix bootstrap.php to use a specified config file in  super global

--- src/protected/application/bootstrap.php
+++ src/protected/application/bootstrap.php
@@ -11,10 +11,12 @@ define('AUTOLOAD_TTL', 60 * 5);
 
 require_once __DIR__."/../vendor/autoload.php";
 
-if(isset($_ENV['MAPASCULTURAIS_CONFIG_FILE']))
+if(isset($_ENV['MAPASCULTURAIS_CONFIG_FILE'])){
     $config = include __DIR__.'/conf/' . $_ENV['MAPASCULTURAIS_CONFIG_FILE'];
-else
+}else if(isset($_SERVER['MAPASCULTURAIS_CONFIG_FILE'])){
+    $config = include __DIR__.'/conf/' . $_SERVER['MAPASCULTURAIS_CONFIG_FILE'];
+}else{
     $config = include __DIR__.'/conf/config.php';
-
+}
 // create the App instance
 $app = App::i()->init($config);
