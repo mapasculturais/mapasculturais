f21e6f9809fb8b584401b848982ce75afc36e451
Gutieri Barboza <eugutierri@gmail.com>
2015-04-24 16:01:07 -0300

    adjustment logo on header on theme SaoJose

--- src/protected/application/themes/SaoJose/layouts/parts/header-logo.php
+++ src/protected/application/themes/SaoJose/layouts/parts/header-logo.php
@@ -1,1 +1,4 @@
-<h1 id="brand-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-lugares-da-cultura.png'); ?>" /></a></h1>
+<h1 id="brand-logo">
+    <a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-lugares-da-cultura.png'); ?>" /></a>
+    <a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-pmsjc.png'); ?>" /></a>
+</h1>
