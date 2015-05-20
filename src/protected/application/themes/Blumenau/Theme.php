2235541d9d96a3766c5f033c07b4241235030b9e
Gutieri Barboza <eugutierri@gmail.com>
2015-05-20 17:37:49 -0300

    renamed files img share

--- src/protected/application/themes/Blumenau/Theme.php
+++ src/protected/application/themes/Blumenau/Theme.php
@@ -34,4 +34,13 @@ class Theme extends BaseV1\Theme{
     static function getThemeFolder() {
         return __DIR__;
     }
+    public function addDocumentMetas() {
+        parent::addDocumentMetas();
+        $app = App::i();
+        foreach ($this->documentMeta as $key => $meta){
+            if(isset($meta['property']) && ($meta['property'] === 'og:image' || $meta['property'] === 'og:image:url')){
+                $this->documentMeta[$key] = array('property' => $meta['property'] , 'content' => $app->view->asset('img/share-bc.png', false));
+            }
+        }
+    }
 }
