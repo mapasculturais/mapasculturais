4e195c11f30f474f515ee656c8b776b0e9afc60d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-06-11 15:13:18 -0300

    fix delete button that was not being shown when entity status = DRAFT

--- src/protected/application/themes/BaseV1/layouts/parts/editable-entity.php
+++ src/protected/application/themes/BaseV1/layouts/parts/editable-entity.php
@@ -8,6 +8,8 @@ $class = $entity->getClassName();
 
 $status_draft = $class::STATUS_DRAFT;
 $status_enabled = $class::STATUS_ENABLED;
+$status_trash = $class::STATUS_TRASH;
+
 
 $can_edit_roles = $this->controller->id == 'agent' && $entity->user->id != $app->user->id && $entity->id == $entity->user->profile->id && $entity->user->canUser('addRole');
 if($this->isEditable()){
@@ -28,16 +30,17 @@ if($this->isEditable()){
         <div class="controles">
             <?php if ($can_edit_roles): ?>
                 <div id="funcao-do-agente" class="dropdown dropdown-select">
-                    <div class="placeholder js-selected"> <?php
-                        if ($entity->user->is('superAdmin'))
-                            echo '<span data-role="superAdmin">' . $app->getRoleName('superAdmin') . '</span>';
-                        elseif ($entity->user->is('admin'))
-                            echo '<span data-role="admin">' . $app->getRoleName('admin') . '</span>';
-                        elseif ($entity->user->is('staff'))
-                            echo '<span data-role="staff">' . $app->getRoleName('staff') . '</span>';
-                        else
-                            echo "<span>Normal</span>";
-                        ?> </div>
+                    <div class="placeholder js-selected">
+                        <?php if ($entity->user->is('superAdmin')): ?>
+                            <span data-role="superAdmin"><?php echo $app->getRoleName('superAdmin'); ?></span>
+                        <?php elseif ($entity->user->is('admin')): ?>
+                            <span data-role="admin"><?php echo $app->getRoleName('admin'); ?></span>
+                        <?php elseif ($entity->user->is('staff')): ?>
+                            <span data-role="staff"><?php echo $app->getRoleName('staff'); ?></span>
+                        <?php else: ?>
+                            <span>Normal</span>
+                        <?php endif; ?>
+                    </div>
                     <div class="submenu-dropdown js-options">
                         <ul>
                             <li>
@@ -75,27 +78,34 @@ if($this->isEditable()){
                 <?php if($entity->isNew() || $entity->status <= 0):  ?>
                     <a class="btn btn-default js-submit-button hltip" data-status="<?php echo $status_draft ?>"  hltitle="Salvar este <?php echo strtolower($entity->getEntityType()) ?> como rascunho.">Salvar rascunho</a>
                     <a class="btn btn-primary js-submit-button hltip" data-status="<?php echo $status_enabled ?>" hltitle="Salvar e publicar este <?php echo strtolower($entity->getEntityType()) ?>.">Publicar</a>
+
                 <?php else: ?>
                     <a class="btn btn-primary js-submit-button" data-status="<?php echo $status_enabled ?>">Salvar</a>
                 <?php endif; ?>
+
             <?php elseif($this->controller->id === 'registration'): ?>
                 <a class="btn btn-primary js-submit-button">Salvar</a>
+
             <?php else: ?>
                 <a class="btn btn-primary js-submit-button" data-status="<?php echo $status_enabled ?>">Salvar</a>
             <?php endif; ?>
         </div>
-    <?php elseif ($entity->canUser('modify')): ?>
+    <?php else: ?>
         <script type="text/javascript">
             MapasCulturais.Messages.alert('Você possui permissão para editar este <?php echo strtolower($entity->entityType) ?>. Use os botões à direita para editar ou excluir.');
         </script>
         <div class="controles">
-            <!-- se estiver na página comum e logado mostrar:-->
-            <a href="<?php echo $entity->editUrl ?>" class="btn btn-primary js-toggle-edit">Editar</a>
+            <?php if ($entity->canUser('modify')): ?>
+                <!-- se estiver na página comum e logado mostrar:-->
+                <a href="<?php echo $entity->editUrl ?>" class="btn btn-primary js-toggle-edit">Editar</a>
+            <?php endif; ?>
 
-            <?php if ($entity->canUser('remove') && $entity->status > 0): ?>
+            <?php if ($entity->canUser('remove') && $entity->status !== $status_trash): ?>
                 <a href="<?php echo $entity->deleteUrl ?>" class="btn btn-danger">Excluir</a>
-            <?php elseif ($entity->canUser('undelete') && $entity->status < 0): ?>
+
+            <?php elseif ($entity->canUser('undelete') && $entity->status === $status_trash): ?>
                 <a href="<?php echo $entity->undeleteUrl ?>" class="btn btn-success">Recuperar</a>
+
                 <?php if($entity->canUser('destroy')): ?>
                     <a class="btn btn-danger" href="<?php echo $entity->destroyUrl; ?>">Excluir Definitivamente</a>
                 <?php endif; ?>
