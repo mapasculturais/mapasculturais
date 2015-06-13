901b40e549535d9d7ee448b673481b519f0c04ac
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-06-13 17:59:12 -0300

    add action to export virada cultural 2015 .docx

--- src/protected/application/themes/SpCultura/Theme.php
+++ src/protected/application/themes/SpCultura/Theme.php
@@ -41,6 +41,196 @@ class Theme extends BaseV1\Theme{
         $app->hook('view.render(<<*>>):before', function() use($app) {
             $this->jsObject['assets']['logo-prefeitura'] = $this->asset('img/logo-prefeitura.png', false);
         });
+
+        $theme = $this;
+
+        $app->hook('GET(project.virada-cultural-2015.docx)', function() use($app, $theme) {
+            $this->requireAuthentication();
+
+            $project_id = 632;
+
+            $entity = $app->repo('Project')->find($project_id);
+
+            $entity->checkPermission('@control');
+
+            $ids = $entity->getChildrenIds();
+
+            $ids[] = $entity->id;
+
+
+            $in = implode(',', array_map(function ($e){ return '@Project:' . $e; }, $ids));
+
+            $events = $app->controller('Event')->apiQuery([
+                '@select' => 'id,name,classificacaoEtaria,singleUrl,occurrences,terms,status,owner.id,owner.name,owner.singleUrl,project.id,project.name',
+                'project' => 'IN(' . $in . ')',
+                '@permissions' => 'view',
+                '@files' => '(avatar.avatarSmall):url'
+            ]);
+
+            $parceiros = [
+                'Centro Acadêmico XI de Agosto' => [],
+                'Cultura Inglesa Festival' => [],
+                'Secretaria da Cultura do Estado de São Paulo' => [],
+                'Coletivo Teatral Commune' => [],
+                'Sesc São Paulo' => [],
+                'Família 100 Conflito' => [],
+                'Movimento Cultural Penha' => [],
+                'RenattodSousa Foto Galeria' => [],
+                'Kaapora Cultural' => [],
+                'Garageclub' => [],
+                'Associação Viva e Deixe Viver Contadores de História' => [],
+                'Garrafas Bar' => [],
+                'Cia. Teatral Bola de Neve' => [],
+                'Memorial da Inclusão: Os Caminhos da Pessoa com Deficiência' => [],
+                'Caixa Cultural São Paulo' => [],
+                'Dia da Música' => [],
+                'Serviço Funerário do Município de São Paulo' => [],
+                'Espaçoarte eventos' => [],
+                'UNAS' => [],
+                'Centro de Tradições Nordestinas' => [],
+                'Virada Coral' => [],
+                'Consulado Geral de Portugal em São Paulo' => [],
+                'Auditório Ibirapuera' => [],
+                'MASP' => []
+            ];
+
+            $eventos_smc = [];
+
+
+            foreach($events as $e){
+                $e['owner']['name'] = trim(str_replace('  ', ' ', $e['owner']['name']));
+                if(isset($parceiros[$e['owner']['name']])){
+                    $parceiros[$e['owner']['name']][] = $e;
+                }else{
+                    $eventos_smc[] = $e;
+                }
+            }
+
+            $enderecos = [];
+
+            $group_events = function($events) use ($enderecos){
+                $spaces = [];
+                foreach($events as $event){
+                    foreach($event['occurrences'] as $occ){
+                        if(!isset($spaces[$occ->space->name])){
+                            $spaces[$occ->space->name] = [$occ->space->endereco];
+
+                        }
+                        if($occ->rule->duration != 1440){
+                            $data = $occ->startsOn->format('Y-m-d') . ' ' . $occ->startsAt->format('H:i');
+                            $spaces[$occ->space->name][$data] = $event['name'];
+                        }else{
+                            if(!isset($spaces[$occ->space->name]['24h'])){
+                                $spaces[$occ->space->name]['24h'] = [];
+                            }
+
+                            $spaces[$occ->space->name]['24h'][] = $event['name'];
+                        }
+                    }
+                }
+
+                foreach($spaces as $name => $evts){
+                    ksort($spaces[$name]);
+                }
+
+                return $spaces;
+            };
+
+            $print_parceiro = function($eventos, $parceiro = false) use ($project_id, $group_events){
+                $projetos = [];
+                $eventos_parceiro = [];
+                foreach($eventos as $evento){
+                    if($evento['project']['id'] != $project_id){
+                        if(!isset($projetos[$evento['project']['name']])){
+                            $projetos[$evento['project']['name']] = [];
+                        }
+
+                        $projetos[$evento['project']['name']][] = $evento;
+                    }else{
+                        $eventos_parceiro[] = $evento;
+                    }
+                }
+
+                $eventos_smc = $group_events($eventos_parceiro);
+                $eventos_projeto = [];
+                foreach($projetos as $name => $evts){
+                    $eventos_projeto[$name] = $group_events($evts);
+                }
+
+//                var_dump($eventos_smc);
+
+                $print = function($eventos, $h = 'h1'){
+                    ksort($eventos);
+                    foreach($eventos as $palco => $evts){
+                        echo "<{$h}>$palco</{$h}>";
+                        $endereco = $evts[0];
+                        unset($evts[0]);
+                        if($endereco != $palco){
+                            echo "<em>$endereco</em><br><br>";
+                        }
+
+                        if(isset($evts['24h'])){
+                        }else{
+                            $format = 'H:i';
+                        }
+                            $format = '\d\i\a d \à\s H:i';
+
+                        foreach($evts as $hora => $nome){
+                            if($hora == '24h'){
+                                foreach($nome as $n){
+                                    echo "  <b>$hora</b>   -   $n<br>";
+                                }
+                            }else{
+                                $dt = new \DateTime($hora);
+                                $hora = $dt->format($format);
+                                echo "  <b>$hora</b>   -   $nome<br>";
+                            }
+                        }
+                    }
+                };
+
+
+                if($parceiro){
+                    $h1 = 'h2';
+                    $h2 = 'h3';
+                    $h3 = 'h4';
+                }else{
+                    $h1 = 'h1';
+                    $h2 = 'h2';
+                    $h3 = 'h3';
+                }
+
+                if($parceiro){
+                    echo "<h1>{$parceiro}</h1>";
+                }
+
+                $print($eventos_smc, $h2);
+
+                foreach($eventos_projeto as $projeto => $evts){
+                    echo "<{$h1}>{$projeto}</{$h1}>";
+                    $print($evts,$h3);
+                }
+
+
+            };
+
+
+            $response = $app->response();
+            $response->header("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            $response->header('Content-Disposition', 'attachment; filename=virada-cultural-2015.docx');
+
+            ?>
+<html>
+    <body>
+        <?php $print_parceiro($eventos_smc); ?>
+
+        <?php foreach($parceiros as $parceiro => $eventos): if(!$eventos) continue; ?>
+            <?php $print_parceiro($eventos, $parceiro); ?>
+        <?php endforeach; ?>
+    </body>
+</html>
+            <?php
+        });
     }
 
 }
