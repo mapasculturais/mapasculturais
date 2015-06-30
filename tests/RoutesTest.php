39912c2b833f82ca4f974da4de0185101997414d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-06-30 15:20:06 -0300

    uncomment route tests

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -29,44 +29,44 @@ class RoutesTest extends MapasCulturais_TestCase {
         return $this->assetStatus('get', 503, $url, $message);
     }
 
-//    function testHome(){
-//        $this->user = null;
-//
-//        $this->assertGet200('/', 'assert that home status code is 200');
-//    }
-//
-//    function testPages(){
-//        $this->user = null;
-//
-//        $this->assertGet200('/sobre/', 'assert that about page status code is 200');
-//        $this->assertGet200('/como-usar/', 'assert that about page status code is 200');
-//    }
-//
-//    function testSearch(){
-//        $this->user = null;
-//
-//        $this->assertGet200('/busca/', 'assert that about page status code is 200');
-//    }
-//
-//    function test404(){
-//        $this->user = null;
-//
-//        $this->assertGet404('/' . uniqid('404-'), 'assert that code is 404');
-//    }
-//
-//    function testEntitiesRoutesWithGuest(){
-//        $this->user = null;
-//
-//        foreach($this->entities as $class => $name){
-//            $entities = $this->app->repo($class)->findAll();
-//
-//            foreach($entities as $e){
-//                $this->assertGet200($e->singleUrl, "assert that the status code of single of {$class} with id {$e->id} is 200 for guest users");
-//                $this->assertGet403($e->deleteUrl, "assert that the status code of delete url of {$class} with id {$e->id} is 401 for guest users");
-//                $this->assertGet401($e->editUrl, "assert that the status code of edit url of {$class} with id {$e->id} is 401 for guest users");
-//            }
-//        }
-//    }
+    function testHome(){
+        $this->user = null;
+
+        $this->assertGet200('/', 'assert that home status code is 200');
+    }
+
+    function testPages(){
+        $this->user = null;
+
+        $this->assertGet200('/sobre/', 'assert that about page status code is 200');
+        $this->assertGet200('/como-usar/', 'assert that about page status code is 200');
+    }
+
+    function testSearch(){
+        $this->user = null;
+
+        $this->assertGet200('/busca/', 'assert that about page status code is 200');
+    }
+
+    function test404(){
+        $this->user = null;
+
+        $this->assertGet404('/' . uniqid('404-'), 'assert that code is 404');
+    }
+
+    function testEntitiesRoutesWithGuest(){
+        $this->user = null;
+
+        foreach($this->entities as $class => $name){
+            $entities = $this->app->repo($class)->findAll();
+
+            foreach($entities as $e){
+                $this->assertGet200($e->singleUrl, "assert that the status code of single of {$class} with id {$e->id} is 200 for guest users");
+                $this->assertGet403($e->deleteUrl, "assert that the status code of delete url of {$class} with id {$e->id} is 401 for guest users");
+                $this->assertGet401($e->editUrl, "assert that the status code of edit url of {$class} with id {$e->id} is 401 for guest users");
+            }
+        }
+    }
 
     function testEntitiesRoutesWithNormalUser(){
         $user = $this->getUser('normal');
