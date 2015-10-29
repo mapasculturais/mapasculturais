6bdda4c401cd7f0da8e94bae6ebf112f46e9ca16
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-10-29 12:02:31 -0200

    change Agent::parent fetch mode to LAZY to improve performance of API

--- src/protected/application/lib/MapasCulturais/Entities/Agent.php
+++ src/protected/application/lib/MapasCulturais/Entities/Agent.php
@@ -136,7 +136,7 @@ class Agent extends \MapasCulturais\Entity
     /**
      * @var \MapasCulturais\Entities\Agent
      *
-     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\Agent", fetch="EAGER")
+     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\Agent", fetch="LAZY")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
      * })
