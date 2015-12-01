be35d0539f1583392251c23e1e9d21d31ead25e5
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-12-01 01:55:33 -0200

    multiselect directive working

--- src/protected/application/themes/BaseV1/assets/js/ng.entity.directive.editableMultiselect.js
+++ src/protected/application/themes/BaseV1/assets/js/ng.entity.directive.editableMultiselect.js
@@ -12,11 +12,23 @@
                 },
                 link: function ($scope, el, attrs) {
                     var def = MapasCulturais.entity.definition[attrs.entityProperty];
-
+                    var entity = MapasCulturais.entity.object;
+                    var originalValue = MapasCulturais.entity.object;
+                    
+                    function resetValues(){
+                        if(entity[$scope.entityProperty]){
+                            $scope.values = entity[$scope.entityProperty].split(';');
+                        }else{
+                            entity[$scope.entityProperty] = '';
+                            $scope.values = [];
+                        }
+                    }
+                    
+                    $scope.inputVal = entity[$scope.entityProperty];
+                    
                     $scope.editBox = EditBox;
                     
                     $scope.terms = Object.keys(def.options);
-                    $scope.values = ['Quilombolas','Outro'];
                     $scope.allowOther = def.allowOther;
                     $scope.allowOtherText = def.allowOtherText;
 
@@ -25,6 +37,28 @@
                     $log.log($scope.title);
 
                     $scope.editBoxId = 'editable-multiselect-' + $scope.entityProperty;
+                    
+                    resetValues();
+                    
+                    $scope.submit = function(){
+                        entity[$scope.entityProperty] = $scope.values.join(';');
+                        $scope.inputVal = entity[$scope.entityProperty];
+                        var $input = jQuery('#' + $scope.entityProperty);
+                        $input.editable('setValue', $scope.inputVal);
+                        EditBox.close($scope.editBoxId);
+                    };
+                    
+                    $scope.cancel = function(){
+                        resetValues();
+                    };
+                    
+                    $scope.valuesText = function(){
+                        if(entity[$scope.entityProperty]){
+                            return entity[$scope.entityProperty].split(';').join('; ');
+                        }else{
+                            return $scope.emptyLabel;
+                        }
+                    }
                 }
             };
         }]);
