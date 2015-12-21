1a19ba2be9807f63e52e725d8f4946e9df28ca4b
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-12-21 16:14:23 -0200

    Removes Google Maps API script from Home, Panel, Project and other possible unneeded routes

--- src/protected/application/themes/BaseV1/Theme.php
+++ src/protected/application/themes/BaseV1/Theme.php
@@ -681,9 +681,12 @@ class Theme extends MapasCulturais\Theme {
         $this->enqueueStyle('vendor', 'leaflet-draw', 'vendor/leaflet/lib/leaflet-plugins-updated-2014-07-25/Leaflet.draw-master/dist/leaflet.draw.css', array('leaflet'));
         $this->enqueueScript('vendor', 'leaflet-draw', 'vendor/leaflet/lib/leaflet-plugins-updated-2014-07-25/Leaflet.draw-master/dist/leaflet.draw-src.js', array('leaflet'));
 
-        $this->enqueueScript('vendor', 'google-maps-api', 'http://maps.google.com/maps/api/js?v=3.2&sensor=false');
+        // Google Maps API only needed in site/search and space, agent and event singles
+        if(preg_match('#site|space|agent|event#',    $this->controller->id) && preg_match('#search|single|edit|create#', $this->controller->action)){
+            $this->enqueueScript('vendor', 'google-maps-api', 'http://maps.google.com/maps/api/js?v=3.2&sensor=false');
+        }
 
-        //Leaflet Plugins (Google)false');
+        //Leaflet Plugins
         $this->enqueueScript('vendor', 'leaflet-google-tile', 'vendor/leaflet/lib/leaflet-plugins-updated-2014-07-25/leaflet-plugins-master/layer/tile/Google.js', array('leaflet'));
 
         $this->enqueueStyle('vendor', 'magnific-popup', "vendor/Magnific-Popup-{$versions['magnific-popup']}/magnific-popup.css");
