8c852e3b4fbfd67aa6e6cc94ea2a8637317f0961
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-01-13 17:35:01 -0200

    force sass to utf-8, useful in some distros

--- scripts/compile-sass.sh
+++ scripts/compile-sass.sh
@@ -22,14 +22,14 @@ cd $DIR/src/protected/tools/
 ASSETS_FOLDER=$(MAPASCULTURAIS_CONFIG_FILE=$CONFIG HTTP_HOST=$DOMAIN REQUEST_METHOD='CLI' REMOTE_ADDR='127.0.0.1' REQUEST_URI='/' SERVER_NAME=127.0.0.1 SERVER_PORT="8000" php get-theme-assets-path.php)
 
 echo "compilando main.css do tema BaseV1"
-sass $BaseV1/css/sass/main.scss:$BaseV1/css/main.css
+sass $BaseV1/css/sass/main.scss:$BaseV1/css/main.css -E "UTF-8"
 
 #echo "aplicando o autoprefixer no main.css do tema BaseV1"
 #autoprefixer-cli $BaseV1/css/main.css
 
 if [ -f $ASSETS_FOLDER/css/sass/main.scss ]; then
   echo "compilando main.css do tema ativo $ASSETS_FOLDER/"
-  sass $ASSETS_FOLDER/css/sass/main.scss:$ASSETS_FOLDER/css/main.css
+  sass $ASSETS_FOLDER/css/sass/main.scss:$ASSETS_FOLDER/css/main.css -E "UTF-8"
 
 #  echo "aplicando o autoprefixer no main.css do tema ativo $ASSETS_FOLDER/"
 #  autoprefixer-cli $ASSETS_FOLDER/css/main.css
