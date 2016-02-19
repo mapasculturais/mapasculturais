0cadc0e6b906284e33072ca4a7d918f1709842a2
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-02-19 20:37:19 -0200

    new example

--- src/protected/application/plugins/mail-examples.php
+++ src/protected/application/plugins/mail-examples.php
@@ -2,12 +2,23 @@
 use MapasCulturais\Entities as e;
 $app = MapasCulturais\App::i();
 
-
+// envia email quando uma entidade for criada
 $app->hook('entity(<<Agent|Space|Event|Project>>).insert:after',function() use($app){
     $app->createAndSendMailMessage([
         'from' => $app->config['plugins.mailExamples.from'],
         'to' => $app->config['plugins.mailExamples.to'],
         'subject' => "Novo $this->entityType criado",
         'body' => "Um $this->entityType de nome $this->name foi criado pelo usuário {$app->user->profile->name}"
+    ]);
+});
+
+
+// envia email a cada acesso a home do site
+$app->hook('GET(site.index):before',function() use($app){
+    $app->createAndSendMailMessage([
+        'from' => $app->config['plugins.mailExamples.from'],
+        'to' => $app->config['plugins.mailExamples.to'],
+        'subject' => "Teste de envio - home",
+        'body' => "Acesso a home do $app->siteName"
     ]);
 });
