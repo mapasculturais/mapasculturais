876afbc290a5fd2805a5256607d0fb850c8102a2
leonardo <leotrujillo@vera.com.uy>
2016-03-03 13:16:48 -0300

    este archivo se debe traducir junto con el datepicker

--- src/protected/application/themes/BaseV1/assets/vendor/moment.pt-br.js
+++ src/protected/application/themes/BaseV1/assets/vendor/moment.pt-br.js
@@ -12,43 +12,43 @@
     }
 }(function (moment) {
     return moment.defineLocale('pt-br', {
-        months : "janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro".split("_"),
-        monthsShort : "jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez".split("_"),
-        weekdays : "domingo_segunda-feira_terça-feira_quarta-feira_quinta-feira_sexta-feira_sábado".split("_"),
-        weekdaysShort : "dom_seg_ter_qua_qui_sex_sáb".split("_"),
-        weekdaysMin : "dom_2ª_3ª_4ª_5ª_6ª_sáb".split("_"),
+        months : "enero_febrero_marzo_abril_mayo_junio_julio_agosto_setiembre_octubre_noviembre_diciembre".split("_"),
+        monthsShort : "ene_feb_mar_abr_may_jun_jul_ago_set_oct_nov_dic".split("_"),
+        weekdays : "domingo_lunes_martes_miércoles_jueves_viernes_sábado".split("_"),
+        weekdaysShort : "dom_lun_mar_mié_jue_vie_sáb".split("_"),
+        weekdaysMin : "dom_lun_mar_mié_jue_vie_sáb".split("_"),
         longDateFormat : {
             LT : "HH:mm",
             L : "DD/MM/YYYY",
             LL : "D [de] MMMM [de] YYYY",
             LLL : "D [de] MMMM [de] YYYY [às] LT",
-            LLLL : "dddd, D [de] MMMM [de] YYYY [às] LT"
+            LLLL : "dddd, D [de] MMMM [de] YYYY [a las] LT"
         },
         calendar : {
-            sameDay: '[Hoje às] LT',
-            nextDay: '[Amanhã às] LT',
-            nextWeek: 'dddd [às] LT',
-            lastDay: '[Ontem às] LT',
+            sameDay: '[Hoy a las] LT',
+            nextDay: '[Mañana a las] LT',
+            nextWeek: 'dddd [a las] LT',
+            lastDay: '[Ayer a las] LT',
             lastWeek: function () {
                 return (this.day() === 0 || this.day() === 6) ?
-                    '[Último] dddd [às] LT' : // Saturday + Sunday
-                    '[Última] dddd [às] LT'; // Monday - Friday
+                    '[Último] dddd [a las] LT' : // Saturday + Sunday
+                    '[Última] dddd [a las] LT'; // Monday - Friday
             },
             sameElse: 'L'
         },
         relativeTime : {
-            future : "em %s",
+            future : "en %s",
             past : "%s atrás",
             s : "segundos",
-            m : "um minuto",
+            m : "un minuto",
             mm : "%d minutos",
-            h : "uma hora",
+            h : "una hora",
             hh : "%d horas",
-            d : "um dia",
-            dd : "%d dias",
-            M : "um mês",
+            d : "un día",
+            dd : "%d días",
+            M : "un mes",
             MM : "%d meses",
-            y : "um ano",
+            y : "un año",
             yy : "%d años"
         },
         ordinal : '%dº'
