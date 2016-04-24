a52dbba9dc5a666838e9f71811badb0493c91da3
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-04-23 21:09:17 -0300

    add missing files on Ceara Theme

---
+++ src/protected/application/themes/Ceara/layouts/parts/header-about-nav.php
@@ -0,0 +1,7 @@
+<nav id="about-nav" class="alignright clearfix">
+	<h1 id="organization-logo"><a href="http://www.ceara.gov.br/" target="_blank"><img src="<?php $this->asset('img/logo-org-ceara.png'); ?>" /></a></h1>
+    <ul id="secondary-menu">
+        <li><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre o Mapa Cultural do Ceará"></a></li>
+        <li><a class="icon icon-help hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></li>
+    </ul>
+</nav>
---
+++ src/protected/application/themes/Ceara/layouts/parts/header-logo.php
@@ -0,0 +1,3 @@
+<h1 id="brand-logo">
+    <a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-ceara.png'); ?>" /></a>
+</h1>
