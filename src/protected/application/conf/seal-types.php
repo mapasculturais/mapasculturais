071fa915f01a2709061a6b66fb3879ed1ba5a73e
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-06-08 16:17:24 -0300

    Adicionando arquivo faltante: seal-types.php

---
+++ src/protected/application/conf/seal-types.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * See https://github.com/Respect/Validation to know how to write validations
+ */
+return array(
+    'metadata' => array(
+        'nomeCompleto' => array(
+            'private' => true,
+            'label' => 'Nome completo ou Razão Social',
+            'validations' => array(
+                //'required' => 'Seu nome completo ou jurídico deve ser informado.'
+            )
+        ),
+        'facebook' => array(
+            'label' => 'Facebook',
+            'validations' => array(
+                "v::url('facebook.com')" => "A url informada é inválida."
+            )
+        ),
+        'twitter' => array(
+            'label' => 'Twitter',
+            'validations' => array(
+                "v::url('twitter.com')" => "A url informada é inválida."
+            )
+        ),
+        'googleplus' => array(
+            'label' => 'Google+',
+            'validations' => array(
+                "v::url('plus.google.com')" => "A url informada é inválida."
+            )
+        ),
+
+    ),
+    'items' => array(
+        1 => array( 'name' => 'Individual' ),
+        2 => array( 'name' => 'Coletivo' ),
+    )
+);
