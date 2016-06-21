d2f1cda3c89cab1d9382dbd11687e52faa89459c
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-06-21 17:25:50 -0300

    db-update create user_meta

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -72,6 +72,38 @@ return [
         $conn->executeQuery("ALTER TABLE ONLY user_app ADD CONSTRAINT usr_user_app_fk FOREIGN KEY (user_id) REFERENCES usr(id);");
 
     },
+            
+                        
+    'create table user_meta' => function() use ($conn) {
+        
+        if($conn->fetchAll("SELECT table_name FROM information_schema.tables WHERE  table_schema = 'public' AND table_name = 'user_meta';")){
+            echo "TABLE user_meta ALREADY EXISTS";
+            return true;
+        }
+
+        $conn->executeQuery("CREATE TABLE user_meta (
+                                object_id integer NOT NULL,
+                                key character varying(32) NOT NULL,
+                                value text,
+                                id integer NOT NULL);");
+
+        $conn->executeQuery("CREATE SEQUENCE user_meta_id_seq
+                                START WITH 1
+                                INCREMENT BY 1
+                                NO MINVALUE
+                                NO MAXVALUE
+                                CACHE 1;");
+
+        $conn->executeQuery("ALTER TABLE user_meta_id_seq OWNER TO mapasculturais;");
+        $conn->executeQuery("ALTER SEQUENCE user_meta_id_seq OWNED BY user_meta.id;");
+        $conn->executeQuery("ALTER TABLE ONLY user_meta ALTER COLUMN id SET DEFAULT nextval('user_meta_id_seq'::regclass);");
+        $conn->executeQuery("ALTER TABLE ONLY user_meta ADD CONSTRAINT user_meta_pk PRIMARY KEY (id);");
+        $conn->executeQuery("CREATE INDEX user_meta_owner_key_index ON user_meta USING btree (object_id, key);");
+        $conn->executeQuery("CREATE INDEX user_meta_owner_key_value_index ON user_meta USING btree (object_id, key, value);");
+        $conn->executeQuery("ALTER TABLE ONLY user_meta ADD CONSTRAINT usr_user_meta_fk FOREIGN KEY (object_id) REFERENCES usr(id);");
+    },
+            
+            
     'resize entity meta key columns' => function() use($conn) {
         $conn->executeQuery('ALTER TABLE space_meta ALTER COLUMN key TYPE varchar(128)');
         $conn->executeQuery('ALTER TABLE agent_meta ALTER COLUMN key TYPE varchar(128)');
