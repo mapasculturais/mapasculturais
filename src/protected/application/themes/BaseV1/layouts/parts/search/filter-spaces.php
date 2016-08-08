88806a21e7cacdf13c5e8b9408e9ed2a6991f555
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-08-08 18:59:58 -0300

    Reset filter-spaces

--- src/protected/application/themes/BaseV1/layouts/parts/search/filter-spaces.php
+++ src/protected/application/themes/BaseV1/layouts/parts/search/filter-spaces.php
@@ -5,7 +5,24 @@
         <a class="icon icon-show-advanced-search" ng-click="toggleAdvancedFilters()"></a>
     </header>
     <div class="simple-filters" ng-show="showSearch()">
-
+        <form class="form-palavra-chave filter search-filter--keyword">
+            <label for="palavra-chave-espaco">Palavra-chave</label>
+            <input ng-model="data.space.keyword" class="search-field" type="text" name="palavra-chave-espaco" placeholder="Buscar <?php $this->dict('entities: spaces') ?>" />
+        </form>
+        <!--.filter-->
+        <div class="filter search-filter--area">
+            <span class="label">Área de Atuação</span>
+            <div class="dropdown">
+                <div class="placeholder">Selecione as áreas</div>
+                <div class="submenu-dropdown">
+                    <ul class="filter-list">
+                        <li ng-repeat="area in areas" ng-class="{'selected':isSelected(data.space.areas, area.id)}" ng-click="toggleSelection(data.space.areas, area.id)">
+                            <span>{{area.name}}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <!--.filter-->
         <div class="filter search-filter--tipo">
             <span class="label">Tipo</span>
@@ -20,14 +37,18 @@
                 </div>
             </div>
         </div>
-
-    <!--.filter-->
-
+        <!--.filter-->
+        <div class="filter search-filter--acessibilidade">
+            <span class="icon icon-check" ng-click="data.space.acessibilidade = !data.space.acessibilidade" ng-class="{'selected':data.space.acessibilidade}"></span>
+            <span class="label show-label" ng-click="data.space.acessibilidade = !data.space.acessibilidade">Acessibilidade</span>
+        </div>
+        <!--.filter-->
+        
         <div class="filter verified-filter search-filter--verified">
             <a class="hltip btn btn-verified" ng-class="{'selected':data.space.isVerified}" title="Exibir somente resultados Verificados" ng-click="toggleVerified('space')"><?php $this->dict('search: verified results') ?></a>
         </div>
         <!-- div.verified-filter -->
-
+        
         <div ng-repeat="entity in ['space']" class="show-advanced-filters ">
             <?php $this->part('search/advanced-filters') ?>
         </div>
