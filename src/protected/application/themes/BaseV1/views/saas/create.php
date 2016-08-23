c21c7e257819662ab87a0a99095ca524f5140a79
annanee <annaflavia@fluture.com.br>
2016-08-23 11:33:12 -0300

    SaaS partials

--- src/protected/application/themes/BaseV1/views/saas/create.php
+++ src/protected/application/themes/BaseV1/views/saas/create.php
@@ -11,9 +11,22 @@
 		<span class="url js-editable" data-edit="certificateText" data-original-title="URL da instalação" data-emptytext="Mapa Cultural Santos">.mapas.cultura.gov.br</span>
 
 		<span class="label">Entidades habilitadas:</span>
-		<span class="enabled-entities js-editable" data-edit="certificateText" data-original-title="Nome da Instalação" data-emptytext="Mapa Cultural Santos">Agentes</span>
-		<span class="enabled-entities js-editable" data-edit="certificateText" data-original-title="Nome da Instalação" data-emptytext="Mapa Cultural Santos">Espaços</span>
-		<span class="enabled-entities js-editable" data-edit="certificateText" data-original-title="Nome da Instalação" data-emptytext="Mapa Cultural Santos">Projetos</span>
-		<span class="enabled-entities js-editable" data-edit="certificateText" data-original-title="Nome da Instalação" data-emptytext="Mapa Cultural Santos">Eventos</span>
+
+        <div id="boas-vindas" class="aba-content">
+            <?php $this->applyTemplateHook('tab-about','begin'); ?>
+            <div class="upload">
+            	<span class="label">Logomarca do Mapas:</span>
+                <p>
+                    <span class="js-editable" data-edit="shortDescription" data-original-title="Descrição Curta" data-emptytext="Insira uma descrição curta" data-tpl='<textarea maxlength="400"></textarea>'><?php echo $this->isEditable() ? $entity->shortDescription : nl2br($entity->shortDescription); ?></span>
+                </p>
+                <?php $this->part('singles/saas-setup'); ?>
+
+                <?php $this->part('singles/location', ['has_private_location' => false]); ?>
+            </div>
+
+            <?php $this->part('singles/space-extra-info') ?>
+
+        </div>
+
 	</div>
 </article>
