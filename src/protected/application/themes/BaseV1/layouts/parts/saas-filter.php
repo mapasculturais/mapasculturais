4c918dd84df73f5ae659259ea552251a13cb4757
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-09-15 18:09:55 -0300

    Included area, type, state and language map filters for SaaS instalation.

--- src/protected/application/themes/BaseV1/layouts/parts/saas-filter.php
+++ src/protected/application/themes/BaseV1/layouts/parts/saas-filter.php
@@ -7,34 +7,40 @@ $tags = $entity->terms['tag'];
 <?php if($this->isEditable() || !empty($tags)): ?>
     <div id="filtros" class="aba-content">
         <span class="label">Filtros:</span>
-        <?php if($this->isEditable() || $entity->filtro_uf): ?>
+        <?php if($this->isEditable() || $entity->filtro_agent_meta_En_Estado): ?>
             <p>
               <span class="label">Estado: </span>
-              <editable-multiselect entity-property="filtro_uf" empty-label="Selecione o(s) estado(s)" allow-other="true" box-title="Estado:"></editable-multiselect>
+              <span class="js-editable" data-edit="filtro_agent_meta_En_Estado" data-original-title="Estado" data-emptytext="Selecione o(s) estado(s) para o(s) Agente(s)"></span>
             </p>
         <?php endif;?>
-        <?php if($this->isEditable() || $entity->filtro_area_atuacao_agente): ?>
+        <?php if($this->isEditable() || $entity->filtro_space_meta_En_Estado): ?>
+            <p>
+              <span class="label">Estado: </span>
+              <span class="js-editable" data-edit="filtro_space_meta_En_Estado" data-original-title="Estado" data-emptytext="Selecione o(s) estado(s) para o(s) Espaço(s)"></span>
+            </p>
+        <?php endif;?>
+        <?php if($this->isEditable() || $entity->filtro_agent_term_area): ?>
             <p>
               <span class="label">Área de Atuação do Agente: </span>
-              <editable-multiselect entity-property="filtro_area_atuacao_agente" empty-label="Selecione a(s) área(s) de atuação" allow-other="true" box-title="Área de atuação:"></editable-multiselect>
+              <span class="js-editable" data-edit="filtro_agent_term_area" data-original-title="Área de Atuação" data-emptytext="Selecione a(s) área(s) de atuação"></span>
             </p>
         <?php endif;?>
-        <?php if($this->isEditable() || $entity->filtro_espaco): ?>
-            <p>
-              <span class="label">Tipo de Espaço: </span>
-              <editable-multiselect entity-property="filtro_espaco" empty-label="Selecione o(s) tipo(s) de espaço(s)" allow-other="true" box-title="Tipo de Espaço:"></editable-multiselect>
-            </p>
-        <?php endif;?>
-        <?php if($this->isEditable() || $entity->filtro_area_atuacao_espaco): ?>
+        <?php if($this->isEditable() || $entity->filtro_space_term_area): ?>
             <p>
               <span class="label">Área de Atuação do Espaço: </span>
-              <editable-multiselect entity-property="filtro_area_atuacao_espaco" empty-label="Selecione a(s) área(s) de atuação" allow-other="true" box-title="Área de atuação:"></editable-multiselect>
+              <span class="js-editable" data-edit="filtro_space_term_area" data-original-title="Área de Atuação" data-emptytext="Selecione a(s) área(s) de atuação"></span>
             </p>
         <?php endif;?>
-        <?php if($this->isEditable() || $entity->filtro_linguagem): ?>
+        <?php if($this->isEditable() || $entity->filtro_space_meta_type): ?>
+            <p>
+              <span class="label">Tipo de Espaço: </span>
+              <span class="js-editable" data-edit="filtro_space_meta_type" data-original-title="Tipo de Espaço" data-emptytext="Selecione o(s) tipo(s) de espaço(s)"></span>
+            </p>
+        <?php endif;?>
+        <?php if($this->isEditable() || $entity->filtro_event_term_linguagem): ?>
             <p>
               <span class="label">Linguagem: </span>
-              <editable-multiselect entity-property="filtro_linguagem" empty-label="Selecione o(s) tipos(s) de linguagem" allow-other="true" box-title="Linguagem:"></editable-multiselect>
+              <span class="js-editable" data-edit="filtro_event_term_linguagem" data-original-title="Linguagem" data-emptytext="Selecione o(s) tipos(s) de linguagem"></span>
             </p>
         <?php endif;?>
     </div>
