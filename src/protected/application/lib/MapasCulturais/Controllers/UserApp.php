6a253367ad05f41bd0f06d0a513955880f57b51b
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-09-22 14:39:31 -0300

    fix user apps controller

--- src/protected/application/lib/MapasCulturais/Controllers/UserApp.php
+++ src/protected/application/lib/MapasCulturais/Controllers/UserApp.php
@@ -12,27 +12,30 @@ use MapasCulturais\Traits;
  *
  */
 class UserApp extends EntityController {
+
     use Traits\ControllerSoftDelete;
 
     /**
-     * Returns the etity with the requested id.
+     * Returns the entity with the requested id.
      *
      * @example for the url http://mapasculturais/agent/33  or http://mapasculturais/agent/id:33 returns the agent with the id 33
      *
      * @return \MapasCulturais\Entity|null
      */
-    public function getRequestedEntity(){
+    public function getRequestedEntity() {
+        $entity = parent::getRequestedEntity();
+        if (!$entity && key_exists(0, $this->urlData)) {
+            $entity = $this->repository->find($this->urlData[0]);
+        } else if (!$entity && !key_exists(0, $this->urlData)) {
 
-        if(!key_exists(0, $this->urlData))
-            return null;
+            return false;
+        }
 
-        $entity = $this->repository->find($this->urlData[0]);
-        
-        if($entity){
+        if ($entity) {
             $entity->checkPermission('view');
         }
-        
+
         return $entity;
     }
-}
 
+}
