186cedf52ba19dc16b881ffdb8352a6988078b18
Leo Germani <leogermani@hacklab.com.br>
2016-10-04 15:42:27 -0300

    Lista de usuarios finalizada, falta item no menu

--- src/protected/application/conf/roles.php
+++ src/protected/application/conf/roles.php
@@ -1,12 +1,21 @@
 <?php
 return array(
     'superAdmin' => array(
-        'name' => 'Superadmin'
+        'name' => 'Superadmin',
+        'singularLabel' => 'Super Administrador',
+        'pluralLabel' => 'Super Administradores',
+        'permissionSuffix' => 'SuperAdmin' // Por exemplo, RemoveRoleSuperAdmin
     ),
     'admin' => array(
-        'name' => 'Admin'
+        'name' => 'Admin',
+        'singularLabel' => 'Administrador',
+        'pluralLabel' => 'Administradores',
+        'permissionSuffix' => 'Admin' // Por exemplo, RemoveRoleAdmin
     ),
     'staff' => array(
-        'name' => 'Equipe'
+        'name' => 'Equipe',
+        'singularLabel' => 'Membro da Equipe',
+        'pluralLabel' => 'Membros da Equipe',
+        'permissionSuffix' => '' // Por exemplo, RemoveRole
     )
 );
