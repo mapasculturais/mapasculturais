bb20a2ea6351b4b9d72210c9b85a05af146266bd
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-10-07 16:57:07 -0300

    reindent Subsite controller file

--- src/protected/application/lib/MapasCulturais/Controllers/Subsite.php
+++ src/protected/application/lib/MapasCulturais/Controllers/Subsite.php
@@ -15,32 +15,33 @@ use MapasCulturais\Entities;
  *
  */
 class Subsite extends EntityController {
-    use
-    	Traits\ControllerUploads,
-    	Traits\ControllerTypes,
-      Traits\ControllerVerifiable,
-      Traits\ControllerSoftDelete,
-      Traits\ControllerDraft,
-      Traits\ControllerArchive,
-      Traits\ControllerAPI;
 
-      /**
-       * Creates a new Subsite
-       *
-       * This action requires authentication and outputs the json with the new event or with an array of errors.
-       *
-       * <code>
-       * // creates the url to this action
-       * $url = $app->createUrl('subsite');
-       * </code>
-       */
-      function POST_index(){
+    use Traits\ControllerUploads,
+        Traits\ControllerTypes,
+        Traits\ControllerVerifiable,
+        Traits\ControllerSoftDelete,
+        Traits\ControllerDraft,
+        Traits\ControllerArchive,
+        Traits\ControllerAPI;
+
+    /**
+     * Creates a new Subsite
+     *
+     * This action requires authentication and outputs the json with the new event or with an array of errors.
+     *
+     * <code>
+     * // creates the url to this action
+     * $url = $app->createUrl('subsite');
+     * </code>
+     */
+    function POST_index() {
         $app = App::i();
 
-        $app->hook('entity(subsite).insert:before', function() use($app){
-          $this->owner = $app->user->profile;
+        $app->hook('entity(subsite).insert:before', function() use($app) {
+            $this->owner = $app->user->profile;
         });
 
         parent::POST_index();
-      }
+    }
+
 }
