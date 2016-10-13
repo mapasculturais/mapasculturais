0edd3190683308042e199b11ddcec5ecd0f229a4
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-10-13 16:21:07 -0300

    Created seal relation owner agent.

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -178,8 +178,12 @@ return [
     	$conn->executeQuery("ALTER TABLE event ADD COLUMN update_timestamp TIMESTAMP(0) WITHOUT TIME ZONE;");
     	$conn->executeQuery("ALTER TABLE seal ADD COLUMN update_timestamp TIMESTAMP(0) WITHOUT TIME ZONE;");
     },
-    
+
     'Fix field options field type from registration field configuration' => function () use($conn) {
         $conn->executeQuery("ALTER TABLE registration_field_configuration ALTER COLUMN field_options TYPE text;");
+    },
+
+    'Created owner seal relation field' => function () use($conn) {
+        $conn->executeQuery("ALTER TABLE seal_relation ADD COLUMN owner_id INTEGER;");
     }
 ];
