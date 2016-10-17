d5e82cb419abac6b800ced54051eba28749772d6
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-10-17 17:07:37 -0200

    fix user getNotification method

--- src/protected/application/lib/MapasCulturais/Entities/User.php
+++ src/protected/application/lib/MapasCulturais/Entities/User.php
@@ -385,6 +385,9 @@ class User extends \MapasCulturais\Entity implements \MapasCulturais\UserInterfa
     }
 
     function getNotifications($status = null){
+        $app = App::i();
+        $app->em->clear('MapasCulturais\Entities\Notification');
+        
         if(is_null($status)){
             $status_operator =  '>';
             $status = '0';
