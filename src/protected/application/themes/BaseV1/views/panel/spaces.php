920a16bd27bcb72bb14316fdcba75a65313b3a24
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-10-18 17:50:45 -0200

    Added full space count on home space page.

--- src/protected/application/themes/BaseV1/views/panel/spaces.php
+++ src/protected/application/themes/BaseV1/views/panel/spaces.php
@@ -7,7 +7,7 @@ $this->layout = 'panel';
 		<a class="btn btn-default add" href="<?php echo $app->createUrl('space', 'create'); ?>">Adicionar <?php $this->dict('entities: new space') ?></a>
 	</header>
     <ul class="abas clearfix clear">
-        <li class="active"><a href="#ativos">Ativos (<?php echo count($enabled); ?>)</a></li>
+        <li class="active"><a href="#ativos">Ativos (<?php echo count($app->user->enabledSpaces); ?>)</a></li>
 		<li><a href="#permitido">Concedidos (<?php echo count($app->user->hasControlSpaces);?>)</a></li>
         <li><a href="#rascunhos">Rascunhos (<?php echo count($draft); ?>)</a></li>
         <li><a href="#lixeira">Lixeira (<?php echo count($trashed); ?>)</a></li>
