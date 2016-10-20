4b2f3cba62492957cf5a73bf07d1e6215e19f898
Leo Germani <leogermani@hacklab.com.br>
2016-10-20 15:46:12 -0200

    Novos metodos e documentação para criação de metadados personalizados

--- src/protected/application/themes/BaseV1/Theme.php
+++ src/protected/application/themes/BaseV1/Theme.php
@@ -1212,4 +1212,35 @@ class Theme extends MapasCulturais\Theme {
 
         }
     }
+
+
+    function registerMetadata($entity_class, $key, $cfg) {
+        $app = \MapasCulturais\App::i();
+        $def = new \MapasCulturais\Definitions\Metadata($key, $cfg);
+        return $app->registerMetadata($def, $entity_class);
+
+    }
+
+    function registerEventMetadata($key, $cfg) {
+        return $this->registerMetadata('MapasCulturais\Entities\Event', $key, $cfg);
+    }
+
+    function registerSpaceMetadata($key, $cfg) {
+        return $this->registerMetadata('MapasCulturais\Entities\Space', $key, $cfg);
+    }
+
+    function registerAgentMetadata($key, $cfg) {
+        return $this->registerMetadata('MapasCulturais\Entities\Agent', $key, $cfg);
+    }
+
+    function registerProjectMetadata($key, $cfg) {
+        return $this->registerMetadata('MapasCulturais\Entities\Project', $key, $cfg);
+    }
+
+    function registerSealMetadata($key, $cfg) {
+        return $this->registerMetadata('MapasCulturais\Entities\Seal', $key, $cfg);
+    }
+
+
+
 }
