6543ac3770c0db2f980adecfe749b34ed8d845d7
Leo Germani <leogermani@hacklab.com.br>
2016-10-21 14:59:11 -0200

    pequenos ajustes de layout

--- src/protected/application/themes/mec-teatro/layouts/parts/tab-tecnica.php
+++ src/protected/application/themes/mec-teatro/layouts/parts/tab-tecnica.php
@@ -1,5 +1,5 @@
 <div id="tab-tecnica" class="aba-content">
-    <div class="tecnica">
+    <div class="tecnica servico">
 
         <?php if($this->isEditable() || $entity->teatros_aforo): ?>
         <p>
@@ -28,29 +28,11 @@
         </p>
         <?php endif; ?>
 
-        <?php if($this->isEditable() || $entity->teatros_aforo): ?>
-        <p>
-            <span class="label">Aforo:</span>
-            <span class="js-editable" data-edit="teatros_aforo" data-original-title="Aforo" data-emptytext="Aforo">
-                <?php echo $entity->teatros_aforo; ?>
-            </span>
-        </p>
-        <?php endif; ?>
-
         <?php if($this->isEditable() || $entity->teatros_altura): ?>
         <p>
             <span class="label">Altura (en metros):</span>
             <span class="js-editable" data-edit="teatros_altura" data-original-title="Altura (en metros)" data-emptytext="Altura (en metros)">
                 <?php echo $entity->teatros_altura; ?>
-            </span>
-        </p>
-        <?php endif; ?>
-
-        <?php if($this->isEditable() || $entity->teatros_aforo): ?>
-        <p>
-            <span class="label">Aforo:</span>
-            <span class="js-editable" data-edit="teatros_aforo" data-original-title="Aforo" data-emptytext="Aforo">
-                <?php echo $entity->teatros_aforo; ?>
             </span>
         </p>
         <?php endif; ?>
@@ -63,31 +45,31 @@
             </span>
         </p>
         <?php endif; ?>
-
+        <hr />
         <?php if($this->isEditable() || $entity->teatros_equipamento_lumnico): ?>
         <p>
             <span class="label">Equipamento Lumnico:</span>
-            <span class="js-editable" data-edit="teatros_equipamento_lumnico" data-original-title="Equipamento Lumnico" data-emptytext="Equipamento Lumnico">
-                <?php echo $entity->teatros_equipamento_lumnico; ?>
-            </span>
+            <p class="js-editable" data-edit="teatros_equipamento_lumnico" data-original-title="Equipamento Lumnico" data-emptytext="Equipamento Lumnico">
+                <?php echo $this->isEditable() ? $entity->teatros_equipamento_lumnico : nl2br($entity->teatros_equipamento_lumnico); ?>
+            </p>
         </p>
         <?php endif; ?>
-
+        <hr />
         <?php if($this->isEditable() || $entity->teatros_equipamento_sonido): ?>
         <p>
             <span class="label">Equipamento de Sonido:</span>
-            <span class="js-editable" data-edit="teatros_equipamento_sonido" data-original-title="Equipamento de Sonido" data-emptytext="Equipamento de Sonido">
-                <?php echo $entity->teatros_equipamento_sonido; ?>
-            </span>
+            <p class="js-editable" data-edit="teatros_equipamento_sonido" data-original-title="Equipamento de Sonido" data-emptytext="Equipamento de Sonido">
+                <?php echo $this->isEditable() ? $entity->teatros_equipamento_sonido : nl2br($entity->teatros_equipamento_sonido); ?>
+            </p>
         </p>
         <?php endif; ?>
-
+        <hr />
         <?php if($this->isEditable() || $entity->teatros_equipamento_audiovisual): ?>
         <p>
             <span class="label">Equipamento Audiovisual:</span>
-            <span class="js-editable" data-edit="teatros_equipamento_audiovisual" data-original-title="Equipamento Audiovisual" data-emptytext="Equipamento Audiovisual">
-                <?php echo $entity->teatros_equipamento_audiovisual; ?>
-            </span>
+            <p class="js-editable" data-edit="teatros_equipamento_audiovisual" data-original-title="Equipamento Audiovisual" data-emptytext="Equipamento Audiovisual">
+                <?php echo $this->isEditable() ? $entity->teatros_equipamento_audiovisual : nl2br($entity->teatros_equipamento_audiovisual); ?>
+            </p>
         </p>
         <?php endif; ?>
 
