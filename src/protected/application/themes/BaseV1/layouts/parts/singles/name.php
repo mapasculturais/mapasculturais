6cdee948115948179b357a7c65c2f3d7021f9155
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-10-25 09:54:59 -0200

    Fix the obligation verification of the name and project about fields on single pages.

--- src/protected/application/themes/BaseV1/layouts/parts/singles/name.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/name.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 $class = isset($disable_editable) ? '' : 'js-editable';
+
+$editEntity = $this->controller->action === 'create' || $this->controller->action === 'edit';
 ?>
+
 <?php $this->applyTemplateHook('name','before'); ?>
-<h2><span class="<?php echo $class ?> required" data-edit="name" data-original-title="Nome de exibição" data-emptytext="Nome de exibição"><?php echo $entity->name; ?></span></h2>
+<h2><span class="<?php echo $class ?> <?php echo ($entity->isPropertyRequired($entity,"name") && $editEntity? 'required': '');?>" data-edit="name" data-original-title="Nome de exibição" data-emptytext="Nome de exibição"><?php echo $entity->name; ?></span></h2>
 <?php $this->applyTemplateHook('name','after'); ?>
