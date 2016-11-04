086bd6577d71d9c9b918fdeaa1ec12061298cf4a
Leo Germani <leogermani@hacklab.com.br>
2016-11-04 19:09:24 -0200

    desabilita tipos de entidades

---
+++ src/protected/application/themes/mec-teatro/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'app.enabled.agents'   => false,
+    'app.enabled.projects' => false,
+    'app.enabled.events'   => false,
+    'app.enabled.seals'   => false,
+    'app.enabled.apps'     => false,
+
+);
