d9cbecacb2b9673e33441f8a0dac29dd33cad205
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-11-18 11:47:59 -0200

    Fix sendmail script to execute with cron service or something similar and updated documentation.

--- scripts/notifications.sh
+++ scripts/notifications.sh
@@ -18,4 +18,10 @@ else
     CONFIG=config.php
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CDIR=$( pwd )
+cd $DIR
+
 MAPASCULTURAIS_CONFIG_FILE=$CONFIG HTTP_HOST=$DOMAIN REQUEST_METHOD='CLI' REMOTE_ADDR='127.0.0.1' REQUEST_URI='/' SERVER_NAME=127.0.0.1 SERVER_PORT="8000" php ../src/protected/send-notifications.php $SAVE_LOG
+
+cd $CDIR
