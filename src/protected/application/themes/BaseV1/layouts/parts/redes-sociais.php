6bcda50777840ba229d405f28a9e096d7ad36b1c
Leo Germani <leogermani@hacklab.com.br>
2016-11-24 11:21:33 -0200

    substituindo _e por esc_attr_e em alguns lugares

--- src/protected/application/themes/BaseV1/layouts/parts/redes-sociais.php
+++ src/protected/application/themes/BaseV1/layouts/parts/redes-sociais.php
@@ -24,19 +24,19 @@
         <h3><?php \MapasCulturais\i::_e("Seguir");?></h3>
 
         <?php if ($this->isEditable() || $entity->twitter): ?>
-        <a class="icon icon-twitter js-editable" data-edit="twitter" data-notext="true" data-original-title="<?php \MapasCulturais\i::_e("Perfil no Twitter");?>"
+        <a class="icon icon-twitter js-editable" data-edit="twitter" data-notext="true" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Perfil no Twitter");?>"
            href="<?php echo $entity->twitter ? $entity->twitter : '#" onclick="return false; ' ?>"
            data-value="<?php echo $entity->twitter ?>"></a>
         <?php endif; ?>
 
         <?php if ($this->isEditable() || $entity->facebook): ?>
-        <a class="icon icon-facebook js-editable" data-edit="facebook" data-notext="true" data-original-title="<?php \MapasCulturais\i::_e("Perfil no Facebook");?>"
+        <a class="icon icon-facebook js-editable" data-edit="facebook" data-notext="true" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Perfil no Facebook");?>"
            href="<?php echo $entity->facebook ? $entity->facebook : '#" onclick="return false; ' ?>"
            data-value="<?php echo $entity->facebook ?>"></a>
         <?php endif; ?>
 
         <?php if ($this->isEditable() || $entity->googleplus): ?>
-        <a class="icon icon-googleplus js-editable" data-edit="googleplus" data-notext="true" data-original-title="<?php \MapasCulturais\i::_e("Perfil no Google Plus");?>"
+        <a class="icon icon-googleplus js-editable" data-edit="googleplus" data-notext="true" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Perfil no Google Plus");?>"
            href="<?php echo $entity->googleplus ? $entity->googleplus : '#" onclick="return false; ' ?>"
            data-value="<?php echo $entity->googleplus ?>"></a>
         <?php endif; ?>
