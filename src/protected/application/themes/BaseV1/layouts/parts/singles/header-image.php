f412438a157e011fcdbc5fce04021be7801fcfdf
Leo Germani <leogermani@hacklab.com.br>
2016-11-28 14:42:17 -0200

    corrige erro no template da imagem de cacecalho

--- src/protected/application/themes/BaseV1/layouts/parts/singles/header-image.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/header-image.php
@@ -8,7 +8,7 @@ if ($header = $entity->getFile('header')){
 <?php $this->applyTemplateHook('header-image','before'); ?>
 <div class="header-image js-imagem-do-header" style="<?php echo $style ?>">        
     <?php if ($this->isEditable()): ?>
-        <a class="btn btn-default edit js-open-editbox" data-target="#editbox-change-header" href="#"<?php \MapasCulturais\i::_e("Editar");?>/a>
+        <a class="btn btn-default edit js-open-editbox" data-target="#editbox-change-header" href="#"><?php \MapasCulturais\i::_e("Editar");?></a>
         <div id="editbox-change-header" class="js-editbox mc-bottom" title="<?php \MapasCulturais\i::_e("Editar Imagem da Capa");?>">
             <?php $this->ajaxUploader($entity, 'header', 'background-image', '.js-imagem-do-header', '', 'header'); ?>
         </div>
