414680e5057b6cbf3e39457a6fd14ce48bcbb044
Rafael Chaves Freitas <rafael.chaves.freitas@gmai.com>
2016-12-09 17:13:08 -0200

    fix seal thumbnail creation

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -243,5 +243,23 @@ return [
                         where name = 'superAdmin'
                     )");
         $conn->executeQuery("UPDATE seal_relation SET owner_id = '$agent_id' WHERE owner_id IS NULL;");
+    },
+    
+    'create avatar thumbs' => function() use($conn){
+        $conn->executeQuery("DELETE FROM file WHERE object_type = 'MapasCulturais\Entities\Agent' AND object_id NOT IN (SELECT id FROM agent)");
+        $conn->executeQuery("DELETE FROM file WHERE object_type = 'MapasCulturais\Entities\Space' AND object_id NOT IN (SELECT id FROM space)");
+        $conn->executeQuery("DELETE FROM file WHERE object_type = 'MapasCulturais\Entities\Project' AND object_id NOT IN (SELECT id FROM project)");
+        $conn->executeQuery("DELETE FROM file WHERE object_type = 'MapasCulturais\Entities\Event' AND object_id NOT IN (SELECT id FROM event)");
+        $conn->executeQuery("DELETE FROM file WHERE object_type = 'MapasCulturais\Entities\Seal' AND object_id NOT IN (SELECT id FROM seal)");
+        
+        $files = $this->repo('SealFile')->findBy(['group' => 'avatar']);
+        
+        foreach($files as $f){
+            $f->transform('avatarSmall');
+            $f->transform('avatarMedium');
+            $f->transform('avatarBig');
+        }
+        
+        $this->disableAccessControl();
     }
 ];
