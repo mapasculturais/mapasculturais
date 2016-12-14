5477c3bba71481fc1dd17848d4fd476d52372326
Rafael Chaves Freitas <rafael.chaves.freitas@gmai.com>
2016-12-14 18:03:34 -0200

    new subsite template parts (refs: #1017)

--- src/protected/application/themes/BaseV1/views/subsite/single.php
+++ src/protected/application/themes/BaseV1/views/subsite/single.php
@@ -11,12 +11,6 @@ $this->includeMapAssets();
 
 $this->part('editable-entity', array('entity'=>$entity, 'action'=>$action));
 
-$editEntity = $this->isEditable();
-
-$slag_editbla = $this->controller->action === 'create';
-
-
-
 $this->enqueueScript('app', 'subsite-map', 'js/single-subsite.js', ['map']);
 
 ?>
@@ -72,39 +66,7 @@ $this->enqueueScript('app', 'subsite-map', 'js/single-subsite.js', ['map']);
 
 <article class="main-content subsite-container">
 
-    <!--.header-image-->
-    <div class="header-content">
-            <?php $this->applyTemplateHook('header-content','begin'); ?>
-            <?php $this->part('singles/avatar', ['entity' => $entity, 'default_image' => 'img/avatar--space.png']); ?>
-            <?php $this->applyTemplateHook('header-content','end'); ?>
-
-            <?php if($this->isEditable() || $entity->nome_instalacao): ?>
-                <p>
-                    <span class="setup-name js-editable required" data-edit="name" data-original-title="Nome da Instalação" data-emptytext="Nome da instalação"><?php echo $entity->name; ?></span>
-                </p>
-            <?php endif; ?>
-
-            <div>
-                <span class="icon"></span><span class="label">Tema:</span>
-                <span class="js-editable required" data-edit="namespace" data-original-title="Tema" data-emptytext="Selecione a o tema a ser utilizado"><?php echo $entity->namespace; ?></span>
-            </div>
-
-            <div>
-                <span class="icon"></span><span class="label">Domínio Principal:</span>
-                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"url") && $editEntity? 'required': '');?>" data-edit="url" data-original-title="Domínio Principal" data-emptytext="Ex: mapas.cultura.gov.br"><?php echo $entity->url; ?></span>
-            </div>
-
-            <div>
-                <span class="icon"></span><span class="label">Domínio Secundário:</span>
-                <span class="js-editable" data-edit="aliasUrl" data-original-title="Domínio Secundário" data-emptytext="Ex: mapas.cultura.gov.br"><?php echo $entity->aliasUrl; ?></span>
-            </div>
-
-    </div>
-    <!--.header-content-->
-    <?php $this->applyTemplateHook('header-content','after'); ?>
-
-    <!--.main-content-header-->
-    <?php $this->applyTemplateHook('header','after'); ?>
+    <?php $this->part('singles/subsite-header', ['entity' => $entity]) ?>
 
     <?php $this->applyTemplateHook('tabs','before'); ?>
     <br>
