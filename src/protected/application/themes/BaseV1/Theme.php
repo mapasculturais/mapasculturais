524fc148fca5fe5b5d5d442ffda9fb88f004f4a1
Marylly Araújo Silva <marylly@hacklab.com.br>
2017-01-24 13:37:53 -0200

    Seals allowed to super admin and saas super admin users.

--- src/protected/application/themes/BaseV1/Theme.php
+++ src/protected/application/themes/BaseV1/Theme.php
@@ -1839,7 +1839,7 @@ class Theme extends MapasCulturais\Theme {
     	if (!$app->user->is('guest')) {
     		$this->jsObject['allowedSeals'] = $app->controller('seal')->apiQuery($query);
 
-        	if($app->user->is('admin') || $app->user->is('superAdmin') || $this->jsObject['allowedSeals'] > 0) {
+        	if($app->user->is('admin') || $app->user->is('superAdmin') || $app->user->is('saasSuperAdmin') || $this->jsObject['allowedSeals'] > 0) {
         		$this->jsObject['canRelateSeal'] = true;
         	} else {
         		$this->jsObject['canRelateSeal'] = false;
