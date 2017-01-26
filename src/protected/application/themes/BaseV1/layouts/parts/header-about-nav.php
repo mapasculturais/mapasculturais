ad42907897e655c359c3fb228228b1c5fffdfdef
Marylly Araújo Silva <marylly@hacklab.com.br>
2017-01-26 11:06:06 -0200

    Fix institute logo url calling.

--- src/protected/application/themes/BaseV1/layouts/parts/header-about-nav.php
+++ src/protected/application/themes/BaseV1/layouts/parts/header-about-nav.php
@@ -1,5 +1,5 @@
 <nav id="about-nav" class="alignright clearfix">
-    <h1 id="organization-logo"><a href="#"><img src="<?php $this->asset('img/marca-da-org.png'); ?>" /></a></h1>
+    <h1 id="organization-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/marca-da-org.png'); ?>" /></a></h1>
     <ul id="secondary-menu">
         <li><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="<?php \MapasCulturais\i::esc_attr_e("Sobre os Mapas Culturais");?>"></a></li>
         <li><a class="icon icon-help hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="<?php \MapasCulturais\i::esc_attr_e("Como usar");?>"></a></li>
