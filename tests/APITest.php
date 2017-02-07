1e1cb9dfad74a0f87ec69eaeb2d0cc6b2998fbcb
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2017-02-07 17:54:56 -0200

    added api tests and fixed select sintax of new api

--- tests/APITest.php
+++ tests/APITest.php
@@ -27,25 +27,94 @@ class APITest extends MapasCulturais_TestCase {
         $rs = $this->api($entity, $_params);
         return $rs->count();
     }
-//
-//    function testFindOneMethod() {
-//        $props = 'id,name,shortDescription';
-//        
-//        $agent1 = $this->app->repo('Agent')->find(1)->simplify($props);
-//        $response1 = $this->findOne('agent', [
-//            'id=EQ(1)',
-//            '@select=' . $props
-//        ]);
-//        
-//        $this->assertEquals($agent1, $response1);
-//        
-//        $agent2 = $this->app->repo('Agent')->find(2)->simplify($props);
-//        $response2 = $this->findOne('agent', [
-//            'id=EQ(2)',
-//            '@select=' . $props
-//        ]);
-//        
-//        $this->assertEquals($agent2, $response2);
-//    }
 
+    function testFindOneMethod() {
+        $simplify = function($entity, $props){
+            return (array) $entity->simplify($props);
+        };
+        
+        $cps = [
+            'id' => $simplify,
+            'id,singleUrl,editUrl,deleteUrl,destroyUrl,endereco,En_CEP' => $simplify,
+
+            'id,name,user.{id,email,profile.{id,name,singleUrl,editUrl,deleteUrl,endereco,En_CEP}}' => function($entity){
+                $user = $entity->ownerUser;
+                $profile = $user->profile;
+                return [
+                    'id' => $entity->id,
+                    'name' => $entity->name,
+                    'user' => [
+                        'id' => $user->id,
+                        'email' => $user->email,
+                        'profile' => [
+                            'id' => $profile->id,
+                            'name' => $profile->name,
+                            'singleUrl' => $profile->singleUrl,
+                            'editUrl' => $profile->editUrl,
+                            'deleteUrl' => $profile->deleteUrl,
+                            'endereco' => $profile->endereco,
+                            'En_CEP' => $profile->En_CEP
+                        ]
+                    ]
+                ];
+            },
+            'id,name,user.id,user.email,user.profile.id,user.profile.name,user.profile.singleUrl,user.profile.endereco' => function($entity){
+                $user = $entity->ownerUser;
+                $profile = $user->profile;
+                return [
+                    'id' => $entity->id,
+                    'name' => $entity->name,
+                    'user' => [
+                        'id' => $user->id,
+                        'email' => $user->email,
+                        'profile' => [
+                            'id' => $profile->id,
+                            'name' => $profile->name,
+                            'singleUrl' => $profile->singleUrl,
+                            'endereco' => $profile->endereco
+                        ]
+                    ]
+                ];
+            },
+        ];
+        
+        foreach([null,'superAdmin','admin','normal'] as $user){
+            $this->user = $user;
+            foreach(['Event'] as $class){
+                $entities = $this->app->repo($class)->findAll();
+                
+                foreach($entities as $entity){
+                    if($entity->status < 1) continue;
+                    
+                    foreach($cps as $props => $proc){
+                        $response = $this->apiFindOne($class, "@select={$props}&id=EQ({$entity->id})");
+                        $_entity = $proc($entity, $props);
+                        
+                        $this->assertEquals($_entity, $response, "asserting {$class} api findOne to \$user");
+                    }
+                }
+            }
+        }
+    }
+            
+    function testQuerySintax(){
+        $this->user = 'admin';
+        
+        $s1 = 'id,name,user.id,user.email,user.profile.id,user.profile.name,user.profile.singleUrl,user.profile.endereco,user.profile.spaces.id,user.profile.spaces.name,user.profile.spaces.singleUrl,user.profile.spaces.endereco';
+        $s2 = 'id,name,user.{id,email,profile.id,profile.name,profile.singleUrl,profile.endereco,profile.spaces.id,profile.spaces.name,profile.spaces.singleUrl,profile.spaces.endereco}';
+        $s3 = 'id,name,user.{id,email,profile.{id,name,singleUrl,endereco,spaces.id,spaces.name,spaces.singleUrl,spaces.endereco}}';
+        $s4 = 'id,name,user.{id,email,profile.{id,name,singleUrl,endereco,spaces.{id,name,singleUrl,endereco}}}';
+        
+        $entities = $this->app->repo('Event')->findAll();
+        foreach($entities as $entity){
+            $r1 = $this->apiFind('event', "id=EQ($entity->id)&@select=$s1");
+            $r2 = $this->apiFind('event', "id=EQ($entity->id)&@select=$s2");
+            $r3 = $this->apiFind('event', "id=EQ($entity->id)&@select=$s3");
+            $r4 = $this->apiFind('event', "id=EQ($entity->id)&@select=$s4");
+            
+            $this->assertEquals($r1, $r2, 'asserting same result for different sitaxes');
+            $this->assertEquals($r1, $r3, 'asserting same result for different sitaxes');
+            $this->assertEquals($r1, $r4, 'asserting same result for different sitaxes');
+        }
+    }
 }
