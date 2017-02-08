df1796b1e6a8cc6fe360bb78b193d2db10937bcd
Leo Germani <leogermani@hacklab.com.br>
2017-02-08 15:20:09 -0200

    FIX verificacao dos selos relacionados a um projeto

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--seals.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--seals.php
@@ -2,9 +2,10 @@
 if(!$app->isEnabled('seals'))
 	return;
 
-$owner = isset($project->registrationSeals->owner) ? $project->registrationSeals->owner : '';
-$institution = isset($project->registrationSeals->institution) ? $project->registrationSeals->institution : '';
-$collective = isset($project->registrationSeals->collective) ? $project->registrationSeals->collective : '';
+$seals = $project->registrationSeals;
+$owner = isset($seals->owner) ? $seals->owner : '';
+$institution = isset($seals->institution) ? $seals->institution : '';
+$collective = isset($seals->collective) ? $seals->collective : '';
 
 $this->addSealsToJs(false, [$owner, $institution, $collective]);
 ?>
