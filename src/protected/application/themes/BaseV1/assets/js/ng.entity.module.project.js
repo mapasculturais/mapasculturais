744175cce03784915f19766e34e1f52ed3a5687c
Leo Germani <leogermani@hacklab.com.br>
2017-02-09 09:50:41 -0200

    fix checagem ao fazer inscrição (fixes LibreCoopUruguay/mapasculturais#80 refs hacklabr/mapasculturais#1103)

--- src/protected/application/themes/BaseV1/assets/js/ng.entity.module.project.js
+++ src/protected/application/themes/BaseV1/assets/js/ng.entity.module.project.js
@@ -1089,12 +1089,12 @@ module.controller('ProjectController', ['$scope', '$rootScope', '$timeout', 'Reg
                         ownerRegistration.push($scope.data.entity.registrations[i].owner);
                     }
                 }
-
+                
                 if(MapasCulturais.entity.object.registrationLimitPerOwner > 0 && ownerRegistration.length >= MapasCulturais.entity.object.registrationLimitPerOwner) {
                     MapasCulturais.Messages.error(labels['limitReached']);
                 }else if(MapasCulturais.entity.object.registrationLimit > 0 && registration.owner && $scope.data.entity.registrations.length >= MapasCulturais.entity.object.registrationLimit){
                     MapasCulturais.Messages.error(labels['VacanciesOver']);
-                }else if(registration.owner && ((!MapasCulturais.entity.object.registrationLimit) || $scope.data.entity.registrations.length <= MapasCulturais.entity.object.registrationLimit)){
+                }else if(registration.owner && (!MapasCulturais.entity.object.registrationLimit || MapasCulturais.entity.object.registrationLimit == 0 || $scope.data.entity.registrations.length <= MapasCulturais.entity.object.registrationLimit)){
                     RegistrationService.register(registration).success(function(rs){
                         document.location = rs.editUrl;
                     });
