0942a1f4373cc829d2e9f4fdb55b270e80b53f17
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-04-04 15:11:23 -0300

    Add hooks to panel/highlighted-message part

--- src/protected/application/themes/BaseV1/layouts/parts/panel/highlighted-message.php
+++ src/protected/application/themes/BaseV1/layouts/parts/panel/highlighted-message.php
@@ -1,3 +1,9 @@
-<p class="highlighted-message">
-    <?php \MapasCulturais\i::_e("Olá");?>, <a href="<?php echo $app->user->profile->singleUrl ?>"><?php echo $app->user->profile->name ?></a>, <?php \MapasCulturais\i::_e("bem-vindo ao painel do");?> <?php $this->dict('site: name'); ?>!
-</p>
+<?php $this->applyTemplateHook('highlighted-message','before'); ?>
+    <p class="highlighted-message">
+        <?php $this->applyTemplateHook('highlighted-message','begin'); ?>
+
+        <?php \MapasCulturais\i::_e("Olá");?>, <a href="<?php echo $app->user->profile->singleUrl ?>"><?php echo $app->user->profile->name ?></a>, <?php \MapasCulturais\i::_e("bem-vindo ao painel do");?> <?php $this->dict('site: name'); ?>!
+
+        <?php $this->applyTemplateHook('highlighted-message','end'); ?>
+    </p>
+<?php $this->applyTemplateHook('highlighted-message','after'); ?>
