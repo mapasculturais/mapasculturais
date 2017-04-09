f6b3371b395c9617342377c2b0f95aa75433dda8
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2017-04-08 21:31:45 -0300

    fix opportunity link

--- src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--content-single.php
+++ src/protected/application/lib/modules/EntityOpportunities/layouts/parts/entity-opportunities--content-single.php
@@ -6,7 +6,7 @@ $evaluation_methods = $app->getRegisteredEvaluationMethods();
 <div id="entity-opportunities" class="aba-content">
     <ul>
         <?php foreach($entity->opportunities as $opp): ?>
-        <li><a href="<?php echo $opp->editUrl ?>"><?php echo $opp->name ?></a></li>
+        <li><a href="<?php echo $opp->singleUrl ?>"><?php echo $opp->name ?></a></li>
         <?php endforeach; ?>
     </ul>
 </div>
