4de69914d6c3bf238a006176ce510157d9d62dd2
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2017-04-09 23:03:18 -0300

    fix project events list

--- src/protected/application/themes/BaseV1/layouts/parts/singles/project-events.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/project-events.php
@@ -17,24 +17,24 @@
         </div>
 
 
-        <article class="objeto clearfix" ng-repeat="event in events" ng-show="!event.hidden" ng-class="{'selected': event.selected, 'evt-publish': event.status == 1, 'evt-draft': event.status == 0}">
+        <article class="objeto clearfix" ng-repeat="event in ::events" ng-show="!event.hidden" ng-class="{'selected': event.selected, 'evt-publish': event.status == 1, 'evt-draft': event.status == 0}">
             <h1><input type='checkbox' ng-model="event.selected" ng-checked="event.selected">
-                <a href='{{event.singleUrl}}'>{{event.name}}</a></h1>
+                <a href='{{::event.singleUrl}}'>{{::event.name}}</a></h1>
             <div class="objeto-content clearfix">
-                <div class="objeto-thumb"><img src="" ng-src="{{event['@files:avatar.avatarSmall'] ? event['@files:avatar.avatarSmall'].url : data.assets.avatarEvent }}"></div>
+                <div class="objeto-thumb"><img src="" ng-src="{{::event['@files:avatar.avatarSmall'] ? event['@files:avatar.avatarSmall'].url : data.assets.avatarEvent }}"></div>
                 <div class="objeto-resumo">
                     <ul class="event-ocurrences">
-                        <li ng-repeat='occ in event.occurrences'>
-                            <a href="{{occ.space.singleUrl}}">{{occ.space.name}}</a> - {{occ.rule.description}} <span ng-if='occ.rule.price'>({{occ.rule.price}})</span>
+                        <li ng-repeat='occ in ::event.occurrences'>
+                            <a href="{{::occ.space.singleUrl}}">{{::occ.space.name}}</a> - {{::occ.rule.description}} <span ng-if='::occ.rule.price'>({{::occ.rule.price}})</span>
                         </li>
                     </ul>
                 </div>
 
                 <div class="objeto-meta">
                     <div><span class="label"><?php \MapasCulturais\i::_e("Status");?>:</span> {{event.status === 0 ? 'rascunho' : 'publicado'}}</div>
-                    <div><span class="label"><?php \MapasCulturais\i::_e("Autor");?>:</span> <a href='{{event.owner.singleUrl}}'>{{event.owner.name}}</a></div>
-                    <div><span class="label"><?php \MapasCulturais\i::_e("Linguagem");?>:</span> {{event.terms.linguagem.join(', ')}}</div>
-                    <div><span class="label"><?php \MapasCulturais\i::_e("Classificação");?>:</span> {{event.classificacaoEtaria}}</div>
+                    <div><span class="label"><?php \MapasCulturais\i::_e("Autor");?>:</span> <a href='{{::event.owner.singleUrl}}'>{{::event.owner.name}}</a></div>
+                    <div><span class="label"><?php \MapasCulturais\i::_e("Linguagem");?>:</span> {{::event.terms.linguagem.join(', ')}}</div>
+                    <div><span class="label"><?php \MapasCulturais\i::_e("Classificação");?>:</span> {{::event.classificacaoEtaria}}</div>
                 </div>
             </div>
         </article>
