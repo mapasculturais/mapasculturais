520b0d02d850ae389622da886b53b1c31edb505d
Leo Germani <leogermani@hacklab.com.br>
2017-05-12 17:10:32 -0300

    melhorando texto de ajuda do campo de espaço relacionado

--- src/protected/application/themes/BaseV1/layouts/parts/singles/project-registrations--space-relations.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/project-registrations--space-relations.php
@@ -13,8 +13,8 @@ $option_label = $entity->$metadata_name ? $entity->$metadata_name : 'dontUse';
 
 <?php if ($entity->isRegistrationOpen() || $this->isEditable()): ?>
         <div id="registration-space-relation" class="registration-fieldset">
-            <h4>4. <?php \MapasCulturais\i::_e("Espaços Culturais");?></h4>
-            <p class="registration-help"><?php \MapasCulturais\i::_e("Selecione o espaço relacionado com o evento");?></p>
+            <h4>4. <?php \MapasCulturais\i::_e("Espaço Cultural");?></h4>
+            <p class="registration-help"><?php \MapasCulturais\i::_e("Uma inscrição pode pedir para que o agente relacione um Espaço Cultural a ela. Indique aqui se quer habilitar esta opção.");?></p>
            <span class="<?php echo $ditable_class ?>" data-edit="<?php echo $metadata_name ?>" data-original-title="Selecione" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione uma opção");?>"><?php echo $option_label ?></span>
         </div>
 
