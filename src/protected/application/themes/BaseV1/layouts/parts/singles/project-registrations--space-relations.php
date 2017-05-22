23d56b7a8cfabf56940559352b05ed44922a9605
Ari Mourao <amlima@gmail.com>
2017-05-22 16:25:06 -0300

    fix issue #2

--- src/protected/application/themes/BaseV1/layouts/parts/singles/project-registrations--space-relations.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/project-registrations--space-relations.php
@@ -1,4 +1,6 @@
 <?php
+use MapasCulturais\App;
+
 $can_edit = $entity->canUser('modifyRegistrationFields');
 
 $ditable_class = $can_edit ? 'js-editable' : '';
@@ -9,13 +11,23 @@ $metadata_name = 'useSpaceRelation';
 
 $option_label = $entity->$metadata_name ? $entity->$metadata_name : 'dontUse';
 
+$projectMeta = \MapasCulturais\Entities\Project::getPropertiesMetadata();
+$message = $projectMeta['useSpaceRelation']['options'];
 ?>
 
 <?php if ($entity->isRegistrationOpen() || $this->isEditable()): ?>
         <div id="registration-space-relation" class="registration-fieldset">
             <h4>4. <?php \MapasCulturais\i::_e("Espaço Cultural");?></h4>
-            <p class="registration-help"><?php \MapasCulturais\i::_e("Uma inscrição pode pedir para que o agente relacione um Espaço Cultural a ela. Indique aqui se quer habilitar esta opção.");?></p>
-           <span class="<?php echo $ditable_class ?>" data-edit="<?php echo $metadata_name ?>" data-original-title="Selecione" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione uma opção");?>"><?php echo $option_label ?></span>
+            <p class="registration-help">
+                <?php \MapasCulturais\i::_e("Uma inscrição pode pedir para que o agente relacione um Espaço Cultural a ela. 
+                                            Indique aqui se quer habilitar esta opção.");?>
+            </p>
+           <span class="<?php echo $ditable_class ?>" 
+                 data-edit="<?php echo $metadata_name ?>" 
+                 data-original-title="Selecione" 
+                 data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione uma opção");?>">
+                <?php \MapasCulturais\i::_e($message[$option_label]); ?>
+           </span>
         </div>
 
     <?php else: ?>
