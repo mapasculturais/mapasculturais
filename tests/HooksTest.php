54a447be4da80d5ca05350e49407c0bbfe5ce5fd
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2017-12-11 17:13:40 -0200

    melhora o teste de ordem de execução dos hooks

--- tests/HooksTest.php
+++ tests/HooksTest.php
@@ -7,21 +7,31 @@ class HooksTest extends MapasCulturais_TestCase{
         $result = [];
 
         $app->hook('test hook order', function() use(&$result){
+            $result[] = 4;
+        }, 11);
+
+        $app->hook('test hook order', function() use(&$result){
             $result[] = 1;
-        });
+        }, 10);
 
         $app->hook('test hook order', function() use(&$result){
             $result[] = 2;
-        });
+        }, 10);
 
         $app->hook('test hook order', function() use(&$result){
             $result[] = 3;
-        });
+        }, 10);
+
+        $app->hook('test hook order', function() use(&$result){
+            $result[] = 0;
+        }, 9);
 
         $app->applyHook('test hook order');
 
-        $this->assertEquals(1, $result[0]);
-        $this->assertEquals(2, $result[1]);
-        $this->assertEquals(3, $result[2]);
+        $this->assertEquals(0, $result[0]);
+        $this->assertEquals(1, $result[1]);
+        $this->assertEquals(2, $result[2]);
+        $this->assertEquals(3, $result[3]);
+        $this->assertEquals(4, $result[4]);
     }
 }
