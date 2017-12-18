71de3d352747074117a236a3a67d3acef516d874
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-12-18 10:59:39 -0200

    Correção de nomes
    Foram alterados erros no nome de variaveis no módulo de oportunidades

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--categories.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--categories.php
@@ -1,7 +1,7 @@
 <?php
 $can_edit = $entity->canUser('modifyRegistrationFields');
 
-$ditable_class = $can_edit ? 'js-editable' : '';
+$editable_class = $can_edit ? 'js-editable' : '';
 
 if ($can_edit) {
     $registration_categories = $entity->registrationCategories ? implode("\n", $entity->registrationCategories) : '';
@@ -15,16 +15,16 @@ if ($can_edit) {
     <p ng-if="!data.entity.canUserModifyRegistrationFields" class="registration-help"><?php \MapasCulturais\i::_e("A edição destas opções estão desabilitadas porque agentes já se inscreveram neste projeto.");?> </p>
     <p>
         <span class="label"><?php \MapasCulturais\i::_e("Título das opções");?></span><br>
-        <span class="<?php echo $ditable_class ?>" data-edit="registrationCategTitle" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Título das opções");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um título para o campo de opções");?>"><?php echo $entity->registrationCategTitle ? $entity->registrationCategTitle : \MapasCulturais\i::__('Categoria'); ?></span>
+        <span class="<?php echo $editable_class ?>" data-edit="registrationCategTitle" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Título das opções");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira um título para o campo de opções");?>"><?php echo $entity->registrationCategTitle ? $entity->registrationCategTitle : \MapasCulturais\i::__('Categoria'); ?></span>
     </p>
     <p>
         <span class="label"><?php \MapasCulturais\i::_e("Descrição das opções");?></span><br>
-        <span class="<?php echo $ditable_class ?>" data-edit="registrationCategDescription" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Descrição das opções");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira uma descrição para o campo de opções");?>"><?php echo $entity->registrationCategDescription ? $entity->registrationCategDescription : \MapasCulturais\i::__('Selecione uma categoria'); ?></span>
+        <span class="<?php echo $editable_class ?>" data-edit="registrationCategDescription" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Descrição das opções");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira uma descrição para o campo de opções");?>"><?php echo $entity->registrationCategDescription ? $entity->registrationCategDescription : \MapasCulturais\i::__('Selecione uma categoria'); ?></span>
     </p>
     <p>
         <span class="label"><?php \MapasCulturais\i::_e("Opções");?></span><br>
 
-        <span class="<?php echo $ditable_class ?> js-categories-values" data-edit="registrationCategories" data-type="textarea" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Opções de inscrição (coloque uma opção por linha)");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira as opções de inscrição");?>"><?php echo $registration_categories; ?></span>
+        <span class="<?php echo $editable_class ?> js-categories-values" data-edit="registrationCategories" data-type="textarea" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Opções de inscrição (coloque uma opção por linha)");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira as opções de inscrição");?>"><?php echo $registration_categories; ?></span>
     </p>
 </div>
 <!-- #registration-categories -->
