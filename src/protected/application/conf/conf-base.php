3eafeb96663ef209eee7ddd3a8ee1a8b660a61c1
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2018-01-09 17:20:43 -0200

    Correção de texto dos campos necessários do agente para fazer uma inscrição

--- src/protected/application/conf/conf-base.php
+++ src/protected/application/conf/conf-base.php
@@ -209,7 +209,7 @@ return array(
         'required' => true,
         'label' => \MapasCulturais\i::__('Agente responsável pela inscrição'),
         'agentRelationGroupName' => 'owner',
-        'description' => \MapasCulturais\i::__('Agente individual (pessoa física) com os campos CPF, Data de Nascimento/Fundação, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
+        'description' => \MapasCulturais\i::__('Agente individual (pessoa física) com os campos CPF, Data de Nascimento/Fundação, Email Privado, Gênero, Telefone 1, Raça/cor, e Gênero obrigatoriamente preenchidos'),
         'type' => 1,
         'requiredProperties' => array('documento', 'raca', 'dataDeNascimento', 'genero', 'emailPrivado', 'telefone1')
     ),
