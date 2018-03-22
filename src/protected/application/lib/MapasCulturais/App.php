86a7cd2df101bf67b3b945bb496b719c53ea7b57
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-03-22 15:08:01 -0300

    adiciona arquivo/linha e chamada nos log de hooks

--- src/protected/application/lib/MapasCulturais/App.php
+++ src/protected/application/lib/MapasCulturais/App.php
@@ -1284,6 +1284,23 @@ class App extends \Slim\Slim{
         }
     }
 
+    function _logHook($name){
+        $n = 1;
+
+        if(strpos($name, 'template(') === 0){
+            $n = 2;
+        }
+
+        $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $filename = $bt[$n]['file'];
+        $fileline = $bt[$n]['line'];
+        $lines = file($filename);
+        $line = trim($lines[$fileline - 1]);
+
+        $this->log->debug("hook >> $name (\033[33m$filename:$fileline\033[0m)");
+        $this->log->debug("     >> \033[32m$line\033[0m\n");
+    }
+
     /**
      * Invoke hook
      * @param  string   $name       The hook name
@@ -1295,8 +1312,14 @@ class App extends \Slim\Slim{
         else if (!is_array($hookArg))
             $hookArg = [$hookArg];
 
-        if ($this->_config['app.log.hook'])
-            $this->log->debug('APPLY HOOK >> ' . $name);
+
+        if ($this->_config['app.log.hook']){
+            $conf = $this->_config['app.log.hook'];
+            if(is_bool($conf) || preg_match('#' . str_replace('*', '.*', $conf) . '#', $name)){
+                $this->_logHook($name);
+                
+            }
+        }
 
         $callables = $this->_getHookCallables($name);
         foreach ($callables as $callable) {
@@ -1317,8 +1340,12 @@ class App extends \Slim\Slim{
         else if (!is_array($hookArg))
             $hookArg = [$hookArg];
 
-        if ($this->_config['app.log.hook'])
-            $this->log->debug('APPLY HOOK BOUND TO >> ' . $name);
+        if ($this->_config['app.log.hook']){
+            $conf = $this->_config['app.log.hook'];
+            if(is_bool($conf) || preg_match('#' . str_replace('*', '.*', $conf) . '#', $name)){
+                $this->_logHook($name);
+            }
+        }
 
         $callables = $this->_getHookCallables($name);
         foreach ($callables as $callable) {
