d5673fd4115e5f9b75a5967c3344a7896b48a6ea
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2018-03-23 16:10:52 -0300

    Fix 'shell.sh' to exec from any path

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-REQUEST_METHOD='CLI' REMOTE_ADDR='127.0.0.1' REQUEST_URI='/' SERVER_NAME=127.0.0.1 SERVER_PORT="8000" php ../src/protected/tools/psysh.php
+REQUEST_METHOD='CLI' REMOTE_ADDR='127.0.0.1' REQUEST_URI='/' SERVER_NAME=127.0.0.1 SERVER_PORT="8000" php $(dirname $0)/../src/protected/tools/psysh.php
