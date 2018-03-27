c498a1fa8c7582d4e985ee6044c573e3cbfa4a58
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-03-27 10:47:22 -0300

    inicio do desenvolvimento da fila de execução da recriação de cache de permissão

---
+++ src/protected/application/lib/MapasCulturais/Entities/PermissionCachePending.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MapasCulturais\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Role
+ *
+ * @ORM\Table(name="permission_cache_pending")
+ * @ORM\Entity
+ * @ORM\entity(repositoryClass="MapasCulturais\Repository")
+ */
+class PermissionCachePending extends \MapasCulturais\Entity{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="role_id_seq", allocationSize=1, initialValue=1)
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="object_id", type="integer", nullable=false)
+     */
+    protected $objectId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="object_type", type="string", length=255, nullable=false)
+     */
+    protected $objectType;
+
+    /**
+     * @var \MapasCulturais\Entities\User
+     *
+     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\User", cascade="persist", )
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="usr_id", referencedColumnName="id", nullable=false)
+     * })
+     */
+    protected $user;
+}
