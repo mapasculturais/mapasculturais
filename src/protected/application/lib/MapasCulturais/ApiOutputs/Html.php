af9cb247c367aa8c2c2507b8ea332fb4c3d9fddd
Asafe Ramos <asafe@asaferamos.com>
2018-03-29 16:15:20 -0300

    resolve problema com acesso as tabs por hash no tema basev1

--- src/protected/application/lib/MapasCulturais/ApiOutputs/Html.php
+++ src/protected/application/lib/MapasCulturais/ApiOutputs/Html.php
@@ -426,6 +426,14 @@ class Html extends \MapasCulturais\ApiOutput{
                     <?php foreach($first_item_keys as $k): ?><?php
                         if($k==='terms'){
                             $v = $item->$k;
+                            
+                            \dump($v);
+                            
+                            $itens = array($v);
+                                                        
+                            foreach ($v as $key => $item) {
+                                \dump($key);
+                            }
 
                             if(property_exists($v, 'area')){ ?><th><?php echo $this->convertToUTF16($this->translate('area')); ?></th><?php }
                             if(property_exists($v, 'tag')){ ?><th><?php echo $this->convertToUTF16($this->translate('tag')); ?></th><?php }
@@ -454,7 +462,7 @@ class Html extends \MapasCulturais\ApiOutput{
                             </th>
                         <?php
                         }
-                    ?><?php endforeach; ?>
+                    ?><?php endforeach; die; ?>
                     <th></th>
                 </tr>
 
