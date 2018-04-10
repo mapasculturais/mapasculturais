8c78dd441c9552a81f22c4f536e4ac63fa8ccb58
Vinicius Nunes <vnicius.nm.ba@gmail.com>
2018-04-10 10:55:10 -0300

    manter a compatibilidade com versões antigas de configs backward compability, atulização documentação

--- src/protected/application/lib/MapasCulturais/App.php
+++ src/protected/application/lib/MapasCulturais/App.php
@@ -1101,12 +1101,21 @@ class App extends \Slim\Slim{
                 $key = substr($key, 1);
             }
             
-            $d = new \stdClass();
-            $d->key = $key;
-            $d->name = $division['name'];
-            $d->metakey = 'geo' . ucfirst($key);
-            $d->display = $display;
-            $result[] = $d;
+            if (!is_array($division)) { // for backward compability version < 4.0, $division is string not a array.
+                $d = new \stdClass();
+                $d->key = $key;
+                $d->name = $division;
+                $d->metakey = 'geo' . ucfirst($key);
+                $d->display = $display;
+                $result[] = $d;
+            } else {
+                $d = new \stdClass();
+                $d->key = $key;
+                $d->name = $division['name'];
+                $d->metakey = 'geo' . ucfirst($key);
+                $d->display = $display;
+                $result[] = $d;
+            }
         }
 
         return $result;
