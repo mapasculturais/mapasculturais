4cc2b21a66860ec7e2bc3fb8bb765cafb1291af6
leonardo <leotrujillo@vera.com.uy>
2018-04-20 18:45:39 +0000

    Update registration-edit--send-button.php
    se agrega función de internacionalización

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-edit--send-button.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-edit--send-button.php
@@ -6,7 +6,7 @@
         <p class="registration-help">
             <strong>
                 <?php // gets full date in the format "26 de {January} de 2015 às 17:00" and uses App translation to replace english month name inside curly brackets to the equivalent in portuguese. It avoids requiring the operating system to have portuguese locale as used in this example: http://pt.stackoverflow.com/a/21642
-                $date = strftime("%d de {%B} de %G às %H:%M", $entity->opportunity->registrationTo->getTimestamp());
+                $date = strftime( \MapasCulturais\i::__("%d de {%B} de %G às %H:%M") , $entity->opportunity->registrationTo->getTimestamp());
                 $full_date = preg_replace_callback("/{(.*?)}/", function($matches) use ($app) {
                     return strtolower($app::txt(str_replace(['{', '}'], ['',''], $matches[0]))); //removes curly brackets from the matched pattern and convert its content to lowercase
                 }, $date);
