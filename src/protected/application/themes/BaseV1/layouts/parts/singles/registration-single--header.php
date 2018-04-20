599a3618b755a64992b0648339e08daef704cdfc
leonardo <leotrujillo@vera.com.uy>
2018-04-20 18:20:27 +0000

    Update registration-single--header.php
    se agrega función de traducción

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--header.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--header.php
@@ -1,6 +1,6 @@
 <div class="alert success">
-    <?php \MapasCulturais\i::_e("Inscrição enviada no dia");?>
-    <?php echo $entity->sentTimestamp->format('d/m/Y à\s H:i:s'); ?>
+    <?php \MapasCulturais\i::_e("Inscrição enviada no dia");?>    
+    <?php echo $entity->sentTimestamp->format( i::__('d/m/Y à\s H:i:s', 'culturaenlinea') ); ?>
 </div>
 
 <h3 class="registration-header"><?php \MapasCulturais\i::_e("Formulário de Inscrição");?></h3>
