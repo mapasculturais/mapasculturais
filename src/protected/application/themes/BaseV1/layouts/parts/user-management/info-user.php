2fe259583bdaa51f145a1fb931e929d5ec4f286a
Vinicius Nunes <vnicius.nm.ba@gmail.com>
2018-06-28 17:53:47 -0300

    divisões em parts para cada entidade, exibição de entidade (ativas, rascunhos, arquivada, lixeira, excluida) #160

--- src/protected/application/themes/BaseV1/layouts/parts/user-management/info-user.php
+++ src/protected/application/themes/BaseV1/layouts/parts/user-management/info-user.php
@@ -5,7 +5,7 @@
   $current_user = $app->user;
   
   $subsitesAdmin = $app->repo('User')->getSubsitesAdminRoles($current_user->id);
-  $this->jsObject['subsitesAdmin'] = $subsitesAdmin;
+  $this->jsObject['subsitesAdmin'] = $subsitesAdmin;  
 ?>
 <div class="user-managerment-infos" ng-init="load(<?=$user->id?>)">  
   <div class="user-info">
@@ -56,208 +56,99 @@
       <li class="active"><a href="#agentes"><?php \MapasCulturais\i::_e("Agentes");?></a></li>
       <li><a href="#espacos"><?php \MapasCulturais\i::_e("Espaços");?></a></li>
       <li><a href="#eventos"><?php \MapasCulturais\i::_e("Eventos");?></a></li>
-      <li><a href="#permissoes"><?php \MapasCulturais\i::_e("Permissões");?></a></li>          
+      <li><a href="#permissoes"><?php \MapasCulturais\i::_e("Permissões");?></a></li>
       <li><a href="#atividade"><?php \MapasCulturais\i::_e("Atividades");?></a></li>
     </ul>
   </div>
     
   <div class="tabs-content">
+    
     <div id="agentes" class="aba-content">
-      <span ng-show="user.agents.spinnerShow">
-        <img src="<?php $this->asset('img/spinner.gif') ?>" />
-        <span><?php \MapasCulturais\i::_e("obtendo agentes..."); ?></span>
-      </span>
-      <div ng-show="!user.agents.spinnerShow">
-        <table class="agents-table entity-table">
-          <caption> 
-              <?=\MapasCulturais\i::_e("Agentes");?>
-          </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <td>Subsite</td>
-              <td>Operações</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="agent in user.agents.list">
-              <td class="fit">
-                <span ng-if="<?=$user->profile->id?> == agent.id">
-                  <a class="icon icon-agent active" title="<?php \MapasCulturais\i::esc_attr_e("Este é o agente padrão.");?>"></a>
-                  <a href={{agent.singleUrl}}>{{agent.id}}</a>
-                </span>
-                <span ng-if="<?=$user->profile->id?> != agent.id">
-                  <a class="icon icon-agent"></a>
-                  <a href={{agent.singleUrl}}>{{agent.id}}</a>
-                </span>
-              </td>
-              <td>{{agent.name}}</td>
-              <td class="fit">{{agent.subsite.name}}</td>
-              <td class="fit">
-                <span ng-if="hasAdmin(agent.subsite) || hasControl(agent.agentRelations, 'agent')">
-                  <a class="btn btn-small btn-danger" ng-if="agent.status == <?=Agent::STATUS_ENABLED?>" href="{{agent.deleteUrl}}"><?php \MapasCulturais\i::_e("excluir");?></a>
-                  <a class="btn btn-small btn-success" ng-if="agent.status == <?=Agent::STATUS_ENABLED?>" href="{{agent.archiveUrl}}"><?php \MapasCulturais\i::_e("arquivar");?></a>
-                  <a class="btn btn-small btn-warning" ng-if="agent.status == <?=Agent::STATUS_DRAFT?>" href="{{agent.publishUrl}}"><?php \MapasCulturais\i::_e("publicar");?></a>
-                  <a class="btn btn-small btn-success" ng-if="agent.status == <?=Agent::STATUS_ARCHIVED?>" href="{{agent.unarchiveUrl}}"><?php \MapasCulturais\i::_e("desarquivar");?></a>
-                  <a class="btn btn-small btn-success" ng-if="agent.status == <?=Agent::STATUS_ARCHIVED?>" href="{{agent.undeleteUrl}}"><?php \MapasCulturais\i::_e("recuperar");?></a>
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <table class="agents-table entity-table">
-          <caption> <?=\MapasCulturais\i::_e("Agentes controlados");?> </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <td>Subsite</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="agent in user.agents.relatedsAgents">
-              <td style="white-space: nowrap;  width:1%;">
-                <span>
-                  <a class="icon icon-agent"></a>
-                  <a href={{agent.singleUrl}}>{{agent.id}}</a>
-                </span>
-              </td>
-              <td>{{agent.name}}</td>
-              <td style="width:1%;">{{agent.subsite.name}}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="tab-table">
+        <button class="tablinks active" data-entity="agentes" data-tab="agents-ativos">     <?php \MapasCulturais\i::_e("Ativos");?>      (<?php echo count($user->enabledAgents);?>)   </button>
+        <button class="tablinks"        data-entity="agentes" data-tab="agents-concedidos"> <?php \MapasCulturais\i::_e("Concedidos");?>  (<?php echo count($user->hasControlAgents);?>)</button>
+        <button class="tablinks"        data-entity="agentes" data-tab="agents-rascunhos">  <?php \MapasCulturais\i::_e("Rascunhos");?>   (<?php echo count($user->draftAgents);?>)     </button>
+        <button class="tablinks"        data-entity="agentes" data-tab="agents-lixeira">    <?php \MapasCulturais\i::_e("Lixeira");?>     (<?php echo count($user->trashedAgents);?>)   </button>
+        <button class="tablinks"        data-entity="agentes" data-tab="agents-arquivo">    <?php \MapasCulturais\i::_e("Arquivo");?>     (<?php echo count($app->user->archivedAgents);?>)</button>
+      </div>
+      
+      <div>
+        <div id="agents-ativos" class="tab-content-table" style="display: block;">
+          <?php $this->part('user-management/user-info/info-agents', array('agents' => $user->enabledAgents)); ?>
+        </div>
+        <div id="agents-concedidos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-agents', array('agents' => $user->hasControlAgents)); ?>
+        </div>
+        <div id="agents-rascunhos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-agents', array('agents' => $user->draftAgents)); ?>
+        </div>
+        <div id="agents-lixeira" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-agents', array('agents' => $user->trashedAgents)); ?>
+        </div>
+        <div id="agents-arquivo" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-agents', array('agents' => $user->archivedAgents)); ?>
+        </div>
       </div>
     </div>
 
     <div id="espacos" class="aba-content">
-      <span ng-show="user.spaces.spinnerShow">
-        <img src="<?php $this->asset('img/spinner.gif') ?>" />
-        <span><?php \MapasCulturais\i::_e("obtendo espaços..."); ?></span>
-      </span>
-      <div ng-show="!user.spaces.spinnerShow">
-        <table class="spaces-table entity-table">
-          <caption> 
-              <?=\MapasCulturais\i::_e("Espaços");?>
-          </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <td>Subsite</td>
-              <td>Operações</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="space in user.spaces.list">
-              <td>
-                <a href={{space.singleUrl}} class="icon icon-space"></a>
-                <a href={{space.singleUrl}}>{{space.id}}</a>
-              </td>
-              <td>{{space.name}}</td>
-              <td>{{space.subsite.name}}</td>
-              <td class="fit">
-                <span ng-if="hasAdmin(space.subsite) || hasControl(space.agentRelations, 'agent')">
-                  <a class="btn btn-small  btn-danger" ng-if="space.status == <?=Space::STATUS_ENABLED?>"  href="{{space.deleteUrl}}">   <?php \MapasCulturais\i::_e("excluir");?></a>
-                  <a class="btn btn-small btn-success" ng-if="space.status == <?=Space::STATUS_ENABLED?>"  href="{{space.archiveUrl}}">  <?php \MapasCulturais\i::_e("arquivar");?></a>
-                  <a class="btn btn-small btn-warning" ng-if="space.status == <?=Space::STATUS_DRAFT?>"    href="{{space.publishUrl}}">  <?php \MapasCulturais\i::_e("publicar");?></a>
-                  <a class="btn btn-small  btn-danger" ng-if="space.status == <?=Space::STATUS_DRAFT?>"    href="{{space.deleteUrl}}">   <?php \MapasCulturais\i::_e("excluir");?></a>
-                  <a class="btn btn-small btn-success" ng-if="space.status == <?=Space::STATUS_ARCHIVED?>" href="{{space.unarchiveUrl}}"><?php \MapasCulturais\i::_e("desarquivar");?></a>
-                  <a class="btn btn-small btn-success" ng-if="space.status == <?=Space::STATUS_ARCHIVED?>" href="{{space.undeleteUrl}}"> <?php \MapasCulturais\i::_e("recuperar");?></a>
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
 
-        <table class="spaces-table entity-table">
-          <caption>
-              <?=\MapasCulturais\i::_e("Espaços controlados");?>
-          </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <td>Subsite</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="space in user.spaces.relatedsSpaces">
-              <td> 
-                <a href={{space.singleUrl}} class="icon icon-space"></a> 
-                <a href={{space.singleUrl}}>{{space.id}}</a> </td>
-              <td>{{space.name}}</td>
-              <td>{{space.subsite.name}}</td>
-            </tr>
-          </tbody>
-        </table>
-
+      <div class="tab-table">
+        <button class="tablinks active" data-entity="espacos" data-tab="spaces-ativos">    <?php \MapasCulturais\i::_e("Ativos");?>      (<?php echo count($user->enabledSpaces); ?>)  </button>
+        <button class="tablinks "       data-entity="espacos" data-tab="spaces-concedidos"><?php \MapasCulturais\i::_e("Concedidos");?>  (<?php echo count($user->hasControlSpaces);?>)</button>
+        <button class="tablinks "       data-entity="espacos" data-tab="spaces-rascunhos"> <?php \MapasCulturais\i::_e("Rascunhos");?>   (<?php echo count($user->draftSpaces); ?>)    </button>
+        <button class="tablinks "       data-entity="espacos" data-tab="spaces-lixeira">   <?php \MapasCulturais\i::_e("Lixeira");?>     (<?php echo count($user->trashedSpaces); ?>)  </button>
+        <button class="tablinks "       data-entity="espacos" data-tab="spaces-arquivo">   <?php \MapasCulturais\i::_e("Arquivo");?>     (<?php echo count($user->archivedSpaces); ?>) </button>
       </div>
+
+      <div>
+        <div id="spaces-ativos" class="tab-content-table" style="display: block;">
+          <?php $this->part('user-management/user-info/info-spaces', array('spaces' => $user->enabledSpaces)); ?>
+        </div>
+        <div id="spaces-concedidos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-spaces', array('spaces' => $user->hasControlSpaces)); ?>
+        </div>
+        <div id="spaces-rascunhos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-spaces', array('spaces' => $user->draftSpaces)); ?>
+        </div>
+        <div id="spaces-lixeira" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-spaces', array('spaces' => $user->trashedSpaces)); ?>
+        </div>
+        <div id="spaces-arquivo" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-spaces', array('spaces' => $user->archivedSpaces)); ?>
+        </div>
+      </div>
+    
     </div>
 
     <div id="eventos" class="aba-content">
-      <span ng-show="user.events.spinnerShow">
-        <img src="<?php $this->asset('img/spinner.gif') ?>" />
-        <span><?php \MapasCulturais\i::_e("obtendo eventos..."); ?></span>
-      </span>
-      <div ng-show="!user.spaces.spinnerShow">
-        <table class="events-table entity-table">
-          <caption>
-            <?=\MapasCulturais\i::_e("Eventos");?>
-          </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <td>Operações</td>
-              <!-- <td>Subsite</td> -->
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="event in user.events.list">
-              <td>
-                <a href={{event.singleUrl}} class="icon icon-event"></a>
-                <a href={{event.singleUrl}}>{{event.id}}</a>
-              </td>
-              <td>{{event.name}}</td>
-              <td class="fit">
-                <span ng-if="hasAdmin(event.subsite)">
-                  <a class="btn btn-small btn-danger"  ng-if="space.status == <?=Event::STATUS_ENABLED?>" href="{{event.deleteUrl}}"><?php \MapasCulturais\i::_e("excluir");?></a>
-                  <a class="btn btn-small btn-danger"  ng-if="space.status == <?=Event::STATUS_DRAFT?>"   href="{{event.deleteUrl}}"><?php \MapasCulturais\i::_e("excluir");?></a>
-                </span>
-              </td>
-              <!-- <td>{{event.subsite.name}}</td> -->
-            </tr>
-          </tbody>
-        </table>
 
-
-        <table class="events-table entity-table">
-          <caption>
-            <?=\MapasCulturais\i::_e("Eventos controlados");?>
-          </caption>
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>Nome</td>
-              <!-- <td>Subsite</td> -->
-            </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="event in user.events.relatedsSpaces">
-              <td> 
-              <td>
-                <a href={{event.singleUrl}} class="icon icon-event"></a>
-                <a href={{event.singleUrl}}>{{event.id}}</a>
-              </td>                
-              <td>{{event.name}}</td>
-              <!-- <td>{{event.subsite.name}}</td> -->
-            </tr>
-          </tbody>
-        </table>
+      <div class="tab-table">
+        <button class="tablinks active" data-entity="eventos" data-tab="events-ativos">    <?php \MapasCulturais\i::_e("Ativos");?>      (<?php echo count($user->enabledEvents); ?>)  </button>
+        <button class="tablinks "       data-entity="eventos" data-tab="events-concedidos"><?php \MapasCulturais\i::_e("Concedidos");?>  (<?php echo count($user->hasControlEvents);?>)</button>
+        <button class="tablinks "       data-entity="eventos" data-tab="events-rascunhos"> <?php \MapasCulturais\i::_e("Rascunhos");?>   (<?php echo count($user->draftEvents); ?>)    </button>
+        <button class="tablinks "       data-entity="eventos" data-tab="events-lixeira">   <?php \MapasCulturais\i::_e("Lixeira");?>     (<?php echo count($user->trashedEvents); ?>)  </button>
+        <button class="tablinks "       data-entity="eventos" data-tab="events-arquivo">   <?php \MapasCulturais\i::_e("Arquivo");?>     (<?php echo count($user->archivedEvents); ?>) </button>
       </div>
+
+      <div>
+        <div id="events-ativos" class="tab-content-table" style="display: block;">
+          <?php $this->part('user-management/user-info/info-events', array('events' => $user->enabledEvents)); ?>
+        </div>
+        <div id="events-concedidos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-events', array('events' => $user->hasControlEvents)); ?>
+        </div>
+        <div id="events-rascunhos" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-events', array('events' => $user->draftEvents)); ?>
+        </div>
+        <div id="events-lixeira" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-events', array('events' => $user->trashedEvents)); ?>
+        </div>
+        <div id="events-arquivo" class="tab-content-table">
+          <?php $this->part('user-management/user-info/info-events', array('events' => $user->archivedEvents)); ?>
+        </div>
+      </div>
+
     </div>
   
     <div id="permissoes" class="aba-content">
