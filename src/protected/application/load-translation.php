cc880f5098d6e00a96d3871045d683e4a5635b81
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-08-08 15:06:25 -0300

    refatora o load-translation.php para aceitar mais de um locale também no arquivo config.php

--- src/protected/application/load-translation.php
+++ src/protected/application/load-translation.php
@@ -4,34 +4,33 @@ use Sinergi\BrowserDetector\Language;
 if ($lcodes = env('APP_LCODE', null)) {
     $lcodes = explode(',', $lcodes);
 
-    $language = new Language();
-    $browser_lcode = $language->getLanguage();
+} else {
+    $_config = require $config_filename;    
+    $lcodes = explode(',', $_config['app.lcode']);
+}
 
-    $lcode = null;
+$language = new Language();
+$browser_lcode = $language->getLanguage();
 
+$lcode = null;
+
+foreach ($lcodes as $lc) {
+    if (str_replace('-', '_', $browser_lcode) === $lc) {
+        $lcode = $lc;
+        break;
+    }
+}
+if (is_null($lcode)) {
     foreach ($lcodes as $lc) {
-        if (str_replace('-', '_', $browser_lcode) === $lc) {
+        if (substr($lc, 0, 2) === substr($browser_lcode, 0, 2)) {
             $lcode = $lc;
             break;
         }
     }
-    if (is_null($lcode)) {
-        foreach ($lcodes as $lc) {
-            if (substr($lc, 0, 2) === substr($browser_lcode, 0, 2)) {
-                $lcode = $lc;
-                break;
-            }
-        }
-    }
+}
 
-    if (is_null($lcode)) {
-        $lcode = $lcodes[0];
-    }
-
-} else {
-    $_config = require $config_filename;
-    
-    $lcode = $_config['app.lcode'];
+if (is_null($lcode)) {
+    $lcode = $lcodes[0];
 }
 
 //Load defaut translation textdomain
