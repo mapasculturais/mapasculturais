c5f9d3d74e4655895ecd80d786aef11d784070a0
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-08-10 09:22:01 -0300

    adiciona plugins de métodos de autenticação ao arquivo de configurações de plugins utilizado pelas imagens docker

--- compose/config.d/plugins.php
+++ compose/config.d/plugins.php
@@ -1,3 +1,8 @@
 <?php
-
-return [];
+return [
+    'plugins' => [
+        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
+        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
+        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary']
+    ]
+];
