4d3ee12900189e29f52c78d2d6879879ad9928fd
benrainir@gmail.com <benrainir@gmail.com>
2018-09-10 15:55:23 -0300

    Corrigido relatorio de avaliacoes , listando apenas avaliacoes de avaliadores

--- src/protected/application/lib/MapasCulturais/Repositories/RegistrationEvaluation.php
+++ src/protected/application/lib/MapasCulturais/Repositories/RegistrationEvaluation.php
@@ -60,22 +60,62 @@ class RegistrationEvaluation extends \MapasCulturais\Repository{
 
     /**
      *
-     * @param \MapasCulturais\Entities\Registration $registration
+     * @param \MapasCulturais\Entities\Opportunity $opportunity
      * @param array $users
-     * @param int $status
+     * @param array $status
      * @return \MapasCulturais\Entities\Registration[]
      */
-    function findByRegistrationAndUsersAndStatus(\MapasCulturais\Entities\Registration $registration, $users, $status){
+    function findByOpportunityAndUsersAndStatus(\MapasCulturais\Entities\Opportunity $opportunity, $users = null, $status = null){
+
+        $dql = "  SELECT e 
+                  FROM MapasCulturais\Entities\RegistrationEvaluation e
+                  JOIN e.registration r
+                  WHERE r.opportunity = :opportunity ";
+
+        $params = ['opportunity' => $opportunity];
+
+        if (!is_null($users)){
+            $dql .= "AND e.user IN (:uids)";
+            $params['uids'] =  $users;
+        }
+
+        if (!is_null($status)){
+            $dql .= "AND e.status IN (:status)";
+            $params['status'] =  $status;
+        }
+
+        $query = $this->_em->createQuery($dql);
+
+        $query->setParameters($params);
+
+        return $query->getResult();
+    }
+
+    /**
+     *
+     * @param \MapasCulturais\Entities\Registration $registration
+     * @param array $users
+     * @param array $status
+     * @return \MapasCulturais\Entities\Registration[]
+     */
+    function findByRegistrationAndUsersAndStatus(\MapasCulturais\Entities\Registration $registration, $users = null, $status = null){
 
         $dql = "  SELECT e 
                   FROM MapasCulturais\Entities\RegistrationEvaluation e 
-                  WHERE e.registration = :registration AND e.status = :status AND e.user IN (:uids)";
+                  WHERE e.registration = :registration ";
 
-        $params = [
-            'registration' => $registration,
-            'status' => $status,
-            'uids' => $users
-        ];
+        $params = ['registration' => $registration];
+
+        if (!is_null($users)){
+            $dql .= "AND e.user IN (:uids)";
+            $params['uids'] =  $users;
+        }
+
+        if (!is_null($status)){
+            $dql .= "AND e.status IN (:status)";
+            $params['status'] =  $status;
+        }
+
         $query = $this->_em->createQuery($dql);
 
         $query->setParameters($params);
