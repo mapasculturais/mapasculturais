a662d69afe3f1a3e19c910855f18b97a577970ef
Asafe Ramos <asafe@asaferamos.com>
2018-09-19 14:16:01 -0300

    remove constant duplicada MODULES_PATH

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,6 @@ define('ACTIVE_THEME_PATH',  THEMES_PATH . 'active/');
 define('PLUGINS_PATH', APPLICATION_PATH.'/plugins/');
 define('MODULES_PATH', APPLICATION_PATH.'lib/modules/');
 define('LANGUAGES_PATH', APPLICATION_PATH . 'translations/');
-define('MODULES_PATH', APPLICATION_PATH . 'lib/modules/');
 
  // Prepare a mock environment
 \Slim\Environment::mock(array_merge(array(
