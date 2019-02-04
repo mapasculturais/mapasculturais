45a041152343d1364047639432b44f82cfa9a41b
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-02-04 20:10:00 -0200

    módulo DeleteAccount quase pronto (refs culturagovbr/mapasculturais#213)

--- src/protected/application/lib/modules/DeleteAccount/Module.php
+++ src/protected/application/lib/modules/DeleteAccount/Module.php
@@ -3,6 +3,7 @@
 namespace DeleteAccount;
 
 use MapasCulturais\App,
+    MapasCulturais\i,
     MapasCulturais\Entities,
     MapasCulturais\Definitions,
     MapasCulturais\Exceptions;
@@ -12,25 +13,7 @@ class Module extends \MapasCulturais\Module{
     public function _init() {
         $app = App::i();
         $theme = $app->view;
-        if(false) $theme = new \MapasCulturais\Themes\BaseV1\Theme;
-
-        $app->hook('mapasculturais.isEditable', function(&$result){
-            if($this->controller->id == 'panel' && $this->controller->action == 'deleteAccount'){
-                $result = true;
-            }
-        });
-        $app->hook('mapasculturais.head', function() use($app, $theme){
-            
-            $theme->jsObject['angularAppDependencies'][] = 'DeleteAccount';
-            $theme->enqueueScript('app', 'delete-account', 'js/ng.delete-account.js');
-            if($theme->controller->id == 'panel' && $theme->controller->action == 'deleteAccount'){
-                $theme->includeAngularEntityAssets($app->user->profile);
-                $theme->includeEditableEntityAssets();
-                $theme->addEntityToJs($app->user->profile);
-            }
-        });
-
-        
+     
         // a cada login salva um novo token
         $app->hook('auth.successful', function() use($app){
             $token = sha1(uniqid(microtime(true), true));
@@ -42,29 +25,60 @@ class Module extends \MapasCulturais\Module{
 
         // ação de apagar a conta
         $app->hook('POST(user.deleteAccount)', function() use($app){
-            // if(false) $this = new \MapasCulturais\Controllers\User;
             $this->requireAuthentication();
 
             if(isset($this->data['token'])){
                 if($this->data['token'] === $app->user->deleteAccountToken){
-                    $this->json($this->data);
+                    if(isset($this->data['agentId']) && $this->data['agentId']){
+                        $target_agent = $app->repo('Agent')->find($this->data['agentId']);
+                        $app->user->transferEntitiesTo($target_agent, true);
+                    }
+
+                    $app->user->delete(true);
+                    $this->json(true);
                 }
             }
 
             $this->errorJson(false,400);
         });
 
+        // coloca o link para a página de apagar a conta
+        $app->hook('template(panel.<<*>>.settings-nav):end', function(){
+            $this->part('delete-account--button');
+        });
+
+        // página do painel de apagar a conta
         $app->hook('GET(panel.deleteAccount)', function(){
             $this->requireAuthentication();
             $this->render('delete-account');
         });
 
-        $app->hook('template(panel.index.content.entities):after', function(){
-            $this->part('delete-account--button');
+              // define o isEditable como verdadeiro na página de apagar conta
+        // é necessário para o componente de selecionar um agente de destino funcionar
+        $app->hook('mapasculturais.isEditable', function(&$result){
+            if($this->controller->id == 'panel' && $this->controller->action == 'deleteAccount'){
+                $result = true;
+            }
+        });
+        
+        // registra o módulo angular da 
+        $app->hook('mapasculturais.head', function() use($app, $theme){
+            $theme->jsObject['angularAppDependencies'][] = 'DeleteAccount';
+            $theme->enqueueScript('app', 'delete-account', 'js/ng.delete-account.js');
+            $theme->localizeScript('delete-account', [
+                'confirm' => i::__('Sua conta será removida e não poderá ser recuperada, deseja continuar?'),
+                'goobye' => i::__('Sua conta foi removida e você será deslogado. =(')
+            ]);
+            if($theme->controller->id == 'panel' && $theme->controller->action == 'deleteAccount'){
+                $theme->includeAngularEntityAssets($app->user->profile);
+                $theme->includeEditableEntityAssets();
+                $theme->addEntityToJs($app->user->profile);
+            }
         });
     }
 
     public function register() {
+        // registra o metadado do token de verificação da remoção da conta.
         $this->registerUserMetadata('deleteAccountToken', ['label' => "Delete Account Token", 'private' => true]);        
     }
 }
--- src/protected/application/lib/modules/DeleteAccount/views/panel/delete-account.php
+++ src/protected/application/lib/modules/DeleteAccount/views/panel/delete-account.php
@@ -8,7 +8,7 @@ $this->bodyProperties['ng-controller'] = "EntityController";
 
 ?>
 <div class="panel-main-content" ng-app="DeleteAccount" ng-controller="DeleteAccountController">
-    <h1><?php i::_e('Remover Conta') ?></h1>
+    <h1><?php i::_e('Apagar Conta') ?></h1>
     <p><?php i::_e('Texto explicando o que acontece com as infos: Aliquam dictum ut risus ut mollis. Morbi quis sem vitae ex volutpat feugiat ut semper metus. Aliquam iaculis congue mi, ac tempus leo semper quis. Duis at tristique tellus, sed posuere ante. Sed tincidunt egestas rhoncus. Nunc faucibus, ligula at gravida sagittis, mauris est gravida est, non elementum felis nibh eget mauris. Maecenas rhoncus ornare elit eget maximus. Donec bibendum convallis turpis, a cursus nisl fringilla vel.')?></p>
     <div>
         <p>
