09746993a86015b3589d0a88b4ff4ec7486ba7ef
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-02-04 16:39:13 -0200

    adiciona menu do lado direito do painel (refs culturagovbr/mapasculturais#213)

--- src/protected/application/themes/BaseV1/layouts/panel.php
+++ src/protected/application/themes/BaseV1/layouts/panel.php
@@ -2,4 +2,6 @@
 $this->part('header', $render_data);
 $this->part('panel-nav', $render_data);
 echo $TEMPLATE_CONTENT;
+$this->part('panel-settings-nav', $render_data);
+
 $this->part('footer', $render_data);
