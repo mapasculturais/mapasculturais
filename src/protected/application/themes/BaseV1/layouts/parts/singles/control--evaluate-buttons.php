b7f85aa11471dd22f379d496189e637fc5a50a93
Nalomy de Souza A. Oliveira <nalomy.souza@secult.ce.gov.br>
2019-03-11 19:06:34 -0300

    add opção de avaliar e ir para a próxima avaliação

--- src/protected/application/themes/BaseV1/layouts/parts/singles/control--evaluate-buttons.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/control--evaluate-buttons.php
@@ -1,5 +1,5 @@
 
-<button class="btn btn-primary js-evaluation-submit"><?php \MapasCulturais\i::_e("Salvar Avaliação");?></button>
+<button class="btn btn-primary js-evaluation-submit js-next"><?php \MapasCulturais\i::_e("Salvar e Avançar >>");?></button>
 <script type="text/javascript">
     MapasCulturais.Messages.help('<?php \MapasCulturais\i::esc_attr_e("Selecione as inscrições e cadastre sua avaliação.");?>');
 </script>
