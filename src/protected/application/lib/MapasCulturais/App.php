3ae0f1288edc58eecdc12f6e83ebe8b13e70e7f6
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-07-16 20:32:56 -0300

    move o gerador de token para a classe App

--- src/protected/application/lib/MapasCulturais/App.php
+++ src/protected/application/lib/MapasCulturais/App.php
@@ -532,6 +532,38 @@ class App extends \Slim\Slim{
         $this->applyHookBoundTo($this, 'mapasculturais.run:after');
     }
 
+
+    
+    /**
+     * http://stackoverflow.com/questions/1846202/php-how-to-generate-a-random-unique-alphanumeric-string/13733588#13733588
+     */
+    protected static function crypto_rand_secure($min, $max) {
+        $range = $max - $min;
+        if ($range < 1)
+            return $min; // not so random...
+        $log = ceil(log($range, 2));
+        $bytes = (int) ($log / 8) + 1; // length in bytes
+        $bits = (int) $log + 1; // length in bits
+        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            $rnd = $rnd & $filter; // discard irrelevant bits
+        } while ($rnd >= $range);
+        return $min + $rnd;
+    }
+
+    static function getToken($length) {
+        $token = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet.= "0123456789";
+        $max = strlen($codeAlphabet) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $token .= $codeAlphabet[self::crypto_rand_secure(0, $max)];
+        }
+        return $token;
+    }
+
     function isEnabled($entity){
         return $this->_config['app.enabled.' . $entity];
     }
