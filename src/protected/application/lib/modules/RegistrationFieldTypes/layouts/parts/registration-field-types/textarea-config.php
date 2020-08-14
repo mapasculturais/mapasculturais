52b04bc48e6b94450e5ffbcf0f07c91e231507bf
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-14 03:12:01 -0300

    refatoração do registro de tipos de campos das oportunidades

---
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/textarea-config.php
@@ -0,0 +1,3 @@
+<div ng-if="field.fieldType === 'textarea'">
+    <input type="text" ng-model="field.maxSize" placeholder="<?php \MapasCulturais\i::esc_attr_e("Quantidade máxima de caracteres");?>"/>
+</div>
