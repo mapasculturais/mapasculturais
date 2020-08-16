5c117eb7b9d87660114643920a30a3c30e29f3c8
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-16 16:56:33 -0300

    iniciando desenvolvimento do campo de agente responsável nas oportunidades [refs: #1467]

---
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/agent-owner-field-config.php
@@ -0,0 +1,23 @@
+<?php
+$app = MapasCulturais\App::i();
+$definitions = MapasCulturais\Entities\Agent::getPropertiesMetadata();
+$agent_fields = $app->modules['RegistrationFieldTypes']->config['availableAgentFields'];
+
+$fields_options = [];
+
+foreach ($agent_fields as $field) {
+    if (isset($definitions[$field])) {
+        $def = $definitions[$field];
+        $fields_options[$field] = $def['label'] ?: $field;
+    } else {
+        $fields_options[$field] = $field;
+    }
+}
+?>
+<div ng-if="field.fieldType === 'agent-owner-field'">
+    <select ng-model="field.agentField">
+        <?php foreach ($fields_options as $key => $label) : ?>
+            <option value="<?= $key ?>"><?= $label ?><option>
+            <?php endforeach; ?>
+    </select>
+</div>
