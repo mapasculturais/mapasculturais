93079c0cee20e76198d64478d6078a4952507c39
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-26 04:04:42 -0300

    salva os campos das oportunidades no blur (refs: #1471)

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/select.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/select.php
@@ -1,3 +1,3 @@
-<select ng-required="field.required" ng-model="entity[fieldName]" >
+<select ng-required="field.required" ng-model="entity[fieldName]" ng-blur="saveField(field, entity[fieldName])" >
     <option ng-repeat="option in field.fieldOptions">{{option}}</option>
 </select>
