d9e35db8dfedb6f8f083fd903659c8ff244693aa
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-26 04:04:42 -0300

    salva os campos das oportunidades no blur (refs: #1471)

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/textarea.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/textarea.php
@@ -1,5 +1,5 @@
 <?php use MapasCulturais\i; ?>
-<textarea ng-required="field.required" ng-model="entity[fieldName]" maxlength='{{ !field.maxSize ?'': field.maxSize }}'></textarea>
+<textarea ng-required="field.required" ng-model="entity[fieldName]" ng-blur="saveField(field, entity[fieldName])"  maxlength='{{ !field.maxSize ?'': field.maxSize }}'></textarea>
 <div ng-if="field.maxSize">
     <?php i::_e('Número de caracteres') ?>:
     {{entity[fieldName].length}} / {{field.maxSize}}
