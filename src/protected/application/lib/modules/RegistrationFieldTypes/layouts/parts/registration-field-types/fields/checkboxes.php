bcbb23c86be0a84bea12b94e495f6570316fb466
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-27 20:32:46 -0300

    remove atibuto required do tipo de campo checkboxes

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkboxes.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkboxes.php
@@ -1,3 +1,3 @@
 <label ng-repeat="option in field.fieldOptions">
-    <input ng-required="field.required" type="checkbox" checklist-model="entity[fieldName]" checklist-value="option"> {{option}}
+    <input type="checkbox" checklist-model="entity[fieldName]" checklist-value="option"> {{option}}
 </label>
