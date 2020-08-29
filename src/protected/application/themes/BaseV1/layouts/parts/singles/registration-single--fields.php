fb61a789002ddb953856f8e9fa228c86a25f4039
brmagrini@gmail.com <brmagrini@gmail.com>
2020-08-28 23:41:20 -0300

    Apresenta itens de campos de pessoas na single

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
@@ -5,7 +5,7 @@
     <ul class="attachment-list" ng-controller="RegistrationFieldsController">
 
         <li ng-repeat="field in data.fields" ng-if="showFieldForCategory(field)" id="field_{{::field.id}}" data-field-id="{{::field.id}}" ng-class=" (field.fieldType != 'section') ? 'js-field attachment-list-item registration-view-mode' : ''">
-            <div ng-if="field.fieldType !== 'file' && field.fieldType !== 'section' && field.config.entityField !== '@location' ">
+            <div ng-if="field.fieldType !== 'file' && field.fieldType !== 'section' && field.fieldType !== 'persons' && field.config.entityField !== '@location' ">
                 <label>{{field.required ? '*' : ''}} {{field.title}}: </label>
                 <span ng-if="entity[field.fieldName] && field.fieldType !== 'textarea'" ng-bind-html="printField(field, entity[field.fieldName])"></span>
                 <p ng-if="entity[field.fieldName] && field.fieldType === 'textarea'" ng-bind-html="printField(field, entity[field.fieldName])" style="white-space: pre-line"></p>
@@ -14,6 +14,12 @@
             <div ng-if="field.fieldType === 'section'">
                 <h4>{{field.title}}</h4>
             </div>
+            <div ng-if="field.fieldType === 'persons'">
+                <label>{{field.required ? '*' : ''}} {{field.title}}: </label> 
+                <div ng-repeat="(key, item) in entity[field.fieldName]" ng-if="item && key !== 'location' && key !== 'publicLocation' " >
+                    <div><b ng-if="item.name">Nome: </b>{{item.name}}<b ng-if="item.cpf"> CPF: </b>{{item.cpf}} <b ng-if="item.relationship">Relação: </b>{{item.relationship}}</div>
+                </div>
+            </div>
             <?php //@TODO pegar endereço do campo endereço (verificar porque não esta salvando corretamente, arquicos location.js e _location.php)?>
             <div ng-if="field.config.entityField === '@location'">
                 <label>{{field.required ? '*' : ''}} {{field.title}}: </label> 
@@ -21,6 +27,7 @@
                 {{key.split('_').pop()}}: {{item}} 
                 </div>
             </div>
+
             <div ng-if="field.fieldType === 'file'">
                 <label>{{::field.required ? '*' : ''}} {{::field.title}}: </label>
                 <a ng-if="field.file" class="attachment-title" href="{{::field.file.url}}" target="_blank" rel='noopener noreferrer'>{{::field.file.name}}</a>
