1afc25f43bcee26dce3e3e830cdb132c4ca2d8fb
Junior-Shyko <franciscoanto@gmail.com>
2020-09-01 09:27:48 -0300

    Add v3 no cep

--- src/protected/application/conf/conf-base.d/registrations.php
+++ src/protected/application/conf/conf-base.d/registrations.php
@@ -40,7 +40,8 @@ return [
         'label' => \MapasCulturais\i::__('Agente responsável pela inscrição'),
         'agentRelationGroupName' => 'owner',
         'description' => \MapasCulturais\i::__('Agente individual (pessoa física) com os campos CPF, Data de Nascimento/Fundação, Gênero, Orientação Sexual, Raça/Cor, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
-        'type' => 1
+        'type' => 1,
+        'requiredProperties' => array('documento', 'raca', 'dataDeNascimento', 'genero', 'emailPrivado', 'telefone1')
     ),
     'registration.agentRelations' => array(
         array(
@@ -48,14 +49,16 @@ return [
             'label' => \MapasCulturais\i::__('Instituição responsável'),
             'agentRelationGroupName' => 'instituicao',
             'description' => \MapasCulturais\i::__('Agente coletivo (pessoa jurídica) com os campos CNPJ, Data de Nascimento/Fundação, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
-            'type' => 2
+            'type' => 2,
+            'requiredProperties' => array('documento', 'dataDeNascimento', 'emailPrivado', 'telefone1')
         ),
         array(
             'required' => false,
             'label' => \MapasCulturais\i::__('Coletivo'),
             'agentRelationGroupName' => 'coletivo',
             'description' => \MapasCulturais\i::__('Agente coletivo sem CNPJ, com os campos Data de Nascimento/Fundação e Email Privado obrigatoriamente preenchidos'),
-            'type' => 2
+            'type' => 2,
+            'requiredProperties' => array('dataDeNascimento', 'emailPrivado')
         )
     ),
     'registration.spaceRelations' => array(
@@ -63,7 +66,8 @@ return [
             'required' => false,
             'label' => \MapasCulturais\i::__('Espaço Relacionado'),
             'description' => \MapasCulturais\i::__('Espaço Relacionado'),
-            'type' => 2
+            'type' => 2,
+            'requiredProperties' => array('endereco', 'telefone1')
         )
     ),
     'registration.spaceProperties' => array(
