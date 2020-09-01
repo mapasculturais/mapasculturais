909409ca2f2e0db2a1420b11ccaa8e809b4f9213
brmagrini@gmail.com <brmagrini@gmail.com>
2020-09-01 14:56:30 -0300

    Alterações nos campos de formulário do mapas

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkbox.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkbox.php
@@ -1,7 +1,7 @@
-<div ng-if="::field.fieldType === 'checkbox'" id="field_{{::field.id}}">
+<div ng-class="field.error ? 'invalidField': '' " ng-if="::field.fieldType === 'checkbox'" id="field_{{::field.id}}">
     <span><?php $this->part('registration-field-types/fields/checkbox') ?></span>
     
-    {{::field.required ? '*' : ''}}
+    <span ng-if="::field.required ">obrigatório</span>   
     
     <div ng-if="::field.description" class="attachment-description">{{::field.description}}</div>
 
--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/links.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/links.php
@@ -1,6 +1,10 @@
 <?php use MapasCulturais\i; ?>
-<div ng-if="::field.fieldType == 'links'" id="field_{{::field.id}}" >
-    <div class="label icon"> {{::field.title}} {{::field.required ? '*' : ''}}</div>
+<div ng-class="field.error ? 'invalidField': '' " ng-if="::field.fieldType == 'links'" id="field_{{::field.id}}" >
+    <div class="label icon"> 
+        {{::field.title}} 
+        <span ng-if="::field.required ">obrigatório</span>       
+
+    </div>
     
     <div ng-if="::field.description" class="attachment-description">{{::field.description}}</div>
 
