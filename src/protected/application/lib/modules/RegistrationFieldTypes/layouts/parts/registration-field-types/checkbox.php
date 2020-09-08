5b85b46a121f902e9cfa0e5c3c23ae9b425a2430
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-09-08 20:36:06 -0300

    melhora exibição da obrigatoriedade do campo de chckbox

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkbox.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkbox.php
@@ -1,8 +1,8 @@
 <div ng-class="field.error ? 'invalidField': '' " ng-if="::field.fieldType === 'checkbox'" id="field_{{::field.id}}">
-    <span><?php $this->part('registration-field-types/fields/checkbox') ?></span>
-    
-    <span ng-if="requiredField(field) ">obrigatório</span>   
+    <span >
+        <?php $this->part('registration-field-types/fields/checkbox') ?>
+        <span ng-if="requiredField(field) ">obrigatório</span>
+    </span>
     
     <div ng-if="::field.description" class="attachment-description">{{::field.description}}</div>
-
 </div>
