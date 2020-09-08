311fa76978325a9a32c5ddd00fe757d9f473a699
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-09-08 20:35:09 -0300

    corrige 'checked' do campo de checkbox

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkbox.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkbox.php
@@ -1,1 +1,1 @@
-<label><input ng-required="requiredField(field)" ng-model="entity[fieldName]" ng-click="saveField(field, entity[fieldName])" type="checkbox" /> {{::field.title}}</label>
+<label><input ng-required="requiredField(field)" ng-model="entity[fieldName]" ng-click="saveField(field, entity[fieldName])" type="checkbox" ng-true-value="'true'"/> {{::field.title}}</label>
