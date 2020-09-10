4615a7d1d38a2e2680d0824ef9b0763ec46b1856
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-09-10 13:14:40 -0300

    adiciona metodo findByProjectAndOpportunityMeta no repositorio de oportunity

--- src/protected/application/lib/MapasCulturais/Repositories/Opportunity.php
+++ src/protected/application/lib/MapasCulturais/Repositories/Opportunity.php
@@ -1,9 +1,40 @@
 <?php
 namespace MapasCulturais\Repositories;
 use MapasCulturais\Traits;
+use MapasCulturais\Entities\ProjectOpportunity;
+use MapasCulturais\Entities\Project;
 
 class Opportunity extends \MapasCulturais\Repository{
     use Traits\RepositoryKeyword,
         Traits\RepositoryAgentRelation;
+
+
+    function findByProjectAndOpportunityMeta($project, $key, $value) {
+        try {
+            $query = $this->_em->createQuery("
+            SELECT 
+                po
+            FROM
+                MapasCulturais\Entities\OpportunityMeta om
+                JOIN MapasCulturais\Entities\ProjectOpportunity po WITH om.owner=po
+                JOIN po.ownerEntity oe
+                WHERE om.key=:key AND om.value=:value AND oe.id=:projectId
+            ");
+        
+            $params = [
+                'projectId' => $project->id,
+                'key'=>$key,
+                'value'=>$value
+            ];
+
+            $query->setParameters($params);
+
+            return $query->getResult();
+        } catch (\Exception $e) {
+            var_dump($e);
+        }
+    }
+
+    
 }
 
