b7fe8162bd7688413d00e7c6319957e4b0ff2bce
brmagrini@gmail.com <brmagrini@gmail.com>
2020-09-12 01:25:57 -0300

    Fix campo links agente coletivo

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/links.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/links.php
@@ -11,5 +11,5 @@
 
     <button ng-click="remove(entity[fieldName], $index); saveField(field, entity[fieldName], 0);" class="btn btn-danger"><?php i::_e('remover') ?></button>
 </div>
-
+{{ (entity[fieldName] = (entity[fieldName] ? entity[fieldName] : []) ) && false ? '' : ''}}
 <button ng-click="entity[fieldName] = entity[fieldName].concat([{}])" class="btn btn-primary"><?php i::_e('adicionar') ?></button>
