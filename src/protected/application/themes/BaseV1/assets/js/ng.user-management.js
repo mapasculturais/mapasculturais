509e3e88d7a60fb6b628b4890bc2f1ce5cd043c2
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-09-24 09:40:16 -0300

    remove codigo sem sentindo

--- src/protected/application/themes/BaseV1/assets/js/ng.user-management.js
+++ src/protected/application/themes/BaseV1/assets/js/ng.user-management.js
@@ -186,22 +186,6 @@
         $scope.selectGroupAdmin = 'saasSuperAdmin';
         $scope.selectSubsite = 'MapasCulturais';
 
-        function verifyIsValidCPF(cpf){
-            cpf = cpf.replace(/\D/g, '');
-            if(cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
-            var result = true;
-            [9,10].forEach(function(j){
-                var soma = 0, r;
-                cpf.split(/(?=)/).splice(0,j).forEach(function(e, i){
-                    soma += parseInt(e) * ((j+2)-(i+1));
-                });
-                r = soma % 11;
-                r = (r <2)?0:11-r;
-                if(r != cpf.substring(j, j+1)) result = false;
-            });
-            return result;
-        }
-
         function isValidCPF(cpf){
             return cpf.match(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/);
         }
