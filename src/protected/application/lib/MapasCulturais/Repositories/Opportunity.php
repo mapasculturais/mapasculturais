bed8deed0ef05fa7df8d4c4eea7d38622664e819
brmagrini@gmail.com <brmagrini@gmail.com>
2020-10-19 21:01:47 -0300

    Troca função que pega oportunidades com datas por função que pega oportunidades publicadas abertas (entre as datas de inicio e fim)

--- src/protected/application/lib/MapasCulturais/Repositories/Opportunity.php
+++ src/protected/application/lib/MapasCulturais/Repositories/Opportunity.php
@@ -3,23 +3,30 @@ namespace MapasCulturais\Repositories;
 use MapasCulturais\Traits;
 use MapasCulturais\Entities\ProjectOpportunity;
 use MapasCulturais\Entities\Project;
+use DateTime;
+
 
 class Opportunity extends \MapasCulturais\Repository{
     use Traits\RepositoryKeyword,
         Traits\RepositoryAgentRelation;
 
-    function findRegistrationDateByIds($opportunitiesIds) {
+    function findOpenRegistrationByIds($opportunitiesIds) {
+        $today = new DateTime('now');
         $query = $this->_em->createQuery("
         SELECT 
             op.id, op.name, op.registrationFrom, op.registrationTo
         FROM
             MapasCulturais\Entities\Opportunity op
         WHERE 
-            op.id in (:opportunitiesIds)
+            op.id in (:opportunitiesIds) AND
+            op.registrationFrom <= :today AND 
+            op.registrationTo >= :today AND 
+            op.status = 1
         ");
     
         $params = [
             'opportunitiesIds' => $opportunitiesIds,
+            'today'            => $today
         ];
 
         $query->setParameters($params);
