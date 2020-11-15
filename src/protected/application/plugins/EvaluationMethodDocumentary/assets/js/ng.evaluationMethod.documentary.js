6efd1fbfc9649c87d25bda3d7774b521c2e9d301
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-11-14 23:17:38 -0300

    botão de aplicar avaliações documentais

---
+++ src/protected/application/plugins/EvaluationMethodDocumentary/assets/js/ng.evaluationMethod.documentary.js
@@ -0,0 +1,74 @@
+(function (angular) {
+    "use strict";
+    var module = angular.module('ng.evaluationMethod.documentary', []);
+    
+
+    module.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        $httpProvider.defaults.transformRequest = function (data) {
+            var result = angular.isObject(data) && String(data) !== '[object File]' ? $.param(data) : data;
+
+            return result;
+        };
+    }]);
+
+
+    module.factory('ApplyDocumentaryEvaluationService', ['$http', '$rootScope', 'UrlService', function ($http, $rootScope, UrlService) {
+        
+        return {
+            apply: function (from, to) {
+                var data = {from: from, to: to};
+                var url = MapasCulturais.createUrl('opportunity', 'applyEvaluationsDocumentary', [MapasCulturais.entity.id]);
+                console.log(url, data);
+                return $http.post(url, data).
+                    success(function (data, status) {
+                        $rootScope.$emit('registration.create', {message: "Opportunity registration was created", data: data, status: status});
+                    }).
+                    error(function (data, status) {
+                        $rootScope.$emit('error', {message: "Cannot create opportunity registration", data: data, status: status});
+                    });
+            },
+        };
+    }]);
+
+    module.controller('ApplyDocumentaryEvaluationResults',['$scope', 'RegistrationService', 'ApplyDocumentaryEvaluationService', 'EditBox', function($scope, RegistrationService, ApplyDocumentaryEvaluationService, EditBox){
+        
+        var evaluation = MapasCulturais.evaluation;
+        var statuses = RegistrationService.registrationStatusesNames.filter((status) => {
+            if(status.value > 1) return status;
+        });
+        $scope.data = {
+            registration: evaluation ? evaluation.evaluationData.status : null,
+            obs: evaluation ? evaluation.evaluationData.obs : null,
+            registrationStatusesNames: statuses,
+            applying: false
+        };
+
+        $scope.getStatusLabel = (status) => {
+            for(var i in statuses){
+                if(statuses[i].value == status){
+                    return statuses[i].label;
+                }
+            }
+            return '';
+        };
+
+        $scope.applyEvaluations = () => {
+            $scope.data.applying = true;
+            ApplyDocumentaryEvaluationService.apply($scope.data.applyFrom, $scope.data.applyTo).
+                success(() => {
+                    $scope.data.applying = false;
+                    MapasCulturais.Messages.success('Avaliações aplicadas com sucesso');
+                    EditBox.close('apply-consolidated-results-editbox');
+                    $scope.data.applyFrom = null;
+                    $scope.data.applyTo = null;
+                }).
+                error((data, status) => {
+                    $scope.data.applying = false;
+                    $scope.data.errorMessage = data.data;
+                    MapasCulturais.Messages.success('As avaliações não foram aplicadas.');
+                })
+        }
+    }]);
+})(angular);
