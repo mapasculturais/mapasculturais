ef9974971dc4e68d37ff737f1b53bdeb5eb3e41c
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-01-29 21:57:25 -0300

    refatora implementação dos campos enuns para que se auto atualizem
    o tipo enum é atualizado no db-update quando houver valores novos

---
+++ src/protected/application/lib/MapasCulturais/DoctrineEnumType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MapasCulturais;
+
+use MyCLabs\Enum\Enum;
+
+abstract class DoctrineEnumType extends Enum
+{
+    abstract public static function getTypeName(): string;
+
+    abstract public static function getKeysValues(): array;
+
+    public static function toArray(): array
+    {
+        $class = get_called_class();
+
+        $result = $class::getKeysValues();
+
+        $app = App::i();
+
+        $class = get_called_class();
+
+        $type_name = $class::getTypeName();
+        $app->applyHook("doctrine.emum({$type_name}).values", [&$result]);
+
+        return $result;
+    }
+}
