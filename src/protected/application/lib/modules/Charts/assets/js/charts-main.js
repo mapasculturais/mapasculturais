39b592032e6fc4dfe5aefdf8ea4624d70211bdaf
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-02-01 16:57:44 -0300

    adicionado módulo para geração de gráficos (Charts)

---
+++ src/protected/application/lib/modules/Charts/assets/js/charts-main.js
@@ -0,0 +1,12 @@
+MapasCulturais.Charts = {
+    dynamicColors: function() {
+        var r = 16 + Math.floor(Math.random() * 230);
+        var g = 16 + Math.floor(Math.random() * 230);
+        var b = 16 + Math.floor(Math.random() * 230);
+        return "rgb(" + r + "," + g + "," + b + ")";
+    },
+
+    charts: {
+
+    }
+};
---
+++ src/protected/application/lib/modules/Charts/layouts/parts/charts/line.php
@@ -0,0 +1,66 @@
+<?php 
+/**
+ * 
+  $this->part('charts/line', [
+       'vertical',
+       'labels' => ['Draft', 'Pending', 'Publish'],
+       'series' => [
+           ['label' => 'Preto', 'data' => [66,120,70], 'color' => 'black'],
+           ['label' => 'Branco', 'data' => [22,44,66], 'color' => 'white'],
+           ['label' => 'Amarelo', 'data' => [77,55,34], 'color' => 'yellow']
+       ]
+  ]);
+ * 
+ */
+$title = $title ?? null;
+$chart_id = uniqid('chart-line-');
+$colors = [];
+$datasets = [];
+
+if (isset($series) && is_array($series)) {
+    $datasets = array_map(function($dataset) {
+        $color_key = 'borderColor';
+        $dataset['fill'] = $dataset['fill'] ?? false;
+        $dataset['borderWidth'] = $dataset['borderWidth'] ?? 2;
+        if(isset($dataset['color'])) {
+            $dataset[$color_key] = $dataset['color'];
+            unset($dataset['color']);
+        }
+        return $dataset;
+    }, $series);
+}
+
+$width = $width ?? '50vw';
+$height = $height ?? '50vw';
+
+?>
+<div class="chart-container chart-line" style="position: relative; height:<?=$height?>; width:<?=$width?>;">
+    <?php if($title): ?>
+        <h2><?= $title ?></h2>
+    <?php endif; ?>
+    <canvas id="<?= $chart_id ?>"></canvas>
+</div>
+
+<script>
+    $(window).on('load', function() {
+        var config = {
+            type: 'line',
+            data: {
+                datasets: <?= json_encode($datasets) ?>,
+                labels: <?= json_encode($labels) ?>,
+            },
+            options: {
+                responsive: true,
+            }
+        };
+        
+        config.data.datasets.forEach(function(dataset) {
+            dataset.backgroundColor = dataset.backgroundColor || MapasCulturais.Charts.dynamicColors();
+        });
+
+        var ctx = document.getElementById("<?= $chart_id ?>").getContext('2d');
+        ctx.canvas.width = 1000;
+		ctx.canvas.height = 300;
+        MapasCulturais.Charts.charts["<?= $chart_id ?>"] = new Chart(ctx, config);
+    });
+</script>
