19c82b7312f23bb958aa9fa9483cde78f0c1077c
Erlei Biazzio <erleibiazzio@gmail.com>
2021-02-03 15:11:21 -0300

    Insere novos gráficos na aba de relatórios e altera querys que busca dados para graficos de  avaliações

--- src/protected/application/lib/modules/Reports/layouts/parts/opportunity-reports--tab.php
+++ src/protected/application/lib/modules/Reports/layouts/parts/opportunity-reports--tab.php
@@ -1,1 +1,1 @@
-<li><a href="#relatorios" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Relatórios");?></a></li>
+<li><a href="#reports" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Relatórios");?></a></li>
--- src/protected/application/lib/modules/Reports/layouts/parts/opportunity-reports.php
+++ src/protected/application/lib/modules/Reports/layouts/parts/opportunity-reports.php
@@ -4,38 +4,49 @@ namespace RegistrationPayments;
 use MapasCulturais\i;
 
 ?>
-<div class="header-reports">   
-    <span>
-        <?php i::_e("Veja abaixo os gráficos referentes a essa oportinidade"); ?>  <br>
-    </span>
-    <div>
-        <h5><?php i::_e("Total de registro ao longo do tempo"); ?></h5>
-    </div>
-
-    <div>
-        <button  class="btn btn-default download"> <?php i::_e("Baixar gráfico em CSV"); ?></button>
-    </div>
-</div>
-
-
-<div class="charts-static">
-    <div class="line">
-        <div class="item">
-            <?php $this->part('registrationsByTime', ['data' => $registrationByTime]); ?>
-        </div>
-    </div>
-
-    <div class="pie">
+<div class="aba-content" id="reports">
+    <div class="header-reports">   
+        <span>
+            <?php i::_e("Veja abaixo os gráficos referentes a essa oportinidade"); ?>  <br>
+        </span>
         <div>
-            <?php $this->part('registrationsDraftVsSent', ['data' => $registrationsByStatus]); ?>
+            <h5><?php i::_e("Total de registro ao longo do tempo"); ?></h5>
         </div>
 
         <div>
-            <?php $this->part('registrationsStatus', ['data' => $registrationsByStatus]); ?>
+            <button  class="btn btn-default download"> <?php i::_e("Baixar gráfico em CSV"); ?></button>
         </div>
+    </div>
 
-        <div>
-            <?php $this->part('registrationsEvaluation', ['data' => $registrationsByEvaluation]); ?>
+
+    <div class="charts-static">
+        <div class="line">
+            <div class="line-one ">
+                <?php $this->part('registrationsByTime', ['data' => $registrationByTime]); ?>
+            </div>
+        </div>
+    
+        <div class="pie">
+            <div class="line-one">
+                <div>
+                    <?php $this->part('registrationsDraftVsSent', ['data' => $registrationsByStatus]); ?>
+                </div>
+                
+                <div>
+                    <?php $this->part('registrationsEvaluation', ['data' => $registrationsByEvaluation]); ?>
+                </div>
+            </div>
+
+            <div class="line-two">
+                <div>
+                    <?php $this->part('registrationsStatus', ['data' => $registrationsByStatus]); ?>
+                </div>
+            </div>
+            <div class="line-three">
+                <div>
+                    <?php $this->part('registrationsByEvaluationStatus', ['data' => $registrationsByEvaluationStatus]); ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
