dcd7e235d1cf73801c4f44a0dac5552b5b4eba12
Erlei Biazzio <erleibiazzio@gmail.com>
2021-02-05 15:18:39 -0300

    Implementa grafíco de inscrições agrupadas por categoria

---
+++ src/protected/application/lib/modules/Reports/layouts/parts/registrationByCategory.php
@@ -0,0 +1,33 @@
+<?php
+use MapasCulturais\i;
+
+//Recebe os valores do sistema
+$label = [];
+$values = [];
+$height = 'auto';
+$width = '100%';
+$total = 0;
+$colors = [];
+$legends = [];
+$title = i::__('Categorias da oportunidade');
+
+//Prepara os dados para o gráfico
+foreach ($data as $key => $value) {
+    $values[] = $value['count'];
+    $label[] = $value['category'];
+    $colors[] = is_callable($color) ? $color() : $color;
+    $legends[] = $value['category'];
+
+}
+
+// Imprime o gráfico na tela
+$this->part('charts/pie', [
+    'labels' => $label,
+    'data' => $values,
+    'colors' => $colors,
+    'height' => $height,
+    'width' => $width,
+    'legends' => $legends,
+    'title' => $title,
+    'top' => 60
+]);
