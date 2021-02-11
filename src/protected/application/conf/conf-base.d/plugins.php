87ec283e916806396810c61a7058039916321c2a
Junior-Shyko <franciscoanto@gmail.com>
2021-02-11 12:45:06 -0300

    Habilitando plugin LocationStateCity

--- src/protected/application/conf/conf-base.d/plugins.php
+++ src/protected/application/conf/conf-base.d/plugins.php
@@ -5,6 +5,7 @@ return [
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
+        //PLUGIN DESENVOLVIDO PELA ESP
         'LocationStateCity' => [
             'namespace' => 'LocationStateCity'
         ],
