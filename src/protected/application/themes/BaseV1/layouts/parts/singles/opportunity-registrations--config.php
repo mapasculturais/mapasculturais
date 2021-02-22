3b3a9d48d0ec3508fb242246dfd821de42887842
José Cage <jose.cage@doesangue.me>
2021-02-22 22:19:37 +0100

    Separar opção de importar e exportar na aba configuração do formulário

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--config.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--config.php
@@ -1,5 +1,5 @@
 <div id="form-config" class="aba-content">
-    <?php $this->part('singles/opportunity-registrations--importexport', ['entity' => $entity]) ?>
+    <?php $this->part('singles/opportunity-registrations--import', ['entity' => $entity]) ?>
 
     <?php if ($this->isEditable()): ?>
         <?php $this->part('singles/opportunity-registrations--agent-relations', ['entity' => $entity]) ?>
@@ -17,4 +17,6 @@
 
         <p><a href="<?php echo $app->createUrl('registration', 'preview', ['opportunityId' => $entity->id]); ?>" target="_blank" class="btn btn-primary"><?php MapasCulturais\i::_e('Pré-visualizar ficha de inscrição') ?></a></p>
     <?php endif; ?>
+
+    <?php $this->part('singles/opportunity-registrations--export', ['entity' => $entity]) ?>
 </div>
