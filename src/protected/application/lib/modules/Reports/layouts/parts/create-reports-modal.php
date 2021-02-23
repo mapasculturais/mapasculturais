6119a8d5f2425dc98023e29eb90f4527175c937d
everaldomatias <matiaseveraldo@gmail.com>
2021-02-23 15:27:02 -0300

    Ajuste no fechamento do modal

--- src/protected/application/lib/modules/Reports/layouts/parts/create-reports-modal.php
+++ src/protected/application/lib/modules/Reports/layouts/parts/create-reports-modal.php
@@ -5,7 +5,7 @@ use MapasCulturais\i;
 
 <!-- <canvas id="dinamic-grafic"></canvas> -->
 
-<div ng-class="{hidden:!data.reportModal}" ng-click="data.reportModal=false" class="bg-reports-modal" id="reportsModal">
+<div ng-class="{hidden:!data.reportModal}" class="bg-reports-modal" id="reportsModal">
     <div class="reports-modal">
 
         <div class="grafic-type" ng-class="{hidden:!data.graficType}">
