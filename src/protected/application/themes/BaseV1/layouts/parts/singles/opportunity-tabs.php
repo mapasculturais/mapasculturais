43196cb3a5be0bc21268e141c1b604ebe75c0706
José Cage <jose.cage@doesangue.me>
2021-02-25 17:29:34 +0100

    Colocar aba configuração dentro do if isEditable

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-tabs.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-tabs.php
@@ -2,10 +2,9 @@
 <ul class="abas clearfix">
     <?php $this->applyTemplateHook('tabs','begin'); ?>
     <li class="active"><a href="#main-content" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Principal");?></a></li>
-    
-    <li><a href="#form-config" rel="noopener noreferrer"><?php \MapasCulturais\i::_e("Configuração do Formulário"); ?></a></li>
 
     <?php if($this->isEditable()): ?>
+        <li><a href="#form-config" rel="noopener noreferrer"><?php \MapasCulturais\i::_e("Configuração do Formulário"); ?></a></li>
         <?php if(!$entity->isNew()): ?>
             <li><a href="#evaluations-config" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Configuração da Avaliação");?></a></li>
         <?php endif; ?>
