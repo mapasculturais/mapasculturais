0589cd3faa58037978c7122c12bd90323768620b
Erlei Biazzio <erleibiazzio@gmail.com>
2021-03-02 18:57:05 -0300

    Implementa forma de exibir as legendas dos gráficos via angular

--- src/protected/application/lib/modules/Reports/layouts/parts/dynamic-reports.php
+++ src/protected/application/lib/modules/Reports/layouts/parts/dynamic-reports.php
@@ -1,3 +1,31 @@
-<div class="dinamic-grafic" style="position: relative; height:0; width:100%;">
-    <canvas id="dinamic-grafic"></canvas>
+<?php
+use MapasCulturais\i;
+//$route = MapasCulturais\App::i()->createUrl('reports', $action, ['opportunity' => $opportunity->id, 'action' => $action]);
+?>
+
+
+<div class="dinamic-grafic-" style="position: relative; height:auto; width:100%;">    
+    <canvas id="dinamic-grafic-"></canvas>
 </div>
+
+<div class="chart-wrap" ng-repeat="grafic in data.loadingGrafics">
+    
+        <header>
+            <h3>Titulo</h3>
+            <a href="" name="{{grafic.identifier}}" class="hltip download" title="<?php i::_e("Baixar em CSV"); ?>"></a>
+        </header>
+        <div class="chart-container dinamic-grafic-{{grafic.identifier}}" style="position: relative; height:200px; width:100%;">
+            <canvas id="dinamic-grafic-{{grafic.identifier}}"></canvas>
+        </div>
+        
+        <footer>
+            <div class="legends-chats">
+                <div class="each" ng-repeat="legend in grafic.legends">
+                    <span class="dot" style="background-color: {{legend.color}}"></span><p>{{legend.value}}</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+
+
