709cbac75e9a554d125286b664b7e1b02e35aa59
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-03-03 22:31:27 -0300

    adiciona cache na busca de metadados das fases anteriores

--- src/protected/application/lib/modules/OpportunityPhases/Module.php
+++ src/protected/application/lib/modules/OpportunityPhases/Module.php
@@ -227,11 +227,23 @@ class Module extends \MapasCulturais\Module{
         });
 
         $app->hook('entity(Registration).get(<<projectName|field_*>>)', function(&$value, $metadata_key) use($app) {
-            
-            while(!$value && $this->previousPhase) {
+            if(!$value && $this->previousPhase) {
                 $this->previousPhase->registerFieldsMetadata();
-                $value = $this->previousPhase->$metadata_key;
+                
+                $cache_id = "registration:{$this->number}:$metadata_key";
+                if($app->cache->contains($cache_id)) {
+                    $value = $app->cache->fetch($cache_id);
+                } else {
+                    $value = $this->previousPhase->$metadata_key;
+                    $app->cache->save($cache_id, $value, DAY_IN_SECONDS);
+                }
             }
+        });
+
+
+        $app->hook('entity(registration).meta(<<projectName|field_*>>).update:after', function() use($app) {
+            $cache_id = "registration:{$this->owner->number}:{$this->key}";
+            $app->cache->delete($cache_id);
         });
 
         // registra os metadados das inscrićões das fases anteriores
