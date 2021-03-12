235a98cf17701a3a862b4522833b2efc99d463f5
José Cage <jose.cage@doesangue.me>
2021-03-12 17:36:08 +0100

    Criar fase de prestação de contas depois da última fase da oportunidade (refs: 1674)

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/widget-opportunity-accountability.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/widget-opportunity-accountability.php
@@ -4,9 +4,11 @@ use MapasCulturais\i;
 
 ?>
 <ul class="evaluation-methods">
+    <label ng-class="{disable:!newPhasePostData.isLastPhase}">
     <li class="evaluation-methods--item">
-        <input type="checkbox" id="hasAccountability" name="hasAccountability" ng-model="hasAccountability" ng-false-value="">
-        <label for="hasAccountability"><?php i::_e("Haverá prestação de contas"); ?></label>
+        <input type="checkbox" id="hasAccountability" name="hasAccountability" ng-model="newPhasePostData.hasAccountability" ng-disabled="!newPhasePostData.isLastPhase" ng-false-value="">
+        <?php i::_e("Haverá prestação de contas"); ?>
         <p class="evaluation-methods--name"><?php i::_e("Assinale caso a oportunidade exija prestação de contas"); ?></p>
     </li>
+    </label>
 </ul>
