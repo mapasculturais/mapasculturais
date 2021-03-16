acc41d2016a6df25e71957b88e1aa6c5b51a97c0
Erlei Biazzio <erleibiazzio@gmail.com>
2021-03-16 13:53:40 -0300

    Cria hooks para criar os modais das entidades

--- src/protected/application/themes/BaseV1/layouts/parts/modal/modal.php
+++ src/protected/application/themes/BaseV1/layouts/parts/modal/modal.php
@@ -1,7 +1,16 @@
+<?php
+$form_part = [
+    'event' => 'modal/form-event'
+];
+
+$part_name = $form_part[$entity_name] ?? 'modal/form';
+
+?>
+
 <a class="<?php echo $classes; ?> js-open-dialog" href="javascript:void(0)"
    data-dialog-block="true" data-dialog="#<?php echo $modal_id; ?>" data-dialog-callback="MapasCulturais.addEntity"
    data-form-action='insert' data-dialog-title="<?php \MapasCulturais\i::esc_attr_e('Modal de Entidade'); ?>">
     <?php echo $text ?>
 </a>
 
-<?php $this->part('modal/form', ['entity_name' => $entity_name, 'entity_classname' => $entity_classname, 'modal_id' => $modal_id, 'use_modal' => true]); ?>
+<?php $this->part($part_name, ['entity_name' => $entity_name, 'entity_classname' => $entity_classname, 'modal_id' => $modal_id, 'use_modal' => true]); ?>
