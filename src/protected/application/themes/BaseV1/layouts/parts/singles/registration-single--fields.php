884e3c226ec42687a31fc63505b59a87bb6498e6
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-03-19 02:41:48 -0300

    novos hooks na visualização da ficha de inscrição após o envio (refs: #1687)

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
@@ -2,10 +2,11 @@
     <!--
     <h4><?php \MapasCulturais\i::_e("Campos adicionais do formulário de inscrição.");?></h4>
     -->
-    
+    <?php $this->applyTemplateHook('registration-field-list', 'before') ?>
     <ul class="attachment-list" ng-controller="RegistrationFieldsController">
-
+        <?php $this->applyTemplateHook('registration-field-list', 'begin') ?>
         <li ng-repeat="field in data.fields" ng-if="showField(field)" id="field_{{::field.id}}" data-field-id="{{::field.id}}" ng-class=" (field.fieldType != 'section') ? 'js-field attachment-list-item registration-view-mode' : ''">
+            <?php $this->applyTemplateHook('registration-field-item', 'begin') ?>
             <div ng-if="field.fieldType !== 'file' && field.fieldType !== 'section' && field.fieldType !== 'persons' && field.config.entityField !== '@location' && field.config.entityField !== '@links' &&  field.fieldType !== 'links' ">
                 <label>{{field.required ? '*' : ''}} {{field.title}}: </label>
                 <span ng-if="entity[field.fieldName] && field.fieldType !== 'textarea'" ng-bind-html="printField(field, entity[field.fieldName])"></span>
@@ -40,6 +41,9 @@
                 <a ng-if="field.file" class="attachment-title" href="{{::field.file.url}}" target="_blank" rel='noopener noreferrer'>{{::field.file.name}}</a>
                 <span ng-if="!field.file"><em><?php \MapasCulturais\i::_e("Arquivo não enviado.");?></em></span>
             </div>
+            <?php $this->applyTemplateHook('registration-field-item', 'end') ?>
         </li>
+        <?php $this->applyTemplateHook('registration-field-list', 'end') ?>
     </ul>
+    <?php $this->applyTemplateHook('registration-field-list', 'after') ?>
 </div>
