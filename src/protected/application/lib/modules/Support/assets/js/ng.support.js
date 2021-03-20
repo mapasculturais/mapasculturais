7df689a499fe61ce5f42eb7cd5a5d715d0e4f1bc
Erlei Biazzio <erleibiazzio@gmail.com>
2021-03-20 20:44:08 -0300

    Separa o controlador angular em 2 (REF.:#1775)

--- src/protected/application/lib/modules/Support/assets/js/ng.support.js
+++ src/protected/application/lib/modules/Support/assets/js/ng.support.js
@@ -11,13 +11,61 @@
         };
     }]);
 
+    module.controller('SupportModal',['$scope', 'SupportService','$window', function($scope, SupportService, $window){        
+        $scope.data = {
+            openModal: false,
+            userPermissions: {},
+            fields: MapasCulturais.entity.registrationFieldConfigurations,
+        };
+
+        $scope.agents = {};
+
+        SupportService.findAgentsRelation().success(function (data, status, headers){
+           $scope.data.agents = data
+        });
+
+        $scope.savePermission = function(field){
+
+        }
+        
+    }]);
+
     module.controller('Support',['$scope', 'SupportService','$window', function($scope, SupportService, $window){        
-        $scope.data = {};
+        $scope.data = {
+            agents: [],
+        };
+
+        SupportService.findAgentsRelation().success(function (data, status, headers){
+           $scope.data.agents = data
+        });
     }]);
     
     module.factory('SupportService', ['$http', '$rootScope', 'UrlService', function ($http, $rootScope, UrlService) {  
        return {
+            findAgentsRelation: function (data) {
+                
+                var url = MapasCulturais.createUrl('support', 'getAgentsRelation', {opportunity_id: MapasCulturais.entity.id});
 
+                return $http.get(url, data).
+                success(function (data, status, headers) {
+                    $rootScope.$emit('registration.create', {message: "Reports found", data: data, status: status});
+                }).
+                error(function (data, status) {
+                    $rootScope.$emit('error', {message: "Reports not found for this opportunity", data: data, status: status});
+                });
+            },
+            setPermissonFields: function (data) {
+                
+                var url = MapasCulturais.createUrl('support', 'setPermissonFields', {opportunity_id: MapasCulturais.entity.id});
+
+                return $http.patch(url, data).
+                success(function (data, status, headers) {
+                    $rootScope.$emit('registration.create', {message: "Reports found", data: data, status: status});
+                }).
+                error(function (data, status) {
+                    $rootScope.$emit('error', {message: "Reports not found for this opportunity", data: data, status: status});
+                });
+            }
        };
     }]);
 
