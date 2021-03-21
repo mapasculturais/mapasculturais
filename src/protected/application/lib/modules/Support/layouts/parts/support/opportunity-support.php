3d9894e4da72f33def9ed5d22c9b7a46226788e2
everaldomatias <matiaseveraldo@gmail.com>
2021-03-20 23:09:18 -0300

    Frontend da listagem de agentes de suporte (refs #1715)

--- src/protected/application/lib/modules/Support/layouts/parts/support/opportunity-support.php
+++ src/protected/application/lib/modules/Support/layouts/parts/support/opportunity-support.php
@@ -4,18 +4,15 @@ use MapasCulturais\i;
 ?>
 <div ng-controller='Support'>
     <?php $this->applyTemplateHook('opportunity-support', 'before'); ?>
-    <div class="aba-content" id="support">
+    <div id="support" class="aba-content" id="support">
+
+        <header>
+            <h4><?php i::_e("Agentes autorizados"); ?></h4>
+            <p><?php i::_e("Gerencie os agentes de suporte dessa oportunidade."); ?></p>
+        </header>
+
         <div class="support-content">
             <?php $this->applyTemplateHook('opportunity-support', 'begin'); ?>
-
-            <header>
-                <div class="title">
-                    <h2><?php i::_e("Agentes autorizados"); ?></h2>
-                </div>
-                <div class="modal-button">
-
-                </div>
-            </header>
 
             <div class="support-body">
                 <div class="committee ng-scope" ng-repeat="(key,agent) in data.agents">
@@ -24,7 +21,7 @@ use MapasCulturais\i;
                             <span ng-click="data.openModal = true" class="btn btn-default add alignright mr10 ng-scope"><?php i::_e("Autorizar campos"); ?></span>
                             <img class="committee--avatar" ng-src="{{agent.owner.avatar.avatarSmall.url}}" src="{{agent.owner.avatar.avatarSmall.url}}">
                             <span class="committee--name ng-binding">{{agent.agent.name}}</span>
-                            <div>{{agent.group}}</div>
+                            <span class="committee--group">{{agent.group}}</span>
                         </div>
 
                         <div class="support-content-modal" ng-if="data.openModal" class="bg-support-modal">
