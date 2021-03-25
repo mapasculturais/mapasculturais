bfe536cedd9db8e0caeace4be9985eb6ec5b0a5c
everaldomatias <matiaseveraldo@gmail.com>
2021-03-25 09:25:31 -0300

    Adiciona array de cores para os gráficos dos relatórios

--- src/protected/application/lib/modules/Reports/Module.php
+++ src/protected/application/lib/modules/Reports/Module.php
@@ -464,12 +464,46 @@ class Module extends \MapasCulturais\Module
 
     public function color()
     {
-        mt_srand((double) microtime() * 1000000);
-        $c = '';
-        while (strlen($c) < 6) {
-            $c .= sprintf("%02X", mt_rand(0, 255));
-        }
-        return "#" . $c;
+
+        $colors = [
+            '#333333',
+            '#1c5690',
+            '#b3b921',
+            '#1dabc6',
+            '#e83f96',
+            '#cc0033',
+            '#9966cc',
+            '#40b4b4',
+            '#cc9933',
+            '#cc3333',
+            '#66cc66',
+            '#003c46',
+            '#d62828',
+            '#5a189a',
+            '#00afb9',
+            '#38b000',
+            '#3a0ca3',
+            '#489fb5',
+            '#245501',
+            '#708d81',
+            '#00bbf9',
+            '#f15bb5',
+            '#ffdab9',
+            '#5f0f40',
+            '#e9ff70',
+            '#fcf6bd',
+            '#4a5759',
+            '#06d6a0',
+            '#cce3de',
+            '#f3ac01'
+        ];
+
+        $rand = mt_rand(0, count($colors) - 1);
+
+        shuffle($colors);
+
+        return $colors[$rand];
+
     }
 
 }
