3e39467467359ce62d0c2061aa9037662e56b953
José Cage <jose.cage@doesangue.me>
2021-03-29 19:17:56 +0100

    Alterado texto da descrição da criação da fase de prestação de contas apenas

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/widget-opportunity-phase-only.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/widget-opportunity-phase-only.php
@@ -9,7 +9,7 @@ use MapasCulturais\i;
         <input type="radio" id="onlyAccountabilityPhase" name="accountability_phase" value="accountability" ng-change="data.step = 'accountability'" ng-model="newPhasePostData.evaluationMethod">
             <?php i::_e('Prestação de Contas'); ?>
         <p class="evaluation-methods--name">
-            <?php i::_e('Assinale caso a oportunidade exija prestação de contas'); ?>
+            <?php i::_e('Indica a criação da fase de prestação de contas'); ?>
         </p>
     </li>
 </label>
