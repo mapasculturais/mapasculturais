cfb02eaddcc92f7f1333a11367bf28eadd39bce4
Erlei Biazzio <erleibiazzio@gmail.com>
2021-04-05 11:43:34 -0300

    Plica estilo nas revisoes da prestação de contas (Ref.: #1745)

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/registration-revisions.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/registration-revisions.php
@@ -1,5 +1,6 @@
 <?php
 use MapasCulturais\i;
+
 $registration = $entity->registration->accountabilityPhase;
 $revisions = $app->repo("EntityRevision")->findEntityRevisions($registration);
 
@@ -13,21 +14,28 @@ $result = [
 ];
 
 ?>
-<ul>
-    <?php foreach ($result['revisions'] as $revision){ ?>
-        <?php foreach($result['dataRevisions'][$revision->id] as $v){?>
-            
-            <?php if($v->key == "openFields"){?>
-                <?php $openFields =  json_decode($v->value, true);?>
-               <?php if($openFields){?>
-                    <?php if(mb_strpos($revision->message,'Campo') !== false){?>
-                        <li><small><?=$revision->message?> <?=i::__("em");?> <?=$revision->createTimestamp->format('d/m/Y H:i:s')?></small></li>      
-                    <?php }?>
-                <?php } else {?>
-                    <li><small><?=i::__("Prestação enviada em");?> <?=$v->timestamp->format('d/m/Y H:i:s')?></small></li>
-                <?php }?>
+<?php $this->applyTemplateHook('widget-registration-revision-accountability', 'before'); ?>
+<div class="widget">
+<?php $this->applyTemplateHook('widget-registration-revision-accountability', 'begin'); ?>
+    <h3><?=i::__("Histórico");?></h3>
+    <ul class="widget-list widget-registration-revision-accountability">
+        <?php foreach ($result['revisions'] as $revision){ ?>
+            <?php foreach($result['dataRevisions'][$revision->id] as $v){?>
                 
+                <?php if($v->key == "openFields"){?>
+                    <?php $openFields =  json_decode($v->value, true);?>
+                <?php if($openFields){?>
+                        <?php if(mb_strpos($revision->message,'Campo') !== false){?>
+                            <li class="widget-list-item"><small><?=$revision->message?> <?=i::__("em");?> <?=$revision->createTimestamp->format('d/m/Y H:i:s')?></small></li>      
+                        <?php }?>
+                    <?php } else {?>
+                        <li class="widget-list-item"><small><?=i::__("Prestação enviada em");?> <?=$v->timestamp->format('d/m/Y H:i:s')?></small></li>
+                    <?php }?>
+                    
+                <?php }?>
             <?php }?>
         <?php }?>
-    <?php }?>
-</ul>
+    </ul>
+    <?php $this->applyTemplateHook('widget-registration-revision-accountability', 'end'); ?>
+</div>
+<?php $this->applyTemplateHook('widget-registration-revision-accountability', 'after'); ?>
