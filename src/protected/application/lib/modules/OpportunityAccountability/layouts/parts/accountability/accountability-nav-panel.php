703348432480bea7cb580a6f7e358689ccf7d95d
Erlei Biazzio <erleibiazzio@gmail.com>
2021-04-07 15:13:29 -0300

    Corrige identificação do li ativo no painel (Ref.: #1758)

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/accountability-nav-panel.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/accountability-nav-panel.php
@@ -1,5 +1,5 @@
 <li>
-    <a <?php if($this->template == 'panel/accountability') echo 'class="active"'; ?> href="<?php echo $app->createUrl('panel', 'accountability') ?>">
+    <a <?php if($this->template == 'panel/accountabilitys') echo 'class="active"'; ?> href="<?php echo $app->createUrl('panel', 'accountability') ?>">
         <span class="icon icon-opportunity"></span> 
         <?php \MapasCulturais\i::_e("Minhas prestações de contas");?>
     </a>
