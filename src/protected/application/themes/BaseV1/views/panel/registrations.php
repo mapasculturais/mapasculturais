29cb3803f8a4ae4b0c667f8b2614d7dd53f8fa35
Erlei Biazzio <erleibiazzio@gmail.com>
2021-04-15 09:20:27 -0300

    Impede que as iscrições em rascunho e fora do prazo de inscrição, sejam exibidas tambem no painel Minhas Inscrições (Ref.: #1759)

--- src/protected/application/themes/BaseV1/views/panel/registrations.php
+++ src/protected/application/themes/BaseV1/views/panel/registrations.php
@@ -1,7 +1,7 @@
 <?php
 use MapasCulturais\Entities\Registration;
 $this->layout = 'panel';
-
+$has_drafts_registration = false;
 $drafts = $app->repo('Registration')->findByUser($app->user, Registration::STATUS_DRAFT);
 $sent = $app->repo('Registration')->findByUser($app->user, 'sent');
 $app->applyHookBoundTo($this, 'panel(registration.panel):begin', [&$sent,&$drafts]);
@@ -22,9 +22,12 @@ $app->applyHookBoundTo($this, 'panel(registration.panel):begin', [&$sent,&$draft
     </ul>
     <div id="ativos">
         <?php foreach($drafts as $registration): ?>
+        <?php if($registration->opportunity->isRegistrationOpen()){?>
+            <?php $has_drafts_registration = true; ?>
             <?php $this->part('panel-registration', array('registration' => $registration)); ?>
+        <?php } ?>
         <?php endforeach; ?>
-        <?php if(!$drafts): ?>
+        <?php if(!$drafts || !$has_drafts_registration): ?>
             <div class="alert info"><?php \MapasCulturais\i::_e("Você não possui nenhum rascunho de inscrição.");?></div>
         <?php endif; ?>
     </div>
