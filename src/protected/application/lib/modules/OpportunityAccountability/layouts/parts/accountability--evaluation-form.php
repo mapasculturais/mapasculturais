dd8be2053fe158987177215270a25b8e6616d706
Erlei Biazzio <erleibiazzio@gmail.com>
2021-04-26 09:45:53 -0300

    Oculta o formulario de prestação de contas e exibe mensagem quando o parecer técnico já foi enviado

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability--evaluation-form.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability--evaluation-form.php
@@ -3,7 +3,7 @@ use MapasCulturais\i;
 use MapasCulturais\Entities\RegistrationEvaluation;
 
 $template_hook_params = ['registration' => $registration, 'opportunity' => $opportunity];
-$disable = ($evaluation->status == RegistrationEvaluation::STATUS_EVALUATED) ? " disabled" : "";
+$disable = ($evaluation->status == RegistrationEvaluation::STATUS_EVALUATED) ? " disabled" : null;
 // $this->jsObject['evaluationConfiguration'] = $entity->evaluationMethodConfiguration;
 ?>
 <?php $this->applyTemplateHook('evaluationForm.accountability', 'before', $template_hook_params) ?>
@@ -11,29 +11,33 @@ $disable = ($evaluation->status == RegistrationEvaluation::STATUS_EVALUATED) ? "
     <?php $this->applyTemplateHook('evaluationForm.accountability', 'begin', $template_hook_params) ?>
     <section>
         <h4> <?php i::_e('Resultado') ?> </h4>
-        <label>
-            <input type="radio" ng-model="evaluationData.result" value="10" <?=$disable?>>
-            <?php i::_e('Aprovado') ?>
-        </label>
+        <?php if(!$disable){?>
+            <label>
+                <input type="radio" ng-model="evaluationData.result" value="10" <?=$disable?>>
+                <?php i::_e('Aprovado') ?>
+            </label>
 
-        <label>
-            <input type="radio" ng-model="evaluationData.result" value="8" <?=$disable?>>
-            <?php i::_e('Aprovado com ressalvas') ?>
-        </label>
+            <label>
+                <input type="radio" ng-model="evaluationData.result" value="8" <?=$disable?>>
+                <?php i::_e('Aprovado com ressalvas') ?>
+            </label>
 
-        <label>
-            <input type="radio" ng-model="evaluationData.result" value="3" <?=$disable?>>
-            <?php i::_e('Não aprovado') ?>
-        </label>
+            <label>
+                <input type="radio" ng-model="evaluationData.result" value="3" <?=$disable?>>
+                <?php i::_e('Não aprovado') ?>
+            </label>
+        <?php } ?>
     </section>
-
-    <section>
-        <h4><?= i::__('Parecer técnico') ?></h4>
-        <textarea ng-model="evaluationData.obs" class="auto-height" <?=$disable?>></textarea>
-    </section>
+    <?php if(!$disable){?>
+        <section>
+            <h4><?= i::__('Parecer técnico') ?></h4>
+            <textarea ng-model="evaluationData.obs" class="auto-height" <?=$disable?>></textarea>
+        </section>
+    <?php } ?>
 
     <section class="actions">
         <?php if (!empty($disable)) { ?>
+            <span><?php i::_e("Parecer técnico já enviado") ?></span>
             <button class="btn btn-primary align-right" ng-click="reopenAccountability()"><?php i::_e("Reabrir prestação de contas") ?></button>
         <?php } else { ?>
             <button class="btn btn-primary align-right" ng-click="sendEvaluation()"><?php i::_e("Finalizar e enviar o parecer técnico") ?></button>
