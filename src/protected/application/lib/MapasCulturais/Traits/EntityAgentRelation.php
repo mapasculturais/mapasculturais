f19de2acff6314ce0b7848fcb1b46d0dc3881f6c
Junior-Shyko <franciscoanto@gmail.com>
2021-04-26 08:48:57 -0300

    Add ObjectType na funcao getGroupRelationsAgent para saber qual a instancia do grupo

--- src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
+++ src/protected/application/lib/MapasCulturais/Traits/EntityAgentRelation.php
@@ -107,6 +107,7 @@ trait EntityAgentRelation {
             if($group){
                 foreach ($group as $groupEntitie) {
                     $entitie = App::i()->repo($entitieType['object_type'])->find($groupEntitie->objectId);
+                    //ADICIONADO OBJECT_TYPE PARA SABER QUAL A INSTANCIA É O GRUPO
                     $groups[] = array(
                         'group'   => $groupEntitie->group,
                         'entitie' => $entitie->name,
