03d6731fb7d1272307105a0301f771f3ea05425e
everaldomatias <matiaseveraldo@gmail.com>
2021-05-14 11:47:29 -0300

    Corrige legendas dos gráficos dinâmicos na single

--- src/protected/application/lib/modules/Reports/layouts/parts/dynamic-reports.php
+++ src/protected/application/lib/modules/Reports/layouts/parts/dynamic-reports.php
@@ -6,7 +6,7 @@ use MapasCulturais\i;
 <?php $this->applyTemplateHook('dynamic-reports', 'before'); ?>
 <div class="charts-dynamic">
     <?php $this->applyTemplateHook('dynamic-reports', 'begin'); ?>
-    <div class="chart-wrap" ng-repeat="(key, graphic) in data.graphics">  
+    <div class="chart-wrap type-{{graphic.typeGraphic}}" ng-repeat="(key, graphic) in data.graphics">  
         <header>
             <h3>{{graphic.title}}</h3>
             <button ng-click="createCsv(graphic.reportData.graphicId)" name="{{graphic.identifier}}" class="hltip download" title="<?php i::_e("Baixar em CSV"); ?>"></button>
@@ -16,34 +16,47 @@ use MapasCulturais\i;
         </header>
         
         <div ng-if="graphic.typeGraphic == 'table'" class="chart-container dynamic-graphic-{{graphic.identifier}} chart-{{graphic.typeGraphic}}" style="position: relative; height:auto; width: 100%;">
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th ng-repeat="(key, label) in graphic.data.labels">{{label}}</th>
-                        <th><?php i::_e("Total"); ?></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr ng-repeat="(key, serie) in graphic.data.series track by $index">                    
-                        <td >{{serie.label}}</td>
-                        <td ng-repeat="(key, value) in serie.data track by $index">{{value}}</td>
-                        <td>{{graphic.data.sumLines[key]}}</td>
-                    </tr>
-                    <tr>
-                        <td><?php i::_e("Total"); ?></td>
-                        <td ng-repeat="(key, sumColumn) in graphic.data.sumColumns track by $index">{{sumColumn}}</td> 
-                        <td>{{graphic.data.total}}</td>
-                    </tr>
-                </tbody>
-            </table> 
-        </div>      
-        <div ng-class="{'chart-scroll':graphic.typeGraphic === 'line' || graphic.typeGraphic === 'bar'}">
-            <div ng-if="graphic.typeGraphic != 'table'" class="chart-container dynamic-graphic-{{graphic.identifier}} chart-{{graphic.typeGraphic}}" style="position: relative; height:auto;" ng-style="{'width': (graphic.typeGraphic == 'pie') ? '60%' : '{{graphic.countData}}%'}">
+            <div class="chart-scroll">    
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th ng-repeat="(key, label) in graphic.data.labels">{{label}}</th>
+                            <th><?php i::_e("Total"); ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr ng-repeat="(key, serie) in graphic.data.series track by $index">                    
+                            <td >{{serie.label}}</td>
+                            <td ng-repeat="(key, value) in serie.data track by $index">{{value}}</td>
+                            <td>{{graphic.data.sumLines[key]}}</td>
+                        </tr>
+                        <tr>
+                            <td><?php i::_e("Total"); ?></td>
+                            <td ng-repeat="(key, sumColumn) in graphic.data.sumColumns track by $index">{{sumColumn}}</td> 
+                            <td>{{graphic.data.total}}</td>
+                        </tr>
+                    </tbody>
+                </table> 
+            </div>
+        </div>
+
+        <div ng-if="graphic.typeGraphic === 'pie'" class="chart-container dynamic-graphic-{{graphic.identifier}} chart-{{graphic.typeGraphic}}" style="position: relative; height:auto; width:60%">
+            <canvas id="dynamic-graphic-{{graphic.identifier}}"></canvas>
+        </div>
+
+        <div ng-if="graphic.typeGraphic === 'bar'" class="chart-container dynamic-graphic-{{graphic.identifier}} chart-{{graphic.typeGraphic}}" style="position: relative; height:auto;" ng-style="'width': '{{graphic.countData}}%'">
+            <div class="chart-scroll">
                 <canvas id="dynamic-graphic-{{graphic.identifier}}"></canvas>
             </div>
         </div>
-            
+
+        <div ng-if="graphic.typeGraphic === 'line'" class="chart-container dynamic-graphic-{{graphic.identifier}} chart-{{graphic.typeGraphic}}" style="position: relative; height:auto; width:100%"">
+            <div class="chart-scroll">
+                <canvas id="dynamic-graphic-{{graphic.identifier}}"></canvas>
+            </div>
+        </div>
+
         <footer>
             <div class="legends-charts" id="dynamic-legends-{{graphic.identifier}}">
                 <div class="each" ng-if="graphic.typeGraphic != 'pie'" ng-repeat="(key, label) in graphic.data.legends">
