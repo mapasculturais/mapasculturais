4b55dd04ab3bc48893db5727f4bd958cf7be653a
Erlei Biazzio <erleibiazzio@gmail.com>
2021-05-27 15:37:27 -0300

    Insere hooks no arquivo para manipulação de conteúdo Ref.: #1840

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--tables.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-registrations--tables.php
@@ -1,4 +1,6 @@
+<?php $this->applyTemplateHook('opportunity-registrations--tables','before'); ?>
 <div id="inscritos" class="aba-content">
+    <?php $this->applyTemplateHook('opportunity-registrations--tables','begin'); ?>
     <?php if ($entity->canUser('@control')): ?>
         <?php $this->part('singles/opportunity-registrations--tables--manager', ['entity' => $entity]) ?>
 
@@ -7,5 +9,7 @@
     <?php elseif ($entity->publishedRegistrations): ?>
         <?php $this->part('singles/opportunity-registrations--tables--published', ['entity' => $entity]) ?>
     <?php endif; ?>
+    <?php $this->applyTemplateHook('opportunity-registrations--tables','end'); ?>
 </div>
+<?php $this->applyTemplateHook('opportunity-registrations--tables','after'); ?>
 <!--#inscritos-->
