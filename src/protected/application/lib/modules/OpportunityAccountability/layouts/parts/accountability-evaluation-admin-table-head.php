3b2bb35b507a1c7a0d9ac38c44c4aecc9e098c33
Erlei Biazzio <erleibiazzio@gmail.com>
2021-05-28 17:55:17 -0300

    Insere data de envio e data do parecer técnico na aba de pareceres Ref.:#1842

---
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability-evaluation-admin-table-head.php
@@ -0,0 +1,7 @@
+<th class="registration-status-col">
+    <?php \MapasCulturais\i::esc_attr_e("Data de envio"); ?>
+</th> 
+
+<th class="registration-status-col">
+    <?php \MapasCulturais\i::esc_attr_e("Data avaliação"); ?>
+</th>  
