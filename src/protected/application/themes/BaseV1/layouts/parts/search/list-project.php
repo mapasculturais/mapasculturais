88e24e68c71b0de5cc2f3d4587be35235aa6a938
pablorgds <pablorgds@gmail.com>
2021-08-13 09:46:27 -0300

    Alteração de inativação da aba de projetos do Mapas Culturais Ticket - 43621.

--- src/protected/application/themes/BaseV1/layouts/parts/search/list-project.php
+++ src/protected/application/themes/BaseV1/layouts/parts/search/list-project.php
@@ -1,10 +1,16 @@
-        <header id="project-list-header" class="entity-list-header clearfix" ng-show="data.global.filterEntity == 'project'">
-            <div class="clearfix">
-                <h1><span class="icon icon-project"></span> <?php \MapasCulturais\i::_e("Projetos");?></h1>
-                <a class="btn btn-accent add" href="<?php echo $app->createUrl('project', 'create') ?>"><?php \MapasCulturais\i::_e("Adicionar projeto");?></a>
-            </div>
-        </header>
-        
-        <div id="lista-dos-projetos" class="lista project" infinite-scroll="data.global.filterEntity === 'project' && addMore('project')" ng-show="data.global.filterEntity === 'project'">
-        <?php $this->part('search/list-project-item'); ?>
-        </div>
+<header id="project-list-header" class="entity-list-header clearfix" ng-show="data.global.filterEntity == 'project'">
+    <!--            <div class="info">-->
+    <!--                <h4>A funcionalidade de inclusão de projetos foi retirada </h4>-->
+    <!--            </div>-->
+    <div class="clearfix">
+        <h1><span class="icon icon-project"></span> <?php \MapasCulturais\i::_e("Projetos");?></h1>
+        <!--                <a class="btn btn-accent add" href="--><?php //echo $app->createUrl('project', 'create') ?><!--">--><?php //\MapasCulturais\i::_e("Adicionar projeto");?><!--</a>-->
+    </div>
+</header>
+<!--        <div id="lista-dos-projetos" class="lista project" infinite-scroll="data.global.filterEntity === 'project' && addMore('project')" ng-show="data.global.filterEntity === 'project'">-->
+<!--        --><?php //$this->part('search/list-project-item'); ?>
+<!--        </div>-->
+<div>
+    <h2 align="center" style="color: red">Aviso!</h2>
+    <span><h3 align="center">A funcionalidade de projetos se encontra em manutenção.</h3></span>
+</div>
