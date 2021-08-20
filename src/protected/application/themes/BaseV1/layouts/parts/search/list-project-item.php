f60b7cc3c10c88c5ac10168b317d082e6a677610
bporto94 <31289015+bporto94@users.noreply.github.com>
2021-08-20 11:58:15 -0300

    Update list-project-item.php
    Voltando as alterações

--- src/protected/application/themes/BaseV1/layouts/parts/search/list-project-item.php
+++ src/protected/application/themes/BaseV1/layouts/parts/search/list-project-item.php
@@ -1,33 +1,24 @@
-<div>
-    <h2 align="center" style="color: red">Aviso!</h2>
-    <span><h3 align="center">A funcionalidade de projetos se encontra em manutenção.</h3></span>
-</div>
-
-
-
-<!--        -->
-<!--        -->
-<!--            <article class="objeto clearfix"  ng-repeat="project in projects" id="agent-result-{{project.id}}">-->
-<!--                <h1><a href="{{project.singleUrl}}">{{project.name}}</a></h1>-->
-<!--                <div class="objeto-content clearfix">-->
-<!--                    <a href="{{project.singleUrl}}" class="js-single-url">-->
-<!--                        <img class="objeto-thumb" ng-src="{{project['@files:avatar.avatarMedium'].url||assetsUrl.avatarProject}}">-->
-<!--                    </a>-->
-<!--                    <p class="objeto-resumo">-->
-<!--                        {{project.shortDescription}}-->
-<!--                    </p>-->
-<!--                    <div class="objeto-meta">-->
-<!--                        --><?php //$this->applyTemplateHook('list.project.meta','begin'); ?>
-<!--                        <div><span class="label">--><?php //\MapasCulturais\i::_e("Tipo");?><!--:</span> <a href="#">{{project.type.name}}</a></div>-->
-<!--                        <div ng-if="readableProjectRegistrationDates(project)"><span class="label">--><?php //\MapasCulturais\i::_e("Inscrições");?><!--:</span> {{readableProjectRegistrationDates(project)}}</div>-->
-<!--                        <div>-->
-<!--                            <span class="label">Tags:</span>-->
-<!--                            <span ng-repeat="tags in project.terms.tag">-->
-<!--                                <a class="tag tag-project" href="--><?php //echo $app->createUrl('site', 'search') ?><!--##(project:(keyword:'{{tags}}'),global:(enabled:(project:!t),filterEntity:project,viewMode:list))">{{tags}}</a>-->
-<!--                            </span>-->
-<!--                        </div>-->
-<!--                        --><?php //$this->applyTemplateHook('list.project.meta','end'); ?>
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </article>-->
-            <!--.objeto-->
+            <article class="objeto clearfix"  ng-repeat="project in projects" id="agent-result-{{project.id}}">
+                <h1><a href="{{project.singleUrl}}">{{project.name}}</a></h1>
+                <div class="objeto-content clearfix">
+                    <a href="{{project.singleUrl}}" class="js-single-url">
+                        <img class="objeto-thumb" ng-src="{{project['@files:avatar.avatarMedium'].url||assetsUrl.avatarProject}}">
+                    </a>
+                    <p class="objeto-resumo">
+                        {{project.shortDescription}}
+                    </p>
+                    <div class="objeto-meta">
+                        <?php $this->applyTemplateHook('list.project.meta','begin'); ?>
+                        <div><span class="label"><?php \MapasCulturais\i::_e("Tipo");?>:</span> <a href="#">{{project.type.name}}</a></div>
+                        <div ng-if="readableProjectRegistrationDates(project)"><span class="label"><?php \MapasCulturais\i::_e("Inscrições");?>:</span> {{readableProjectRegistrationDates(project)}}</div>
+                        <div>
+                            <span class="label">Tags:</span>
+                            <span ng-repeat="tags in project.terms.tag">
+                                <a class="tag tag-project" href="<?php echo $app->createUrl('site', 'search') ?>##(project:(keyword:'{{tags}}'),global:(enabled:(project:!t),filterEntity:project,viewMode:list))">{{tags}}</a>
+                            </span>
+                        </div>
+                        <?php $this->applyTemplateHook('list.project.meta','end'); ?>
+                    </div>
+                </div>
+            </article>
+            .objeto
