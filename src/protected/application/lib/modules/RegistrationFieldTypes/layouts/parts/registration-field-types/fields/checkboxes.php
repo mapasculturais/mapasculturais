d0c4484d3c89b63eb9b492801e43cd0075ea58b8
erleibiazzio <erleibiazzio@gmail.com>
2021-08-25 21:17:00 -0300

    Ajusta campos do formulário atendendo issues Ref.: #1859, #1858 e #1857

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkboxes.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/checkboxes.php
@@ -1,4 +1,5 @@
-<div class="checkbox-group" ng-repeat="option in ::field.fieldOptions">
+
+<div class="{{setClassColumn(field.fieldOptions)}}" ng-repeat="option in ::field.fieldOptions">
     <label>
         <input type="checkbox" checklist-model="entity[fieldName]" ng-click="saveField(field, entity[fieldName])" checklist-value="option.indexOf(':') >= 0 ? option.split(':')[0] : option"> {{::option.indexOf(':') >= 0 ? option.split(':')[1] : option}}
     </label>
