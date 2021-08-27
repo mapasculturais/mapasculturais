fcc455821297620d49eb94a8eba69c91c97c67d2
erleibiazzio <erleibiazzio@gmail.com>
2021-08-27 13:45:17 -0300

    Insere quebra de linha no bloco section dos formulários

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/section.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/section.php
@@ -1,4 +1,5 @@
 <div ng-if="::field.fieldType == 'section'" id="field_{{::field.id}}" >
+    <br>
     <div class="label"> {{::field.title}} </div>
 
     <div ng-if="::field.description" class="attachment-description">{{::field.description}}</div>
