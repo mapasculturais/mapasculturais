05929736732a59653abe724506f37f4e21503b61
Bruno Porto <bporto92@gmail.com>
2021-09-15 10:03:47 -0300

    teste erros

--- src/index.php
+++ src/index.php
@@ -1,8 +1,4 @@
 <?php
 require_once __DIR__.'/protected/application/bootstrap.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 MapasCulturais\App::i()->run();
