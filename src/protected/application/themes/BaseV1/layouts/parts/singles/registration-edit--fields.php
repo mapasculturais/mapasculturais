a846eddad0695fa2cd06302e6c1b6b3ea91d37b4
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-10-15 12:07:01 -0300

    refatora autosave das entiades para salvar após 60 segundos da última modificação

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-edit--fields.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-edit--fields.php
@@ -3,7 +3,7 @@
     <h4><?php \MapasCulturais\i::_e("Campos adicionais");?></h4>
     <p class="registration-help"><?php \MapasCulturais\i::_e("Para efetuar sua inscrição, informe os campos abaixo.");?></p>
     -->
-    <ul class="attachment-list" ng-controller="RegistrationFieldsController">
+    <ul class="attachment-list">
         <li id="wrapper-{{field.fieldName}}" ng-repeat="field in ::data.fields" ng-if="showField(field)" on-repeat-done="registration-fields" data-field-name="{{field.fieldName}}" class="attachment-list-item registration-edit-mode attachment-list-item-type-{{field.fieldType}}">
             <?php $this->part('singles/registration-field-edit') ?>
 
