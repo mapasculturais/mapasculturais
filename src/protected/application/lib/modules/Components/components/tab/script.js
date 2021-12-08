797a931c54aa7c01906fd4ef98a2ef68747d7cc4
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-12-08 15:09:30 -0300

    addiciona componente para abas <tabs> e <tab> (refs #1885)

---
+++ src/protected/application/lib/modules/Components/components/tab/script.js
@@ -0,0 +1,73 @@
+app.component('tab', {
+    props: {
+        panelClass: {
+            type: String,
+            default: 'tabs-component-panel'
+        },
+        id: {
+            type: String,
+            default: null
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        prefix: {
+            type: String,
+            default: ''
+        },
+        suffix: {
+            type: String,
+            default: ''
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false
+        },
+        cacheTls: {
+            type: Number,
+            default: 60000
+        }
+    },
+    setup(props) {
+        const isActive = Vue.ref(false)
+        const cached = Vue.ref(false)
+        const tabsProvider = Vue.inject('tabsProvider')
+        const header = props.prefix + props.name + props.suffix
+        const computedId = props.id ? props.id : props.name.toLowerCase().replace(/ /g, '-')
+        const hash = '#' + (!props.isDisabled ? computedId : '')
+        let timeout;
+        Vue.watch(
+            () => tabsProvider.activeTabHash,
+            () => {
+                isActive.value = hash === tabsProvider.activeTabHash
+                clearTimeout(timeout);
+                if(isActive.value) {
+                    cached.value = true
+                } else {
+                    timeout = setTimeout(() => {
+                        cached.value = false
+                    }, props.cacheTls)
+
+                }
+            }
+        )
+        Vue.onBeforeMount(() => {
+            tabsProvider.tabs.push({
+                name: props.name,
+                header: header,
+                isDisabled: props.isDisabled,
+                hash: hash,
+                index: tabsProvider.tabs.length
+            })
+        })
+        return {
+            header,
+            computedId,
+            hash,
+            isActive,
+            cached
+        }
+    },
+    template: $TEMPLATES['tab']
+});
