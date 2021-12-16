3938eb53bd5d885cfa649a5e8daa9729bb4eb9c8
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-12-16 01:19:28 -0300

    continuação no desenvolvimento das classes Entity e API da base dos compoente Vue (refs #1880)

--- src/protected/application/lib/modules/Components/assets/js/components-base/Entity.js
+++ src/protected/application/lib/modules/Components/assets/js/components-base/Entity.js
@@ -1,14 +1,13 @@
 class Entity {
     constructor(objectType, id, scope) {
         this.id = id;
+        this.__scope = scope;
         this.__objectType = objectType;
-        this.__api = new API(objectType, scope || 'default');
-        this.__properties = this.__api.getEntityDescription('!relations');
-        this.__relations = this.__api.getEntityDescription('relations');
+        this.__properties = this.API.getEntityDescription('!relations');
+        this.__relations = this.API.getEntityDescription('relations');
         this.__skipDataProperties = ['createTimestamp', 'updateTimestamp'];
         
         this.__lists = [];
-        
         this.__processing = false;    
     }
 
@@ -55,60 +54,123 @@ class Entity {
         return result;
     }
 
+    get API () {
+        return new API(this.__objectType, this.__scope || 'default');
+    }
+
     get singleUrl() {
-        return this.__api.createUrl('single', [this.id]);
+        return this.API.createUrl('single', [this.id]);
+    }
+
+    get destroyUrl() {
+        return this.API.createUrl('destroy', [this.id]);
+    }
+
+    get publishUrl() {
+        return this.API.createUrl('publish', [this.id]);
+    }
+
+    get archiveUrl() {
+        return this.API.createUrl('archive', [this.id]);
     }
 
     get cacheId() {
-        return this.__api.createCacheId(this.id);
+        return this.API.createCacheId(this.id);
     }
 
     async save() {
         const messages = useMessages();
 
-        this.__processing = true;
-        return this.__api.persistEntity(this)
+        this.__processing = 'salvando';
+        return this.API.persistEntity(this)
             .then(() => {
                 this.__processing = false;
-                messages.alert('Modificações salvas')
             })
             .catch((error) => {
                 this.__processing = false;
                 console.log(error);
-                messages.error('Erro')
             });
     }
 
     async delete() {
         const messages = useMessages();
 
-        this.__processing = true;
-        return this.__api.deleteEntity(this)
+        this.__processing = 'excluindo';
+        return this.API.deleteEntity(this)
             .then(() => {
                 this.__processing = false;
                 this.__lists.forEach((list) => {
                     let index = list.indexOf(this);
-                    list.splice(index,1);
+                    if (index >= 0){
+                        list.splice(index,1);
+                    }
                 });
-                messages.alert('Objeto removido');
             })
             .catch((error) => {
                 this.__processing = false;
                 console.log(error);
-                messages.error('Erro ao remover objeto');
             });
         
     }
 
     async destroy() {
+        const messages = useMessages();
+
+        this.__processing = 'excluindo definitivamente';
+        return this.API.destroyEntity(this)
+            .then(() => {
+                this.__processing = false;
+                this.__lists.forEach((list) => {
+                    let index = list.indexOf(this);
+                    if (index >= 0){
+                        list.splice(index,1);
+                    }
+                });
+            })
+            .catch((error) => {
+                this.__processing = false;
+                console.log(error);
+            });
 
     }
 
     async publish() {
+        const messages = useMessages();
 
+        this.__processing = 'publicando';
+        return this.API.publishEntity(this)
+            .then(() => {
+                this.__processing = false;
+                this.__lists.forEach((list) => {
+                    let index = list.indexOf(this);
+                    if (index >= 0){
+                        list.splice(index,1);
+                    }
+                });
+            })
+            .catch((error) => {
+                this.__processing = false;
+                console.log(error);
+            });
     }
 
     async archive() {
+        const messages = useMessages();
 
+        this.__processing = 'arquivando';
+        return this.API.archiveEntity(this)
+            .then(() => {
+                this.__processing = false;
+                this.__lists.forEach((list) => {
+                    let index = list.indexOf(this);
+                    if (index >= 0){
+                        list.splice(index,1);
+                    }
+                });
+            })
+            .catch((error) => {
+                this.__processing = false;
+                console.log(error);
+            });
     }
 }
