5317dc087aafb3660455fda65862ed3227e2b86d
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-12-16 01:22:31 -0300

    componente <loading> exibe a mensagem da entity.__processing (refs #1880)

--- src/protected/application/lib/modules/Components/components/loading/template.php
+++ src/protected/application/lib/modules/Components/components/loading/template.php
@@ -1,4 +1,6 @@
 <?php 
 use MapasCulturais\i;
 ?>
-<span v-if="condition || entity?.__processing"><?php i::_e('carredando...') ?></span>
+<span v-if="condition || entity?.__processing">
+    <img src="<?php $this->asset('img/spinner.gif') ?>"> {{entity?.__processing || '<?php i::_e('carredando...') ?>'}}
+</span>
