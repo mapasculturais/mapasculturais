4dd81dc85521760fa71beeffc1a7c037c38110f9
Leonardo <leopiccionia@gmail.com>
2022-01-31 09:18:39 -0300

    Fix typo

--- src/protected/application/lib/modules/Components/components/loading/template.php
+++ src/protected/application/lib/modules/Components/components/loading/template.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 use MapasCulturais\i;
 ?>
 <span v-if="condition || entity?.__processing">
-    <img src="<?php $this->asset('img/spinner.gif') ?>"> {{entity?.__processing || '<?php i::_e('carredando...') ?>'}}
+    <img src="<?php $this->asset('img/spinner.gif') ?>"> {{entity?.__processing || '<?php i::_e('carregando...') ?>'}}
 </span>
