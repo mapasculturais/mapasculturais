46d23f39ff4b1ffea95217cc70aade569fc8a37d
Leonardo <leopiccionia@gmail.com>
2022-02-01 10:37:37 -0300

    Remove implementation details from component template

--- src/protected/application/lib/modules/Components/components/tabs/template.php
+++ src/protected/application/lib/modules/Components/components/tabs/template.php
@@ -5,11 +5,11 @@ $this->import('tab');
     <ul class="tabs-component__buttons" role="tablist">
         <li v-for="tab in tabs" :key="tab.slug"
             class="tabs-component__button"
-            :class="[tab.slug, tab.disabled && 'tab-component__button--disabled', (tab.slug === activeTab.slug) && 'tab-component__button--active']"
+            :class="[tab.slug, tab.disabled && 'tab-component__button--disabled', isActive(tab) && 'tab-component__button--active']"
             role="presentation">
             <a
                 :aria-controls="tab.hash"
-                :aria-selected="tab.slug === activeTab.slug"
+                :aria-selected="isActive(tab)"
                 :href="tab.hash"
                 role="tab"
                 @click="selectTab(tab.slug, $event)">
