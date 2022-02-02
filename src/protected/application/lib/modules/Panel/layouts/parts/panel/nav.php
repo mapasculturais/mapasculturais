22065befd51593ac693a90e3235757ced62777f5
Leonardo <leopiccionia@gmail.com>
2022-02-02 15:53:44 -0300

    Experiment new icon set

--- src/protected/application/lib/modules/Panel/layouts/parts/panel/nav.php
+++ src/protected/application/lib/modules/Panel/layouts/parts/panel/nav.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 use MapasCulturais\i;
 ?>
 <h2><span><?=i::__('Menu do painel')?></h2>
@@ -6,31 +6,36 @@ use MapasCulturais\i;
     <ul>
         <li>
             <a href="#">
-                <iconify icon="lucide:layout-dashboard"></iconify>
+                <!-- <iconify icon="lucide:layout-dashboard"></iconify> -->
+                <iconify icon="mdi:view-dashboard-outline"></iconify>
                 <span><?=i::__('Visão geral')?></span>
             </a>
         </li>
         <li>
             <a href="#">
-                <iconify icon="fa-solid:user-friends"></iconify>
+                <!-- <iconify icon="fa-solid:user-friends"></iconify> -->
+                <iconify icon="mdi:account-multiple-outline"></iconify>
                 <span><?=i::__('Meus agentes')?></span>
             </a>
         </li>
         <li>
             <a href="#">
-                <iconify icon="clarity:building-line"></iconify>
+                <!-- <iconify icon="clarity:building-line"></iconify> -->
+                <iconify icon="mdi:domain"></iconify>
                 <span><?=i::__('Meus espaços')?></span>
             </a>
         </li>
         <li>
             <a href="#">
-                <iconify icon="ant-design:calendar"></iconify>
+                <!-- <iconify icon="ant-design:calendar"></iconify> -->
+                <iconify icon="mdi:calendar-month"></iconify>
                 <span><?=i::__('Meus eventos')?></span>
             </a>
         </li>
         <li>
             <a href="#">
-                <iconify icon="ri:file-list-2-line"></iconify>
+                <!-- <iconify icon="ri:file-list-2-line"></iconify> -->
+                <iconify icon="mdi:clipboard-text-outline"></iconify>
                 <span><?=i::__('Meus projetos')?></span>
             </a>
         </li>
