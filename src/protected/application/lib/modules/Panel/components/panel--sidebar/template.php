7757a8aad7ed6c937c63e6118fba34bf0eaf6f30
Leonardo <leopiccionia@gmail.com>
2022-02-04 12:42:43 -0300

    Adiciona seções à sidebar do painel

--- src/protected/application/lib/modules/Panel/components/panel--sidebar/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--sidebar/template.php
@@ -13,37 +13,74 @@ use MapasCulturais\i;
         <ul>
             <li>
                 <a href="#">
-                    <!-- <iconify icon="lucide:layout-dashboard"></iconify> -->
                     <iconify icon="mdi:view-dashboard-outline"></iconify>
                     <span><?=i::__('Visão geral')?></span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <!-- <iconify icon="fa-solid:user-friends"></iconify> -->
                     <iconify icon="mdi:account-multiple-outline"></iconify>
                     <span><?=i::__('Meus agentes')?></span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <!-- <iconify icon="clarity:building-line"></iconify> -->
                     <iconify icon="mdi:domain"></iconify>
                     <span><?=i::__('Meus espaços')?></span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <!-- <iconify icon="ant-design:calendar"></iconify> -->
                     <iconify icon="mdi:calendar-month"></iconify>
                     <span><?=i::__('Meus eventos')?></span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <!-- <iconify icon="ri:file-list-2-line"></iconify> -->
                     <iconify icon="mdi:clipboard-text-outline"></iconify>
                     <span><?=i::__('Meus projetos')?></span>
+                </a>
+            </li>
+        </ul>
+        <h3><?=i::__('Editais e oportunidades')?></h3>
+        <ul>
+            <li>
+                <a href="#">
+                    <iconify icon="mdi:lightbulb-on-outline"></iconify>
+                    <span><?=i::__('Minhas inscrições')?></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <iconify icon="mdi:lightbulb-on-outline"></iconify>
+                    <span><?=i::__('Minhas oportunidades')?></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <iconify icon="mdi:lightbulb-on-outline"></iconify>
+                    <span><?=i::__('Prestações de contas')?></span>
+                </a>
+            </li>
+        </ul>
+        <h3><?=i::__('Outras opções')?></h3>
+        <ul>
+            <li>
+                <a href="#">
+                    <iconify icon="grommet-icons:connect"></iconify>
+                    <span><?=i::__('Vincular cadastro')?></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <iconify icon="mdi:puzzle-outline"></iconify>
+                    <span><?=i::__('Meus apps')?></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <iconify icon="mdi:cog-outline"></iconify>
+                    <span><?=i::__('Configuração de conta')?></span>
                 </a>
             </li>
         </ul>
