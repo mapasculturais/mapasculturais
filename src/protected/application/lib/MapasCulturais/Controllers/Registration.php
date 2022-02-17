d13a47538e29f09c31be3f4f4c3fc608d1e6c127
erleibiazzio <erleibiazzio@gmail.com>
2022-02-17 16:57:07 -0300

    Aplica redução no nome do campo para evitar problemas na rename do arquivo de anexo no processo de upload Ref.: #1929

--- src/protected/application/lib/MapasCulturais/Controllers/Registration.php
+++ src/protected/application/lib/MapasCulturais/Controllers/Registration.php
@@ -97,7 +97,7 @@ class Registration extends EntityController {
             $finfo = pathinfo($this->name);
             $hash = uniqid();
 
-            $this->name = $this->owner->number . ' - ' . $hash . ' - ' . preg_replace ('/[^\. \-\_\p{L}\p{N}]/u', '', $rfc->title) . '.' . $finfo['extension'];
+            $this->name = $this->owner->number . ' - ' . $hash . ' - ' . substr( preg_replace ('/[^\. \-\_\p{L}\p{N}]/u', '', $rfc->title),0,64) . '.' . $finfo['extension'];
             $tmpFile = $this->tmpFile;
             $tmpFile['name'] = $this->name;
             $this->tmpFile = $tmpFile;
