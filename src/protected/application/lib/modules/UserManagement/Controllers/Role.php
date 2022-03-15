761166ab837453bfad2f72114da07fa951ddc318
erleibiazzio <erleibiazzio@gmail.com>
2022-03-15 14:35:43 -0300

    Define o entityClassName do controller Role Ref.: #1932

--- src/protected/application/lib/modules/UserManagement/Controllers/Role.php
+++ src/protected/application/lib/modules/UserManagement/Controllers/Role.php
@@ -5,6 +5,13 @@ use MapasCulturais\App;
 use MapasCulturais\Traits;
 
 class Role extends \MapasCulturais\Controllers\EntityController {
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->entityClassName = "MapasCulturais\\Entities\\Role";
+    }
+
     use Traits\ControllerAPI,
         Traits\ControllerSoftDelete,
         Traits\ControllerDraft;
