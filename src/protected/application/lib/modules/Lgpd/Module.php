bae53138ef5b4bfe7b85c47a3951989c4055be1d
robedj <ed.wassouf@gmail.com>
2022-03-30 17:46:50 -0400

    Registro dos metadados Ref.:#1947

--- src/protected/application/lib/modules/Lgpd/Module.php
+++ src/protected/application/lib/modules/Lgpd/Module.php
@@ -4,11 +4,18 @@ namespace Lgpd;
 
 class Module extends \MapasCulturais\Module{
     public function _init(){
-        
+       
+
     }
     public function register()
     {
-  
+        $this->registerMetadata( 'MapasCulturais\\Entities\\Agent', 'acept_lgpd', [
+            'label'=> 'Aceite dos termos e condicoes da LGPD',
+            'type'=>'array',
+            'private'=> true,
+            'default'=> null,
+            
+        ]);
     }
 
 }
