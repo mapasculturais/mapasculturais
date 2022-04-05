6df29009ea1c8b4cefc122c9a31af27af254f783
robedj <ed.wassouf@gmail.com>
2022-04-05 15:21:31 -0400

    Separa endpoints do termo de uso e politica de privacidade Ref.:#1947

--- src/protected/application/lib/modules/LGPD/Controller.php
+++ src/protected/application/lib/modules/LGPD/Controller.php
@@ -12,35 +12,61 @@ class Controller  extends \MapasCulturais\Controller{
     function __construct()
     {
         parent::construct();
-     
     }
 
     public function GET_termsOfUsage(){ 
         $app= App::i();
         $this->render('terms-of-usage');
     }
-    public function POST_acept ()
+    public function GET_policePrivacy ()
     {
         $app= App::i();
-   
+        $this->render('privacy-police');
+    }
     
-        $aceptTerms = [
+    public function POST_aceptPolicePrivacy ()
+    {
+        $app= App::i();
+        $config = $app->config['module.LGPD'];
+ 
+        $text = $config['privacyPolice'];
+        $acept_terms[md5($text)] = [
             'timestamp' => (new DateTime())->getTimestamp(),
-            // 'md5' => md5($text),
-            // 'texto' => $text,
+            'md5' => md5($text),
+            'text' => $text,
+        ];
+
+        var_dump($acept_terms);
+        exit;
+                
+    }
+     
+    public function POST_aceptTermsOfUsage ()
+    {
+        $app= App::i();
+        $config = $app->config['module.LGPD'];
+       
+        $text = $config['termsOfUsage'];
+        $acept_terms[md5($text)] = [
+            'timestamp' => (new DateTime())->getTimestamp(),
+            'md5' => md5($text),
+            'text' => $text,
         
         ];
-        $agent = $app->repo('Agent')->find(['id' => $app->user->profile->id]);
         
+        var_dump($acept_terms);
+        exit;
+
+        $agent = $app->repo('Agent')->find(['id' => $app->user->profile->id]);
         if( $_acept_lgpd = json_decode($agent->acept_lgpd, true)){
             $flag = false;
             foreach($_acept_lgpd as $term){
-                if($term['md5'] == $aceptTerms['md5']){
+                if($term['md5'] == $acept_terms['md5']){
                     $flag = true;
                 }
             }
             if(!$flag){    
-                $_acept_lgpd[] = $aceptTerms;
+                $_acept_lgpd[] = $acept_terms;
                 $aceptTerms =$_acept_lgpd;
             }
         }
