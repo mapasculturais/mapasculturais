7142087fdf6c923596453b5e7601920167730a30
robedj <ed.wassouf@gmail.com>
2022-04-05 15:23:30 -0400

    Cria arquivos partes para exibicao dos termos de uso e politica de privacidade Ref.:#1947

---
+++ src/protected/application/lib/modules/LGPD/layouts/parts/lgpd/acept-policePrivacy.php
@@ -0,0 +1,10 @@
+<div class="box">
+    <form action='<?= $url ?>' method="POST"> 
+    <?= $config['privacyPolice']?>
+    <button type='submit'>Aceitar privacidade</button>
+
+    </form>
+      
+</div>
+
+
