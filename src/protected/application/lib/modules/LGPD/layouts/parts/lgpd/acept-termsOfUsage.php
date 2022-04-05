b4380964f4f333853e93f5d0065f922f31e7f1b1
robedj <ed.wassouf@gmail.com>
2022-04-05 15:23:30 -0400

    Cria arquivos partes para exibicao dos termos de uso e politica de privacidade Ref.:#1947

---
+++ src/protected/application/lib/modules/LGPD/layouts/parts/lgpd/acept-termsOfUsage.php
@@ -0,0 +1,11 @@
+<div class="box">
+    <form action='<?= $url ?>' method="POST"> 
+    <?= $config['termsOfUsage']?>
+    <button type='submit'>Aceitar</button>
+
+    </form>
+      
+</div>
+
+
+<!-- montar um formulario -->
