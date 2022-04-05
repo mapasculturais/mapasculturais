65089ade7979486e0886760724c70447952b650c
robedj <ed.wassouf@gmail.com>
2022-04-05 15:30:11 -0400

    Renomeia view de aceite de termos de uso  Ref.:#1947

--- src/protected/application/lib/modules/LGPD/views/lgpd/terms-of-usage.php
+++ src/protected/application/lib/modules/LGPD/views/lgpd/terms-of-usage.php
@@ -1,2 +1,10 @@
 <?php
-echo 'qualquer coisa';
+
+use MapasCulturais\App;
+
+$app = App::i();
+$config = $app->config['module.LGPD'];
+
+$url = $app->createUrl('lgpd','aceptTermsOfUsage');
+$this->part('lgpd/acept-termsOfUsage', ['url' => $url, 'config' => $config]);
+
