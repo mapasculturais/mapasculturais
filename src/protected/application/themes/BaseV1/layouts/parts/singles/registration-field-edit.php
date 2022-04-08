cefe388c0b9895a2b6d059f597b5fb50534fee34
Victor Magalhaes <victor.magalhaesp@gmail.com>
2022-04-08 14:01:26 -0300

    altera ng repeat para ul - exluir arquivo

--- src/protected/application/themes/BaseV1/layouts/parts/singles/registration-field-edit.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/registration-field-edit.php
@@ -31,8 +31,8 @@ foreach ($definitions as $def) {
         </li>
     </ul>
 
-    <ul ng-if="field.multiple" class="widget-list js-downloads js-slimScroll">
-        <li ng-repeat="file in field.file" id="{{file.id}}" class="widget-list-item is-editable">
+    <ul ng-if="field.multiple" ng-repeat="file in field.file track by $index" class="widget-list js-downloads js-slimScroll">
+        <li id="{{file.id}}" class="widget-list-item is-editable">
             <a href="{{file.url}}" target="_blank" rel='noopener noreferrer'><span>{{file.description}} - {{file.name}}</span></a>
             <div class="botoes">
                 <a hltip ng-click="removeFile(file)" class="delete hltip" title="<?php \MapasCulturais\i::esc_attr_e("Excluir arquivo");?>"></a>
