b2d42c77dbc81b64283b49003f0dfdce9618907a
robedj <ed.wassouf@gmail.com>
2022-04-08 12:00:39 -0400

    botao de aceite pro usuario logado  Ref.:#1947

--- src/protected/application/lib/modules/LGPD/views/lgpd/acept.php
+++ src/protected/application/lib/modules/LGPD/views/lgpd/acept.php
@@ -1,3 +1,4 @@
+
 <?php use MapasCulturais\i; ?>
 
 <div class="box">
@@ -6,11 +7,11 @@
     
     <?php if(!$app->user->is('guest')):?>
         <?php if($accepted): ?>
-         <p>   <?= sprintf(i::__('Aceito em %s'), date(i::__('d/m/Y à\s H:i'), $accepted->timestamp)) ?></p> 
-            <?php else: ?>
-                <form action='<?= $url ?>' method="POST"> 
-                    <button type='submit'><?= i::__('Aceitar') ?> </button>
-                </form>
-        <?php endif ?>
-    <?php endif ?>
+            <p><?= sprintf(i::__('Aceito em %s'), date(i::__('d/m/Y à\s H:i'), $accepted->timestamp)) ?></p> 
+        <?php else: ?>
+            <form action='<?= $url ?>' method="POST"> 
+                <button type='submit'><?= i::__('Aceitar') ?> </button>
+            </form>
+        <?php endif; ?>
+    <?php endif; ?>
 </div>
