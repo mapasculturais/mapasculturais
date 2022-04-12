59567b329bd0568db3efd243c6e3c26a9f9c08fb
robedj <ed.wassouf@gmail.com>
2022-04-12 17:05:01 -0400

    Ajustes no css e html do texto .:#1950

--- src/protected/application/lib/modules/LGPD/views/lgpd/accept.php
+++ src/protected/application/lib/modules/LGPD/views/lgpd/accept.php
@@ -10,13 +10,13 @@ use MapasCulturais\i;
             <p ><?= sprintf(i::__('Aceito em %s'), date(i::__('d/m/Y à\s H:i'), $accepted->timestamp)) ?></p> 
         <?php else: ?>
             <div class='align-button'>
-            <form action='<?= $url ?>' method="POST"> 
-                <button type='submit'><?= i::__('Aceitar') ?> </button>
-            </form>
+                <form action='<?= $url ?>' method="POST"> 
+                    <button class='accept' type='submit'><?= i::__('Aceitar') ?> </button>
+                </form>
                 <form action ="<?=$app->createUrl('auth', 'logout') ?>" method="POST"> 
-                <button type = 'submit'><?=i::__('Não Aceitar')?></button>
-            </form>
-        </div>
+                    <button class='refuse' type = 'submit'><?=i::__('Recusar')?></button>
+                </form>
+            </div>
         <?php endif; ?>
     <?php endif; ?>
 </div>
