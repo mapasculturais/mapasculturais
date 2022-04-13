d6b8e7b85267f8dfbdcfdf6a4d69ab971e9ddf26
robedj <ed.wassouf@gmail.com>
2022-04-13 12:17:02 -0400

    Tratar o caso do md5 para letras maiusculas e minusculas .:#1951

--- src/protected/application/lib/modules/LGPD/Module.php
+++ src/protected/application/lib/modules/LGPD/Module.php
@@ -2,6 +2,7 @@
 
 namespace LGPD;
 
+use Doctrine\ORM\Mapping\Entity;
 use MapasCulturais\App;
 class Module extends \MapasCulturais\Module{
    
@@ -21,12 +22,17 @@ class Module extends \MapasCulturais\Module{
         $app->hook('GET(<<*>>):before,-GET(lgpd.<<*>>):before', function() use ($app){
             if($app->user->is('guest'))
                 return;
-            
+           
+            if(!isset($_SESSION['_getReferer']) || empty($_SESSION['_getReferer'])){
+                $_SESSION['_getReferer'] = $app->request()->getReferer();
+            } 
+            var_dump($_SERVER['HTTP_REFERER']);
+            die;
             $user = $app->user;
             $config = $app->config['module.LGPD'];
             
             foreach($config as $key => $value){
-                $term_hash = md5($value['text']);
+                $term_hash = self::createHash($value['text']);
                 $accept_terms = $user->{"lgpd_{$key}"};
                 if(!isset($accept_terms->$term_hash)){
                     $url =  $app->createUrl('lgpd', 'accept', [$key]);
@@ -35,7 +41,15 @@ class Module extends \MapasCulturais\Module{
             }
         });
     }
-
+    public static function createHash(string $text):string
+    {
+        $text = str_replace(" ", "", trim($text));
+        $text = strip_tags($text);
+        $text = str_replace("\n", "", trim($text));
+        $text = str_replace("\t", "", trim($text));
+        $text = strtolower($text);
+        return md5($text);
+    }
     public function register() 
     {
         $app= App::i();
