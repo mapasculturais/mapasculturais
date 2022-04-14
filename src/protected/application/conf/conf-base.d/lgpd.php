2b2e9ad13cfdca25fe99ef2003f07da9021b8e8f
erleibiazzio <erleibiazzio@gmail.com>
2022-04-14 11:05:57 -0300

    Deixa as configuações do modulo LGPD por default  vazia para evitar redirecionamento sem configurações de termos Ref.: #1945

--- src/protected/application/conf/conf-base.d/lgpd.php
+++ src/protected/application/conf/conf-base.d/lgpd.php
@@ -1,16 +1,5 @@
 <?php
 
 return [
-    'module.LGPD' => [
-        'termsOfUsage'=> [
-           'title' => 'Termos de uso', 
-           'text' => 'Texto dos termos de uso', 
-        ],
-        'privacyPolice'=>[
-            'title' => 'Política de privacidade', 
-            'text'  => 'Texto da poítica de privacidade', 
-        ],
-        
-    ]
-
+    'module.LGPD' => []
 ];
