a58a377cc041976731e980a7f7f2773fef9cd1bd
erleibiazzio <erleibiazzio@gmail.com>
2022-04-18 16:31:58 -0300

    Adiciona opção de selecionar os campos de resumo, categoria, agente relacionado e espaçoes relacionados na lista de campos liberados para os avaliadores Ref.: #1945

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations-fields--config.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations-fields--config.php
@@ -2,8 +2,24 @@
     <div>
         <h4><?php \MapasCulturais\i::_e("Configurar campos visíveis para os avaliadores");?></h4>
         <div>
+            <label>
+                <input type="checkbox" ng-model="data.category" ng-click="selectFields('category')"> <?php \MapasCulturais\i::_e("Categoria");?>
+            </label>
+
+            <label>
+                <input type="checkbox"  ng-model="data.projectName" ng-click="selectFields('projectName')"> <?php \MapasCulturais\i::_e("Nome do projeto");?>
+            </label>
+
+            <label>
+                <input type="checkbox" ng-model="data.agentsSummary" ng-click="selectFields('agentsSummary')"> <?php \MapasCulturais\i::_e("Resumo dos agentes");?>
+            </label>
+
+            <label>
+                <input type="checkbox" ng-model="data.spaceSummary" ng-click="selectFields('spaceSummary')"> <?php \MapasCulturais\i::_e("Resumo dos agentes");?>
+            </label>
+
             <label ng-repeat="field in data.fields">
-                <input type="checkbox"> {{field.title}}
+                <input type="checkbox" ng-click="selectFields(field)"> {{field.title}}
             </label>
         </div>
         
