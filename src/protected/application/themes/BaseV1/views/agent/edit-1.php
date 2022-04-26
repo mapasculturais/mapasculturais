52525dd82b9219ff6283136ff23c856261586024
robedj <ed.wassouf@gmail.com>
2022-04-26 16:55:54 -0400

    Cartão de visitas funcionando  na visão de edição do agente Ref:.#1962

--- src/protected/application/themes/BaseV1/views/agent/edit-1.php
+++ src/protected/application/themes/BaseV1/views/agent/edit-1.php
@@ -47,6 +47,7 @@ $editEntity = $this->controller->action === 'create' || $this->controller->actio
                             <?php $this->part('singles/type', ['entity' => $entity]) ?><!--.part/singles/type.php -->
                             <?php $this->part('singles/name', ['entity' => $entity]) ?><!--.part/singles/name.php -->
                         </div>
+                        <hr>
                         <div class="edit-card-header-widgets">
                             <?php $this->part('widget-areas', array('entity'=>$entity)); ?>
                             <?php $this->part('widget-tags', array('entity'=>$entity)); ?>
@@ -60,7 +61,17 @@ $editEntity = $this->controller->action === 'create' || $this->controller->actio
                         <?php \MapasCulturais\i::_e("caracteres. Você deve alterar seu texto ou este será cortado ao salvar.");?>
                     </div>
                 <?php endif; ?>
-                <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"shortDescription") && $editEntity? 'required': '');?>" data-edit="shortDescription" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Descrição Curta");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira uma descrição curta");?>" data-showButtons="bottom" data-tpl='<textarea maxlength="400"></textarea>'><?php echo $this->isEditable() ? $entity->shortDescription : nl2br($entity->shortDescription); ?></span>
+                
+               <?php if($this->isEditable() || $entity->site): ?>
+                    <p><span class="label"><?php \MapasCulturais\i::_e("Site");?>:</span>
+                    <?php if($this->isEditable()): ?>
+                        <span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"site") && $editEntity? 'required': '');?>" data-edit="site" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Site");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira a url de seu site");?>"><?php echo $entity->site; ?></span></p>
+                    <?php else: ?>
+                        <a class="url" href="<?php echo $entity->site; ?>"><?php echo $entity->site; ?></a>
+                    <?php endif; ?>
+                <?php endif; ?>    
+               
+                <span class="label"><?php \MapasCulturais\i::_e("Descrição curta:");?></span><span class="js-editable <?php echo ($entity->isPropertyRequired($entity,"shortDescription") && $editEntity? 'required': '');?>" data-edit="shortDescription" data-original-title="<?php \MapasCulturais\i::esc_attr_e("Descrição Curta");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Insira uma descrição curta");?>" data-showButtons="bottom" data-tpl='<textarea maxlength="400"></textarea>'><?php echo $this->isEditable() ? $entity->shortDescription : nl2br($entity->shortDescription); ?></span>
                 <p align=”left”> <?php $this->part('redes-sociais', array('entity'=>$entity));?></p>
                 <!--  -->
                 
