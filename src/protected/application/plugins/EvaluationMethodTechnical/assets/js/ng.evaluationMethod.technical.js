1f7afa525329784f9d8a1ef6a33e2fdaf1900b7d
erleibiazzio <erleibiazzio@gmail.com>
2022-04-30 12:24:39 -0300

    Registra metadado que salva o valor percentual máximo para as politicas afirmativas

--- src/protected/application/plugins/EvaluationMethodTechnical/assets/js/ng.evaluationMethod.technical.js
+++ src/protected/application/plugins/EvaluationMethodTechnical/assets/js/ng.evaluationMethod.technical.js
@@ -51,6 +51,7 @@
                 sumAffiermativePoliciefieldsPercent: 0,
                 isActiveAffirmativePolicies: MapasCulturais.isActiveAffirmativePolicies == "true" ? true : false,
                 fieldConfig: [],
+                affirmativePolicie: 0.00,
                 
                 debounce: 2000
             };
@@ -74,6 +75,7 @@
                     enableViability: $scope.data.enableViability,
                     affirmativePolicies: JSON.stringify($scope.data.criteriaAffirmativePolicies) == "[]" ? null : $scope.data.criteriaAffirmativePolicies,
                     isActiveAffirmativePolicies: $scope.data.isActiveAffirmativePolicies,
+                    affirmativePoliciesRoof: $scope.data.affirmativePolicie.roof || 0.00
                 };
 
                 $scope.data.criteria.forEach(function (crit) {
