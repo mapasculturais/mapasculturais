151d3f0c70ff7c4a78e849f80c8a48a55f9b9f08
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-06 17:47:28 -0300

    Ajustes no template - #1986

--- src/protected/application/lib/modules/Components/components/entity-terms/template.php
+++ src/protected/application/lib/modules/Components/components/entity-terms/template.php
@@ -1,7 +1,7 @@
 <div v-if="entity.terms?.[taxonomy]" class="entity-terms">
 
-    <h4 v-if="title == ''"> tax: {{taxonomy}} </h4>
-    <h4 v-else> title: {{title}} </h4>
+    <h4 v-if="title == ''"> {{taxonomy}} </h4>
+    <h4 v-else> {{title}} </h4>
     
     <ul class="terms">
         <li v-for="term in entity.terms[taxonomy]"> 
