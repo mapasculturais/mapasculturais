08f974990767d2eca63f7b5d3a0ac8cdf0aeaaf6
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-06 18:32:00 -0300

    Criação do componente <share-links> - #1987

---
+++ src/protected/application/lib/modules/Components/components/share-links/README.md
@@ -0,0 +1,19 @@
+# Componente `<entity-terms>`
+Mostra os termos da entidade,
+  
+## Propriedades
+- **title**: *String* (opcional) - Título do componente;
+- **text**: *String* (opcional) - Texto de compartilhamento;
+
+### Importando componente
+```PHP
+<?php 
+$this->import('share-links');
+?>
+```
+### Exemplos de uso
+```PHP
+<!-- utilizaçao básica para listagem dos links de compartilhamento -->
+<share-links title="Compartilhar" text="Veja este link:"></share-links>
+
+```
---
+++ src/protected/application/lib/modules/Components/components/share-links/script.js
@@ -0,0 +1,43 @@
+app.component('share-links', {
+    template: $TEMPLATES['share-links'],
+    emits: [],
+
+    setup(props, { slots }) {
+        const hasSlot = name => !!slots[name]
+        return { hasSlot }
+    },
+
+    props: {
+        title: {
+            type: String,
+            default: 'Compartilhar'
+        },
+        text: {
+            type: String,
+            default: ''
+        }
+    },
+
+    methods: {
+        click( action ) {
+            switch(action){
+                case 'twitter':
+                    window.open('https://twitter.com/intent/tweet?text=' + this.text + ' - ' + encodeURIComponent(document.URL)); 
+                    break;
+                case 'facebook':
+                    window.open('http://www.facebook.com/share.php?href=' + encodeURIComponent(document.URL));
+                    break;
+                case 'whatsapp':
+                    window.open('https://api.whatsapp.com/send?text=' + this.text + ' - ' + encodeURIComponent(document.URL));
+                    break;
+                case 'whatsapp-mobile':
+                    window.open('whatsapp://send?text=' + this.text + ' - ' + encodeURIComponent(document.URL));
+                    break;
+                case 'telegram':
+                    window.open('https://telegram.me/share/url?url=' + this.text + ' - ' + encodeURIComponent(document.URL));
+                    break;
+            }
+            return false;
+        }
+    },
+});
