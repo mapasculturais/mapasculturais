4891e9642fc9b3f95046c6dec052b5157cebf3e5
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-05-07 00:03:19 -0300

    melhorias no componente <field> possibilitando a definição do label e do tipo do campo

--- src/protected/application/lib/modules/Components/components/field/template.php
+++ src/protected/application/lib/modules/Components/components/field/template.php
@@ -1,19 +1,19 @@
-<label v-if="showLabel" :for="propId">{{description.label}}</label>
+<label v-if="showLabel" :for="propId">{{label || description.label}}</label>
 <small v-if="hasErrors()">
     {{getErrors().join('; ')}}
 </small>
-<template v-if="!hasSlot('input')">
+<slot name="input">
     <?php //@todo implementar registro de tipos de campos (#1895) ?>
     <input v-if="is('string')" v-model="value" :id="propId" :name="prop" type="text" @change="change()">
 
     <textarea v-if="is('text')" v-model="value" :id="propId" :name="prop" @change="change()"></textarea>
 
-    <input v-if="is('date') || is('number')" v-model="value" :id="propId" :name="prop" :type="type" :min="description.min" :max="description.max" :step="description.step" @change="change()">
+    <input v-if="is('date') || is('number')" v-model="value" :id="propId" :name="prop" :type="fieldType" :min="description.min" :max="description.max" :step="description.step" @change="change()">
 
-    <input v-if="is('email') || is('url')" v-model="value" :id="propId" :name="prop" :type="type" @change="change()">
+    <input v-if="is('email') || is('url')" v-model="value" :id="propId" :name="prop" :type="fieldType" @change="change()">
     
     <select v-if="is('select')" v-model="value" :id="propId" :name="prop" @change="change()">
-        <option v-for="optionValue in description.optionsOrder" :value="optionValue">{{description.options[value]}}</option>
+        <option v-for="optionValue in description.optionsOrder" :value="optionValue">{{description.options[optionValue]}}</option>
     </select>
     
     <template v-if="is('radio')">
@@ -27,5 +27,4 @@
             <input v-model="value" type="checkbox" :value="optionValue" @change="change()"> {{description.options[optionValue]}} 
         </label>
     </template>
-</template>
-<slot v-if="hasSlot('input')" name="input"></slot>
+</slot>
