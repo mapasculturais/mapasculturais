366511c33b7c6f28fe0aefb5c9317b0c991b3b1b
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-05-06 23:45:08 -0300

    só inicializa o módulo Panel se o tema ativo for o BaseV2

--- src/protected/application/lib/modules/Panel/Module.php
+++ src/protected/application/lib/modules/Panel/Module.php
@@ -6,6 +6,14 @@ use MapasCulturais\App;
 
 class Module extends \MapasCulturais\Module
 {
+    function __construct(array $config = [])
+    {
+        $app = App::i();
+        if ($app->view instanceof \MapasCulturais\Themes\BaseV2\Theme) {
+            parent::__construct($config);
+        }
+    }
+
     function _init()
     {
     }
