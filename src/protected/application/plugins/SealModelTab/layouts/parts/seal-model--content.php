f054932e52925639624df7041b59510af564e7f7
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-05-10 16:39:23 -0300

    atualiza nome do hook baseado no plugin

--- src/protected/application/plugins/SealModelTab/layouts/parts/seal-model--content.php
+++ src/protected/application/plugins/SealModelTab/layouts/parts/seal-model--content.php
@@ -3,8 +3,8 @@
         <p><span class="label">Modelos disponíveis: </span>
             <span class="js-editable" id="seal_model" data-edit="seal_model" data-original-title="Modelos disponíveis"><?php echo $entity->seal_model; ?></span>
         </p>
-        <?php $this->applyTemplateHook('seal-layout','begin'); ?>
-        <?php $this->applyTemplateHook('seal-layout','end'); ?>
+        <?php $this->applyTemplateHook('selo-layout','begin'); ?>
+        <?php $this->applyTemplateHook('selo-layout','end'); ?>
         <p>Preview:</p>
         <p class="seal-model-preview">
             <img src="">
