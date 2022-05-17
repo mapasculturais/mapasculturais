9d5b30ef55f6e1ae5e93e5ca10f2f3688b0129d1
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-05-17 20:32:26 -0300

    adiciona timestamp no log do execute-job

--- src/protected/tools/execute-job.php
+++ src/protected/tools/execute-job.php
@@ -5,7 +5,7 @@ ini_set('memory_limit', '2048M');
 require __DIR__ . '/../application/bootstrap.php';
 
 if($app->config['app.log.jobs']){
-    $app->log->debug('EXECUTE JOB');
+    $app->log->debug('EXECUTE JOB ' . date('Y-m-d H:i:s'));
 }
 
 $app = MapasCulturais\App::i();
