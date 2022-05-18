011aaae92d1fbfa4dcc0c50443876416dcb94ec1
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-18 18:19:05 -0300

    Redireciona links para uma nova aba - componente <entity-links> - #2017

--- src/protected/application/lib/modules/Components/components/entity-links/template.php
+++ src/protected/application/lib/modules/Components/components/entity-links/template.php
@@ -1,8 +1,8 @@
-<div  class="entity-links">
+<div class="entity-links">
     <h2> {{title}} </h2>
 
     <div class="links">
-        <a v-for="link in entity.metalists.links" :href="link.value" >
+        <a v-for="link in entity.metalists.links" :href="link.value" target="_blank" >
             <iconify icon="eva:link-outline" /> {{link.title}}
         </a>
     </div>
