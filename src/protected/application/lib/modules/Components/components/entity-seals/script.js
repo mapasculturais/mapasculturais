4fa3d4af54d4a84954f29757c7e52bd1c3c5c083
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-19 11:06:57 -0300

    Criação do componente <entity-seals> - #2018

---
+++ src/protected/application/lib/modules/Components/components/entity-seals/script.js
@@ -0,0 +1,20 @@
+app.component('entity-seals', {
+    template: $TEMPLATES['entity-seals'],
+    emits: [],
+
+    setup(props, { slots }) {
+        const hasSlot = name => !!slots[name]
+        return { hasSlot }
+    },
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+        title: {
+            type: String,
+            default: 'Verificações'
+        }
+    },
+});
