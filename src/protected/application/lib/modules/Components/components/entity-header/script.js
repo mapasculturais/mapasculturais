f0d06867fdeb8c6b5836c54dc2abca4b3a85e401
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-19 13:48:32 -0300

    Renomeia componente <entity-header>

--- src/protected/application/lib/modules/Components/components/entity-header/script.js
+++ src/protected/application/lib/modules/Components/components/entity-header/script.js
@@ -1,5 +1,5 @@
-app.component('panel--entity-header', {
-    template: $TEMPLATES['panel--entity-header'],
+app.component('entity-header', {
+    template: $TEMPLATES['entity-header'],
     methods: {
         url (source) {
             return `url(${source})`
