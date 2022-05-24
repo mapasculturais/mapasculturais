d120cd56b88954e71ba485b4cc9f1d1f7bfce2de
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-24 08:05:19 -0300

    Criação e Estilização do componente <entity-header> - #2020

--- src/protected/application/lib/modules/Components/components/entity-header/template.php
+++ src/protected/application/lib/modules/Components/components/entity-header/template.php
@@ -1,64 +1,111 @@
 <?php
 use MapasCulturais\i;
 ?>
-<header class="entity-header" :class="{ 'entity-header--no-image': false }">
-    <div class="entity-header__cover" :style="{ '--url': url('') }">
+
+{{entity}}
+
+<header class="entity-header" :class="{ 'entity-header--no-image': !entity.files.header }">
+
+    <div class="entity-header__cover" :style="{ '--url': url(entity.files.header?.url) }">
         <nav class="entity-header__breadcrumbs" aria-label="<?= i::__('Breadcrumbs') ?>">
             <slot name="breadcrumbs">
                 <ul>
                     <li>
-                        <a href="#">Oportunidades</a>
+                        <a href="#"> {{entity.__objectType}} </a>
                     </li>
                     <li aria-current="page">
-                        Título da oportunidade
+                        {{entity.name}}
                     </li>
                 </ul>
             </slot>
         </nav>
+    
+        <iconify v-if="!entity.files.header?.url" icon="fa:user" />
+
     </div>
-    <div class="entity-header__main">
-        <img class="entity-header__avatar" src="">
-        <div class="entity-header__title-row">
-            <h1 class="entity-header__title">Título da oportunidade</h1>
-            <div class="entity-header__terms">
-                <slot name="terms">
-                    <dl>
-                        <dt><?= i::__('Tipo') ?></dt>
-                        <dd>Edital</dd>
-                    </dl>
-                    <dl>
-                        <dt><?= i::__('Entidade') ?></dt>
-                        <dd>A descoberta da abayomi pela Comunidade Jesus Cristo Libertador, no Telégrafo</dd>
-                    </dl>
-                </slot>
+
+    <div class="entity-header__content">
+
+        <div class="entity-header__content-leftSide">
+            <div class="avatar">
+                <img v-if="entity.files.avatar" :src="entity.files.avatar?.transformations?.avatarBig?.url">
+
+                <iconify v-else icon="fa:user" />
+
+                <!-- <iconify icon="ri:file-list-2-line" />              Projetos        -->
+                <!-- <iconify icon="clarity:building-line" />            Espaços         -->
+                <!-- <iconify icon="ant-design:calendar-twotone" />      Eventos         -->
+                <!-- <iconify icon="fa-solid:user-friends" />            Agentes         -->
+                <!-- <iconify icon="fa:user" />                          Agente          -->
+                <!-- <iconify icon="icons8:idea" />                      Oportunidades   -->                
+                
             </div>
-        </div>
-        <div class="entity-header__description-row">
-            <nav class="entity-header__share" aria-label="<?= i::__('Compartilhar') ?>">
-                <a class="button button--text button--icon" aria-label="Twitter">
+
+            <nav class="share" aria-label="<?= i::__('Compartilhar') ?>">
+                <a v-if="entity.twitter" :href="entity.twitter" class="button button--text button--icon" aria-label="Twitter" target="_blank">
                     <iconify icon="fa6-brands:twitter"></iconify>
                 </a>
-                <a class="button button--text button--icon" aria-label="Facebook">
-                    <iconify icon="fa6-brands:facebook"></iconify>
+                <a v-if="entity.facebook" :href="entity.facebook" class="button button--text button--icon" aria-label="Facebook" target="_blank">
+                    <iconify icon="la:facebook-f"></iconify>
                 </a>
-                <a class="button button--text button--icon" aria-label="Instagram">
+                <a v-if="entity.instagram" :href="entity.instagram" class="button button--text button--icon" aria-label="Instagram" target="_blank">
                     <iconify icon="fa6-brands:instagram"></iconify>
                 </a>
-                <a class="button button--text button--icon" aria-label="Telegram">
-                    <iconify icon="fa6-brands:telegram"></iconify>
+                <a v-if="entity.telegram" :href="entity.telegram" class="button button--text button--icon" aria-label="Telegram" target="_blank">
+                    <iconify icon="bxl:telegram"></iconify>
                 </a>
-                <a class="button button--text button--icon" aria-label="Pinterest">
+                <a v-if="entity.pinterest" :href="entity.pinterest" class="button button--text button--icon" aria-label="Pinterest" target="_blank">
                     <iconify icon="fa6-brands:pinterest-p"></iconify>
                 </a>
-                <a class="button button--text button--icon" aria-label="WhatsApp">
+                <a v-if="entity.whatsapp" :href="entity.whatsapp" class="button button--text button--icon" aria-label="WhatsApp" target="_blank">
                     <iconify icon="fa6-brands:whatsapp"></iconify>
                 </a>
             </nav>
-            <div class="entity-header__description">
+        </div>
+
+        <div class="entity-header__content-rightSide">
+
+            <div class="data">
+                <h1 class="title"> {{entity.name}} </h1>
+                <div class="metadata">
+                    <slot name="metadata">
+                        <dl>
+                            <dt><?= i::__('Tipo') ?></dt>
+                            <dd class="type"> {{entity.type.name}} </dd>
+                        </dl>
+                    </slot>
+                </div>
+            </div>
+
+            <nav class="share share-mobile" aria-label="<?= i::__('Compartilhar') ?>">
+                <a v-if="entity.twitter" :href="entity.twitter" class="button button--text button--icon" aria-label="Twitter" target="_blank">
+                    <iconify icon="fa6-brands:twitter"></iconify>
+                </a>
+                <a v-if="entity.facebook" :href="entity.facebook" class="button button--text button--icon" aria-label="Facebook" target="_blank">
+                    <iconify icon="la:facebook-f"></iconify>
+                </a>
+                <a v-if="entity.instagram" :href="entity.instagram" class="button button--text button--icon" aria-label="Instagram" target="_blank">
+                    <iconify icon="fa6-brands:instagram"></iconify>
+                </a>
+                <a v-if="entity.telegram" :href="entity.telegram" class="button button--text button--icon" aria-label="Telegram" target="_blank">
+                    <iconify icon="bxl:telegram"></iconify>
+                </a>
+                <a v-if="entity.pinterest" :href="entity.pinterest" class="button button--text button--icon" aria-label="Pinterest" target="_blank">
+                    <iconify icon="fa6-brands:pinterest-p"></iconify>
+                </a>
+                <a v-if="entity.whatsapp" :href="entity.whatsapp" class="button button--text button--icon" aria-label="WhatsApp" target="_blank">
+                    <iconify icon="fa6-brands:whatsapp"></iconify>
+                </a>
+            </nav>
+
+            <div class="description">
                 <slot name="description">
-                    <p>Ac massa tempus mattis dictum. Eu molestie morbi a mattis pretium et lectus egestas euismod. Cras at quis tincidunt vel feugiat enim, felis ut amet. Nibh sit nulla eget purus quam porta non. Erat condimentum sapien amet suspendisse diam, nunc massa consectetur. Morbi sed ac massa elementum. Rhoncus viverra lorem interdum eu quis facilisis tempus. Auctor laoreet varius eu pretium congue. É isso aí.</p>
+                    <p> {{entity.shortDescription}} </p>
                 </slot>
             </div>
+
         </div>
+
     </div>
+
 </header>
