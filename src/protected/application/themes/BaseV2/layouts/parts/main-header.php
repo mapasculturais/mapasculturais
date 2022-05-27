29d76614ae40a29b461e009f4f6cbd1015b49b32
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-05-27 11:49:25 -0300

    Criação do header - WIP - #2024

---
+++ src/protected/application/themes/BaseV2/layouts/parts/main-header.php
@@ -0,0 +1,100 @@
+<header class="main-header">
+
+    <div class="main-header__logo">
+        <h1>Mapa Cultural</h1>
+    </div>     
+
+    <ul class="main-header__menu">
+        <a href="" class="main-header__menu--item home">
+            <li> 
+                <span class="icon"> <iconify icon="fluent:home-12-regular" /> </span>
+                <p class="label"> Home </p>      
+            </li>
+        </a>  
+        <a href="" class="main-header__menu--item opportunities">
+            <li>
+                <span class="icon"> <iconify icon="icons8:idea" /> </span>
+                <p class="label"> Oportunidades </p>
+            </li>
+        </a>
+        <a href="" class="main-header__menu--item agents">
+            <li>
+                <span class="icon"> <iconify icon="fa-solid:user-friends" /> </span>
+                <p class="label"> Agentes </p>
+            </li>
+        </a>
+        <a href="" class="main-header__menu--item events">
+            <li> 
+                <span class="icon"> <iconify icon="ant-design:calendar-twotone" /> </span>
+                <p class="label"> Eventos </p>
+            </li>
+        </a>  
+        <a href="" class="main-header__menu--item spaces">
+            <li> 
+                <span class="icon"> <iconify icon="clarity:building-line" /> </span>
+                <p class="label"> Espaços </p>       
+            </li>
+        </a> 
+        <a href="" class="main-header__menu--item projects">
+            <li> 
+                <span class="icon"> <iconify icon="ri:file-list-2-line" /> </span>
+                <p class="label"> Projetos </p>      
+            </li>
+        </a> 
+    </ul>
+
+    <!-- <iconify icon="ri:file-list-2-line" />              Projetos        -->
+    <!-- <iconify icon="clarity:building-line" />            Espaços         -->
+    <!-- <iconify icon="ant-design:calendar-twotone" />      Eventos         -->
+    <!-- <iconify icon="fa-solid:user-friends" />            Agentes         -->
+    <!-- <iconify icon="fa:user" />                          Agente          -->
+    <!-- <iconify icon="icons8:idea" />                      Oportunidades   -->  
+
+    <!-- <ul class="main-header__menu">
+
+        <li>
+            <a>  </a>
+        </li>
+        
+        <a class="main-header__items--item">  </a>
+        <a class="main-header__items--item">  </a>
+        <a class="main-header__items--item">  </a>
+        <a class="main-header__items--item">  </a>
+        <a class="main-header__items--item">  </a>
+    </ul> -->
+
+    <div class="main-header__options">
+        
+        <div class="main-header__options--loggedIn">
+
+            <div class="select-wrapper">
+                <select>
+                    <option> Painel de controle </option>
+                    <option> Editais e Oportunidades </option>
+                    <option> Meus eventos </option>
+                    <option> Meus agentes </option>
+                    <option> Meus espaços </option>
+                </select>
+            </div>
+
+            <!-- <ul>
+                <li> Painel de controle
+                    <ul>
+                        <li> <a href=""> Painel de controle         </a> </li>
+                        <li> <a href=""> Editais e Oportunidades    </a> </li>
+                        <li> <a href=""> Meus eventos               </a> </li>
+                        <li> <a href=""> Meus agentes               </a> </li>
+                        <li> <a href=""> Meus espaços               </a> </li>
+                    </ul>
+                </li>
+            </ul> -->
+
+        </div>
+
+        <div class="main-header__options--loggedOff">
+            
+        </div>
+
+    </div>
+
+</header>
