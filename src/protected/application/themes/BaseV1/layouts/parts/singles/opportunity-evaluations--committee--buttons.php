583a5de6a3c7034385f8127eae8eeab1af99eb1c
erleibiazzio <erleibiazzio@gmail.com>
2022-05-31 17:36:05 -0300

    Melhora texto do toltip do botão enviar avaliações Ref.:#2012

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations--committee--buttons.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations--committee--buttons.php
@@ -1,7 +1,10 @@
+<?php
+$date = $entity->registrationTo;
+?>
 <div id="evaluation-committee-buttons" class="clearfix" style="margin-bottom: 20px;">
     <?php if($entity->canUser('sendUserEvaluations')): ?>
         <a href="<?php echo $entity->sendEvaluationsUrl ?>" class="btn btn-primary alignright"><?php \MapasCulturais\i::_e('Enviar Avaliações'); ?></a>
     <?php elseif($entity->canUser('evaluateRegistrations')): ?>
-        <a class="btn btn-primary disabled hltip alignright" title="<?php \MapasCulturais\i::esc_attr_e('É necessário avaliar todas as inscrições antes de enviar') ?>"><?php \MapasCulturais\i::_e('Enviar Avaliações'); ?></a>
+        <a class="btn btn-primary disabled hltip alignright" title="<?php \MapasCulturais\i::esc_attr_e('É necessário avaliar todas as inscrições e aguardar o prazo de encerramento da fase em '.$date->format("d/m/Y")) ?>"><?php \MapasCulturais\i::_e('Enviar Avaliações'); ?></a>
     <?php endif; ?>
 </div>
