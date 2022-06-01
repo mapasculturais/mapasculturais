e9118e6da7dec30798067594d2f6eb8beffbbc24
robedj <ed.wassouf@gmail.com>
2022-06-01 12:45:29 -0400

    Adicao da propriedade editable no componente entity-seals  Ref.:#2030

--- src/protected/application/lib/modules/Components/components/entity-seals/template.php
+++ src/protected/application/lib/modules/Components/components/entity-seals/template.php
@@ -6,6 +6,11 @@
             <img v-if="seal.files?.avatar" :src="seal.files.avatar?.transformations?.avatarSmall?.url">
             <span class="icon" v-if="editable"><iconify icon="codicon:chrome-close"/></span>
         </div>
+        <div class="addSeal " v-if="editable">
+            <span class="icon"><iconify icon="ant-design:plus-outlined"/></span>
+
+        </div>
+
     </div>
    
 </div>
