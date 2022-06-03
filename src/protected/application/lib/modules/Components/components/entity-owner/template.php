f021a8a4f62eefb92b6a1db093b21ae51a4a0fdd
robedj <ed.wassouf@gmail.com>
2022-06-03 14:35:28 -0400

    Adição da propriedade editable no entity-owner Ref.:#2038

--- src/protected/application/lib/modules/Components/components/entity-owner/template.php
+++ src/protected/application/lib/modules/Components/components/entity-owner/template.php
@@ -1,17 +1,17 @@
 <div v-if="owner" class="entity-owner">
     <h4>{{title}}</h4>
     <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
-            <div class="entity-owner__owner--img">
-                <img v-if="owner.files.avatar.url != ''" class="profile" :src="owner.files.avatar.url">
-                <div v-else class="placeholder">
-                    <iconify icon="bi:image-fill" />
-                </div>
+        <div class="entity-owner__owner--img">
+            <img v-if="owner.files.avatar.url != ''" class="profile" :src="owner.files.avatar.url">
+            <div v-else class="placeholder">
+                <iconify icon="bi:image-fill" />
             </div>
-            <div class="entity-owner__owner--name">
-                {{owner.name}}
-            </div>
+        </div>
+        <div class="entity-owner__owner--name">
+            {{owner.name}}
+        </div>
     </a>
-    <div class="entity-owner__edit">
+    <div v-if="editable" class="entity-owner__edit" >
         <a class="entity-owner__edit--btn">
             <iconify icon="material-symbols:change-circle-outline"/><h4> Alterar Propriedade</h4>
         </a>
