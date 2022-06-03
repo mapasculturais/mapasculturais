9c2fca5d2ad9c9626a6aba7ef63553687c5e27d0
robedj <ed.wassouf@gmail.com>
2022-06-03 14:28:57 -0400

    Adição do link altera propriedade do entity-owner Ref.:#2038

--- src/protected/application/lib/modules/Components/components/entity-owner/template.php
+++ src/protected/application/lib/modules/Components/components/entity-owner/template.php
@@ -1,16 +1,19 @@
 <div v-if="owner" class="entity-owner">
     <h4>{{title}}</h4>
-    <a :href="owner.singleUrl" :title="owner.shortDescription">
-        <div class="owner">
-            <div class="owner-img">
-                <img v-if="owner.files.avatar.url != ''" class="owner-img-profile" :src="owner.files.avatar.url">
-                <div v-else class="owner-img-placeholder">
+    <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
+            <div class="entity-owner__owner--img">
+                <img v-if="owner.files.avatar.url != ''" class="profile" :src="owner.files.avatar.url">
+                <div v-else class="placeholder">
                     <iconify icon="bi:image-fill" />
                 </div>
             </div>
-            <div class="owner-name">
+            <div class="entity-owner__owner--name">
                 {{owner.name}}
             </div>
-        </div>
     </a>
+    <div class="entity-owner__edit">
+        <a class="entity-owner__edit--btn">
+            <iconify icon="material-symbols:change-circle-outline"/><h4> Alterar Propriedade</h4>
+        </a>
+    </div>
 </div>
