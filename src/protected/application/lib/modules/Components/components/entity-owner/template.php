12ee750657b1ebb6c32e0d69effd8472e019a1d9
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-07 14:37:15 -0300

    Ajustes no componente <entity-owner> para uso do componente <select-entity> no modo de edição - #2037

--- src/protected/application/lib/modules/Components/components/entity-owner/template.php
+++ src/protected/application/lib/modules/Components/components/entity-owner/template.php
@@ -1,8 +1,11 @@
+<?php
+$this->import('select-entity');
+?>
 <div v-if="owner" class="entity-owner">
     <h4>{{title}}</h4>
     <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
         <div class="entity-owner__owner--img">
-            <img v-if="owner.files.avatar.url != ''" class="profile" :src="owner.files.avatar.url">
+            <img v-if="owner.files" class="profile" :src="owner.files?.avatar?.url">
             <div v-else class="placeholder">
                 <iconify icon="bi:image-fill" />
             </div>
@@ -11,9 +14,17 @@
             {{owner.name}}
         </div>
     </a>
-    <div v-if="editable" class="entity-owner__edit" >
-        <a class="entity-owner__edit--btn">
-            <iconify icon="material-symbols:change-circle-outline"/><h4> Alterar Propriedade</h4>
-        </a>
+
+    <div v-if="editable" class="entity-owner__edit">
+        
+        <select-entity type="agent" @select="changeOwner($event)">            
+            <template #btn="{ toggle }">
+                <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
+                    <iconify icon="material-symbols:change-circle-outline"/> <h4> Alterar Propriedade</h4>    
+                </a>
+            </template>
+        </select-entity>
+
     </div>
+
 </div>
