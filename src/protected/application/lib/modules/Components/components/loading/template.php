b13c5e725019ae165d887cbc30f2b430a031b07b
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-07 14:37:52 -0300

    Adiciona classe para estilização do componente <loading>

--- src/protected/application/lib/modules/Components/components/loading/template.php
+++ src/protected/application/lib/modules/Components/components/loading/template.php
@@ -1,6 +1,6 @@
 <?php
 use MapasCulturais\i;
 ?>
-<span v-if="condition || entity?.__processing">
+<span v-if="condition || entity?.__processing" class="loading">
     <img src="<?php $this->asset('img/spinner.gif') ?>"> {{entity?.__processing || '<?php i::_e('carregando...') ?>'}}
 </span>
