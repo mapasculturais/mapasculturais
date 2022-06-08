b79ef794620f34c606862cd5cfec974d2387c1bb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-08 03:29:33 -0300

    refatoração do componente <entity-terms> - #1986

--- src/protected/application/lib/modules/Components/components/entity-terms/template.php
+++ src/protected/application/lib/modules/Components/components/entity-terms/template.php
@@ -1,19 +1,20 @@
 <div v-if="entity.terms?.[taxonomy]" class="entity-terms">
 
-    <h4 v-if="title == ''"> {{taxonomy}} </h4>
-    <h4 v-else> {{title}} </h4>
+    <h4 class="entity-terms__title" v-if="title == ''"> {{taxonomy}} </h4>
+    <h4 class="entity-terms__title" v-else> {{title}} </h4>
     
     <ul class="entity-terms__terms">
-        <li class="entity-terms__terms--term" v-for="term in entity.terms[taxonomy]"> 
+
+        <li class="button--solid entity-terms__terms--term" v-for="term in entity.terms[taxonomy]"> 
             <a> {{term}} </a> 
-            
-            <a v-if="editable"><iconify icon="codicon:chrome-close"/></a>
-             
+            <a v-if="editable"><iconify icon="gg:close"/></a>             
         </li>
-        <li class="entity-terms__terms--addNew" v-if="editable">
+
+        <li class="button--primary  entity-terms__terms--addNew" v-if="editable">
             Adicionar nova
-            <span><iconify icon="fluent:add-12-regular"/></span>
+            <span><iconify icon="fluent:add-20-filled"/></span>
         </li>
+
     </ul>
 
 </div>
