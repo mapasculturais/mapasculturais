22f51dc6e1d872eabf00ec34ab344e27916af732
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-08 03:31:13 -0300

    refatoração do componente <entity-seals> - #2018

--- src/protected/application/lib/modules/Components/components/entity-seals/template.php
+++ src/protected/application/lib/modules/Components/components/entity-seals/template.php
@@ -2,15 +2,16 @@
     <h4 class="entity-seals__title"> {{title}} </h4>
 
     <div class="entity-seals__seals">
+        
         <div class="entity-seals__seals--seal" v-for="seal in entity.seals">
             <div v-if="seal.files?.avatar" class="image">
                 <img :src="seal.files.avatar?.transformations?.avatarSmall?.url">
             </div>
             <span class="icon" v-if="editable"><iconify icon="gg:close"/></span>
         </div>
-        <div class="addSeal " v-if="editable">
+        
+        <div class="entity-seals__seals--addSeal" v-if="editable">
             <span class="icon"><iconify icon="fluent:add-20-filled"/></span>
-
         </div>
 
     </div>
