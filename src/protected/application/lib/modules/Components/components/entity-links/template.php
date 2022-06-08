273db9141cb6b78efc2c0749fe61a3fe19f497b1
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-08 03:19:30 -0300

    refatoração do componente <entity-links> - #2017

--- src/protected/application/lib/modules/Components/components/entity-links/template.php
+++ src/protected/application/lib/modules/Components/components/entity-links/template.php
@@ -1,26 +1,26 @@
-<?php
+<?php 
 use MapasCulturais\i;
-
 ?>
 <div class="entity-links">
     <h4 class="entity-links__title"> {{title}} </h4>
 
     <ul class="entity-links__links">
+
         <li class="entity-links__links--item" v-for="link in entity.metalists.links">
             <a class="link" :href="link.value" target="_blank" >
-                <iconify icon="eva:link-outline" /> {{link.title}}
-            </a>
+                <iconify icon="eva:link-outline" /> 
+                {{link.title}}
+            </a>            
             <div class="edit">
-                <a> <iconify icon="zondicons:edit-pencil"/></a>
-                <a><iconify icon="ooui:trash"/></a>
+                <a> <iconify icon="zondicons:edit-pencil" /> </a>
+                <a> <iconify icon="ooui:trash" /> </a>
             </div>
         </li>
-        <li class="entity-links__links--addNew">
-            <?php i::_e("Adicionar novo")?>
-            <span><iconify icon="fluent:add-12-regular"></iconify></span>
+
+        <li class="button--primary entity-links__links--addNew">
+            <?php i::_e("Adicionar novo link")?>
+            <span> <iconify icon="fluent:add-12-regular" /> </span>
         </li>
 
     </ul>
 </div>
-
-<!-- :href="file.url" -->
