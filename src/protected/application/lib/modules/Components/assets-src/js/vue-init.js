6663ca61ca1293934e39624250160d647457e057
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-09 16:00:05 -0300

    Adição da dependência Vue Advanced Cropper - #2035

--- src/protected/application/lib/modules/Components/assets-src/js/vue-init.js
+++ src/protected/application/lib/modules/Components/assets-src/js/vue-init.js
@@ -2,6 +2,7 @@ import { Icon } from '@iconify/vue'
 import * as Pinia from 'pinia'
 import * as Vue from 'vue'
 import VueFinalModal from 'vue-final-modal'
+import * as VueAdvancedCropper from "vue-advanced-cropper";
 
 const app = Vue.createApp({})
 const pinia = Pinia.createPinia()
@@ -9,6 +10,7 @@ const pinia = Pinia.createPinia()
 app.use(pinia)
 app.use(VueFinalModal)
 app.component('Iconify', Icon)
+app.component('Cropper', VueAdvancedCropper)
 
 window.$MAPAS = typeof Mapas !== 'undefined' ? Mapas : MapasCulturais
 window.$DESCRIPTIONS = $MAPAS?.EntitiesDescription ?? []
@@ -18,6 +20,7 @@ window.app = app
 window.Pinia = Pinia
 window.pinia = pinia
 window.Vue = Vue
+window.VueAdvancedCropper = VueAdvancedCropper
 
 document.addEventListener('DOMContentLoaded', () => {
     app.mount('#main-app')
