d0219eb11612cd0d93409f540e2bee87f59fe4c1
robedj <ed.wassouf@gmail.com>
2022-06-15 12:55:06 -0400

    W.I.P Criação do header na  tela de edição do Agente na Base V2 Ref.:2043

--- src/protected/application/lib/modules/Entities/components/entity-header/script.js
+++ src/protected/application/lib/modules/Entities/components/entity-header/script.js
@@ -1,9 +1,37 @@
 app.component('entity-header', {
     template: $TEMPLATES['entity-header'],
+    data() {
+        return {
+            icon: ''
+        }
+    },
     props: {
+        editable: {
+            type: Boolean,
+            default: false
+        },
         entity: {
             type: Entity,
             required: true
+        }
+    },
+    created() {
+        switch(this.entity.__objectType) {
+            case 'agent': 
+                this.icon = "fa-solid:user-friends";
+                break;
+            case 'project':
+                this.icon = "ri:file-list-2-line";
+                break;
+            case 'space':
+                this.icon = "clarity:building-line";
+                break;
+            case 'opportunity':
+                this.icon = "icons8:idea";
+                break;
+            case 'event':
+                this.icon = "ant-design:calendar-twotone";
+                break;
         }
     },
     methods: {
