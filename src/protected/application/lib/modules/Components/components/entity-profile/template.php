4a15616368c68f697d4525423a185c0e735042ca
robedj <ed.wassouf@gmail.com>
2022-06-20 11:28:05 -0400

    Criação do componente placeHolder entity-profile Ref.:2043

---
+++ src/protected/application/lib/modules/Components/components/entity-profile/template.php
@@ -0,0 +1,11 @@
+<?php
+use MapasCulturais\i;
+?>
+
+<div class="profileImg">
+    <div class="profileImg__img">
+        <iconify icon="bi:image-fill" />
+        <!-- <img href="" class="select-profileImg__img--img" /> -->
+    </div>
+    <label> <?php i::_e("Selecionar imagem de perfil"); ?> </label>
+</div>
