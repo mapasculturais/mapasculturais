ee23df1cdac87e9f347eda21f591e41509505f39
robedj <ed.wassouf@gmail.com>
2022-06-20 09:57:24 -0400

    W.I.P Criação do card do Agente na tela single-1  Base V2 Ref.:#2043

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -1,24 +1,30 @@
 <?php 
 $this->layout = 'entity'; 
-$this->import('entity-terms share-links files-list entity-links entity-owner entity-seals entity-header entity-gallery popover image-uploader');
+$this->import('entity-terms card container share-links files-list entity-links entity-owner entity-seals entity-header entity-gallery popover image-uploader');
 
 ?>
-<entity-header :entity="entity"></entity-header>
+<container class="single-1">
 
+    <entity-header :entity="entity"></entity-header>
+    
+    <main>
+        <entity-links :entity="entity" title="Links"></entity-links>
+        <entity-gallery :entity="entity"></entity-gallery>
+        <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
+        
+    </main>
+    
+    <aside>
+        <entity-terms :entity="entity" taxonomy="area" title="Áreas de interesse" :editable="true"></entity-terms>
+
+        <entity-owner :entity="entity" title="Publicado por"></entity-owner>
+
+        <entity-seals :entity="entity" title="Verificações" :editable="true"></entity-seals>
+
+        <entity-terms :entity="entity" taxonomy="tag" title="Tags" :editable="true"></entity-terms>  
+
+        <share-links title="Compartilhar" text="Veja este link:"></share-links>
+    </aside>
+    
+</container>
 <image-uploader group="avatar" ></image-uploader>
-
-<entity-terms :entity="entity" taxonomy="area" title="Áreas de interesse" :editable="true"></entity-terms>
-
-<entity-owner :entity="entity" title="Publicado por"></entity-owner>
-
-<entity-seals :entity="entity" title="Verificações" :editable="true"></entity-seals>
-
-<entity-terms :entity="entity" taxonomy="tag" title="Tags" :editable="true"></entity-terms>  
-
-<share-links title="Compartilhar" text="Veja este link:"></share-links>
-
-<entity-links :entity="entity" title="Links"></entity-links>
-
-<files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
-
-<entity-gallery :entity="entity"></entity-gallery>
