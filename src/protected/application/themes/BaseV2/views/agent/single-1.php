4cbb50af3945d52f41b7b9f662dcd355e023f1b1
robedj <ed.wassouf@gmail.com>
2022-06-20 11:30:20 -0400

    W.I.P Alteração no arquivo single-1  Base V2 Ref.:#2044

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -10,22 +10,21 @@ $this->import('entity-terms card container share-links files-list entity-links e
     <div class="divider"></div>
     
     <main>
+        <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
         <entity-links :entity="entity" title="Links"></entity-links>
         <entity-gallery :entity="entity"></entity-gallery>
-        <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
         
     </main>
     
     <aside>
         <entity-terms :entity="entity" taxonomy="area" title="Áreas de interesse"></entity-terms>
-
-
+        
+        
         <entity-seals :entity="entity" title="Verificações"></entity-seals>
-
+        
         <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
-
+        
         <entity-owner :entity="entity" title="Publicado por"></entity-owner>
-
 
         <share-links title="Compartilhar" text="Veja este link:"></share-links>
     </aside>
