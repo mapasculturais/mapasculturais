30ca8def379e61d7081e7d843dbd3674535d1f73
robedj <ed.wassouf@gmail.com>
2022-06-21 16:11:04 -0400

    Ajuste no css da single1 na Base V2 Ref.:#2044

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -3,31 +3,29 @@ $this->layout = 'entity';
 $this->import('entity-terms card container share-links files-list entity-links entity-owner entity-seals entity-header entity-gallery popover image-uploader');
 
 ?>
-<container class="single-1">
+<div class="main-app single-1">
 
     <entity-header :entity="entity"></entity-header>
     
-    <div class="divider"></div>
-    
-    <main>
-        <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
-        <entity-links :entity="entity" title="Links"></entity-links>
-        <entity-gallery :entity="entity"></entity-gallery>
+    <container class="single-1__content">
         
-    </main>
-    
-    <aside>
-        <entity-terms :entity="entity" taxonomy="area" title="Áreas de interesse"></entity-terms>
+        <div class="divider"></div>
         
+        <main>
+            <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
+            <entity-links :entity="entity" title="Links"></entity-links>
+            <entity-gallery :entity="entity"></entity-gallery>
+            
+        </main>
         
-        <entity-seals :entity="entity" title="Verificações"></entity-seals>
+        <aside>
+            <entity-terms :entity="entity" taxonomy="area" title="Áreas de interesse"></entity-terms>
+            <entity-seals :entity="entity" title="Verificações"></entity-seals>
+            <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
+            <entity-owner :entity="entity" title="Publicado por"></entity-owner>
+            <share-links title="Compartilhar" text="Veja este link:"></share-links>
+        </aside>
         
-        <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
-        
-        <entity-owner :entity="entity" title="Publicado por"></entity-owner>
-
-        <share-links title="Compartilhar" text="Veja este link:"></share-links>
-    </aside>
-    
-</container>
+    </container>
+</div>
 <image-uploader group="avatar" ></image-uploader>
