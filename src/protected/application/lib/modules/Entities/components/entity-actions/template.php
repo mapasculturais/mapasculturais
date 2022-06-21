a52f797c7b12f77d21dbabbf3613b94ade8f3630
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-06-21 15:42:41 -0300

    Ajuste no componente entity-actions - #2043

--- src/protected/application/lib/modules/Entities/components/entity-actions/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-actions/template.php
@@ -2,28 +2,32 @@
 use MapasCulturais\i;
 ?>
 <div class="entity-actions">
+
+    <div class="entity-actions__content">
     
-    <div class="entity-actions__groupBtn">
+        <div class="entity-actions__content--groupBtn">
 
-        <button class="button entity-actions__groupBtn--btn" @click="entity.delete()">
-            <?php i::_e("Excluir") ?>
-        </button>
+            <button class="button btn" @click="entity.delete()">
+                <?php i::_e("Excluir") ?>
+            </button>
 
-    </div>
+        </div>
 
-    <div class="entity-actions__groupBtn">
+        <div class="entity-actions__content--groupBtn">
 
-        <button class="button entity-actions__groupBtn--btn" @click="entity.archive()">
-            <?php i::_e("Arquivar") ?>
-        </button>
+            <button class="button btn" @click="entity.archive()">
+                <?php i::_e("Arquivar") ?>
+            </button>
 
-        <button class="button entity-actions__groupBtn--btn" @click="entity.save()">
-            <?php i::_e("Salvar") ?>
-        </button>
+            <button class="button btn" @click="entity.save()">
+                <?php i::_e("Salvar") ?>
+            </button>
 
-        <button class="button entity-actions__groupBtn--btn publish" @click="entity.publish()">
-            <?php i::_e("Publicar") ?>
-        </button>
+            <button class="button btn publish" @click="entity.publish()">
+                <?php i::_e("Publicar") ?>
+            </button>
+
+        </div>
 
     </div>
 
