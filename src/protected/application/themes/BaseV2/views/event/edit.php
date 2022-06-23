5c9287afe8298cfd90625d4e833456ca905ee12f
robedj <ed.wassouf@gmail.com>
2022-06-23 13:21:06 -0400

    Criação da tela de edição da entidade Evento na Base V2 Ref.:#2053

---
+++ src/protected/application/themes/BaseV2/views/event/edit.php
@@ -0,0 +1,13 @@
+<?php 
+use MapasCulturais\i;
+$this->layout = 'entity'; 
+$this->import('entity-profile entity-cover entity-terms entity-header popover field entity-header entity-actions tabs main-menu container card entity-owner');
+?>
+<div class="main-app edit-event">
+
+<entity-header :entity="entity" :editable="true"></entity-header>
+
+
+
+
+</div>
---
+++ src/protected/application/themes/BaseV2/views/event/single.php
@@ -0,0 +1,5 @@
+<?php 
+use MapasCulturais\i;
+$this->layout = 'entity'; 
+$this->import('entity-profile entity-cover entity-terms entity-header popover field entity-header entity-actions tabs main-menu container card entity-owner');
+?>
