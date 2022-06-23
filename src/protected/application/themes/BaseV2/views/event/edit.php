05cbe316cb30f7125e57a07f0c14ce63db7e64de
robedj <ed.wassouf@gmail.com>
2022-06-23 14:28:22 -0400

    W.I.P Ajustes no html da tela de edição da entidade Evento na Base V2 Ref.:#2053

--- src/protected/application/themes/BaseV2/views/event/edit.php
+++ src/protected/application/themes/BaseV2/views/event/edit.php
@@ -1,21 +1,22 @@
 <?php 
 use MapasCulturais\i;
 $this->layout = 'entity'; 
-$this->import('entity-profile entity-cover entity-terms entity-header popover field entity-header entity-actions tabs main-menu container card entity-owner');
+$this->import('space-card entity-profile entity-cover entity-terms entity-header popover field entity-header entity-actions tabs main-menu container card entity-owner');
 ?>
 
 <div class="main-app edit-event">
 
     <entity-header :entity="entity" :editable="true"></entity-header>
     <container class="edit-event__container">
-        <card class="card-event">
-        <template #title>
-            <h3 class="card-event__title--title"><?php i::_e("Nome do Evento")?></h3>
-            <p class="card-event__title--description"><?php i::_e("Evento")?></p>
-        </template>
-
-        </card>
-
+        <main class="edit-event__container--main">
+            <space-card>
+                
+                
+            </space-card>
+        </main>
+        <aside class="edit-event__content--aside">
+        <card></card>
+        </aside>
 
     </container>
 
