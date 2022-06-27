8f3fe7b2799d465261d3f35c4ec174a0ca07e49c
robedj <ed.wassouf@gmail.com>
2022-06-27 13:42:09 -0400

    Exibição do endereço na visão da single do Agente individual Ref.:#2043

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -1,17 +1,25 @@
 <?php 
 $this->layout = 'entity'; 
-$this->import('entity-terms card container share-links files-list entity-links entity-owner entity-seals entity-header entity-gallery popover image-uploader entity-social-media');
+$this->import('entity-terms card container share-links files-list entity-links entity-owner entity-seals entity-header entity-gallery popover image-uploader entity-social-media main-menu');
 
 ?>
 <div class="main-app single-1">
 
     <entity-header :entity="entity"></entity-header>
     
+    
     <container class="single-1__content">
         
         <div class="divider"></div>
         
         <main>
+            <div class="row">
+                {{entity.En_Nome_Logradouro}}, {{entity.En_Num}}, {{entity.En_Bairro}}, {{entity.En_CEP}}, {{entity.En_Municipio}}, {{entity.En_Estado}}
+            </div>
+            <div class="row">
+                <h2>Descrição Detalhada</h2>
+                <p>{{entity.longDescription}}</p>
+            </div>
             <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
             <entity-links :entity="entity" title="Links"></entity-links>
             <entity-gallery :entity="entity"></entity-gallery>
@@ -25,6 +33,7 @@ $this->import('entity-terms card container share-links files-list entity-links e
             <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
             <entity-owner :entity="entity" title="Publicado por"></entity-owner>
             <share-links title="Compartilhar" text="Veja este link:"></share-links>
+            
         </aside>
         
     </container>
