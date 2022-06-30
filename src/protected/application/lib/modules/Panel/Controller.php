38d1db99d8c7cf7761303271dfdc2f96ad0b6981
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-06-29 23:33:35 -0300

    cria rotas do painel para agentes, espaços, eventos e projetos

--- src/protected/application/lib/modules/Panel/Controller.php
+++ src/protected/application/lib/modules/Panel/Controller.php
@@ -12,5 +12,25 @@ class Controller extends \MapasCulturais\Controller
     function GET_index() {
         $this->requireAuthentication();
         $this->render('index');
-    }    
+    }
+
+    function GET_agents() {
+        $this->requireAuthentication();
+        $this->render('agents');
+    }
+
+    function GET_spaces() {
+        $this->requireAuthentication();
+        $this->render('spaces');
+    }
+
+    function GET_projects() {
+        $this->requireAuthentication();
+        $this->render('projects');
+    }
+
+    function GET_events() {
+        $this->requireAuthentication();
+        $this->render('events');
+    }
 }
