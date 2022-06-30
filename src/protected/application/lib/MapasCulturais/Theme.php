5ab6cb4745fb7072b5c32ac1353fa5b2ab58d72c
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-06-29 21:41:04 -0300

    adiciona o agentRelations à query do Theme::addRequestedEntityToJs (refs #1999)

--- src/protected/application/lib/MapasCulturais/Theme.php
+++ src/protected/application/lib/MapasCulturais/Theme.php
@@ -681,10 +681,10 @@ abstract class Theme extends \Slim\View {
         
         if ($entity_class_name && $entity_id) {
             $app = App::i();
-            $query = new ApiQuery($entity_class_name, ['@select' => '*', 'id' => "EQ({$entity_id})"]);
+            $query = new ApiQuery($entity_class_name, ['@select' => '*,agentRelations', 'id' => "EQ({$entity_id})"]);
             $owner_prop = ($entity_class_name == Entities\Agent::class) ? 'parent' : 'owner';
             $e = $query->findOne();
-
+            
             if ($owner_id = $e[$owner_prop]) {
                 $query = new ApiQuery(Entities\Agent::class, ['@select' => 'name, terms, files.avatar, singleUrl, shortDescription', 'id' => "EQ({$owner_id})"]);
                 $owner = $query->findOne();
