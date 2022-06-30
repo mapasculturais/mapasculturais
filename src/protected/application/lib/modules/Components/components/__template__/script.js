43b1be0d8a36572f09ca8dd89f34322f5f15454b
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-06-30 20:45:11 -0300

    nova versão do template de componente (refs: #1880)

--- src/protected/application/lib/modules/Components/components/__template__/script.js
+++ src/protected/application/lib/modules/Components/components/__template__/script.js
@@ -1,32 +1,89 @@
-app.component('__template__', {
+/**
+ * Vue Lifecycle
+ * 1. setup
+ * 2. beforeCreate
+ * 3. created
+ * 4. beforeMount
+ * 5. mounted
+ * 
+ * // sempre que há modificação nos dados
+ *  - beforeUpdate
+ *  - updated
+ * 
+ * 6. beforeUnmount
+ * 7. unmounted                  
+ */
+ app.component('__template__', {
     template: $TEMPLATES['__template__'],
-    emits: [],
+    
+    // define os eventos que este componente emite
+    emits: ['namesDefined'],
 
-    setup(props, { slots }) {
+    setup(props, { attrs, slots, emit, expose }) {
         const hasSlot = name => !!slots[name]
         return { hasSlot }
     },
 
-    created() {
+    beforeCreate() { },
+    created() { },
+
+    beforeMount() { },
+    mounted() { },
+
+    beforeUpdate() { },
+    updated() { },
+
+    beforeUnmount() {},
+    unmounted() {},
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+
+        name: {
+            type: String,
+            required: true,
+            validator: (value) => {
+                return value.length > 3;
+            }
+        },
+
+        lastname: {
+            type: String
+        },
+
+        nickname: {
+            type: String
+        },
 
     },
 
     data() {
         return {
-            message: 'test'
+            displayName: this.nickname || this.name,
+            fullname: this.fullname ? `${this.name} ${this.lastname}` : `${this.name}`
         }
     },
 
-    props: {
-        name: {
-            type: String,
-            default: ''
+    computed: {
+        compareDisplayName() {
+            return this.entity.name == this.displayName;
+        },
+
+        compareFullname() {
+            return this.entity.nomeCompleto == this.fullname;
         }
     },
     
     methods: {
-        doSomething () {
+        defineNames () {
+            this.entity.name = this.displayName;
+            this.entity.nomeCompleto = this.fullname;
 
+            // emite o evento enviando o data
+            this.$emit('namesDefined', this.entity);
         }
     },
 });
--- src/protected/application/lib/modules/Components/components/__template__/template.php
+++ src/protected/application/lib/modules/Components/components/__template__/template.php
@@ -1,1 +1,8 @@
-<p>{{message}}</p>
+<?php
+use MapasCulturais\i;
+?>
+<slot :fullname="fullname" :displayName="displayName" :compareDisplayName="compareDisplayName" :compareFullname="compareFullname" >
+    <p>{{entity.name}} {{compareDisplayName}} {{displayName}}</p>
+    <p>{{entity.nomeCompleto}} {{compareFullname}} {{fullname}}</p>
+</slot>
+<button @click="defineNames()"><?= i::__('Definir Nomes') ?></button>
