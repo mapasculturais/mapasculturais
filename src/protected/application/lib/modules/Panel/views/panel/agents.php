bdebae317b82517f7207371937d4fd553a398085
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-06-30 22:19:40 -0300

    novo header para a página 'meus agentes' (ref #2064)

--- src/protected/application/lib/modules/Panel/views/panel/agents.php
+++ src/protected/application/lib/modules/Panel/views/panel/agents.php
@@ -3,11 +3,23 @@ use MapasCulturais\i;
 
 $this->import('panel--entity-tabs panel--entity-card');
 ?>
-<div class="panel-list panel-main-content">
-    <header class="panel-header clearfix">
-        <h2><?php i::_e('Meus Agentes') ?></h2>
-    </header>
-    
+
+<div class="panel__row">
+    <h1>
+        <iconify icon="mdi:account-multiple-outline"></iconify>
+        <?= i::__('Meus agentes') ?>
+    </h1>
+    <a class="panel__help-link" href="#"><?=i::__('Ajuda')?></a>
+</div>
+<div class="panel__row">
+    <p><?=i::__('Nesta seção você visualiza e gerencia seu perfil de usuário e outros agentes criados')?></p>
+    <a class="button button--large button--primary" href="#">
+        <iconify icon="mdi:account-multiple-plus"></iconify>
+        <span><?=i::__('Criar agente')?></span>
+    </a>
+</div>
+
+<div class="panel-list panel-main-content">    
     <panel--entity-tabs type="agent" select="id,name,terms,files.avatar" #default={entity}>
         <panel--entity-card :entity="entity"></panel--entity-card>
     </panel--entity-tabs>
