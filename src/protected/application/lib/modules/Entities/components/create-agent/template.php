cdb678f786165c6e6fd472c3819e569a5c622e10
robedj <ed.wassouf@gmail.com>
2022-07-04 11:42:59 -0400

    Ajustes na modal do componente create-agent  Ref.:#2061

--- src/protected/application/lib/modules/Entities/components/create-agent/template.php
+++ src/protected/application/lib/modules/Entities/components/create-agent/template.php
@@ -5,12 +5,16 @@ $this->import('modal field');
 ?>
 
 <div class="create-agent">
-   <modal @open="createInstance()">
-    <div class="create-agent__fields">
-        <field :entity="instance" prop="name"></field>
-        <field :entity="instance"  prop="name"></field>
-        <field :entity="entity"  prop="name"></field>
-    </div>
+   <modal title="Criar Agente" @open="createInstance()">
+       <label>Crie um agente com informações básicas<br>
+           e de forma rápida</label>
+        <div class="create-agent__fields">
+            
+            <field label="Selecione o tipo de agente" :entity="instance"  prop="name"></field>
+            <field label="Nome ou título" :entity="instance" prop="name"></field>
+            
+            <field label="Selecione a área de atuação" :entity="entity"  prop="name"></field>
+        </div>
         <template #button="modal">
             <button class="button button--primary" @click="modal.open()">Criar Agente</button>
         </template>
@@ -18,8 +22,8 @@ $this->import('modal field');
             <div class="create-agent__buttons">
 
                 <button class="button button--primary" @click="createPublic(modal)">Criar e Publicar</button>
-                <button class="button button--primary" @click="createDraft(modal)">Criar em Rascunho</button>
-                <button class="button button--primary" @click="cancel(modal)">Cancelar</button>
+                <button class="button button--solid" @click="createDraft(modal)">Criar em Rascunho</button>
+                <button class="button button--text" @click="cancel(modal)">Cancelar</button>
             </div>
         </template>
    </modal>
