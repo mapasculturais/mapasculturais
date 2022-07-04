fade0964ba74d905865fa250d8a6aca263b5275f
robedj <ed.wassouf@gmail.com>
2022-07-04 10:49:31 -0400

    Alteração no campo Endereço Ref.:#2043

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -12,6 +12,7 @@ $this->import('entity-terms container share-links files-list entity-links entity
         <div class="divider"></div>
         
         <main>
+            <h3>Endereço</h3>
             <div class="row">
                 {{entity.En_Nome_Logradouro}}, {{entity.En_Num}}, {{entity.En_Bairro}}, {{entity.En_CEP}}, {{entity.En_Municipio}}, {{entity.En_Estado}}
             </div>
@@ -21,18 +22,20 @@ $this->import('entity-terms container share-links files-list entity-links entity
             </div>
             <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
             <entity-links :entity="entity" title="Links"></entity-links>
+            <create-agent></create-agent>
             <entity-gallery :entity="entity"></entity-gallery>
 
         </main>
         
         <aside>
             <entity-terms :entity="entity" taxonomy="area" title="Linguagens culturais"></entity-terms>
+            <create-agente :entity="entity"></create-agente>
             <entity-social-media :entity="entity"></entity-social-media>
             <entity-seals :entity="entity" title="Verificações"></entity-seals>
             <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
             <entity-owner :entity="entity" title="Publicado por"></entity-owner>
             <share-links title="Compartilhar" text="Veja este link:"></share-links>
-            
+            <
         </aside>
         
     </container>
