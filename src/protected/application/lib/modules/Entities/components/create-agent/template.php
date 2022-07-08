0a75780242e9289357a5dce98d7b0e736a07a877
robedj <ed.wassouf@gmail.com>
2022-07-08 12:29:52 -0400

    Adição do titulo na área de atuação e remoção de espaços em branco Ref.:#2061

--- src/protected/application/lib/modules/Entities/components/create-agent/template.php
+++ src/protected/application/lib/modules/Entities/components/create-agent/template.php
@@ -1,18 +1,15 @@
  <?php 
 use MapasCulturais\i;
-
-
 $this->import('modal mapas-field entity-terms'); 
 ?>
 
-        
-<modal title="Criar Agente" classes="create-entity" button-label="Criar Agente" >
+<modal title="Criar Agente" class="create-modal" button-label="Criar Agente" >
     <template #default>
-        <label>Crie um agente com informações básicas<br>e de forma rápida</label>
+        <label><?php i::_e('Crie um agente com informações básicas')?><br><?php i::_e('e de forma rápida')?></label>
         <div class="create-modal__fields">
             <mapas-field :entity="entity"  :editable="true" label="Selecione o tipo do agente" prop="type"></mapas-field>
             <mapas-field :entity="entity" label="Nome ou título"  prop="name"></mapas-field>
-            <entity-terms :entity="entity" :editable="true" taxonomy='area'></entity-terms>
+            <entity-terms :entity="entity" :editable="true" taxonomy='area' title="Área de Atuação"></entity-terms>
             <mapas-field :entity="entity" prop="shortDescription" label="Adicione uma Descrição curta para o Agente"></mapas-field>
             <mapas-field :entity="entity" v-for="field in fields" :prop="field"></mapas-field>
         </div>
@@ -21,11 +18,11 @@ $this->import('modal mapas-field entity-terms');
         <slot :modal="modal"></slot>
     </template>
     <template #actions="modal">
-        <div class="create-modal__buttons" ">
+        <div class="create-modal__buttons">
 
-            <button class="button button--primary" @click="createPublic(modal)">Criar e Publicar</button>
-            <button class="button button--solid" @click="createDraft(modal)">Criar em Rascunho</button>
-            <button class="button button--text" @click="cancel(modal)">Cancelar</button>
+            <button class="button button--primary" @click="createPublic(modal)"><?php i::_e('Criar e Publicar')?></button>
+            <button class="button button--solid" @click="createDraft(modal)"><?php i::_e('Criar em Rascunho')?></button>
+            <button class="button button--text button--text-del " @click="cancel(modal)"><?php i::_e('Cancelar')?></button>
         </div>
     </template>
 </modal>
