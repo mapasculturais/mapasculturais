e8dadf2c5db7a49b6e1d51bfba34d1575ecf377e
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-08 00:10:35 -0300

    corrige utilização do componente entity-terms na modal de criação de agentes (refs #2061)

--- src/protected/application/lib/modules/Entities/components/create-agent/template.php
+++ src/protected/application/lib/modules/Entities/components/create-agent/template.php
@@ -11,10 +11,9 @@ $this->import('modal mapas-field entity-terms');
         <div class="create-modal__fields">
             <mapas-field :entity="entity"  :editable="true" label="Selecione o tipo do agente" prop="type"></mapas-field>
             <mapas-field :entity="entity" label="Nome ou título"  prop="name"></mapas-field>
-            <entity-terms :entity="entity" :editable="true" taxonomy="Área de Atuação"></entity-terms>
+            <entity-terms :entity="entity" :editable="true" taxonomy='area'></entity-terms>
             <mapas-field :entity="entity" prop="shortDescription" label="Adicione uma Descrição curta para o Agente"></mapas-field>
             <mapas-field :entity="entity" v-for="field in fields" :prop="field"></mapas-field>
-
         </div>
     </template>
     <template #button="modal">
