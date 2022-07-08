fe77420afcd3af66e66dda02cb2d003d4fedcae5
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-08 00:05:08 -0300

    Implementado modo de edição do componente <entity-terms> (refs #2029)
    - precisa estilizar e terminar de escrever o README.md

--- src/protected/application/lib/modules/Entities/components/entity-terms/script.js
+++ src/protected/application/lib/modules/Entities/components/entity-terms/script.js
@@ -2,9 +2,25 @@ app.component('entity-terms', {
     template: $TEMPLATES['entity-terms'],
     emits: [],
 
-    setup(props, { slots }) {
-        const hasSlot = name => !!slots[name]
-        return { hasSlot }
+    beforeCreate() {
+        this.definition = $TAXONOMIES[this.taxonomy];
+    
+        if (!this.definition) {
+            throw Error(`Taxonomia ${this.taxonomy} não registrada na aplicação`);
+        }
+
+        this.entity.terms[this.taxonomy] = this.entity.terms[this.taxonomy] || [];
+    },
+
+    data() {
+        return {
+            allowInsert: this.definition.allowInsert,
+            terms: this.definition.terms || [],
+            label: this.title || this.definition.name,
+            entityTerms: this.entity.terms[this.taxonomy],
+
+            filter: '',
+        };
     },
 
     props: {
@@ -23,6 +39,77 @@ app.component('entity-terms', {
         title: {
             type: String,
             default: ''
+        }
+    },
+
+    computed: {
+        filteredTerms() {
+            if(this.allowInsert && this.filter.trim().length == 0) {
+                return [];
+            }
+
+            return this.terms.filter((term) => {
+                const _filter = this.filter.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+                const _term = term.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+                if(_term.indexOf(_filter) >= 0) {
+                    return term;
+                }
+            })
+        }
+    },
+
+    methods: {
+        loadTerms() {
+            if (this.definition.terms.length == 0) {
+                const api = new API('term');
+                const response = api.GET('api/list/' + this.taxonomy);
+                response.then((r) => r.json().then((r) => {
+                    this.definition.terms = r;
+                    this.terms = r;
+                }));
+            }
+        },
+
+        remove(term) {
+            const terms = this.entityTerms;
+            const indexOf = terms.indexOf(term);
+            terms.splice(indexOf,1);
+        },
+
+        toggleTerm(term) {
+            const terms = this.entityTerms;
+            if (terms.indexOf(term) >= 0) {
+                this.remove(term);
+            } else {
+                terms.push(term);
+            }
+        },
+
+        underlinedTerm(term) {
+            const _filter = this.filter.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            const _term = term.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            const indexOf = _term.indexOf(_filter);
+            if(indexOf >= 0) {
+                const part0 = term.substr(0, indexOf);
+                const part1 = term.substr(indexOf, this.filter.length);
+                const part2 = term.substr(indexOf + this.filter.length);
+                return `${part0}<u>${part1}</u>${part2}`;
+            } else {
+                return term;
+            }
+        },
+
+        addTerm(term, popover) {
+            if (this.entityTerms.indexOf(term) < 0) {
+                this.entityTerms.push(term);
+            }
+
+            if (this.terms.indexOf(term) < 0) {
+                this.terms.push(term);
+            }
+
+            popover.close();
         }
     }
 });
--- src/protected/application/lib/modules/Entities/components/entity-terms/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-terms/template.php
@@ -1,20 +1,52 @@
-<div v-if="entity.terms?.[taxonomy]" class="entity-terms">
+<?php
+$this->import('popover')
+?>
+<div v-if="editable || entity.terms?.[taxonomy]" class="entity-terms">
 
     <h4 class="entity-terms__title" v-if="title == ''"> {{taxonomy}} </h4>
     <h4 class="entity-terms__title" v-else> {{title}} </h4>
     
     <ul class="entity-terms__terms">
+        <popover openside="down-right" @close="this.filter = ''" @open="loadTerms()">
+            <template #button="popover">
+                <li @click="popover.toggle()" class="button--primary  entity-terms__terms--addNew" v-if="editable">
+                    Adicionar nova
+                    <span><iconify icon="fluent:add-20-filled"/></span>
+                </li>
+            </template>
 
-        <li class="button--solid entity-terms__terms--term" v-for="term in entity.terms[taxonomy]"> 
+            <!-- Modo Tags -->
+            <template v-if="allowInsert" #default="popover">
+                <form @submit="addTerm(filter, popover)">
+                    <input type="text" v-model="filter" placeholder="">
+                    <button>+</button>
+                </form>
+                <ul v-if="terms.length > 0" style="max-height:200px; overflow-y:scroll">
+                    <li @click="addTerm(term, popover)" v-for="term in filteredTerms">
+                        <span v-html="underlinedTerm(term)"></span>
+                    </li>
+                </ul>
+            </template>
+
+            <!-- Modo Área de Atuação -->
+            <template v-if="!allowInsert" #default="popover">
+                <input type="text" v-model="filter" placeholder="Filtre">
+                <ul v-if="terms.length > 0" style="max-height:200px; overflow-y:scroll">
+                    <li v-for="term in filteredTerms">
+                        <label>
+                            <input type="checkbox" 
+                                :checked="entityTerms.indexOf(term) >= 0"
+                                @change="toggleTerm(term)" > 
+                            <span v-html="underlinedTerm(term)"></span>
+                        </label>
+                    </li>
+                </ul>
+            </template>
+        </popover>
+
+        <li class="button--solid entity-terms__terms--term" v-for="term in entityTerms"> 
             <a> {{term}} </a> 
-            <a v-if="editable"><iconify icon="gg:close"/></a>             
+            <a v-if="editable" @click="remove(term)"><iconify icon="gg:close"/></a>             
         </li>
-
-        <li class="button--primary  entity-terms__terms--addNew" v-if="editable">
-            Adicionar nova
-            <span><iconify icon="fluent:add-20-filled"/></span>
-        </li>
-
     </ul>
-
 </div>
