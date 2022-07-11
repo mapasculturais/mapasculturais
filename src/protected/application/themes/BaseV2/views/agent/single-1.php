b753a8bc2cb8b412b3efb563a02bc66fe9897755
robedj <ed.wassouf@gmail.com>
2022-07-11 11:47:25 -0400

    Ajustes de responsividade nas telas do Base V2 Ref.:#2077

--- src/protected/application/themes/BaseV2/views/agent/single-1.php
+++ src/protected/application/themes/BaseV2/views/agent/single-1.php
@@ -14,9 +14,10 @@ $this->import('
         <div class="divider"></div>
         
         <main>
-            <h3>Endereço</h3>
+            
             <div class="row">
-                {{entity.En_Nome_Logradouro}}, {{entity.En_Num}}, {{entity.En_Bairro}}, {{entity.En_CEP}}, {{entity.En_Municipio}}, {{entity.En_Estado}}
+                <h3>Endereço</h3>
+                <p>{{entity.En_Nome_Logradouro}}, {{entity.En_Num}}, {{entity.En_Bairro}}, {{entity.En_CEP}}, {{entity.En_Municipio}}, {{entity.En_Estado}}</p>
             </div>
             <div class="row">
                 <h2>Descrição Detalhada</h2>
@@ -24,13 +25,11 @@ $this->import('
             </div>
             <files-list :files="entity.files.downloads" title="Arquivos para download"></files-list>
             <entity-links :entity="entity" title="Links"></entity-links>
-            <create-agent></create-agent>
             <entity-gallery :entity="entity"></entity-gallery>
         </main>
         
         <aside>
             <entity-terms :entity="entity" taxonomy="area" title="Linguagens culturais"></entity-terms>
-            <create-agente :entity="entity"></create-agente>
             <entity-social-media :entity="entity"></entity-social-media>
             <entity-seals :entity="entity" title="Verificações"></entity-seals>
             <entity-terms :entity="entity" taxonomy="tag" title="Tags"></entity-terms>  
