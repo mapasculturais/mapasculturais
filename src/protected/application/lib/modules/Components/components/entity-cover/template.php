105d65f343e957d3febad99a10e340b408144509
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-11 07:41:27 -0300

    Aplica o <image-uploader> no componente <entity-cover> - #2049

--- src/protected/application/lib/modules/Components/components/entity-cover/template.php
+++ src/protected/application/lib/modules/Components/components/entity-cover/template.php
@@ -3,6 +3,19 @@ use MapasCulturais\i;
 ?>
 
 <div class="entity-cover">
-    <iconify icon="bi:image"/>
-    <h5><?php i::_e("Adicionar Imagem de Capa")?></h5>
+    <image-uploader :entity="entity" group="header" :aspect-ratio="425/96" :circular="false">
+        <template #default="modal">
+            <div class="entity-cover__cover">                
+                <div v-if="entity.files.header" class="entity-cover__cover--img">
+                    <img :src="entity.files.header?.transformations?.header?.url" class="img" />
+                    <label class="label"> <?php i::_e("Alterar Imagem de Capa")?> </label>
+                </div>
+
+                <div v-else class="entity-cover__cover--newImg">
+                    <iconify icon="bi:image-fill" />
+                    <label class="label"> <?php i::_e("Adicionar Imagem de Capa")?> </label>
+                </div>
+            </div>
+        </template>
+    </image-uploader>
 </div>
