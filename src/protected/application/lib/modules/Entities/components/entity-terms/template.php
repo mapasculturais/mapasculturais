4fed983e470eefdc96c43de8170c4982417ef46c
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-12 00:44:25 -0300

    Ajustes no componente <entity-terms>

--- src/protected/application/lib/modules/Entities/components/entity-terms/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-terms/template.php
@@ -1,49 +1,55 @@
 <?php
+use MapasCulturais\i;
 $this->import('popover')
 ?>
 <div v-if="editable || entity.terms?.[taxonomy]" class="entity-terms">
 
     <h4 class="entity-terms__title" v-if="title == ''"> {{taxonomy}} </h4>
     <h4 class="entity-terms__title" v-else> {{title}} </h4>
-    
-    <ul class="entity-terms__terms">
-        <popover openside="down-right" @close="this.filter = ''" @open="loadTerms()">
-            <template #button="popover">
-                <li @click="popover.toggle()" class="button--primary  entity-terms__terms--addNew" v-if="editable">
-                    Adicionar nova
-                    <span><iconify icon="fluent:add-20-filled"/></span>
-                </li>
-            </template>
 
-            <!-- Modo Tags -->
-            <template v-if="allowInsert" #default="popover">
-                <form @submit="addTerm(filter, popover)">
-                    <input type="text" v-model="filter" placeholder="">
-                    <button>+</button>
+    <popover openside="down-right" @close="this.filter = ''" @open="loadTerms()">
+        <template #button="popover">
+            <button @click="popover.toggle()" class="button button--rounded button--sm button--icon button--primary" v-if="editable">
+                <?php i::_e("Adicionar nova") ?>
+                <iconify icon="ps:plus"></iconify>
+            </button>
+        </template>
+
+        <!-- Modo Tags -->
+        <template v-if="allowInsert" #default="popover">
+            <div class="entity-terms__popover">
+                <form class="entity-terms__popover--form" @submit="addTerm(filter, popover)">
+                    <input type="text" v-model="filter" class="entity-terms__popover--input" placeholder="Filter">
+                    <button class="entity-terms__popover--button"><iconify icon="ic:baseline-search"></iconify></button>
                 </form>
-                <ul v-if="terms.length > 0" style="max-height:200px; overflow-y:scroll">
+                <ul v-if="terms.length > 0">
                     <li @click="addTerm(term, popover)" v-for="term in filteredTerms">
                         <span v-html="underlinedTerm(term)"></span>
                     </li>
                 </ul>
-            </template>
+            </div>
+        </template>
 
-            <!-- Modo Área de Atuação -->
-            <template v-if="!allowInsert" #default="popover">
-                <input type="text" v-model="filter" placeholder="Filtre">
-                <ul v-if="terms.length > 0" style="max-height:200px; overflow-y:scroll">
+        <!-- Modo Área de Atuação -->
+        <template v-if="!allowInsert" #default="popover">
+            <div class="entity-terms__popover">
+                <input type="text" v-model="filter" class="entity-terms__popover--input" placeholder="Filter">
+                <ul v-if="terms.length > 0">
                     <li v-for="term in filteredTerms">
-                        <label>
+                        <label class="entity-terms__popover--field">
                             <input type="checkbox" 
                                 :checked="entityTerms.indexOf(term) >= 0"
-                                @change="toggleTerm(term)" > 
-                            <span v-html="underlinedTerm(term)"></span>
+                                @change="toggleTerm(term)"
+                                class="input" > 
+                            <span class="text" v-html="underlinedTerm(term)"></span>
                         </label>
                     </li>
                 </ul>
-            </template>
-        </popover>
+            </div>
+        </template>
+    </popover>
 
+    <ul class="entity-terms__terms">
         <li class="button--solid entity-terms__terms--term" v-for="term in entityTerms"> 
             <a> {{term}} </a> 
             <a v-if="editable" @click="remove(term)"><iconify icon="gg:close"/></a>             
