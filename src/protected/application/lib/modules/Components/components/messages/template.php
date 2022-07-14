261f63e65745127d551fa713eb9e4f5ab7f234c1
robedj <ed.wassouf@gmail.com>
2022-07-14 13:33:01 -0400

    Estilização do snackbar do componente messages Ref.:#2076

--- src/protected/application/lib/modules/Components/components/messages/template.php
+++ src/protected/application/lib/modules/Components/components/messages/template.php
@@ -5,11 +5,10 @@ $this->import('mapas-container');
 <mapas-container class="messages">
     <template  v-for="message in messages">
         <div class="messages__message" :class="message.type">
+            <div class="messages__message--snackbar"></div>
             <a @click="message.active=false"></a>
             <div class="messages__message--text">
-                
-            <div class="message.type">
-            {{message.text}}
+                {{message.text}}
             </div>
         </div>
         
