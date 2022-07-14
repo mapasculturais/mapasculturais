4ffad6327263880c08f60ca85404ed212947f23f
robedj <ed.wassouf@gmail.com>
2022-07-14 13:16:52 -0400

    W.I.P Estilização do snackbar: Transparência e posição da mensagem e ícones Ref.:#2076

--- src/protected/application/lib/modules/Components/components/messages/template.php
+++ src/protected/application/lib/modules/Components/components/messages/template.php
@@ -6,7 +6,12 @@ $this->import('mapas-container');
     <template  v-for="message in messages">
         <div class="messages__message" :class="message.type">
             <a @click="message.active=false"></a>
-            <div class="messages__message--text"><label class="message.type"></label>{{message.text}}</div>
+            <div class="messages__message--text">
+                
+            <div class="message.type">
+            {{message.text}}
+            </div>
+        </div>
         
         </div>
     </template>
