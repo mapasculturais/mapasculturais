4c5a18cffc7994cdfd4688078c734351a1671972
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-14 10:49:46 -0300

    remove id do content do componente <tab> (refs #1885)

--- src/protected/application/lib/modules/Components/components/tab/template.php
+++ src/protected/application/lib/modules/Components/components/tab/template.php
@@ -1,6 +1,6 @@
 <?php
 $this->import('tabs');
 ?>
-<section v-if="isActive || cached" v-show="isActive" :aria-hidden="!isActive" class="tab-component" :class="slug" :id="slug" role="tabpanel">
+<section v-if="isActive || cached" v-show="isActive" :aria-hidden="!isActive" class="tab-component" :class="slug" role="tabpanel">
     <slot></slot>
 </section>
