8cc685fee8c08fa099a607c38f9c8639fc7ba819
robedj <ed.wassouf@gmail.com>
2022-07-18 11:13:24 -0400

    Estilização do snackbar do componente messages Ref.:#2076

--- src/protected/application/lib/modules/Components/components/messages/template.php
+++ src/protected/application/lib/modules/Components/components/messages/template.php
@@ -2,16 +2,18 @@
 use MapasCulturais\i;
 $this->import('mapas-container');
 ?>
-<mapas-container class="messages">
-    <template  v-for="message in messages">
-        <div class="messages__message" :class="message.type">
-            <div class="messages__message--text">
-                {{message.text}}
+<div class="messages">
+    <div class="messages__content">
+        <template  v-for="message in messages">
+            <div class="messages__content--message" :class="message.type">
+                <div class="messages__content--message-text">
+                    {{message.text}}
+                </div>
+                <a class="messages__content--message-close" @click="message.active=false">
+                    <iconify icon="gg:close"></iconify>
+                </a>
             </div>
-            <a class="messages__message--close" @click="message.active=false">
-                <iconify icon="gg:close"></iconify>
-            </a>
-        </div>
-    </template>
-</mapas-container>
+        </template>
+    </div>
+</div>
 
