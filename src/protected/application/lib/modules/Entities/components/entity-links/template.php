2e1b1ae1a8e8a4abe0734c1fc12247355dc38907
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-19 00:36:28 -0300

    Adição do modo de edição do <entity-links> - #2059

--- src/protected/application/lib/modules/Entities/components/entity-links/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-links/template.php
@@ -3,15 +3,10 @@ use MapasCulturais\i;
 ?>
 <div class="entity-links">
     <h4 class="entity-links__title"> {{title}} </h4>
-  
-    <button class="button button-primary" @click="success()">Sucesso</button>
-    <button class="button button-primary" @click="error()">Erro</button>
-    <button class="button button-primary" @click="warning()">Aviso</button>
-    
-    <ul class="entity-links__links">
 
+    <ul class="entity-links__links">
         <li class="entity-links__links--item" v-for="link in entity.metalists.links">
-            <a class="link" :href="link.value" target="_blank" >
+            <a class="link" :class="{'editable': editable}" :href="link.value" target="_blank" >
                 <iconify icon="eva:link-outline" /> 
                 {{link.title}}
             </a>            
@@ -20,11 +15,36 @@ use MapasCulturais\i;
                 <a> <iconify icon="ooui:trash" /> </a>
             </div>
         </li>
-
-        <li v-if="editable" class="button--primary entity-links__links--addNew">
-            <?php i::_e("Adicionar novo link")?>
-            <span> <iconify icon="fluent:add-20-filled" /> </span>
-        </li>
-
     </ul>
+
+    <popover v-if="editable" openside="down-right">
+        <template #button="{ toggle }">
+            <slot name="button" :toggle="toggle"> 
+                <a class="button button--primary button--icon button--primary-outline" @click="toggle()">
+                    <?php i::_e("Adicionar")?>
+                    <iconify icon="fluent:add-20-filled"></iconify>
+                </a>
+            </slot>
+        </template>
+
+        <template #default="{ close }">
+            <div class="entity-links__newLink">
+                <div class="field">
+                    <label><?php i::_e('Título do link') ?></label>
+                    <input v-model="newLinkTitle" class="newLinkTitle" type="text" name="newLinkTitle" />
+                </div>
+
+                <div class="field">
+                    <label><?php i::_e('Link') ?></label>
+                    <input v-model="newLink" class="newLink" type="text" name="newLink" />
+                </div>
+                
+                <div class="newGroup--actions">
+                    <button class="button button--text"  @click="close()"> <?php i::_e("Cancelar") ?> </button>
+                    <button @click="addLink(newLinkTitle, newLink)" class="button button--solid"> <?php i::_e("Confirmar") ?> </button>
+                </div>
+            </div>
+
+        </template>
+    </popover>
 </div>
