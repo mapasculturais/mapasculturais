f6c8a38c460716d6744ccd2f79ae570220b0bfd4
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-20 14:49:58 -0300

    modifica ícone do loading

--- src/protected/application/lib/modules/Components/components/loading/template.php
+++ src/protected/application/lib/modules/Components/components/loading/template.php
@@ -2,5 +2,5 @@
 use MapasCulturais\i;
 ?>
 <span v-if="condition || entity?.__processing" class="loading">
-    <iconify icon="eos-icons:loading"></iconify> {{entity?.__processing || '<?php i::_e('carregando...') ?>'}}
+    <iconify icon="eos-icons:three-dots-loading"></iconify> {{entity?.__processing || '<?php i::_e('carregando...') ?>'}}
 </span>
