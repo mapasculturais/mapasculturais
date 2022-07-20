9ac2735152a690050c2a74a0826417eed192bb78
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-20 15:47:52 -0300

    internacionalização do componente <select-entity> (Refs #2037)

--- src/protected/application/lib/modules/Components/components/select-entity/script.js
+++ src/protected/application/lib/modules/Components/components/select-entity/script.js
@@ -2,9 +2,10 @@ app.component('select-entity', {
     template: $TEMPLATES['select-entity'],
     emits: ['select'],
 
-    setup(props, { slots }) {
-        const hasSlot = name => !!slots[name]
-        return { hasSlot }
+    setup() { 
+        // os textos estão localizados no arquivo texts.php deste componente 
+        const text = Utils.getTexts('select-entity')
+        return { text }
     },
 
     data() {
@@ -21,33 +22,33 @@ app.component('select-entity', {
     created() {
         switch (this.type) {
             case 'agent':
-                this.itensText = 'Selecione um de seus agentes';
-                this.buttonText = 'Crie um novo agente';
-                this.placeholder = 'Pesquise por agentes';
+                this.itensText = this.text('Selecione um dos agentes');
+                this.buttonText = this.text('Crie um novo agente');
+                this.placeholder = this.text('Pesquise por agentes');
                 this.icon = 'fa-solid:user-friends';
                 break;
             case 'space':
-                this.itensText = 'Selecione um de seus espaços';
-                this.buttonText = 'Crie um novo espaço';
-                this.placeholder = 'Pesquise por espaços';
+                this.itensText = this.text('Selecione um dos espaços');
+                this.buttonText = this.text('Crie um novo espaço');
+                this.placeholder = this.text('Pesquise por espaços');
                 this.icon = 'clarity:building-line';
                 break;
             case 'event':
-                this.itensText = 'Selecione um de seus eventos';
-                this.buttonText = 'Crie um novo evento';
-                this.placeholder = 'Pesquise por eventos';
+                this.itensText = this.text('Selecione um dos eventos');
+                this.buttonText = this.text('Crie um novo evento');
+                this.placeholder = this.text('Pesquise por eventos');
                 this.icon = 'ant-design:calendar-twotone';
                 break;
             case 'project':
-                this.itensText = 'Selecione um de seus projetos';
-                this.buttonText = 'Crie um novo projeto';
-                this.placeholder = 'Pesquise por projetos';
+                this.itensText = this.text('Selecione um dos projetos');
+                this.buttonText = this.text('Crie um novo projeto');
+                this.placeholder = this.text('Pesquise por projetos');
                 this.icon = 'ri:file-list-2-line';
                 break;
             case 'opportunity':
-                this.itensText = 'Selecione uma de suas oportunidades';
-                this.buttonText = 'Crie uma nova oportunidade';
-                this.placeholder = 'Pesquise por oportunidades';
+                this.itensText = this.text('Selecione uma das oportunidades');
+                this.buttonText = this.text('Crie uma nova oportunidade');
+                this.placeholder = this.text('Pesquise por oportunidades');
                 this.icon = 'icons8:idea';
                 break;
         }
