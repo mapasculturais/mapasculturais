d90d5a3830d7e955aa3d6f5caabad3abf650e319
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-20 12:42:03 -0300

    Ajustes no componente <entity-terms>

--- src/protected/application/lib/modules/Entities/components/entity-terms/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-terms/template.php
@@ -17,13 +17,16 @@ $this->import('popover')
 
         <!-- Modo Tags -->
         <template v-if="allowInsert" #default="popover">
-            <div class="entity-terms__popover">
-                <form class="entity-terms__popover--form" @submit="addTerm(filter, popover)">
-                    <input type="text" v-model="filter" class="entity-terms__popover--input" placeholder="<?= i::__('Busque') ?>">
-                    <button class="entity-terms__popover--button"><iconify icon="ic:baseline-search"></iconify></button>
+            <div class="entity-terms__tags">
+                <form class="entity-terms__tags--form" @submit="addTerm(filter, popover)">
+                    <input type="text" v-model="filter" class="entity-terms__tags--form-input" placeholder="<?= i::__('Adicione uma nova tag') ?>">
+                    <button class="button button--primary button--icon entity-terms__tags--form-addBtn" type="submit">
+                        <iconify icon="gridicons:plus"></iconify>
+                    </button>
+                    <!-- <button class="entity-terms__popover--button"><iconify icon="ic:baseline-search"></iconify></button> -->
                 </form>
-                <ul v-if="terms.length > 0">
-                    <li @click="addTerm(term, popover)" v-for="term in filteredTerms">
+                <ul class="entity-terms__tags--list" v-if="filteredTerms.length > 0">
+                    <li class="entity-terms__tags--list-item" @click="addTerm(term, popover)" v-for="term in filteredTerms">
                         <span v-html="highlightedTerm(term)"></span>
                     </li>
                 </ul>
@@ -32,11 +35,11 @@ $this->import('popover')
 
         <!-- Modo Área de Atuação -->
         <template v-if="!allowInsert" #default="popover">
-            <div class="entity-terms__popover">
-                <input type="text" v-model="filter" class="entity-terms__popover--input" placeholder="<?= i::__('Filtre') ?>">
-                <ul v-if="terms.length > 0">
+            <div class="entity-terms__area">
+                <input type="text" v-model="filter" class="entity-terms__area--input" placeholder="<?= i::__('Filtro') ?>">
+                <ul v-if="terms.length > 0" class="entity-terms__area--list">
                     <li v-for="term in filteredTerms">
-                        <label class="entity-terms__popover--field">
+                        <label class="entity-terms__area--list-item">
                             <input type="checkbox" 
                                 :checked="entityTerms.indexOf(term) >= 0"
                                 @change="toggleTerm(term)"
@@ -45,14 +48,18 @@ $this->import('popover')
                         </label>
                     </li>
                 </ul>
+
+                <button class="button button--solid button--solid-dark" @click="popover.toggle()">
+                    <?php i::_e('Confirmar')?>
+                </button>
             </div>
         </template>
     </popover>
 
     <ul class="entity-terms__terms">
         <li class="button--solid entity-terms__terms--term" v-for="term in entityTerms"> 
-            <a> {{term}} </a> 
-            <a v-if="editable" @click="remove(term)"><iconify icon="gg:close"/></a>             
+            {{term}}
+            <iconify v-if="editable" @click="remove(term)" icon="gg:close"></iconify>
         </li>
     </ul>
 </div>
