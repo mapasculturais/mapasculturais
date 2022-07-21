4622864666b99d51342d82d78c5c309af51b6bad
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-20 21:17:53 -0300

    implementadas ações de adicionar e remover administradores no componente <entity-admins> (Refs #2050)

--- src/protected/application/lib/modules/Entities/components/entity-admins/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-admins/template.php
@@ -4,20 +4,28 @@ use MapasCulturais\i;
 
 <div class="entity-admins" v-if="hasGroups()">
 
-    <h3><?php i::_e("Grupo de administradores")?></h3>
+    <h3><?php i::_e("Administradores")?></h3>
 
-    <div v-for="(groupAgents, groupName) in group" class="entity-related-agents__group">
+    <div class="entity-related-agents__group">
         
         <div class="entity-related-agents__group--agents">
 
-            <div v-for="agent in groupAgents" class="agent">
+            <div v-for="agent in group" class="agent">
                 <a :href="agent.singleUrl" class="agent__img">
                     <img v-if="agent.files.avatar" :src="agent.files.avatar?.transformations?.avatarMedium?.url" class="agent__img--img" />
                     <iconify v-else icon="bi:image-fill" />
                 </a>
 
                 <div v-if="editable" class="agent__delete">
-                    <iconify icon="gg:close"/>
+                    <!-- remover agente -->
+                    <confirm-button @confirm="removeAgent(agent)">
+                        <template #button="modal">
+                            <iconify @click="modal.open()" icon="gg:close"/>
+                        </template> 
+                        <template #message="message">
+                            <?php i::_e('Remover agente relacionado?') ?>
+                        </template> 
+                    </confirm-button>
                 </div>
             </div>
             
@@ -25,7 +33,7 @@ use MapasCulturais\i;
     
         <div class="entity-related-agents__group--actions">
 
-            <select-entity type="agent" @select="changeOwner($event)" openside="down-right">            
+            <select-entity type="agent" @select="addAgent($event)" :query="query" openside="down-right">            
                 <template #button="{ toggle }">
                     <button class="button button--rounded button--sm button--icon button--primary" @click="toggle()"> <?php i::_e('Adicionar administrador') ?> <iconify icon="ps:plus"/> </button>
                 </template>
