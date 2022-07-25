8b338456a2872fce536c39ba716e0282d7eec8f4
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-25 09:19:30 -0300

    criação e edição de links e vídeos (refs #2059 e #2058)

--- src/protected/application/lib/modules/Entities/components/entity-gallery-video/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-gallery-video/template.php
@@ -7,31 +7,53 @@ use MapasCulturais\i;
 
     <div class="entity-gallery__list">   
 
-        <div v-if="entity.metalists?.videos" v-for="(video, index) in videos()" class="entity-gallery__list--video">
+        <div v-if="entity.metalists?.videos" v-for="(metalist, index) in videos" class="entity-gallery__list--video">
             <div class="entity-gallery__list--video-img">
-                <img @click="openVideo(index); open()" :src="video.data.thumbnail" />
+                <img @click="openVideo(index); open()" :src="metalist.video.thumbnail" />
             </div>
 
-            <p @click="openVideo(index); open()" class="entity-gallery__list--video-label"> {{video.title}} </p>
+            <p @click="openVideo(index); open()" class="entity-gallery__list--video-label"> {{metalist.title}} </p>
 
             <div v-if="editable" class="entity-gallery__list--video-actions">
-                <a> <iconify icon="zondicons:edit-pencil"></iconify> </a>
+                <popover @open="metalist.newData = {...metalist}" openside="down-right">
+                    <template #button="{ toggle, close }">
+                        <a @click="toggle()"> <iconify icon="zondicons:edit-pencil"></iconify> </a>
+                    </template>
+                    <template #default="{close}">
+                        <form @submit="save(metalist).then(close); $event.preventDefault()" class="entity-related-agents__addNew--newGroup">
+                            <div class="field">
+                                <label><?php i::_e('Link do vídeo') ?></label>
+                                <input v-model="metalist.newData.value" type="url" />
+                            </div>
+                            
+                            <div class="field">
+                                <label><?php i::_e('Nome do vídeo') ?></label>
+                                <input v-model="metalist.newData.title" type="text" />
+                            </div>
 
-                <confirm-button @confirm="">
-                    <template #button="modal">
-                        <a @click="modal.open()"> <iconify icon="ooui:trash"></iconify> </a>
+                            <div class="actions">
+                                <a class="button button--text"  @click="close()"> <?php i::_e("Cancelar") ?> </a>
+                                <button class="button button--primary"> <?php i::_e("Confirmar") ?> </button>
+                            </div>
+                        </form>
+                    </template>
+                </popover>
+                
+                <confirm-button @confirm="metalist.delete()">
+                    <template #button="{open}">
+                        <a @click="open()"> <iconify icon="ooui:trash" /> </a>
                     </template> 
                     <template #message="message">
-                        <?php i::_e('Deseja excluir esse vídeo?') ?>
+                        <?php i::_e('Deseja remover este vídeo?') ?>
                     </template> 
                 </confirm-button>
+                
             </div>
         </div>
-
     </div>
 
     <div v-if="editable" class="entity-gallery__addNew">
-        <popover v-if="editable" openside="down-right">
+        <popover v-if="editable" openside="right-up">
             <template #button="{ toggle }">
                 <slot name="button" :toggle="toggle"> 
                     <a class="button button--primary button--icon button--primary-outline" @click="toggle()">
@@ -42,20 +64,22 @@ use MapasCulturais\i;
             </template>
 
             <template #default="{ close }">
-                <div class="field">
-                    <label><?php i::_e('URL do vídeo') ?></label>
-                    <input v-model="newVideo" class="newVideo" type="text" name="newVideo" />
-                </div>
+                <form @submit="create().then(close); $event.preventDefault();">
+                    <div class="field">
+                        <label><?php i::_e('URL do vídeo') ?></label>
+                        <input v-model="metalist.value" class="newVideo" type="url" name="newVideo" />
+                    </div>
 
-                <div class="field">
-                    <label><?php i::_e('Descrição do vídeo') ?></label>
-                    <input v-model="newVideoDesc" class="newVideoDesc" type="text" name="newVideoDesc" />
-                </div>
-                
-                <div class="actions">
-                    <button class="button button--text"  @click="close()"> <?php i::_e("Cancelar") ?> </button>
-                    <button @click="addLink(newVideoDesc, newVideo)" class="button button--solid"> <?php i::_e("Confirmar") ?> </button>
-                </div>
+                    <div class="field">
+                        <label><?php i::_e('Título do vídeo') ?></label>
+                        <input v-model="metalist.title" class="newVideoDesc" type="text" name="newVideoDesc" />
+                    </div>
+                    
+                    <div class="actions">
+                        <a class="button button--text"  @click="close()"> <?php i::_e("Cancelar") ?> </a>
+                        <button class="button button--solid"> <?php i::_e("Confirmar") ?> </button>
+                    </div>
+                </form>
             </template>        
         </popover>
     </div>
@@ -64,7 +88,7 @@ use MapasCulturais\i;
         <div @click="close" class="entity-gallery__full--overlay"> </div>
 
         <div class="entity-gallery__full--video">
-            <iframe v-if="actualVideo.data?.provider == 'youtube'" :src="'http://www.youtube.com/embed/'+actualVideo.data?.videoID" height="565"></iframe>
+            <iframe v-if="actualVideo.video?.provider == 'youtube'" :src="'https://www.youtube.com/embed/'+actualVideo.video?.videoID" height="565"></iframe>
             <p v-else> <?php i::_e("Sem vimeo por enquanto.")?> </p>
             <div class="description">{{actualVideo.title}}</div>
         </div>
