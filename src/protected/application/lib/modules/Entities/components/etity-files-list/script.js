6598953f7a90d1e12e99cd1e84d67037c2c9a85e
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-07-26 09:47:03 -0300

    renomeia o componente <files-list> para <entity-files-list> e faz ele receber uma entidade e o nome do grupo de arquivos (refs: #2106)

---
+++ src/protected/application/lib/modules/Entities/components/etity-files-list/script.js
@@ -0,0 +1,38 @@
+app.component('entity-files-list', {
+    template: $TEMPLATES['entity-files-list'],
+    emits: [],
+
+    setup() { 
+        // os textos estão localizados no arquivo texts.php deste componente 
+        const text = Utils.getTexts('entity-files-list')
+        return { text }
+    },
+
+    created() {
+
+    },
+
+    computed: {
+        files: () => {
+            return this.entity.files?.[this.group] || []
+        }
+    },
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+        group: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    
+    methods: {
+    },
+});
