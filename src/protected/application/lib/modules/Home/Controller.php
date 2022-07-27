d9411d8ff08b2083b32b77c49bbe963f3aa5c2e4
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-27 11:13:01 -0300

    Criação do módulo da home - #2097

---
+++ src/protected/application/lib/modules/Home/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Home;
+
+class Controller extends \MapasCulturais\Controller
+{
+    function __construct()
+    {
+        $this->layout = 'home';
+    }
+    
+    function GET_index() {
+        $this->requireAuthentication();
+        $this->render('index');
+    }
+}
