6a2faa4ef9e644b28478a37790643c72166fbdd0
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-26 22:47:45 -0300

    Ajusta botões de ação da dos grupos no componente <entity-related-agents>

--- src/protected/application/lib/modules/Entities/components/entity-related-agents/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-related-agents/template.php
@@ -9,38 +9,8 @@ $this->import('popover confirm-button');
 
     <div v-for="(groupAgents, groupName) in groups" class="entity-related-agents__group">
 
-        <label class="entity-related-agents__group--name"> {{groupName}} </label>
-
-        <!-- lista de agentes -->
-        <div class="entity-related-agents__group--agents">
-            <div v-for="agent in groupAgents" class="agent">
-                <a :href="agent.singleUrl" class="agent__img">
-                    <img v-if="agent.files.avatar" :src="agent.files.avatar?.transformations?.avatarMedium?.url" class="agent__img--img" />
-                    <iconify v-else icon="bi:image-fill" />
-                </a>
-
-                <div v-if="editable" class="agent__delete">
-                    <!-- remover agente -->
-                    <confirm-button @confirm="removeAgent(groupName, agent)">
-                        <template #button="modal">
-                            <iconify @click="modal.open()" icon="gg:close"/>
-                        </template> 
-                        <template #message="message">
-                            <?php i::_e('Remover agente relacionado?') ?>
-                        </template> 
-                    </confirm-button>
-                </div>
-            </div>
-            
-        </div>
-    
-        <div class="entity-related-agents__group--actions">
-
-            <select-entity type="agent" @select="addAgent(groupName, $event)" :query="queries[groupName]" openside="down-right">
-                <template #button="{ toggle }">
-                    <button class="button button--rounded button--sm button--icon button--primary" @click="toggle()"> <?php i::_e('Adicionar agente') ?> <iconify icon="ps:plus"/> </button>
-                </template>
-            </select-entity>
+        <div class="entity-related-agents__group--name">
+            <label> {{groupName}} </label> 
 
             <!-- botões de ação do grupo -->
             <div v-if="editable" class="act">
@@ -57,7 +27,7 @@ $this->import('popover confirm-button');
                     <template #default="popover">
                         <form @submit="renameGroup(groupName, groupAgents.newGroupName, popover); $event.preventDefault()" class="entity-related-agents__addNew--newGroup">
                             <div class="row">
-                                <div class="col">
+                                <div class="col-12">
                                     <input v-model="groupAgents.newGroupName" class="input" type="text" name="newGroup" placeholder="<?php i::esc_attr_e('Digite o nome do grupo') ?>" />
                                 </div>
                             </div>
@@ -83,7 +53,38 @@ $this->import('popover confirm-button');
                     </template>
                 </confirm-button>
             </div>
+        </div>
+
+
+        <!-- lista de agentes -->
+        <div class="entity-related-agents__group--agents">
+            <div v-for="agent in groupAgents" class="agent">
+                <a :href="agent.singleUrl" class="agent__img">
+                    <img v-if="agent.files.avatar" :src="agent.files.avatar?.transformations?.avatarMedium?.url" class="agent__img--img" />
+                    <iconify v-else icon="bi:image-fill" />
+                </a>
+
+                <div v-if="editable" class="agent__delete">
+                    <!-- remover agente -->
+                    <confirm-button @confirm="removeAgent(groupName, agent)">
+                        <template #button="modal">
+                            <iconify @click="modal.open()" icon="gg:close"/>
+                        </template> 
+                        <template #message="message">
+                            <?php i::_e('Remover agente relacionado?') ?>
+                        </template> 
+                    </confirm-button>
+                </div>
+            </div>
             
+        </div>
+    
+        <div class="entity-related-agents__group--actions">
+            <select-entity type="agent" @select="addAgent(groupName, $event)" :query="queries[groupName]" openside="down-right">
+                <template #button="{ toggle }">
+                    <button class="button button--rounded button--sm button--icon button--primary" @click="toggle()"> <?php i::_e('Adicionar agente') ?> <iconify icon="ps:plus"/> </button>
+                </template>
+            </select-entity>            
         </div>
         
     </div>
