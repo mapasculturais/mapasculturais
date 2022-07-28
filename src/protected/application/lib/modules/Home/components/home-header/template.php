f5ac848f4e618d816e2feb05d0c92581faf645fa
robedj <ed.wassouf@gmail.com>
2022-07-28 10:34:20 -0400

    Refatoração no estilo do componente home-header Ref.:2100

--- src/protected/application/lib/modules/Home/components/home-header/template.php
+++ src/protected/application/lib/modules/Home/components/home-header/template.php
@@ -4,22 +4,22 @@ use MapasCulturais\i;
 ?>
 
 <div class="home-header">
+    <div class="home-header__content">
+        <div class="home-header__content--left">
+            <div class="content">
+                <label class="content__title">
+                    {{title}}
+                </label>
+                <p class="content__description">
+                    {{description}}
+                </p>
+            </div>
+        </div>
 
-    <div class="home-header__left">
-        <div class="home-header__left--content">
-            <label class="title">
-                {{title}}
-            </label>
-            <p class="description">
-                {{description}}
-            </p>
+        <div class="home-header__content--right">
+            <div class="img">
+                <img src="<?php $this->asset('img/home/mapa.jpg') ?>" />
+            </div>
         </div>
     </div>
-
-    <div class="home-header__right">
-        <div class="img">
-            <img src="<?php $this->asset('img/home/mapa.jpg') ?>" />
-        </div>
-    </div>
-
 </div>
