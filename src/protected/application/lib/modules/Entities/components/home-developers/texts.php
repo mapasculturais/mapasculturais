3463651584e5977d48d08bd7a4d9215a02a43939
robedj <ed.wassouf@gmail.com>
2022-07-28 10:21:15 -0400

    Remoção de código em desuso no script e texts no componente home-developers  Ref.:2091

--- src/protected/application/lib/modules/Entities/components/home-developers/texts.php
+++ src/protected/application/lib/modules/Entities/components/home-developers/texts.php
@@ -2,6 +2,5 @@
 use MapasCulturais\i;
 
 return [
-    'texto localizado' => i::__('Aqui o texto localizado que será exibido'),
-    'outro texto localizado' => i::__('Outro texto localizado')
+    
 ];
