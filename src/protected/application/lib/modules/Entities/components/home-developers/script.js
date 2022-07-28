cebcf3860a1849cf6c3615a5e2ce6b447151ee84
robedj <ed.wassouf@gmail.com>
2022-07-28 10:21:15 -0400

    Remoção de código em desuso no script e texts no componente home-developers  Ref.:2091

--- src/protected/application/lib/modules/Entities/components/home-developers/script.js
+++ src/protected/application/lib/modules/Entities/components/home-developers/script.js
@@ -1,36 +1,11 @@
 app.component('home-developers', {
     template: $TEMPLATES['home-developers'],
-    emits: [],
-
-    setup(props, { slots }) {
-        const hasSlot = name => !!slots[name]
-        return { hasSlot }
-    },
-
-    created() {
-
-    },
-
-    data() {
-        return {
-            message: 'test'
-        }
-    },
-
+   
     props: {
-        entity: {
-            type: Entity,
-            required: true
-        },
-        editable: {
-            type: Boolean,
-            default: false
-        }
+        
     },
-    
-    methods: {
-        doSomething () {
 
-        }
+    methods: {
+        
     },
 });
