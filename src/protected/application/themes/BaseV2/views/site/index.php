c9540a4551a6952d6f3b9ccb73c2a4c420e209ed
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-07-29 11:27:20 -0300

    utilização do componente <home-opportunities>

--- src/protected/application/themes/BaseV2/views/site/index.php
+++ src/protected/application/themes/BaseV2/views/site/index.php
@@ -1,8 +1,9 @@
 <?php 
 use MapasCulturais\i;
-$this->import('home-header home-developers home-entities');
+$this->import('home-header home-entities home-opportunities home-developers');
 ?>
 
 <home-header></home-header>
+<home-opportunities></home-opportunities>
 <home-entities></home-entities>
 <home-developers></home-developers>
