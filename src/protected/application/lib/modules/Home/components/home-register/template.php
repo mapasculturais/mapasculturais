b93f4a18ee4481cf8cb91bf101ce95b571473de3
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-01 15:25:42 -0300

    Ajusta o background do componente <home-register> - #2102

--- src/protected/application/lib/modules/Home/components/home-register/template.php
+++ src/protected/application/lib/modules/Home/components/home-register/template.php
@@ -2,7 +2,10 @@
 use MapasCulturais\i;
 ?>
 <div class="home-register">
-    <img class="home-register__background" src="<?php $this->asset('img/home/mapa.jpg') ?>" />
+    <div class="home-register__background">
+        <img class="home-register__background--img" src="<?php $this->asset('img/home/home-register/background.png') ?>" />
+        <div class="home-register__background--mask"></div>
+    </div>
     <div class="home-register__content">
         <label class="home-register__content--title"><?php i::_e('Faça seu cadastro e colabore com o Mapas Culturais') ?></label>
         <p class="home-register__content--description">Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Secretaria da Cultura.
