d4e5b841d101521a22fd1e688ea1f14c7accdc40
robedj <ed.wassouf@gmail.com>
2022-08-01 12:59:24 -0400

    Estilização do componente home-map. Ref.:2103

--- src/protected/application/lib/modules/Home/components/home-map/template.php
+++ src/protected/application/lib/modules/Home/components/home-map/template.php
@@ -4,11 +4,10 @@ use MapasCulturais\i;
 <div class="home-map">
     <div class="home-map__content">
         <label class="home-map__content--title"><?php i::_e('Visualize também no mapa') ?></label>
-        <p class="home-map__content--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum et, rhoncus semper et, nulla. 
-        </p>
-        <div class="home-map__content--mapBtn">
-            <button class="home-map__content--mapBtn-button button "><?php i::_e('Mostrar')?></iconify></button>
 
+        <p class="home-map__content--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum et, rhoncus semper et, nulla. </p>
+
+        <img class="home-map__content--img" src="<?php $this->asset('img/home/home-map/Map.png') ?>" />
         </div>
     </div>
 </div>
