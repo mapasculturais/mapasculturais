461a851f41a44110dc8e1edbb1c3e017579fcac8
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-01 15:07:40 -0300

    Ajustes de layout do componente <home-opportunities> - #2098

--- src/protected/application/lib/modules/Home/components/home-opportunities/script.js
+++ src/protected/application/lib/modules/Home/components/home-opportunities/script.js
@@ -1,28 +1,33 @@
 app.component('home-opportunities', {
     template: $TEMPLATES['home-opportunities'],
 
+    components: {
+        carousel: Vue3Carousel.Carousel,
+        slide: Vue3Carousel.Slide,
+        pagination: Vue3Carousel.Pagination,
+        navigation: Vue3Carousel.Navigation
+    },
+
     setup() { 
         // os textos estão localizados no arquivo texts.php deste componente 
         const text = Utils.getTexts('home-opportunities')
         return { text }
     },
 
-    mounted() { 
-        window.onload = function () {
+    created() { 
+        /* window.onload = function () {
+            
             this.lista = document.querySelector('.home-opportunities__content--cards-list');
             this.btnLeft = document.querySelector('.actions').firstElementChild;
             this.btnRight = document.querySelector('.actions').lastElementChild;
-
-            /*  card = 328;
-                gap/lastGap = 40;
-                scrollMax = (((card+gap) * (totalElementos - 3)) - lastGap);  */
-            this.scrollMax = ((368 * (this.lista.childElementCount - 3)) - 40); 
             this.scrollMin = 0;
-            
-            // Verifica se existem mais cartões que a largura máxima da lista
-            this.tooMuchCards = ((368 * this.lista.childElementCount) >= this.lista.offsetWidth) ? true : false;
 
-            if (this.tooMuchCards) {
+            // card = 328;
+            // gap/lastGap = 40;
+            // scrollMax = (((card+gap) * (totalElementos - 3)) - lastGap); 
+            this.scrollMax = (this.lista.childElementCount > 3) ? ((368 * (this.lista.childElementCount - 3)) - 40) : ((368 * this.lista.childElementCount) - 40); 
+
+            if ((368 * this.lista.childElementCount) >= this.lista.offsetWidth) {
                 // desabilida apenas o left
                 this.btnLeft.disabled = true;
             } else {
@@ -30,70 +35,118 @@ app.component('home-opportunities', {
                 this.btnLeft.disabled = true;
                 this.btnRight.disabled = true;
             }
-        }
-    },
+
+            console.log(this.lista);
+        }*/
+    }, 
 
     props: {
         select: {
             type: String,
-            default: '<id,name,shortDescription,terms,seals>'
+            default: 'id,name,shortDescription,terms,seals'
         },
         query: {
             type: Object,
             default: {"@permissions": ""}
+        },
+        allBreakpoints: {
+            type: Object
         }
     },
 
-    data() {
+    data: () => ({
+        // carousel settings
+        settings: {
+            itemsToShow: 1,
+            snapAlign: 'center',
+        },
+        // breakpoints are mobile first
+        breakpoints: {
+            1200: {
+                itemsToShow: 3.2,
+                snapAlign: "start"
+            },
+            1100: {
+                itemsToShow: 3,
+                snapAlign: "start"
+            },
+            1000: {
+                itemsToShow: 2.8,
+                snapAlign: "start"
+            },
+            900: {
+                itemsToShow: 2.6,
+                snapAlign: "start"
+            },
+            800: {
+                itemsToShow: 2.2,
+                snapAlign: "start"
+            },
+            700: {
+                itemsToShow: 2,
+                snapAlign: "start"
+            },
+            600: {
+                itemsToShow: 1.5,
+                snapAlign: "start"
+            },
+            500: {
+                itemsToShow: 1,
+                snapAlign: "start"
+            },
+        },
+    }),
+
+    /* data() {
         return {
+            leftDisabled: false,
+            rightDisabled: false,
             scroll: 0,
             lista: '',
             btnLeft: '',
             btnRight: '',
             scrollMax: 0,
-            scrollMin: 0,
-            tooMuchCards: false
+            scrollMin: 0
         }
-    },
+    }, */
 
     computed: {
-        showActions() {
-            
-        }
     },
     
     methods: {
-
-        right() {    
-            if (this.tooMuchCards) {                
+        /* right() {    
+            console.log(this.lista);
+            if ((368 * this.lista.childElementCount) >= this.lista.offsetWidth) {       
                 if (this.btnLeft.disabled == true) this.btnLeft.disabled = false;
                 
-                if (this.scroll >= this.scrollMax) {
+                if (scroll >= scrollMax) {
                     this.scroll = this.scrollMax;
                     this.btnRight.disabled = true;
                 } else {
-                    this.scroll = this.scroll + 368;
+                    this.scroll = scroll + 368;
                     this.btnRight.disabled = false;
                 }
 
-                this.lista.scroll({ left: this.scroll, behavior: 'smooth' });
+                this.lista.scroll({ left: scroll, behavior: 'smooth' });
             }
         },
 
         left() {
-            if (this.tooMuchCards) {
-                if (this.btnRight.disabled == true) this.btnRight.disabled = false;
+            console.log(this.lista);
+            if ((368 * this.lista.childElementCount) >= this.lista.offsetWidth) {
+                if (btnRight.disabled == true) btnRight.disabled = false;
 
-                if (this.scroll <= this.scrollMin) {
+                if (scroll <= scrollMin) {
                     this.scroll = this.scrollMin
                     this.btnLeft.disabled = true;
                 } else {
-                    this.scroll = this.scroll - 368;
+                    this.scroll = scroll - 368;
                     this.btnLeft.disabled = false;
                 }
 
-                this.lista.scroll({ left: this.scroll, behavior: 'smooth' });
+                this.lista.scroll({ left: scroll, behavior: 'smooth' });
             }
-        }
+        } */
+
     },
 });
--- src/protected/application/lib/modules/Home/components/home-opportunities/template.php
+++ src/protected/application/lib/modules/Home/components/home-opportunities/template.php
@@ -8,16 +8,6 @@ $this->import('entities entity-card');
     <div class="home-opportunities__content">
         <div class="home-opportunities__content--title">
             <label> <?php i::_e('Oportunidades do momento')?> </label>
-
-            <div class="actions">
-                <button class="actions--btn" @click="left()">
-                    <iconify icon="akar-icons:arrow-left"></iconify>
-                </button>
-
-                <button class="actions--btn" @click="right()">
-                    <iconify icon="akar-icons:arrow-right"></iconify>
-                </button>
-            </div>
         </div>
 
         <div class="home-opportunities__content--description">
@@ -26,32 +16,19 @@ $this->import('entities entity-card');
 
         <div class="home-opportunities__content--cards">
             <entities type="opportunity" :select="select" :query="query">
-                <template #header="{entities}">
-
-                    <div class="home-opportunities__filter">
-                        <form class="home-opportunities__filter--form" @submit="entities.refresh(); $event.preventDefault();">
-                            <input v-model="query['@keyword']" class="input" type="text" name="search" />
-                            
-                            <button class="button" type="submit">
-                                <iconify icon="ant-design:search-outlined"></iconify>
-                            </button>    
-                        </form>
-
-                        <button class="button button--primary button--icon filter">
-                            <iconify icon="ic:baseline-filter-alt"></iconify>
-                            <?php i::_e('Filtrar')?>
-                        </button>
-                    </div>
-
-                </template>
-
                 <template #default="{entities}">
-
-                    <ul class="home-opportunities__content--cards-list">
-                        <li v-for="entity in entities"> 
+                    
+                    <carousel v-if="entities.length > 0" :settings="settings" :breakpoints="breakpoints">
+                        <slide v-for="entity in entities" :key="entity.id">
                             <entity-card :entity="entity"></entity-card> 
-                        </li>
-                    </ul>
+                        </slide>                        
+
+                        <template #addons>
+                            <div class="actions">
+                                <navigation :slideWidth="368" />
+                            </div>
+                        </template>
+                    </carousel>
 
                 </template>
             </entities>
