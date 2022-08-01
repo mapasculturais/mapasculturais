9c62d0f4bbdffe4d68f4a30c466e780fc0dee383
robedj <ed.wassouf@gmail.com>
2022-08-01 12:19:31 -0400

    Criação do componente home-map. Ref.:2103

---
+++ src/protected/application/lib/modules/Home/components/home-map/template.php
@@ -0,0 +1,14 @@
+<?php
+use MapasCulturais\i;
+?>
+<div class="home-map">
+    <div class="home-map__content">
+        <label class="home-map__content--title"><?php i::_e('Visualize também no mapa') ?></label>
+        <p class="home-map__content--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum et, rhoncus semper et, nulla. 
+        </p>
+        <div class="home-map__content--mapBtn">
+            <button class="home-map__content--mapBtn-button button "><?php i::_e('Mostrar')?></iconify></button>
+
+        </div>
+    </div>
+</div>
--- src/protected/application/themes/BaseV2/views/site/index.php
+++ src/protected/application/themes/BaseV2/views/site/index.php
@@ -1,10 +1,11 @@
 <?php 
 use MapasCulturais\i;
-$this->import('home-header home-entities home-opportunities home-developers home-register');
+$this->import('home-header home-entities home-map home-opportunities home-developers home-register');
 ?>
 
 <home-header></home-header>
 <home-opportunities></home-opportunities>
 <home-register></home-register>
+<home-map></home-map>
 <home-entities></home-entities>
 <home-developers></home-developers>
