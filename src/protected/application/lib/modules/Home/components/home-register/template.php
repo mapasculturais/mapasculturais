2a9c58e13d4853353a6edf3d849da527c996569b
robedj <ed.wassouf@gmail.com>
2022-08-01 11:29:09 -0400

    Estilização do componente home register modo- web Ref.:2102

--- src/protected/application/lib/modules/Home/components/home-register/template.php
+++ src/protected/application/lib/modules/Home/components/home-register/template.php
@@ -2,11 +2,11 @@
 use MapasCulturais\i;
 ?>
 <div class="home-register">
-    
+    <img class="home-register__background" src="<?php $this->asset('img/home/mapa.jpg') ?>" />
     <div class="home-register__content">
         <label class="home-register__content--title"><?php i::_e('Faça seu cadastro e colabore com o Mapas Culturais') ?></label>
         <p class="home-register__content--description">Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Secretaria da Cultura.
         </p>
-        <button class="home-register__content--button button button--icon"><?php i::_e('Fazer Cadastro')?><iconify icon="akar-icons:arrow-right"></iconify></button>
+        <button class="home-register__content--button button button--icon button--bg"><?php i::_e('Fazer Cadastro')?><iconify icon="akar-icons:arrow-right"></iconify></button>
     </div>
 </div>
