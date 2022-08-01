15c18bb4ef084825239aa1f9b822d19c25f0f020
robedj <ed.wassouf@gmail.com>
2022-08-01 10:45:34 -0400

    Criação do componente home register Ref.:2102

--- src/protected/application/themes/BaseV2/views/site/index.php
+++ src/protected/application/themes/BaseV2/views/site/index.php
@@ -1,9 +1,10 @@
 <?php 
 use MapasCulturais\i;
-$this->import('home-header home-entities home-opportunities home-developers');
+$this->import('home-header home-entities home-opportunities home-developers home-register');
 ?>
 
 <home-header></home-header>
 <home-opportunities></home-opportunities>
 <home-entities></home-entities>
 <home-developers></home-developers>
+<home-register></home-register>
