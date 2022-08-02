0d9e9e979cf42a7634984a869de50e839397d5e1
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-02 09:00:08 -0300

    Ajuste nos componentes <home-header>, <container> e <files-list>

--- src/protected/application/lib/modules/Entities/components/entity-files-list/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-files-list/template.php
@@ -1,8 +1,14 @@
+<?php
+use MapasCulturais\i;
+?>
+
 <div v-if="entity.files.downloads" class="files-list">
     <h2> {{title}} </h2>
     <div class="files">
         <a v-for="file in entity.files.downloads" :download="file.name" :href="file.url">
-            <iconify icon="el:download-alt" /> {{file.description}}
+            <iconify icon="el:download-alt" /> 
+            <span v-if="file.description">{{file.description}}</span>
+            <span v-else> <? i::_e('Sem descrição') ?> </span>
         </a>
     </div>
 </div>
