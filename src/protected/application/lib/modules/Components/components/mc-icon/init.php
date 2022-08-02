fe6647f6e276f7ac36bd3f91e654ada7f368ab45
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-02 00:54:31 -0300

    cria componente <mc-icon> para servir de wrapper do <iconify> (refs #1880)

---
+++ src/protected/application/lib/modules/Components/components/mc-icon/init.php
@@ -0,0 +1,18 @@
+<?php
+// https://icon-sets.iconify.design/
+$iconset = [
+    'agent' => 'fa-solid:user',
+    'agent-1' => 'fa-solid:user',
+    'agent-2' => 'fa-solid:user-friends',
+    'space' => 'clarity:building-line',
+    'event' => 'ant-design:calendar-twotone',
+    'project' => 'ri:file-list-2-line',
+    'opportunity' => 'icons8:idea',
+
+    'edit' => 'zondicons:edit-pencil'
+    // @todo completar
+];
+
+$app->applyHook('component(mc-icon).iconset', [&$iconset]);
+
+$this->jsObject['config']['iconset'] = $iconset;
