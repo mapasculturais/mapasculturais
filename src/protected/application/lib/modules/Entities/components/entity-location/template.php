736f10f2e9379b65896bc5e96de1b503f9d98512
robedj <ed.wassouf@gmail.com>
2022-08-03 13:07:38 -0400

    W.I.P Ajuste no componente entity-location REF.:#2055

--- src/protected/application/lib/modules/Entities/components/entity-location/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-location/template.php
@@ -9,37 +9,39 @@ $this->import('entity-field entity-map');
         <label ><?= i::__('Endereço')?></label>
     </div>
 
-    <div v-if="editable">
-        <div class="col-4 sm:col-12">
-            <entity-field :entity="entity" prop="En_CEP"></entity-field>
-        </div>
+    <div class="col-12" v-if="editable">
+        <div class="grid-12">
+            <div class="col-4 sm:col-12">
+                <entity-field :entity="entity" prop="En_CEP"></entity-field>
+            </div>
 
-        <div class="col-8 sm:col-12">
-            <entity-field :entity="entity" prop="En_Nome_Logradouro"></entity-field>
-        </div>                        
+            <div class="col-8 sm:col-12">
+                <entity-field :entity="entity" prop="En_Nome_Logradouro"></entity-field>
+            </div>                        
 
-        <div class="col-2 sm:col-4">
-            <entity-field :entity="entity" prop="En_Num"></entity-field>
-        </div>
+            <div class="col-2 sm:col-4">
+                <entity-field :entity="entity" prop="En_Num"></entity-field>
+            </div>
 
-        <div class="col-10 sm:col-8">
-            <entity-field :entity="entity" prop="En_Bairro"></entity-field>
-        </div>
+            <div class="col-10 sm:col-8">
+                <entity-field :entity="entity" prop="En_Bairro"></entity-field>
+            </div>
 
-        <div class="col-12">
-            <entity-field :entity="entity" prop="En_Complemento"></entity-field>
-        </div>
+            <div class="col-12">
+                <entity-field :entity="entity" prop="En_Complemento"></entity-field>
+            </div>
 
-        <div class="col-6 sm:col-12">
-            <entity-field :entity="entity" prop="En_Municipio"></entity-field>
-        </div>
+            <div class="col-6 sm:col-12">
+                <entity-field :entity="entity" prop="En_Municipio"></entity-field>
+            </div>
 
-        <div class="col-6 sm:col-12">
-            <entity-field :entity="entity" prop="En_Estado"></entity-field>
-        </div>
+            <div class="col-6 sm:col-12">
+                <entity-field :entity="entity" prop="En_Estado"></entity-field>
+            </div>
 
-        <div class="col">
-            <entity-field :entity="entity" prop="publicLocation"></entity-field>
+            <div class="col-6">
+                <entity-field :entity="entity" prop="publicLocation"></entity-field>
+            </div>
         </div>
     </div>
 
