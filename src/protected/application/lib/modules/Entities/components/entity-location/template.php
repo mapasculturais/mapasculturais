634853c6111dfca85e8070bc7ea2aa906e498089
robedj <ed.wassouf@gmail.com>
2022-08-03 12:50:58 -0400

    Ajuste no componente entity-location e criação do ReadMe REF.:#2055

--- src/protected/application/lib/modules/Entities/components/entity-location/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-location/template.php
@@ -8,37 +8,39 @@ $this->import('entity-field entity-map');
     <div class="entity-location__title col-12">
         <label ><?= i::__('Endereço')?></label>
     </div>
-    
-    <div class="col-4 sm:col-12">
-        <entity-field :entity="entity" prop="En_CEP"></entity-field>
-    </div>
 
-    <div class="col-8 sm:col-12">
-        <entity-field :entity="entity" prop="En_Nome_Logradouro"></entity-field>
-    </div>                        
+    <div v-if="editable">
+        <div class="col-4 sm:col-12">
+            <entity-field :entity="entity" prop="En_CEP"></entity-field>
+        </div>
 
-    <div class="col-2 sm:col-4">
-        <entity-field :entity="entity" prop="En_Num"></entity-field>
-    </div>
+        <div class="col-8 sm:col-12">
+            <entity-field :entity="entity" prop="En_Nome_Logradouro"></entity-field>
+        </div>                        
 
-    <div class="col-10 sm:col-8">
-        <entity-field :entity="entity" prop="En_Bairro"></entity-field>
-    </div>
+        <div class="col-2 sm:col-4">
+            <entity-field :entity="entity" prop="En_Num"></entity-field>
+        </div>
 
-    <div class="col-12">
-        <entity-field :entity="entity" prop="En_Complemento"></entity-field>
-    </div>
+        <div class="col-10 sm:col-8">
+            <entity-field :entity="entity" prop="En_Bairro"></entity-field>
+        </div>
 
-    <div class="col-6 sm:col-12">
-        <entity-field :entity="entity" prop="En_Municipio"></entity-field>
-    </div>
+        <div class="col-12">
+            <entity-field :entity="entity" prop="En_Complemento"></entity-field>
+        </div>
 
-    <div class="col-6 sm:col-12">
-        <entity-field :entity="entity" prop="En_Estado"></entity-field>
-    </div>
+        <div class="col-6 sm:col-12">
+            <entity-field :entity="entity" prop="En_Municipio"></entity-field>
+        </div>
 
-    <div class="col">
-        <entity-field :entity="entity" prop="publicLocation"></entity-field>
+        <div class="col-6 sm:col-12">
+            <entity-field :entity="entity" prop="En_Estado"></entity-field>
+        </div>
+
+        <div class="col">
+            <entity-field :entity="entity" prop="publicLocation"></entity-field>
+        </div>
     </div>
 
     <div class="col-12">
