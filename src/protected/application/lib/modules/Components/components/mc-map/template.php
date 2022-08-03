830cd06b4079658750a57f82140507c09e8432bd
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-02 23:29:43 -0300

    adiciona efeitos de animação no zoom do mapa no componente <mc-map> (Refs #2056)

--- src/protected/application/lib/modules/Components/components/mc-map/template.php
+++ src/protected/application/lib/modules/Components/components/mc-map/template.php
@@ -4,9 +4,9 @@ use MapasCulturais\i;
 $this->import('mc-map-marker');
 ?>
 <div style="height: 400px;">
-    <l-map v-model="defaultZoom" v-model:zoom="defaultZoom" :center="center">
+    <l-map v-model="defaultZoom" v-model:zoom="defaultZoom" :center="center" zoom-animation fade-animation>
         <l-tile-layer :url="tileServer"></l-tile-layer>
-        <l-control-layers />
+        
         <slot></slot>
     </l-map>
 </div>
