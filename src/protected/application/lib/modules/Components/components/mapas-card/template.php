1b534fc164e8cf22e2a3a48377f412a9447b65a3
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-02 23:28:30 -0300

    adiciona o slot padrão ao componente mapas-card (refs #1880)

--- src/protected/application/lib/modules/Components/components/mapas-card/template.php
+++ src/protected/application/lib/modules/Components/components/mapas-card/template.php
@@ -1,8 +1,9 @@
 <div class="mapas-card">
     <div class="mapas-card__title">
-        <slot name="title" />
+        <slot name="title"></slot>
     </div>
     <div class="mapas-card__content">
-        <slot name="content" />
+        <slot></slot>
+        <slot name="content"></slot>
     </div>
 </div>
