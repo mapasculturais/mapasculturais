375afe7aeed08d5cd3192f73e58087d75507e3a2
robedj <ed.wassouf@gmail.com>
2022-08-04 12:48:17 -0400

    Ajustes no pin do Mapa do componente ref.:#2055

--- src/protected/application/lib/modules/Components/components/mc-map-marker/template.php
+++ src/protected/application/lib/modules/Components/components/mc-map-marker/template.php
@@ -6,7 +6,7 @@ $this->import('mc-icon');
 
 <l-marker :lat-lng="entity.location" :draggable="draggable" @moveend="moved($event)">
     <l-icon>
-        <div style="background-color: brown; padding: 5px; border-radius: 10px;">
+        <div :class="[entity.__objectType+'__background', 'mc-map-marker']">
             <mc-icon :entity="entity"></mc-icon>
         </div>
     </l-icon>
