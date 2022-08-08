116ac14cf123215c7db8899d6b96fd6d7978b64d
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-08 10:06:39 -0300

    Utilização do componente <home-feature> na homepage - #2099

--- src/protected/application/themes/BaseV2/views/site/index.php
+++ src/protected/application/themes/BaseV2/views/site/index.php
@@ -1,11 +1,12 @@
 <?php 
 use MapasCulturais\i;
-$this->import('home-header home-entities home-map home-opportunities home-developers home-register');
+$this->import('home-header home-entities home-map home-opportunities home-developers home-register home-feature');
 ?>
 
 <home-header></home-header>
 <home-opportunities></home-opportunities>
+<home-entities></home-entities>
+<home-feature></home-feature>
 <home-register></home-register>
 <home-map></home-map>
-<home-entities></home-entities>
 <home-developers></home-developers>
