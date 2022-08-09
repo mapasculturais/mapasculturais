4ea9c1f0db10a04e4d0dff8901619e5d390d2a17
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-09 13:07:15 -0300

    Aplica filtros no componente <home-opportunities> - #2098

--- src/protected/application/lib/modules/Home/components/home-opportunities/script.js
+++ src/protected/application/lib/modules/Home/components/home-opportunities/script.js
@@ -14,71 +14,94 @@ app.component('home-opportunities', {
         return { text }
     },
 
-    created() { 
-        
-    }, 
-
     props: {
-        select: {
-            type: String,
-            default: 'id,name,shortDescription,terms,seals'
-        },
-        query: {
-            type: Object,
-            default: {"@permissions": ""}
-        },
-        allBreakpoints: {
-            type: Object
+    },
+
+    data() {
+        return {
+            // query
+            query: {
+                '@order' : 'registrationFrom ASC',
+                '@select' : 'id,name,shortDescription,terms,seals',
+                '@verified' : '1'
+            },
+
+            // carousel settings
+            settings: {
+                itemsToShow: 1,
+                snapAlign: 'center',
+            },
+
+            // breakpoints are mobile first
+            breakpoints: {
+                1200: {
+                    itemsToShow: 3.2,
+                    snapAlign: "start"
+                },
+                1100: {
+                    itemsToShow: 3,
+                    snapAlign: "start"
+                },
+                1000: {
+                    itemsToShow: 2.8,
+                    snapAlign: "start"
+                },
+                900: {
+                    itemsToShow: 2.6,
+                    snapAlign: "start"
+                },
+                800: {
+                    itemsToShow: 2.2,
+                    snapAlign: "start"
+                },
+                700: {
+                    itemsToShow: 2,
+                    snapAlign: "start"
+                },
+                600: {
+                    itemsToShow: 1.5,
+                    snapAlign: "start"
+                },
+                500: {
+                    itemsToShow: 1,
+                    snapAlign: "start"
+                },
+            }
         }
     },
 
-    data: () => ({
-        // carousel settings
-        settings: {
-            itemsToShow: 1,
-            snapAlign: 'center',
-        },
-        // breakpoints are mobile first
-        breakpoints: {
-            1200: {
-                itemsToShow: 3.2,
-                snapAlign: "start"
-            },
-            1100: {
-                itemsToShow: 3,
-                snapAlign: "start"
-            },
-            1000: {
-                itemsToShow: 2.8,
-                snapAlign: "start"
-            },
-            900: {
-                itemsToShow: 2.6,
-                snapAlign: "start"
-            },
-            800: {
-                itemsToShow: 2.2,
-                snapAlign: "start"
-            },
-            700: {
-                itemsToShow: 2,
-                snapAlign: "start"
-            },
-            600: {
-                itemsToShow: 1.5,
-                snapAlign: "start"
-            },
-            500: {
-                itemsToShow: 1,
-                snapAlign: "start"
-            },
-        },
-    }),
-
     computed: {
+        getQuery() {
+            this.query['registrationFrom'] = 'LTE('+this.futureDate()+')';
+            this.query['registrationTo'] = 'GTE('+this.actualDate()+')';
+            
+
+            console.log(this.query);
+
+            return this.query;
+        }
     },
     
     methods: {
-    }
+        actualDate() {
+            var data = new Date();
+            var dia = String(data.getDate()).padStart(2, '0');
+            var mes = String(data.getMonth() + 1).padStart(2, '0');
+            var ano = data.getFullYear();
 
+            return (ano + '-' + mes + '-' + dia);
+        },
+
+        futureDate() {
+            var date = this.actualDate();
+            var futureDate = new Date(date.replace(/\-/gi, ', '));
+            futureDate.setMonth(futureDate.getMonth() + (1));
+
+            var dia = String(futureDate.getDate()).padStart(2, '0');
+            var mes = String(futureDate.getMonth() + 1).padStart(2, '0');
+            var ano = futureDate.getFullYear();
+
+            return (ano + '-' + mes + '-' + dia);
+        }
+    },
 });
