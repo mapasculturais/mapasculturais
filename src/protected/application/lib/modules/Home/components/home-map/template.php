4972feff7dd8f0c3cac459529c341126929bd294
robedj <ed.wassouf@gmail.com>
2022-08-09 09:03:09 -0400

    Refatoração no componente home-map Ref.:#2103

--- src/protected/application/lib/modules/Home/components/home-map/template.php
+++ src/protected/application/lib/modules/Home/components/home-map/template.php
@@ -8,17 +8,8 @@ $this->import('mc-map-marker mc-map entities');
     <div class="home-map__content">
         <label class="home-map__content--title"><?php i::_e('Visualize também no mapa') ?></label>
         <p class="home-map__content--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum et, rhoncus semper et, nulla. </p>
-            
-            <div v-for="entity in allEntities">
-                    {{entity.id}}
-            </div>
-        <!-- <mc-map>
-            
-            <div v-for="(entity, index) in allEntities">
-                <mc-map-marker :entity="entity"></mc-map-marker>
-            
-            </div>
-
-        </mc-map> -->
+        <mc-map>
+                <mc-map-marker v-for="entity in entities" :entity="entity"></mc-map-marker>
+        </mc-map>
     </div>
 </div>
