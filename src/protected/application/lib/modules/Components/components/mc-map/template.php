4fccb888489ac0db17a64ef7088c9e5d4477a7d6
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-09 17:52:35 -0300

    Ajustes gerais de sobreposição

--- src/protected/application/lib/modules/Components/components/mc-map/template.php
+++ src/protected/application/lib/modules/Components/components/mc-map/template.php
@@ -3,7 +3,7 @@ use MapasCulturais\i;
 
 $this->import('mc-map-marker');
 ?>
-<div style="height: 400px;">
+<div class="mc-map">
     <l-map v-model="defaultZoom" v-model:zoom="defaultZoom" :center="center" zoom-animation fade-animation>
         <l-tile-layer :url="tileServer"></l-tile-layer>
         
