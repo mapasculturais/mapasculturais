b4cc2acffe33a2bae298662e078a989b8aba9126
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-09 13:08:09 -0300

    Aplica filtros no componente <home-feature> - #2099

--- src/protected/application/lib/modules/Home/components/home-feature/script.js
+++ src/protected/application/lib/modules/Home/components/home-feature/script.js
@@ -14,66 +14,76 @@ app.component('home-feature', {
         return { text }
     },
 
-    props: {
-        select: {
-            type: String,
-            default: 'id,name,shortDescription,terms,files,seals,classificacaoEtaria,terms'
-        },
-        query: {
-            type: Object,
-            default: {"@permissions": ""}
-        },
-        allBreakpoints: {
-            type: Object
+    created(){
+
+        this.spaceAPI = new API('space');
+        this.agentAPI = new API('agent');
+        this.findEntities();
+    },
+    
+    data() {
+        return{
+            agents: [],
+            spaces: [],
+
+            // carousel settings
+            settings: {
+                itemsToShow: 1,
+                snapAlign: 'center',
+            },
+
+            // breakpoints are mobile first
+            breakpoints: {
+                1200: {
+                    itemsToShow: 3.2,
+                    snapAlign: "start"
+                },
+                1100: {
+                    itemsToShow: 3,
+                    snapAlign: "start"
+                },
+                1000: {
+                    itemsToShow: 2.8,
+                    snapAlign: "start"
+                },
+                900: {
+                    itemsToShow: 2.6,
+                    snapAlign: "start"
+                },
+                800: {
+                    itemsToShow: 2.2,
+                    snapAlign: "start"
+                },
+                700: {
+                    itemsToShow: 2,
+                    snapAlign: "start"
+                },
+                600: {
+                    itemsToShow: 1.5,
+                    snapAlign: "start"
+                },
+                500: {
+                    itemsToShow: 1,
+                    snapAlign: "start"
+                },
+            }
         }
     },
 
-    data: () => ({
-        // carousel settings
-        settings: {
-            itemsToShow: 1,
-            snapAlign: 'center',
-        },
-        // breakpoints are mobile first
-        breakpoints: {
-            1200: {
-                itemsToShow: 3.2,
-                snapAlign: "start"
-            },
-            1100: {
-                itemsToShow: 3,
-                snapAlign: "start"
-            },
-            1000: {
-                itemsToShow: 2.8,
-                snapAlign: "start"
-            },
-            900: {
-                itemsToShow: 2.6,
-                snapAlign: "start"
-            },
-            800: {
-                itemsToShow: 2.2,
-                snapAlign: "start"
-            },
-            700: {
-                itemsToShow: 2,
-                snapAlign: "start"
-            },
-            600: {
-                itemsToShow: 1.5,
-                snapAlign: "start"
-            },
-            500: {
-                itemsToShow: 1,
-                snapAlign: "start"
-            },
-        },
-    }),
-
     computed: {
+        entities() {
+            const entities = this.spaces.concat(this.agents);
+            return entities;
+        }
     },
     
     methods: {
+        async findEntities() {
+            const query = {
+                '@select': 'id,name,shortDescription,location,terms,seals',
+            }
+            this.spaces = await this.spaceAPI.find(query);
+            this.agents = await this.agentAPI.find(query);
+        }  
     },
 });
