f75e016d933de89c7e4c350a19120a40a24900cf
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-09 17:52:10 -0300

    busca do endereço por CEP e criação do entity.endereco - #2055

--- src/protected/application/lib/modules/Entities/components/entity-location/script.js
+++ src/protected/application/lib/modules/Entities/components/entity-location/script.js
@@ -8,6 +8,11 @@ app.component('entity-location', {
         return { hasSlot }
     },
 
+    updated() {
+        this.address();
+        console.log('teste');
+    },
+
     props: {
         entity: {
             type: Entity,
@@ -19,4 +24,38 @@ app.component('entity-location', {
         }        
         
     },
+
+    methods: {
+        address() {
+            var address = '';        
+            address += this.entity.En_Nome_Logradouro;
+            address += address != '' && this.entity.En_Complemento  ? ', '+this.entity.En_Complemento : this.entity.En_Complemento;
+            address += address != '' && this.entity.En_Bairro       ? ', '+this.entity.En_Bairro : this.entity.En_Bairro;
+            address += address != '' && this.entity.En_Municipio    ? ', '+this.entity.En_Municipio : this.entity.En_Municipio;
+            address += address != '' && this.entity.En_Num          ? ', '+this.entity.En_Num : this.entity.En_Num;
+            address += address != '' && this.entity.En_Estado       ? ', '+this.entity.En_Estado : this.entity.En_Estado;
+            address += address != '' && this.entity.En_CEP          ? ', '+this.entity.En_CEP : this.entity.En_CEP;
+           
+            this.entity.endereco = address;
+        },
+
+        pesquisacep(valor) {
+            //Nova variável "cep" somente com dígitos.
+            var cep = valor.replace(/\D/g, '');                
+            if (cep != "") {
+                var validacep = /^[0-9]{8}$/;   
+                if(validacep.test(cep)) {     
+                    fetch('//viacep.com.br/ws/'+ cep +'/json/')
+                        .then((response) => response.json())
+                        .then((data) => {
+                            this.entity.En_Nome_Logradouro = data.logradouro;
+                            this.entity.En_Bairro = data.bairro;
+                            this.entity.En_Municipio = data.localidade;
+                            this.entity.En_Estado = data.uf;
+                            this.address();
+                        });    
+                } 
+            } 
+        }
+    }
 });
--- src/protected/application/lib/modules/Entities/components/entity-location/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-location/template.php
@@ -12,47 +12,42 @@ $this->import('entity-field entity-map');
     <div class="col-12" v-if="editable">
         <div class="grid-12">
             <div class="col-4 sm:col-12">
-                <entity-field :entity="entity" prop="En_CEP"></entity-field>
+                <entity-field @change="address(); pesquisacep(entity.En_CEP);" :entity="entity" prop="En_CEP"></entity-field>
             </div>
 
             <div class="col-8 sm:col-12">
-                <entity-field :entity="entity" prop="En_Nome_Logradouro"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Nome_Logradouro"></entity-field>
             </div>                        
 
             <div class="col-2 sm:col-4">
-                <entity-field :entity="entity" prop="En_Num"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Num"></entity-field>
             </div>
 
             <div class="col-10 sm:col-8">
-                <entity-field :entity="entity" prop="En_Bairro"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Bairro"></entity-field>
             </div>
 
             <div class="col-12">
-                <entity-field :entity="entity" prop="En_Complemento"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Complemento"></entity-field>
             </div>
 
             <div class="col-6 sm:col-12">
-                <entity-field :entity="entity" prop="En_Municipio"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Municipio"></entity-field>
             </div>
 
             <div class="col-6 sm:col-12">
-                <entity-field :entity="entity" prop="En_Estado"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="En_Estado"></entity-field>
             </div>
 
             <div class="col-6">
-                <entity-field :entity="entity" prop="publicLocation"></entity-field>
+                <entity-field @change="address()" :entity="entity" prop="publicLocation"></entity-field>
             </div>
         </div>
     </div>
 
     <div class="col-12">
         <p class="entity-location__address">
-            <span v-if="entity.En_Nome_Logradouro">{{entity.En_Nome_Logradouro}}, </span>
-            <span v-if="entity.En_Num">{{entity.En_Num}}, </span>
-            <span v-if="entity.En_Bairro">{{entity.En_Bairro}}. </span>
-            <span v-if="entity.En_Municipio"> {{entity.En_Municipio}}/</span>
-            <span v-if="entity.En_Estado">{{entity.En_Estado}} - </span>            
-            <span v-if="entity.En_CEP">CEP: {{entity.En_CEP}}. </span>
+            <span>{{entity.endereco}}</span>
         </p>
         <entity-map :entity="entity" :editable="editable"></entity-map>
     </div>
