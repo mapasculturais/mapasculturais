9c8175ebad41521d851548a405be40dfab909b2e
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-10 20:58:15 -0300

    refatoração do componente home-map para utilizar clusterizar os marcadores (refs #2129 e #2103)

--- src/protected/application/lib/modules/Home/components/home-map/script.js
+++ src/protected/application/lib/modules/Home/components/home-map/script.js
@@ -1,24 +1,31 @@
 app.component('home-map', {
     template: $TEMPLATES['home-map'],
     
-    created(){
+    async created(){
 
         this.spaceAPI = new API('space');
         this.agentAPI = new API('agent');
-        this.findEntities();
+        
+        const query = {
+            '@select': 'id,name,location',
+            '@verified': 1
+        }
+        this.spaces = await this.spaceAPI.find(query);
+        this.agents = await this.agentAPI.find(query);
     },
     
     data() {
-        return{
-            agents: [],
-            spaces: [],
+        return {
+            agents: Vue.shallowReactive([]),
+            spaces: Vue.shallowReactive([]),
         }
     },
 
     computed: {
         entities() {
             const entities = this.spaces.concat(this.agents);
-            return entities;
+             
+            return Vue.shallowReactive(entities);
         }
         // captar eventos
         // concatenar no foreach usando push
@@ -28,12 +35,5 @@ app.component('home-map', {
     },
 
     methods: {
-        async findEntities() {
-            const query = {
-                '@select': 'id,name,shortDescription,location,terms,seals',
-            }
-            this.spaces = await this.spaceAPI.find(query);
-            this.agents = await this.agentAPI.find(query);
-        }
     },
 });
