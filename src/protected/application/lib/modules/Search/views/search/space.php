44701b339929cc46a9704eabfee91e6b4a601274
robedj <ed.wassouf@gmail.com>
2022-08-10 11:51:35 -0400

    Criação do esqueleto das páginas de buscas das entidades Ref.:#2134

--- src/protected/application/lib/modules/Search/views/search/space.php
+++ src/protected/application/lib/modules/Search/views/search/space.php
@@ -4,14 +4,14 @@ use MapasCulturais\i;
 $this->import('mapas-breadcrumb tabs search-header mc-map create-agent');
 $this->breadcramb = [
     ['label'=> i::__('Inicio'), 'url' => $app->createUrl('index')],
-    ['label'=> i::__('Agentes'), 'url' => $app->createUrl('agents')],
+    ['label'=> i::__('Espaços'), 'url' => $app->createUrl('spaces')],
 ];
 ?>
 
 
-<div class="agent">
+<div class="space">
     <mapas-breadcrumb></mapas-breadcrumb>
-        <search-header type="agent">
+        <search-header type="space">
             <template #create>
                 <create-agent></create-agent>
             </template>
