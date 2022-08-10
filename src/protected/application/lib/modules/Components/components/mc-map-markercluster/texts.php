b464a45166c31d540b3112078b66f90c6dcc79a4
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-10 20:51:47 -0300

    arquivo de internacionalização do componente mc-map-markercluster

--- src/protected/application/lib/modules/Components/components/mc-map-markercluster/texts.php
+++ src/protected/application/lib/modules/Components/components/mc-map-markercluster/texts.php
@@ -1,7 +1,4 @@
 <?php
 use MapasCulturais\i;
 
-return [
-    'texto localizado' => i::__('Aqui o texto localizado que será exibido'),
-    'outro texto localizado' => i::__('Outro texto localizado')
-];
+return [];
