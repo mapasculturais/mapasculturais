40f47229ca2fb334648027548385eac6a91d3c0b
robedj <ed.wassouf@gmail.com>
2022-08-11 12:42:14 -0400

    Ajustes no esqueleto das páginas de busca  Ref.:#2134

--- src/protected/application/lib/modules/Search/views/search/space.php
+++ src/protected/application/lib/modules/Search/views/search/space.php
@@ -8,13 +8,34 @@ $this->breadcramb = [
 ];
 ?>
 
-
-<div class="space">
+<div class="search">
     <mapas-breadcrumb></mapas-breadcrumb>
-        <search-header type="space">
-            <template #create>
-                <create-agent></create-agent>
+
+    <search-header class="search__header" type="space">
+        <template #create>
+            <create-agent></create-agent>
+        </template>
+        <template #actions>
+                <tabs class="search__header--tabs">
+                    <template  #before-tablist>
+                        <label class="search__header--tabs-label">
+                        Visualizar como:
+                        </label> 
+                    </template>
+                    
+                    <tab icon="list" label="Lista" slug="primary">
+                        <h2>Conteúdo principal</h2>
+                    </tab>
+                    <tab icon="map" label="Mapa" slug="secondary">
+                        <h2>Conteúdo secundário</h2>
+                    </tab>
+                    
+                    <template #after-tablist>
+                       <div class="search__header--tabs-filter">
+                           <input type="text"/>
+                       </div>
+                    </template>
+                </tabs>
             </template>
-            
-        </search-header>
+    </search-header>
 </div>
