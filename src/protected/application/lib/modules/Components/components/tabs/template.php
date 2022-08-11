e2ac3895398052726aa7ff73a7a3f2e8b5b49247
robedj <ed.wassouf@gmail.com>
2022-08-11 12:40:55 -0400

    Ajustes no template  do componente tabs  Ref.:#2134

--- src/protected/application/lib/modules/Components/components/tabs/template.php
+++ src/protected/application/lib/modules/Components/components/tabs/template.php
@@ -3,26 +3,31 @@ $this->import('tab');
 ?>
 <div class="tabs-component">
     <div class="tabs-component__header">
-        <slot name="before-tablist"></slot>
-        <ul class="tabs-component__buttons" role="tablist">
-            <li v-for="tab in tabs" :key="tab.slug"
-                class="tabs-component__button"
-                :class="[tab.slug, tab.disabled && 'tabs-component__button--disabled', isActive(tab) && 'tabs-component__button--active']"
-                role="presentation">
-                <a
-                    :aria-controls="tab.hash"
-                    :aria-selected="isActive(tab)"
-                    :href="tab.hash"
-                    role="tab"
-                    @click="selectTab(tab.slug, $event)">
-                    <slot name="header" :tab="tab">
-                        <mc-icon v-if="tab.icon" :name="tab.icon"></mc-icon>
-                        <span>{{ tab.label }}</span>
-                    </slot>
-                </a>
-            </li>
-        </ul>
-        <slot name="after-tablist"></slot>
+        <div class="tabs-component__header--left">
+            <slot name="before-tablist"></slot>
+            <ul class="tabs-component__buttons" role="tablist">
+                <li v-for="tab in tabs" :key="tab.slug"
+                    class="tabs-component__button"
+                    :class="[tab.slug, tab.disabled && 'tabs-component__button--disabled', isActive(tab) && 'tabs-component__button--active']"
+                    role="presentation">
+                    <a
+                        :aria-controls="tab.hash"
+                        :aria-selected="isActive(tab)"
+                        :href="tab.hash"
+                        role="tab"
+                        @click="selectTab(tab.slug, $event)">
+                        <slot name="header" :tab="tab">
+                            <mc-icon v-if="tab.icon" :name="tab.icon"></mc-icon>
+                            <span>{{ tab.label }}</span>
+                        </slot>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="tabs-component__header--right">
+            <slot name="after-tablist"></slot>
+        </div>
     </div>
     <div class="tabs-component__panels">
         <slot></slot>
