8d451ac9828ee472a28edcbb331bc129f00b575d
erleibiazzio <erleibiazzio@gmail.com>
2022-08-12 13:05:26 -0300

    Ajusta definition para se poder trabalhar com os novos campos de período de avaliação Ref.: #2112

--- src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations--config.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/opportunity-evaluations--config.php
@@ -3,21 +3,25 @@ use MapasCulturais\i;
 
 $configuration = $entity->evaluationMethodConfiguration;
 $definition = $configuration->definition;
+$propertiesMetadata = $configuration->getPropertiesMetadata();
+$this->jsObject['entity']['definition']['evaluationFrom']  = $propertiesMetadata['evaluationFrom'];
+$this->jsObject['entity']['definition']['evaluationTo']  = $propertiesMetadata['evaluationTo'];
+
 ?>
 
 <div id="evaluations-config" class="aba-content" ng-controller="EvaluationMethodConfigurationController">
 
-    <div class="highlighted-message clearfix" id="opportunity-main-info">
+    <div class="highlighted-message clearfix">
         <div class="registration-dates clear">
             <?php /* Translators: "de" como início de um intervalo de data *DE* 25/1 a 25/2 às 13:00 */ ?>
             <?php \MapasCulturais\i::_e("Avaliações abertas de");?>
-            <strong class="js-editable" data-type="date" data-yearrange="2000:+25" data-viewformat="dd/mm/yyyy" data-edit="evaluateFrom" <?php echo $entity->evaluateFrom ? "data-value='" . $entity->evaluateFrom->format('Y-m-d') . "'" : ' '?> data-showbuttons="false" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Data inicial");?>"><?php echo $entity->evaluateFrom ? $entity->evaluateFrom->format('d/m/Y') : \MapasCulturais\i::__("Data inicial"); ?></strong>
+            <strong class="js-editable" data-type="date" data-yearrange="2000:+25" data-viewformat="dd/mm/yyyy" data-edit="evaluationFrom" <?php echo $configuration->evaluationFrom ? "data-value='" . $configuration->evaluationFrom->format('Y-m-d') . "'" : ' '?> data-showbuttons="false" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Data inicial");?>"><?php echo $configuration->evaluationFrom ? $configuration->evaluationFrom->format('d/m/Y') : \MapasCulturais\i::__("Data inicial"); ?></strong>
             <?php /* Translators: "a" indicando intervalo de data de 25/1 *A* 25/2 às 13:00 */ ?>
             <?php \MapasCulturais\i::_e("a");?>
-            <strong class="js-editable" data-type="date" data-yearrange="2000:+25" data-viewformat="dd/mm/yyyy" data-edit="evaluateTo" <?php echo $entity->evaluateTo ? "data-value='".$entity->evaluateTo->format('Y-m-d') . "'" : ''?> data-timepicker="#evaluateTo_time" data-showbuttons="false" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Data final");?>"><?php echo $entity->evaluateTo ? $entity->evaluateTo->format('d/m/Y') : \MapasCulturais\i::__("Data final"); ?></strong>
+            <strong class="js-editable" data-type="date" data-yearrange="2000:+25" data-viewformat="dd/mm/yyyy" data-edit="evaluationTo" <?php echo $configuration->evaluationTo ? "data-value='".$configuration->evaluationTo->format('Y-m-d') . "'" : ''?> data-timepicker="#evaluationTo_time" data-showbuttons="false" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Data final");?>"><?php echo $configuration->evaluationTo ? $configuration->evaluationTo->format('d/m/Y') : \MapasCulturais\i::__("Data final"); ?></strong>
             <?php /* Translators: "às" indicando horário de data de 25/1 a 25/2 *ÀS* 13:00 */ ?>
             <?php \MapasCulturais\i::_e("às");?>
-            <strong class="js-editable" id="evaluateTo_time" data-datetime-value="<?php echo $entity->evaluateTo ? $entity->evaluateTo->format('Y-m-d H:i') : ''; ?>" data-placeholder="<?php \MapasCulturais\i::esc_attr_e("Hora final");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Hora final");?>"><?php echo $entity->evaluateTo ? $entity->evaluateTo->format('H:i') : ''; ?></strong>
+            <strong class="js-editable" id="evaluationTo_time" data-datetime-value="<?php echo $configuration->evaluationTo ? $configuration->evaluationTo->format('Y-m-d H:i') : ''; ?>" data-placeholder="<?php \MapasCulturais\i::esc_attr_e("Hora final");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Hora final");?>"><?php echo $configuration->evaluationTo ? $configuration->evaluationTo->format('H:i') : ''; ?></strong>
         </div>
     </div>
     
