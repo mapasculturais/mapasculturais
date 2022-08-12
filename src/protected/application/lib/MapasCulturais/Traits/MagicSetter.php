a6d9b970288cc5b845af9462e304a81e8fd545f4
erleibiazzio <erleibiazzio@gmail.com>
2022-08-12 17:48:12 -0300

    Faz com que seja possível criar setters via hook

--- src/protected/application/lib/MapasCulturais/Traits/MagicSetter.php
+++ src/protected/application/lib/MapasCulturais/Traits/MagicSetter.php
@@ -1,6 +1,8 @@
 <?php
 namespace MapasCulturais\Traits;
 
+use MapasCulturais\App;
+
 /**
  * Defines the magic setter method the be used when trying to set a protected or private property.
  *
@@ -13,6 +15,8 @@ trait MagicSetter{
      * else if the property name doesn't starts with an undercore set the value of the property directly.
      */
     public function __set($name, $value){
+
+        
         if(method_exists($this, 'set' . $name)){
             $setter = 'set' . $name;
             $this->$setter($value);
@@ -26,9 +30,20 @@ trait MagicSetter{
             return $this->__metadata__set($name, $value);
 
         }elseif($name[0] !== '_'){
+
+            if ($name != 'hookClassPath' && $name != 'hookPrefix' && $this->__enableMagicSetterHook ?? false) {
+                $app = App::i();
+                $hookPrefix = self::getHookPrefix();
+                $hook_name =  "{$hookPrefix}.set({$name})";
+                $app->applyHookBoundTo($this, $hook_name, [&$value, $name]);
+            }
+
             $this->$name = $value;
             return true;
         }
+
+       
+
     }
 
     /**
