2f21c83aa7e25832e97f4b67cfb88fd8cd3cafe1
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-15 13:55:34 -0300

    remove textos incompletos no search-list

--- src/protected/application/lib/modules/Search/components/search-list/texts.php
+++ src/protected/application/lib/modules/Search/components/search-list/texts.php
@@ -1,9 +1,5 @@
 <?php
 use MapasCulturais\i;
 
-return [
-    'spaceText' => i::__('Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.'),
-    'agentLabel' => i::__(),
-    'filterLabel'=> i::__(),
-    
+return [    
 ];
