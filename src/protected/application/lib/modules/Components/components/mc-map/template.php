aa1ddec565f2acd254fcb6da538e7f8b6cb5bb61
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-17 01:44:50 -0300

    implementação do clustermarker no componente mc-map (refs: #2056 e #2119)

--- src/protected/application/lib/modules/Components/components/mc-map/template.php
+++ src/protected/application/lib/modules/Components/components/mc-map/template.php
@@ -1,5 +1,6 @@
 <?php
 use MapasCulturais\i;
+$this->import('mc-icon')
 ?>
 <div class="mc-map">
     <l-map 
@@ -13,7 +14,20 @@ use MapasCulturais\i;
         use-global-leaflet
         @ready="handleMapSetup()">
         <l-tile-layer :url="tileServer"></l-tile-layer>
-        
         <slot></slot>
+
+        <div ref="agent1" class="agent__background mc-map-marker">
+            <mc-icon name="agent-1"></mc-icon>
+        </div>
+        <div ref="agent2" class="agent__background mc-map-marker">
+            <mc-icon name="agent-2"></mc-icon>
+        </div>
+        <div ref="space" class="space__background mc-map-marker">
+            <mc-icon name="space"></mc-icon>
+        </div>
+        <div ref="event" class="event__background mc-map-marker">
+            <mc-icon name="event"></mc-icon>
+        </div>        
     </l-map>
 </div>
+
