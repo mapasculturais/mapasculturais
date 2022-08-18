459ff93a52ed344f24c0e18bdf810e51bc154b39
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-17 22:16:23 -0300

    remoção do construct sem uso no controller do search

--- src/protected/application/lib/modules/Search/Controller.php
+++ src/protected/application/lib/modules/Search/Controller.php
@@ -4,25 +4,27 @@ namespace Search;
 
 class Controller extends \MapasCulturais\Controller
 {
-    function __construct()
-    {
-    }
     
     function GET_index() {
         $this->render('index', []);
     }
+
     function GET_agents() {
         $this->render('agent', []);
     }
+    
     function GET_events() {
         $this->render('event', []);
     }
+
     function GET_projects() {
         $this->render('project', []);
     }
+
     function GET_opportunities() {
         $this->render('opportunity', []);
     }
+
     function GET_spaces() {
         $this->render('space', []);
     }
