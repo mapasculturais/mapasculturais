98e9728bd34c160bceca08d208b3cb946e9f41a7
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-18 13:17:56 -0300

    Ajusta página de busca do agente

--- src/protected/application/lib/modules/Search/views/search/agent.php
+++ src/protected/application/lib/modules/Search/views/search/agent.php
@@ -1,46 +1,82 @@
 <?php 
 use MapasCulturais\i;
  
-$this->import('mapas-breadcrumb mapas-card mapas-container search-map search-header search-list tabs create-agent');
+$this->import('
+    search tabs search-list search-map search-filter-agent create-agent
+    ');
 $this->breadcramb = [
     ['label'=> i::__('Inicio'), 'url' => $app->createUrl('index')],
     ['label'=> i::__('Agentes'), 'url' => $app->createUrl('agents')],
 ];
 ?>
 
+<search page-title="Agentes" entity-type="agent">    
 
-<div class="search">
-    <mapas-breadcrumb></mapas-breadcrumb>
-        <search-header class="search__header" type="agent">
-            <template #create>
-                <create-agent></create-agent>
+    <template #create-button>
+        <create-agent></create-agent>
+    </template>
+
+    <template #default="{query, api}">
+        <tabs class="search__tabs">
+            <template  #before-tablist>
+                <label class="search__tabs--before">
+                    Visualizar como:
+                </label> 
             </template>
-            <template #actions>
-                <tabs class="search__header--tabs">
-                    <template  #before-tablist>
-                        <label class="search__header--tabs-label">
-                        Visualizar como:
-                        </label> 
-                    </template>
-                    
-                    <tab icon="list" label="Lista" slug="list">
-                        <mapas-container class="search-list">
-                            <search-list type="agent"></search-list>
-                        </mapas-container>
-                    </tab>
+            
+            <tab icon="list" label="Lista" slug="list">
+                <div class="search__tabs--list">
 
-                    <tab icon="map" label="Mapa" slug="map">
-                        <search-map type="agent"></search-map>
-                    </tab>
-                    
-                    <template #after-tablist>
-                       <div class="search__header--tabs-filter">
-                           <input type="text"/>
-                       </div>
-                    </template>
-                </tabs>
-            </template>
-        </search-header>
-</div>
+                    <search-list type="agent" :api="api">
+                        <template #filter>
+                            <search-filter-agent :api="api"></search-filter-agent>
+                        </template>
+                    </search-list>
 
+                </div>
+            </tab>
+        
+            <tab icon="map" label="Mapa" slug="map">
+                <div class="search__tabs--map">
 
+                    <search-map type="agent" :api="api">
+                        <template #filter>
+                            <search-filter-agent :api="api" position="map"></search-filter-agent>
+                        </template>
+                    </search-map>
+
+                </div>
+            </tab>
+        </tabs>
+    </template>
+</search>
+
+<!-- <div class="search">
+
+    <header class="search__header">
+    </header>    
+
+    <tabs class="search__tabs">
+        label
+        <template  #before-tablist>
+            <label class="search__tabs--before">
+                Visualizar como:
+            </label> 
+        </template>
+        
+        list page
+        <tab icon="list" label="Lista" slug="list">
+            <div class="search__tabs--list">
+                <search-list type="agent"></search-list>
+            </div>
+        </tab>
+    
+        map page
+        <tab icon="map" label="Mapa" slug="map">
+            <div class="search__tabs--map">
+                <search-map type="agent"></search-map>
+            </div>
+        </tab>
+    </tabs>
+
+</div> -->
