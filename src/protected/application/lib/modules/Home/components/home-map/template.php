4bc28edaaa5b81fa897b2ceb6cb6ef204fc21548
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-18 23:08:20 -0300

    aplicação do componente mc-map-card nos componentes home-map e search-map (refs #2162)

--- src/protected/application/lib/modules/Home/components/home-map/template.php
+++ src/protected/application/lib/modules/Home/components/home-map/template.php
@@ -1,11 +1,15 @@
 <?php
 use MapasCulturais\i;
-$this->import('mc-map entities');
+$this->import('mc-map entities mc-map-card');
 ?>
 <div class="home-map">
     <div class="home-map__content">
         <label class="home-map__content--title"><?php i::_e('Visualize também no mapa') ?></label>
         <p v-if="text" class="home-map__content--description">{{text}}</p>
-        <mc-map :entities="entities"></mc-map>
+        <mc-map :entities="entities">
+            <template #popup="{entity}">
+                <mc-map-card :entity="entity"></mc-map-card>
+            </template>
+        </mc-map>
     </div>
 </div>
