3751e33f4903ede5baf406bf165a884f13843970
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-19 17:52:39 -0300

    Ajustes no estilo dos filtros da página de busca dos agentes - #2137 #2147 #2142

--- src/protected/application/lib/modules/Search/components/search-filter-agent/template.php
+++ src/protected/application/lib/modules/Search/components/search-filter-agent/template.php
@@ -4,28 +4,32 @@ $this->import('search-filter');
 ?>
 
 <search-filter :position="position" :pseudo-query="pseudoQuery">
-    <form>
+    <form class="form">
+        <label class="form__label">
+            <?= i::_e('Filtros de eventos') ?>
+        </label>
+
         <div class="field">
-            <label><input v-model="pseudoQuery['@verified']" type="checkbox"> <?php i::_e('Somente entidades verificadas') ?> </label>
+            <label><input v-model="pseudoQuery['@verified']" type="checkbox"> <?php i::_e('Agentes oficiais') ?> </label>
         </div>  
 
         <div class="field">
-            <label> <?php i::_e('Tipo') ?>
-                <select v-model="pseudoQuery['type']">
-                    <option :value="undefined"> <? i::_e('Todos')?> </option>
-                    <option value="1"> <?php i::_e('Agente Individual') ?> </option>
-                    <option value="2"> <?php i::_e('Agente Coletivo') ?> </option>
-                </select>
-            </label>
+            <label> <?php i::_e('Tipo') ?></label>
+
+            <select v-model="pseudoQuery['type']">
+                <option :value="undefined"> <? i::_e('Todos')?> </option>
+                <option value="1"> <?php i::_e('Agente Individual') ?> </option>
+                <option value="2"> <?php i::_e('Agente Coletivo') ?> </option>
+            </select>
         </div>
 
         <div class="field">
-            <label> <?php i::_e('Área de atuação') ?>
-                <select v-model="pseudoQuery['term:area']" placeholder="<? i::_e('Selecione as áreas')?>">
-                    <option :value="undefined"> <? i::_e('Todos')?> </option>
-                    <option v-for="term in terms" :key="term"> {{term}} </option>
-                </select>
-            </label>
+            <label> <?php i::_e('Área de atuação') ?></label>
+
+            <select v-model="pseudoQuery['term:area']" placeholder="<? i::_e('Selecione as áreas')?>">
+                <option :value="undefined"> <? i::_e('Todos')?> </option>
+                <option v-for="term in terms" :key="term"> {{term}} </option>
+            </select>
         </div>
     </form>
 </search-filter>
