12733992d19db686d3ec920406fb196120827fa1
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-21 09:48:11 -0300

    Criação da tela de busca dos espaços - #2139 #2145 #2149

---
+++ src/protected/application/lib/modules/Search/components/search-filter-project/template.php
@@ -0,0 +1,26 @@
+<?php
+use MapasCulturais\i;
+$this->import('search-filter');
+?>
+
+<search-filter :position="position" :pseudo-query="pseudoQuery">
+    <form class="form">
+        <label class="form__label">
+            <?= i::_e('Filtros de projeto') ?>
+        </label>
+
+        <div class="field">
+            <label> <?php i::_e('Status do projeto') ?> </label>
+            <label><input v-model="pseudoQuery['@verified']" type="checkbox"> <?php i::_e('Projetos oficiais') ?> </label>
+        </div>  
+
+        <div class="field">
+            <label> <?php i::_e('Tipos de projetos') ?></label>
+
+            <select placeholder="<? i::_e('Selecione as áreas')?>"> <!-- v-model="pseudoQuery['term:area']" -->
+                <option :value="undefined"> <? i::_e('Todos')?> </option>
+                <option v-for="term in terms" :key="term"> {{term}} </option>
+            </select>
+        </div>
+    </form>
+</search-filter>
