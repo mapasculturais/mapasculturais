f9d853c4e7d587065a9406ffe4fd899751a1a228
erleibiazzio <erleibiazzio@gmail.com>
2022-08-22 18:50:11 -0300

    Cria Job que executa no início de uma fase de coleta de dados Ref.: #2113

--- src/protected/application/lib/modules/Opportunities/Module.php
+++ src/protected/application/lib/modules/Opportunities/Module.php
@@ -20,6 +20,7 @@ class Module extends \MapasCulturais\Module{
         // Registro de Jobs
         $app->registerJobType(new Jobs\StartEvaluationPhase(Jobs\StartEvaluationPhase::SLUG));
         $app->registerJobType(new Jobs\StartRegistrationPhase(Jobs\StartRegistrationPhase::SLUG));
+        $app->registerJobType(new Jobs\StartPhaseDataCollection(Jobs\StartPhaseDataCollection::SLUG));
 
         // Executa Job no início da fase
         $app->hook("entity(Opportunity).save:finish ", function() use ($app){
@@ -32,8 +33,18 @@ class Module extends \MapasCulturais\Module{
         $app->hook("entity(EvaluationMethodConfiguration).save:finish ", function() use ($app){
             $data = ['opportunity' => $this->opportunity];
 
-            $app->enqueueJob(Jobs\StartEvaluationPhase::SLUG, $data, $this->evaluationFrom->format("Y-m-d H:i:s"));
+            if($this->evaluationFrom){
+                $app->enqueueJob(Jobs\StartEvaluationPhase::SLUG, $data, $this->evaluationFrom->format("Y-m-d H:i:s"));
+            }
             
+        });
+
+        // Executa Job no início de uma fase de coleta de dados
+        $app->hook("module(OpportunityPhases).createNextPhase(<<*>>):after", function() use ($app){
+            if($this->opportunity_data_collection){
+                $data = ['opportunity' => $this];
+                $app->enqueueJob(Jobs\StartPhaseDataCollection::SLUG, $data, $this->registrationFrom->format("Y-m-d H:i:s"));
+            }
         });
     }
 
