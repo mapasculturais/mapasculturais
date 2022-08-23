09fcd6d0577f07d68bd319e00035549bcf8fbdfb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-23 20:24:33 -0300

    Ajusta o componente <theme-logo> para receber um link clicável - #2151

--- src/protected/application/lib/modules/Components/components/theme-logo/template.php
+++ src/protected/application/lib/modules/Components/components/theme-logo/template.php
@@ -1,14 +1,16 @@
 <?php
 use MapasCulturais\i;
 ?>
-<div class="theme-logo" :style="{ '--logo-color': color }">
-    <div class="theme-logo__logo">
-        <div class="theme-logo__logo--part1"></div>
-        <div class="theme-logo__logo--part2"></div>
-    </div>
-    
-    <div class="theme-logo__text">
-        <label class="theme-logo__text--title">{{title}}</label>
-        <small class="theme-logo__text--subtitle" v-if="subtitle">{{subtitle}}</small>
-    </div>
+<div :style="{ '--logo-color': color }">
+    <a class="theme-logo" :href="href">    
+        <div class="theme-logo__logo">
+            <div class="theme-logo__logo--part1"></div>
+            <div class="theme-logo__logo--part2"></div>
+        </div>
+        
+        <div class="theme-logo__text">
+            <span class="theme-logo__text--title">{{title}}</span>
+            <small class="theme-logo__text--subtitle" v-if="subtitle">{{subtitle}}</small>
+        </div>
+    </a>
 </div>
