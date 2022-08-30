cbce2925710816b249bc5f5f704b8322173a0a6c
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-08-30 07:26:27 -0300

    Criação do componente <mc-header-menu> - #2024

---
+++ src/protected/application/lib/modules/Components/components/mc-header-menu/template.php
@@ -0,0 +1,29 @@
+<?php
+use MapasCulturais\i;
+$this->import('mc-icon');
+?>
+
+<!-- Logo -->
+<div class="mc-header-logo">
+    <button class="mc-header-menu__btn-mobile" @click="toggleMobile()">
+        <mc-icon name="menu-mobile"></mc-icon>
+    </button>
+
+    <slot name="logo"></slot>
+</div>
+
+<!-- Menu principal -->
+<ul class="mc-header-menu">   
+    <slot name="default"></slot>
+</ul>
+
+<ul v-if="openMobile" class="mc-header-menu mobile">   
+    <slot name="default"></slot>
+
+    <li> 
+        <a href="<?= $app->createUrl('panel', 'index') ?>" class="mc-header-menu--item painel">
+            <span class="icon"> <mc-icon name="dashboard"> </span>
+            <p class="label"> <?php i::_e('Painel de controle') ?> </p>      
+        </a> 
+    </li>
+</ul>
