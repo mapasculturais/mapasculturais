266f4a7d5680e1863cee6bfaeaaf74e5508f1b42
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-08-30 19:04:11 -0300

    controller envia initial_pseudo_query para todas as páginas de busca (refs #2135)

--- src/protected/application/lib/modules/Search/Controller.php
+++ src/protected/application/lib/modules/Search/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Search;
 
+use MapasCulturais\App;
+
 class Controller extends \MapasCulturais\Controller
 {
     
@@ -10,23 +12,27 @@ class Controller extends \MapasCulturais\Controller
     }
 
     function GET_agents() {
-        $this->render('agent', []);
+        $this->render('agent', ['initial_pseudo_query' => []]);
     }
     
     function GET_events() {
-        $this->render('event', []);
+        $app = App::i();
+        $initial_pseudo_query = [
+            '@from' => date('Y-m-d'),
+            '@to' => date('Y-m-d', strtotime($app->config['search.events.to']))
+        ];
+        $this->render('event', ['initial_pseudo_query' => $initial_pseudo_query]);
     }
 
     function GET_projects() {
-        $this->render('project', []);
+        $this->render('project', ['initial_pseudo_query' => []]);
     }
 
     function GET_opportunities() {
-        $this->render('opportunity', []);
+        $this->render('opportunity', ['initial_pseudo_query' => []]);
     }
 
     function GET_spaces() {
-        $this->render('space', []);
+        $this->render('space', ['initial_pseudo_query' => []]);
     }
-    
 }
