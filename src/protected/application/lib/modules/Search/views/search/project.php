abb94cdc26f8f3121553b1be612d4b8ba85a1f2e
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-09-01 13:26:42 -0300

    Aplica tradução nas telas de busca das entidades (Ref.: #2136, #2137, #2138, #2139 e #2140)

--- src/protected/application/lib/modules/Search/views/search/project.php
+++ src/protected/application/lib/modules/Search/views/search/project.php
@@ -1,34 +1,27 @@
 <?php 
 use MapasCulturais\i;
  
-$this->import('
-    search tabs search-list search-map search-filter-project 
-    '); /* create-project */
+$this->import('search tabs search-list search-map search-filter-project');
 $this->breadcramb = [
     ['label'=> i::__('Inicio'), 'url' => $app->createUrl('index')],
     ['label'=> i::__('Projetos'), 'url' => $app->createUrl('projects')],
 ];
 ?>
 
-<search page-title="Projetos" entity-type="project" >    
-
+<search page-title="<?php i::esc_attr_e('Projetos') ?>" entity-type="project" >    
     <template #create-button>
-        Botão criar projeto<!-- <create-project></create-project> -->
+        <!-- @TODO: Criação e aplicação do componente <create-project> -->
+        <?= i::_e('botão criar projeto') ?>
     </template>
-
     <template #default="{pseudoQuery}">
-
         <div class="tabs-component__panels">
             <div class="search__tabs--list">
-    
                 <search-list :pseudo-query="pseudoQuery" type="project">
                     <template #filter>
                         <search-filter-project :pseudo-query="pseudoQuery"></search-filter-project>
                     </template>
                 </search-list>
-    
             </div>
         </div>
-        
     </template>
 </search>
