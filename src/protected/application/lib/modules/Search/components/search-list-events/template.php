c37f5e367ca977e8e997eb6362777b265ffada68
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-09-05 23:27:01 -0300

    Ajustes de estilização na listagem da página de busca dos agentes - #2143
     - Estilização dos cards dos eventos <occurrence-card>
     - Estilização dos marcadores temporais
     - Alteração de icone utilizado no card dos eventos

--- src/protected/application/lib/modules/Search/components/search-list-events/template.php
+++ src/protected/application/lib/modules/Search/components/search-list-events/template.php
@@ -10,16 +10,19 @@ $this->import('entities mapas-card occurrence-card loading');
         </div>
     </div>
     <loading :condition="loading"></loading>    
-    <div v-if="!loading" class="col-9" v-for="occurrence in occurrences" :key="occurrence._reccurrence_string">
-        <div v-if="newDate(occurrence)">
-            <h2 v-if="occurrence.starts.isToday()"><?= i::__('Hoje') ?></h2>
-            <h2 v-else-if="occurrence.starts.isTomorrow()"><?= i::__('Amanhã') ?></h2>
-            <h2 v-else-if="occurrence.starts.isYesterday()"><?= i::__('Ontem') ?></h2>
-            <template v-else>
-                <h2>{{occurrence.starts.day()}}</h2>
-                <strong>{{occurrence.starts.month()}}</strong>
-            </template>
-            <h3>{{occurrence.starts.weekday()}}</h3>
+    <div v-if="!loading" class="col-9 search-list__cards" v-for="occurrence in occurrences" :key="occurrence._reccurrence_string">
+        <div v-if="newDate(occurrence)" class="search-list__cards--date">
+            <div class="search-list__cards--date-info">
+                <h2 v-if="occurrence.starts.isToday()"><?= i::__('Hoje') ?></h2>
+                <h2 v-else-if="occurrence.starts.isTomorrow()"><?= i::__('Amanhã') ?></h2>
+                <h2 v-else-if="occurrence.starts.isYesterday()"><?= i::__('Ontem') ?></h2>
+                <template v-else>
+                    <label class="day">{{occurrence.starts.day()}}</label>
+                    <strong class="month">{{occurrence.starts.month()}}</strong>
+                </template>
+                <label class="weekend">{{occurrence.starts.weekday()}}</label>
+            </div>            
+            <div class="search-list__cards--date-line"></div>
         </div>
         <occurrence-card :occurrence="occurrence"></occurrence-card> 
     </div>
