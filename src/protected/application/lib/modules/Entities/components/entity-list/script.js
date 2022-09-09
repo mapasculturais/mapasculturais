fa324ae4f93b6ecf7846eab18442279e932b8f3e
robedj <ed.wassouf@gmail.com>
2022-09-09 13:11:58 -0400

    W.I.P Estilização do componente entity-list - in progress Ref.: #2166

---
+++ src/protected/application/lib/modules/Entities/components/entity-list/script.js
@@ -0,0 +1,52 @@
+app.component('entity-list', {
+    template: $TEMPLATES['entity-list'],
+    emits: [],
+    
+    setup() {
+        // os textos estão localizados no arquivo texts.php deste componente 
+        const text = Utils.getTexts('entity-list')
+        return { text }
+    },
+
+    data() {
+        const ids = this.entity[this.propertyName]
+        return {
+            query: {
+                'id': `IN(${ids})`,
+            },
+        };
+    },
+
+    props: {
+
+        entity: {
+            type: Entity,
+            required: true
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+
+        editable: {
+            type: Boolean,
+            default: false,
+            
+        },
+        propertyName: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+
+    },
+
+    methods: {
+
+    },
+    
+});
