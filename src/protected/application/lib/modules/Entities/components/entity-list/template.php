caa2054a3b6db7b225be1d58f927b1444605bfa4
robedj <ed.wassouf@gmail.com>
2022-09-12 18:43:51 -0400

    Ajustes no css do entity-list e no template Ref.: Ref #2166

--- src/protected/application/lib/modules/Entities/components/entity-list/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-list/template.php
@@ -9,11 +9,11 @@ $this->import('entities mc-link');
         <div class="col-12__owner--title">
             <h4>{{title}}</h4>
         </div>
-        <entities class="col-12 entity-list__owner--content" select="id,name,files.avatar" order="name ASC" :type="type" :query="query" #default="{entities}">
-            <li v-for="entity in entities">
+        <entities  select="id,name,files.avatar" order="name ASC" :type="type" :query="query" #default="{entities}">
+            <li class="col-12 entity-list__owner--content" v-for="entity in entities">
                 <mc-link :entity="entity">
                     <div class="entity-list__owner--content-name">
-                       <label class="entity-list__owner--content-name-label">{{entity.name}}</label>
+                       <a class="entity-list__owner--content-name-label">{{entity.name}}</a>
                     </div>
                     <img v-if="entity.files.avatar?.transformations?.avatarSmall?.url" class="entity-list__owner--content-img" :src="entity.files.avatar?.transformations?.avatarSmall?.url">
                     <div v-else class="entity-list__owner--content-placeholder">
