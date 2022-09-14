757d87f5ec5adaa7cface79cfa6a78848a0384bd
robedj <ed.wassouf@gmail.com>
2022-09-14 13:34:46 -0400

    Ajustes na identação da single do espaço e exibição do label Propriedade REf.: #2166

--- src/protected/application/themes/BaseV2/views/space/single.php
+++ src/protected/application/themes/BaseV2/views/space/single.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 use MapasCulturais\i;
-$this->layout = 'entity'; 
-
+$this->layout = 'entity';
 $this->import('
     mapas-container mc-map mc-map-marker space-info mapas-breadcrumb tabs
     entity-terms share-links entity-files-list entity-links
@@ -10,9 +9,9 @@ $this->import('
     ');
 
 $this->breadcramb = [
-    ['label'=> i::__('Inicio'), 'url' => $app->createUrl('panel', 'index')],
-    ['label'=> i::__('Espaços'), 'url' => $app->createUrl('panel', 'spaces')],
-    ['label'=> $entity->name, 'url' => $app->createUrl('space', 'single', [$entity->id])],
+    ['label' => i::__('Inicio'), 'url' => $app->createUrl('panel', 'index')],
+    ['label' => i::__('Espaços'), 'url' => $app->createUrl('panel', 'spaces')],
+    ['label' => $entity->name, 'url' => $app->createUrl('space', 'single', [$entity->id])],
 ];
 ?>
 
@@ -20,24 +19,24 @@ $this->breadcramb = [
     <mapas-breadcrumb></mapas-breadcrumb>
     <entity-header :entity="entity"></entity-header>
 
-    <tabs class="tabs">  
+    <tabs class="tabs">
 
         <tab icon="map" label="<?= i::_e('Informações') ?>" slug="info">
             <div class="tabs__info">
 
-                <mapas-container>        
+                <mapas-container>
                     <main>
                         <div class="grid-12">
-                           <div class="col-12">
+                            <div class="col-12">
                                 <space-info :entity="entity"></space-info>
-                           </div>
-                            
+                            </div>
+
                             <div class="divider col-12"></div>
                             <div class="col-12">
-                                    <h2><?= i::_e("Descrição Detalhada"); ?></h2>
-                                    <p>{{entity.longDescription}}</p>
+                                <h2><?= i::_e("Descrição Detalhada"); ?></h2>
+                                <p>{{entity.longDescription}}</p>
                             </div>
-                                
+
                             <div class="col-12">
                                 <entity-files-list :entity="entity" group="downloads" title="<?= i::_e('Arquivos para download'); ?>"></entity-files-list>
                             </div>
@@ -52,22 +51,22 @@ $this->breadcramb = [
                             <div class="property col-12">
                                 <button class="button button--primary button--md">"<?= i::_e('Reinvindicar Propriedade'); ?>"</button>
                             </div>
-                            
+
                         </div>
                     </main>
-                    
+
                     <aside>
                         <div class="grid-12">
                             <div class="col-12">
                                 <entity-social-media :entity="entity"></entity-social-media>
                             </div>
-                            
+
                             <div class="col-12">
                                 <entity-seals :entity="entity" title="Verificações"></entity-seals>
                             </div>
-                            
+
                             <div class="col-12">
-                                <entity-related-agents :entity="entity"  title="<?= i::_e('Agentes Relacionados'); ?>"></entity-related-agents>  
+                                <entity-related-agents :entity="entity" title="<?= i::_e('Agentes Relacionados'); ?>"></entity-related-agents>
                             </div>
 
                             <div class="col-12">
@@ -77,9 +76,10 @@ $this->breadcramb = [
                             <div class="col-12">
                                 <share-links title="<?= i::_e('Compartilhar'); ?>" text="<?= i::_e('Veja este link:'); ?>"></share-links>
                             </div>
-                            
+
                             <div class="col-12">
-                                <entity-owner title="<?= i::_e('Publicado por'); ?>" :entity="entity"></entity-links>
+                                <entity-owner title="<?= i::_e('Publicado por'); ?>" :entity="entity">
+                                    </entity-links>
                             </div>
 
                             <div class="col-12">
@@ -87,11 +87,14 @@ $this->breadcramb = [
                             </div>
 
                             <div v-if="entity.children.length >0 || entity.relatedOpportunities >0" class="col-12">
-                                <h4>Propriedades do Espaço</h4>
-                                
+
+                                <div class="col-12">
+                                    <h4>Propriedades do Espaço</h4>
+                                </div>
+
                                 <entity-list :entity="entity" title="Subespaços" property-name="children" type="space"></entity-list>
 
-                                <div v-if="entity.relatedOpportunities.length > 0"  class="col-12">
+                                <div v-if="entity.relatedOpportunities.length > 0" class="col-12">
                                     <entity-list :entity="entity" title="Oportunidades" property-name="relatedOpportunities" type="opportunity"></entity-list>
                                 </div>
                             </div>
@@ -99,10 +102,10 @@ $this->breadcramb = [
                         </div>
                     </aside>
                 </mapas-container>
-                
+
             </div>
         </tab>
-    
+
         <tab icon="events" label="<?= i::_e('Agenda') ?>" slug="agenda">
             <div class="tabs__agenda">
 
