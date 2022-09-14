8cdb564df4c547532610daf16b27f5a5226d81ec
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-09-14 17:56:19 -0300

    Ajustes na responsividade e interação do componente <create-occurrence> - (Ref.: #2173)

--- src/protected/application/lib/modules/Entities/components/create-occurrence/script.js
+++ src/protected/application/lib/modules/Entities/components/create-occurrence/script.js
@@ -5,22 +5,38 @@ app.component('create-occurrence' , {
     setup() { 
       
     },
-    
+
     created() {
-        
+        this.createEntity()
     },
 
     data() {
+        var actualDate = new Date();
         return {
+            entity: null,
             step: 0,
+            autoDescription: '',
+            free: false,
+            frequency: '',
+            eventId: 0,
+            spaceId: 0,
+            startsAt: '00:00',
+            duration: 0,
+            endsAt: '00:00',
+            startsOn: actualDate.getFullYear()+'-'+(actualDate.getMonth()+1 < 10 ? '0'+(actualDate.getMonth()+1) : actualDate.getMonth()+1)+'-'+actualDate.getDate(),
+            until: 0,
+            description: '',
+            price: 0,
+            day: [false, false, false, false, false, false, false],
+
         }
     },
 
     props: {
-        entity: {
+        /* entity: {
             type: Entity,
             required: true
-        },
+        }, */
 
         editable: {
             type: Boolean,
@@ -36,6 +52,8 @@ app.component('create-occurrence' , {
 
         next() {
             if(this.step < 5) {
+                if (this.step == 4)
+                    this.updateDescription();
                 ++this.step
             }
         },
@@ -44,6 +62,95 @@ app.component('create-occurrence' , {
             if(this.step > 0) {
                 --this.step;
             }
+        },
+
+        createEntity() {
+            this.entity = new Entity('new-occurrence');
+        },
+
+        updateDescription() {
+            var description = '';
+            var month = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+            var days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+            var weekDays = [];
+            this.day.forEach(function(status, index) {
+                if(status) {
+                    weekDays.push(index);
+                }
+            });
+
+            var startData = new Date(this.startsOn + 'T00:00');
+            var startDay = startData.getDate();
+            var startMonth = (startData.getMonth() + 1);
+            var startYear = startData.getFullYear();
+
+            var endData = new Date(this.until + 'T00:00');
+            var endDay = endData.getDate();
+            var endMonth = (endData.getMonth() + 1);
+            var endYear = endData.getFullYear();
+
+            if (this.frequency == 'once') {
+                description += "Dia " + startDay + " de " + month[startMonth] + " de " + startYear;
+            } else {
+                if (this.frequency == 'daily') {
+                    description += 'Diariamente, ';
+                } else if (this.frequency == 'weekly') {
+                    if (weekDays[0] == '0' || weekDays[0] == '6') {
+                        description += 'Todo ';
+                    } else {
+                        description += 'Toda ';
+                    }
+
+                    var count = 1;
+                    weekDays.forEach(function(day, index) {
+                        description += days[day];
+                        count ++;
+                        if (count == weekDays.length)
+                            description += ' e ';
+                        else if (count < weekDays.length)
+                            description += ', '
+                    });
+                }
+
+                if (startYear != endYear) {
+                    description += ' de ' + startDay + " de " + month[startMonth] + " de " + startYear;
+                    description += ' a ' + endDay + " de " + month[endMonth] + " de " + endYear;
+                } else {
+                    if (startMonth != endMonth) {
+                        description += ' de ' + startDay + " de " + month[startMonth] + ' a ' + endDay + " de " + month[endMonth];
+                    } else {
+                        description += ' de ' + startDay + ' a ' + endDay + " de " + month[endMonth] + " de " + endYear;
+                    }
+                }
+            }
+
+            if (this.startsAt) {
+                if (this.startsAt.substring(0,2) == '01')
+                    description += ' à ' + this.startsAt;
+                else
+                    description += ' às ' + this.startsAt;
+            }
+
+            this.autoDescription = description;
+        },
+
+        create() {
+
+            /* data = {
+                eventId: this.eventId,
+                spaceId: this.spaceId,
+                startsAt: this.startsAt,
+                duration: this.duration,
+                endsAt: this.endsAt,
+                frequency: this.frequency,
+                startsOn: this.startsOn,
+                until: this.until,
+                day: this.day,
+                description: this.description,
+                price: this.price,
+            } */
+
         }
        
     },
--- src/protected/application/lib/modules/Entities/components/create-occurrence/template.php
+++ src/protected/application/lib/modules/Entities/components/create-occurrence/template.php
@@ -1,6 +1,6 @@
 <?php 
 use MapasCulturais\i;
-$this->import('modal'); 
+$this->import('modal entity-field'); 
 ?>
 
 <modal title="Inserir ocorrência no evento" classes="create-occurrence" button-label="" >
@@ -11,8 +11,12 @@ $this->import('modal');
                 <label class="create-occurrence__section--title"> <?= i::_e('Vincular um espaço para o evento') ?> </label>
 
                 <div class="create-occurrence__section--link-space">
+                    <!-- Seletor de entidades - espaços -->
                     <button class="button button--icon button--text-outline"> <mc-icon name="add"></mc-icon> <?= i::_e('Adicionar') ?> </button>
+
                     <?= i::_e('ou') ?>
+
+                    <!-- create space -->
                     <button class="button button--icon button--primary-outline"> <mc-icon name="add"></mc-icon> <?= i::_e('Crie um novo espaço') ?> </button>
                 </div>
             </div>
@@ -22,15 +26,16 @@ $this->import('modal');
 
                 <div class="grid-12">
                     <div class="col-6 sm:col-12">
+                        <!-- <entity-field :entity="entity" label=<?php i::esc_attr_e("Data inicial:")?> ></entity-field> -->
                         <div class="create-occurrence__section--field">
                             <span class="label"><?= i::_e('Data inicial:') ?></span>
-                            <input type="date" />
+                            <input v-model="startsOn" type="date" />
                         </div>
                     </div>
                     <div class="col-6 sm:col-12">
                         <div class="create-occurrence__section--field">
                             <span class="label"><?= i::_e('Data final:') ?></span>
-                            <input type="date" />
+                            <input v-model="until" type="date" />
                         </div>
                     </div>
                 </div>     
@@ -43,13 +48,13 @@ $this->import('modal');
                     <div class="col-6 sm:col-12">
                         <div class="create-occurrence__section--field">
                             <span class="label"><?= i::_e('Horário inicial:') ?></span>
-                            <input type="time" />
+                            <input v-model="startsAt" type="time" />
                         </div>
                     </div>
                     <div class="col-6 sm:col-12">
                         <div class="create-occurrence__section--field">
                             <span class="label"><?= i::_e('Horário final:') ?></span>
-                            <input type="time" />
+                            <input v-model="endsAt" type="time" />
                         </div>
                     </div>
                 </div>  
@@ -59,23 +64,23 @@ $this->import('modal');
                 <span class="create-occurrence__section--title"> <?= i::_e('Qual a frequência do evento?') ?> </span>
 
                 <div class="create-occurrence__section--fields">
-                    <label class="create-occurrence__section--fields-field"> <input type="radio" name="frequency" value="once" /> <?= i::_e('uma vez') ?> </label>
-                    <label class="create-occurrence__section--fields-field"> <input type="radio" name="frequency" value="weekly" /> <?= i::_e('semanal') ?> </label>
-                    <label class="create-occurrence__section--fields-field"> <input type="radio" name="frequency" value="daily" /> <?= i::_e('todos os dias') ?> </label>
+                    <label class="create-occurrence__section--fields-field"> <input v-model="frequency" type="radio" name="frequency" value="once" /> <?= i::_e('uma vez') ?> </label>
+                    <label class="create-occurrence__section--fields-field"> <input v-model="frequency" type="radio" name="frequency" value="weekly" /> <?= i::_e('semanal') ?> </label>
+                    <label class="create-occurrence__section--fields-field"> <input v-model="frequency" type="radio" name="frequency" value="daily" /> <?= i::_e('todos os dias') ?> </label>
                 </div>
             </div>
 
-            <div :class="['col-12', 'create-occurrence__section', {'active' : step==3}]">
+            <div v-if="frequency=='weekly'" :class="['col-12', 'create-occurrence__section', {'active' : step==3}]">
                 <span class="create-occurrence__section--title"> <?= i::_e('Que dias da semana o evento se repete?') ?> </span>
 
                 <div class="create-occurrence__section--fields">
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[0]"> <?= i::_e('Domingo') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[1]"> <?= i::_e('Segunda') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[2]"> <?= i::_e('Terça') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[3]"> <?= i::_e('Quarta') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[4]"> <?= i::_e('Quinta') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[5]"> <?= i::_e('Sexta') ?> </label>
-                    <label class="create-occurrence__section--fields-field"><input type="checkbox" name="day[6]"> <?= i::_e('Sabado') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[0]" type="checkbox" name="day[0]"> <?= i::_e('Domingo') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[1]" type="checkbox" name="day[1]"> <?= i::_e('Segunda') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[2]" type="checkbox" name="day[2]"> <?= i::_e('Terça') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[3]" type="checkbox" name="day[3]"> <?= i::_e('Quarta') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[4]" type="checkbox" name="day[4]"> <?= i::_e('Quinta') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[5]" type="checkbox" name="day[5]"> <?= i::_e('Sexta') ?> </label>
+                    <label class="create-occurrence__section--fields-field"><input v-model="day[6]" type="checkbox" name="day[6]"> <?= i::_e('Sabado') ?> </label>
                 </div>
             </div>
 
@@ -87,14 +92,14 @@ $this->import('modal');
                         <div class="col-12">
                             <div class="create-occurrence__section--fields">
                                 <span class="label"> <?= i::_e('O evento será gratuito?') ?> </span>
-                                <label class="create-occurrence__section--fields-field"> <input type="radio" name="free" :value="true" /> <?= i::_e('Sim') ?> </label>
-                                <label class="create-occurrence__section--fields-field"> <input type="radio" name="free" :value="false" /> <?= i::_e('Não') ?> </label>
+                                <label class="create-occurrence__section--fields-field"> <input v-model="free" type="radio" name="free" :value="true" /> <?= i::_e('Sim') ?> </label>
+                                <label class="create-occurrence__section--fields-field"> <input v-model="free" type="radio" name="free" :value="false" /> <?= i::_e('Não') ?> </label>
                             </div>
                         </div>
-                        <div class="col-6 sm:col-12">
+                        <div class="col-6 sm:col-12" v-if="!free">
                             <div class="create-occurrence__section--field">
                                 <span class="label"><?= i::_e('Valor da entrada:') ?></span>
-                                <input type="number" />
+                                <input v-model="price" type="number" />
                             </div>
                         </div>
                         <div class="col-6 sm:col-12">
@@ -116,7 +121,7 @@ $this->import('modal');
                             <div class="create-occurrence__section--field">
                                 <span class="label"><?= i::_e('Descrição legível de data e horário') ?></span>
                                 <div class="auto-description">
-                                    Capoeira no Sangue Estrada da Pirelli, conjunto Beija flor, WE 10, QD 07, 18, Próximo a feira, Marituba, 67105-740, Ananindeua, PA
+                                    <span>{{autoDescription}}</span>
                                     <button class="button button--icon button--sm"> <mc-icon name="copy"></mc-icon> copiar </button>
                                 </div>
                             </div>
@@ -124,7 +129,7 @@ $this->import('modal');
                         <div class="col-12">
                             <div class="create-occurrence__section--field">
                                 <span class="label"><small><?= i::_e('Você pode usar a descrição gerada pelo sistema OU criar uma descrição customizada prenchendo o campo abaixo') ?></small></span>
-                                <input type="text" placeholder="<?= i::_e('Preencha aqui o resumo customizado   ') ?>"/>
+                                <input v-model="description" type="text" name="description" placeholder="<?= i::_e('Preencha aqui o resumo customizado') ?>"/>
                             </div>
                         </div>
                     </div>
@@ -142,10 +147,10 @@ $this->import('modal');
 
         <div class="mobile">
             <div class="button-group">
-                <button v-if="step === 0" class="button button--text" @click="cancel(modal)"><?php i::_e('Cancelar')?></button>
-                <button v-if="step > 0" class="button button--text" @click="prev()"><?php i::_e('Voltar')?></button>
-                <button v-if="step < 5"class="button button--primary button--icon" @click="next()"><?php i::_e('Próximo')?><mc-icon name="next"></mc-icon></button>
-                <button v-if="step === 5" class="button button--primary" @click="createPublic(modal)"><?php i::_e('Concluir')?></button>
+                <button v-if="step==0" class="button button--text" @click="cancel(modal)"><?php i::_e('Cancelar')?></button>
+                <button v-if="step>0" class="button button--text" @click="prev()"><?php i::_e('Voltar')?></button>
+                <button v-if="step<5" class="button button--primary button--icon" @click="next()"><?php i::_e('Próximo')?><mc-icon name="next"></mc-icon></button>
+                <button v-if="step==5" class="button button--primary" @click="create()"><?php i::_e('Concluir')?></button>
             </div>
 
             <div class="pagination">
@@ -161,7 +166,7 @@ $this->import('modal');
         <div class="desktop">
             <div class="button-group">
                 <button class="button button--text" @click="cancel(modal)"><?php i::_e('Cancelar')?></button>
-                <button class="button button--primary" @click="createPublic(modal)"><?php i::_e('Inserir ocorrência')?></button>
+                <button class="button button--primary" @click="create()"><?php i::_e('Inserir ocorrência')?></button>
             </div>
         </div>
 
