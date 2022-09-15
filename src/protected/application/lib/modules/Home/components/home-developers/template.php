0831f504cfeb10943b83385ecb5710836632baf0
robedj <ed.wassouf@gmail.com>
2022-09-15 13:06:06 -0400

    Adição dos textos na home Ref.: #2200

--- src/protected/application/lib/modules/Home/components/home-developers/template.php
+++ src/protected/application/lib/modules/Home/components/home-developers/template.php
@@ -7,7 +7,7 @@ use MapasCulturais\i;
         <span class="dev-icon"><mc-icon name="code"></mc-icon></span>
         <label class="home-developers__content--title"><?php i::_e('Alô desenvolvedores,') ?></label>
         <div class="home-developers__content--description">
-            <?php i::_e('Existem algumas maneiras de desenvolvedores interagirem com o <b>mapas culturais</b>. A primeira é através da nossa API, com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o <b>mapas culturais</b> é construído a partir do <u>sofware livre Mapas Culturais</u>, criado em parceria com o Instituto TIM, e você pode contribuir para o seu desenvolvimento através do GitHub.') ?>
+            <?php i::_e('Além disso, o mapas culturais é construído a partir do sofware livre <b>Mapas Culturais</b> , criado em parceria com o Instituto TIM. Atualmente, sua principal mantenedora é a hacklab/ e você pode contribuir para o seu desenvolvimento através do GitHub.') ?>
         </div>
         <div class="home-developers__content--link">
             <a class="link" href="https://github.com/mapasculturais"> 
