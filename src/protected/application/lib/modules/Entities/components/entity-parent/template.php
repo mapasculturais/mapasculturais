8a193b963faac6068ac1ff16068fd35356006114
robedj <ed.wassouf@gmail.com>
2022-09-21 09:24:04 -0400

    Estilização do componente entity-parent Ref.: #2187

--- src/protected/application/lib/modules/Entities/components/entity-parent/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-parent/template.php
@@ -4,25 +4,25 @@ use MapasCulturais\i;
 
 $this->import('select-entity');
 ?>
-<div v-if="owner" class="entity-owner">
+<div v-if="parent" class="entity-parent">
     <h4>{{entity.name}} {{title}}</h4>
-    <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
-        <div class="entity-owner__owner--img">
-            <img v-if="owner.files.avatar" class="profile" :src="owner.files?.avatar?.url">
+    <a class="entity-parent__parent" :href="parent.singleUrl" :title="parent.shortDescription">
+        <div class="entity-parent__parent--img">
+            <img v-if="parent.files.avatar" class="profile" :src="parent.files?.avatar?.url">
             <div v-else class="placeholder">
                 <mc-icon name="agent-1"></mc-icon>
             </div>
         </div>
-        <div class="entity-owner__owner--name">
+        <div class="entity-parent__parent--name">
             {{entity.parent.name}}
         </div>
     </a>
 
-    <div v-if="editable" class="entity-owner__edit">
+    <div class="entity-parent__edit">
         
-        <select-entity type="agent" @select="changeOwner($event)" openside="right-down">            
+        <select-entity :type="type" @select="changeParent($event)" openside="right-down">            
             <template #button="{ toggle }">
-                <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
+                <a class="entity-parent__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
                     <mc-icon name="exchange"></mc-icon>
                     <h4><?php i::_e('Alterar Propriedade') ?></h4>    
                 </a>
