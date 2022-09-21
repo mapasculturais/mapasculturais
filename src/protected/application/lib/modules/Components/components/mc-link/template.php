84b1bd0587712c6220558ec949ce50b40c01daa2
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-09-20 23:15:19 -0300

    previne a exibição duplicada de ícones no componente <mc-link>

--- src/protected/application/lib/modules/Components/components/mc-link/template.php
+++ src/protected/application/lib/modules/Components/components/mc-link/template.php
@@ -3,8 +3,8 @@ $this->import('mc-icon');
 ?>
 <a :href="url" :class="classes">
     <mc-icon v-if="typeof icon != 'string' && icon && entity" :entity="entity"></mc-icon>
-    <mc-icon v-if="icon && !entity" :name="icon"></mc-icon>
-    <mc-icon v-if="typeof icon == 'string'" :name="icon"></mc-icon>
+    <mc-icon v-else-if="icon && !entity" :name="icon"></mc-icon>
+    <mc-icon v-else-if="typeof icon == 'string'" :name="icon"></mc-icon>
     <slot>
         <template v-if="label">{{label}}</template>
         <template v-if="!label && entity">{{entity.name}}</template>
