52675cd38aa8e7db9abce5e29ca4520bb01cf6f9
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-09-21 11:00:18 -0300

    Criação dos cards das entidades ná página principal do painel - 2180

--- src/protected/application/lib/modules/Panel/Controller.php
+++ src/protected/application/lib/modules/Panel/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Panel;
 
+use MapasCulturais\App;
+
 class Controller extends \MapasCulturais\Controller
 {
     function __construct()
@@ -11,7 +13,18 @@ class Controller extends \MapasCulturais\Controller
     
     function GET_index() {
         $this->requireAuthentication();
-        $this->render('index');
+        $app = App::i();
+        $count = new \stdClass();
+
+        $count->spaces          = $app->controller('space')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+        $count->agents          = $app->controller('agent')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+        $count->events          = $app->controller('event')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+        $count->projects        = $app->controller('project')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+        $count->opportunities   = $app->controller('opportunity')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+        $count->subsite         = $app->controller('subsite')->apiQuery(['@count'=>1]);
+        $count->seals           = $app->controller('seal')->apiQuery(['@count'=>1, 'user' => 'EQ(' . $app->user->id . ')']);
+
+        $this->render('index', ['count'=>$count]);
     }
 
     function GET_agents() {
