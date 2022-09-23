67c3974d9fd41ecfc8ba80f458dcfeff23299832
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-09-23 02:55:24 -0300

    remove componente user-management--delete-role-modal (refs #1912)

--- src/protected/application/lib/modules/UserManagement/components/panel--card-user/template.php
+++ src/protected/application/lib/modules/UserManagement/components/panel--card-user/template.php
@@ -3,35 +3,40 @@
 use MapasCulturais\i;
 
 $this->import('
+    confirm-button
     panel--entity-card 
     user-management--add-role-modal
-    user-management--delete-role-modal
 ');
 ?>
 
 <panel--entity-card :entity="entity">
     <template #title>
-        <slot name="card-title" :entity="entity" v-if="entity.profile">{{entity.profile.name}}</slot>
+        <slot name="card-title" :entity="entity" v-if="entity.profile">{{username}}</slot>
     </template>
     <template #header-actions>
-        <slot name="card-actions">
-            <button class="entity-card__header-action">
-                <mc-icon name="favorite"></mc-icon>
-                <span><?=i::__('Favoritar')?></span>
-            </button>
-        </slot>
+        <slot name="card-actions"></slot>
     </template>
     <template #default>
-        <slot name="card-content">
-            <?=i::__('Funções')?>
-            <ul>
-                <li v-for="role in entity.roles" class="roles">
-                    <user-management--delete-role-modal :role="role" :user="entity"></user-management--delete-role-modal>
+        <div class="mc-tag-list">
+            <code>ID: {{entity.id}}</code>
+            <h4><?=i::__('Funções do usuário:')?></h4>
+            <ul class="mc-tag-list__tagList">
+                <li v-for="role in entity.roles" class="primary__border-solid primary__color mc-tag-list__tagList--tag">
+                    <strong v-if="role.subsite">{{`<?= i::esc_attr__('${role.name} em ${role.subsite.name}') ?>`}}</strong>
+                    <strong v-else>{{role.name}}</strong>
+                    <confirm-button 
+                        :message="`<?= i::esc_attr__('Deseja remover a função "${role.name}" do usuário "${username}"?') ?>`"
+                        @confirm="role.delete(true)">
+                        <template #button="modal">
+                            <mc-icon @click="modal.open()" name='delete'></mc-icon>
+                        </template>
+                        
+                    </confirm-button>
                 </li>
-                <li class="roles">    
+                <li class="primary__background mc-tag-list__tagList--tag">    
                     <user-management--add-role-modal :user="entity"></user-management--add-role-modal>                
                 </li>
             </ul>
-        </slot>
+        </div>
     </template>
 </panel--entity-card>
