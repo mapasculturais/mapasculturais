99151cbe0083c3ef0265f63f5c808b11c16d04fc
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-09-23 20:49:56 -0300

    Padronização e estilização das telas do painel de controle

--- src/protected/application/lib/modules/UserManagement/views/panel/system-roles.php
+++ src/protected/application/lib/modules/UserManagement/views/panel/system-roles.php
@@ -4,26 +4,33 @@ $this->layout = 'panel';
 
 $this->import('
     confirm-button
-    mc-icon
+    mc-icon mc-icon
     system-roles--card
     panel--entity-tabs
     system-roles--modal
 ');
 
 ?>
-<div class="panel__row">
-    <h1>
-        <mc-icon name="role"></mc-icon>
-        <?= i::__('Funções de usuários') ?>
-    </h1>
-    <a class="panel__help-link" href="#"><?=i::__('Ajuda')?></a>
-</div>
-<div class="panel__row">
-    <p><?=i::__('Nesta seção você visualiza e gerencia as funções de usuário customizadas.')?></p>
-    <system-roles--modal list="system-role:publish"></system-roles--modal>
-</div>
 
-<div class="panel-list panel-main-content">
+<div class="panel-page">
+    <header class="panel-page__header">
+        <div class="panel-page__header-title">
+            <div class="title">
+                <div class="title__icon default"> <mc-icon name="role"></mc-icon> </div>
+                <div class="title__title"> <?= i::_e('Funções de usuários') ?> </div>
+            </div>
+            <div class="help">
+                <a class="panel__help-link" href="#"><?=i::__('Ajuda?')?></a>
+            </div>
+        </div>
+        <p class="panel-page__header-subtitle">
+            <?= i::_e('Nesta seção você visualiza e gerencia as funções de usuário customizadas.') ?>
+        </p>
+        <div class="panel-page__header-actions">
+            <system-roles--modal list="system-role:publish"></system-roles--modal>
+        </div>
+    </header>
+    
     <panel--entity-tabs type="system-role" user="" select="id,status,name,slug,permissions" #default={entity}>
         <system-roles--card :entity="entity"></system-roles--card>
     </panel--entity-tabs>
