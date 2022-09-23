c50bda90410ca401f1980db10225ed8a414f661d
robedj <ed.wassouf@gmail.com>
2022-09-23 09:03:59 -0400

    Ajustes no css do painel do menu Ref.: #2113

--- src/protected/application/lib/modules/Panel/components/panel--nav/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--nav/template.php
@@ -3,9 +3,9 @@ use MapasCulturais\i;
 
 $this->import('mc-link')
 ?>
-<nav :classes="classes">
+<nav :class="classes">
     <slot name='begin'></slot>
-    <template v-for="group in groups" :key="group.id">
+    <template  v-for="group in groups" :key="group.id">
         <h3 v-if="group.label">{{group.label}}</h3>
         <ul v-if="group.items.length > 0">
             <li 
