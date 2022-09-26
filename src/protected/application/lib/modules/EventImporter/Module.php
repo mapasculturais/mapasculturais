8986f884d61542cba842e7d526093bb5a36f9f57
LimaSystem <wolegario49@gmail.com>
2022-09-26 17:59:12 -0300

    Insere template que cria o botão para upload do arquivo CSV Ref.: #2214

--- src/protected/application/lib/modules/EventImporter/Module.php
+++ src/protected/application/lib/modules/EventImporter/Module.php
@@ -7,6 +7,14 @@ class Module extends \MapasCulturais\Module
 {
     function _init()
     {
+        $app = App::i();
+
+       //Inseri parte para upload na sidbar direita
+        $app->hook('template(opportunity.edit.sidebar-right):end',function(){
+           /** @var Theme $this */
+           $entity = $this->controller->requestedEntity; 
+           $this->part('upload-csv-event',['entity' => $entity]);
+        });
     }
 
     function register()
