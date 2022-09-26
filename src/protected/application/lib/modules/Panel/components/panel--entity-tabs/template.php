d13f576e3d390f5cec3a272d94c5d985d2a10ec1
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-09-26 18:21:10 -0300

    filtro e ordenação do componente <panel--entity-tabs> (refs #1893)

--- src/protected/application/lib/modules/Panel/components/panel--entity-tabs/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--entity-tabs/template.php
@@ -26,22 +26,24 @@ $tabs = $tabs ?? [
     <tab v-if="showTab('<?=$status?>')" cache key="<?$status?>" label="<?=$label?>" slug="<?=$status?>">
         <entities :name="type + ':<?=$status?>'" :type="type" 
             :select="select"
-            :query="queries['<?=$status?>']" :limit="50">
+            :query="queries['<?=$status?>']" 
+            :limit="50" watch-query>
 
             <template v-if="true" #header="{entities}">
-                <div class="entity-tabs__filters panel__row">
+                <form class="entity-tabs__filters panel__row" @submit="$event.preventDefault();">
                     <input type="search" class="entity-tabs__search-input"
                         aria-label="<?=i::__('Palavras-chave')?>"
                         placeholder="<?=i::__('Buscar por palavras-chave')?>"
-                        v-model="entities.query['@keyword']">
-                    <button type="button" class="button button--solid" @click="entities.refresh()">
-                        <?=i::__('Filtrar')?>
-                    </button>
-                    <button type="button" class="button button--icon button--solid">
-                        <mc-icon name="sort"></mc-icon>
-                        <?=i::__('Ordenar')?>
-                    </button>
-                </div>
+                        v-model="queries['<?=$status?>']['@keyword']">
+
+                    <select v-model="queries['<?=$status?>']['@order']">
+                        <option value="name ASC"><?= i::__('ordem alfabética') ?></option>
+                        <option value="createTimestamp DESC"><?= i::__('mais recentes primeiro') ?></option>
+                        <option value="createTimestamp ASC"><?= i::__('mais antigas primeiro') ?></option>
+                        <option value="updateTimestamp DESC"><?= i::__('modificadas recentemente') ?></option>
+                        <option value="updateTimestamp ASC"><?= i::__('modificadas há mais tempo') ?></option>
+                    </select>
+                </form>
             </template>
 
             <template #default="{entities}">
