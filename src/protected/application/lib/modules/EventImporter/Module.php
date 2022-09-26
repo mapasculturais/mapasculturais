b2c5e2c2fbcc5dfbbfe009d1defcf62cf5925147
LimaSystem <wolegario49@gmail.com>
2022-09-26 17:54:37 -0300

    Registro do controlador Ref.: #2214

--- src/protected/application/lib/modules/EventImporter/Module.php
+++ src/protected/application/lib/modules/EventImporter/Module.php
@@ -10,5 +10,9 @@ class Module extends \MapasCulturais\Module
 
     function register()
     {
+        $app = App::i();
+
+        //Registro do controloador
+        $app->registerController('eventimporter', Controller::class);
     }
 }
