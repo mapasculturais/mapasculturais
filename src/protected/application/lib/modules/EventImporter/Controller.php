e7a67f1611c0df40bfe82d769c27caa2d98199ee
LimaSystem <wolegario49@gmail.com>
2022-09-27 18:53:16 -0300

    Cria processos para ler arquivos CSV Ref.:  #2214

--- src/protected/application/lib/modules/EventImporter/Controller.php
+++ src/protected/application/lib/modules/EventImporter/Controller.php
@@ -2,10 +2,57 @@
 
 namespace EventImporter;
 
+use League\Csv\Reader;
+use MapasCulturais\App;
+use League\Csv\Statement;
 class Controller extends \MapasCulturais\Controller
 {
 
-   function POST_uploadFile()
+   function GET_uploadFile()
    {
+      $app = App::i();
+
+      $request = $this->data;
+
+      $file = $app->repo('File')->find($request['file']);
+      $file_dir = $file->path;
+      
+      if(file_exists($file_dir)){
+         $ext = pathinfo($file_dir, PATHINFO_EXTENSION);
+         $csv = $file_dir;
+         if($ext === 'zip'){
+            $csv = $this->unzipFile($file_dir);
+         }
+
+         $this->processCSV($csv);
+
+      }
+      
+   }
+
+   //Manipula arquivos compactados
+   public function unzipFile(string $file_dir){
+      $csv = null;
+      return $csv;
+   }
+
+   //Processa arquivos CSV
+   
+   public function processCSV(string $file_dir){
+
+      $stream = fopen($file_dir,'r');
+
+      $csv = Reader::createFromStream($stream);
+      $csv->setDelimiter(";");
+      $csv->setHeaderOffset(0);
+      
+      $stm = (new Statement());
+      $data = $stm->process($csv);
+
+      $result = [];
+      foreach($data as $value){
+         $result[]=$value;
+      }
+      
    }
 }
