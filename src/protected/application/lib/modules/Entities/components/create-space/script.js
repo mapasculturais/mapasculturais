e6b7772143c61b37cd1b8400bb1465353fe86ca5
robedj <ed.wassouf@gmail.com>
2022-09-26 23:04:21 -0400

    Criação do componente create-space Ref.: #2062

--- src/protected/application/lib/modules/Entities/components/create-space/script.js
+++ src/protected/application/lib/modules/Entities/components/create-space/script.js
@@ -1,32 +1,88 @@
-app.component('create-space', {
+app.component('create-space' , {
     template: $TEMPLATES['create-space'],
-    emits: [],
+    emits: ['create'],
 
-    setup(props, { slots }) {
-        const hasSlot = name => !!slots[name]
-        return { hasSlot }
+    setup() { 
+        // os textos estão localizados no arquivo texts.php deste componente 
+        const text = Utils.getTexts('create-space')
+        return { text }
     },
-
+    
     created() {
-
+        this.iterationFields()
     },
 
     data() {
         return {
-            message: 'test'
+            entity: null,
+            fields: [],
         }
     },
 
     props: {
-        entity: {
-            type: Entity,
-            required: true 
+        editable: {
+            type: Boolean,
+            default:true
+        },
+    },
+
+    computed: {
+        areaErrors() {
+            return this.entity.__validationErrors['term-area'];
+        },
+        areaClasses() {
+            return this.areaErrors ? 'field error' : 'field';
         }
     },
     
     methods: {
-        doSomething () {
+        iterationFields() {
+            let skip = [
+                'createTimestamp', 
+                'id',
+                'location',
+                'name', 
+                'shortDescription', 
+                'status', 
+                'type',
+                '_type', 
+                'userId',
+            ];
+            Object.keys($DESCRIPTIONS.space).forEach((item)=>{
+                if(!skip.includes(item) && $DESCRIPTIONS.space[item].required){
+                    this.fields.push(item);
+                }
+            })
+        },
+        createEntity() {
+            this.entity = Vue.ref(new Entity('space'));
+            console.log(this.entity);
+            this.entity.type = 1;
+            this.entity.terms = {area: []}
 
+        },
+        createDraft(modal) {
+            this.entity.status = 0;
+            this.save(modal);
+        },
+        createPublic(modal) {
+            //lançar dois eventos
+            this.entity.status = 1;
+            this.save(modal);
+        },
+        save (modal) {
+            modal.loading(true);
+            this.entity.save().then((response) => {
+                modal.close();
+                this.$emit('create',response)
+            }).catch((e) => {
+                modal.loading(false);
+            });
+        },
+
+        destroyEntity() {
+            // para o conteúdo da modal não sumir antes dela fechar
+            setTimeout(() => this.entity = null, 200);
         }
     },
 });
--- src/protected/application/lib/modules/Entities/components/create-space/template.php
+++ src/protected/application/lib/modules/Entities/components/create-space/template.php
@@ -1,19 +1,27 @@
 <?php 
 use MapasCulturais\i;
+$this->import('
+    entity-field 
+    entity-terms
+    modal 
+'); 
 ?>
-
-<div class="create-space">
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
+<modal title="Criar Espaço" classes="create-modal" button-label="Criar Espaço" @open="createEntity()" @close="destroyEntity()">
+    <template v-if="entity" #default>
+        <label><?php i::_e('Crie um espaço com informações básicas')?><br><?php i::_e('e de forma rápida')?></label>
+        <div class="create-modal__fields">
+        <entity-field :entity="entity" hide-required label=<?php i::esc_attr_e("Nome ou título")?>  prop="name"></entity-field>
+        <entity-field :entity="entity" hide-required  :editable="true" label="<?php i::esc_attr_e("Selecione o tipo do espaço")?>" prop="type"></entity-field>
+            <entity-terms :entity="entity" :editable="true" :classes="areaClasses" taxonomy='area' title="<?php i::esc_attr_e("Área de Atuação") ?>"></entity-terms>
+            <small class="field__error" v-if="areaErrors">{{areaErrors.join(', ')}}</small>
+        </div>
+    </template>
+    <template #button="modal">
+        <slot :modal="modal"></slot>
+    </template>
+    <template #actions="modal">
+        <button class="button button--primary" @click="createPublic(modal)"><?php i::_e('Criar e Publicar')?></button>
+        <button class="button button--solid-dark" @click="createDraft(modal)"><?php i::_e('Criar em Rascunho')?></button>
+        <button class="button button--text button--text-del " @click="modal.close()"><?php i::_e('Cancelar')?></button>
+    </template>
+</modal>
