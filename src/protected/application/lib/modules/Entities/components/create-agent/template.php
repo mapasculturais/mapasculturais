2dce314edba633a3d3d83dd1ce5006308f6adace
robedj <ed.wassouf@gmail.com>
2022-09-30 17:00:17 -0400

    ajuste no template do create-agent Ref.: #2061

--- src/protected/application/lib/modules/Entities/components/create-agent/template.php
+++ src/protected/application/lib/modules/Entities/components/create-agent/template.php
@@ -1,5 +1,6 @@
  <?php 
 use MapasCulturais\i;
+ 
 $this->import('
     entity-field 
     entity-terms
@@ -9,7 +10,7 @@ $this->import('
 ?>
 
 <modal title="Criar Agente" classes="create-modal" button-label="Criar Agente" @open="createEntity()" @close="destroyEntity()">
-    <template v-if="entity" #default>
+    <template v-if="entity && !entity.id" #default>
         <label><?php i::_e('Crie um agente com informações básicas')?><br><?php i::_e('e de forma rápida')?></label>
         <div class="create-modal__fields">
             <entity-field :entity="entity" hide-required  :editable="true" label="<?php i::esc_attr_e("Selecione o tipo do agente")?>" prop="type"></entity-field>
@@ -18,10 +19,11 @@ $this->import('
             <small class="field__error" v-if="areaErrors">{{areaErrors.join(', ')}}</small>
             <entity-field :entity="entity" hide-required prop="shortDescription" label="<?php i::esc_attr_e("Adicione uma Descrição curta para o Agente")?>"></entity-field>
             <entity-field :entity="entity" hide-required v-for="field in fields" :prop="field"></entity-field>
+           {{entity.id}}
         </div>
     </template>
 
-    <template title="Agente Criado" v-if="entity?.id" #default>
+    <template v-if="entity?.id" #default>
         <h4><strong><?php i::_e('Agente criado!')?> </strong></h4>
         <label><?php i::_e('Você pode completar as informações do seu agente agora ou pode deixar para depois.  ');?></label>
     </template>
