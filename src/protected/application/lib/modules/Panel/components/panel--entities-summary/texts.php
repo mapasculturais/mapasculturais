89a73394de023cfd1826ff0708d897f93fa35bbb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-10-03 10:30:58 -0300

    Refatoração na descrição dos cards (Ref.: #2205)  - Descrições no singular/plural dependendo da quantidade de itens  - Adiciona tradução nas descrições

--- src/protected/application/lib/modules/Panel/components/panel--entities-summary/texts.php
+++ src/protected/application/lib/modules/Panel/components/panel--entities-summary/texts.php
@@ -2,4 +2,14 @@
 use MapasCulturais\i;
 
 return [
+    'agente' => i::__('Agente'),
+    'agentes' => i::__('Agentes'),
+    'oportunidade' => i::__('Oportunidade'),
+    'oportunidades' => i::__('Oportunidades'),
+    'evento' => i::__('Evento'),
+    'eventos' => i::__('Eventos'),
+    'espaço' => i::__('Espaço'),
+    'espaços' => i::__('Espaços'),
+    'projeto' => i::__('Projeto'),
+    'projetos' => i::__('Projetos'),
 ];
