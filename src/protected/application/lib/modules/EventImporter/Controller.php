2eb729d49ae177f67234bd9c22d44cd79e349186
LimaSystem <wolegario49@gmail.com>
2022-10-04 11:53:01 -0300

    Remove processamento de arquivos do tipo zip Ref.: #2215

--- src/protected/application/lib/modules/EventImporter/Controller.php
+++ src/protected/application/lib/modules/EventImporter/Controller.php
@@ -3,14 +3,10 @@
 namespace EventImporter;
 
 use Exception;
-use ZipArchive;
 use League\Csv\Reader;
-use FilesystemIterator;
 use MapasCulturais\App;
 use League\Csv\Statement;
 use MapasCulturais\Entities\Event;
-use RecursiveIteratorIterator;
-use RecursiveDirectoryIterator;
 
 class Controller extends \MapasCulturais\Controller
 {
@@ -24,52 +20,10 @@ class Controller extends \MapasCulturais\Controller
       $file_dir = $file->path;
 
       if (file_exists($file_dir)) {
-         $ext = pathinfo($file_dir, PATHINFO_EXTENSION);
-         $csv = $file_dir;
-
-         if ($ext === 'zip') {
-            $csv = $this->unzipFile($file_dir);
-         }
-
-         $data = $this->processCSV($csv);
+         $data = $this->processCSV($file_dir);
+      }else{
+         throw new Exception("Arquivo CSV não existe. Erro ao processar");
       }
-   }
-
-   //Manipula arquivos compactados
-   public function unzipFile(string $file_dir)
-   {
-      $request = $this->data;
-
-      $csv_file_dir = [];
-      if (file_exists($file_dir)) {
-         $dir_base = dirname($file_dir);
-         $extract_dir = $dir_base . '/' . md5($file_dir . '_file' . $request['file']);
-
-         $zip = new ZipArchive();
-         if ($zip->open($file_dir)) {
-            $zip->extractTo($extract_dir);
-            $zip->close();
-
-            if (file_exists($extract_dir)) {
-
-               $allowed_files = ['text/csv', 'image/jpeg', 'image/png', 'image/jpg', 'text/plain'];
-
-               $recusive_dir = new RecursiveDirectoryIterator($extract_dir, FilesystemIterator::SKIP_DOTS);
-               $files = new RecursiveIteratorIterator($recusive_dir);
-               $finfo = finfo_open(FILEINFO_MIME_TYPE);
-
-               foreach ($files as $file) {
-                  $mime = finfo_file($finfo, $file);
-                  if (!in_array($mime, $allowed_files)) {
-                     die("Arquivo {$file} é inválido");
-                  }
-               }
-
-               $csv_file_dir = glob($extract_dir . '/*.csv');
-            }
-         }
-      }
-      return $csv_file_dir[0];
    }
 
    //Processa arquivos CSV
@@ -117,7 +71,5 @@ class Controller extends \MapasCulturais\Controller
          $event->projectId = $project->id;
          $event->save(true);
       }
-
-
    }
 }
