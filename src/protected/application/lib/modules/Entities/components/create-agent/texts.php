e3197d8b21f92dbeeb6c770a8c9b961395729e99
robedj <ed.wassouf@gmail.com>
2022-10-04 08:19:00 -0400

    W.I.P: Alteração no arquivo texts do create-agent para adicionar a tradução do rascunho do agente Ref.: #2061

--- src/protected/application/lib/modules/Entities/components/create-agent/texts.php
+++ src/protected/application/lib/modules/Entities/components/create-agent/texts.php
@@ -4,4 +4,5 @@ use MapasCulturais\i;
 return [
     'agenteCriado' => i::__('Agente Criado!'),
     'criarAgente' => i::__('Criar Agente'),
+    'criarRascunho' => i::__('Agente criado em rascunho!'),
 ];
