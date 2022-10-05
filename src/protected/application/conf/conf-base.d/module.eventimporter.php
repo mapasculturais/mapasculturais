359c733cf5f84097abae97fdb98c8b17f4a395b8
LimaSystem <wolegario49@gmail.com>
2022-10-05 17:50:55 -0300

    Cria arquivo de configuração com valores default Ref .: # 2215

---
+++ src/protected/application/conf/conf-base.d/module.eventimporter.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "eventimporter.frequence_list" => ['uma vez','once','semanal','weekly','diariamente','daily'],
+    "eventimporter.rating_list" => ['livre','10 anos','12 anos','14 anos','16 anos','18 anos'],
+    "eventimporter.days_list_positive" => ['sim','x','1'],
+];
