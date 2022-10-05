6b5937bcba891b83f51c8efb6bf193c918ae7010
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-10-05 16:18:21 -0300

    Possibilidade da visão definir o ítem de menu ativo
    ```
    $this->activeNav = 'panel/agents'; // ativa o menu Meus Agentes
    ```

--- src/protected/application/lib/modules/Panel/components/panel--nav/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--nav/template.php
@@ -1,18 +1,15 @@
 <?php
 use MapasCulturais\i;
 
-$this->import('mc-link')
+$this->import('mc-link');
 ?>
 <nav :class="classes">
     <slot name='begin'></slot>
     <template  v-for="group in groups" :key="group.id">
         <h3 v-if="group.label">{{group.label}}</h3>
         <ul v-if="group.items.length > 0">
-            <li 
-                v-for="item in group.items" 
-                :key="`${group.id}:${item.route}`" 
-                >
-                <mc-link :route="item.route" :icon="item.icon" :class="{'active': item.route == route}">{{item.label}}</mc-link>
+            <li v-for="item in group.items" :key="`${group.id}:${item.route}`" >
+                <mc-link :route="item.route" :icon="item.icon" :class="{'active': active(item)}">{{item.label}}</mc-link>
             </li>
         </ul>
     </template>
