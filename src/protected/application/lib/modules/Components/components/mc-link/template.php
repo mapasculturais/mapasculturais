04ac95a1ad51016090d91d75135a07687d91a976
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-10-05 11:40:22 -0300

    possibilidade de posicionar o ícone do mc-link à direita utilizando a propriedade right-icon

--- src/protected/application/lib/modules/Components/components/mc-link/template.php
+++ src/protected/application/lib/modules/Components/components/mc-link/template.php
@@ -2,11 +2,18 @@
 $this->import('mc-icon');
 ?>
 <a :href="url" :class="classes">
+    <template v-if="!rightIcon">
     <mc-icon v-if="typeof icon != 'string' && icon && entity" :entity="entity"></mc-icon>
     <mc-icon v-else-if="icon && !entity" :name="icon"></mc-icon>
     <mc-icon v-else-if="typeof icon == 'string'" :name="icon"></mc-icon>
+    </template>
     <slot>
         <template v-if="label">{{label}}</template>
         <template v-if="!label && entity">{{entity.name}}</template>
     </slot>
+    <template v-if="rightIcon">
+    <mc-icon v-if="typeof icon != 'string' && icon && entity" :entity="entity"></mc-icon>
+    <mc-icon v-else-if="icon && !entity" :name="icon"></mc-icon>
+    <mc-icon v-else-if="typeof icon == 'string'" :name="icon"></mc-icon>
+    </template>
 </a>
