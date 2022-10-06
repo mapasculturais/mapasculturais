971b6a6e1ec214d6a9f9f75895b10b53b2683580
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-10-06 07:12:32 -0300

    Estilização e estrutura do componente <mc-multiselect>

--- src/protected/application/lib/modules/Components/components/mc-multiselect/template.php
+++ src/protected/application/lib/modules/Components/components/mc-multiselect/template.php
@@ -10,20 +10,25 @@ $this->import('popover');
 
         <template #default="popover">
             <div class="mc-multiselect__content">
-                <input v-if="!hideFilter" type="text" v-model="model.filter" class="mc-multiselect__content--input" placeholder="<?= i::__('Filtro') ?>">
 
-                <ul v-if="items.length > 0" class="mc-multiselect__content--list">
+                <div class="mc-multiselect__content-form">
+                    <input v-if="!hideFilter" type="text" v-model="model.filter" class="input" placeholder="<?= i::__('Filtro') ?>">
+                </div>
+
+                <ul v-if="items.length > 0" class="mc-multiselect__content-list">
                     <li v-for="item in filteredItems">
-                        <label class="mc-multiselect__content--list-item">
+                        <label class="item">
                             <input type="checkbox" :checked="model.indexOf(item) >= 0" @change="toggleItem(item, popover)" class="input">
                             <span class="text" v-html="highlightedItem(item)"></span>
                         </label>
                     </li>
                 </ul>
-
-                <button v-if="!hideButton" class="button button--solid button--solid-dark" @click="popover.toggle()">
-                    <?php i::_e('Confirmar') ?>
-                </button>
+                
+                <div class="mc-multiselect__content-button">
+                    <button v-if="!hideButton" class="button button--primary" @click="popover.toggle()">
+                        <?php i::_e('Confirmar') ?>
+                    </button>
+                </div>
             </div>
         </template>
     </popover>
