b2752155f10ade0e5544797a2aa9d426675ae0ec
erleibiazzio <erleibiazzio@gmail.com>
2022-10-07 17:28:09 -0300

    Criação de novas configurações default Ref.:#2215

--- src/protected/application/conf/conf-base.d/module.eventimporter.php
+++ src/protected/application/conf/conf-base.d/module.eventimporter.php
@@ -2,13 +2,13 @@
 
 return [
     "eventimporter.week_days" => [
-        'MODAY',
-        'TUESDAY',
-        'WEDNESDAY',
-        'THURSDAY',
-        'FRIDAY',
-        'SATURDAY',
-        'SUNDAY'
+        'MODAY' => 'seg',
+        'TUESDAY' => 'ter',
+        'WEDNESDAY' => 'qua',
+        'THURSDAY' => 'qui',
+        'FRIDAY' => 'sex',
+        'SATURDAY' => 'sab',
+        'SUNDAY' => 'dom'
     ],
     "eventimporter.frequence_list_allowed" => [
         'uma vez' => 'once',
@@ -18,7 +18,21 @@ return [
         'diariamente' => 'daily',
         'daily' => 'daily',
     ],
-    "eventimporter.use_endsat" => ['uma vez','once'],
-    "eventimporter.rating_list_allowed" => ['livre','10 anos','12 anos','14 anos','16 anos','18 anos'],
-    "eventimporter.days_list_positive" => ['sim','x','1'],
+    "eventimporter.use_endsat" => ['uma vez', 'once'],
+    "eventimporter.rating_list_allowed" => ['livre', '10 anos', '12 anos', '14 anos', '16 anos', '18 anos'],
+    "eventimporter.days_list_positive" => ['sim', 'x', '1'],
+    "eventimporter.dic_months" => [
+        "January" => "Janeiro",
+        "February" => "Fevereiro",
+        "March" => "Março",
+        "April" => "Abril",
+        "May" => "Maio",
+        "June" => "Junho",
+        "July" => "Julho",
+        "August" => "Agosto",
+        "September" => "Setembro",
+        "October" => "Outubro",
+        "November" => "Novembro",
+        "December" => "Dezembro",
+    ]
 ];
