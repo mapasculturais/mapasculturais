f6138ae36afb6b10e3fa3daaa872645fbcdbed21
robedj <ed.wassouf@gmail.com>
2022-10-06 22:53:18 -0400

    W.I.P ajuste na pagina do projeto, criação do componente entity-activity-card para atividades do projeto [placeholder] ref.: #2067

---
+++ src/protected/application/lib/modules/Entities/components/entity-activity-card/template.php
@@ -0,0 +1,32 @@
+<?php
+
+use MapasCulturais\i;
+
+$this->import('
+    mc-icon  mc-map-card
+');
+?>
+<div class="entity-activity-card">
+    <div class="entity-activity-card__header">
+        <label class="entity-activity-card__header--title">Atividades do projeto</label>
+    </div>
+    <div class="entity-activity-card__content ">
+        <div class="entity-activity-card__content--header">
+            <label>fechoNome da Entidade Inserida</label>
+        </div>
+        <div class="entity-activity-card__content--info">
+            ahsuihasihauhuiashuiashuiahuihsauihushasuihasuihsuisahuiasuias
+        </div>
+    </div>
+    <div class="entity-activity-card__aside">
+        <div class="entity-activity-card__aside--left">
+            <mc-icon name="edit"></mc-icon>
+            <label class="entity-activity-card__aside--left-label">Editar</label>
+
+        </div>
+        <div class="entity-activity-card__aside--right">
+            <mc-icon name="trash"></mc-icon><label>Excluir</label>
+
+        </div>
+    </div>
+</div>
