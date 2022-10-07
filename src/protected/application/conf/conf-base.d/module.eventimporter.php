71686e2d032ed2da2670906224142cf8a1896b4d
erleibiazzio <erleibiazzio@gmail.com>
2022-10-06 21:01:38 -0300

    Ajusta configurações default do Modulo Ref.:#2245

--- src/protected/application/conf/conf-base.d/module.eventimporter.php
+++ src/protected/application/conf/conf-base.d/module.eventimporter.php
@@ -1,7 +1,24 @@
 <?php
 
 return [
-    "eventimporter.frequence_list" => ['uma vez','once','semanal','weekly','diariamente','daily'],
-    "eventimporter.rating_list" => ['livre','10 anos','12 anos','14 anos','16 anos','18 anos'],
+    "eventimporter.week_days" => [
+        'MODAY',
+        'TUESDAY',
+        'WEDNESDAY',
+        'THURSDAY',
+        'FRIDAY',
+        'SATURDAY',
+        'SUNDAY'
+    ],
+    "eventimporter.frequence_list_allowed" => [
+        'uma vez' => 'once',
+        'once' => 'once',
+        'semanal' => 'weekly',
+        'weekly' => 'weekly',
+        'diariamente' => 'daily',
+        'daily' => 'daily',
+    ],
+    "eventimporter.use_endsat" => ['uma vez','once'],
+    "eventimporter.rating_list_allowed" => ['livre','10 anos','12 anos','14 anos','16 anos','18 anos'],
     "eventimporter.days_list_positive" => ['sim','x','1'],
 ];
