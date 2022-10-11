a04e937cae3c5019709f010e6d564b366c407373
erleibiazzio <erleibiazzio@gmail.com>
2022-10-11 13:49:23 -0300

    Deixa a configuração default de permissão diretamente no Modulo Ref.: #2215

--- src/protected/application/conf/conf-base.d/module.eventimporter.php
+++ src/protected/application/conf/conf-base.d/module.eventimporter.php
@@ -2,9 +2,5 @@
 
 return [
     'module.EventImporter' => [
-        "enabled" => function(){
-            $app = MapasCulturais\App::i();
-            return $app->user->is("admin");
-        },
     ]
 ];
