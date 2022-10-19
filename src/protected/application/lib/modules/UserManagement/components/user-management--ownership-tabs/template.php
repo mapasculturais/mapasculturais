5181a8d1ebb9b2fb09de3c104cf378fb8ebedf26
robedj <ed.wassouf@gmail.com>
2022-10-19 17:19:03 -0400

    Ajustes na estilização user-details.scss Ref.: #2223

--- src/protected/application/lib/modules/UserManagement/components/user-management--ownership-tabs/template.php
+++ src/protected/application/lib/modules/UserManagement/components/user-management--ownership-tabs/template.php
@@ -8,7 +8,7 @@ $this->import('
 ?>
 <panel--entity-tabs :type='type' :user="user.id" :select="select">
     <template #before-list>
-        <div class="grid-12 panel--entity-tabs">
+        <div class="grid-12 panel--entity-tabs--identify">
             <div class="col-2"><?= i::__('ID') ?></div>
             <div class="col-4"><?= i::__('Nome') ?></div>
             <div class="col-3"><?= i::__('Subsite') ?></div>
@@ -22,13 +22,14 @@ $this->import('
             <div class="col-3">{{entity.subsite?.name}}</div>
             <div class="col-3 panel--entity-tabs__content--editable">
                 
-                <mc-link :entity="entity" route="edit" icon='edit' class="button button--icon"><?= i::__('Editar') ?></mc-link>
+                <mc-link :entity="entity" route="edit" icon='edit' class="button button--icon panel--entity-tabs__content--editable-edit"><?= i::__('Editar') ?></mc-link>
                 <panel--entity-actions 
                     :entity="entity"
                     @deleted="moveEntity(entity)" 
                     @archived="moveEntity(entity)" 
                     @published="moveEntity(entity)"
-                >
+                    class="panel__entity-actions--editable"
+                    >
                 </panel--entity-actions>
             </div>
         </div>
