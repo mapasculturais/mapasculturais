114990dca0d42f7b66f62d296d24909db1c7635a
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-10-25 10:38:19 -0300

    Ajustes na listagem de ocorrências dos eventos (Ref.: #2174)  - Ajustes na versão editável do componente  - Formatação no valor da entrada do evento  - Ajustes de estilizaçao do componente  - Consulta feita através do componente <entities>

--- src/protected/application/lib/modules/Entities/components/entity-occurrence-list/script.js
+++ src/protected/application/lib/modules/Entities/components/entity-occurrence-list/script.js
@@ -1,27 +1,11 @@
 app.component('entity-occurrence-list', {
     template: $TEMPLATES['entity-occurrence-list'],
-    
-    // define os eventos que este componente emite
-    emits: ['namesDefined'],
 
     setup() { 
         // os textos estão localizados no arquivo texts.php deste componente 
         const text = Utils.getTexts('entity-occurrence-list')
         return { text }
     },
-
-    beforeCreate() { },
-    created() {
-    },
-
-    beforeMount() { },
-    mounted() { },
-
-    beforeUpdate() { },
-    updated() { },
-
-    beforeUnmount() {},
-    unmounted() {},
 
     props: {
         entity: {
@@ -32,43 +16,19 @@ app.component('entity-occurrence-list', {
             type: Boolean,
             default: false
         },
-        pseudoQuery: {
-            type: Object,
-            default: {}
-        },
     },
 
     data() {
-        return {
-            occurrences: [],
-            loading: false,
-            space: this.entity /* Alterar do this.entity para o espaço vinculado */
-        }
+        return {}
     },
 
-    computed: {
-        spaceQuery() {
-            const query = Utils.parsePseudoQuery({
-                'space:id': this.space.id,
-                ...this.pseudoQuery
-            });
-
-            query['event:@select'] = 'id,name,terms,files.avatar,classificacaoEtaria';
-
-            return query;
-        }
-    },
-    
     methods: {
-        spaceRawProcessor (entity) {
-            entity = Utils.entityRawProcessor(entity);
-            entity['@icon'] = 'event';
-            
-            return entity;
-        },
-
-        occurrenceRawProcessor (entity) {
-            return Utils.occurrenceRawProcessor(entity, this.eventApi);
+        formatPrice(price) {
+            if (/^\d+(?:\.\d+)?$/.test(price)) {
+	            return parseFloat(price).toLocaleString('pt-BR', { style: 'currency', currency: __('currency', 'entity-occurrence-list')  });
+            } else {
+                return price;
+            }
         },
     },
 });
--- src/protected/application/lib/modules/Entities/components/entity-occurrence-list/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-occurrence-list/template.php
@@ -3,8 +3,6 @@
 use MapasCulturais\i;
 
 $this->import('
-    modal
-    entity-field
     create-occurrence
     entities
     mc-icon
@@ -12,23 +10,20 @@ $this->import('
 ?>
 
 <div class="entity-occurrence-list">
-
-    <div class="entity-occurrence-list__editable">
+    <div v-if="editable" class="entity-occurrence-list__editable">
         <label class="entity-occurrence-list__editable--title">
             <?= i::_e('Data, hora e local do evento') ?>
         </label>
         <p class="entity-occurrence-list__editable--description">
             <?= i::_e('Adicione data, hora e local da ocorrência do seu evento. Você pode criar várias ocorrências com informações diferentes.') ?>
         </p>
-        <create-occurrence v-if="editable" @create="addToOccurrenceList($event)"></create-occurrence>
+        <create-occurrence @create="addToOccurrenceList($event)"></create-occurrence>
     </div>
-
     <div class="entity-occurrence-list__occurrences">
-
-        <entities type="event" endpoint="occurrences" :raw-processor="occurrenceRawProcessor" :query="spaceQuery" watch-query>
-
+        <label class="occurrence__title">Agenda</label>
+        <entities type="eventOccurrence" endpoint="find" :query="{event: `EQ(${entity.id})`}" select="*,space.{name,endereco,files.avatar}" loading>
             <template #default="{entities}">
-                <div v-for="occurrence in entities" class="occurrence" :key="occurrence._reccurrence_string">
+                <div v-for="occurrence in entities" class="occurrence" :class="{'edit': editable}" :key="occurrence._reccurrence_string">
                     <div class="occurrence__header">
                         <div class="occurrence__header--title">
                             <mc-icon name="pin"></mc-icon> {{occurrence.space.name}}
@@ -37,15 +32,24 @@ $this->import('
                             <mc-icon name="map"></mc-icon> <?= i::_e('Ver mapa') ?>
                         </a>
                     </div>
+                    <div class="occurrence__address">
+                        <p>{{occurrence.space.endereco}}</p>
+                    </div>
                     <div class="occurrence__content">
                         <div class="occurrence__content--ticket">
-                            <mc-icon name="date"></mc-icon> {{occurrence.starts._date.toLocaleString('pt-BR').substr(0, 10)}}
+                            <mc-icon name="date"></mc-icon>
+                            <span class="ticket">
+                                {{occurrence.rule.startsOn}}
+                            </span>
                         </div>
                         <div class="occurrence__content--price">
-                            <mc-icon name="ticket"></mc-icon> <?= i::_e('Detalhes da entrada') ?>
+                            <mc-icon name="ticket"></mc-icon>
+                            <span class="price">
+                                {{formatPrice(occurrence.rule.price)}}
+                            </span>
                         </div>
                     </div>
-                    <div class="occurrence__actions">
+                    <div v-if="editable" class="occurrence__actions">
                         <a class="occurrence__actions--edit">
                             <mc-icon name="edit"></mc-icon><?= i::_e('Editar') ?>
                         </a>
@@ -55,12 +59,11 @@ $this->import('
                     </div>
                 </div>
             </template>
-
             <template #loading>
-                <div> <mc-icon name="loading"></mc-icon> </div>
+                <div>
+                    <mc-icon name="loading"></mc-icon>
+                </div>
             </template>
-
         </entities>
-
     </div>
 </div>
