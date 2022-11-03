d09cd7177ff4e5259835cadd3f6df9d2b68d7119
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-11-03 15:00:06 -0300

    Pequeno reparo no mc-tag-list - for com parâmetro errado

--- src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
+++ src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
@@ -4,7 +4,7 @@ use MapasCulturais\i;
 
 <div class="mc-tag-list">
     <ul class="mc-tag-list__tagList">
-        <li :class="[classes, 'mc-tag-list__tagList--tag']" v-for="tag in itens"> 
+        <li :class="[classes, 'mc-tag-list__tagList--tag']" v-for="tag in tags"> 
             {{this.labels ? this.labels[tag] : tag}}
             <mc-icon v-if="editable" name="delete" @click="remove(tag)"></mc-icon>             
         </li>
