8ebfd82531b25521085dbd2a37c604412fe4bfeb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-11-03 11:07:16 -0300

    Adiciona nova propriedade opcional para as labels no componente <mc-tag-list> (Ref.: #2226)

--- src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
+++ src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
@@ -4,10 +4,9 @@ use MapasCulturais\i;
 
 <div class="mc-tag-list">
     <ul class="mc-tag-list__tagList">
-        <li :class="[classes, 'mc-tag-list__tagList--tag']" v-for="tag in tags "> 
-            {{tag}}
-            <mc-icon v-if="editable"  name="delete" @click="remove(tag)"></mc-icon> 
-            
+        <li :class="[classes, 'mc-tag-list__tagList--tag']" v-for="tag in itens"> 
+            {{this.labels ? this.labels[tag] : tag}}
+            <mc-icon v-if="editable" name="delete" @click="remove(tag)"></mc-icon>             
         </li>
     </ul>
 </div>
