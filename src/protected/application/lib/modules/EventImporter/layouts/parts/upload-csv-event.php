b0ee209f4d324f2ffc70dd9be99f9032049aa9a7
erleibiazzio <erleibiazzio@gmail.com>
2022-11-09 11:01:22 -0300

    Ajusta array que salva informações do Ref.:#2215

--- src/protected/application/lib/modules/EventImporter/layouts/parts/upload-csv-event.php
+++ src/protected/application/lib/modules/EventImporter/layouts/parts/upload-csv-event.php
@@ -26,18 +26,25 @@ $template = '
     </div>
     <div class="js-eventImporter">
         <?php if(is_array($files)): foreach($files as $file): ?>
-            <?php $file_process = $app->user->profile->event_importer_processed_file?>
+            <?php $file_process = $app->user->profile->event_importer_processed_file->{$file->name}?>
             <article id="file-<?php echo $file->id ?>" class="objeto <?php if($this->isEditable()) echo i::_e(' is-editable'); ?>" >
                 <h1><a href="<?php echo $file->url;?>" download><?php echo $file->description ? $file->description : $file->name;?></a></h1>
                 <div class="objeto-meta">
-                    <?php if($processed_at = $file_process->{$file->name} ?? null): ?>                        
+                    <?php if($file_process): ?>                        
                     <div><span class="label"><?= i::_e('Arquivo:')?> </span> <?=$file->name?></div>
                     <div><span class="label"><?= i::_e('Data de envio:')?> </span> <?=$file->createTimestamp->format("d/m/Y H:i")?></div>
-                    <div><span class="label"><?= i::_e('processado em:')?> </span> <?=$processed_at?></div>
+                    <div><span class="label"><?= i::_e('processado em:')?> </span> <?=$file_process->date?></div>
+                    <div><span class="label"><?= i::_e('Quantidade processada:')?> </span> <?=$file_process->countProsess?></div> <br> <br>
+
+                    <span class="label"><?= i::_e('ID dos Eventos:')?></span> <br>
+                    <?php foreach($file_process->eventsIdList as $id):?>
+                        <?php $event_url = $app->createUrl("evento/{$id}")?>
+                        <div><a href="<?=$event_url?>" target="_blank"><?=$id?></a></div>
+                    <?php endforeach;?>
                     <?php endif; ?>
                 </div>
                 <div class="entity-actions">
-                    <?php if(!$processed_at = $file_process->{$file->name} ?? null): ?>
+                    <?php if(!$file_process): ?>
                     <a href="<?=$url?>?file=<?=$file->id?>" class="btn btn-small btn-primary js-validador-process"><?= i::_e('Processar')?></a>
                     <a data-href="<?php echo $file->deleteUrl?>" data-target="#file-<?php echo $file->id ?>" data-configm-message="Remover este arquivo?" class="btn btn-small btn-danger js-remove-item"><?= i::_e('Excluir')?></a>
                     <?php endif; ?>
