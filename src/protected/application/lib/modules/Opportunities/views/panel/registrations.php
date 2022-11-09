42a7cde1d5eebda5955dee726b554133b76862c4
robedj <ed.wassouf@gmail.com>
2022-11-09 13:05:22 -0400

    Criação da pagina do painel minhas oportunidades Ref.: #2182

---
+++ src/protected/application/lib/modules/Opportunities/views/panel/registrations.php
@@ -0,0 +1,32 @@
+<?php
+use MapasCulturais\i;
+$this->import('
+    mc-icon 
+    panel--entity-card
+    panel--entity-tabs 
+');
+?>
+
+<div class="panel-page">
+    <header class="panel-page__header">
+        <div class="panel-page__header-title">
+            <div class="title">
+                <div class="title__icon opportunity__background">
+                    <mc-icon name="opportunity"></mc-icon>
+                </div>
+                <div class="title__title"> <?= i::_e('Minhas inscrições') ?> </div>
+            </div>
+            <div class="help">
+                <a class="panel__help-link" href="#"><?= i::__('Ajuda?') ?></a>
+            </div>
+        </div>
+        <p class="panel-page__header-subtitle">
+            <?= i::_e('Nesta seção você pode adicionar e gerenciar suas inscrições') ?>
+        </p>
+        <div class="panel-page__header-actions">
+            
+        </div>
+    </header>
+
+    <panel--entity-tabs type="registration"></panel--entity-tabs>
+</div>
