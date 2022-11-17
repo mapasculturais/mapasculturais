1e518f9cbbd7cc750fb8c940e4df7a6a0aa5e471
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-11-17 15:48:24 -0300

    Ajusta o tamanho das imagens de capa das entidades

--- src/protected/application/lib/modules/Entities/components/entity-cover/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-cover/template.php
@@ -3,14 +3,12 @@ use MapasCulturais\i;
 ?>
 
 <div :class = "classes" class="entity-cover">
-    <image-uploader :entity="entity" group="header" :aspect-ratio="99/16" :circular="false">
+    <image-uploader :entity="entity" group="header" :aspect-ratio="3/1" :circular="false">
         <template #default="modal">
-            <div class="entity-cover__cover">      
-
+            <div class="entity-cover__cover">  
                 <div v-if="entity.files.header" id="header<?= date("Ymd") ?>" class="entity-cover__cover--img" :style="{ '--url': 'url('+entity.files.header?.transformations?.header.url+')' }">
                     <label class="label" for="header<?= date("Ymd") ?>"> <?php i::_e("Alterar Imagem de Capa")?> </label>
                 </div>
-
                 <div v-else class="entity-cover__cover--newImg">
                     <mc-icon name="image"></mc-icon>
                     <label class="label"> <?php i::_e("Adicionar Imagem de Capa")?> </label>
