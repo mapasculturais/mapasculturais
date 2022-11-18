eb7d95a58b94b3a962724d9b24257ca506a7866f
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-11-17 21:54:32 -0300

    Ajustes de estilização nos componentes home-opportunities e home-feature (Ref.: #2234)

--- src/protected/application/lib/modules/Home/components/home-opportunities/template.php
+++ src/protected/application/lib/modules/Home/components/home-opportunities/template.php
@@ -5,33 +5,34 @@ $this->import('entities entity-card');
 ?>
 
 <div class="home-opportunities">
-    <div class="home-opportunities__content">
-        <div class="home-opportunities__content--title">
-            <label> <?php i::_e('Oportunidades do momento')?> </label>
-        </div>
-
-        <div class="home-opportunities__content--description">
-        <label> <?php i::_e('Cadastre-se, participe de editais e oportunidade e concorra aos benefícios sem sair de casa')?> </label>
-        </div>
-
-        <div class="home-opportunities__content--cards">
-            <entities type="opportunity" :query="getQuery">
-                <template #default="{entities}">
-                    
-                    <carousel v-if="entities.length > 0" :settings="settings" :breakpoints="breakpoints">
-                        <slide v-for="entity in entities" :key="entity.id">
-                            <entity-card :entity="entity"></entity-card> 
-                        </slide>                        
-
-                        <template v-if="entities.length > 1" #addons>
-                            <div class="actions">
-                                <navigation :slideWidth="368" />
-                            </div>
-                        </template>
-                    </carousel>
-
-                </template>
-            </entities>
-        </div>
-    </div>
+	<div class="home-opportunities__header">
+		<div class="home-opportunities__header title">
+			<label> <?php i::_e('Oportunidades do momento')?> </label>
+		</div>        
+		<div class="home-opportunities__header description">
+			<label> <?php i::_e('Cadastre-se, participe de editais e oportunidade e concorra aos benefícios sem sair de casa')?> </label>
+		</div>
+	</div>    
+	<div class="home-opportunities__content">
+		<div class="home-opportunities__content cards">
+			<entities type="opportunity" :query="getQuery">
+				<template #default="{entities}">                    
+					<carousel v-if="entities.length > 0" :settings="settings" :breakpoints="breakpoints">
+						<slide v-for="entity in entities" :key="entity.id">
+							<entity-card :entity="entity">
+								<template #labels> 
+									Inscrições abertas
+								</template>
+							</entity-card> 
+						</slide> 
+						<template v-if="entities.length > 1" #addons>
+							<div class="actions">
+								<navigation :slideWidth="368" />
+							</div>
+						</template>
+					</carousel>
+				</template>
+			</entities>
+		</div>
+	</div>
 </div>
