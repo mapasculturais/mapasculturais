1bc51910e3bb344984098c6be18150157570b35b
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-11-17 21:54:32 -0300

    Ajustes de estilização nos componentes home-opportunities e home-feature (Ref.: #2234)

--- src/protected/application/lib/modules/Home/components/home-feature/template.php
+++ src/protected/application/lib/modules/Home/components/home-feature/template.php
@@ -2,69 +2,87 @@
 use MapasCulturais\i;
 
 $this->import('
-    entities 
-    entity-card 
-    loading
-    tabs
+	entities 
+	entity-card 
+	loading
+	tabs
 ');
 ?>
 
 <div class="home-feature">
-    <div class="home-feature__content">
-        <div class="home-feature__content--title">
-            <label> <?php i::_e('Em destaque')?> </label>
-        </div>
+	<div class="home-feature__header">
+		<div class="home-feature__header title">
+			<label> <?php i::_e('Em destaque')?> </label>
+		</div>        
+		<div class="home-feature__header description">
+			<label><?php i::_e('Confira os últimos destaques de cada uma das entidades')?></label>
+		</div>
+	</div>	
+	<div class="home-feature__content">
+		<div class="home-feature__content cards">
+			<loading :condition="!entities.length"></loading>
+			<tabs v-if="entities.length > 0">
+				<tab label="Todos" slug="all">
+					<carousel :settings="settings" :breakpoints="breakpoints">
+						<slide v-for="entity in entities" :key="entity.__objectId">
+							<entity-card :entity="entity">
+								<template #labels> 
+									<div :class="['entityType',  entity.__objectType+'__background']">
+										<mc-icon :entity="entity"></mc-icon>
+										{{entity.__objectType}}
+									</div>
+								</template>
+							</entity-card> 
+						</slide>                        
 
-        <div class="home-feature__content--description">
-        <label><?php i::_e('Saiba tudo que acontece no Mapas Culturais, acesse:')?></label>
-        </div>
+						<template v-if="entities.length > 1" #addons>
+							<div class="actions">
+								<navigation :slideWidth="368" />
+							</div>
+						</template>
+					</carousel>
+				</tab>
+				<tab label="Agentes" slug="agents">
+					<carousel v-if="agents.length > 0" :settings="settings" :breakpoints="breakpoints">
+						<slide v-for="entity in agents" :key="entity.__objectId">
+							<entity-card :entity="entity">
+								<template #labels> 
+									<div :class="['entityType',  entity.__objectType+'__background']">
+										<mc-icon :entity="entity"></mc-icon>
+										{{entity.__objectType}}
+									</div>
+								</template>
+							</entity-card> 
+						</slide>                        
 
-        <div class="home-feature__content--cards">
-            <loading :condition="!entities.length"></loading>
-            <tabs v-if="entities.length > 0">
-                <tab label="Todos" slug="all">
-                    <carousel :settings="settings" :breakpoints="breakpoints">
-                        <slide v-for="entity in entities" :key="entity.__objectId">
-                            <entity-card :entity="entity"></entity-card> 
-                        </slide>                        
+						<template v-if="agents.length > 1" #addons>
+							<div class="actions">
+								<navigation :slideWidth="368" />
+							</div>
+						</template>
+					</carousel>
+				</tab>
+				<tab label="Espaços" slug="spaces">
+					<carousel v-if="spaces.length > 0" :settings="settings" :breakpoints="breakpoints">
+						<slide v-for="entity in spaces" :key="entity.__objectId">
+							<entity-card :entity="entity">
+								<template #labels> 
+									<div :class="['entityType',  entity.__objectType+'__background']">
+										<mc-icon :entity="entity"></mc-icon>
+										{{entity.__objectType}}
+									</div>
+								</template>
+							</entity-card> 
+						</slide>                        
 
-                        <template v-if="entities.length > 1" #addons>
-                            <div class="actions">
-                                <navigation :slideWidth="368" />
-                            </div>
-                        </template>
-                    </carousel>
-                </tab>
-
-                <tab label="Agentes" slug="agents">
-                    <carousel v-if="agents.length > 0" :settings="settings" :breakpoints="breakpoints">
-                        <slide v-for="entity in agents" :key="entity.__objectId">
-                            <entity-card :entity="entity"></entity-card> 
-                        </slide>                        
-
-                        <template v-if="agents.length > 1" #addons>
-                            <div class="actions">
-                                <navigation :slideWidth="368" />
-                            </div>
-                        </template>
-                    </carousel>
-                </tab>
-
-                <tab label="Espaços" slug="spaces">
-                    <carousel v-if="spaces.length > 0" :settings="settings" :breakpoints="breakpoints">
-                        <slide v-for="entity in spaces" :key="entity.__objectId">
-                            <entity-card :entity="entity"></entity-card> 
-                        </slide>                        
-
-                        <template v-if="spaces.length > 1" #addons>
-                            <div class="actions">
-                                <navigation :slideWidth="368" />
-                            </div>
-                        </template>
-                    </carousel>
-                </tab>
-            </tabs>
-            
-        </div>
-    </div>
+						<template v-if="spaces.length > 1" #addons>
+							<div class="actions">
+								<navigation :slideWidth="368" />
+							</div>
+						</template>
+					</carousel>
+				</tab>
+			</tabs>			
+		</div>
+	</div>
 </div>
