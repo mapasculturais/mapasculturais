3e3b95867ffd9cbde6e19459a44cd2fc1e0a6b14
robedj <ed.wassouf@gmail.com>
2022-11-22 21:34:58 -0400

    W.I.P: Criação do componente create-opportunity Ref.: #2249

---
+++ src/protected/application/lib/modules/Entities/components/create-opportunity/texts.php
@@ -0,0 +1,8 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'oportunidadeCriada' => i::__('Oportunidade Criada!'),
+    'criarAgente' => i::__('Criar Oportunidade'),
+    'criarRascunho' => i::__('Oportunidade criada em rascunho!'),
+];
