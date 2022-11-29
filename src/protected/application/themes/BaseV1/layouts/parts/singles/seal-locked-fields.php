b76293c9a3e7d0683c7681d4e23a8fc3c2ee1754
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-11-28 23:29:12 -0300

    melhora interface de configuraćao de campos bloqueados pelos selos (refs #2254)

--- src/protected/application/themes/BaseV1/layouts/parts/singles/seal-locked-fields.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/seal-locked-fields.php
@@ -1,12 +1,15 @@
 <?php
+
+use MapasCulturais\i;
+
 $props = $this->getLockedFieldsSeal();
 ?>
-
-<div id="seal-config">
-    <span class="js-editable " style="display:none;" id="locked-fields" data-value="[]" type="text" data-edit="lockedFields"></span>
+<div id="locked-fields">
+    <p class="alert info"><?= i::__('Selecione abaixo os campos que devem ser bloqueados nos agentes e espaços que possuírem este selo') ?> </p>
+    <input class="js-editable " style="display:none;" id="locked-fields-input" data-value="[]" type="hidden" data-edit="lockedFields" />
     <form class="js-locked-fields">
         <div class="fields">
-            <h2> Agente </h2>
+            <h2><?php $this->dict('entities: Agents') ?></h2>
             <?php foreach ($props['agent'] as  $field => $values) : ?>
                 <div>
                     <label>
@@ -18,7 +21,7 @@ $props = $this->getLockedFieldsSeal();
         </div>
 
         <div class="fields">
-            <h2> Espaço</h2>
+            <h2><?php $this->dict('entities: Spaces') ?></h2>
             <?php foreach ($props['space'] as  $field => $values) : ?>
                 <div>
                     <label>
