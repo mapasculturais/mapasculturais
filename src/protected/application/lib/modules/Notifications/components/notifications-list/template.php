fb501dd3caf0821d3faa69592ed4135c7398083f
rohilario <rodrigohilariocomercial@gmail.com>
2022-12-01 20:23:49 -0300

    adicionando mockup de card para notificacao <notifications-list> (refs #2273)

--- src/protected/application/lib/modules/Notifications/components/notifications-list/template.php
+++ src/protected/application/lib/modules/Notifications/components/notifications-list/template.php
@@ -4,15 +4,64 @@
 use Doctrine\ORM\Query\Expr\Select;
 use MapasCulturais\i;
 
-$this->import('mapas-card');
+$this->import('
+    mapas-card
+    confirm-button
+');
 
 ?>
 
     <entities type="notification" :query='query' #default='{entities}'>
         <mapas-card v-for="entity in entities" :key="entity.__objectId">
-            <template #title>
-                <div v-html='entity.message'></div>
-            </template>    
-                {{entity}}
+<!--            <template #title>-->
+<!--                <div v-html='entity.message'></div>-->
+<!--            </template>    -->
+<!--                {{entity}}-->
+            <div class="grid-12">
+                <div class="col-1">
+                    <img src="" />
+                </div>
+                <div class="col-11">
+                    <p style="font-size: 16px;line-height: 22px;"><b>Secretaria de Cultura do Pará</b> convidou você para <b>ser avaliador</b> na oportunidade <b>Preamar da Paz.</b> </p>
+                    <p style="font-size: 14px;line-height: 19px;">Há alguns minutos</p>
+                    <div class="grid-12">
+                        <div class="col-2">
+                            <button class="button button--primary-outline">
+                                Rejeitar
+                            </button>
+                        </div>
+                        <div class="col-2">
+                            <button class="button button--primary">
+                                Aceitar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </mapas-card>
     </entities>
+
+<mapas-card>
+    <div class="grid-12">
+        <div class="col-1">
+            <img src="../../assets/avatar.svg" />
+            Icon
+        </div>
+        <div class="col-11">
+            <p style="font-size: 16px;line-height: 22px;"><b>Secretaria de Cultura do Pará</b> convidou você para <b>ser avaliador</b> na oportunidade <b>Preamar da Paz.</b> </p>
+            <p style="font-size: 14px;line-height: 19px;">Há alguns minutos</p>
+            <div class="grid-12">
+                <div class="col-2">
+                    <button class="button button--primary-outline">
+                        Rejeitar
+                    </button>
+                </div>
+                <div class="col-2">
+                    <button class="button button--primary">
+                        Aceitar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</mapas-card>
