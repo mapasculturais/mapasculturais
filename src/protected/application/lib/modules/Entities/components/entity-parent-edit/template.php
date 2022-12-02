d1e85a9086de11dfeb71bd46bc47ffee4286cba1
robedj <ed.wassouf@gmail.com>
2022-12-02 10:03:11 -0400

    Renomear componente entity-parent para entity-parent-edit Ref: #2267

--- src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
@@ -4,23 +4,23 @@ use MapasCulturais\i;
 
 $this->import('select-entity');
 ?>
-<div v-if="parent" class="entity-parent">
+<div v-if="parent" class="entity-parent-edit">
     <h4><?php i::_e('{{entity.name}}')?> <?php i::_e('{{title}}') ?></h4>
-    <a class="entity-parent__parent" :href="parent.singleUrl" :title="parent.shortDescription">
-        <div class="entity-parent__parent--img">
+    <a class="entity-parent-edit__parent" :href="parent.singleUrl" :title="parent.shortDescription">
+        <div class="entity-parent-edit__parent--img">
             <img v-if="parent.files.avatar" class="profile" :src="parent.files?.avatar?.url">
             <div v-else class="placeholder">
                 <mc-icon name="agent-1"></mc-icon>
             </div>
         </div>
-        <div class="entity-parent__parent--name">
+        <div class="entity-parent-edit__parent--name">
         <?php i::_e('{{entity.parent.name}}') ?>
         </div>
     </a>
-    <div class="entity-parent__edit">        
+    <div class="entity-parent-edit__edit">        
         <select-entity :type="type" @select="changeParent($event)" openside="right-down">            
             <template #button="{ toggle }">
-                <a class="entity-parent__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
+                <a class="entity-parent-edit__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
                     <mc-icon name="exchange"></mc-icon>
                     <h4><?php i::_e('Alterar Propriedade') ?></h4>    
                 </a>
