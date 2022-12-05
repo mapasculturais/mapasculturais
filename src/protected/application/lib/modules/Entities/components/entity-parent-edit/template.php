0f71d59715edf18c54e3664e8892ce0ab56e6cdf
robedj <ed.wassouf@gmail.com>
2022-12-05 12:49:29 -0400

    Alteração no arquivo entity-nasted ref.: #2267

--- src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
@@ -4,8 +4,8 @@ use MapasCulturais\i;
 
 $this->import('select-entity');
 ?>
-<div v-if="parent" class="entity-parent-edit">
-    <h4><?php i::_e('{{entity.name}}')?> <?php i::_e('{{title}}') ?></h4>
+<div v-if="parent" :class="['entity-parentss-edit' ,classes]">
+    <h4 class="entity-parentss-edit__title">{{entity.name}} {{title}}</h4>
     <a class="entity-parent-edit__parent" :href="parent.singleUrl" :title="parent.shortDescription">
         <div class="entity-parent-edit__parent--img">
             <img v-if="parent.files.avatar" class="profile" :src="parent.files?.avatar?.url">
@@ -22,12 +22,18 @@ $this->import('select-entity');
             <template #button="{ toggle }">
                 <a class="entity-parent-edit__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
                     <mc-icon name="exchange"></mc-icon>
-                    <h4><?php i::_e('Alterar Propriedade') ?></h4>    
+                    <h4><?php i::_e('Alterar Supraespaço') ?></h4>    
                 </a>
             </template>
         </select-entity>
     </div>
 </div>
-<div v-else>
-    <button class="button button--primary-outline  button--icon">Adicionar Supraespaço</button>
+<div v-if="!parent">
+    <select-entity :type="type" @select="changeParent($event)" openside="right-down">            
+        <template #button="{ toggle }">
+            <a class="entity-parent-edit__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
+                <button class="button button--primary-outline  button--icon">Adicionar Supraespaço</button>
+            </a>
+        </template>
+    </select-entity>
 </div>
