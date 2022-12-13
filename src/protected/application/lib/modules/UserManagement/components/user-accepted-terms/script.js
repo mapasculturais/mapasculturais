af11a4a09edae36016cf38645ceacf509744ad43
robedj <ed.wassouf@gmail.com>
2022-12-13 15:23:53 -0400

    W.I.P :Ajustes no arquivo js do componente user-accepted-terms para separar os dados dos termos para o front[criação de um objeto] Ref.: #2225

--- src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/script.js
+++ src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/script.js
@@ -12,6 +12,7 @@ app.component('user-accepted-terms', {
     },
 
     data() {
+        // Termo
         const terms = $MAPAS.config.LGPD;
 
         return {
@@ -24,9 +25,52 @@ app.component('user-accepted-terms', {
             type: Entity,
             required: true
         },
+
     },
-    
+    computed: {
+        // politica de privacidade
+        privacyPolicy() {
+
+            var accepted = {};
+            for (const [acceptedmd5, term] of Object.entries(this.user.lgpd_privacyPolicy)) {
+
+                accepted.ip = term.ip;
+                accepted.timestamp = term.timestamp;
+                accepted.md5 = term.md5;
+                accepted.acceptedmd5 = acceptedmd5;
+                accepted.slug = __('Política de privacidade', 'user-accepted-terms');
+            }
+            return accepted;
+        },
+        termsOfUsage() {
+            var accepted = {};
+            for (const [acceptedmd5, term] of Object.entries(this.user.lgpd_termsOfUsage)) {
+
+                accepted.ip = term.ip;
+                accepted.timestamp = term.timestamp;
+                accepted.md5 = term.md5;
+                accepted.acceptedmd5 = acceptedmd5;
+                accepted.slug = __('Termos de uso', 'user-accepted-terms');
+
+            }
+            return accepted;
+        },
+        termsUse() {
+            var accepted = {};
+            for (const [acceptedmd5, term] of Object.entries(this.user.lgpd_termsUse)) {
+
+                accepted.ip = term.ip;
+                accepted.timestamp = term.timestamp;
+                accepted.md5 = term.md5;
+                accepted.acceptedmd5 = acceptedmd5;
+                accepted.slug = __('Autorização de uso de imagem', 'user-accepted-terms');
+
+            }
+            return accepted;
+        }
+    },
+
     methods: {
-        
+
     },
 });
