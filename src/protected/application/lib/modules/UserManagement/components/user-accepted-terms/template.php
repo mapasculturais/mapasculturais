83caf2359a1bfd4554f0e277c5db3ee21db5c27e
robedj <ed.wassouf@gmail.com>
2022-12-13 15:25:08 -0400

    W.I.P :Ajustes no template do componente user-accepted-terms.: #2225

--- src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/template.php
+++ src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/template.php
@@ -6,18 +6,37 @@ use MapasCulturais\i;
 
 // ');
 ?>
-
+<!-- 1- Verificar se o usuario tem os termos
+     2- Verificar se a hash do termo atualizado existe -->
+<!-- 3 - Verificar Timestamp, ip. -->
 <div class="user-accepted-terms__privacy">
     <div class="user-accepted-terms__privacy-title">
         <label class="user-accepted-terms__privacy-title"><?= i::__('Aceite de termos') ?></label>
-       <div v-if="user"class="verified">
+        <div v-if="user" class="verified">
 
-        <div v-for="(term, slug) in terms" class="for">
-            <label v-if="user.LGPD.lgpd_privacypolicy == term.md5">O usuario aceitou os termos em</label>
-            {{user.LGPD.privacyPolicy}}
-            {{term.title}}
-            {{term.md5}}
-        </div>
+            <div v-for="(term, slug) in terms" class="for">
+                <!-- {{term.md5}} -->
+                
+                
+                <!-- {{userslug}} -->
+                <div v-if="term.md5 == privacyPolicy.acceptedmd5" class="verifica">
+                    
+                {{privacyPolicy.slug}} aceito em {{privacyPolicy}}
+                {{privacyPolicy.acceptedmd5}}
+                    
+                    
+                </div>
+                <div v-if="term.md5 == termsOfUsage.acceptedmd5" class="verifica">
+                    {{termsOfUsage.acceptedmd5}}
+                    qualquer coisa
+                    
+                </div>
+                <div v-if="term.md5 == termsUse.acceptedmd5" class="verifica">
+                    {{termsUse.acceptedmd5}}
+                    qualquer coisa
+                    
+                </div>
+            </div>
         </div>
     </div>
     <div class="user-accepted-terms__privacy-accept">
