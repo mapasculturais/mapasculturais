87bbff99f310d5b7b84d71d4a550ce0ff0899d68
rohilario <rodrigohilariocomercial@gmail.com>
2022-12-13 20:54:30 -0300

    Adiciona metodo para internacionalizar texto (refs #2273)

--- src/protected/application/lib/modules/Notifications/layouts/parts/notifications/header-notification.php
+++ src/protected/application/lib/modules/Notifications/layouts/parts/notifications/header-notification.php
@@ -1,6 +1,7 @@
 <?php
 /** @var MapasCulturais\Theme $this */
 use MapasCulturais\App;
+use MapasCulturais\i;
 $app = App::i();
 
 $this->import('
@@ -12,7 +13,7 @@ $this->import('
 <?php $this->applyTemplateHook('header-notification', 'before') ?>
 <?php if (!$app->user->is('guest')): ?>
     <notification-modal #default="{modal}">
-        <a @click="modal.open">Notificações</a>
+        <a @click="modal.open"><?php i::esc_attr_e('Notificações') ?></a>
     </notification-modal>
 <?php endif; ?>
 <?php $this->applyTemplateHook('header-notification', 'after') ?>
