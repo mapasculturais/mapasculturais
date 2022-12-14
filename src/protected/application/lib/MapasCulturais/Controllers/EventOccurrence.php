b0be7f209247ad403584b3b09ffdf12962fd4156
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-12-14 14:55:55 -0300

    Refatoração dos campos da tabela event-occurrence e adaptação nas funções de criação da ocorrência (Ref.: #2275)

--- src/protected/application/lib/MapasCulturais/Controllers/EventOccurrence.php
+++ src/protected/application/lib/MapasCulturais/Controllers/EventOccurrence.php
@@ -43,14 +43,16 @@ class EventOccurrence extends EntityController {
 
     function POST_create($data = null){
         $this->requireAuthentication();
-        
         $app = App::i();
         $app->applyHookBoundTo($this, "POST({$this->id}.create):data", ['data' => &$data]);
         
         $event = $app->repo('Event')->find($this->postData['eventId']);
         $occurrence = new \MapasCulturais\Entities\EventOccurrence;
         $occurrence->event = $event;
-
+        $occurrence->description = $this->postData['description'];
+        $occurrence->price = $this->postData['price'];
+        $occurrence->priceInfo = $this->postData['priceInfo'];
+        
         if (@$this->postData['spaceId']) {
             $occurrence->space = $app->repo('Space')->find($this->postData['spaceId']);
         }
@@ -61,7 +63,7 @@ class EventOccurrence extends EntityController {
         if ($errors = $occurrence->validationErrors) {
             $this->errorJson($errors);
         } else {
-            $this->_finishRequest($occurrence);
+            $this->_finishRequest($occurrence, true);
         }
     }
 
