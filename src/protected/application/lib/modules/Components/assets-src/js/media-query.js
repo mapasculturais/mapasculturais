0f9e58261b403c2294b486f3c617bcd308c4cef9
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-12-15 22:36:39 -0300

    pequena refatoração no plugin Vue MediaQuery

--- src/protected/application/lib/modules/Components/assets-src/js/media-query.js
+++ src/protected/application/lib/modules/Components/assets-src/js/media-query.js
@@ -2,17 +2,17 @@ import * as Vue from 'vue'
 
 export default {
     install(app) {
-        app.config.globalProperties.mediaQueries = Vue.reactive([]);
+        const mediaQueries = Vue.reactive([]);
         app.config.globalProperties.$media = (query) => {
-            if (!app.config.globalProperties.mediaQueries[query]) {
+            if (!mediaQueries[query]) {
                 const mql = globalThis.matchMedia(`(${query})`)
-                app.config.globalProperties.mediaQueries[query] = mql.matches
+                mediaQueries[query] = mql.matches
 
                 mql.addEventListener("change", (event) => {
-                    app.config.globalProperties.mediaQueries[query] = event.matches
+                    mediaQueries[query] = event.matches
                 });
             }
-            return app.config.globalProperties.mediaQueries[query]
+            return mediaQueries[query]
         }
     }
 }
