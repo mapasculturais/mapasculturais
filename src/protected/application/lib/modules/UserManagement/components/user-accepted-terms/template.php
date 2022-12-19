b9b26413bed12ba82371b5c8686bcb846356756c
robedj <ed.wassouf@gmail.com>
2022-12-19 18:00:22 -0400

    Ajustes na internacionalização do label dos termos de uso no componente accepted-terms Ref.: #2225

--- src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/template.php
+++ src/protected/application/lib/modules/UserManagement/components/user-accepted-terms/template.php
@@ -9,9 +9,10 @@ use MapasCulturais\i;
         <div v-if="user" class="user-accepted-terms__privacy--accept-title-box">
             <div class="boxterm">
                 <div v-for="(term, slug) in terms" class="boxterm__list">
-                    <div v-if="user['lgpd_'+ slug]?.[term.md5]" class="boxterm__list-term">
+                    <div v-if="user['lgpd_'+ slug]?.[term.md5]">
                         <label class="boxterm__list-subterm">
-                            <label><?= i::__('{{term.title}} aceitos em {{formatDate(user["lgpd_"+slug][term.md5].timestamp)}} pelo ip {{user["lgpd_"+slug][term.md5].ip}}')?></label> 
+                            <label class="boxterm__list-subterm-title"><?= i::__('{{term.title}}')?></label>
+                            <label class="boxterm__list-subterm-content"><?= i::__(' aceitos em {{formatDate(user["lgpd_"+slug][term.md5].timestamp)}} pelo ip {{user["lgpd_"+slug][term.md5].ip}}')?></label> 
                         </label>
                     </div>
                 </div>
