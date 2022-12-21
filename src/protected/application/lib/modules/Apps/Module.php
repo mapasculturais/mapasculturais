b87de21d4355ab5d52a499405573df8ec0fa5693
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-12-20 22:53:29 -0300

    impossibilita que a API retorne chaves de terceiros (refs #2253)

--- src/protected/application/lib/modules/Apps/Module.php
+++ src/protected/application/lib/modules/Apps/Module.php
@@ -8,6 +8,11 @@ class Module extends \MapasCulturais\Module {
     {
         $app = App::i();
 
+        // impossibilita que a API retorne chaves de terceiros
+        $app->hook('ApiQuery(UserApp).params', function (&$params) {
+            $params['user'] = 'EQ(@me)';
+        });
+
         // reabilita a view create para o BaseV1
         if($app->view instanceof \MapasCulturais\Themes\BaseV1\Theme) {
             $app->hook('GET(app.create)', function() {
