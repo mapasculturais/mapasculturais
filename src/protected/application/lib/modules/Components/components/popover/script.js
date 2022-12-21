280cd64de5d9c997d58db9ec4a77d02d4dda4b22
robedj <ed.wassouf@gmail.com>
2022-12-21 16:12:49 -0400

    Ajustes no poppover pra fechar com esc e pra o duplo clique nao abrir novamente o poppover Ref.: #2295 #2294

--- src/protected/application/lib/modules/Components/components/popover/script.js
+++ src/protected/application/lib/modules/Components/components/popover/script.js
@@ -9,7 +9,8 @@ app.component('popover', {
 
     data() {
         return {
-            active: false
+            active: false,
+            click: -1,
         }
     },
 
@@ -51,11 +52,17 @@ app.component('popover', {
                     contained = true;
                 }
             };
-
+            
             if (!contained) { 
+                
                 this.close();
             };
-        })
+        }),
+        document.addEventListener('keydown', (e) => {
+            if((e.key=="27") || (e.key =="Escape")) {
+                this.close();
+            }            
+        });
     },
 
     methods: {
@@ -68,15 +75,20 @@ app.component('popover', {
             }
         },
         open() {
-            this.active = true;
-            this.$emit('open', this);
+            if((this.click)%2==0){
+                this.active = true;
+                this.$emit('open', this);
+            }
+
         },
         close() {
+            this.click++;
             this.active = false;
             this.$emit('close', this);
         },
         toggle() {
             this.active ? this.close() : this.open();
+            console.log(this.click);
         }
     },
 });
