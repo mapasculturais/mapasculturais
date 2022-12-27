4161c1da196cc0e47df6254cba09fc774d0479da
rohilario <rodrigohilariocomercial@gmail.com>
2022-12-27 11:12:05 -0300

    Altera componente de notificacoes do menu item modal (refs #2273)

--- src/protected/application/lib/modules/Notifications/components/notification-modal/template.php
+++ src/protected/application/lib/modules/Notifications/components/notification-modal/template.php
@@ -25,7 +25,7 @@ $this->import('
         </div>
     </template>
     <template #button="modal">
-        <div class="grid-2">
+        <div class="grid-2" v-if="typeStyle == 'normal'">
             <div class="col-6">
                 <a class="notification_header--link" @click="modal.open"><?= i::__('Notificações') ?></a>
             </div>
@@ -38,6 +38,21 @@ $this->import('
                         <span>{{ notificationsCount }}</span>
                     </div>
                 </a>
+            </div>
+        </div>
+        <div class="grid-2" v-else-if="typeStyle == 'button'">
+            <div class="col-6 notification_header--container">
+                <a @click="modal.open">
+                    <div>
+                        <mc-icon width="18" name='notification'></mc-icon>
+                    </div>
+                    <div v-if="notificationsCount > 0" class="notification_header--badge">
+                        <span>{{ notificationsCount }}</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6">
+                <a class="notification_header--link" @click="modal.open"><?= i::__('Notificações') ?></a>
             </div>
         </div>
     </template>
