bd6d1b192ade83a241ae8f39a1d0571a2a07a3ef
robedj <ed.wassouf@gmail.com>
2022-12-27 19:37:30 -0400

    Criação do componente app-card-content Ref.: #2299

---
+++ src/protected/application/lib/modules/Apps/views/panel/apps.php
@@ -0,0 +1,48 @@
+<?php
+
+use MapasCulturais\i;
+
+$this->import('
+    app-card-content
+    mc-icon 
+    panel--entity-card
+    panel--entity-tabs 
+');
+?>
+
+<div class="panel-page">
+    <header class="panel-page__header">
+        <div class="panel-page__header-title">
+            <div class="title">
+                <div class="title__icon app__background">
+                    <mc-icon name="app"></mc-icon>
+                </div>
+                <div class="title__title"> <?= i::_e('Meus aplicativos') ?> </div>
+            </div>
+            <div class="help">
+                <a class="panel__help-link" href="#"><?= i::__('Ajuda?') ?></a>
+            </div>
+        </div>
+        <p class="panel-page__header-subtitle">
+            <?= i::_e('Nesta seção você pode adicionar e gerenciar seus aplicativos') ?>
+        </p>
+        <div class="panel-page__header-actions">
+
+        </div>
+    </header>
+
+    <panel--entity-tabs tabs="publish,trash" select="name,privateKey,publicKey,status" type="app">
+        <template #card-title="{entity}">
+            <label>{{entity.name}}</label>
+            <!-- ver se tem como colocar o icone  -->
+            <!-- aqui vai ficar a verificação do editar,flag dentro do entity (aberto ou fechado) -->
+            <!-- desconsiderar o avatar -->
+        </template>
+        <template #card-content="{entity}">
+            <app-card-content :entity="entity"></app-card-content>
+        </template>
+        <template #entity-actions-right>
+            &nbsp;
+        </template>
+    </panel--entity-tabs>
+</div>
