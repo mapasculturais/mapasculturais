1abe2a90a0e08c69221ca0e58fb449b9425733fa
robedj <ed.wassouf@gmail.com>
2022-12-27 19:36:58 -0400

    Criação do componente app-card-content Ref.: #2299

--- src/protected/application/lib/modules/Panel/components/panel--nav/init.php
+++ src/protected/application/lib/modules/Panel/components/panel--nav/init.php
@@ -22,9 +22,7 @@ $nav_items = [
     'more' => [
         'label' => i::__('Outras opções'),
         'items' => [
-            ['route' => 'panel/apps', 'icon' => 'app', 'label' => i::__('Integrações')],
             ['route' => 'panel/my-account', 'icon' => 'account', 'label'=> i::__('Conta e Privacidade')],
-            ['route' => 'panel/my-apps', 'icon' => 'app', 'label'=> i::__('Meus aplivativos')],
         ]
     ],
 
