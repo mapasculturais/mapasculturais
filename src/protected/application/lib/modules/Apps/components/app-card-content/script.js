ff8d522c1a2e03b9c3c271244917f25a9d0459f5
robedj <ed.wassouf@gmail.com>
2022-12-28 16:18:46 -0400

    Adição da função de internacionalização e exibição da mensagem pro usuario no meus aplicativos Ref.: #2299

--- src/protected/application/lib/modules/Apps/components/app-card-content/script.js
+++ src/protected/application/lib/modules/Apps/components/app-card-content/script.js
@@ -4,6 +4,8 @@ app.component('app-card-content', {
 
     setup() {
         const messages = useMessages();
+        const text = Utils.getTexts('app-card-content');
+        return { text }
 
     },
 
@@ -45,11 +47,15 @@ app.component('app-card-content', {
             this.showPrivateKey = !this.showPrivateKey;
         },
         copyPublicKey() {
+            messages.success(__('message','app-card-content'));
+
+
             if (this.entity.publicKey) {
                 navigator.clipboard.writeText(this.entity.publicKey);
             }
         },
         copyPrivateKey() {
+            messages.success(__('message','app-card-content'));
             if (this.entity.privateKey) {
                 navigator.clipboard.writeText(this.entity.privateKey);
             }
