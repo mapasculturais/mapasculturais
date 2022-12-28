c63e92547351d111c4324e77cf8c19a4a8d6a13a
robedj <ed.wassouf@gmail.com>
2022-12-28 16:46:40 -0400

    W.I.P: Ajustes no  componente create-apps Ref.: #2299 #2304

---
+++ src/protected/application/lib/modules/Apps/components/create-apps/texts.php
@@ -0,0 +1,8 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'appCriado' => i::__('Aplicativo Criado!'),
+    'criarApp' => i::__('Criar Aplicativo'),
+    'criarRascunho' => i::__('Aplicativo criado em rascunho'),
+];
