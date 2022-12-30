1644a316b166412e943756482cd55973f595723e
robedj <ed.wassouf@gmail.com>
2022-12-30 16:34:52 -0400

    Refatoração no componente create-app para abertura de modal Ref.:2304

--- src/protected/application/lib/modules/Apps/components/create-apps/texts.php
+++
@@ -1,8 +0,0 @@
-<?php
-use MapasCulturais\i;
-
-return [
-    'appCriado' => i::__('Aplicativo Criado!'),
-    'criarApp' => i::__('Criar Aplicativo'),
-    'criarRascunho' => i::__('Aplicativo criado em rascunho'),
-];
