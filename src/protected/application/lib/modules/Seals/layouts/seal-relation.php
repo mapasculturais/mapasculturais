939338b8e6ba7f0e6433ad2206d81047e331086b
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2022-12-31 18:12:00 -0300

    Criação do layout para o 'seal-relation' (Ref.: #2282)

---
+++ src/protected/application/lib/modules/Seals/layouts/seal-relation.php
@@ -0,0 +1,16 @@
+<?php
+$this->import('entity');
+$this->jsObject['requestedEntity'] = $relation;
+if ($entity->usesTypes()) {
+    $this->bodyClasses[] = "{$entity->entityType}-{$entity->type->id}";
+} else {
+    $this->bodyClasses[] = "{$entity->entityType}";
+}
+?>
+<?php $this->part('header', $render_data) ?>
+<?php $this->part('main-header', $render_data) ?>
+<entity #default="{entity}">
+<?= $TEMPLATE_CONTENT ?>
+</entity>
+<?php $this->part('main-footer', $render_data) ?>
+<?php $this->part('footer', $render_data); 
