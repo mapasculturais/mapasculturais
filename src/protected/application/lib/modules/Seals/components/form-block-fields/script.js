7fb4385986a5a9fdbd9378443353c2b4c6d7f21b
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-05 19:13:59 -0300

    Adicionando escape de campos para form checkbox dos campos bloqueados  (refs #2285)

--- src/protected/application/lib/modules/Seals/components/form-block-fields/script.js
+++ src/protected/application/lib/modules/Seals/components/form-block-fields/script.js
@@ -12,73 +12,54 @@ app.component('form-block-fields', {
     },
     data () {
         return {
-            spaces: $DESCRIPTIONS.space,
-            agents: $DESCRIPTIONS.agent,
-            lockedFields: $MAPAS.requestedEntity.lockedFields
+            spaces: [],
+            agents: []
         }
     },
-    computed: {
-        iterationFieldsSpaces() {
-            const spaces = []
+    mounted () {
+        this.iterationAgentFields()
+        this.iterationSpaceFields()
+    },
+    methods: {
+        iterationAgentFields() {
             let skip = [
                 'createTimestamp',
                 'id',
                 'location',
-                'name',
-                'shortDescription',
-                'status',
                 'type',
                 '_type',
                 'userId',
             ];
-            Object.keys($DESCRIPTIONS.space).forEach((item) => {
-                if(!skip.includes(item) && $DESCRIPTIONS.space[item].required){
-                    spaces.push({ label: $DESCRIPTIONS.space[item], value: false });
-                }
-            })
-            return spaces
-        },
-        iterationFieldsAgents() {
-            const agents = []
-            let skip = [
-                'createTimestamp',
-                'id',
-                'location',
-                'name',
-                'shortDescription',
-                'status',
-                'type',
-                '_type',
-                'userId',
-            ];
-            Object.keys($DESCRIPTIONS.agent).forEach((item) => {
+            Object.keys($DESCRIPTIONS.agent).forEach((item)=>{
                 if(!skip.includes(item) && $DESCRIPTIONS.agent[item].required){
-                    agents.push({ label: $DESCRIPTIONS.agent[item].label, value: false });
+                    this.agents.push({ value: item, label: $DESCRIPTIONS.agent[item].label });
                 }
             })
-            return agents
+        },
+        iterationSpaceFields() {
+            let skip = [
+                'createTimestamp',
+                'id',
+                'location',
+                'type',
+                '_type',
+                'userId',
+            ];
+            Object.keys($DESCRIPTIONS.space).forEach((item)=>{
+                if(!skip.includes(item) && $DESCRIPTIONS.space[item].required){
+                    this.spaces.push({ value: item, label: $DESCRIPTIONS.space[item].label });
+                }
+            })
+        },
+        saveValueSpaceLockedFields (val) {
+            console.log(val)
+            $MAPAS.requestedEntity.lockedFields.push({ space: val })
+            console.log($MAPAS.requestedEntity.lockedFields)
+        },
+        saveValueAgentLockedFields (val) {
+            console.log(val)
+            $MAPAS.requestedEntity.lockedFields.push({ agent: val })
+            console.log($MAPAS.requestedEntity.lockedFields)
         }
     }
-    // data () {
-    //     return {
-    //         agents: [],
-    //         spaces: []
-    //     }
-    // },
-    // mounted () {
-    //     this.agents = [
-    //         { label: 'Lorem ipsum dolor sit amet', value: false },
-    //         { label: 'Lorem ipsum dolor sit amet', value: false },
-    //         { label: 'Lorem ipsum', value: false },
-    //         { label: 'dolor sit', value: false },
-    //         { label: 'sit amet', value: false }
-    //     ]
-    //     this.spaces = [
-    //         { label: 'Lorem ipsum dolor sit amet', value: false },
-    //         { label: 'Lorem ipsum dolor sit amet', value: false },
-    //         { label: 'Lorem ipsum', value: false },
-    //         { label: 'dolor sit', value: false },
-    //         { label: 'sit amet', value: false }
-    //     ]
-    // }
 });
