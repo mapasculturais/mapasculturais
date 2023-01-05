66e7f441b108afba27cfb0ee2b98a3b2e4491355
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-04 22:19:23 -0300

    Adicionando propriedades de agentes e espacos no campos bloqueados (refs #2285)

--- src/protected/application/lib/modules/Seals/components/form-block-fields/script.js
+++ src/protected/application/lib/modules/Seals/components/form-block-fields/script.js
@@ -12,24 +12,73 @@ app.component('form-block-fields', {
     },
     data () {
         return {
-            agents: [],
-            spaces: []
+            spaces: $DESCRIPTIONS.space,
+            agents: $DESCRIPTIONS.agent,
+            lockedFields: $MAPAS.requestedEntity.lockedFields
         }
     },
-    mounted () {
-        this.agents = [
-            { label: 'Lorem ipsum dolor sit amet', value: false },
-            { label: 'Lorem ipsum dolor sit amet', value: false },
-            { label: 'Lorem ipsum', value: false },
-            { label: 'dolor sit', value: false },
-            { label: 'sit amet', value: false }
-        ]
-        this.spaces = [
-            { label: 'Lorem ipsum dolor sit amet', value: false },
-            { label: 'Lorem ipsum dolor sit amet', value: false },
-            { label: 'Lorem ipsum', value: false },
-            { label: 'dolor sit', value: false },
-            { label: 'sit amet', value: false }
-        ]
+    computed: {
+        iterationFieldsSpaces() {
+            const spaces = []
+            let skip = [
+                'createTimestamp',
+                'id',
+                'location',
+                'name',
+                'shortDescription',
+                'status',
+                'type',
+                '_type',
+                'userId',
+            ];
+            Object.keys($DESCRIPTIONS.space).forEach((item) => {
+                if(!skip.includes(item) && $DESCRIPTIONS.space[item].required){
+                    spaces.push({ label: $DESCRIPTIONS.space[item], value: false });
+                }
+            })
+            return spaces
+        },
+        iterationFieldsAgents() {
+            const agents = []
+            let skip = [
+                'createTimestamp',
+                'id',
+                'location',
+                'name',
+                'shortDescription',
+                'status',
+                'type',
+                '_type',
+                'userId',
+            ];
+            Object.keys($DESCRIPTIONS.agent).forEach((item) => {
+                if(!skip.includes(item) && $DESCRIPTIONS.agent[item].required){
+                    agents.push({ label: $DESCRIPTIONS.agent[item].label, value: false });
+                }
+            })
+            return agents
+        }
     }
+    // data () {
+    //     return {
+    //         agents: [],
+    //         spaces: []
+    //     }
+    // },
+    // mounted () {
+    //     this.agents = [
+    //         { label: 'Lorem ipsum dolor sit amet', value: false },
+    //         { label: 'Lorem ipsum dolor sit amet', value: false },
+    //         { label: 'Lorem ipsum', value: false },
+    //         { label: 'dolor sit', value: false },
+    //         { label: 'sit amet', value: false }
+    //     ]
+    //     this.spaces = [
+    //         { label: 'Lorem ipsum dolor sit amet', value: false },
+    //         { label: 'Lorem ipsum dolor sit amet', value: false },
+    //         { label: 'Lorem ipsum', value: false },
+    //         { label: 'dolor sit', value: false },
+    //         { label: 'sit amet', value: false }
+    //     ]
+    // }
 });
