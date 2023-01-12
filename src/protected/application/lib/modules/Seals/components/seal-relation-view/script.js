12294f151035b71f305e4ce8ba3a6e75aeda7878
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-11 22:23:46 -0300

    Integracao com os dados da visualizacao do selo do agente (refs #2286)

--- src/protected/application/lib/modules/Seals/components/seal-relation-view/script.js
+++ src/protected/application/lib/modules/Seals/components/seal-relation-view/script.js
@@ -10,7 +10,29 @@ app.component('seal-relation-view', {
             required: false
         },
     },
-    data () {
-        return {}
+    computed: {
+      seal () {
+        return $MAPAS.requestedEntity?.seal
+      },
+      agent () {
+        return $MAPAS.requestedEntity?.agent
+      },
+      certificateText () {
+          return $MAPAS.requestedEntity?.certificateText
+      },
+      dateValidFormatted () {
+          const dateObj = new Date($MAPAS.requestedEntity?.validateDate?.date)
+          return dateObj.toLocaleDateString("pt-BR")
+      },
+      dateCreated () {
+          const dateObj = new Date($MAPAS.requestedEntity?.createTimestamp?.date)
+          return dateObj.toLocaleDateString("pt-BR")
+      },
+      isValid () {
+          const validDateString = $MAPAS.requestedEntity?.validateDate?.date
+          const now = new Date()
+          const validDate = new Date(validDateString)
+          return validDate.getTime() > now.getTime()
+      }
     }
 });
