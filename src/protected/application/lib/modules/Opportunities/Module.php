3e049efcad3a88fd9be37405e66ad6f2d2e79bb7
LimaSystem <wolegario49@gmail.com>
2023-01-13 12:13:07 -0500

    Move o  hook  do endpoint findPhasesById para o modulo Opportunity Ref.: #2115

--- src/protected/application/lib/modules/Opportunities/Module.php
+++ src/protected/application/lib/modules/Opportunities/Module.php
@@ -42,6 +42,30 @@ class Module extends \MapasCulturais\Module{
             
         });
 
+        $app->hook('GET(opportunity.findPhasesById)', function() use($app) {
+
+            $this->requireAuthentication();
+
+            $result = [];
+            $opportunity = $app->repo('Opportunity')->find($this->data['id']);
+            if($opportunity_phases = $opportunity->allPhases){
+                foreach($opportunity_phases as $key => $opportunity){
+                    $resgistration_status = $opportunity->countRegistrationsByStatus;
+                    $result[] = [
+                        'opportunity_phase' => ($key+1),
+                        'total_registration' => $opportunity->countRegistrations['qtd'],
+                        'registrations_status' => $resgistration_status,
+                        'evaluations' => $opportunity->countEvaluations,
+
+                    ];
+                }
+
+            }
+
+            $this->json($result);
+            
+        });
+
         /* == ROTAS DO PAINEL == */
         $app->hook('GET(panel.opportunities)', function() use($app) {
             $this->requireAuthentication();
