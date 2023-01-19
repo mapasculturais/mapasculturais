bd95e9d56d48779a2d1a90a93ff9c24b5ce0d9de
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-19 17:28:26 -0300

    add delete-account

---
+++ src/protected/application/lib/modules/UserManagement/views/panel/delete-account.php
@@ -0,0 +1,23 @@
+<?php
+use MapasCulturais\i;
+$this->layout = 'panel';
+
+$this->import('user-management--delete');
+
+?>
+
+
+<div class="panel-page">
+  <header class="panel-page__header">
+    <div class="panel-page__header-title">
+      <div class="title">
+        <div class="title__icon default"> <mc-icon name="trash"></mc-icon> </div>
+        <div class="title__title"> <?php i::esc_attr_e('Exclua sua conta') ?> </div>
+      </div>
+      <div class="help">
+        <a class="panel__help-link" href="#"><?=i::__('Ajuda?')?></a>
+      </div>
+    </div>
+  </header>
+  <user-management--delete :user="entity"></user-management--delete>
+</div>
