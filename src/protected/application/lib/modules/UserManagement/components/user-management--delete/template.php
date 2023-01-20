f1c5623b26345e74ffa36816345763798ff1b72a
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-19 22:11:26 -0300

    Ajustes finos <user-management--delete> refs 2276

--- src/protected/application/lib/modules/UserManagement/components/user-management--delete/template.php
+++ src/protected/application/lib/modules/UserManagement/components/user-management--delete/template.php
@@ -2,7 +2,13 @@
 
 use MapasCulturais\i;
 
-$this->import('modal loading panel--entity-actions select-entity mapas-card');
+$this->import('
+modal 
+loading 
+panel--entity-actions 
+select-entity 
+mapas-card
+');
 ?>
 
 <mapas-card>
@@ -11,13 +17,15 @@ $this->import('modal loading panel--entity-actions select-entity mapas-card');
         <p><?= i::__('Algumas informações, como por exemplo as inscrições em editais continuarão acessíveis. Você pode escolher por transferir suas entidades para outro usuário, 
                                 que será questionado se deseja recebê-las. No caso do usuário se negar a receber as entidades, estas serão excluidas.') ?></p>
         <p><?= i::__('Se desejar escolha o usuário para receber suas entidades:') ?></p>
-        <select-entity type="agent" @select="switchUser($event)" openside="down-right">
-            <template #button="{ toggle }">
-                <button class="button button--rounded button--sm button--icon button--primary" @click="toggle()">
-                  <?php i::_e('Selecione o usuário') ?>
-                </button>
-            </template>
-        </select-entity>
+        <div class="user-management__content--action">
+            <select-entity type="agent" @select="switchUser($event)">
+                <template #button="{ toggle }">
+                    <button class="button button--sm button--icon button--primary" @click="toggle()">
+                    <?php i::_e('Selecione o usuário') ?>
+                    </button>
+                </template>
+            </select-entity>
+        </div>
     </div>
     <div>
         <!-- <panel--entity-actions :entity="user"></panel--entity-actions> -->
