f9f91bc9eed6d81ec97d04dac23a4b54617d93fe
erleibiazzio <erleibiazzio@gmail.com>
2023-01-24 14:41:38 -0300

    Revert "Move o  hook  do endpoint findPhasesById para o modulo Opportunity Ref.: #2115"
    This reverts commit 69831af2d75cc7c8052281a4cf6dab841947d7a0.

--- src/protected/application/lib/modules/OpportunityPhases/Module.php
+++ src/protected/application/lib/modules/OpportunityPhases/Module.php
@@ -211,6 +211,22 @@ class Module extends \MapasCulturais\Module{
 
         $app->view->enqueueStyle('app', 'plugin-opportunity-phases', 'css/opportunity-phases.css');
 
+        $app->hook('GET(opportunity.findPhasesById)', function() use($app) {
+            
+            $result = [];
+            $opportunity = $app->repo('Opportunity')->find($this->data['id']);
+            if($opportunity_phases = $opportunity->allPhases){
+                foreach($opportunity_phases as $key => $opportunity){
+                    $resgistration_status = $opportunity->countRegistrationsByStatus;
+                    $result[] = [
+                        'opportunity_phase' => ($key+1),
+                        'registrations_status' => $resgistration_status
+                    ];
+                }
+            }
+
+        });
+
         $app->hook('view.render(<<*>>):before', function() use($app) {
             $this->jsObject['angularAppDependencies'][] = 'OpportunityPhases';
             $app->view->enqueueScript('app', 'ng.opportunityPhases', 'js/ng.opportunityPhases.js', ['mapasculturais']);
