966ceb13af92274f4272c5cfae353711e3753d53
erleibiazzio <erleibiazzio@gmail.com>
2023-01-30 11:20:20 -0300

    Restrutura template do upload de arquivos do recurso Ref.: #2353

--- src/protected/application/lib/modules/OpportunityClaimForm/layouts/parts/claim-form-upload.php
+++ src/protected/application/lib/modules/OpportunityClaimForm/layouts/parts/claim-form-upload.php
@@ -9,20 +9,18 @@ $template = '
 <article id="file-{{id}}" class="objeto">
     <h1><a href="{{url}}" rel="noopener noreferrer">{{description}}</a></h1> 
     <div class="botoes">
-        <a href="'.$url.'?file={{id}}" class="btn btn-small btn-primary js-formClaim-process">Processar</a>
         <a data-href="{{deleteUrl}}" data-target="#file-{{id}}" data-configm-message="Remover este arquivo?" class="btn btn-small btn-danger js-remove-item" data-hltip-classes="hltip-ajuda" title="Excluir arquivo" rel="noopener noreferrer">Excluir</a>
     </div>
 </article>';
 ?>
-<div id="event-importer">
+<div id="claim-form" class="widget">
     <div style="margin:1em 0em; text-align: right;">
-
-        <a class="add btn btn-default js-open-editbox hltip" data-target="#form-claim-file" href="#"> <?= i::_e('Vincular arquivo')?></a>
+        <a class="add btn btn-default js-open-editbox hltip" data-target="#editboc-formClaimUpload-file" href="#"> <?= i::_e('Vincular arquivo')?></a>
     </div>
-    <div id="form-claim-file" class="js-editbox mc-left" title="<?= i::_e('Importar CSV de eventos')?>" data-submit-label="Enviar">
-        <?php $this->ajaxUploader($entity, 'formClaimUpload', 'append', '.js-formClaim', $template, '', false, false, false)?>
+    <div id="editboc-formClaimUpload-file" class="js-editbox mc-left" title="<?= i::_e('Vincular aqruivo ao recurso')?>" data-submit-label="Enviar">
+        <?php $this->ajaxUploader($entity, 'formClaimUpload', 'append', 'ul.js-formClaimUpload', $template, '', false, false, false)?>
     </div>
-    <div class="js-formClaim">
+    <div class="js-formClaimUpload">
         <?php if(is_array($files)):?>
             <?php foreach($files as $file): ?>
                 <article id="file-<?php echo $file->id ?>" class="objeto <?php if($this->isEditable()) echo i::_e(' is-editable'); ?>" >
