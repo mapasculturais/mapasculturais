b2c0fd5b993a97f9a2b907280abd5f4361bd2ae0
LimaSystem <wolegario49@gmail.com>
2023-01-31 18:04:35 -0500

    Remove formulario antigo de solicitação de recurso Ref.:#2356

--- src/protected/application/lib/modules/OpportunityClaimForm/layouts/parts/claim-form.php
+++ src/protected/application/lib/modules/OpportunityClaimForm/layouts/parts/claim-form.php
@@ -1,30 +1,9 @@
 <?php
 
-use MapasCulturais\Controllers\Registration;
 use MapasCulturais\i;
-$app = \MapasCulturais\App::i();
-$user = $app->user;
 ?>
 
-<div class="opportunity-claim-box">
-    <input class="button-form-opportunity-claim btn-primary" ng-click="form[<?php echo $registration->id?>] = !form[<?php echo $registration->id?>]" type="button" name="opportunity-claim" value="<?php i::esc_attr_e('Solicitar Recurso'); ?>">
-</div>
+<p><strong><?php \MapasCulturais\i::_e("Para solicitar recurso acessar sua fixa de incrição.");?></strong></p>
 
-<form class="form-opportunity-claim js-opportunity-claim-form" ng-show="form[<?php echo $registration->id?>]" ng-controller="OpportunityClaimController">
-    <p>
-        <?php i::_e("Mensagem");?>:<br />
-        <textarea ng-model="data.message" type="text" rows="5" cols="30" name="message"></textarea>
-    </p>
-    <p>
 
-        <button class="js-submit-button opportunity-claim-form"
-                ng-click="
-                send(<?php echo $registration->id?>);
-                form[<?php echo $registration->id?>] = false;
-            "
-        >
-            <?php i::_e("Enviar");?>
-        </button>
-    </p>
 
-</form>
