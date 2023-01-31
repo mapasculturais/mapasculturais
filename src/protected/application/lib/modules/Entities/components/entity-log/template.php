9277a72a963527739da45e47d8a9f0820bf14719
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-30 22:21:47 -0300

    Componente para exibicao de historico de alteracoes <entity-log>

---
+++ src/protected/application/lib/modules/Entities/components/entity-log/template.php
@@ -0,0 +1,17 @@
+<?php
+use MapasCulturais\i;
+?>
+
+<div :class="classes">
+    <h4><?= i::__('Histórico de alterações') ?></h4>
+    <div>
+        <p class="entity-log__history--title">30/05/2022</p>
+        <p><a href="#" class="entity-log__history--link">Alterações realizadas 13:34:44</a></p>
+        <p><a href="#" class="entity-log__history--link">Alterações realizadas 13:34:44</a></p>
+        <p><a href="#" class="entity-log__history--link">Alterações realizadas 13:34:44</a></p>
+    </div>
+    <div>
+        <p class="entity-log__history--title">30/05/2022</p>
+        <p><a href="#" class="entity-log__history--link">Alterações realizadas 13:34:44</a></p>
+    </div>
+</div>
