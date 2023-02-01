6a8a4ce856a51fb227a67fc0b6ecb289a575e405
rohilario <rodrigohilariocomercial@gmail.com>
2023-01-31 23:43:52 -0300

    Ajuste de estilos na pagina de visualizacao da oportunidade <single> (refs#2334)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-subscription-content/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-subscription-content/template.php
@@ -5,23 +5,23 @@ use MapasCulturais\i;
 
 <div class="grid-12">
     <div class="col-12">
-        <p class="opportunity__period--title">
+        <p class="controller-opportunity__period--title">
           <?= i::__("Período de inscrições") ?>
         </p>
-        <div class="opportunity__period--content">
-            <p class="opportunity__period--description">
+        <div class="controller-opportunity__period--content">
+            <p class="controller-opportunity__period--description">
               <?= i::__("Inscrições abertas de 05/03/2022 a 21/03/2022 às 12:00") ?>
             </p>
         </div>
     </div>
     <div class="col-12">
-        <p class="opportunity__subscription--title">
+        <p class="controller-opportunity__subscription--title">
           <?= i::__("Inscreva-se") ?>
         </p>
-        <p class="opportunity__subscription--description">
+        <p class="controller-opportunity__subscription--description">
           <?= i::__("Você precisa acessar sua conta ou  criar uma cadastro na plataforma para poder se inscrever em editais ou oportunidades") ?>
         </p>
-        <button class="button button--primary">
+        <button class="controller-opportunity__full button button--primary w-100">
           <?= i::__("Fazer inscrição") ?>
         </button>
     </div>
