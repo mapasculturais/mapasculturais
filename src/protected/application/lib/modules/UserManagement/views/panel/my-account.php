b16150e3f71093ee73ef6ab5a383aed3e7369b54
robedj <ed.wassouf@gmail.com>
2023-02-02 17:19:41 -0400

    Responsividade das telas do painel - Refatoração dos estilos Ref.: #2354

--- src/protected/application/lib/modules/UserManagement/views/panel/my-account.php
+++ src/protected/application/lib/modules/UserManagement/views/panel/my-account.php
@@ -22,49 +22,47 @@ $this->import('
 <entity #default='{entity}'>
 
     <div class="p-user-detail">
-        <div class="panel-main">
-            <header class="p-user-detail__header">
+        <header class="p-user-detail__header">
 
-                <div class="p-user-detail__header-top">
-                    <div class="account__left">
-                        <div class="title">
-                            <div class="title__background">
-                                <mc-icon class="title__background-icon" name="account"></mc-icon>
-                            </div>
-                        </div>
-                        <label class="account__left-privacy"><?= i::__('Conta e privacidade') ?></label>
+            <div class="p-user-detail__header-top">
+                <div class="left">
+                    <div class="left-icon">
+                        <mc-icon class="icon" name="account"></mc-icon>
                     </div>
-                    <a class="panel__help-link" href="#"><?= i::__('Ajuda?') ?></a>
+                    <label class="left-title"><?= i::__('Conta e privacidade') ?></label>
                 </div>
-                <div class="p-user-detail__header-content">
-                    <div class="management-icon">
-                        <mc-icon name="agent-1"></mc-icon>
+                <a class="right-help" href="#"><?= i::__('Ajuda?') ?></a>
+            </div>
+
+            <div class="p-user-detail__header-content">
+                <div class="management-icon">
+                    <mc-icon name="agent-1"></mc-icon>
+                </div>
+                <div class="management-content ">
+                    <div class="management-content__label">
+                        <label class="management-content__label--name">{{entity.profile?.name}}</label>
+                        <div class="management-content__label--delete">
+                            <mc-link icon="trash" route='panel/deleteAccount'><?= i::__('Excluir') ?></mc-link>
+                        </div>
                     </div>
-                    <div class="management-content ">
-                        <div class="management-content__label">
-                            <label class="management-content__label--name">{{entity.profile?.name}}</label>
-                            <div class="management-content__label--delete">
-                                <mc-link icon="trash" route='panel/deleteAccount'><?=i::__('Excluir')?></mc-link>
-                            </div>
-                        </div>
-                        <div class="management-content__info">
-                            <p>ID: {{entity.id}}</p>
-                            <p><?= i::__('Último login') ?>: {{entity.lastLoginTimestamp.date('long year')}} <?= i::__('às') ?> {{entity.lastLoginTimestamp.time()}}</p>
-                            <p>
-                                <?= i::__('Status') ?>:
-                                <span v-if="entity.status == 1"><?= i::__('Ativo') ?></span>
-                                <span v-if="entity.status == -10"><?= i::__('Excluído') ?></span>
-                            </p>
-                            <p><?= i::__('Data de criação') ?>: {{entity.createTimestamp.date('long year')}} <?= i::__('às') ?> {{entity.createTimestamp.time()}}</p>
-                        </div>
+                    <div class="management-content__info">
+                        <p>ID: {{entity.id}}</p>
+                        <p><?= i::__('Último login') ?>: {{entity.lastLoginTimestamp.date('long year')}} <?= i::__('às') ?> {{entity.lastLoginTimestamp.time()}}</p>
+                        <p>
+                            <?= i::__('Status') ?>:
+                            <span v-if="entity.status == 1"><?= i::__('Ativo') ?></span>
+                            <span v-if="entity.status == -10"><?= i::__('Excluído') ?></span>
+                        </p>
+                        <p><?= i::__('Data de criação') ?>: {{entity.createTimestamp.date('long year')}} <?= i::__('às') ?> {{entity.createTimestamp.time()}}</p>
                     </div>
                 </div>
-            </header>
-            <entity-seals :entity="entity.profile" :editable="entity.profile.currentUserPermissions?.createSealRelation"></entity-seals>
+            </div>
+        </header>
+        <entity-seals :entity="entity.profile" :editable="entity.profile.currentUserPermissions?.createSealRelation"></entity-seals>
             
-            <user-accepted-terms :user="entity"></user-accepted-terms>
-            
-            <div class="user-function">
+             <user-accepted-terms :user="entity"></user-accepted-terms> 
+
+        <div class="user-function">
                 <label class="user-function__label"><?= i::__('Funções do Usuário') ?></label>
                 <div class="user-function__box">
                     <label class="user-function__box--label"><?= i::__('Título de Função de usuário em Subsite') ?> </label>
@@ -73,14 +71,12 @@ $this->import('
                     </div>
                 </div>
             </div>
-        </div>
 
         <div class="p-user-detail__property-label">
             <h3><?= i::__('Minhas propriedades por entidades') ?></h3>
         </div>
         <div class="p-user-detail__property-content">
             <div class="tabs-component">
-                <!-- tabs component p-user-detail__content-footer tabs-component--user -->
                 <tabs class="tabs-component__entities">
                     <tab label="<?= i::esc_attr__('Agentes') ?>" slug="agents" icon='agent' classes="tabs-component-button--active-agent">
                         <user-management--ownership-tabs :user="entity" type="agent" classes="tabs-component__header footer-content-tabs"></user-management--ownership-tabs>
