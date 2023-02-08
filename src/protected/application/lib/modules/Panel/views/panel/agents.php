4481479b3932a3ac91a29fa09cf30b37300c47aa
robedj <ed.wassouf@gmail.com>
2023-02-08 16:20:19 -0400

    W.I.P Alteração no card da tela de agentes Ref.: #2350

--- src/protected/application/lib/modules/Panel/views/panel/agents.php
+++ src/protected/application/lib/modules/Panel/views/panel/agents.php
@@ -1,7 +1,8 @@
 <?php
+
 use MapasCulturais\i;
 
-$this->import('create-agent panel--entity-tabs panel--entity-card mc-icon');
+$this->import('create-agent panel--entity-tabs panel--entity-actions panel--entity-card mc-icon');
 ?>
 
 <div class="panel-page">
@@ -12,21 +13,27 @@ $this->import('create-agent panel--entity-tabs panel--entity-card mc-icon');
                 <div class="title__title"> <?= i::_e('Meus agentes') ?> </div>
             </div>
             <div class="help">
-                <a class="panel__help-link" href="#"><?=i::__('Ajuda?')?></a>
+                <a class="panel__help-link" href="#"><?= i::__('Ajuda?') ?></a>
             </div>
         </div>
         <p class="panel-page__header-subtitle">
             <?= i::_e('Nesta seção você visualiza e gerencia seu perfil de usuário e outros agentes criados') ?>
         </p>
         <div class="panel-page__header-actions">
-            <create-agent  :editable="true" #default="{modal}"  >
+            <create-agent :editable="true" #default="{modal}">
                 <button @click="modal.open()" class="button button--primary button--icon">
                     <mc-icon name="add"></mc-icon>
-                    <span><?=i::__('Criar agente')?></span>
+                    <span><?= i::__('Criar agente') ?></span>
                 </button>
             </create-agent>
         </div>
     </header>
-    
-    <panel--entity-tabs type="agent"></panel--entity-tabs>
+
+    <panel--entity-tabs type="agent">
+
+        <template #entity-actions-right={entity}>
+            <panel--entity-actions :entity="entity" buttons="publish" publish="<?php i::esc_attr_e('Publicar') ?>"></panel--entity-actions>
+            <a :href="entity.singleUrl" class="button button--primary-outline button--icon"><?php i::_e('Acessar') ?> <mc-icon name="arrow-right"></mc-icon></a>
+        </template>
+    </panel--entity-tabs>
 </div>
