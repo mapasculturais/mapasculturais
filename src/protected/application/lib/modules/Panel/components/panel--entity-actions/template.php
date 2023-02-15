c7a0db11db0e4bff29c17be71a24e6e13baf812f
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-02-15 00:04:09 -0300

    corrige botões de ações das entidades nas listagens do painel (refs #2350)

--- src/protected/application/lib/modules/Panel/components/panel--entity-actions/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--entity-actions/template.php
@@ -5,41 +5,49 @@ use MapasCulturais\Entity;
 $this->import('confirm-button');
 $this->import('loading');
 ?>
-
-<div class="panel__entity-actions">
+<div v-if="entity.__processing" class="panel__entity-actions">
     <loading :entity="entity"></loading>
-    
-    <confirm-button v-if="!entity.__processing && publishButton && entity.status == <?= Entity::STATUS_TRASH ?>"
+</div>
+<div v-if="!entity.__processing" class="panel__entity-actions">
+    <confirm-button v-if="publishButton && entity.status == <?= Entity::STATUS_TRASH ?>"
         @confirm="undeleteEntity($event)" 
         button-class="button unpublish button--primary button--icon"
-        message="<?php i::esc_attr_e("Você está certo que deseja recuperar esta entidade da lixeira?") ?>"><?php i::_e('Recuperar') ?></confirm-button>
+        message="<?php i::esc_attr_e("Você está certo que deseja recuperar esta entidade da lixeira?") ?>">
+            <?php i::_e('Recuperar') ?>
+    </confirm-button>
     
-    <confirm-button v-if="!entity.__processing && publishButton && entity.status != <?= Entity::STATUS_TRASH ?> && entity.status != <?= Entity::STATUS_ENABLED ?>"
+    <confirm-button v-if="publishButton && entity.status != <?= Entity::STATUS_TRASH ?> && entity.status != <?= Entity::STATUS_ENABLED ?>"
         @confirm="publishEntity($event)"
         button-class="button publish button--primary button--icon"
-        message="<?php i::esc_attr_e("Você está certo que deseja publicar esta entidade?") ?>"><?php i::_e('Publicar') ?></confirm-button>
+        message="<?php i::esc_attr_e("Você está certo que deseja publicar esta entidade?") ?>">
+            <?php i::_e('Publicar') ?>
+    </confirm-button>
     
-    <confirm-button v-if="!entity.__processing && archiveButton && entity.status != <?= Entity::STATUS_ARCHIVED ?> && hasStatus('archived')"
+    <confirm-button v-if="archiveButton && entity.status != <?= Entity::STATUS_ARCHIVED ?> && hasStatus('archived')"
         @confirm="archiveEntity($event)"
         button-class="button--text archive button--icon button--sm panel__entity-actions--archive"
         message="<?php i::esc_attr_e("Você está certo que deseja arquivar esta entidade?") ?>">
             <mc-icon name="archive"></mc-icon>
             <span><?php i::_e('Arquivar') ?></span>
-        </confirm-button>
+    </confirm-button>
     
-    <confirm-button v-if="!entity.__processing && deleteButton && entity.status != <?= Entity::STATUS_TRASH ?> && hasStatus('trash')"
+    <confirm-button v-if="deleteButton && entity.status != <?= Entity::STATUS_TRASH ?> && hasStatus('trash')"
         @confirm="deleteEntity($event)"
         button-class="button--text delete button--icon button--sm panel__entity-actions--trash"
         message="<?php i::esc_attr_e("Você está certo que deseja excluir esta entidade?") ?>">
             <mc-icon name="trash"></mc-icon>
             <span><?php i::_e('Excluir') ?></span>
-        </confirm-button>
+    </confirm-button>
     
-    <confirm-button v-if="!entity.__processing && destroyButton && entity.status == <?= Entity::STATUS_TRASH ?>"
+    <confirm-button v-if="destroyButton && entity.status == <?= Entity::STATUS_TRASH ?>"
         @confirm="destroyEntity($event)"
-        message="<?php i::esc_attr_e("Você está certo que deseja excluir definitivamente esta entidade?") ?>"><?php i::_e('Excluir permanentemente') ?></confirm-button>
-    
-    <confirm-button v-if="!entity.__processing && archiveButton && entity.status != <?= Entity::STATUS_ARCHIVED ?> && entity.status != <?= Entity::STATUS_DRAFT ?> && entity.status != <?= Entity::STATUS_TRASH ?>"
+        button-class="button--text delete button--icon button--sm panel__entity-actions--trash"
+        message="<?php i::esc_attr_e("Você está certo que deseja excluir definitivamente esta entidade?") ?>">
+        <mc-icon name="trash"></mc-icon>
+        <?php i::_e('Excluir permanentemente') ?>
+    </confirm-button>
+
+    <confirm-button v-if="draftButton && entity.status != <?= Entity::STATUS_ARCHIVED ?> && hasStatus('draft')"
         @confirm="draftEntity($event)"
         button-class="button--text archive button--icon button--sm"
         message="<?php i::esc_attr_e("Você está certo que deseja transformar esta entidade em rascunho?") ?>">
