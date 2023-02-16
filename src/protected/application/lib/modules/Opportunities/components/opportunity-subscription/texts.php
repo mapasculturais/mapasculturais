c613ad408824cdfb012541b7ed317c2810c11e13
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-02-16 19:14:05 -0300

    implementa requisição para fazer inscrição em oportunidade no componente <opportunity-subscription> (refs #2390)

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-subscription/texts.php
@@ -0,0 +1,8 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'selecione categoria' => i::__('Por favor, selecione a categoria da inscrição'),
+    'selecione agente' => i::__('Por favor, selecione o agente responsável pela inscrição'),
+    'selecione agente e categoria' => i::__('Por favor, selecione o agente responsável e a categoria da inscrição'),
+];
