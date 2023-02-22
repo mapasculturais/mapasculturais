e46a35bf81c2ad935d96066b55f6eb4904c4f8a2
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-21 23:58:55 -0300

    Criação do componente <panel--registration-tabs> (Ref.: #2183)

---
+++ src/protected/application/lib/modules/Opportunities/components/panel--registration-tabs/README.md
@@ -0,0 +1,14 @@
+# Componente `<panel--registration-tabs>`
+
+### Importando componente
+```PHP
+<?php 
+$this->import('panel--registration-tabs');
+?>
+```
+### Exemplos de uso
+```HTML
+<!-- utilizaçao básica -->
+<panel--registration-tabs></panel--registration-tabs>
+
+```
---
+++ src/protected/application/lib/modules/Opportunities/components/panel--registration-tabs/init.php
@@ -0,0 +1,23 @@
+<?php
+$qa_params = [
+    'type' => 'EQ(1)',
+];
+
+if ($app->user->is('admin')) {
+    $qa_params['user'] = 'EQ(@me)';
+} else {
+    $qa_params['@permissions'] = '@control';
+}
+
+$qa = new MapasCulturais\ApiQuery(MapasCulturais\Entities\Agent::class, $qa_params);
+
+$q = new MapasCulturais\ApiQuery(MapasCulturais\Entities\Registration::class, [
+    'status' => 'EQ(0)',
+    '@permissions' => 'view',
+]);
+
+$q->addFilterByApiQuery($qa, 'id', 'owner');
+
+$result = $q->getCountResult();
+
+$this->jsObject['config']['panelRegistrationTabs']['totalDrafts'] = $result;
