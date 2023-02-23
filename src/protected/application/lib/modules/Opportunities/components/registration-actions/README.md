e790e6f69a925a01aeb67bc543d3978e7dafdc8a
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-23 00:50:31 -0300

    Criação do componente <registration-actions> (Ref.: #2394)

---
+++ src/protected/application/lib/modules/Opportunities/components/registration-actions/README.md
@@ -0,0 +1,18 @@
+# Componente `<registration-actions>`
+Componente de ações em uma inscrição
+  
+## Propriedades
+- *Entity **entity*** - Entidade
+
+### Importando componente
+```PHP
+<?php 
+$this->import('registration-actions');
+?>
+```
+### Exemplos de uso
+```HTML
+<!-- utilizaçao básica -->
+<registration-actions :entity="entity"></registration-actions>
+
+```
---
+++ src/protected/application/lib/modules/Opportunities/components/registration-actions/template.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ * 
+ */
+
+use MapasCulturais\i;
+?>
+
+<div class="registration-actions">
+    <div class="registration-actions__primary">
+        <button class="button button--large button--xbg button--primary" @click="save()"> <?= i::__('Salvar') ?> </button>
+    </div>
+    <div class="registration-actions__secondary">
+        <button class="button button--large button--primary-outline"> <?= i::__('Salvar para depois') ?> </button>
+        <button class="button button--large button--primary-outline" @click="exit()"> <?= i::__('Sair') ?> </button>
+    </div>
+</div>
