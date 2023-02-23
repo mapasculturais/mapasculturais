aba6752c30f85ecc4e3e196b7db81ed5ca137c11
rohilario <rodrigohilariocomercial@gmail.com>
2023-02-23 20:58:28 -0300

    Alterando prop para verificar se a fase e a primeira ou ultima <opportunity-phases-config> (refs #2409)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phases-config/texts.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phases-config/texts.php
@@ -2,5 +2,7 @@
 use MapasCulturais\i;
 
 return [
-    'nao foi possivel remover fase' => i::__('Não foi possível remover a fase')
+    'nao foi possivel remover fase' => i::__('Não foi possível remover a fase'),
+    'excluir_fase_avaliacao' => i::__("Excluir fase de avaliação"),
+    'excluir_fase_coleta_dados' => i::__("Excluir fase de coleta de dados"),
 ];
