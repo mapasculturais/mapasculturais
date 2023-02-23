e17a41cacf581874d8d8bec0cad80f950d186312
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-23 00:56:59 -0300

    W.I.P Criação da base da página de inscrição em oportunidade (Ref.: #2394)

---
+++ src/protected/application/lib/modules/Opportunities/views/registration/edit.php
@@ -0,0 +1,107 @@
+<?php
+
+use MapasCulturais\i;
+
+$this->layout = 'registrations';
+
+$this->import('
+    entity-header
+    mapas-breadcrumb
+    mapas-card
+    mapas-container
+    mc-icon
+    select-entity
+    stepper
+
+    registration-actions
+    registration-related-agents
+    registration-related-space
+');
+
+$this->breadcrumb = [
+    ['label' => i::__('Inicio'), 'url' => $app->createUrl('panel', 'index')],
+    ['label' => i::__('Minhas oportunidades'), 'url' => $app->createUrl('panel', 'opportunity')],
+    ['label' => $entity->name, 'url' => $app->createUrl('opportunity', 'single', [$entity->id])],
+];
+
+$stepsLabels = "['teste 1', 'teste 2', 'teste 3', 'teste 4', 'teste 5', 'teste 6', 'teste 7', 'teste 8', 'teste 9', 'teste 10', 'teste 11']";
+
+/**
+ * @todo registration-form
+ */
+?>
+
+<div class="main-app registration single">
+    <mapas-breadcrumb></mapas-breadcrumb>
+    <entity-header :entity="entity"></entity-header>
+
+    <div class="registration__content">
+        <div class="registration__content--title">
+            <?= i::__('Formulário de inscrição') ?>
+        </div>
+
+        <div class="registration__content--steps">
+            <stepper :steps="<?= $stepsLabels ?>" :actual-step="3" only-active-label small></stepper>
+        </div>
+
+        <mapas-container>
+            <main class="grid-12">
+
+                <div class="col-12 info">
+                    <p class="title"> <?= i::__('Informações da inscrição') ?> </p>
+
+                    <div class="content">
+                        <div class="content__info">
+                            <p class="title"> <?= i::__('Inscrição') ?> </p>
+                            <p class="info">on-63348175</p>
+                        </div>
+                        <div class="content__info">
+                            <p class="title"> <?= i::__('Data') ?> </p>
+                            <p class="info">06/04/2022</p>
+                        </div>
+                        <div class="content__info">
+                            <p class="title"> <?= i::__('Categoria') ?> </p>
+                            <p v-if="entity.category" class="info">{{entity.category}}</p>
+                            <p v-if="!entity.category" class="info">Sem categoria</p>
+                        </div>
+                    </div>
+                </div>
+
+                <section class="section">
+                    <div class="section__title">
+                        <?= i::__('Título da seção') ?>
+                    </div>
+                    <div class="section__content"> 
+                        
+                        <div class="card owner">                            
+                            <div class="card__title"> 
+                                <?= i::__('Agente responsável') ?> 
+                            </div>
+
+                            <div class="card__content">
+                                <div class="owner">
+                                    <div class="owner__image">
+                                        <img v-if="entity.owner.files?.avatar" :src="entity.owner.files?.avatar?.transformations?.avatarSmall?.url" />
+                                        <mc-icon v-if="!entity.owner.files?.avatar" name="image"></mc-icon>
+                                    </div>
+                                    <div class="owner__name">
+                                        {{entity.owner.name}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <registration-related-agents :registration="entity"></registration-related-agents>
+                        <registration-related-space :registration="entity"></registration-related-space>
+
+                    </div>
+                </section>
+            </main>
+
+            <aside>
+                <registration-actions :entity="entity"></registration-actions>
+            </aside>
+        </mapas-container>
+    </div>
+
+</div>
