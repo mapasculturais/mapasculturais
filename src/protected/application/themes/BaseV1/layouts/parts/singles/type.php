b5fe29f0f3ec230313135145c3be8f25936b938b
erleibiazzio <erleibiazzio@gmail.com>
2023-02-24 08:47:31 -0300

    Bloqueia a alteração de tipo no front caso o usuário nao tenha permissão  changeType Ref.:#2405

--- src/protected/application/themes/BaseV1/layouts/parts/singles/type.php
+++ src/protected/application/themes/BaseV1/layouts/parts/singles/type.php
@@ -1,13 +1,19 @@
 <?php 
 $_entity = $this->controller->id; 
 $class = isset($disable_editable) ? '' : 'js-editable-type';
+
 ?>
 <?php $this->applyTemplateHook('type','before'); ?>
 <div class="entity-type <?php echo $_entity ?>-type">
     <div class="icon icon-<?php echo $_entity ?>"></div>
+    <?php if($entity->canUser('changeType')):?>
     <a href="#" class='<?php echo $class ?> required' data-original-title="<?php \MapasCulturais\i::esc_attr_e("Tipo");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione um tipo");?>" data-entity='<?php echo $_entity ?>' data-value='<?php echo $entity->type ?>'>
         <?php echo $entity->type ? $entity->type->name : ''; ?>
     </a>
+    <?php else:?>
+        <?php echo $entity->type ? $entity->type->name : ''; ?>
+    <?php endif?>
+
 </div>
 <!--.entity-type-->
 <?php $this->applyTemplateHook('type','after'); ?>
