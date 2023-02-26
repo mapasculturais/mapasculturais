2d8396f92cf59f027c57b16142703e738e67e6af
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-26 08:43:31 -0300

    Criação dos metodos do componente <relation-actions> (Ref.: #2394)

--- src/protected/application/lib/modules/Opportunities/components/registration-actions/script.js
+++ src/protected/application/lib/modules/Opportunities/components/registration-actions/script.js
@@ -2,24 +2,42 @@ app.component('registration-actions', {
     template: $TEMPLATES['registration-actions'],
 
     props: {
-        entity: {
+        registration: {
             type: Entity,
             required: true
         },
     },
     
     methods: {
-        save() {
-            this.entity.save().then(() => {
-                this.exit();
-            });
+        async send() {
+            const data = {id: this.registration.id};
+            if (this.registration.category) {
+                data.category = this.registration.category;
+            }
+            
+            try {
+                this.registration.disableMessages();
+                await this.save();
+                this.registration.enableMessages();
+
+                await this.registration.POST('send', {data}).then((response) => {
+                    console.log(response);
+                });
+            } catch(error) {
+                console.log(error);
+            }
+        },
+        async save() {
+            return this.registration.save(false);
         },
         exit() {
-            if (window.history.length > 2) {
-                window.history.back();
-            } else {
-                window.location.href = $MAPAS.baseURL+'panel';
-            }
+            this.registration.save().then(() => {
+                if (window.history.length > 2) {
+                    window.history.back();
+                } else {
+                    window.location.href = $MAPAS.baseURL+'panel';
+                }
+            });
         },
     },
 });
