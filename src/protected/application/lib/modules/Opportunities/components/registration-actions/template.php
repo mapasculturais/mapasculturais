e4f2c0ff7da40965dd36c0ec665b4d9e8fcccffc
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-26 08:43:31 -0300

    Criação dos metodos do componente <relation-actions> (Ref.: #2394)

--- src/protected/application/lib/modules/Opportunities/components/registration-actions/template.php
+++ src/protected/application/lib/modules/Opportunities/components/registration-actions/template.php
@@ -10,10 +10,10 @@ use MapasCulturais\i;
 
 <div class="registration-actions">
     <div class="registration-actions__primary">
-        <button class="button button--large button--xbg button--primary" @click="save()"> <?= i::__('Salvar') ?> </button>
+        <button class="button button--large button--xbg button--primary" @click="send()"> <?= i::__('Enviar') ?> </button>
     </div>
     <div class="registration-actions__secondary">
-        <button class="button button--large button--primary-outline"> <?= i::__('Salvar para depois') ?> </button>
+        <button class="button button--large button--primary-outline" @click="save()"> <?= i::__('Salvar para depois') ?> </button>
         <button class="button button--large button--primary-outline" @click="exit()"> <?= i::__('Sair') ?> </button>
     </div>
 </div>
