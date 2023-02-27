66e37ef08ffb547e3206f9bf03d908aa072d62f9
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-27 18:35:38 -0300

    Criação do header das telas de inscrição (Ref.: #2394 #2415)

---
+++ src/protected/application/lib/modules/Opportunities/components/registration-header/README.md
@@ -0,0 +1,18 @@
+# Componente `<registration-header>`
+Header da tela de inscrições
+  
+## Propriedades
+- *Entity **registration*** - Entidade
+
+### Importando componente
+```PHP
+<?php 
+$this->import('registration-header');
+?>
+```
+### Exemplos de uso
+```HTML
+<!-- utilizaçao básica -->
+<registration-header :registration="registration"></registration-header>
+
+```
---
+++ src/protected/application/lib/modules/Opportunities/components/registration-header/template.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ * 
+ */
+
+use MapasCulturais\i;
+?>
+
+<?php $this->applyTemplateHook('registration-header', 'before'); ?>
+<header class="registration-header">
+    <?php $this->applyTemplateHook('registration-header', 'begin'); ?>
+
+    <div class="registration-header__content">
+        <div class="image">
+            <div class="icon">
+                <mc-icon name="image"></mc-icon>
+            </div>
+        </div>
+        <div class="title">
+            <span class="title__title">{{registration.opportunity.name}}</span>
+            <div class="title__info">
+                <div class="data">
+                    <div class="data__title"> <?= i::__('Tipo') ?>: </div>
+                    <div class="data__info opportunity__color"> {{registration.opportunity.type.name}} </div>
+                </div>
+                <div class="data">
+                    <div class="data__title"> <?= i::__('Projeto') ?>: </div>
+                    <div class="data__info"> {{registration.opportunity.ownerEntity.name}} </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php $this->applyTemplateHook('registration-header', 'end'); ?>
+</header>
+<?php $this->applyTemplateHook('registration-header', 'after'); ?>
