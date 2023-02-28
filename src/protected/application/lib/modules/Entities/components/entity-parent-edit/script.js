915ff55d9d43043032d5c4df3b838ba75b1219da
robedj <ed.wassouf@gmail.com>
2023-02-28 17:52:22 -0400

    Adiciona label para exibição do botão adicionar supra entidade no componente entity-parent-edit Ref.: #2341

--- src/protected/application/lib/modules/Entities/components/entity-parent-edit/script.js
+++ src/protected/application/lib/modules/Entities/components/entity-parent-edit/script.js
@@ -38,6 +38,10 @@ app.component('entity-parent-edit', {
             type: [String, Array, Object],
             required: false
         },
+        label: {
+            type: String,
+            required: true
+        }
 
     },
 
