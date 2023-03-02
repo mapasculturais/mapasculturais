fc435012832118df14eff6dea310a66e69efd4d5
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-02 00:58:34 -0300

    Adição da propriedade no-title para cards sem título

--- src/protected/application/lib/modules/Components/components/mapas-card/script.js
+++ src/protected/application/lib/modules/Components/components/mapas-card/script.js
@@ -5,5 +5,11 @@ app.component('mapas-card', {
     setup(props, { slots }) {
         const hasSlot = name => !!slots[name]
         return { hasSlot }
+    },
+    props: {
+        noTitle: {
+            type: Boolean,
+            default: false
+        },
     }
 });
--- src/protected/application/lib/modules/Components/components/mapas-card/template.php
+++ src/protected/application/lib/modules/Components/components/mapas-card/template.php
@@ -3,7 +3,7 @@
 use MapasCulturais\i;
 ?>
 <div class="mapas-card">
-    <div class="mapas-card__title">
+    <div v-if="!noTitle" class="mapas-card__title">
         <slot name="title"></slot>
     </div>
     <div class="mapas-card__content">
