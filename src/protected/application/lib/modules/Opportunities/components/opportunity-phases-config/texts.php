0925b6f3743f5f4c93d3fa16b72b3d27962c6b5f
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-01 21:33:37 -0300

    Alterando componente para botao e adicionando acao <opportunity-phases-config> (refs #2420)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phases-config/texts.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phases-config/texts.php
@@ -5,4 +5,5 @@ return [
     'nao foi possivel remover fase' => i::__('Não foi possível remover a fase'),
     'excluir_fase_avaliacao' => i::__("Excluir fase de avaliação"),
     'excluir_fase_coleta_dados' => i::__("Excluir fase de coleta de dados"),
+    'confirmar_publicacao' => i::__("Tem certeza que deseja publicar o resultado?")
 ];
