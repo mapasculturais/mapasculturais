3738483586015d3516e7688aea1a164fb6be1f14
erleibiazzio <erleibiazzio@gmail.com>
2023-03-02 10:56:07 -0300

    Garante que um usuário do grupo admin consiga criar agentes em amos os tipos Ref.: #2407

--- src/protected/application/lib/MapasCulturais/Entities/Agent.php
+++ src/protected/application/lib/MapasCulturais/Entities/Agent.php
@@ -311,7 +311,9 @@ class Agent extends \MapasCulturais\Entity
 
     public function setType($type)
     {
-        if($this->isNew() && $this->owner){
+        $app = App::i();
+
+        if(!$app->user->is('admin') && $this->isNew() && $this->owner){
             $this->_type = 2;
         }else{
             $this->_type = $type;
