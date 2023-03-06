7ebffae93176cb4fcc098214bacbb2beb39ecfe6
LimaSystem <wolegario49@gmail.com>
2023-03-06 18:08:46 -0500

    Altera o botão de criação de entidades nas telas de listagem para usar o modal de criação rapida ref.:#2437

--- src/protected/application/themes/BaseV1/layouts/parts/search/list-agent.php
+++ src/protected/application/themes/BaseV1/layouts/parts/search/list-agent.php
@@ -1,6 +1,6 @@
         <header id="agent-list-header" class="entity-list-header clearfix" ng-show="data.global.filterEntity == 'agent'">
             <h1><span class="icon icon-agent"></span> <?php \MapasCulturais\i::_e("Agentes");?></h1>
-            <a class="btn btn-accent add" href="<?php echo $app->createUrl('agent', 'create'); ?>"><?php \MapasCulturais\i::_e("Adicionar agente");?></a>
+            <?php $this->renderModalFor('agent', false, \MapasCulturais\i::__("Adicionar agente"), "btn btn-accent add"); ?>
         </header>
         
         <div id="lista-dos-agentes" class="lista agent" infinite-scroll="data.global.filterEntity === 'agent' && addMore('agent')" ng-show="data.global.filterEntity === 'agent'">
