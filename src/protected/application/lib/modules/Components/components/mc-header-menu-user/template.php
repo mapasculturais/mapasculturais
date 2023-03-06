a93910c4c8fd43467fd82258639a62f38e202807
robedj <ed.wassouf@gmail.com>
2023-03-06 12:21:04 -0400

    W.I.P: Refatoração do menu do painel de Controle versão mobile ref: #2344

--- src/protected/application/lib/modules/Components/components/mc-header-menu-user/template.php
+++ src/protected/application/lib/modules/Components/components/mc-header-menu-user/template.php
@@ -1,9 +1,12 @@
 <?php
+
 /**
  * @var MapasCulturais\Themes\BaseV2 $this
  * @var MapasCulturais\App $app
  */
+
 use MapasCulturais\i;
+
 $this->import('
     panel--nav
     popover
@@ -15,10 +18,10 @@ $this->import('
 <div class="mc-header-menu-user">
     <?php $this->applyTemplateHook('header-menu-user', 'begin') ?>
     <!-- Menu desktop -->
-    <?php $this->applyTemplateHook('header-menu-user--desktop','before'); ?>
+    <?php $this->applyTemplateHook('header-menu-user--desktop', 'before'); ?>
     <popover openside="down-left" class="mc-header-menu-user__desktop">
-        <?php $this->applyTemplateHook('header-menu-user--desktop','begin'); ?>
-        <template #button="{ toggle }">            
+        <?php $this->applyTemplateHook('header-menu-user--desktop', 'begin'); ?>
+        <template #button="{ toggle }">
             <a class="mc-header-menu-user__user" @click="toggle()">
                 <div class="mc-header-menu-user__user--name">
                     <?= i::_e('Minha conta') ?>
@@ -38,7 +41,7 @@ $this->import('
                         <li><mc-link :entity='profile' icon><?= i::__('Meu Perfil') ?></mc-link></li>
                         <?php $this->applyTemplateHook('header-menu-user--itens', 'end') ?>
                     </ul>
-                    <h3><?= i::__('Menu do Painel de controle') ?></h3>
+                    <!-- <h3><?= i::__('Menu do Painel de controle') ?></h3> -->
                 </template>
 
                 <template #end>
@@ -47,15 +50,15 @@ $this->import('
             </panel--nav>
             <?php $this->applyTemplateHook('header-menu-user--desktop', 'after') ?>
         </template>
-        <?php $this->applyTemplateHook('header-menu-user--desktop','end'); ?>
+        <?php $this->applyTemplateHook('header-menu-user--desktop', 'end'); ?>
     </popover>
-    <?php $this->applyTemplateHook('header-menu-user--desktop','after'); ?>
+    <?php $this->applyTemplateHook('header-menu-user--desktop', 'after'); ?>
 
     <!-- Menu mobile -->
-    <?php $this->applyTemplateHook('header-menu-user--mobile','before'); ?>
+    <?php $this->applyTemplateHook('header-menu-user--mobile', 'before'); ?>
     <div class="mc-header-menu-user__mobile">
-        <?php $this->applyTemplateHook('header-menu-user--mobile','begin'); ?>
-        <div class="mc-header-menu-user__mobile--button">            
+        <?php $this->applyTemplateHook('header-menu-user--mobile', 'begin'); ?>
+        <div class="mc-header-menu-user__mobile--button">
             <a href="#main-app" class="mc-header-menu-user__user" @click="toggleMobile()">
                 <div class="mc-header-menu-user__user--name">
                     <?= i::_e('Minha conta') ?>
@@ -68,7 +71,7 @@ $this->import('
         <div v-if="open" class="mc-header-menu-user__mobile--list">
             <div class="close">
                 <theme-logo href="<?= $app->createUrl('site', 'index') ?>"></theme-logo>
-                <a class="close__btn"  href="#main-app" @click="toggleMobile()">
+                <a class="close__btn" href="#main-app" @click="toggleMobile()">
                     <mc-icon name="close"></mc-icon>
                 </a>
             </div>
@@ -76,17 +79,19 @@ $this->import('
             <panel--nav>
                 <template #begin>
                     <?php $this->applyTemplateHook('header-menu-user--mobile', 'begin') ?>
-                    <mc-link :entity='profile' icon><?= i::__('Meu Perfil') ?></mc-link>
-                    <mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link>
                 </template>
 
                 <template #end>
+                    <div class=" header-menu-user--mobile-profile">
+                        <mc-link :entity='profile' icon><label><?= i::__('Meu Perfil') ?></label></mc-link>
+                        <mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link>
+                    </div>
                     <?php $this->applyTemplateHook('header-menu-user--mobile', 'end') ?>
                 </template>
             </panel--nav>
             <?php $this->applyTemplateHook('header-menu-user--mobile', 'after') ?>
         </div>
-        <?php $this->applyTemplateHook('header-menu-user--mobile','end'); ?>
+        <?php $this->applyTemplateHook('header-menu-user--mobile', 'end'); ?>
     </div>
     <?php $this->applyTemplateHook('header-menu-user', 'end') ?>
 </div>
