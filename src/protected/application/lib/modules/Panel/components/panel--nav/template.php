9e5c6c2b85abcd8f2e61aa8e46ffc62bb1a2188e
robedj <ed.wassouf@gmail.com>
2023-03-06 15:36:46 -0400

    Adição de link no painel com refatoração na prop entity para receber o usuario logado Ref.: #2344

--- src/protected/application/lib/modules/Panel/components/panel--nav/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--nav/template.php
@@ -1,18 +1,21 @@
 <?php
+
 use MapasCulturais\i;
 
 $this->import('mc-link');
 ?>
-<nav class="panel-nav" :class="classes">
+<nav class="panel-nav" :entity="entity" :class="classes">
     <slot name='begin'></slot>
-    <template  v-for="group in groups" :key="group.id">
+    <template v-for="group in groups" :key="group.id">
         <h3 v-if="group.label">{{group.label}}</h3>
         <ul v-if="group.items.length > 0">
-            <li v-for="item in group.items" :key="`${group.id}:${item.route}`" >
+            <li v-for="item in group.items" :key="`${group.id}:${item.route}`">
                 <mc-link :route="item.route" :icon="item.icon" :class="{'active': active(item)}">{{item.label}}</mc-link>
             </li>
         </ul>
     </template>
-    <slot name='end'></slot>
+    <slot name='end'>
+        <li><mc-link :entity='entity' icon><?= i::__('Meu Perfil') ?></mc-link></li> 
+       <li><mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link></li> 
+    </slot>
 </nav>
-
