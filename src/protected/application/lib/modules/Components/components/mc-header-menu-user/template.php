4b758142bb19db248a4c8c382a43c6c8f0fcc271
robedj <ed.wassouf@gmail.com>
2023-03-06 14:43:49 -0400

    W.I.P: Refatoração do menu do painel de Controle versão mobile ref: #2344

--- src/protected/application/lib/modules/Components/components/mc-header-menu-user/template.php
+++ src/protected/application/lib/modules/Components/components/mc-header-menu-user/template.php
@@ -38,13 +38,16 @@ $this->import('
                     <?php $this->applyTemplateHook('header-menu-user--desktop', 'begin') ?>
                     <ul>
                         <?php $this->applyTemplateHook('header-menu-user--itens', 'begin') ?>
-                        <li><mc-link :entity='profile' icon><?= i::__('Meu Perfil') ?></mc-link></li>
+
+
                         <?php $this->applyTemplateHook('header-menu-user--itens', 'end') ?>
                     </ul>
                     <!-- <h3><?= i::__('Menu do Painel de controle') ?></h3> -->
                 </template>
 
                 <template #end>
+                    <li><mc-link :entity='profile' icon><?= i::__('Meu Perfil') ?></mc-link></li>
+                    <li><mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link></li>
                     <?php $this->applyTemplateHook('header-menu-user--desktop', 'end') ?>
                 </template>
             </panel--nav>
