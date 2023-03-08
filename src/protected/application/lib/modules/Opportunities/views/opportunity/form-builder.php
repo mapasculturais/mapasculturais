275df873165999bf9ba7d32d79f4a6fc57e74ff7
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-08 18:47:13 -0300

    Adicionando acao para voltar pagina usando mc-link no slot no opportunity-header (opportunity/formBuilder) (refs #2421)

--- src/protected/application/lib/modules/Opportunities/views/opportunity/form-builder.php
+++ src/protected/application/lib/modules/Opportunities/views/opportunity/form-builder.php
@@ -10,16 +10,21 @@ $this->breadcrumb = [
 
 $this->import('
     entity-header
-    opportunity-form-builder
     entity-actions
     mapas-breadcrumb
+    mc-link
+    opportunity-form-builder
     opportunity-header
 ')
 ?>
 
 <div class="main-app form-builder">
     <mapas-breadcrumb></mapas-breadcrumb>
-    <opportunity-header :opportunity="entity"></opportunity-header>
+    <opportunity-header :opportunity="entity">
+        <template #button>
+            <mc-link class="button button--primary-outline" :entity="entity.parent || entity" route="edit" hash="config" icon="arrow-left">Voltar</mc-link>
+        </template>
+    </opportunity-header>
 
     <opportunity-form-builder :entity="entity.parent ? entity.parent : entity" :is-first-phase="entity.parent == null || entity.isFirstPhase"></opportunity-form-builder>
 
