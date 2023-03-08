d7cceac4a8b7b298e6690488430b1dc825dcd435
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-08 16:51:22 -0300

    Componente para criar nova fase de data collection na oportunidade <opportunity-phase-config-data-collection> (refs #2453)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/texts.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/texts.php
@@ -2,5 +2,6 @@
 use MapasCulturais\i;
 
 return [
+    'nao foi possivel remover fase' => i::__('Não foi possível remover a fase'),
     'excluir_fase_coleta_dados' => i::__("Excluir fase de coleta de dados")
 ];
