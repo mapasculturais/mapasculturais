f09ddb4ce4547ff2300646b9b19c3074aa4aff5e
erleibiazzio <erleibiazzio@gmail.com>
2023-03-08 11:27:14 -0300

    Define forma de bloquear acesso a áreas que requer autenticação se os dados obrigatórios do agent não estão preenchidos

--- src/protected/application/conf/conf-base.d/module.profilecompletion.php
+++ src/protected/application/conf/conf-base.d/module.profilecompletion.php
@@ -2,6 +2,7 @@
 //Configurações Módulo ProfileCompletion
 return [
     "module.ProfileCompletion" => [
-        'enable' => env('MODULE_PROFILECOMPLETIN_ENABLE',true)
+        'enable' => env('MODULE_PROFILECOMPLETIN_ENABLE',true),
+        'checkRequiredFieldsAgents' => env('MODULE_PROFILECOMPLETION_CHECK_REQUIRED_FIELDS_AGENTS',false)
     ]    
 ];
