41349b334ea68ae0544b80ded05486c3d9423f72
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-08 12:57:30 -0300

    Remoção de componentes em desuso (Ref.: #2355 #2386)

--- src/protected/application/lib/modules/Components/components/timeline-item/template.php
+++
@@ -1,7 +0,0 @@
-<section class="timeline-item">
-    <div class="item">
-      <span :style="getBackgroundColour(itemTimeline.color)" class="dot" />
-      <h4 class="title-item" v-html="itemTimeline.title" />
-      <p class="description-item" v-html="itemTimeline.description" />
-    </div>
-  </section>
