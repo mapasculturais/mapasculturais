028b9be9c0bacb3296fd61c711b38ce950f5d69d
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-09 15:10:49 -0300

    Sobrescreve a função MapasCulturais.Messages.showMessage para enviar postMessage  (Ref.: #2443)

---
+++ src/protected/application/lib/modules/BaseV1EmbedTools/assets/js/embedtools.js
@@ -0,0 +1,30 @@
+(function($){
+    $(function () {
+        MapasCulturais.Messages.showMessage = function(type, message) {
+            if (type == "danger") {
+                type = "error";
+            }
+
+            let sendMessage = {
+                type: 'message',
+                data: {
+                    type,
+                    message
+                }
+            }
+
+            window.parent.postMessage(sendMessage, '*');
+        }
+
+        function sendSizes() {
+            window.parent.postMessage({
+                type: 'resize',
+                data: {
+                    height: document.body.offsetHeight+1,
+                }
+            }, '*');
+        }
+
+        setInterval(sendSizes, 200);
+    })
+})(jQuery)
