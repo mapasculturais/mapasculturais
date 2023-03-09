33e5605b86a5b816f42298fbefe0e884c7ecca1e
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-09 15:12:02 -0300

    Cria componente <v1-embed-tool> que fará o embed utilizando o iframe (Ref.: #2444)

---
+++ src/protected/application/lib/modules/BaseV1EmbedTools/components/v1-embed-tool/template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ * 
+ */
+
+use MapasCulturais\i;
+?>
+<iframe :src="url" ref="iframe" :style="{height: iframeHeight, maxHeight: maxHeight, minHeight: minHeight, border: 'none'}" ></iframe>
