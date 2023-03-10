2d25331c0d91acbda71117bc870cd4528c30d7e9
robedj <ed.wassouf@gmail.com>
2023-03-10 15:23:32 -0400

    Ajustes na versão mobile do componente panel-last-edited Ref.: #2446

--- src/protected/application/lib/modules/Panel/components/panel--last-edited/script.js
+++ src/protected/application/lib/modules/Panel/components/panel--last-edited/script.js
@@ -8,25 +8,32 @@ app.component('panel--last-edited', {
         navigation: Vue3Carousel.Navigation
     },
 
-    setup() { 
+    props: {
+        limit: {
+            type: Number,
+            default: 5
+        }
+    },
+
+    setup() {
         // os textos estão localizados no arquivo texts.php deste componente 
         const text = Utils.getTexts('panel--last-edited')
         return { text }
     },
 
-    async created(){
+    async created() {
         const agentAPI = new API('agent');
         const spaceAPI = new API('space');
         const eventAPI = new API('event');
         const projectAPI = new API('project');
         const opportunityAPI = new API('opportunity');
-        
+
         const query = this.query;
         query['@select'] = 'id,name,shortDescription,singleUrl,updateTimestamp,type';
         query['@order'] = 'updateTimestamp DESC';
         query['user'] = `EQ(@me)`;
 
-        if(this.limit) {
+        if (this.limit) {
             query['@limit'] = this.limit;
         }
 
@@ -36,9 +43,9 @@ app.component('panel--last-edited', {
         this.projects = await projectAPI.find(query);
         this.opportunities = await opportunityAPI.find(query);
     },
-    
+
     data() {
-        return{
+        return {
             query: {},
             agents: [],
             spaces: [],
@@ -48,44 +55,70 @@ app.component('panel--last-edited', {
 
             // carousel settings
             settings: {
+
                 itemsToShow: 1.2,
                 snapAlign: 'center',
             },
             breakpoints: {
+                1300: {
+                    itemsToScrool: 2.95,
+                    itemsToShow: 2.3,
+                    snapAlign: "start"
+                },
                 1200: {
-                    itemsToShow: 3.2,
+                    itemsToScrool: 2.8,
+                    itemsToShow: 2.2,
                     snapAlign: "start"
                 },
                 1100: {
-                    itemsToShow: 3,
+                    itemsToScrool: 2.6,
+                    itemsToShow: 2,
                     snapAlign: "start"
                 },
                 1000: {
-                    itemsToShow: 2.8,
+                    itemsToShow: 1.8,
                     snapAlign: "start"
                 },
                 900: {
-                    itemsToShow: 2.6,
+                    itemsToShow: 1.4,
                     snapAlign: "start"
                 },
                 800: {
-                    itemsToShow: 2.4,
+                    itemsToShow: 1.2,
                     snapAlign: "start"
                 },
                 700: {
-                    itemsToShow: 2.7,
+                    itemsToShow: 1.8,
                     snapAlign: "start"
                 },
                 600: {
-                    itemsToScrool: 1,
+                    itemsToScrool: 1.8,
 
-                    itemsToShow: 0.9,
+                    itemsToShow: 1.9,
                     snapAlign: "start"
                 },
                 500: {
+                    itemsToScrool: 1.5,
+
+                    itemsToShow: 1.25,
+                    snapAlign: "start"
+                },
+                400: {
+                    itemsToScrool: 1.4,
+
+                    itemsToShow: 1.2,
+                    snapAlign: "start"
+                },
+                360: {
                     itemsToScrool: 1,
 
-                    itemsToShow: 0.9,
+                    itemsToShow: 1.,
+                    snapAlign: "start"
+                },
+                340: {
+                    itemsToScrool: 1,
+
+                    itemsToShow: 1.,
                     snapAlign: "start"
                 },
             }
@@ -95,12 +128,12 @@ app.component('panel--last-edited', {
     computed: {
         entities() {
             if (this.projects.metadata && this.spaces.metadata && this.agents.metadata && this.opportunities.metadata && this.events.metadata) {
-                const entities = this.projects.concat(this.spaces, this.agents, this.opportunities, this.events);                
-                entities.sort((a,b) => {
+                const entities = this.projects.concat(this.spaces, this.agents, this.opportunities, this.events);
+                entities.sort((a, b) => {
                     let dateA = a.updateTimestamp.date('sql');
                     let dateB = b.updateTimestamp.date('sql');
 
-                    return (dateA.localeCompare(dateB));                    
+                    return (dateA.localeCompare(dateB));
                 });
                 return entities.slice(0, this.limit);;
             } else {
@@ -108,11 +141,12 @@ app.component('panel--last-edited', {
             }
         }
     },
-    
-    props: {
-        limit: {
-            type: Number,
-            default: 5
+    methods: {
+        resizeSlides() {
+            this.$refs.carousel.updateSlideWidth();
+            console.log('teste');
+
         }
     },
+
 });
