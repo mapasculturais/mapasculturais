3fb6d7ef1b8221000e45d830af00d86bbbcbeceb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-13 18:40:04 -0300

    Ajusta largura total do iframe (Ref.: #2444)

--- src/protected/application/lib/modules/BaseV1EmbedTools/components/v1-embed-tool/template.php
+++ src/protected/application/lib/modules/BaseV1EmbedTools/components/v1-embed-tool/template.php
@@ -7,4 +7,4 @@
 
 use MapasCulturais\i;
 ?>
-<iframe :src="url" ref="iframe" :style="{height: iframeHeight, maxHeight: maxHeight, minHeight: minHeight, border: 'none'}" ></iframe>
+<iframe :src="url" ref="iframe" :style="{height: iframeHeight, maxHeight: maxHeight, minHeight: minHeight, width: '100%', border: 'none'}" ></iframe>
