29dd8c7aa3e76a546c5131c66694f6c740d8a8d3
erleibiazzio <erleibiazzio@gmail.com>
2023-03-13 14:43:58 -0300

    Insere select para carregar os campo da oportunidade ref.: #2449

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
@@ -2,12 +2,12 @@
 
 use MapasCulturais\i;
 ?>
-<div ng-controller="TechnicalEvaluationMethodConfigurationController" class="technical-evaluation-configuration registration-fieldset">
+<div ng-controller="QualificationEvaluationMethodConfigurationController" class="technical-evaluation-configuration registration-fieldset">
     <h4><?php i::_e('Critérios') ?></h4>
-    <p><?php i::_e('Configure abaixo os critérios de avaliação documental') ?>
+    <p><?php i::_e('Configure abaixo os critérios de avaliação de habilitação documental') ?>
     <section id="{{section.id}}" ng-repeat="section in data.sections">
         <header>
-            <input ng-model="section.name" placeholder="<?php i::_e('informe o nome do criterio') ?>" class="section-name edit" ng-change="save({sections: data.sections})" ng-model-options='{ debounce: data.debounce }'>
+            <input ng-model="section.name" placeholder="<?php i::_e('informe o nome da seção') ?>" class="section-name edit" ng-change="save({sections: data.sections})" ng-model-options='{ debounce: data.debounce }'>
             <button ng-if="section.name.trim().length > 0" ng-click="deleteSection(section)" class="btn btn-danger delete alignright"><?php i::_e('Remover seção') ?></button>
             <button ng-if="section.name.trim().length == 0" ng-click="deleteSection(section)" class="btn btn-default delete alignright"><?php i::_e('Cancelar') ?></button>
         </header>
@@ -16,12 +16,16 @@ use MapasCulturais\i;
             <tr>
                 <th class="criterion-title"><?php i::_e('Título do critério') ?></th>
                 <th>
-                    <button ng-click="addCriterion(section)" class="btn btn-default add" title="<?php i::_e('Adicionar critério') ?>"></button>
+                    <button ng-click="addCriterion(section)" class="btn btn-default add" title="<?php i::_e('Adicionar sessão') ?>"></button>
                 </th>
             </tr>
 
             <tr id="{{cri.id}}" ng-repeat="cri in data.criteria" ng-if="cri.sid == section.id">
-                <td class="criterion-title"><input ng-model="cri.title" placeholder="<?php i::_e('informe o título do critério') ?>" class="criteria-fields" ng-change="save({criteria: data.criteria})" ng-model-options='{ debounce: data.debounce }'></td>
+                <td class="criterion-title">
+                    <select ng-model="data.qualifications[cri.id].field">
+                        <option ng-repeat="field in data.registrationFieldConfigurations" value="{{field.id}}">#{{field.id}} - {{field.title}} </option>
+                    </select>
+                </td>
                 <td>
                     <button ng-click="editableCriterion(cri)" class="btn btn-warning editable" title="<?php i::_e('Editar critério') ?>"></button>
                     <button ng-click="deleteCriterion(cri)" class="btn btn-danger delete" title="<?php i::_e('Remover critério') ?>"></button>
@@ -29,7 +33,7 @@ use MapasCulturais\i;
             </tr>
         </table>
     </section>
-    <button ng-click="addSection()" class="btn btn-default add"><?php i::_e('Adicionar criterios') ?></button>
+    <button ng-click="addSection()" class="btn btn-default add"><?php i::_e('Adicionar seção') ?></button>
 
     <br><br>
     <hr>
