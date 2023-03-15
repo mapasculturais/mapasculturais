85201b62ae8e3828b29211b848b51e266f266329
LimaSystem <wolegario49@gmail.com>
2023-03-15 11:30:00 -0500

    Ajusta css  do formulário de avaliação do novo método de habilitação documental Ref.:#2450

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-form.php
@@ -1,26 +1,25 @@
 <?php
 
 use MapasCulturais\i;
-
-
 ?>
-<div ng-controller="QualificationEvaluationMethodConfigurationController" class="technical-evaluation-form">
-    {{data.criteria}}
+<div ng-controller="QualificationEvaluationMethodConfigurationController" class="qualification-evaluation-configuration registration-fieldset">
     <div>
         <strong> <?php i::_e('Comprovante de endereço') ?> </strong>
-        <select style="width:300px" name="data[obs]" ng-model="evaluation['obs']">
+        <select class="select-option"  name="data[obs]" ng-model="evaluation['obs']">
             <option>Sem comprovante de endereço</option>
             <option>qualquer coisa2</option>
         </select>
     </div>
-
+    <br><br>
     <label>
-        <strong><?php i::_e('Observações') ?> <strong>
-                <textarea name="data[obs]" ng-model="evaluation['obs']"></textarea>
+        <strong class="textearea-strong"><?php i::_e('Observações') ?> </strong>
+        <br>
+            <textarea class="textearea-text" name="data[obs]" ng-model="evaluation['obs']"></textarea>
     </label>
     <br><br>
     <div>
         <label>
+            <br>
             <strong> <?php i::_e('Status:inabilitado') ?> </strong>
         </label>
     </div>
