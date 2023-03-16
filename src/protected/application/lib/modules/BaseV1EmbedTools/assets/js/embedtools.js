9fb547f766aaba4f67b2d7992a4ec43109e013aa
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-03-15 21:03:30 -0300

    modifica embedtools.js para funcionar corretamente

--- src/protected/application/lib/modules/BaseV1EmbedTools/assets/js/embedtools.js
+++ src/protected/application/lib/modules/BaseV1EmbedTools/assets/js/embedtools.js
@@ -1,30 +1,28 @@
 (function($){
-    $(function () {
-        MapasCulturais.Messages.showMessage = function(type, message) {
-            if (type == "danger") {
-                type = "error";
-            }
-
-            let sendMessage = {
-                type: 'message',
-                data: {
-                    type,
-                    message
-                }
-            }
-
-            window.parent.postMessage(sendMessage, '*');
+    MapasCulturais.Messages.showMessage = function(type, message) {
+        if (type == "danger") {
+            type = "error";
         }
 
-        function sendSizes() {
-            window.parent.postMessage({
-                type: 'resize',
-                data: {
-                    height: document.body.offsetHeight+1,
-                }
-            }, '*');
+        let sendMessage = {
+            type: 'message',
+            data: {
+                type,
+                message
+            }
         }
 
-        setInterval(sendSizes, 200);
-    })
+        window.parent.postMessage(sendMessage, '*');
+    }
+
+    function sendSizes() {
+        window.parent.postMessage({
+            type: 'resize',
+            data: {
+                height: document.body.offsetHeight+1,
+            }
+        }, '*');
+    }
+
+    setInterval(sendSizes, 200);
 })(jQuery)
