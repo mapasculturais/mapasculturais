19de20e13481c8dc6df821719a7ff177edde35b2
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-03-15 23:03:29 -0300

    adiciona propriedade iframe-id ao componente v1-embed-tool

--- src/protected/application/lib/modules/BaseV1EmbedTools/components/v1-embed-tool/template.php
+++ src/protected/application/lib/modules/BaseV1EmbedTools/components/v1-embed-tool/template.php
@@ -7,4 +7,4 @@
 
 use MapasCulturais\i;
 ?>
-<iframe :src="url" ref="iframe" :style="{height: iframeHeight, maxHeight: maxHeight, minHeight: minHeight, width: '100%', border: 'none'}" ></iframe>
+<iframe :id="iframeId" :src="url" ref="iframe" :style="{height: iframeHeight, maxHeight: maxHeight, minHeight: minHeight, width: '100%', border: 'none'}" ></iframe>
