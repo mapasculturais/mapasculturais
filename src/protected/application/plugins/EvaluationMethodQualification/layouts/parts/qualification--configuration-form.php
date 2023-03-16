55dc2ba7a4303e070e82d313e836161d25b7f5b4
erleibiazzio <erleibiazzio@gmail.com>
2023-03-16 20:24:24 -0300

    Aplica melhorias no layout da tela de configuração da avaliação EvaluationMethodQualification Ref.: #2450

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
@@ -24,14 +24,24 @@ use MapasCulturais\i;
 
             <tr id="{{cri.id}}" ng-repeat="cri in data.criteria" ng-if="cri.sid == section.id">
                 <td>
-                    <input type="text" ng-model="cri.description" ng-model-options='{ debounce: data.debounce }' placeholder="<?php i::_e('Descrição do critério') ?>" class="section-name edit" ng-change="save({sections: data.sections})">
-                    <edit-box id="{{cri.id}}" position="right" title="<?php i::esc_attr_e("Informe as opções do critério separadas quebrando linha");?>" cancel-label="<?php i::esc_attr_e("Fechar");?>" close-on-cancel='true' spinner-condition="data.spinner">
-                        <textarea ng-model="data.options[cri.id]" ng-model-options='{ debounce: data.debounce }' cols="70" rows="5" ng-change="save({sections: data.sections})"></textarea>
-                    </edit-box>
+                    <div>
+                        <input type="text" ng-model="cri.description" ng-model-options='{ debounce: data.debounce }' placeholder="<?php i::_e('Descrição do critério') ?>" class="section-name edit" ng-change="save({sections: data.sections})">
+                        <edit-box id="{{cri.id}}" position="right" title="<?php i::esc_attr_e("Informe as opções do critério separadas quebrando linha"); ?>" cancel-label="<?php i::esc_attr_e("Fechar"); ?>" close-on-cancel='true' spinner-condition="data.spinner">
+                            <textarea ng-model="data.options[cri.id]" ng-model-options='{ debounce: data.debounce }' cols="70" rows="5" ng-change="save({sections: data.sections})"></textarea>
+                        </edit-box>
+                    </div>
                 </td>
                 <td>
-                    <a id="delete-account--button" ng-click="editbox.open(cri.id, $event)" rel='noopener noreferrer' class="btn btn-primary add"><?php i::_e('Definir opções') ?></a>
-                    <button ng-click="deleteCriterion(cri)" ng-model-options='{ debounce: data.debounce }' class="btn btn-danger delete" title="<?php i::_e('Remover critério') ?>"></button>
+                    <div>
+                        <div>
+                            <input type="checkbox" ng-model="cri.notApplyOption" ng-model-options='{ debounce: data.debounce }' ng-change="save({sections: data.sections})">
+                            <?php i::_e('Habilitar a opção Não se aplica') ?>
+                        </div>
+                       <div>
+                       <a id="delete-account--button" ng-click="editbox.open(cri.id, $event)" rel='noopener noreferrer' class="btn btn-primary add"><?php i::_e('Definir opções') ?></a>
+                        <button ng-click="deleteCriterion(cri)" ng-model-options='{ debounce: data.debounce }' class="btn btn-danger delete" title="<?php i::_e('Remover critério') ?>"></button>
+                       </div>
+                    </div>
                 </td>
                 <td></td>
             </tr>
