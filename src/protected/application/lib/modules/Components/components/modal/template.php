52bdf4b11cc24a5f6a4bccde67f0ef8e9980d394
robedj <ed.wassouf@gmail.com>
2023-03-16 11:25:31 -0400

    Corrige abertura da modal no popover no mobile Ref.: #2468

--- src/protected/application/lib/modules/Components/components/modal/template.php
+++ src/protected/application/lib/modules/Components/components/modal/template.php
@@ -9,15 +9,15 @@ use MapasCulturais\i;
             <button v-if="closeButton" class="modal__close" @click="close()"> <mc-icon name="close"></mc-icon> </button>
         </div>
         <div class="modal__content">
-            <slot :close="close" :open="open" :loading="loading"></slot>
+            <slot :close="close" :open="open" :toggle="toggle" :loading="loading"></slot>
         </div>
         <div class="modal__action">
             <loading :condition="processing"></loading>
-            <slot v-if="!processing" name="actions" :close="close" :open="open" :loading="loading"></slot>
+            <slot v-if="!processing" name="actions" :close="close" :open="open" :toggle="toggle" :loading="loading"></slot>
         </div>
     </template>
 </vue-final-modal>
 
-<slot name="button" :close="close" :open="open" :loading="loading">
+<slot name="button" :close="close" :open="open" :toggle="toggle" :loading="loading">
     <button :class="['button',buttonClasses]" @click="open()">{{buttonLabel || '<?= i::__('Defina a propriedade `buttonLabel` do componente modal') ?>'}}</button>
 </slot>
