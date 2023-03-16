4244f0e8ccd5b0e0cca61397c43795aba15c854e
robedj <ed.wassouf@gmail.com>
2023-03-16 08:06:49 -0400

    W.I.P:Refatoração do poppover para abrir modal na versão mobile Ref.#2480

--- src/protected/application/lib/modules/Components/components/popover/template.php
+++ src/protected/application/lib/modules/Components/components/popover/template.php
@@ -1,9 +1,11 @@
 <?php
+
 use MapasCulturais\i;
 ?>
 
 <div ref="content">
-    <VDropdown :triggers="[]" :shown="active" :autoHide="false" @apply-show="focus()">
+
+    <VDropdown v-if="!isMobile()" :triggers="[]" :shown="active" :autoHide="false" @apply-show="focus()">
         <slot name="button" :open="open" :close="close" :toggle="toggle" :active="active">
             <button :class="['button', buttonClasses]">{{buttonLabel || '<?= i::__('Defina a propriedade button-label do componente popover') ?>'}}</button>
         </slot>
@@ -13,4 +15,24 @@ use MapasCulturais\i;
             </div>
         </template>
     </VDropdown>
+
+
+    <modal >
+        <template #default>
+            <div ref="content" class="popover__content">
+                <slot :open="modal.open" :close="modal.close" :toggle="modal.toggle" :active="modalOpen"></slot>
+            </div>
+        </template>
+
+        <template #button="modal">
+            <slot name="button" :open="modal.open" :close="modal.close" :toggle="modal.toggle" :active="modalOpen">
+                <button :class="['button', buttonClasses]">{{buttonLabel || '<?= i::__('Defina a propriedade button-label do componente popover') ?>'}}</button>
+            </slot>
+        </template>
+
+        <template #actions="modal">
+            <a class="button button--secondary" @click="modal.close()"><?php i::_e('Fechar') ?></a>
+        </template>
+    </modal>
+
 </div>
