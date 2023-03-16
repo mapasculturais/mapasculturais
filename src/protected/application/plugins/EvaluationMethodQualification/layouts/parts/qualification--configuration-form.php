b55b39e198804b7a6ff27053c923ca399cb54be0
erleibiazzio <erleibiazzio@gmail.com>
2023-03-16 08:17:44 -0300

    Ajusta Tabela da tela de configuração Ref.: #2450

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
@@ -30,8 +30,6 @@ use MapasCulturais\i;
                 </td>
                 <td>
                     <input type="text" ng-model="cri.description" ng-model-options='{ debounce: data.debounce }' placeholder="<?php i::_e('Descrição do critério') ?>" class="section-name edit" ng-change="save({sections: data.sections})">
-                </td>
-                <td>
                     <edit-box id="{{cri.id}}" position="right" title="<?php i::esc_attr_e("Informe as opções do critério separadas quebrando linha");?>" cancel-label="<?php i::esc_attr_e("Fechar");?>" close-on-cancel='true' spinner-condition="data.spinner">
                         <textarea ng-model="data.options[cri.id]" ng-model-options='{ debounce: data.debounce }' cols="70" rows="5" ng-change="save({sections: data.sections})"></textarea>
                     </edit-box>
@@ -40,6 +38,7 @@ use MapasCulturais\i;
                     <a id="delete-account--button" ng-click="editbox.open(cri.id, $event)" rel='noopener noreferrer' class="btn btn-primary add"><?php i::_e('Definir opções') ?></a>
                     <button ng-click="deleteCriterion(cri)" ng-model-options='{ debounce: data.debounce }' class="btn btn-danger delete" title="<?php i::_e('Remover critério') ?>"></button>
                 </td>
+                <td></td>
             </tr>
         </table>
     </section>
