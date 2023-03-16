c29205b00ec89cfd581d3da551e55129cceb4f3e
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-15 21:31:55 -0300

    Ativa o autosave no entity-field dos campos das fases da aba de configuracao (refs #2482)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-evaluation/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-evaluation/template.php
@@ -8,11 +8,11 @@ $this->import('
 <mapas-card>
     <div class="evaluation-step grid-12">
         <div class="config-input col-12">
-            <entity-field :entity="phase" prop="name" label="<?= i::esc_attr__('Título') ?>" hide-required></entity-field>
+            <entity-field :entity="phase" prop="name" :autosave="300" label="<?= i::esc_attr__('Título') ?>" hide-required></entity-field>
         </div>
 
-        <entity-field :entity="phase" prop="evaluationFrom" classes="col-6 sm:col-12" :min="minDate?._date" :max="phase.evaluationTo?._date"></entity-field>
-        <entity-field :entity="phase" prop="evaluationTo" classes="col-6 sm:col-12" :min="phase.evaluationFrom?._date" :max="maxDate?._date"></entity-field>
+        <entity-field :entity="phase" prop="evaluationFrom" :autosave="300" classes="col-6 sm:col-12" :min="minDate?._date" :max="phase.evaluationTo?._date"></entity-field>
+        <entity-field :entity="phase" prop="evaluationTo" :autosave="300" classes="col-6 sm:col-12" :min="phase.evaluationFrom?._date" :max="maxDate?._date"></entity-field>
 
 
         <div class="evaluation-text col-12">
@@ -21,10 +21,12 @@ $this->import('
         <div class="evaluation-config col-12 field">
             <label> <?= i::__("Texto configuração geral") ?>
             </label>
-            <textarea v-model="phase.infos['general']" class="evaluation-config__area" rows="10"></textarea>
+<!--            <entity-field :entity="phase" prop="infos"></entity-field>-->
+                <textarea v-model="phase.infos['general']" class="evaluation-config__area" rows="10"></textarea>
         </div>
-        <div class="col-6 sm:col-12 field" v-for="category in categories">
-            <label> {{ category }}
+        <div class="col-6 sm:col-12 field" v-for="(category, index) in categories">
+            <label :key="index"> {{ category }}
+<!--                <entity-field :entity="phase" prop="infos[category]"></entity-field>-->
                 <textarea v-model="phase.infos[category]" style="width: 100%" rows="10"></textarea>
             </label>
         </div>
@@ -38,9 +40,6 @@ $this->import('
                     </a>
                 </template>
             </confirm-button>
-        </div>
-        <div class="phase-delete col-6">
-            <a @click="phase.save()" class="phase-delete__trash " href="#"><mc-icon name="upload"></mc-icon><label class="phase-delete__label"><?= i::__("Salvar") ?></label></a>
         </div>
     </div>
 </mapas-card>
