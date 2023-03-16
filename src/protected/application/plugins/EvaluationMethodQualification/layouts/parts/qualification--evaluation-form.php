fc853b9c5901299cc4ca010d9c4dd7da9fa451a9
erleibiazzio <erleibiazzio@gmail.com>
2023-03-16 08:18:28 -0300

    Reestrutura leyout do formulário de avaliação Ref. #2450

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-form.php
@@ -1,38 +1,50 @@
 <?php
 
-namespace EvaluationMethodQualification;
-
 use MapasCulturais\i;
+
+$plugin = $app->plugins['EvaluationMethodQualification'];
 
 $params = ['registration' => $entity, 'opportunity' => $opportunity];
 ?>
 <?php $this->applyTemplateHook('evaluationForm.qualification', 'before', $params); ?>
-<div id="qualification-evaluation-form" class="qualification-evaluation-form">
+<div ng-controller="QualificationEvaluationMethodFormController" class="qualification-evaluation-form">
     <?php $this->applyTemplateHook('evaluationForm.qualification', 'begin', $params); ?>
-    <div id="qualification-evaluation-form--container"></div>
+    <section ng-repeat="section in ::data.sections">
+        <table>
+            <tr>
+                <th colspan="2">
+                    {{section.name}}
+                </th>
+            </tr>
+
+            <tr ng-repeat="cri in ::data.criteria" ng-if="cri.sid == section.id">
+                <td>
+                    <?php echo $plugin->step ?><label for="{{cri.id}}">{{cri.description}}:</label>
+                </td>
+                <td>
+                    <select>
+                        <option ng-repeat="option in ::cri.options">{{option}}</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr class="subtotal">
+                <td><?php i::_e('Subtotal') ?></td>
+                <td>{{subtotalSection(section)}}</td>
+            </tr>
+        </table>
+    </section>
+    <hr>
+    <label>
+        <?php i::_e('Parecer Técnico') ?>
+        <textarea name="data[obs]" ng-model="evaluation['obs']"></textarea>
+    </label>
+    <hr>
+
+    <div class='total'>
+        <?php i::_e('Pontuação Total'); ?>: <strong>{{total(total)}}</strong><br>
+        <?php i::_e('Pontuação Máxima'); ?>: <strong>{{max(total)}}</strong>
+    </div>
     <?php $this->applyTemplateHook('evaluationForm.qualification', 'end', $params); ?>
 </div>
 <?php $this->applyTemplateHook('evaluationForm.qualification', 'after', $params); ?>
-
-<script id='qualification-evaluation-form-template' class='js-mustache-template' type="html/template">
-    <?php $this->applyTemplateHook('evaluationForm.qualification.template', 'before', $params); ?>
-    <div id="evaluatin-field-{{id}}" class="qualification-evaluation-form--field">
-        <?php $this->applyTemplateHook('evaluationForm.qualification.template', 'begin', $params); ?>
-        <h6>{{label}}</h6>
-        <div>
-            <select name="data[{{id}}][qualification]">
-                <option>Opção 1</option>
-                <option>Opção 2</option>
-            </select>
-        </div>
-
-        {{olegario}}
-        
-        <label class="textarea-label">
-            <?php i::_e('Justificativa / Observações') ?><br>
-            <textarea name="data[{{id}}][obs]">{{obs}}</textarea>
-        </label>
-        <?php $this->applyTemplateHook('evaluationForm.qualification.template', 'end', $params); ?>
-    </div>
-    <?php $this->applyTemplateHook('evaluationForm.qualification.template', 'after', $params); ?>
-</script>
