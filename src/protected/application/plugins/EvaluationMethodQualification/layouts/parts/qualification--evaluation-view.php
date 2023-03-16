db55e0d069e5b8269ae736dbfb7da21eadba37a2
erleibiazzio <erleibiazzio@gmail.com>
2023-03-16 20:27:16 -0300

    Ajusta tela de visualização da avaliação  na tela da inscrição na avaliação EvaluationMethodQualification ref.: #2450

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
@@ -1,28 +1,24 @@
 <?php
 use MapasCulturais\i;
 ?>
-<div ng-controller="TechnicalEvaluationMethodFormController" class="technical-evaluation-form">
-    <div ng-if="!data.empty">
-        <strong><?php i::_e('Pontuações'); ?>:</strong>
+<div ng-controller="QualificationEvaluationMethodFormController" class="qualification-evaluation-form">
+    <div>
+        <strong><?php i::_e('Resultados das seções'); ?>:</strong>
         <section ng-repeat="section in ::data.sections">
-             {{section.name}}: <strong>{{subtotalSection(section)}}</strong>
+             {{section.name}}: 
+             <strong ng-if="subtotalSection(section) == 'Habilitado'" class="approved">{{subtotalSection(section)}}</strong>
+             <strong ng-if="subtotalSection(section) == 'Inabilitado'" class="repproved">{{subtotalSection(section)}}</strong>
         </section>
         <hr>
         <section class='total'>
-            <?php i::_e('Total'); ?>: <strong>{{total(total)}}</strong><br>
-            <?php i::_e('Máxima'); ?>: <strong>{{max(total)}}</strong>
+            <?php i::_e('Status'); ?>: 
+            <strong ng-if="total() == 'Habilitado'" class="approved">{{total(total)}}</strong>
+            <strong ng-if="total() == 'Inabilitado'" class="repproved">{{total(total)}}</strong><br>
         </section>
         <hr>
         <label>
             <strong><?php i::_e('Parecer Técnico') ?>:</strong>
             <p>{{evaluation['obs']}}</p>
         </label>
-        <div ng-show="data.enableViability=='true'">
-            <hr>
-            <label>
-                <strong><?php i::_e('Exequibilidade Orçamentária') ?>:</strong>
-                <p>{{viabilityLabel(evaluation['viability'])}}</p>
-            </label>
-        </div>
     </div>
 </div>
