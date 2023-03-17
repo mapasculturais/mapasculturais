a81b2aba1e7dcf1f8c93d132830d31c8b70c83b7
robedj <ed.wassouf@gmail.com>
2023-03-17 13:19:25 -0400

    adição do titulo no componente entity-select na modal Ref.: #2480

--- src/protected/application/lib/modules/Components/components/select-entity/template.php
+++ src/protected/application/lib/modules/Components/components/select-entity/template.php
@@ -4,7 +4,7 @@ use MapasCulturais\i;
 
  $this->import('entities popover') ?>
 
-    <popover :openside="openside" :button-label="buttonLabel" :button-classes="[buttonClasses, type + '__color']"> 
+    <popover :openside="openside" :button-label="buttonLabel" :title="itensText" :button-classes="[buttonClasses, type + '__color']"> 
         <template #button="{ toggle }">
             <slot name="button" :toggle="toggle"></slot>
         </template>
