4752276b080f802b3b01f9d9e7aae417a06da9cd
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-20 21:19:30 -0300

    Habilita o autosave na data de publicação do resultado <opportunity-basic-info> (refs #2487)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-basic-info/script.js
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-basic-info/script.js
@@ -3,12 +3,6 @@ app.component('opportunity-basic-info' , {
 
     data () {
         return {
-            locale: $MAPAS.config.locale,
-            dateStart: '',
-            dateEnd: '',
-            dateFinalResult: '',
-            dayNames: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-            accountability: false
         };
     },
 
@@ -18,33 +12,4 @@ app.component('opportunity-basic-info' , {
             required: true
         }
     },
-
-    mounted() {
-        this.dateStart = $MAPAS.requestedEntity.registrationFrom?.date;
-        this.dateEnd = $MAPAS.requestedEntity.registrationTo?.date;
-    },
-
-    watch: {
-      dateStart: {
-          handler (val) {
-              if($MAPAS.requestedEntity.registrationFrom){
-                $MAPAS.requestedEntity.registrationFrom.date = val;
-              }
-          }
-      },
-
-      dateEnd: {
-          handler (val) {
-            if($MAPAS.requestedEntity.registrationTo){
-                $MAPAS.requestedEntity.registrationTo.date = val;
-            }  
-          }
-      }
-    },
-
-    methods: {
-        dateFormat(date) {
-            return new Date(date).toLocaleString();
-        }
-    }
 });
