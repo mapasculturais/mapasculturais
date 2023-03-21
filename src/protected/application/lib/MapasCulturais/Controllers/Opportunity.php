f30879891ba8202c45473c98972f6a231ce52440
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-21 19:20:53 -0300

    Criando nova rota para listagem dos inscritos <registrations> (refs #2488)

--- src/protected/application/lib/MapasCulturais/Controllers/Opportunity.php
+++ src/protected/application/lib/MapasCulturais/Controllers/Opportunity.php
@@ -1078,4 +1078,27 @@ class Opportunity extends EntityController {
       $this->render('form-builder', ['entity' => $entity]);
     }
   }
+
+  function GET_registrations() {
+    $this->requireAuthentication();
+    $app = App::i();
+
+    $entity = $this->requestedEntity;
+
+    if (!$entity) {
+      $app->pass();
+    }
+
+    $entity->checkPermission('modify');
+
+    if($entity->usesNested()){
+
+      $child_entity_request = $app->repo('RequestChildEntity')->findOneBy(['originType' => $entity->getClassName(), 'originId' => $entity->id]);
+
+      $this->render('registrations', ['entity' => $entity, 'child_entity_request' => $child_entity_request]);
+
+    }else{
+      $this->render('registrations', ['entity' => $entity]);
+    }
+  }
 }
