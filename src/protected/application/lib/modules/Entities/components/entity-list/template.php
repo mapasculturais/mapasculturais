4c3aef767827dbbe4a1787a6ef5323d4775b98fb
robedj <ed.wassouf@gmail.com>
2023-03-21 13:52:11 -0400

    Refatoração no componente entity-list REF: #2426

--- src/protected/application/lib/modules/Entities/components/entity-list/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-list/template.php
@@ -11,19 +11,21 @@ $this->import('
 <div v-if="this.ids.length>0" class="entity-list">
     <?php $this->applyTemplateHook('entity-list','begin'); ?>
     <label class="col-12 entity-list__title"> {{title}} </label>
-    <ul class="entity-list__list">
-        <entities select="id,name,files.avatar" order="name ASC" :type="type" :query="query" #default="{entities}">
-            <li class="col-12 entity-list__list-item" v-for="entity in entities">
-                <mc-link class="entity-list__list-item-link" :entity="entity">
-                    <div class="entity-list__list-item-img">
-                        <img v-if="entity.files.avatar?.transformations?.avatarSmall?.url" :src="entity.files.avatar?.transformations?.avatarSmall?.url">
-                        <mc-icon v-if="!entity.files.avatar?.transformations?.avatarSmall?.url" name="agent-1"></mc-icon>
-                    </div>
-                    <div class="entity-list__list-item"> {{entity.name}} </div>
-                </mc-link>
-            </li>
-        </entities>
-    </ul>
+    <entities select="id,name,files.avatar" order="name ASC" :type="type" :query="query" #default="{entities}">
+        <slot :entities="entities">
+            <ul v-if="entities.length>0" class="entity-list__list">
+                <li v-for="entity in entities" class="col-12 entity-list__list-item">
+                    <mc-link class="entity-list__list-item-link" :entity="entity">
+                        <div class="entity-list__list-item-img">
+                            <img v-if="entity.files.avatar?.transformations?.avatarSmall?.url" :src="entity.files.avatar?.transformations?.avatarSmall?.url">
+                            <mc-icon v-if="!entity.files.avatar?.transformations?.avatarSmall?.url" name="agent-1"></mc-icon>
+                        </div>
+                        <div class="entity-list__list-item"> {{entity.name}} </div>
+                    </mc-link>
+                </li>
+            </ul>
+        </slot>
+    </entities>
     <?php $this->applyTemplateHook('entity-list','end'); ?>
 </div>
 <?php $this->applyTemplateHook('entity-list','after'); ?>
