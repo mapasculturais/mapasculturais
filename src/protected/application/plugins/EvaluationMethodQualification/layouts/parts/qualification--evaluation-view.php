c35bdf430a0dd01f3712c0213838ad0973bca83a
LimaSystem <wolegario49@gmail.com>
2023-03-21 14:35:14 -0500

    Ajusta internacionalização do método de avaliação habilitação documental Ref.:#2497

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
@@ -6,8 +6,8 @@ use MapasCulturais\i;
         <strong><?php i::_e('Resultados das seções'); ?>:</strong>
         <section ng-repeat="section in ::data.sections">
              {{section.name}}: 
-             <strong ng-if="subtotalSection(section) == <?php i::_e('Habilitado')?>" class="approved">{{subtotalSection(section)}}</strong>
-             <strong ng-if="subtotalSection(section) == <?php i::_e('Inabilitado')?>" class="repproved">{{subtotalSection(section)}}</strong>
+             <strong ng-if="subtotalSection(section) == 'Habilitado'" class="approved">{{subtotalSection(section)}}</strong>
+             <strong ng-if="subtotalSection(section) == 'Inabilitado'" class="repproved">{{subtotalSection(section)}}</strong>
         </section>
         <hr>
         <section class='total'>
