e8cc307ff065bad3e5644903ac24b13f750cc4da
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-03-22 14:43:08 -0300

    Ajustes no opportunity-header - Exibição do tipo do onwerEntity e link para o mesmo (Ref.: #2475)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-header/texts.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-header/texts.php
@@ -1,4 +1,10 @@
 <?php
 use MapasCulturais\i;
 
-return [];
+return [
+    'Oportunidade' => i::__('Oportunidade'),
+    'Agente' => i::__('Agente'),
+    'Evento' => i::__('Evento'),
+    'Espaço' => i::__('Espaço'),
+    'Projeto' => i::__('Projeto'),
+];
