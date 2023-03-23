8620082be69b22a64f5296fb0ea85c6c087b9b91
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-03-23 15:17:25 -0300

    corrige cache de entidades para o escopo default no API.js

--- src/protected/application/lib/modules/Components/assets/js/components-base/API.js
+++ src/protected/application/lib/modules/Components/assets/js/components-base/API.js
@@ -23,16 +23,19 @@ globalThis.useEntitiesCache = Pinia.defineStore('entitiesCache', {
 
     actions: {
         store(entity, scope) {
+            scope = scope || 'default';
             this[scope] = this[scope] || {};
             this[scope][entity.cacheId] = entity;
         },
 
         remove(entity, scope) {
+            scope = scope || 'default';
             this[scope] = this[scope] || {};
             delete this[scope][entity.cacheId];
         },
         
         fetch(cacheId, scope) {
+            scope = scope || 'default';
             this[scope] = this[scope] || {};
             return this[scope][cacheId];
         }
