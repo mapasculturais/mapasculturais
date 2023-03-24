fd3045d6f0c2b09f9b535aa324d705b8d94dbc41
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-24 19:49:20 -0300

    Ajustado pagina de configuracao de formulario de coleta de dados (refs #2421)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
@@ -7,11 +7,11 @@ $this->layout = 'entity';
 <div class="form-builder__bg-content form-builder__bg-content--spacing">
     <h4><?= i::__("Categorias de inscrição") ?></h4>
     <span class="subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></span>
-    <div v-if="phase && phase.registrationCategTitle">
-        <entity-field :entity="phase" prop="registrationCategTitle"></entity-field>
+    <div>
+        <entity-field :entity="entity" prop="registrationCategTitle"></entity-field>
     </div>
-    <div v-if="phase && phase.registrationCategTitle">
-        <entity-field :entity="phase" prop="registrationCategDescription"></entity-field>
+    <div>
+        <entity-field :entity="entity" prop="registrationCategDescription"></entity-field>
     </div>
     <div>
         <p>Categorias</p>
