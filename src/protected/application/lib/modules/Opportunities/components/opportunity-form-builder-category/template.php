38dcb694c81cce6ac0677c35be856ab2a828df4e
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-24 09:25:41 -0300

    Incluindo entity-field nos campos de input <opportunity-form-builder-category> (refs #2505)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
@@ -2,19 +2,16 @@
 use MapasCulturais\i;
 $this->layout = 'entity';
 
-$this->import('entity-field')
 ?>
 
 <div class="form-builder__bg-content form-builder__bg-content--spacing">
-    <h4>Categorias de inscrição</h4>
-    <span class="subtitle">Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, "categorias" ou "modalidades".</span>
-    <div>
-        <p>Grupo de categorias</p>
-        <input type="text" />
+    <h4><?= i::__("Categorias de inscrição") ?></h4>
+    <span class="subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></span>
+    <div v-if="phase && phase.registrationCategTitle">
+        <entity-field :entity="phase" prop="registrationCategTitle"></entity-field>
     </div>
-    <div>
-        <p>Descrição do grupo</p>
-        <input type="text" />
+    <div v-if="phase && phase.registrationCategTitle">
+        <entity-field :entity="phase" prop="registrationCategDescription"></entity-field>
     </div>
     <div>
         <p>Categorias</p>
