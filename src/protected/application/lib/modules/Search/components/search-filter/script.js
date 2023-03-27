a6a0b2ab169aafbb06e16531d0fbd9e7664cb43d
robedj <ed.wassouf@gmail.com>
2023-03-27 15:23:19 -0400

    Correção da exibição do header na classe showMenu via js, no componente search-filter Ref.: #2528

--- src/protected/application/lib/modules/Search/components/search-filter/script.js
+++ src/protected/application/lib/modules/Search/components/search-filter/script.js
@@ -46,6 +46,7 @@ app.component('search-filter', {
             const header = document.getElementById('main-header');
             if (!this.showMenu) {
                 header.style.top=0;
+                // header.style.position = 'fixed';
             }
             else {
                 header.removeAttribute('style');
