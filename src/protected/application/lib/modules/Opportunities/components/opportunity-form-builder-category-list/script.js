ec0dc28130cde99bb23206ad533837f83ff768a3
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-26 22:24:52 -0300

    Componente para listagem e edicao das categorias <opportunity-form-builder-category-list> (refs #2505)

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category-list/script.js
@@ -0,0 +1,37 @@
+app.component('opportunity-form-builder-category-list' , {
+    template: $TEMPLATES['opportunity-form-builder-category-list'],
+
+    setup() {
+        // os textos estão localizados no arquivo texts.php deste componente
+        const text = Utils.getTexts('opportunity-form-builder-category-list');
+        return { text }
+    },
+    created () {
+      this.categories = $MAPAS.requestedEntity.registrationCategories;
+    },
+    data () {
+      return {
+          categories: [],
+          category: null
+      };
+    },
+    methods: {
+      addCategory () {
+          this.categories.push(this.category);
+      },
+      clear () {
+          this.category = null;
+      },
+      deleteItem (item) {
+          const index = this.categories.indexOf(item);
+          this.categories.splice(index, 1);
+      }
+    },
+    watch: {
+        categories: {
+            handler(val) {
+                $MAPAS.requestedEntity.registrationCategories = val;
+            }
+        }
+    }
+});
