41ee5528d39690b2271b3b1e2aed6540a2252adb
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-03-27 22:46:36 -0300

    transforma a modal de notificação num popover

--- src/protected/application/lib/modules/Notifications/components/notification-modal/template.php
+++ src/protected/application/lib/modules/Notifications/components/notification-modal/template.php
@@ -6,32 +6,36 @@ $this->import('
     mc-link
     modal 
     notification-list
+    popover
 ');
 ?>
-<modal v-if="notificationsCount>0" :title="modalTitle" classes="create-modal notification-modal" button-label="<?= i::esc_attr__("Notificações") ?>" @open="" @close="">
+
+<popover openside="down-right">
     <template #default>
-        <div class="grid-12 notification-modal__header">
-            <div class="col-6 " v-if="notificationsCount>0">
-                <label class="count">
-                    <?= i::__('Você tem') ?>
-                    <span class="count-counter">{{ notificationsCount }}</span>
-                    <?= i::__('notificação') ?>
-                </label>
+        <div style="max-width: 500px;">
+            <div class="grid-12 notification-modal__header">
+                <div class="col-6 " v-if="notificationsCount>0">
+                    <label class="count">
+                        <?= i::__('Você tem') ?>
+                        <span class="count-counter">{{ notificationsCount }}</span>
+                        <?= i::__('notificação') ?>
+                    </label>
+                </div>
             </div>
-        </div>
-        <div class="grid-12 notification-modal__content">
-            <div class="col-12  notifications">
-                <notification-list styleCss='divider'></notification-list>
-            </div>
-            <div class="col-12 link" v-if="notificationsCount">
-                <mc-link route="panel/index" hash="notifications"><?= i::__('Ver todas as notificações') ?></mc-link>
+            <div class="grid-12 notification-modal__content" style="max-height: 700px; overflow-y:auto">
+                <div class="col-12  notifications">
+                    <notification-list styleCss='divider'></notification-list>
+                </div>
+                <div class="col-12 link" v-if="notificationsCount">
+                    <mc-link route="panel/index" hash="notifications"><?= i::__('Ver todas as notificações') ?></mc-link>
+                </div>
             </div>
         </div>
     </template>
-    <template #button="modal">
-        <!-- v-if="viewport == 'desktop'" -->
-        <div v-if="viewport=='desktop'" class="notification-modal__menu-desk" @click="modal.open">
-            <label class="label" @click="modal.open"><?= i::__('Notificações') ?></label>
+
+    <template #button="popover">
+        <div v-if="viewport=='desktop'" class="notification-modal__menu-desk" @click="popover.toggle()">
+            <label class="label"><?= i::__('Notificações') ?></label>
             <div class="icon">
                 <mc-icon name='notification'></mc-icon>
                 <span v-if="notificationsCount>0" class="count">{{notificationsCount}}</span>
@@ -39,13 +43,12 @@ $this->import('
         </div>
 
         <!-- Refatorar -->
-        <a v-if="viewport=='mobile'" class="notification-modal__menu-mobile" @click="modal.open">
+        <a v-if="viewport=='mobile'" class="notification-modal__menu-mobile" @click="popover.toggle()">
             <div class="icon">
                 <mc-icon name='notification'></mc-icon>
                 <span v-if="notificationsCount>0" class="count">{{notificationsCount}}</span>
             </div>
-            <label class="label" @click="modal.open"><?= i::__('Notificações') ?></label>
+            <label class="label"><?= i::__('Notificações') ?></label>
         </a>
-           
     </template>
-</modal>
+</popover>
