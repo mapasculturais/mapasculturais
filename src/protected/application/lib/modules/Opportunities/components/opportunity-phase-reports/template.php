af36717b81b3b0b6a1f4a05d855193334ec01531
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-28 19:46:40 -0300

    Ajuste na logica de incorporacao de fases <opportunity-phase-reports> (refs #2489)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-reports/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-reports/template.php
@@ -12,41 +12,41 @@ $this->import('
     v1-embed-tool
 ');
 ?>
-<mc-stepper-vertical :items="phases" allow-multiple>
+<mc-stepper-vertical :items="newPhases" allow-multiple>
     <template #header-title="{index, item}">
         <div class="phase-stepper">
-            <h2 v-if="index" class="phase-stepper__name">{{ isJoinedPhaseLabel(index)}}</h2>
-            <h2 v-if="!index" class="phase-stepper__period">{{ isJoinedPhaseLabel(index)}}</h2>
+            <h2 v-if="index" class="phase-stepper__name">{{ item.label }}</h2>
+            <h2 v-if="!index" class="phase-stepper__period">{{ item.label }}</h2>
             <p class="phase-stepper__type" v-if="item.__objectType == 'opportunity' && !item.isLastPhase">
                 <label class="phase-stepper__type--name"><?= i::__('Tipo') ?></label>:
                 <label class="phase-stepper__type--item"><?= i::__('Coleta de dados') ?></label>
             </p>
             <p v-if="item.__objectType == 'evaluationmethodconfiguration'" class="phase-stepper__type">
-                <label class="phase-stepper__type--name"><?= i::__('Tipo') ?></label>: <label class="phase-stepper__type--item">{{evaluationMethods[item.type].name}}</label>
+                <label class="phase-stepper__type--name"><?= i::__('Tipo') ?></label>: <label class="phase-stepper__type--item">{{ item.type }}</label>
             </p>
         </div>
     </template>
     <template #default="{index, item}">
 
-        <template v-if="isJoinedPhase(index)">
-            <mapas-card v-if="item.opportunity && item.opportunity.id">
-                <v1-embed-tool route="reportmanager" :id="item.opportunity.id"></v1-embed-tool>
+        <template v-if="item.phases.length > 1">
+            <mapas-card v-if="item.phases[0].opportunity && item.phases[0].opportunity.id">
+                <v1-embed-tool route="reportmanager" :id="item.phases[0].opportunity.id"></v1-embed-tool>
             </mapas-card>
-            <mapas-card v-if="item && item.id">
-                <v1-embed-tool route="reportmanager" :id="item.id"></v1-embed-tool>
+            <mapas-card v-if="item.phases[0] && item.phases[0].id">
+                <v1-embed-tool route="reportmanager" :id="item.phases[0].id"></v1-embed-tool>
             </mapas-card>
         </template>
 
         <template v-else>
-            <template v-if="item.__objectType == 'evaluationmethodconfiguration'">
-                <mapas-card v-if="item.opportunity && item.opportunity.id">
-                    <v1-embed-tool route="reportmanager" :id="item.opportunity.id"></v1-embed-tool>
+            <template v-if="item.phases[0] && item.phases[0].__objectType == 'evaluationmethodconfiguration'">
+                <mapas-card v-if="item.phases[0].opportunity && item.phases[0].opportunity.id">
+                    <v1-embed-tool route="reportmanager" :id="item.phases[0].opportunity.id"></v1-embed-tool>
                 </mapas-card>
             </template>
 
-            <template v-if="item.__objectType == 'opportunity'">
-                <mapas-card v-if="item && item.id">
-                    <v1-embed-tool route="reportmanager" :id="item.id"></v1-embed-tool>
+            <template v-if="item.phases[0] && item.phases[0].__objectType == 'opportunity'">
+                <mapas-card v-if="item.phases[0] && item.phases[0].id">
+                    <v1-embed-tool route="reportmanager" :id="item.phases[0].id"></v1-embed-tool>
                 </mapas-card>
             </template>
         </template>
