64a5f594844e8b2d3cca331fc3644849c547b656
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-03-28 15:11:28 -0300

    refatoração das páginas de busca para a responsabilidade da definição dos campos a serem retornados pela api passar para as páginas e não ficarem mais no ocomponente search-list

--- src/protected/application/lib/modules/Search/views/search/agent.php
+++ src/protected/application/lib/modules/Search/views/search/agent.php
@@ -34,7 +34,7 @@ $this->breadcrumb = [
             </template>
             <tab icon="list" label="<?php i::esc_attr_e('Lista') ?>" slug="list">
                 <div class="search__tabs--list">
-                    <search-list :pseudo-query="pseudoQuery" type="agent">
+                    <search-list :pseudo-query="pseudoQuery" type="agent" select="name,type,shortDescription,files.avatar,seals,endereco,terms" >
                         <template #filter>
                             <search-filter-agent :pseudo-query="pseudoQuery"></search-filter-agent>
                         </template>
