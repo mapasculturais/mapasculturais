87022615eae0db9c4ba3cde85a5a43312a267376
LimaSystem <wolegario49@gmail.com>
2023-03-30 11:00:46 -0500

    Passa o metodo de avaliação documental de plugin para modulo Ref.:#2533

--- src/protected/application/conf/conf-base.d/plugins.php
+++ src/protected/application/conf/conf-base.d/plugins.php
@@ -4,7 +4,6 @@ return [
     'plugins' => [
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
-        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
         'EvaluationMethodQualification' => ['namespace' => 'EvaluationMethodQualification'],
     ]
 ];
