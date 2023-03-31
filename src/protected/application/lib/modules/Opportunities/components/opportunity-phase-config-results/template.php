1df91c27c2afd0bc32f16dd9abca154a0000af8b
rohilario <rodrigohilariocomercial@gmail.com>
2023-03-30 22:49:26 -0300

    Adiciona o componente de publicacao de resultado em na configuracao de fases em <opportunity-phase-config-result> (refs #2534)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-results/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-results/template.php
@@ -2,37 +2,12 @@
 use MapasCulturais\i;
 $this->import('
     confirm-button
-    mc-link
+    opportunity-phase-publish-date-config
 ');
 ?>
 
 <mapas-card>
     <div class="config-phase grid-12">
-
-        <template v-if="phase.publishedRegistrations">
-            <div class="col-12">
-                <button class="button button--text button--text-danger"><?= i::__("Despublicar") ?></button>
-            </div>
-        </template>
-        <template v-else>
-            <div class="col-6 sm:col-12">
-                <entity-field :entity="phase" prop="publishTimestamp" :autosave="300" classes="col-6 sm:col-12" :min="minDate?._date"></entity-field>
-            </div>
-            <div class="col-6 sm:col-12 phase-publish__auto">
-                <input type="checkbox" v-model="phase.autoPublish"><?= i::__("Publicar resultados automaticamente"); ?>
-            </div>
-        </template>
-
-        <div class="config-phase__line-bottom col-12 "></div>
-
-        <div class="col-6">
-            <confirm-button v-if="!isPublishLocked" :message="text('confirmar_publicacao')" @confirm="addPublishRegistrations()">
-                <template #button="modal">
-                    <button class="button button--primary" @click="modal.open()">
-                      <?= i::__("Publicar Resultados") ?>
-                    </button>
-                </template>
-            </confirm-button>
-        </div>
+        <opportunity-phase-publish-date-config :hide-checkbox="phase.publishTimestamp == null" :phase="phase" button-position="right" hideDescription />
     </div>
 </mapas-card>
