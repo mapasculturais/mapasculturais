90afd1220a9cfad1469ed766f01be3246e8cdf4d
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2023-04-01 00:59:36 -0300

    salvamento automático da data de início das inscrições no componente opportunity-phase-config-data-collection

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
@@ -11,8 +11,8 @@ $this->import('
         <div class="config-phase__title col-12">
             <h3 class="config-phase__title--title"><?= i::__("Configuração da fase") ?></h3>
         </div>
-        <entity-field :entity="phase" prop="registrationFrom" classes="col-6 sm:col-12" :min="minDate?._date" :max="phase.registrationTo?._date"></entity-field>
-        <entity-field :entity="phase" prop="registrationTo" :autosave="300" classes="col-6 sm:col-12" :min="phase.registrationFrom?._date" :max="maxDate?._date"></entity-field>
+        <entity-field :entity="phase" prop="registrationFrom" :autosave="300" :min="minDate?._date" :max="phase.registrationTo?._date" classes="col-6 sm:col-12"></entity-field>
+        <entity-field :entity="phase" prop="registrationTo" :autosave="300" :min="phase.registrationFrom?._date" :max="maxDate?._date" classes="col-6 sm:col-12"></entity-field>
         <div class="col-6 sm:col-12">
             <mc-link :entity="phase" route='formBuilder' class="config-phase__info--button button--primary button" icon="external">
               <?= i::__("Configurar formulário") ?>
