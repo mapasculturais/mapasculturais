b722350cfb5cd4dea7250827dba43ed7cc5b54a5
robedj <ed.wassouf@gmail.com>
2023-04-03 14:57:43 -0400

    Corrige exibição do título da modal no componente create-opportunity Ref.: #2546

--- src/protected/application/lib/modules/Entities/components/create-opportunity/script.js
+++ src/protected/application/lib/modules/Entities/components/create-opportunity/script.js
@@ -33,55 +33,50 @@ app.component('create-opportunity', {
             return this.areaErrors ? 'field error' : 'field';
         },
         modalTitle() {
-            if (this.entity?.id) {
-                if (this.entity.status == 1) {
-                    return __('oportunidadeCriada', 'create-opportunity');
-                } else {
-                    if(!this.entity?.id)
-                    return __('criarRascunho', 'create-opportunity');
-                }
-            } else {
-                if(!this.entity?.id)
-                    return __('criarRascunho', 'create-opportunity');
-                else
-                    return __('criarOportunidade', 'create-opportunity');
+            if (!this.entity?.id) {
+                return __('criarOportunidade', 'create-opportunity');
+                console.log(this.entity.id);
+            }
+            if(this.entity.status==0){
+                return __('oportunidadeCriada', 'create-opportunity');
 
             }
-        },
+
+        }
     },
 
     methods: {
-        createEntity() {
+    createEntity() {
 
-            this.entity = new Entity('opportunity');
-            this.entity.type = 1;
-        },
-        createDraft(modal) {
-            this.entity.status = 0;
-            this.save(modal);
-        },
-        createPublic(modal) {
-            //lançar dois eventos
-            this.entity.status = 1;
-            this.save(modal);
-        },
-        save(modal) {
-            modal.loading(true);
-            this.entity.save().then((response) => {
-                this.$emit('create', response);
-                modal.loading(false);
-                Utils.pushEntityToList(this.entity);
-            }).catch((e) => {
-                modal.loading(false);
-            });
-        },
-        setEntity(Entity) {
-            this.entity.ownerEntity = Entity;
-        },
-
-        destroyEntity() {
-            // para o conteúdo da modal não sumir antes dela fechar
-            setTimeout(() => this.entity = null, 200);
-        }
+        this.entity = new Entity('opportunity');
+        this.entity.type = 1;
     },
+    createDraft(modal) {
+        this.entity.status = 0;
+        this.save(modal);
+    },
+    createPublic(modal) {
+        //lançar dois eventos
+        this.entity.status = 1;
+        this.save(modal);
+    },
+    save(modal) {
+        modal.loading(true);
+        this.entity.save().then((response) => {
+            this.$emit('create', response);
+            modal.loading(false);
+            Utils.pushEntityToList(this.entity);
+        }).catch((e) => {
+            modal.loading(false);
+        });
+    },
+    setEntity(Entity) {
+        this.entity.ownerEntity = Entity;
+    },
+
+    destroyEntity() {
+        // para o conteúdo da modal não sumir antes dela fechar
+        setTimeout(() => this.entity = null, 200);
+    }
+},
 });
