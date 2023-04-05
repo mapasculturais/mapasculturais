ff6b25137ecfb0f85d6b84df375734fa77ff0cd8
robedj <ed.wassouf@gmail.com>
2023-04-05 18:00:25 -0400

    Ajustes nas classes do componente opportunity-form-builder-category Ref.: #2561

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
@@ -1,21 +1,30 @@
 <?php
 use MapasCulturais\i;
 $this->layout = 'entity';
-
-$this->import('opportunity-form-builder-category-list')
-
+$this->import('opportunity-form-builder-category-list mapas-card')
 ?>
 
-<div class="form-builder__bg-content form-builder__bg-content--spacing">
-    <h4><?= i::__("Categorias de inscrição") ?></h4>
-    <span class="subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></span>
-    <div>
-        <entity-field :entity="entity" prop="registrationCategTitle"></entity-field>
-    </div>
-    <div>
-        <entity-field :entity="entity" prop="registrationCategDescription"></entity-field>
-    </div>
-    <div>
-        <opportunity-form-builder-category-list></opportunity-form-builder-category-list>
-    </div>
-</div>
+<!-- class="form-builder__bg-content form-builder__bg-content--spacing" poderia ser um mc-card -->
+<mapas-card>
+    <template #title>
+        <div class="category-header">
+            <label class="category-header__title"><?= i::__("Categorias de inscrição") ?></label>
+            <!-- classe antiga class="subtitle" -->
+            <div class="category-header__subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></div>
+        </div>
+            
+
+    </template>
+    <template #default>
+
+        <div class="grid-12">
+            <!-- </header -->
+            <entity-field :entity="entity" prop="registrationCategTitle" classes="col-12"></entity-field>
+
+            <entity-field :entity="entity" prop="registrationCategDescription" classes="col-12"></entity-field>
+
+            <opportunity-form-builder-category-list class="col-12"></opportunity-form-builder-category-list>
+        </div>
+    </template>
+
+</mapas-card>
