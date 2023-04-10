4518fe69744dea2ac2de1235c69f84f07ba35b26
robedj <ed.wassouf@gmail.com>
2023-04-10 17:15:22 -0400

    Ajuste nas classes do template do componente opportunity-form-builder-category Ref.: #2557 2550

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
@@ -7,22 +7,17 @@ $this->import('opportunity-form-builder-category-list mapas-card')
 <!-- class="form-builder__bg-content form-builder__bg-content--spacing" poderia ser um mc-card -->
 <mapas-card>
     <template #title>
-        <div class="category-header">
-            <label class="category-header__title"><?= i::__("Categorias de inscrição") ?></label>
+        <div class="card-header">
+            <label class="card-header__title"><?= i::__("Categorias de inscrição") ?></label>
             <!-- classe antiga class="subtitle" -->
-            <div class="category-header__subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></div>
+            <div class="card-header__subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></div>
         </div>
-            
-
     </template>
     <template #default>
 
         <div class="grid-12">
-            <!-- </header -->
             <entity-field :entity="entity" prop="registrationCategTitle" classes="col-12"></entity-field>
-
             <entity-field :entity="entity" prop="registrationCategDescription" classes="col-12"></entity-field>
-
             <opportunity-form-builder-category-list class="col-12"></opportunity-form-builder-category-list>
         </div>
     </template>
