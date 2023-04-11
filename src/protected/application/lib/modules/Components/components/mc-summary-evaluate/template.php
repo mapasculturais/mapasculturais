cf5be3c2f60f39d39190e763dc4d3c88e3f7e706
erleibiazzio <erleibiazzio@gmail.com>
2023-04-11 17:14:48 -0300

    Cria Base do componente mc-summary-evaluate Ref.:#2563

---
+++ src/protected/application/lib/modules/Components/components/mc-summary-evaluate/template.php
@@ -0,0 +1,16 @@
+<div>
+    <div>
+        <h4><?= MapasCulturais\i::__('Avaliações pendente') ?></h4>
+        <span>{{summary.pending}} <?= MapasCulturais\i::__('avaliações disponíveis') ?></span>
+    </div>
+
+    <div>
+        <h4><?= MapasCulturais\i::__('Avaliações concluídas') ?></h4>
+        <span>{{summary.completed}} <?= MapasCulturais\i::__('avaliações') ?></span>
+    </div>
+
+    <div>
+        <h4><?= MapasCulturais\i::__('Avaliações enviadas') ?></h4>
+        <span>{{summary.send}} <?= MapasCulturais\i::__('avaliações') ?></span>
+    </div>
+</div>
