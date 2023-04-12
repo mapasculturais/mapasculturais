55a7d387e010cc32b8d1113e6a7c541d68a39f46
robedj <ed.wassouf@gmail.com>
2023-04-12 16:12:55 -0400

    Retirada do footer da pagina de evento quando no mapa. Ref.: #2579

--- src/protected/application/lib/modules/Search/components/search/script.js
+++ src/protected/application/lib/modules/Search/components/search/script.js
@@ -38,6 +38,7 @@ app.component('search', {
 
     methods: {
         changeTab(tab) {
+            console.log(tab.tab.slug);
             if (tab.tab.slug == 'map') {
                 this.globalState.hideFooter();
             } else {
