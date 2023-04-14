fadc6282225a37ba178fb95d9ad62b8b51c8ed27
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-04-14 16:43:07 -0300

    Adiciona o slot default (Ref.: #2584)

--- src/protected/application/lib/modules/Components/components/mc-alert/template.php
+++ src/protected/application/lib/modules/Components/components/mc-alert/template.php
@@ -18,7 +18,7 @@ $this->import('
         <mc-icon v-if="type === 'success'" name="circle-checked"></mc-icon>
         <mc-icon v-if="type === 'warning'" name="exclamation"></mc-icon>
         <p class="text">
-            <?= i::__('Você tem inscrições não finalizadas. Acesse a aba')?> <strong><?= i::__('Não Enviadas') ?></strong> <?= i::__('para visualizar.') ?>
+            <slot></slot>
         </p>
     </div>
     <button v-if="closeButton" class="alert__button" @click="close"><?= i::__('Fechar') ?></button>
