1fe744f151dae9a19112398365b981ff15366cec
erleibiazzio <erleibiazzio@gmail.com>
2023-04-14 16:24:34 -0300

    Ajusta verificação do usuário no resumo das avaliações na tela do Avaliador Ref.: #2563

--- src/protected/application/lib/modules/Components/components/mc-summary-evaluate/init.php
+++ src/protected/application/lib/modules/Components/components/mc-summary-evaluate/init.php
@@ -7,21 +7,23 @@ $data = [];
 if($entity instanceof MapasCulturais\Entities\EvaluationMethodConfiguration){
     $conn = $app->em->getConnection();
 
-    $complement = "";
-
-    if(!$app->user->is('admin')){
-        $agent = $app->user->profile;
-        $complement.= " AND valuer_agent_id = {$agent->id}";
-    }
-    
-    $buildQuery = function($colluns = "*", $params = "", $type = "fetchAll") use ($conn, $entity){
-        return $conn->$type("SELECT {$colluns} FROM evaluations e WHERE opportunity_id = {$entity->id} {$params}");
+    $complement = function($collun) use ($app){
+        $str = "";
+        if(!$app->user->is('admin')){
+            $user = $app->user;
+            $str.= " AND e.{$collun} = {$user->id}";
+        }
+        return $str;
     };
-    
-    $pending = $buildQuery("DISTINCT count(e.registration_id) as qtd", "AND e.evaluation_status IS NULL {$complement}", "fetchAssoc");
+
+    $buildQuery = function($colluns = "*", $params = "", $type = "fetchAll") use ($conn, $entity){
+        return $conn->$type("SELECT {$colluns} FROM evaluations e WHERE opportunity_id = {$entity->opportunity->id} {$params}");
+    };
+
+    $pending = $buildQuery("DISTINCT count(e.registration_id) as qtd", "AND e.evaluation_status IS NULL {$complement('valuer_user_id')}", "fetchAssoc");
     $data['pending'] = $pending['qtd'];
     
-    $completed = $buildQuery("DISTINCT count(e.registration_id) as qtd", "AND e.evaluation_status = 1 {$complement}", "fetchAssoc");
+    $completed = $buildQuery("DISTINCT count(e.registration_id) as qtd", "AND e.evaluation_status = 1 {$complement('valuer_user_id')}", "fetchAssoc");
     $data['completed'] = $completed['qtd'];
     
     $send = $buildQuery("DISTINCT count(e.registration_id) as qtd", "AND e.evaluation_status = 2 {$complement}", "fetchAssoc");
