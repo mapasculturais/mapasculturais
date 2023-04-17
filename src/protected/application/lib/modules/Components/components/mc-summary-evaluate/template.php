bfb638a0cfaa1f93e96b673b51cc90b311c896a0
robedj <ed.wassouf@gmail.com>
2023-04-17 10:49:57 -0400

    Criação do estilo para o componente mc-summary-evaluate Ref.: #2563

--- src/protected/application/lib/modules/Components/components/mc-summary-evaluate/template.php
+++ src/protected/application/lib/modules/Components/components/mc-summary-evaluate/template.php
@@ -1,20 +1,20 @@
-<div>
-    <div>
+<div class="mc-summary-evaluate">
+    <div class="mc-summary-evaluate__pending">
         <h4><?= MapasCulturais\i::__('Avaliações pendente') ?></h4>
         <span>{{summary.pending}} <?= MapasCulturais\i::__('avaliações disponíveis') ?></span>
     </div>
 
-    <div>
+    <div class="mc-summary-evaluate__started">
         <h4><?= MapasCulturais\i::__('Avaliações iniciadas') ?></h4>
         <span>{{summary.started}} <?= MapasCulturais\i::__('avaliações') ?></span>
     </div>
 
-    <div>
+    <div class="mc-summary-evaluate__completed">
         <h4><?= MapasCulturais\i::__('Avaliações concluídas') ?></h4>
         <span>{{summary.completed}} <?= MapasCulturais\i::__('avaliações') ?></span>
     </div>
 
-    <div>
+    <div class="mc-summary-evaluate__sent">
         <h4><?= MapasCulturais\i::__('Avaliações enviadas') ?></h4>
         <span>{{summary.send}} <?= MapasCulturais\i::__('avaliações') ?></span>
     </div>
