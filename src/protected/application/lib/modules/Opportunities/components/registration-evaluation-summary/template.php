4376225364f9df21fe88f5751f39281c7b415026
rohilario <rodrigohilariocomercial@gmail.com>
2023-04-19 10:27:16 -0300

    Ajustado template do componente <registration-evaluation-summary>

--- src/protected/application/lib/modules/Opportunities/components/registration-evaluation-summary/template.php
+++ src/protected/application/lib/modules/Opportunities/components/registration-evaluation-summary/template.php
@@ -14,7 +14,7 @@ $this->import('
 ');
 ?>
 <mc-summary-evaluate>
-    <mc-side-menu :is-open="open" @toggle="toggle" text-button="<?= i::__("Lista de avaliações") ?>"">
+    <mc-side-menu :is-open="open" @toggle="toggle" text-button="<?= i::__("Lista de avaliações") ?>">
         <v1-embed-tool></v1-embed-tool>
     </mc-side-menu>
 </mc-summary-evaluate>
