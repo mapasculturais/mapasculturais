f2771f005062442d09a4b67cc6656f39663df57a
robedj <ed.wassouf@gmail.com>
2023-04-20 15:51:57 -0400

    Ajustes no estilo da edição da oportunidade e readequação das classes Ref.: #2423

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
@@ -29,8 +29,10 @@ $this->import('
             <confirm-button message="Confirma a execução da ação?" @confirm="deletePhase($event, phase, index)">
                 <template #button="modal">
                     <a class="phase-delete__trash" @click="modal.open">
-                        <mc-icon name="trash"></mc-icon>
-                        <label class="phase-delete__label">{{ text('excluir_fase_coleta_dados') }}</label>
+                        <div class="icon">
+                            <mc-icon name="trash"></mc-icon>
+                        </div>
+                        <label class="label">{{ text('excluir_fase_coleta_dados') }}</label>
                     </a>
                 </template>
             </confirm-button>
