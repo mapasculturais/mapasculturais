fe1e58e3f8d9032ea14d520159c7aec9c59455ce
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-04-20 00:26:59 -0300

    automatização da importação das inscrições (refs #2612)

--- src/protected/application/lib/modules/Opportunities/Jobs/StartDataCollectionPhase.php
+++ src/protected/application/lib/modules/Opportunities/Jobs/StartDataCollectionPhase.php
@@ -2,6 +2,7 @@
 namespace Opportunities\Jobs;
 
 use MapasCulturais\App;
+use MapasCulturais\Entities\Opportunity;
 use MapasCulturais\Definitions\JobType;
 
 class StartDataCollectionPhase extends JobType
@@ -14,6 +15,21 @@ class StartDataCollectionPhase extends JobType
     }
 
     protected function _execute(\MapasCulturais\Entities\Job $job){
+        $app = App::i();
+        
+        /** @var Opportunity $opportunity */
+        $opportunity = $job->opportunity;
+        
+        /** @var \OpportunityPhases\Module $module */
+        $module = $app->modules['OpportunityPhases'];
+        
+        /**
+         * importa as inscrições da fase anterior 
+         */
+        if (!$opportunity->isFirstPhase && $opportunity->isDataCollection ) {
+            $module->importLastPhaseRegistrations($opportunity->previousPhase, $opportunity, true);
+        }
+        
         echo "> $job->opportunity " . __CLASS__ . "\n\n";
         return true;
     }
