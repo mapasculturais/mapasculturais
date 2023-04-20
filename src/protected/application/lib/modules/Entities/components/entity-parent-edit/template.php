4834cb259b9908ea57bd1474cdbe2a3c14a28581
robedj <ed.wassouf@gmail.com>
2023-04-20 14:35:15 -0400

    Ajustes no template do componente entity-parent-edit para exibir labels especificos para espaço e projeto Ref.:#2597

--- src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-parent-edit/template.php
@@ -8,6 +8,7 @@ select-entity
 ');
 ?>
 <div v-if="parent" :class="['entity-parents-edit' ,classes]">
+    
     <h4 class="entity-parentss-edit__title">{{entity.name}} {{title}}</h4>
     <a class="entity-parent-edit__parent" :href="parent.singleUrl" :title="parent.shortDescription">
         <div class="entity-parent-edit__parent--img">
@@ -25,7 +26,8 @@ select-entity
             <template #button="{ toggle }">
                 <a class="entity-parent-edit__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()">
                     <mc-icon name="exchange"></mc-icon>
-                    <h4><?php i::_e( "Trocar supra projeto") ?></h4>
+                    <h4 v-if="type == 'space'"><?php i::_e( "Trocar supra espaço") ?></h4>
+                    <h4 v-if="type == 'project'"><?php i::_e( "Trocar supra projeto") ?></h4>
                 </a>
             </template>
         </select-entity>
@@ -35,7 +37,8 @@ select-entity
     <select-entity :type="type" @select="changeParent($event)" :query="{id: `!EQ(${entity.id})`}" openside="right-down">
         <template #button="{ toggle }">
             <h4 class="title"><?php i::_e("{{label}}")?></h4>
-            <span class="text"><?php i::_e("Selecione um projeto para que o seu projeto atual seja vinculado como integrante") ?></span>
+            <span v-if="type == 'space'" class="text"><?php i::_e("Selecione um espaço para que o seu espaço atual seja vinculado como integrante") ?></span>
+            <span v-if="type == 'project'" class="text"><?php i::_e("Selecione um projeto para que o seu projeto atual seja vinculado como integrante") ?></span>
             <a class="entity-parent-edit__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()">
                 <button class="add-button button button--primary-outline  button--icon "><mc-icon class="teste" name="add"></mc-icon><?php i::_e("Adicionar")?></button>
             </a>
