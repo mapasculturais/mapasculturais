df86bac77c5b37e2322706cb4f9c86fd3838b3de
erleibiazzio <erleibiazzio@gmail.com>
2023-04-21 15:50:29 -0300

    Seta o formulario de avaliação fora dos aricles das inscrições para que nao exista conflitos entre o angular das avaliações e opportunity Ref.: #2623

---
+++ src/protected/application/lib/modules/BaseV1EmbedTools/layouts/parts/evaluation--form.php
@@ -0,0 +1,48 @@
+<?php
+use MapasCulturais\i;
+
+$opportunity = $entity->opportunity;
+$configuration = $opportunity->evaluationMethodConfiguration;
+$definition = $configuration->definition;
+$evaluationMethod = $definition->evaluationMethod;
+$evaluation_form_part_name = $evaluationMethod->getEvaluationFormPartName();
+$evaluation_view_part_name = $evaluationMethod->getEvaluationViewPartName();
+
+$params = ['opportunity' => $opportunity, 'entity' => $entity, 'evaluationMethod' => $evaluationMethod];
+
+$evaluation = $this->getCurrentRegistrationEvaluation($entity);
+
+$infos = (array) $configuration->infos;
+
+$evaluationAgent = false;
+
+foreach ($opportunity->getEvaluationCommittee() as $evaluation_user) {
+    if ($evaluation_user->agent === \MapasCulturais\App::i()->user->profile) {
+        $evaluationAgent = true;
+    }
+}
+$action = 'single';
+
+
+?>
+<?php if ($action === 'single' && $entity->canUser('viewUserEvaluation')) : ?>
+    <div id="registration-evaluation-form" >
+        <?php if ($evaluationAgent && $entity->canUser('evaluate')) : ?>
+            <form>
+                <?php if ($opportunity->canUser('@control') && $evaluation) : ?>
+                    <div>
+                        <strong><?php i::_e('Avaliador') ?>:</strong> <?php echo $evaluation->user->profile->name ?>
+                        <input type="hidden" name="uid" value="<?php echo $evaluation->user->id; ?>" />
+                    </div>
+                <?php endif; ?>
+                <?php $this->part($evaluation_form_part_name, $params); ?>
+                <hr>
+                <div style="text-align: right;">
+                    <button class="btn btn-primary js-evaluation-submit js-next"><?php i::_e('Finalizar Avaliação e Avançar'); ?> &gt;&gt;</button>
+                </div>
+            </form>
+        <?php elseif ($entity->canUser('viewUserEvaluation')) : ?>
+        <?php endif; ?>
+        <?php $this->part($evaluation_view_part_name, $params); ?>
+    </div>
+<?php endif; ?>
