0fd9d716a5e99f87fc6e9f455c86ac15184a0050
erleibiazzio <erleibiazzio@gmail.com>
2023-04-25 17:50:13 -0300

    Cria endpoint que cria ifreme para carregar o formulario de avaliação dentro da tela de avaliação Ref.: #2626

--- src/protected/application/lib/modules/BaseV1EmbedTools/views/embedtools/registration-view.php
+++ src/protected/application/lib/modules/BaseV1EmbedTools/views/embedtools/registration-view.php
@@ -1,5 +1,11 @@
 <?php
 $action = preg_replace("#^(\w+/)#", "", $this->template);
+
+
+$avaliable_evaluationFields = $entity->opportunity->avaliableEvaluationFields ?? [];
+$app->view->jsObject['avaliableEvaluationFields'] = $avaliable_evaluationFields;
+
+
 $_params = [
     'entity' => $entity,
     'action' => $action,
