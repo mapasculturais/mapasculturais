a60db0d14a355d73e23d36529b9771172a5c5a6c
robedj <ed.wassouf@gmail.com>
2023-04-26 17:04:21 -0400

    opportunity-form-import Ref.: #2627

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-import/script.js
@@ -0,0 +1,77 @@
+app.component('opportunity-form-import', {
+    template: $TEMPLATES['opportunity-form-import'],
+
+
+    emits: [],
+
+    setup() {
+        // os textos estão localizados no arquivo texts.php deste componente 
+        const text = Utils.getTexts('entity-files-list')
+        return { text }
+    },
+
+    created() {
+
+    },
+
+    computed: {
+        files() {
+            return this.entity.files?.[this.group] || []
+        }
+    },
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+        group: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        editable: {
+            type: Boolean,
+            default: false
+        },
+        classes: {
+            type: [String, Array, Object],
+            required: false
+        },
+
+    },
+
+    data() {
+        return {
+            newFile: {}
+        }
+    },
+
+    methods: {
+        setFile() {
+            this.newFile = this.$refs.file.files[0];
+        },
+
+        upload(popover) {
+            let data = {
+                group: this.group,
+                description: this.newFile.description
+            };
+
+            this.entity.upload(this.newFile, data).then((response) => {
+                this.$emit('uploaded', this);
+                popover.close()
+            });
+
+            return true;
+        },
+
+        rename(file, popopver) {
+            file.description = file.newDescription;
+            file.save().then(() => popopver.close());
+        }
+    },
+});
