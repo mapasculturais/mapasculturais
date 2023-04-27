eb5d5dab2cfcaa19c5244eef335c231430512a4c
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-04-27 18:25:25 -0300

    Adiciona campo para ordenação dos resultados e mostra quantos resultados foram encontrados nas páginas de busca (Ref.: #2607)

--- src/protected/application/lib/modules/Search/components/search-list/template.php
+++ src/protected/application/lib/modules/Search/components/search-list/template.php
@@ -2,11 +2,16 @@
 
 use MapasCulturais\i;
 
-$this->import('entities mapas-card entity-card');
+$this->import('
+    entities
+    entity-card
+    mapas-card
+    mc-icon
+');
 ?>
 
 <div class="grid-12 search-list">
-    <entities :type="type" :select="select" :query="query" :limit="limit" watch-query>
+    <entities :type="type" :select="select" :query="query" order="createTimestamp DESC" :limit="limit" watch-query>
         <template #header="{entities}">
             <div class="col-3 search-list__filter">
                 <div class="search-list__filter--filter">
@@ -16,12 +21,30 @@ $this->import('entities mapas-card entity-card');
         </template>
 
         <template #default="{entities}">
-            <div class="col-9 ">
-                <div  class="grid-12">
+            <div class="col-9">
+                <div class="grid-12">
+                    
+                    <div class="col-12 search-list__order">
+                        <div class="field">
+                            <label> <? i::_e('Ordenar por')?>  </label>
+                            <select v-model="entities.query['@order']">
+                                <option value="name ASC"> <?php i::_e('Ordem alfabetica crescente') ?> </option>
+                                <option value="name DESC"> <?php i::_e('Ordem alfabetica decrescente') ?> </option>
+                                <option value="createTimestamp ASC"> <?php i::_e('Data de criação crescente') ?> </option>
+                                <option value="createTimestamp DESC"> <?php i::_e('Data de criação decrescente') ?> </option>
+                                <option v-if="type == 'opportunity'" value="registrationFrom ASC"> <?php i::_e('Início das inscrições crescente') ?> </option>
+                                <option v-if="type == 'opportunity'" value="registrationFrom DESC"> <?php i::_e('Início das inscrições decrescente') ?> </option>
+                            </select>
+                        </div>
+                        <div class="foundResults">
+                            {{entities.metadata.count}} {{entityType}} <?= i::__('encontrados') ?> 
+                        </div>
+                    </div>
+
                     <entity-card :entity="entity" v-for="entity in entities" :key="entity.__objectId" class="col-12">
                         <template #type> <span>{{typeText}} <span :class="['upper', entity.__objectType+'__color']">{{entity.type.name}}</span></span></template>
                     </entity-card>
-                </div  >
+                </div>
             </div>
         </template>
     </entities>
