6e9d2b79137c8256257a279db37be43b0a0d2740
erleibiazzio <erleibiazzio@gmail.com>
2023-04-27 11:24:18 -0300

    Remove botão de importar do componente opportunitty-form-export Ref.: #2627

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-export/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-export/template.php
@@ -11,10 +11,7 @@ modal
 
 <div class="opportunity-form-export">
   <div class="opportunity-form-export__button">
-
-    <button type="button" class="view  button">
-      <?php i::_e("Importar") ?>
-    </button><button type="button" class="export  button">
+    <button type="button" class="export  button">
       <?php i::_e("Exportar") ?>
     </button>
 
