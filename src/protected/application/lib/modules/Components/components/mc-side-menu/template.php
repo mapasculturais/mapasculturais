d1e5dd37cf9e63338ffa5fb28035dbed489a3d7a
erleibiazzio <erleibiazzio@gmail.com>
2023-04-28 17:20:32 -0300

    Ajusta template do mc-side-menu para atender a estrutura do figma

--- src/protected/application/lib/modules/Components/components/mc-side-menu/template.php
+++ src/protected/application/lib/modules/Components/components/mc-side-menu/template.php
@@ -7,26 +7,65 @@
  */
 
 use MapasCulturais\i;
+
+$this->import('mc-icon');
 ?>
 
 
-<div :class="['mc-side-menu', {'isOpen': isOpen}]">
-    <button class="mc-side-menu__button" @click="toggleMenu()">
-        <label class="label">{{textButton }}</label>
-        <div class="icon">
-            <mc-icon v-if="!isOpen" name="arrow-right-ios"></mc-icon>
-            <mc-icon v-if="isOpen" name="arrow-left-ios"></mc-icon>
-        </div>
-    </button>
-
-
-    <template v-if="isOpen">
-        <div class="mc-side-menu__container" @click="emitToggle">
-            <div class="mc-side-menu__container--content" @click="e => stopPropagation(e)">
-                <slot>
-                    {{ content }}
-                </slot>
+<div v-if="viewLoad == 'list'">
+    <div :class="['mc-side-menu', {'isOpen': isOpen}]">
+        <button class="mc-side-menu__button" @click="toggleMenu()">
+            <label class="label">{{textButton }}</label>
+            <div class="icon">
+                <mc-icon v-if="!isOpen" name="arrow-right-ios"></mc-icon>
+                <mc-icon v-if="isOpen" name="arrow-left-ios"></mc-icon>
             </div>
-        </div>
-    </template>
+        </button>
+
+
+        <template v-if="isOpen">
+            <div>
+                <div>
+                    <input type="text" v-model="keywords">
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" v-model="pending">
+                        <?= i::__('Mostrar somente pendentes') ?>
+                    </label>
+                </div>
+            </div>
+            <ul>
+                <li v-for="evaluation in evaluations">
+
+                    <div>
+                        <mc-icon name='agent-1'></mc-icon>
+                        <span>{{evaluation.agentname}}</span>
+                    </div>
+                    <div>
+                        <div>
+                            <label><?= i::__('Inscrição') ?></label>
+                            <span>
+                                <strong>{{evaluation.registrationid}}</strong>
+                            </span>
+                        </div>
+                        <div>
+                            <label><?= i::__('Data da inscrição') ?></label>
+                            <span>
+                                <strong>{{dateFormat(entity.createTimestamp)}}</strong>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <label><?= i::__('Status da avaliaçõe') ?></label>
+                        <span v-if="evaluation.resultString">{{evaluation.resultString}}</span>
+                        <span v-if="!evaluation.resultString"> <?= i::__('Pendente') ?></span>
+                    </div>
+                    <div>
+                        <a :href="evaluation.url" type="button"><?= i::__('Acessar') ?></a>
+                    </div>
+                </li>
+            </ul>
+        </template>
+    </div>
 </div>
