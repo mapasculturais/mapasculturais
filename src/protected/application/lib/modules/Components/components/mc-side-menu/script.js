8fa2d0be7de86e9afdcb2a44d72d8b90af1f79a3
erleibiazzio <erleibiazzio@gmail.com>
2023-04-28 17:19:20 -0300

    Implementa a busca das availiações na API e os metodos de navegação entre leas

--- src/protected/application/lib/modules/Components/components/mc-side-menu/script.js
+++ src/protected/application/lib/modules/Components/components/mc-side-menu/script.js
@@ -28,19 +28,86 @@ app.component('mc-side-menu', {
     },
     data() {
         return {
+            evaluations: [],
             isOpen: false,
+            pending: false,
+            keywords: "",
+            timeOut: null,
         }
     },
-    
+    watch: {
+        'keywords'(_new, _old) {
+            this.timeOutFind(_new, _old);
+        },
+        'pending'(_new, _old) {
+            this.timeOutFind(_new, _old);
+        }
+    },
     methods: {
-        emitToggle () {
+        timeOutFind(_new, _old) {
+            if (_new != _old) {
+                clearTimeout(this.timeOut);
+                this.timeOut = setTimeout(() => {
+                    this.getEvaluations();
+                }, 1500);
+            }
+        },
+        async getEvaluations() {
+            let args = {};
+            args['@select'] = "id,singleUrl,category,owner.{id,name,singleUrl},consolidatedResult,evaluationResultString,status";
+            args['@opportunity'] = this.entity.opportunity.id;
+            args['@keyword'] = 'like(' + this.keywords + ')';
+
+            if (this.pending) {
+                args['@pending'] = true;
+            }
+
+            api = new API('opportunity');
+            let url = api.createApiUrl('findRegistrationsAndEvaluations', args);
+
+            await api.GET(url).then(response => response.json().then(objs => {
+                this.evaluations = objs.map(function (item) {
+                    item.url = Utils.createUrl('registration', 'evaluation', [item.registrationid]);;
+                    return item;
+                });
+                this.evaluations.sort((a, b) => (a.registrationid - b.registrationid));
+            }));
+        },
+        previousEvaluation(data) {
+            this.goTo(data)
+        },
+        nextEvaluation(data) {
+            this.goTo(data)
+        },
+        goTo(data) {
+
+            var index = null;
+            this.evaluations.forEach((obj, i) => {
+                if (obj.registrationid === data.detail.registrationId) {
+                    index = data.type === "nextEvaluation" ? i + 1 : i - 1;
+                }
+            });
+
+            if (index >= 0 && index < this.evaluations.length) {
+                var url = this.evaluations[index].url.href;
+                window.location.href = url;
+            }
+
+        },
+        dateFormat(value) {
+            const dateObj = new Date(value._date);
+            return dateObj.toLocaleDateString("pt-BR");
+        },
+        emitToggle() {
             this.$emit('toggle');
         },
-        stopPropagation (event) {
-            event.stopPropagation();
+        stopPropagation(event) {
+            if (this.viewLoad == "list") {
+                event.stopPropagation();
+            }
         },
-        toggleMenu () {
-            this.isOpen= this.isOpen ? false : true; 
+        toggleMenu() {
+            this.isOpen = this.isOpen ? false : true;
         },
     },
 });
