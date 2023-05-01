6efa4082854dbd715037702e83e14b46eb0994bf
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-01 11:57:26 -0300

    os arquivos de layout, passa as informações da variavel entity para o addRequestedEntityToJs

--- src/protected/application/lib/modules/Opportunities/layouts/registrations.php
+++ src/protected/application/lib/modules/Opportunities/layouts/registrations.php
@@ -3,7 +3,7 @@
 use MapasCulturais\ApiQuery;
 
 $this->import('entity');
-$this->addRequestedEntityToJs();
+$this->addRequestedEntityToJs($entity ? $entity->className : null, $entity ? $entity->id : null);
 $query = new ApiQuery(\MapasCulturais\Entities\Opportunity::class, [
     'id' => "EQ({$entity->opportunity->id})",
     '@select' => "*"
