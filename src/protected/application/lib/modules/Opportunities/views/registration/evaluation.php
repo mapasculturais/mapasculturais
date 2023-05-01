78284862c68cfe98ebe7b6d2498d3c06424012a9
erleibiazzio <erleibiazzio@gmail.com>
2023-05-01 14:15:43 -0300

    Carrega comonente responsável pelo carregamento dos agentes relacionados a inscrição Ref.: #2640

--- src/protected/application/lib/modules/Opportunities/views/registration/evaluation.php
+++ src/protected/application/lib/modules/Opportunities/views/registration/evaluation.php
@@ -11,6 +11,7 @@ $this->import('
     mc-icon
     mc-side-menu
     opportunity-header
+    mc-summary-agents
     mc-summary-evaluate
     v1-embed-tool 
     registration-evaluation-actions
@@ -31,6 +32,8 @@ $breadcrumb = [
 ];
 
 $breadcrumb[] = ['label' => i::__('Formulário de avaliação')];
+
+$app->view->jsObject['avaliableEvaluationFields'] = $entity->opportunity->avaliableEvaluationFields;
 
 $this->breadcrumb = $breadcrumb;
 ?>
@@ -69,24 +72,8 @@ $this->breadcrumb = $breadcrumb;
                         </div>
                     </div>
                 </div>
-
-                <section class="section">
-                    <div class="section__content">
-                        <div class="card owner">
-                            <div class="card__title">
-                                <?= i::__('Dados do proponente') ?>
-                            </div>
-                            <p><strong><?= i::__("Nome:") ?></strong> <?= i::__("Lorem ipsum ipsum") ?></p>
-                            <p><strong><?= i::__("Descrição curta:") ?></strong> <?= i::__("Est nesciunt excepturi et laborum exercitationem aut dolor veritatis et omnis velit.") ?></p>
-                            <p><strong><?= i::__("CPF ou CNPJ::") ?></strong> <?= i::__("642.653.000-06") ?></p>
-                            <p><strong><?= i::__("Email:") ?></strong> <?= i::__("emaildecontato@email.com.br") ?></p>
-                            <p><strong><?= i::__("Raça:") ?></strong> <?= i::__("Informação oculta") ?></p>
-                            <p><strong><?= i::__("Gênero:") ?></strong> <?= i::__("Informação oculta") ?></p>
-                            <p><strong><?= i::__("Endereço:") ?></strong> <?= i::__("Rua dos Protótipos, 85, Cidade Fícticia, Ceará, Brasil") ?></p>
-                            <p><strong><?= i::__("CEP:") ?></strong> <?= i::__("18619-408") ?></p>
-                        </div>
-                    </div>
-                </section>
+                
+                <mc-summary-agents></mc-summary-agents>
 
                 <div class="col-12 registration-info">
                     <p class="registration-info__title"><?= i::__('Dados informados no formulário') ?></p>
