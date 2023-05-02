0a7f0beda12021a6abedfd09002968e94d4153cc
erleibiazzio <erleibiazzio@gmail.com>
2023-05-01 22:03:36 -0300

    Ajusta exibição de espaço vinculado Ref.: #2640

--- src/protected/application/lib/modules/Components/components/mc-summary-sapaces/template.php
+++ src/protected/application/lib/modules/Components/components/mc-summary-sapaces/template.php
@@ -10,14 +10,15 @@ $this->import('
 ?>
 <mapas-card v-if="canSee('spaceSummary')">
     <template v-slot:title>
-        <div>
+        <div v-if="opportunity.useSpaceRelationIntituicao && opportunity.useSpaceRelationIntituicao !== 'dontUse'">
             <div>
                 <h4><strong><?= i::__("Espaço Vinculado") ?> </strong></h4>
             </div>
             <div>
-                <img v-if="space.files?.avatar" :src="space.files?.avatar?.transformations?.avatarMedium?.url" />
-                <mc-icon v-if="!space.files?.avatar" name="space"></mc-icon>
-                <span>{{space.name}}</span>
+                <img v-if="space?.files?.avatar" :src="space?.files?.avatar?.transformations?.avatarMedium?.url" />
+                <mc-icon v-if="!space?.files?.avatar" name="space"></mc-icon>
+                <span>{{space?.name}}</span>
+                <span v-if="!space"><?= i::__("Espaço não informado") ?></span>
             </div>
             <div v-if="space">
                 <div><small><strong><?= i::__("Nome:") ?></strong> {{space?.name}}</small></div>
