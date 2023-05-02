659cc9a0caceeb2abd805280cf21972b7609d0f1
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-01 23:24:03 -0300

    corrige abertura dos mc-selects

--- src/protected/application/lib/modules/RegistrationFieldTypes/assets/js/rfc/datepicker.js
+++ src/protected/application/lib/modules/RegistrationFieldTypes/assets/js/rfc/datepicker.js
@@ -1,31 +1,33 @@
 $(function () {
-    let flatpack = MapasCulturais.flatpickr;
-    flatpack.locale = MapasCulturais.lcode.substr(0,2);
-    flatpack.dateFormat = "Y-m-d";
-    flatpack.altInput= true;
-
-    $('input[type="date"]').each(function() {
-        let $this = $(this);
-        if (!$this.data('flatpickr')) {
-            $this.attr('type', 'text');
-            $this.flatpickr({
-                allowInput: true,
-                locale: MapasCulturais.lcode.substr(0,2),
-                dateFormat: $this.data('enableTime') ? "Y-m-d H:i" : "Y-m-d",
-                enableTime: $this.data('enableTime'),
-                altInput: true,
-                altFormat: $this.data('enableTime') ? MapasCulturais.flatpickr.altFormat+ ' H:i' : MapasCulturais.flatpickr.altFormat,
-                onChange: function(selectedDates, dateStr, instance) {
-                    setTimeout(() => {
-                        $this.trigger('blur')
-                    }, 10);
-                },
-                disableMobile: "true",
-            });
-
-            setTimeout(function () { 
-                $this.next().mask('99/99/9999');
-            });
-        }
-    });
+    setTimeout(function() {
+        let flatpack = MapasCulturais.flatpickr;
+        flatpack.locale = MapasCulturais.lcode.substr(0,2);
+        flatpack.dateFormat = "Y-m-d";
+        flatpack.altInput= true;
+    
+        $('input[type="date"]').each(function() {
+            let $this = $(this);
+            if (!$this.data('flatpickr')) {
+                $this.attr('type', 'text');
+                $this.flatpickr({
+                    allowInput: true,
+                    locale: MapasCulturais.lcode.substr(0,2),
+                    dateFormat: $this.data('enableTime') ? "Y-m-d H:i" : "Y-m-d",
+                    enableTime: $this.data('enableTime'),
+                    altInput: true,
+                    altFormat: $this.data('enableTime') ? MapasCulturais.flatpickr.altFormat+ ' H:i' : MapasCulturais.flatpickr.altFormat,
+                    onChange: function(selectedDates, dateStr, instance) {
+                        setTimeout(() => {
+                            $this.trigger('blur')
+                        }, 10);
+                    },
+                    disableMobile: "true",
+                });
+    
+                setTimeout(function () { 
+                    $this.next().mask('99/99/9999');
+                });
+            }
+        });
+    },20);
 });
