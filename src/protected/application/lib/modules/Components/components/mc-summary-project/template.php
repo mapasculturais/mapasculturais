68204ca4797fb29c6ba14e959f06f4723de65534
erleibiazzio <erleibiazzio@gmail.com>
2023-05-02 12:59:16 -0300

    Faz correções nas condicionais que fazem exibir os componentes de resumo Ref.: #2640

--- src/protected/application/lib/modules/Components/components/mc-summary-project/template.php
+++ src/protected/application/lib/modules/Components/components/mc-summary-project/template.php
@@ -8,16 +8,16 @@ $this->import('
     
 ');
 ?>
-<mapas-card v-if="canSee('projectName')">
+<mapas-card v-if="projectName && canSee('projectName')">
     <template #:title>
         <div>
             <div>
                 <h4><strong><?= i::__("Nome do projeto") ?> </strong></h4>
             </div>
-            <div>
+            <div v-if="projectName !== 0">
                 <mc-icon name="project"></mc-icon>
                 <span v-if="projectName">{{projectName}}</span>
-                <span v-if="!projectName && opportunity.useAgentRelationInstituicao"><?= i::__("Nomedo projeto não informado") ?></span>
+                <span v-if="!projectName"><?= i::__("Nome do projeto não informado") ?></span>
             </div>
         </div>
     </template>
