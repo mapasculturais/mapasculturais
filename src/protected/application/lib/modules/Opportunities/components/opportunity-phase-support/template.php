a595e83f177cfe8ea5f174825ba282a1ac3d00a3
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-05-04 16:16:21 -0300

    Criação do componente <opportunity-phase-support> (Ref.: #2658)

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-support/template.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ * 
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    mc-stepper-vertical
+');
+?>
+
+<mc-stepper-vertical :items="phases" allow-multiple>
+    <template #header-title="{index, item}">
+        <div class="stepper-header__content">
+            <div class="info">
+                <h2 v-if="index" class="info__title">{{item.name}}</h2>
+                <h2 v-if="!index" class="info__title"><?= i::__('Período de inscrição') ?></h2>
+                <div v-if="item.type && item.type != ''" class="info__type">
+                    <span class="title"> <?= i::__('Tipo') ?>: </span>
+                    <span class="type"><?= i::__('Coleta de dados') ?></span>
+                </div>
+            </div>
+        </div>
+    </template>
+    <template #header-actions="{index, item}">
+        <mc-link route="support/list" :params="[item.id]" class="button button--primary button-support"> <?= i::__('Realizar suporte') ?> </mc-link>
+    </template>
+</mc-stepper-vertical>
