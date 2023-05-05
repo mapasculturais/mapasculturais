44e6690b2ef31e61585c72046e9b848e8e341147
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-05 17:37:21 -0300

    corrige e renomeia labels da ordenação das buscas

--- src/protected/application/lib/modules/Search/components/search-list/template.php
+++ src/protected/application/lib/modules/Search/components/search-list/template.php
@@ -11,11 +11,32 @@ $this->import('
 ?>
 
 <div class="grid-12 search-list">
-    <entities :type="type" :select="select" :query="query" order="createTimestamp DESC" :limit="limit" watch-query>
+    <entities :type="type" :select="select" :query="query" :order="order" :limit="limit" watch-query>
         <template #header="{entities}">
             <div class="col-3 search-list__filter">
                 <div class="search-list__filter--filter">
                     <slot name="filter"></slot>
+                </div>
+            </div>
+            <div v-if="entities.loading" class="col-9">
+                <div class="grid-12">
+                    <div class="col-12 search-list__order">
+                        <div class="field">
+                            <select v-model="order">
+                                <option value="name ASC"> <?php i::_e('Ordem alfabética (A - Z)') ?> </option>
+                                <option value="name DESC"> <?php i::_e('Ordem alfabética (Z - A)') ?> </option>
+                                <option value="createTimestamp ASC"> <?php i::_e('Mais antigas primeiro') ?> </option>
+                                <option value="createTimestamp DESC"> <?php i::_e('Mais recentes primeiro') ?> </option>
+                                <option value="updateTimestamp DESC"> <?php i::_e('Modificadas recentemente') ?> </option>
+                                <option value="updateTimestamp ASC"> <?php i::_e('Modificadas há mais tempo') ?> </option>
+                                <option v-if="type == 'opportunity'" value="registrationFrom ASC"> <?php i::_e('Início das inscrições (recentes-antigas)') ?> </option>
+                                <option v-if="type == 'opportunity'" value="registrationFrom DESC"> <?php i::_e('Início das inscrições (antigas-recentes)') ?> </option>
+                            </select>
+                        </div>
+                        <div class="foundResults">
+                            {{entities.metadata.count}} {{entityType}} <?= i::__('encontrados') ?> 
+                        </div>
+                    </div>
                 </div>
             </div>
         </template>
@@ -23,17 +44,15 @@ $this->import('
         <template #default="{entities}">
             <div class="col-9">
                 <div class="grid-12">
-                    
                     <div class="col-12 search-list__order">
                         <div class="field">
-                            <label> <? i::_e('Ordenar por')?>  </label>
-                            <select v-model="entities.query['@order']">
-                                <option value="name ASC"> <?php i::_e('Ordenar por nome (A - Z)') ?> </option>
-                                <option value="name DESC"> <?php i::_e('Ordenar por nome (Z - A)') ?> </option>
-                                <option value="createTimestamp ASC"> <?php i::_e('Tempo de criação (recentes-antigas)') ?> </option>
-                                <option value="createTimestamp DESC"> <?php i::_e('Tempo de criação (antigas-recentes)') ?> </option>
-                                <option value="updateTimestamp ASC"> <?php i::_e('Ordenar por última edição (recentes-antigas)') ?> </option>
-                                <option value="updateTimestamp DESC"> <?php i::_e('Ordenar por última edição (antigas-recentes)') ?> </option>
+                            <select v-model="order">
+                                <option value="name ASC"> <?php i::_e('Ordem alfabética (A - Z)') ?> </option>
+                                <option value="name DESC"> <?php i::_e('Ordem alfabética (Z - A)') ?> </option>
+                                <option value="createTimestamp ASC"> <?php i::_e('Mais antigas primeiro') ?> </option>
+                                <option value="createTimestamp DESC"> <?php i::_e('Mais recentes primeiro') ?> </option>
+                                <option value="updateTimestamp DESC"> <?php i::_e('Modificadas recentemente') ?> </option>
+                                <option value="updateTimestamp ASC"> <?php i::_e('Modificadas há mais tempo') ?> </option>
                                 <option v-if="type == 'opportunity'" value="registrationFrom ASC"> <?php i::_e('Início das inscrições (recentes-antigas)') ?> </option>
                                 <option v-if="type == 'opportunity'" value="registrationFrom DESC"> <?php i::_e('Início das inscrições (antigas-recentes)') ?> </option>
                             </select>
