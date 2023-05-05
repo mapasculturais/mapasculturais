2bc45da6c1bab2c3e169962e0b8236b9e91de6ef
erleibiazzio <erleibiazzio@gmail.com>
2023-05-04 22:09:36 -0300

    Cria textos de internacionalização Ref.: #2684

--- src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/text.php
+++ src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/text.php
@@ -5,4 +5,6 @@ return [
     'Instituição responsável' => i::__('Instituição responsável'),
     'Agente coletivo' => i::__('Agente coletivo'),
     'Campo não identificado' => i::__('Campo não identificado'),
+    'Avaliação reaberta' => i::__('Avaliação reaberta'),
+    'Avaliação enviada' => i::__('Avaliação enviada'),
 ];
