29c28b50c4fed0e82d7e4e663f66f7b4528637f5
robedj <ed.wassouf@gmail.com>
2023-05-09 16:41:23 -0400

    Ajusta o menu sidebar para exibir a opção meu perfil e sair Ref.:#2677

--- src/protected/application/lib/modules/Panel/components/panel--nav/template.php
+++ src/protected/application/lib/modules/Panel/components/panel--nav/template.php
@@ -9,11 +9,10 @@ use MapasCulturais\i;
 
 $this->import('mc-link');
 ?>
-<nav v-if="viewport=='desktop'" class="panel-nav" :entity="entity" :class="classes">
+<nav v-if="viewport=='desktop'" class="panel-nav" :class="classes">
     <slot name='begin'></slot>
     <div class="panel-nav__left">
         <template v-for="group in leftGroups" :key="group.id">
-
             <h3 v-if="group.label">{{group.label}}</h3>
             <ul v-if="group.items.length > 0">
                 <li v-for="item in group.items" :key="`${group.id}:${item.route}`">
@@ -21,11 +20,17 @@ $this->import('mc-link');
                 </li>
             </ul>
         </template>
+        <template v-if="sidebar">
+            <div class="panel-nav__line"></div>
+            <div class="panel-nav__right">
+                <li><mc-link :entity='entity' icon><?= i::__('Meu Perfil') ?></mc-link></li>
+                <li><mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link></li>
+            </div>
+        </template>
     </div>
-    <div class="vertical__line"></div>
+    <div v-if="!sidebar" class="vertical__line"></div>
     <div v-if="!sidebar" class="panel-nav__right">
         <template v-for="group in rightGroups" :key="group.id">
-
             <h3 v-if="group.label">{{group.label}}</h3>
             <ul v-if="group.items.length > 0">
                 <li v-for="item in group.items" :key="`${group.id}:${item.route}`">
@@ -39,12 +44,10 @@ $this->import('mc-link');
             <li><mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link></li>
         </div>
     </div>
-
 </nav>
 
 <nav v-if="viewport=='mobile'" class="panel-nav" :entity="entity" :class="classes">
     <template v-for="group in groupsColumn" :key="group.id">
-
         <h3 v-if="group.label">{{group.label}}</h3>
         <ul v-if="group.items.length > 0">
             <li v-for="item in group.items" :key="`${group.id}:${item.route}`">
@@ -56,5 +59,4 @@ $this->import('mc-link');
         <li><mc-link :entity='entity' icon><?= i::__('Meu Perfil') ?></mc-link></li>
         <li><mc-link route='auth/logout' icon="logout"><?= i::__('Sair') ?></mc-link></li>
     </div>
-
 </nav>
