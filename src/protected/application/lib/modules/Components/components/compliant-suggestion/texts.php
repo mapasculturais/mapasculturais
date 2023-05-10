497ddf180a8f46afa59ae63f400b19feffa72d2e
robedj <ed.wassouf@gmail.com>
2023-05-10 08:37:05 -0400

    Renomeia componente compliant-suggestion para complaint-suggestion e altera chamada do componente Ref.: #2644

--- src/protected/application/lib/modules/Components/components/compliant-suggestion/texts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use MapasCulturais\i;
-
-return [
-    'Recaptcha inválida' => i::__('Recaptcha inválida'),
-    'Todos os campos são obrigatorio' => i::__('Todos os campos são obrigatorio'),
-    'Dados enviados com suscesso' => i::__('Dados enviados com suscesso'),
-];
