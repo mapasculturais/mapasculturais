74148296e47f9acdedd3c5eaed302c5cfc0945b4
robedj <ed.wassouf@gmail.com>
2023-05-10 08:37:05 -0400

    Renomeia componente compliant-suggestion para complaint-suggestion e altera chamada do componente Ref.: #2644

--- src/protected/application/lib/modules/Components/components/compliant-suggestion/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = [
-    'recaptcha' => [
-        'sitekey' =>  $app->_config['app.recaptcha.key'],
-    ]
-];
-$this->jsObject['compliantSuggestionConfig'] = $config;
-$this->jsObject['notification_type'] = $app->getRegisteredMetadata('MapasCulturais\Entities\Notification');
