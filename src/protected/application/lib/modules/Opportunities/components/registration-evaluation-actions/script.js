d735ff53185901104afd3078e265ee5ee4c077d5
erleibiazzio <erleibiazzio@gmail.com>
2023-05-10 17:07:40 -0300

    Barra a exibição dos botões de ação das avaliações caso nao tenha a pemissão currentUserPermissions.evaluate Ref.: #2684

--- src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/script.js
+++ src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/script.js
@@ -70,6 +70,11 @@ app.component('registration-evaluation-actions', {
             return this.text('Campo não identificado');
         },
         showActions(registration, action){
+
+            if(!registration.currentUserPermissions.evaluate){
+                return false;
+            }
+
             var result = false;
             this.evaluationRegistrationList.forEach(function(item){
                 if(item.registrationid == registration.id){
