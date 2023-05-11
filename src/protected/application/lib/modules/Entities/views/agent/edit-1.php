170e7d016fe797820f1673ab3348bc27cce93656
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-05-11 11:40:03 -0300

    Adiciona propriedade 'classes' no componente <entity-links> (Ref.: no issue)

--- src/protected/application/lib/modules/Entities/views/agent/edit-1.php
+++ src/protected/application/lib/modules/Entities/views/agent/edit-1.php
@@ -115,9 +115,7 @@ $this->breadcrumb = [
                     <div class="grid-12">
                         <entity-field :entity="entity" classes="col-12" prop="longDescription"></entity-field>
                         <entity-files-list :entity="entity" classes="col-12" group="downloads" title="<?php i::_e('Adicionar arquivos para download'); ?>" editable></entity-files-list>
-                        <div class="col-12">
-                            <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>                    
-                        </div>
+                        <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>   
                         <entity-gallery-video :entity="entity" classes="col-12" title="<?php i::_e('Adicionar vídeos') ?>" editable></entity-gallery-video>                    
                         <entity-gallery :entity="entity" classes="col-12" title="<?php i::_e('Adicionar fotos na galeria') ?>" editable></entity-gallery>
                     </div>
