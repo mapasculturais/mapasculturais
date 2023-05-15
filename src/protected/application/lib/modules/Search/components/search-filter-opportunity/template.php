9ffb4389f38212c9c3617b6726d1f59cbdf1a4d4
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-15 16:58:15 -0300

    corrige filtros dos campos de seleção única do filtro das páginas de busca

--- src/protected/application/lib/modules/Search/components/search-filter-opportunity/template.php
+++ src/protected/application/lib/modules/Search/components/search-filter-opportunity/template.php
@@ -19,8 +19,10 @@ $this->import('search-filter mc-multiselect mc-icon mc-tag-list');
         </div>
         <div class="field">
             <label> <?php i::_e('Tipo de oportunidade') ?></label>
-            <mc-multiselect :model="pseudoQuery['type']" :items="types" #default="{popover}" hide-filter hide-button>
-                <input class="mc-multiselect--input" @focus="popover.open()" placeholder="<?= i::esc_attr__('Selecione os tipos: ') ?>">
+            <mc-multiselect :model="pseudoQuery['type']" :items="types" hide-filter hide-button>
+                <template #default="{popover, setFilter, filter}">
+                    <input class="mc-multiselect--input" @keyup="setFilter($event.target.value)" @focus="popover.open()" placeholder="<?= i::esc_attr__('Selecione os tipos: ') ?>">
+                </template>
             </mc-multiselect>
             <mc-tag-list editable :tags="pseudoQuery['type']" :labels="types" classes="opportunity__background opportunity__color"></mc-tag-list>
 
