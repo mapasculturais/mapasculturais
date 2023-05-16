0ec524b88d99e6a00f73fe45753eb66e5a6cda6a
robedj <ed.wassouf@gmail.com>
2023-05-16 09:15:02 -0400

    W.I.P: Refatoração do componente agent-data Ref.: #2722

--- src/protected/application/lib/modules/Entities/components/agent-data/template.php
+++ src/protected/application/lib/modules/Entities/components/agent-data/template.php
@@ -6,31 +6,31 @@ $this->import('
 mapas-card
 ');
 ?>
-<div class="agent-data col-12 grid-12">
-   
-    <div class="col-12 agent-data__title">
+<div class="col-12 agent-data">
+
+    <div class="agent-data__title">
         <h4 class="title">{{title}}</h4>
     </div>
-    <div class="col-12 grid-12 agent-data__fields">
-        <div class="col-6 agent-data__fields--field">
+    <div class="agent-data__fields">
+        <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("MEI") ?></label>
             <div class="box">
                 {{entity.cnpj}}
             </div>
         </div>
-        <div class="col-6 agent-data__fields--field">
+        <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("Email Pessoal") ?></label>
             <div class="box">
                 {{entity.email}}
             </div>
         </div>
-        <div class="col-6 agent-data__fields--field">
+        <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("Nome Completo") ?></label>
             <div class="box">
                 {{entity.name}}
             </div>
         </div>
-        <div class="col-6 agent-data__fields--field">
+        <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("Email Secundário") ?></label>
             <div class="box">
                 {{entity.emailPrivado}}
@@ -55,5 +55,5 @@ mapas-card
                 {{entity.emailPrivado}}
             </div>
         </div> -->
-    </div>
+    <!-- </div> -->
 </div>
