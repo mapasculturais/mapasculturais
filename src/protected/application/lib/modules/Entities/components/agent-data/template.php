9db74ad9eb3e2c5ece49d059cc589701262505d2
robedj <ed.wassouf@gmail.com>
2023-05-16 10:17:34 -0400

    Estilização e adição de dados pessoais e sensiveis no componente agent-data Ref.: #2722

--- src/protected/application/lib/modules/Entities/components/agent-data/template.php
+++ src/protected/application/lib/modules/Entities/components/agent-data/template.php
@@ -13,47 +13,119 @@ mapas-card
     </div>
     <div class="agent-data__fields">
         <div class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Nome Completo") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.name}}</label>
+            </div>
+        </div>
+        <div class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Nome Social") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.nomeSocial}}</label>
+            </div>
+        </div>
+        <div class="agent-data__fields--field">
+            <label class="title"><?php i::_e("CPF") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.cnpj}}</label>
+            </div>
+        </div>
+        <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("MEI") ?></label>
             <div class="box">
-                {{entity.cnpj}}
+                <label class="box__content">{{entity.cnpj}}</label>
             </div>
         </div>
         <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("Email Pessoal") ?></label>
             <div class="box">
-                {{entity.email}}
+                <label class="box__content">{{entity.email}}</label>
             </div>
         </div>
         <div class="agent-data__fields--field">
-            <label class="title"><?php i::_e("Nome Completo") ?></label>
+            <label class="title"><?php i::_e("Telefone Público") ?></label>
             <div class="box">
-                {{entity.name}}
+                <label class="box__content">{{entity.telefonePublico}}</label>
             </div>
         </div>
-        <div class="agent-data__fields--field">
+        <div v-if="entity.currentUserPermissions.viewPrivateData" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Email Secundário") ?></label>
             <div class="box">
-                {{entity.emailPrivado}}
+                <label class="box__content">{{entity.emailPrivado}}</label>
+            </div>
+        </div>
+        <div class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Endereço") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.endereco}}</label>
             </div>
         </div>
     </div>
-    <!-- <div v-if="entity.currentUserPermissions.viewPrivateData" class="agent-data__title">
+    <div v-if="entity.currentUserPermissions.viewPrivateData" class="agent-data__secondTitle">
 
         <h4 class="title">{{secondTitle}}</h4>
     </div>
     <div v-if="entity.currentUserPermissions.viewPrivateData" class="agent-data__fields">
 
         <div class=" agent-data__fields--field">
-            <label class="title"><?php i::_e("Nome Completo") ?></label>
+            <label class="title"><?php i::_e("Data de Nascimento") ?></label>
             <div class="box">
-                {{entity.cnpj}}
+                <label class="box__content">{{entity.dataDeNascimento}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Gênero") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.genero}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Orientação Sexual") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.orientacaoSexual}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Agente Itinerante") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.agenteItinerante}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Raça/Cor") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.raca}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Escolaridade") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.escolaridade}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Pessoa portadora de deficiência") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.pessoaDeficiente}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Outras Comunidades Tradicionais") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.comunidadesTradicionalOutros}}</label>
+            </div>
+        </div>
+        <div class=" agent-data__fields--field">
+            <label class="title"><?php i::_e("Comunidades Tradicionais") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.comunidadesTradicional}}</label>
             </div>
         </div>
         <div class=" agent-data__fields--field">
             <label class="title"><?php i::_e("Email Secundário") ?></label>
             <div class="box">
-                {{entity.emailPrivado}}
+                <label class="box__content">{{entity.emailPrivado}}</label>
             </div>
-        </div> -->
-    <!-- </div> -->
+        </div>
+    </div>
 </div>
