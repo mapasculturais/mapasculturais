e28365c22175921322010dfc2749fdb1c0da963e
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-05-16 09:28:18 -0300

    Atualiza as cores da logo para as cores padrão (Ref.: 2151)

--- src/protected/application/lib/modules/Components/components/theme-logo/init.php
+++ src/protected/application/lib/modules/Components/components/theme-logo/init.php
@@ -1,10 +1,10 @@
 <?php
 $this->jsObject['config']['logo'] = [
     'color' => [
-        'bg1' => "var(--mc-primary-500)",
-        'bg2' => "var(--mc-secondary-500)",
-        'bg3' => "var(--mc-primary-500)",
-        'bg4' => "var(--mc-secondary-500)",
+        'bg1' => "var(--mc-primary-300)",
+        'bg2' => "var(--mc-primary-500)",
+        'bg3' => "var(--mc-primary-300)",
+        'bg4' => "var(--mc-primary-500)",
     ],
     'title' => $app->config['logo.title'],
     'subtitle' => $app->config['logo.subtitle'],
