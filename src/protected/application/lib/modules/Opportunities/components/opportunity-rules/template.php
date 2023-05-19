3f7f00acb8cec862e192084bb31fe1c510b65984
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-05-19 11:57:43 -0300

    Criação do componente para upload e listagem do regulamento da oportunidade (Ref.: #2692)

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-rules/template.php
@@ -0,0 +1,62 @@
+<?php 
+use MapasCulturais\i;
+
+$this->import('confirm-button popover modal image-uploader');
+?>
+
+<div :class="classes" v-if="file || editable" class="opportunity-rules">
+    <label class="opportunity-rules__title"> {{title}} </label>
+
+    <ul v-if="file" class="opportunity-rules__list">
+        <li class="opportunity-rules__list--item">
+            <a class="opportunity-rules__list--item-link" :download="file.name" :href="file.url">
+                <mc-icon name="download" :class="entity.__objectType+'__color'"></mc-icon>
+                <span v-if="file.name">{{file.name}}</span>
+                <span v-else> <? i::_e('Sem descrição') ?> </span>
+            </a>
+
+            <div v-if="editable" class="edit">                
+                <confirm-button @confirm="file.delete()">
+                    <template #button="modal">
+                        <a @click="modal.open()"> <mc-icon name="trash"></mc-icon> </a>
+                    </template> 
+                    <template #message="message">
+                        <?php i::_e('Deseja remover o link?') ?>
+                    </template> 
+                </confirm-button>                
+            </div>
+        </li>
+    </ul>
+
+    <popover v-if="editable" title="<?php i::_e('Adicionar arquivo')?>" openside="down-right">
+        <template #button="popover">
+            <slot name="button"> 
+                <a v-if="!file" @click="popover.toggle()" class="button button--primary button--icon button--primary-outline button-up">
+                    <mc-icon name="upload"></mc-icon>
+                    <?php i::_e("Enviar")?>
+                </a>
+
+                <a v-if="file" @click="popover.toggle()" class="button button--primary button--icon button--primary-outline button-up">
+                    <mc-icon name="upload"></mc-icon>
+                    <?php i::_e("Atualizar")?>
+                </a>
+            </slot>
+        </template>
+
+        <template #default="{popover, close}">
+            <form @submit="upload(popover); $event.preventDefault();" class="entity-files__newFile">
+                <div class="grid-12">                    
+                    <div class="col-12">
+                        <div class="field">
+                            <label><?php i::_e('Arquivo') ?></label>
+                            <input type="file" @change="setFile" ref="file"> 
+                        </div>
+                    </div> 
+
+                    <button class="col-6 button button--text" type="reset" @click="close"> <?php i::_e("Cancelar") ?> </button>
+                    <button class="col-6 button button--primary" type="submit" @click="close"> <?php i::_e("Confirmar") ?> </button>
+                </div>
+            </form>
+        </template>
+    </popover>
+</div>
