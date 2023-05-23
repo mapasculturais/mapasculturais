bb1d6ad08661eaf8ed086f10e3738b2997a225c4
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-23 10:56:17 -0300

    método para criar hooks de componente: Theme::applyComponentHook

--- src/protected/application/lib/modules/Components/Module.php
+++ src/protected/application/lib/modules/Components/Module.php
@@ -105,6 +105,34 @@ class Module extends \MapasCulturais\Module {
             $this->part('main-app--end');
         },1000);
 
+
+        /** 
+         * Cria um hook para o componente
+         * 
+         * o hook será no formato "component(component-name):param1" quando só um parâmtro for enviado
+         * o hook será no formato "component(component-name).param1:param2" quando 2 parâmtros forem enviados
+         * 
+         * @param string $param1
+         * @param string $param2
+         * @param string $sufix
+         */
+        $app->hook('Theme::applyComponentHook', function ($result, string $sufix, $param1 = [], $param2 = []) use($app) {
+            /** @var \MapasCulturais\Themes\BaseV2\Theme $this */
+
+            $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,3);
+            preg_match("#.+?/([^/]+)/template.php#", $bt[2]['file'], $match);
+            $component_name = $match[1];
+            if(is_string($param1)) {
+                $hook_name = "component($component_name).$sufix:$param1";
+                $params = $param2;
+            } else {
+                $hook_name = "component($component_name):$sufix";
+                $params = $param1;
+            }
+
+            $app->applyHookBoundTo($this, $hook_name, $params);
+        });
+
         /**
          * Importa um componente
          *
@@ -202,27 +230,28 @@ class Module extends \MapasCulturais\Module {
                 throw new Exceptions\TemplateNotFound("Component {$component} not found");
             }
 
-            $app->applyHookBoundTo($this, "component({$component}):before", [&$__template_path]);
-
+            
             ob_start(function ($output) {
                 return $output;
             });
-
+            
             if ($app->mode == APPMODE_DEVELOPMENT) {
                 echo "<!-- $component -->\n";
             }
 
+            $app->applyHookBoundTo($this, "component({$component}):before", [&$__data, &$__template_path]);
+            
             extract($__data);
 
             include $__template_path;
+
+            $app->applyHookBoundTo($this, "component({$component}):after", [$__data]);
 
             if ($app->mode == APPMODE_DEVELOPMENT) {
                 echo "\n<!-- /$component -->\n";
             }
 
             $__html = ob_get_clean();
-
-            $app->applyHookBoundTo($this, "component({$component}):after", [$__template_path, &$__html]);
 
             return $__html;
         });
