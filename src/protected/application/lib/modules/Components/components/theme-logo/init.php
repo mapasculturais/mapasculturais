22a79f8b59ab32c3d4ea7877b113a85b2c2e1fca
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-25 14:59:55 -0300

    corrige configuração da imagem do logo

--- src/protected/application/lib/modules/Components/components/theme-logo/init.php
+++ src/protected/application/lib/modules/Components/components/theme-logo/init.php
@@ -7,6 +7,6 @@ $this->jsObject['config']['logo'] = [
     'title' => $app->config['logo.title'],
     'subtitle' => $app->config['logo.subtitle'],
     'colors' => $app->config['logo.colors'],
-    'image' => $this->asset($app->config['logo.image'], false),
+    'image' => $app->config['logo.image'] ? $this->asset($app->config['logo.image'], false) : '',
     'hideLabel' => $app->config['logo.hideLabel'],
 ];
