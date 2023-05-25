b0f17018727f314435134a146a99252431e06a80
robedj <ed.wassouf@gmail.com>
2023-05-25 15:39:15 -0400

    Ajusta campos de exibição do agente individual Ref.: #2758

--- src/protected/application/lib/modules/Entities/components/agent-data/template.php
+++ src/protected/application/lib/modules/Entities/components/agent-data/template.php
@@ -11,7 +11,7 @@ mapas-card
     <div v-if="verifyEntity()" class="agent-data__title">
         <h4 class="title">{{title}}</h4>
     </div>
-    <div v-if="entity.name?.length>0  verifyEntity()" class="agent-data__fields">
+    <div v-if="entity.name?.length>0" class="agent-data__fields">
         <div class="agent-data__fields--field">
             <label class="title"><?php i::_e("Nome Completo") ?></label>
             <div class="box">
@@ -36,28 +36,34 @@ mapas-card
                 <label class="box__content">{{entity.cnpj}}</label>
             </div>
         </div>
-        <div v-if="entity.email?.length>0" class="agent-data__fields--field">
-            <label class="title"><?php i::_e("Email Pessoal") ?></label>
-            <div class="box">
-                <label class="box__content">{{entity.email}}</label>
-            </div>
-        </div>
         <div v-if="entity.telefonePublico?.length>0" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Telefone Público") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.telefonePublico}}</label>
             </div>
         </div>
+        <div v-if="entity.telefonePublico?.length>0" class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Telefone Público 1") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.telefone1}}</label>
+            </div>
+        </div>
+        <div v-if="entity.telefonePublico?.length>0" class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Telefone Público 2") ?></label>
+            <div class="box">
+                <label class="box__content">{{entity.telefone2}}</label>
+            </div>
+        </div>
         <div v-if="entity.emailPrivado?.lenght>0" class="agent-data__fields--field">
-            <label class="title"><?php i::_e("Email Secundário") ?></label>
+            <label class="title"><?php i::_e("Email Pessoal") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.emailPrivado}}</label>
             </div>
         </div>
-        <div v-if="entity.endereco?.lenght>0" class="agent-data__fields--field">
-            <label class="title"><?php i::_e("Endereço") ?></label>
+        <div v-if="entity.emailPrivado?.lenght>0 && verifyEntity()" class="agent-data__fields--field">
+            <label class="title"><?php i::_e("Email Público") ?></label>
             <div class="box">
-                <label class="box__content">{{entity.endereco}}</label>
+                <label class="box__content">{{entity.emailPublico}}</label>
             </div>
         </div>
     </div>
@@ -65,7 +71,7 @@ mapas-card
 
         <h4 class="title">{{secondTitle}}</h4>
     </div>
-    <div v-if="entity.currentUserPermissions.viewPrivateData" class="agent-data__fields">
+    <div v-if="entity.currentUserPermissions.viewPrivateData && verifyEntity()" class="agent-data__fields">
 
         <div v-if="entity.dataDeNascimento!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Data de Nascimento") ?></label>
@@ -73,49 +79,49 @@ mapas-card
                 <label class="box__content">{{entity.dataDeNascimento.date('long year')}}</label>
             </div>
         </div>
-        <div v-if="entity.genero?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.genero!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Gênero") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.genero}}</label>
             </div>
         </div>
-        <div v-if="entity.orientacaoSexual?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.orientacaoSexual!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Orientação Sexual") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.orientacaoSexual}}</label>
             </div>
         </div>
-        <div v-if="entity.agenteItinerante?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.agenteItinerante!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Agente Itinerante") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.agenteItinerante}}</label>
             </div>
         </div>
-        <div v-if="entity.raca?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.raca!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Raça/Cor") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.raca}}</label>
             </div>
         </div>
-        <div v-if="entity.escolaridade?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.escolaridade!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Escolaridade") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.escolaridade}}</label>
             </div>
         </div>
-        <div v-if="entity.pessoaDeficiente?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.pessoaDeficiente!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Pessoa portadora de deficiência") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.pessoaDeficiente}}</label>
             </div>
         </div>
-        <div v-if="entity.comunidadesTradicional?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.comunidadesTradicional!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Comunidades Tradicionais") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.comunidadesTradicional}}</label>
             </div>
         </div>
-        <div v-if="entity.comunidadesTradicionalOutros?.lenght>0" class="agent-data__fields--field">
+        <div v-if="entity.comunidadesTradicionalOutros!=null" class="agent-data__fields--field">
             <label class="title"><?php i::_e("Outras Comunidades Tradicionais") ?></label>
             <div class="box">
                 <label class="box__content">{{entity.comunidadesTradicionalOutros}}</label>
