88fffb2ee52add14f3ed00e33e63dc5e89b10024
robedj <ed.wassouf@gmail.com>
2023-05-26 16:00:56 -0400

    W.I.P: Estilização das telas de erros Ref.: #2674

--- src/protected/application/themes/BaseV2/views/site/error-403.php
+++ src/protected/application/themes/BaseV2/views/site/error-403.php
@@ -1,8 +1,25 @@
 <?php
+
 use MapasCulturais\i;
+$this->import('
+error-display
+');
 ?>
-<h1><?= i::__('Permissão Negada') ?></h1>
-<?php 
-if ($app->mode == 'development') {
-    \dump($e);
-}
+
+
+<div class="error-404">
+    <div class="content">
+        <div class="left">
+            <h1 class="left__title"><?= i::__('Erro 404') ?></h1>
+            <label class="left__message"><?= i::__('Acesso Privado') ?></label>
+            <label class="left__content"><?= i::__(' A página que você está tentando acessar é particular. 
+            Faça login com outra conta ou solicite acesso.') ?></label>
+        </div>
+        <div class="right">
+            <img src="<?php $this->asset('/img/error403.png', true, true) ?>">
+        </div>
+    </div>
+    <div class="btn">
+        <button class="button button--primary btn-error"><label class="btn__label"></label><?= i::__('Voltar para a página inicial') ?></label></button>
+    </div>
+</div>
