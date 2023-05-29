22232394dd211b9ced1dd17c0cdbc140aa273ab1
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-05-29 20:51:05 -0300

    corrige comportamento dos selects do componente create-opportunity (refs #2777)

--- src/protected/application/lib/modules/Entities/components/create-opportunity/template.php
+++ src/protected/application/lib/modules/Entities/components/create-opportunity/template.php
@@ -26,56 +26,56 @@ $this->import('
                 <div class="select-list__item">
                     <select-entity type="project" @select="setEntity($event)" openside="down-right">
                         <template #button="{ toggle }">
-                            <div class="inner">
-                                <label class="itemLabel" for="btnProject">
-                                    <input v-model="entityTypeSelected" type="radio" id="btnProject" name="inputName" value="project" />
-                                    <span @click="toggle()"><?php i::_e('Projeto') ?> </span>
-                                </label>
+                            <label class="inner">
+                                <span class="itemLabel">
+                                    <input v-model="entityTypeSelected" @click="toggle()" type="radio" name="inputName" value="project" />
+                                    <span><?php i::_e('Projeto') ?> </span>
+                                </span>
 
-                                <a :class="{'disabled': entityTypeSelected!='project'}" class="selectButton" @click="toggle()"><?php i::_e('Selecionar') ?> </a>
-                            </div>
+                                <a :class="{'disabled': entityTypeSelected!='project'}" class="selectButton"><?php i::_e('Selecionar') ?> </a>
+                            </label>
                         </template>
                     </select-entity>
                 </div>
                 <div class="select-list__item">
                     <select-entity type="event" @select="setEntity($event)" openside="down-right">
                         <template #button="{ toggle }">
-                            <div class="inner">
-                                <label class="itemLabel" @click="toggle()" for="btnEvent">
-                                    <input v-model="entityTypeSelected" type="radio" id="btnEvent" name="inputName" value="event" />
-                                    <span @click="toggle()"><?php i::_e('Evento') ?> </span>
-                                </label>
+                            <label class="inner">
+                                <span class="itemLabel">
+                                    <input v-model="entityTypeSelected" @click="toggle()" type="radio" name="inputName" value="event" />
+                                    <span><?php i::_e('Evento') ?> </span>
+                                </span>
 
-                                <a :class="{'disabled': entityTypeSelected!='event'}" class="selectButton" @click="toggle()"><?php i::_e('Selecionar') ?> </a>
-                            </div>
+                                <a :class="{'disabled': entityTypeSelected!='event'}" class="selectButton"><?php i::_e('Selecionar') ?> </a>
+                            </label>
                         </template>
                     </select-entity>
                 </div>
                 <div class="select-list__item">
                     <select-entity type="space" @select="setEntity($event)" openside="down-right">
                         <template #button="{ toggle }">
-                            <div class="inner">
-                                <label class="itemLabel" @click="toggle()" for="btnSpace">
-                                    <input v-model="entityTypeSelected" type="radio" id="btnSpace" name="inputName" value="space" />
-                                    <span @click="toggle()"><?php i::_e('Espaço') ?> </span>
-                                </label>
+                            <label class="inner">
+                                <span class="itemLabel">
+                                    <input v-model="entityTypeSelected" @click="toggle()" type="radio" name="inputName" value="space" />
+                                    <span><?php i::_e('Espaço') ?> </span>
+                                </span>
 
-                                <a :class="{'disabled': entityTypeSelected!='space'}" class="selectButton" @click="toggle()"><?php i::_e('Selecionar') ?> </a>
-                            </div>
+                                <a :class="{'disabled': entityTypeSelected!='space'}" class="selectButton"><?php i::_e('Selecionar') ?> </a>
+                            </label>
                         </template>
                     </select-entity>
                 </div>
                 <div class="select-list__item">
                     <select-entity type="agent" @select="setEntity($event)" openside="down-right">
                         <template #button="{ toggle }">
-                            <div class="inner">
-                                <label class="itemLabel" @click="toggle()" for="btnAgent">
-                                    <input v-model="entityTypeSelected" type="radio" id="btnAgent" name="inputName" value="agent" />
-                                    <span @click="toggle()"><?php i::_e('Agente') ?> </span>
-                                </label>
+                            <label class="inner">
+                                <span class="itemLabel">
+                                    <input v-model="entityTypeSelected" @click="toggle()" type="radio" name="inputName" value="agent" />
+                                    <span><?php i::_e('Agente') ?> </span>
+                                </span>
 
-                                <a :class="{'disabled': entityTypeSelected!='agent'}" class="selectButton" @click="toggle()"><?php i::_e('Selecionar') ?> </a>
-                            </div>
+                                <a :class="{'disabled': entityTypeSelected!='agent'}" class="selectButton"><?php i::_e('Selecionar') ?> </a>
+                            </label>
                         </template>
                     </select-entity>
                 </div>
@@ -94,7 +94,7 @@ $this->import('
                     <div class="entity-selected__info">
                         <select-entity :type="entityTypeSelected" @select="setEntity($event)" openside="right-down">
                             <template #button="{ toggle }">
-                                <a class="entity-selected__info--btn" :class="entityTypeSelected + '__color'" @click="toggle()">
+                                <a class="entity-selected__info--btn" :class="entityTypeSelected + '__color'">
                                     <mc-icon :class="[entityTypeSelected + '__color']" name="exchange"></mc-icon>
                                     <h4 :class="[entityTypeSelected + '__color']"><?php i::_e('Alterar') ?></h4>
                                 </a>
