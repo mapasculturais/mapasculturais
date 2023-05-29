3c53b2c46c44a4db69b77f0c15571c2cc1446177
robedj <ed.wassouf@gmail.com>
2023-05-29 10:17:24 -0400

    Ajustes conforme o figma, da pagina de erro 403 e 404 Ref.: #2764

--- src/protected/application/themes/BaseV2/views/site/error-404.php
+++ src/protected/application/themes/BaseV2/views/site/error-404.php
@@ -1,25 +1,31 @@
 <?php
+
 use MapasCulturais\i;
 
 ?>
-<div class="error-403">
-    <div class="content">
-        <div class="left">
-            <h1 class="left__title"><?= i::__('Erro 404') ?></h1>
-            <label class="left__content"><?= i::__(' Ops, página 
+
+<div class="error-page error-403">
+    <div class="error-card">
+
+        <div class="content">
+            <div class="left">
+                <h1 class="left__title"><?= i::__('Erro 404') ?></h1>
+                <label class="left__content"><?= i::__(' Ops, página 
 não encontrada.') ?></label>
-        </div>
-        <div class="right">
-            <img src="<?php $this->asset('/img/404.png', true, true) ?>">
-        </div>
+            </div>
+            <div class="right">
+                <img src="<?php $this->asset('/img/404.png', true, true) ?>">
+            </div>
 
-    </div>
-    <div class="message-error">
-        <label class="text"><?= i::__('Essa página não existe mais ou mudou de endereço. Mas não se preocupe, 
+        </div>
+        <div class="message-error">
+            <label class="text"><?= i::__('Essa página não existe mais ou mudou de endereço. Mas não se preocupe, 
         existem muitas outras páginas para conhecer aqui no Mapas.') ?></label>
-    </div>
-    <div class="btn">
+        </div>
+        <div class="btn">
 
-        <button class="button button--primary btn-error"><label class="btn__label"></label><?= i::__('Voltar para a página inicial') ?></label></button>
+            <button class="button button--primary btn-error"><label class="btn__label"></label><?= i::__('Voltar para a página inicial') ?></label></button>
+        </div>
     </div>
+
 </div>
