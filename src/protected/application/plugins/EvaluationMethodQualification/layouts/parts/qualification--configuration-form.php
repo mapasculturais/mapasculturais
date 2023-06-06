7f8c24849851bd09c5d49365e5dc8d7eda3e37cc
erleibiazzio <erleibiazzio@gmail.com>
2023-06-06 18:32:33 -0300

    Corrige ortografia da palavra critério na nova avaliação EvaluationMethodQualification

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--configuration-form.php
@@ -9,7 +9,7 @@ use MapasCulturais\i;
         <hr>
         <header>
             <input ng-model="section.name" placeholder="<?php i::_e('informe o nome da seção') ?>" class="section-name edit" ng-change="save({sections: data.sections})" ng-model-options='{ debounce: data.debounce }'>
-            <button class="btn btn-default add" ng-click="addCriterion(section)" title="<?php i::_e('Adicionar creitério') ?>"> <?php i::_e('Adicionar creitério') ?></button>
+            <button class="btn btn-default add" ng-click="addCriterion(section)" title="<?php i::_e('Adicionar critério') ?>"> <?php i::_e('Adicionar critério') ?></button>
             <button ng-if="section.name.trim().length > 0" ng-click="deleteSection(section)" class="btn btn-danger delete alignright"><?php i::_e('Remover seção') ?></button>
             <button ng-if="section.name.trim().length == 0" ng-click="deleteSection(section)" class="btn btn-default delete alignright"><?php i::_e('Cancelar') ?></button>
         </header>
@@ -29,7 +29,7 @@ use MapasCulturais\i;
                 <td>
                     <div>
                         <a id="delete-account--button" ng-click="editbox.open(cri.id, $event)" rel='noopener noreferrer' class="btn btn-primary add"><?php i::_e('Configurar critério') ?></a>
-                        <edit-box id="{{cri.id}}" position="right" title="<?php i::esc_attr_e("Configuração do creitério"); ?> {{cri.name}}" cancel-label="<?php i::esc_attr_e("Fechar"); ?>" close-on-cancel='true' spinner-condition="data.spinner">
+                        <edit-box id="{{cri.id}}" position="right" title="<?php i::esc_attr_e("Configuração do critério"); ?> {{cri.name}}" cancel-label="<?php i::esc_attr_e("Fechar"); ?>" close-on-cancel='true' spinner-condition="data.spinner">
                             <div>
                                 <label > <?php i::esc_attr_e("Opções ou motivos de inabilitação"); ?></label>
                                 <textarea ng-model="data.options[cri.id]" ng-model-options='{ debounce: data.debounce }' cols="70" rows="5" ng-change="save({sections: data.sections})"></textarea>
