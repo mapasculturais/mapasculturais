03680206581dc713431320ab694541efcb88b917
robedj <ed.wassouf@gmail.com>
2023-06-06 12:13:28 -0400

    Exibição do Publicado por no componente entity-owner somente para entidade diferente da que publicou Ref.: #2786

--- src/protected/application/lib/modules/Entities/components/entity-owner/template.php
+++ src/protected/application/lib/modules/Entities/components/entity-owner/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
@@ -11,28 +12,28 @@ $this->import('
     select-entity
 ');
 ?>
-<div v-if="owner" class="entity-owner" :class="classes">
-    <h4>{{title}}</h4>
-    <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
-        <div class="entity-owner__owner--img">
-            <img v-if="owner.files.avatar" class="profile" :src="owner.files?.avatar?.url">
-            <div v-else class="placeholder">
-                <mc-icon name="agent-1"></mc-icon>
+<div v-if="owner && global.auth.user.profile.id != owner.id" class="entity-owner" :class="classes">
+        <h4>{{title}}</h4>
+        <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
+            <div class="entity-owner__owner--img">
+                <img v-if="owner.files.avatar" class="profile" :src="owner.files?.avatar?.url">
+                <div v-else class="placeholder">
+                    <mc-icon name="agent-1"></mc-icon>
+                </div>
             </div>
-        </div>
-        <div class="entity-owner__owner--name">
-            {{owner.name}}
-        </div>
-    </a>
+            <div class="entity-owner__owner--name">
+                {{owner.name}}
+            </div>
+        </a>
 
-    <div v-if="editable" class="entity-owner__edit">
-        <select-entity :query="query" type="agent" @select="changeOwner($event)" permissions="" openside="up-right">
-            <template #button="{ toggle }">
-                <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()"> 
-                    <mc-icon name="exchange"></mc-icon>
-                    <h4><?php i::_e('Alterar Propriedade') ?></h4>    
-                </a>
-            </template>
-        </select-entity>
-    </div>
+        <div v-if="editable" class="entity-owner__edit">
+            <select-entity :query="query" type="agent" @select="changeOwner($event)" permissions="" openside="up-right">
+                <template #button="{ toggle }">
+                    <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()">
+                        <mc-icon name="exchange"></mc-icon>
+                        <h4><?php i::_e('Alterar Propriedade') ?></h4>
+                    </a>
+                </template>
+            </select-entity>
+        </div>
 </div>
