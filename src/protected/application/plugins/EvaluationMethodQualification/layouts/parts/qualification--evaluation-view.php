7bac5bbc53e9693c159b6dbe7ff27df5255d1cf4
erleibiazzio <erleibiazzio@gmail.com>
2023-06-06 21:38:33 -0300

    Ajusta exibição da view do metodo de avaliação EvaluationMethodQualificantio para exibir os critérios de cada seção mesmo em modo de visualização

--- src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
+++ src/protected/application/plugins/EvaluationMethodQualification/layouts/parts/qualification--evaluation-view.php
@@ -8,8 +8,19 @@ use MapasCulturais\i;
              {{section.name}}: 
              <strong ng-if="subtotalSection(section) == '<?php i::_e('Habilitado')?>'" class="approved">{{subtotalSection(section)}}</strong>
              <strong ng-if="subtotalSection(section) == '<?php i::_e('Inabilitado')?>'" class="repproved">{{subtotalSection(section)}}</strong>
+            
+             <div ng-repeat="cri in ::data.criteria" ng-if="cri.sid == section.id">
+                <small ng-if="evaluation[cri.id] == '<?php i::_e('Habilitado')?>'">
+                    <strong>&nbsp&nbsp {{cri.name}}:</strong> 
+                    <span class="approved">{{evaluation[cri.id]}}</span>
+                </small>
+                <small ng-if="evaluation[cri.id] == '<?php i::_e('Inabilitado')?>'">
+                    <strong>&nbsp&nbsp {{cri.name}}:</strong>
+                    <span class="approved">{{evaluation[cri.id]}}</span>
+                </small>
+             </div>
+             <hr>
         </section>
-        <hr>
         <section class='total'>
             <?php i::_e('Status'); ?>: 
             <strong ng-if="total() == '<?php i::_e('Habilitado')?>'" class="approved">{{total(total)}}</strong>
