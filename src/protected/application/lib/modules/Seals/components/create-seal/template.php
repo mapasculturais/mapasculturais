96096c2f3b048391f707b6551d1293c09c359012
robedj <ed.wassouf@gmail.com>
2023-06-08 15:27:37 -0400

    Internacionalização na modal de criação de selos Ref.: #2790

--- src/protected/application/lib/modules/Seals/components/create-seal/template.php
+++ src/protected/application/lib/modules/Seals/components/create-seal/template.php
@@ -14,7 +14,7 @@ $this->import('
     seal-form-valid-period
 ');
 ?>
-<mc-modal :title="modalTitle" classes="create-modal" button-label="Criar Selo" @open="createEntity()" @close="destroyEntity()">
+<mc-modal :title="modalTitle" classes="create-modal" button-label="<?php i::_e('Criar Selo')?>" @open="createEntity()" @close="destroyEntity()">
      <template v-if="entity && !entity.id" #default>
          <label><?php i::_e('Crie um selo com informações básicas') ?><br><?php i::_e('e de forma rápida') ?></label>
          <div class="create-modal__fields">
