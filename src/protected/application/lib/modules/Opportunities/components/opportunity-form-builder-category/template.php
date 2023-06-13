3076c12af344242475a4fff31a83de2ad667519f
robedj <ed.wassouf@gmail.com>
2023-06-13 08:54:21 -0400

    altera template do componente opportunity-category Ref.: #2831

--- src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-form-builder-category/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
@@ -9,17 +10,20 @@ use MapasCulturais\i;
 $this->layout = 'entity';
 
 $this->import('
-    opportunity-form-builder-category-list
+    opportunity-form-builder-category-list 
+    mc-card
 ');
 ?>
 <div class="opportunity-category">
     <div class="opportunity-category__header">
-            <label class="opportunity-category__title"><?= i::__("Categorias de inscrição") ?></label>
-            <div class="opportunity-category__subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></div>
+            <label class="card-header__title"><?= i::__("Categorias de inscrição") ?></label>
+            <!-- classe antiga class="subtitle" -->
+            <div class="card-header__subtitle"><?= i::__("Crie opções para as pessoas escolherem na hora de se inscrever, como, por exemplo, \"categorias\" ou \"modalidades\".") ?></div>
         </div>
-    <div class="opportunity-category__content">
-        <entity-field :entity="entity" prop="registrationCategTitle" classes="col-12"></entity-field>
-        <entity-field :entity="entity" prop="registrationCategDescription" classes="col-12"></entity-field>
-        <opportunity-form-builder-category-list :entity="entity" class="col-12"></opportunity-form-builder-category-list>
-    </div>
+    
+        <div class="opportunity-category__content grid-12">
+            <entity-field :entity="entity" prop="registrationCategTitle" classes="col-12"></entity-field>
+            <entity-field :entity="entity" prop="registrationCategDescription" classes="col-12"></entity-field>
+            <opportunity-form-builder-category-list :entity="entity" class="col-12"></opportunity-form-builder-category-list>
+        </div>
 </div>
