cc74074b1eb9803f8c3ab28ba110051b4e6b46a6
robedj <ed.wassouf@gmail.com>
2023-06-13 16:28:38 -0400

    W.I.P : Ajusta template componente opportunity-phase-config-data-collection Ref.: #2831

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
@@ -13,17 +13,16 @@ $this->import('
     opportunity-category
 ');
 ?>
-<mc-card>
-    <div class="config-phase grid-12">
-        <div class="config-phase__line col-12 "></div>
+    <div class="opportunity-phase-config-data-collection grid-12">
+        <div class="opportunity-phase-config-data-collection__hLine col-12 "></div>
         <entity-field :entity="phase" prop="registrationFrom" :autosave="300" :min="minDate?._date" :max="phase.registrationTo?._date" classes="col-6 sm:col-12"></entity-field>
         <entity-field :entity="phase" prop="registrationTo" :autosave="300" :min="phase.registrationFrom?._date" :max="maxDate?._date" classes="col-6 sm:col-12"></entity-field>
-        <div class="config-phase__category col-12">
+        <div class="opportunity-phase-config-data-collection__category col-12">
             <div v-if="phase.isFirstPhase">
                 <opportunity-category :entity="phase"></opportunity-category>
             </div>
-            <div class="vertLine"></div>
-            <div class="config-phase__registration">
+            <div class="opportunity-phase-config-data-collection__vLine"></div>
+            <div class="opportunity-phase-config-data-collection__registration">
                 <entity-field :entity="phase" prop="registrationLimit" :autosave="300" classes="col-3"></entity-field>
                 <entity-field :entity="phase" prop="registrationLimitPerOwner" :autosave="300" classes="col-3"></entity-field>
             </div>
@@ -35,15 +34,13 @@ $this->import('
             </mc-link>
         </div>
         <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
-            <div class="config-phase__line col-12 "></div>
+            <div class="opportunity-phase-config-data-collection__hLine col-12 "></div>
             <opportunity-phase-publish-date-config :phase="phase" :phases="phases" hide-description hide-button></opportunity-phase-publish-date-config>
         </template>
-        <!-- <div class="config-phase__line col-12 "></div> -->
-
-        <div class="phase-delete col-12" v-if="!phase.isLastPhase && !phase.isFirstPhase">
+        <div class="opportunity-phase-config-data-collection__delete col-12" v-if="!phase.isLastPhase && !phase.isFirstPhase">
             <mc-confirm-button message="Confirma a execução da ação?" @confirm="deletePhase($event, phase, index)">
                 <template #button="modal">
-                    <a class="phase-delete__trash" @click="modal.open">
+                    <a class="opportunity-phase-config-data-collection__trash" @click="modal.open">
                         <div class="icon">
                             <mc-icon name="trash"></mc-icon>
                         </div>
@@ -53,4 +50,3 @@ $this->import('
             </mc-confirm-button>
         </div>
     </div>
-</mc-card>
