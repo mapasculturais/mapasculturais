d2bb7a51a2f27df43dc60d92fd686239576b8d9d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-06-14 15:45:46 -0300

    ajuste nos arquivos de configuração

--- src/conf/config.php
+++ src/conf/config.php
@@ -1,10 +1,17 @@
 <?php
 $config = [];
 
-$config_files = glob(PROTECTED_PATH . 'config/*.php');
-
+// arquivos da pasta /config
+$config_files = glob(CONFIG_PATH . '*.php');
 sort($config_files);
+foreach($config_files as $config_file) {
+    $config = array_merge($config, include ($config_file) );
+}
 
+// arquivos da pasta config/config.d
+// esta pasta é onde devem entrar as configurações da instalação
+$config_files = glob(PROTECTED_PATH . 'config.d/*.php');
+sort($config_files);
 foreach($config_files as $config_file) {
     $config = array_merge($config, include ($config_file) );
 }
