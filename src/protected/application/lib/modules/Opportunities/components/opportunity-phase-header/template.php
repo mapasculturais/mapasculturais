39fd408b1ac1beb07b14779af7b936ed551e9ba4
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-06-14 12:23:55 -0300

    Ajustes no estilo do componente opportunity-phase-header (Ref.: #2828)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-header/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-header/template.php
@@ -7,20 +7,20 @@
 
 use MapasCulturais\i;
 ?>
-<div :class="classes" class="grid-12 form-builder__header opportunity-phase-header">
-    <div class="sm:col-12 form-builder__title" :class="titleColClass">
+<div :class="classes" class="grid-12 opportunity-phase-header">
+    <div class="sm:col-12 opportunity-phase-header__title" :class="titleColClass">
         <h3 class="opportunity__color">{{ title }}</h3>
     </div>
-    <div v-if="dateFrom" class="col-2 sm:col-4 form-builder__period">
-        <h4 class="form-builder__label"><?= i::__("Data de início") ?></h4>
-        <h4 class="form-builder__value opportunity__color">{{ dateFrom }}</h4>
+    <div v-if="dateFrom" class="col-2 sm:col-4">
+        <h6 class="bold"><?= i::__("Data de início") ?></h6>
+        <h4 class="opportunity__color bold">{{ dateFrom }}</h4>
     </div>
-    <div v-if="dateTo" class="col-2 sm:col-4 form-builder__period">
-        <h4 class="form-builder__label"><?= i::__("Data final") ?></h4>
-        <h4 class="form-builder__value opportunity__color">{{ dateTo }}</h4>
+    <div v-if="dateTo" class="col-2 sm:col-4">
+        <h6 class="bold"><?= i::__("Data final") ?></h6>
+        <h4 class="opportunity__color bold">{{ dateTo }}</h4>
     </div>
-    <div v-if="publishDate" class="col-2 sm:col-4 form-builder__period">
-        <h4 class="form-builder__label"><?= i::__("Data de publicação") ?></h4>
-        <h4 class="form-builder__value opportunity__color">{{ publishDate }}</h4>
+    <div v-if="publishDate" class="col-2 sm:col-4">
+        <h6 class="bold"><?= i::__("Data de publicação") ?></h6>
+        <h4 class="opportunity__color bold">{{ publishDate }}</h4>
     </div>
 </div>
