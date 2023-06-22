ddfdf4577f0ceb054bb3a7abc77ced1afac9ce00
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-06-22 01:37:06 -0300

    Adiciona botão 'voltar' para retornar para a configuração de formulário (Ref.: #2750)

--- src/protected/application/lib/modules/Opportunities/views/opportunity/preview-form.php
+++ src/protected/application/lib/modules/Opportunities/views/opportunity/preview-form.php
@@ -10,6 +10,7 @@ use MapasCulturais\i;
 $this->layout = "entity";
 
 $this->import('
+    mc-container
     opportunity-header
     registration-info
     v1-embed-tool
@@ -25,13 +26,18 @@ $this->import('
 
         <mc-container>
             <main class="grid-12">
-
                 <registration-info :registration="entity" classes="col-12"></registration-info>
 
                 <section class="col-12">
                     <v1-embed-tool iframe-id="preview-form" route="registrationformpreview" :id="entity.id"></v1-embed-tool>
                 </section>
             </main>
+
+            <aside>
+                <a href="<?= $app->createUrl('opportunity', 'formBuilder', [$entity->id]) ?>" class="button button--large button--primary-outline">
+                    <?= i::__("Voltar") ?>
+                </a>
+            </aside>
         </mc-container>
     </div>
 </div>
