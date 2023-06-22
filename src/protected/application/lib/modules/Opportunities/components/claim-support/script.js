2d698f06748e30fe1225df83a02da3289bde7f58
robedj <ed.wassouf@gmail.com>
2023-06-22 14:03:36 -0400

    Componente de recurso Ref.: #2771

---
+++ src/protected/application/lib/modules/Opportunities/components/claim-support/script.js
@@ -0,0 +1,28 @@
+app.component('claim-support', {
+    template: $TEMPLATES['claim-support'],
+
+    setup() { },
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+
+    },
+
+    data() {
+        return {
+            claim: {},
+        }
+    },
+
+    computed: {
+
+        modalTitle() {
+            return 'Solicitar Recurso';
+
+        },
+    },
+    methods: {},
+});
