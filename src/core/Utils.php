594893d674e47d4e9f9d4426c8fd74182edc0cec
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-06-22 16:28:50 -0300

    correção para compatibilidade com o php 8.2

--- src/core/Utils.php
+++ src/core/Utils.php
@@ -164,7 +164,7 @@ class Utils {
 
     static function formatCnpjCpf($value) {
       $CPF_LENGTH = 11;
-      $cnpj_cpf = preg_replace("/\D/", '', $value);
+      $cnpj_cpf = preg_replace("/\D/", '', $value ?: '');
       
       if (strlen($cnpj_cpf) === $CPF_LENGTH) {
         return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
