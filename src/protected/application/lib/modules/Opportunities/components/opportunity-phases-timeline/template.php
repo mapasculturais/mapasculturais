0c17d6ec8f27f7b7857005a6b0de92c4cb63b0e4
robedj <ed.wassouf@gmail.com>
2023-06-23 08:37:47 -0400

    estilização e ajustes no template do componente claim-support Ref.:#2771

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phases-timeline/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phases-timeline/template.php
@@ -7,6 +7,7 @@
 use MapasCulturais\i;
 $this->import('
     mc-link
+    claim-support
 ');
 ?>
 <section :class="['timeline', {'center': center}, {'big': big}]">
@@ -14,22 +15,20 @@ $this->import('
         <div class="item__dot"> <span class="dot"></span> </div>
         <div class="item__content">
             <?php $this->applyComponentHook('item', 'begin'); ?>
-            
             <div v-if="item.isFirstPhase" class="item__content--title"> <?= i::__('Fase de inscrições') ?> </div>
             <div v-if="!item.isFirstPhase" class="item__content--title"> {{item.name}} </div>
-            
             <div v-if="!item.isLastPhase" class="item__content--description">
                 <?= i::__('de') ?> <span v-if="dateFrom(item)">{{dateFrom(item)}}</span>
                 <?= i::__('a') ?> <span v-if="dateTo(item)">{{dateTo(item)}}</span>
                 <?= i::__('às') ?> <span v-if="hour(item)">{{hour(item)}}</span>
             </div>
-            <div v-if="item.__objectType=='evaluationmethodconfiguration'">
+            <div v-if="item.__objectType=='evaluationmethodconfiguration'" class="opportunity-phases-timeline__claim-support">
                 <claim-support :entity="entity"></claim-support>
             </div>
             <div v-if="item.isLastPhase" class="item__content--description">
                 <span v-if="item.publishTimestamp">
                     {{item.publishTimestamp.date('2-digit year')}}
-                    <?= i::__('às') ?> 
+                    <?= i::__('às') ?>
                     {{item.publishTimestamp.time()}}
                 </span>
             </div>
