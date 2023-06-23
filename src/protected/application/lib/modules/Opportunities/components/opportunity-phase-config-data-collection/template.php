66fb5d4e5dce432ad06e293fc7e245a0c54379e9
robedj <ed.wassouf@gmail.com>
2023-06-23 16:05:01 -0400

    Ajusta template conforme o figma do opportunity-form-builder-category-list Ref.: #2864

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
@@ -18,6 +18,11 @@ $this->import('
         <div class="opportunity-data-collection__horizontal-line col-12 "></div>
         <entity-field :entity="phase" prop="registrationFrom" :autosave="300" :min="minDate?._date" :max="phase.registrationTo?._date" classes="col-6 sm:col-12"></entity-field>
         <entity-field :entity="phase" prop="registrationTo" :autosave="300" :min="phase.registrationFrom?._date" :max="maxDate?._date" classes="col-6 sm:col-12"></entity-field>
+        <div class="col-12 grid-12">
+            <mc-link :entity="phase" route='formBuilder' class="config-phase__info-button button--primary button col-6" icon="external" right-icon>
+              <?= i::__("Configurar formulário") ?>
+            </mc-link>
+        </div>
         <div class="opportunity-data-collection__category col-12">
             <div v-if="phase.isFirstPhase">
                 <opportunity-category :entity="phase"></opportunity-category>
@@ -29,12 +34,7 @@ $this->import('
             </div>
         </div>
 
-        <div class="col-12 grid-12">
-            <mc-link :entity="phase" route='formBuilder' class="config-phase__info-button button--primary button col-6" icon="external" right-icon>
-              <?= i::__("Configurar formulário") ?>
-            </mc-link>
-        </div>
-        <div class="col-12 sm:col-12">
+        <div class="col-12 sm:col-6">
             <opportunity-enable-claim></opportunity-enable-claim>
         </div>
         <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
