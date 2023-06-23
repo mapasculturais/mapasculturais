9d7e4a4f883280b1ea54ecea8e34689b726eaa85
robedj <ed.wassouf@gmail.com>
2023-06-23 13:59:24 -0400

    Criação do componente opportunity-enable-claim ref.: #2771

---
+++ src/protected/application/lib/modules/OpportunityClaimForm/components/opportunity-enable-claim/script.js
@@ -0,0 +1,20 @@
+app.component('opportunity-enable-claim' , {
+    template: $TEMPLATES['opportunity-enable-claim'],
+
+    setup() {
+        const text = Utils.getTexts('opportunity-enable-claim');
+        return { text };
+    },
+
+    props: {
+       
+    },
+
+    computed: {
+     
+    },
+
+    methods: {
+      
+    },
+});
