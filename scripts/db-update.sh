a5a826a45c2362d03a41f010f4a4b2628f9727d0
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-06-23 23:09:52 -0300

    correção no script scripts/db-update.sh

--- scripts/db-update.sh
+++ scripts/db-update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CDIR=$( pwd )
+cd $DIR
 
 if [[ $1 ]]; then
     DOMAIN=$1;
@@ -23,3 +26,5 @@ DISABLE_PLUGINS=1 MAPASCULTURAIS_CONFIG_FILE=$CONFIG HTTP_HOST=$DOMAIN REQUEST_M
 
 ## depois executa incluindo os plugins
 MAPASCULTURAIS_CONFIG_FILE=$CONFIG HTTP_HOST=$DOMAIN REQUEST_METHOD='CLI' REMOTE_ADDR='127.0.0.1' REQUEST_URI='/' SERVER_NAME=127.0.0.1 SERVER_PORT="8000" php ../src/tools/apply-updates.php $SAVE_LOG
+
+cd $CDIR
