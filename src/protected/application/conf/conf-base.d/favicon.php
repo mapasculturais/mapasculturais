65d57d23a5a0aace07c2d9dc455743636968f16c
LimaSystem <wolegario49@gmail.com>
2023-06-27 11:01:14 -0300

    ajusta os favicon do ambiente

--- src/protected/application/conf/conf-base.d/favicon.php
+++ src/protected/application/conf/conf-base.d/favicon.php
@@ -1,9 +1,9 @@
 <?php
 
 return[
-    'favicon.image16x16' => env('FAVICON_IMAGE', 'img/favicon-16x16.png'),
-    'favicon.image32x32' => env('FAVICON_IMAGE', 'img/favicon-32x32.png'),
-    'favicon.imagesvg' => env('FAVICON_IMAGE', 'img/favicon_32px_geraL-SVG.svg'),
-    'favicon.imageico' => env('FAVICON_IMAGE', 'img/favicon-mapas-generico.ico'),
-    'favicon.imagexml' => env('FAVICON_IMAGE', 'img/browserconfig.xml'),
+    'favicon.ico' => env('FAVICON_IMAGE', 'img/favicon.ico'),
+    'favicon.svg' => env('FAVICON_IMAGE', 'img/favicon.svg'),
+    'favicon.180' => env('FAVICON_IMAGE', 'img/favicon-180x180.png'),
+    'favicon.192' => env('FAVICON_IMAGE', 'img/favicon-192x192.png'),
+    'favicon.512' => env('FAVICON_IMAGE', 'img/favicon-512x512.png'),
 ];
