8e71e904b103928ebd2ea731db551b7b9bef207c
LimaSystem <wolegario49@gmail.com>
2023-06-27 10:52:34 -0300

    Ajusta o carregamento dos favicon no ambiente

--- src/protected/application/conf/conf-base.d/favicon.php
+++ src/protected/application/conf/conf-base.d/favicon.php
@@ -3,4 +3,7 @@
 return[
     'favicon.image16x16' => env('FAVICON_IMAGE', 'img/favicon-16x16.png'),
     'favicon.image32x32' => env('FAVICON_IMAGE', 'img/favicon-32x32.png'),
+    'favicon.imagesvg' => env('FAVICON_IMAGE', 'img/favicon_32px_geraL-SVG.svg'),
+    'favicon.imageico' => env('FAVICON_IMAGE', 'img/favicon-mapas-generico.ico'),
+    'favicon.imagexml' => env('FAVICON_IMAGE', 'img/browserconfig.xml'),
 ];
