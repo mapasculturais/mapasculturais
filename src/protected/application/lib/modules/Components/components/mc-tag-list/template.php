830fea341d8b910f8868c75c888fcae82cd17779
robedj <ed.wassouf@gmail.com>
2023-06-27 10:26:00 -0400

    Ajustes no mc-tag-list com classe para view e classe para edit Ref.: #2867

--- src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
+++ src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
@@ -5,10 +5,10 @@
  */
 ?>
 <div class="mc-tag-list">
-    <ul class="mc-tag-list__tagList">
-        <li :class="[classes, 'mc-tag-list__tagList--tag']" v-for="tag in tags"> 
-            {{this.labels ? this.labels[tag] : tag}}
-            <mc-icon v-if="editable" name="delete" @click="remove(tag)"></mc-icon>             
-        </li>
-    </ul>
+  <ul class="mc-tag-list__tagList">
+    <li v-for="tag in tags" :class="[classes, {'mc-tag-list__tagList--view': !editable, 'mc-tag-list__tagList--tag': editable}]">
+      <span>{{ this.labels ? this.labels[tag] : tag }}</span>
+      <mc-icon v-if="editable" name="delete" @click="remove(tag)"></mc-icon>
+    </li>
+  </ul>
 </div>
