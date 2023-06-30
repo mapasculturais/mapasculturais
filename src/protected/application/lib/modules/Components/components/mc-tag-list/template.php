2706a65779dbb475f9a1e051789451ca4adc86f2
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-06-30 11:54:11 -0300

    Refatoração da estilização do componente mc-tag-list pro padrão BEM (Ref.: #2871)

--- src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
+++ src/protected/application/lib/modules/Components/components/mc-tag-list/template.php
@@ -6,7 +6,7 @@
 ?>
 <div class="mc-tag-list">
   <ul class="mc-tag-list__tagList">
-    <li v-for="tag in tags" :class="[classes, {'mc-tag-list__tagList--view': !editable, 'mc-tag-list__tagList--tag': editable}]">
+    <li v-for="tag in tags" class="mc-tag-list__tag" :class="[classes, {'mc-tag-list__tag--editable': editable}]">
       <span>{{ this.labels ? this.labels[tag] : tag }}</span>
       <mc-icon v-if="editable" name="delete" @click="remove(tag)"></mc-icon>
     </li>
