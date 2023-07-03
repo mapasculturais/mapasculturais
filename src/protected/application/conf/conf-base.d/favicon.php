dcc0afeaccfe3870109aeb4999fa7906fa2a36f5
LimaSystem <wolegario49@gmail.com>
2023-07-03 17:51:42 -0300

    Define prefixo destintos nas configurações ENV dos favicons

--- src/protected/application/conf/conf-base.d/favicon.php
+++ src/protected/application/conf/conf-base.d/favicon.php
@@ -1,9 +1,9 @@
 <?php
 
 return[
-    'favicon.ico' => env('FAVICON_IMAGE', 'img/favicon.ico'),
-    'favicon.svg' => env('FAVICON_IMAGE', 'img/favicon.svg'),
-    'favicon.180' => env('FAVICON_IMAGE', 'img/favicon-180x180.png'),
-    'favicon.192' => env('FAVICON_IMAGE', 'img/favicon-192x192.png'),
-    'favicon.512' => env('FAVICON_IMAGE', 'img/favicon-512x512.png'),
+    'favicon.ico' => env('FAVICON_IMAGE_ICO', 'img/favicon.ico'),
+    'favicon.svg' => env('FAVICON_IMAGE_SVG', 'img/favicon.svg'),
+    'favicon.180' => env('FAVICON_IMAGE_180', 'img/favicon-180x180.png'),
+    'favicon.192' => env('FAVICON_IMAGE_192', 'img/favicon-192x192.png'),
+    'favicon.512' => env('FAVICON_IMAGE_512', 'img/favicon-512x512.png'),
 ];
