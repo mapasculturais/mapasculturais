fc0f0cfa35b1a03c99d3084c72ff86e4d2f21957
erleibiazzio <erleibiazzio@gmail.com>
2023-07-03 10:26:22 -0300

    Corrige exibição do nome da fase de avaliação na listagem de avaliações

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-evaluation/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-evaluation/template.php
@@ -12,10 +12,9 @@ use MapasCulturais\i;
     <div v-for="item in phases">
         <div class="card-evaluation">
             <div class="card_evaluation__content">
-                <h3 class="card-evaluation__content--title">{{item.opportunity.name}}</h3>
+                <h3 class="card-evaluation__content--title">{{item.name}}</h3>
                 <div class="card-evaluation__content--items">
                     <div class="phase">
-                        <div class="phase__title"><label class="phase__title--title"><?= i::__('Fase') ?>: </label><span class="item">{{item.name}}</span></div>
                         <div class="phase__title"><label class="phase__title--title"><?= i::__('Tipo') ?>: </label><span class="item">{{evaluationTypes[item.type]}}</span></div>
                     </div>
                     <div class="period"><label class="period__label"> <?= i::__('PERÍODO DE AVALIAÇÃO') ?>: </label><span class="period__content">{{item.evaluationFrom.date('numeric year')}} <?= i::__('até') ?> {{item.evaluationTo.date('numeric year')}} as {{item.evaluationFrom.time('long year')}}</span></div>
