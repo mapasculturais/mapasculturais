5528f4edad28671a1695563e619c9361f8b338ee
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-07-03 16:49:56 -0300

    correção das configurações

--- config/middlewares.php
+++ config/middlewares.php
@@ -1,8 +1,5 @@
 <?php 
 
 return [
-    'slim.middlewares' => 
-        env('APP_MODE', 'production') == 'production' ? 
-            [] :
-            [new \MapasCulturais\Middlewares\ExecutionTime(true, false)]
+    'slim.middlewares' => []
 ];
