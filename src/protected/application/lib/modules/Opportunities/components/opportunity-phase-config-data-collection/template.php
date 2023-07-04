6bde187d0f61ca0c9c9e4867903575c7eeb7bfaa
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-07-04 15:28:06 -0300

    Ajustes na estilização nas configurações de fases (Ref.: no issue)

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-config-data-collection/template.php
@@ -15,37 +15,36 @@ $this->import('
 ');
 ?>
     <div class="opportunity-data-collection grid-12">
-        <div class="opportunity-data-collection__horizontal-line col-12 "></div>
-        <entity-field :entity="phase" prop="registrationFrom" :autosave="300" :min="minDate?._date" :max="phase.registrationTo?._date" classes="col-6 sm:col-12"></entity-field>
-        <entity-field :entity="phase" prop="registrationTo" :autosave="300" :min="phase.registrationFrom?._date" :max="maxDate?._date" classes="col-6 sm:col-12"></entity-field>
-        <div class="col-12 grid-12">
-            <mc-link :entity="phase" route='formBuilder' class="config-phase__info-button button--primary button col-6" icon="external" right-icon>
-              <?= i::__("Configurar formulário") ?>
-            </mc-link>
-        </div>
-        <div class="opportunity-data-collection__category col-12">
-            <div v-if="phase.isFirstPhase">
-                <opportunity-category :entity="phase"></opportunity-category>
+        <div class="grid-12 col-12 opportunity-data-collection__section">        
+            <entity-field :entity="phase" prop="registrationFrom" :autosave="300" :min="minDate?._date" :max="phase.registrationTo?._date" classes="col-6 sm:col-12"></entity-field>
+            <entity-field :entity="phase" prop="registrationTo" :autosave="300" :min="phase.registrationFrom?._date" :max="maxDate?._date" classes="col-6 sm:col-12"></entity-field>
+
+            <div class="col-12 grid-12">
+                <mc-link :entity="phase" route='formBuilder' class="config-phase__info-button button--primary button col-6" icon="external" right-icon>
+                    <?= i::__("Configurar formulário") ?>
+                </mc-link>
             </div>
-            <!-- <div class="opportunity-data-collection__vertical-line"></div> -->
-            <div class="opportunity-data-collection__registration">
-                <entity-field :entity="phase" prop="registrationLimit" :autosave="300" classes="col-3"></entity-field>
-                <entity-field :entity="phase" prop="registrationLimitPerOwner" :autosave="300" classes="col-3"></entity-field>
+
+            <div class="opportunity-data-collection__category col-12">
+                <opportunity-category v-if="phase.isFirstPhase" :entity="phase"></opportunity-category>
+                
+                <div class="opportunity-data-collection__registration grid-12">
+                    <h4 class="bold col-12"><?= i::__("Limites na inscrição") ?></h4>
+                    <entity-field :entity="phase" prop="registrationLimit" :autosave="300" classes="col-12"></entity-field>
+                    <entity-field :entity="phase" prop="registrationLimitPerOwner" :autosave="300" classes="col-12"></entity-field>
+                </div>
             </div>
         </div>
 
-        <div class="col-12 grid-12">
-            <mc-link :entity="phase" route='formBuilder' class="config-phase__info-button button--primary button col-6" icon="external" right-icon>
-              <?= i::__("Configurar formulário") ?>
-            </mc-link>
-        </div>
         <div class="col-12 sm:col-12">
             <opportunity-enable-claim :entity="phase"></opportunity-enable-claim>
         </div>
+
         <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
             <div class="opportunity-data-collection__horizontal-line col-12 "></div>
             <opportunity-phase-publish-date-config :phase="phase" :phases="phases" hide-description hide-button></opportunity-phase-publish-date-config>
         </template>
+
         <div class="opportunity-data-collection__delete col-12" v-if="!phase.isLastPhase && !phase.isFirstPhase">
             <mc-confirm-button message="Confirma a execução da ação?" @confirm="deletePhase($event, phase, index)">
                 <template #button="modal">
