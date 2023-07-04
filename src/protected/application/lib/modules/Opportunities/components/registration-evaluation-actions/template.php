d77d7c0aef9cbd6a9c4b75aff90dcbfb2d6ef484
erleibiazzio <erleibiazzio@gmail.com>
2023-07-04 10:19:46 -0300

    Adiciona ícones nos botões de ação na tela da avaliação Ref.:#2880

--- src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/template.php
+++ src/protected/application/lib/modules/Opportunities/components/registration-evaluation-actions/template.php
@@ -8,6 +8,7 @@ use MapasCulturais\i;
 
 $this->import('
     v1-embed-tool
+    mc-icon
 ');
 ?>
 <div class="registration-evaluation-actions__buttons" v-if="evaluationRegistrationList">
@@ -19,16 +20,25 @@ $this->import('
             <button class="button button--primary" @click="save()"> <?= i::__('Salvar e continuar depois') ?> </button>
         </div>
         <div class="col-12" v-if="showActions(registration, 'send')">
-            <button class="button button--primary" @click="send(registration)"> <?= i::__('Enviar avaliação') ?> </button>
+            <button class="button button--primary button--icon" @click="send(registration)"> 
+                <?= i::__('Enviar avaliação') ?> 
+                <mc-icon name="upload"></mc-icon>
+            </button>
         </div>
         <div class="col-12" v-if="showActions(registration, 'reopen')">
             <button class="button button--primary" @click="reopen(registration)"> <?= i::__('Reabrir avaliação') ?> </button>
         </div>
         <div class="col-6">
-            <button v-if="firstRegistration?.registrationid != registration.id" class="button button--primary-outline" @click="previous()"> <?= i::__('Anterior') ?> </button>
+            <button v-if="firstRegistration?.registrationid != registration.id" class="button button--primary-outline button--icon" @click="previous()"> 
+                <mc-icon name="arrow-left-ios"></mc-icon>
+                <?= i::__('Anterior') ?> 
+            </button>
         </div>
         <div class="col-6">
-            <button v-if="lastRegistration?.registrationid != registration.id" class="button button--primary-outline" @click="next()"> <?= i::__('Próximo') ?> </button>
+            <button v-if="lastRegistration?.registrationid != registration.id" class="button button--primary-outline button--icon" @click="next()"> 
+                <?= i::__('Próximo') ?> 
+                <mc-icon name="arrow-right-ios"></mc-icon>
+            </button>
         </div>
     </div>
 </div>
