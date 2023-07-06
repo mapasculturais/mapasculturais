65c34bfd6bd311e576ac68841486e83548c9eb62
erleibiazzio <erleibiazzio@gmail.com>
2023-07-06 08:57:23 -0300

    Cria componente para aplicar as avaliações no método de avaliação documental Ref.: #2884

---
+++ src/protected/application/lib/modules/EvaluationMethodDocumentary/components/evaluation-method-documentary--apply/template.php
@@ -0,0 +1,54 @@
+<?php
+
+use MapasCulturais\i;
+
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+$this->import('
+    mc-modal
+');
+?>
+
+<mc-modal :title="modalTitle" classes="create-modal create-app" button-label="<?php i::_e('Aplicar avaliações') ?>">
+
+    <template #default>
+        <div class="col-12">
+            <div class="col-6">
+                <label><?php i::_e('Avaliação') ?></label> <br>
+                <select v-model="applyData.from">
+                    <option value="all"><?php i::_e('Todos') ?></option>
+                    <option v-for="item in consolidatedResults" :value="item.evaluation">{{valueToString(item.evaluation)}} ({{item.num}} <?php i::_e('Inscrições') ?>)</option>
+                </select>
+            </div>
+
+            <div class="col-6">
+                <label><?php i::_e('Status') ?></label> <br>
+                <select v-model="applyData.to">
+                    <option v-for="item in statusList" :value="item.status">{{item.label}}</option>
+                </select>
+            </div>
+        </div>
+        <br>
+        <div class="col-12">
+            <label>
+                <?php i::_e('Status') ?>
+                <input type="checkbox" v-model="applyAll">
+            </label>
+            <small><?php i::_e('Deixando desmarcado, o status será aplicado somente às inscrições com status pendente')?></small>
+        </div>
+    </template>
+
+    <template #actions="modal">
+        <div class="col-12">
+            <div class="col-6">
+                <button class="button button--primary" @click="apply(modal)"><?php i::_e('Aplicar') ?></button>
+            </div>
+            <div class="col-6">
+                <button class="button button--text button--text-del " @click="modal.close()"><?php i::_e('Cancelar') ?></button>
+            </div>
+        </div>
+    </template>
+</mc-modal>
