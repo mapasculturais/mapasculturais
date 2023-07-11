fcad2ab6d96952bf07c2fd5b0307fbf2d63cabe7
robedj <ed.wassouf@gmail.com>
2023-07-11 17:35:32 -0400

    Ajusta estilo do stepper para acompanhamento de inscrição Ref.: #2895

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phases-timeline/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phases-timeline/template.php
@@ -17,9 +17,9 @@ $this->import('
             <div v-if="item.isFirstPhase" class="item__content--title"> <?= i::__('Fase de inscrições') ?> </div>
             <div v-if="!item.isFirstPhase" class="item__content--title"> {{item.name}} </div>
             <div v-if="!item.isLastPhase" class="item__content--description">
-                <?= i::__('de') ?> <span v-if="dateFrom(item)">{{dateFrom(item)}}</span>
+                <h5 class="semibold"><?= i::__('de') ?> <span v-if="dateFrom(item)">{{dateFrom(item)}}</span>
                 <?= i::__('a') ?> <span v-if="dateTo(item)">{{dateTo(item)}}</span>
-                <?= i::__('às') ?> <span v-if="hour(item)">{{hour(item)}}</span>
+                <?= i::__('às') ?> <span v-if="hour(item)">{{hour(item)}}</span></h5>
             </div>
             <div v-if="item.isLastPhase" class="item__content--description">
                 <span v-if="item.publishTimestamp">
