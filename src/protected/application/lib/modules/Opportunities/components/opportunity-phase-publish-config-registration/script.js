623886061faddbbd5d12e18bd82a5a7b2ab027a9
robedj <ed.wassouf@gmail.com>
2023-07-11 17:37:01 -0400

    ajuste estilo do componente opportunity-phase-publish-config-registration Ref.: #2890

---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/script.js
@@ -0,0 +1,74 @@
+app.component('opportunity-phase-publish-config-registration' , {
+    template: $TEMPLATES['opportunity-phase-publish-config-registration'],
+
+    setup() {
+        const text = Utils.getTexts('opportunity-phase-publish-config-registration');
+        return { text };
+    },
+
+    props: {
+        phase: {
+            type: Entity,
+            required: true
+        },
+        phases: {
+            type: Array,
+            required: true
+        },
+        hideButton: {
+            type: Boolean,
+            default: false
+        },
+        hideDatepicker: {
+            type: Boolean,
+            default: false
+        },
+        hideCheckbox: {
+            type: Boolean,
+            default: false
+        },
+        hideDescription: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        minDate () {
+            return this.phase.evaluationTo?._date || this.phase.registrationTo?._date;
+        },
+        maxDate () {
+            if(!this.phase.isLastPhase) {
+                return this.lastPhase?.publishTimestamp?._date;
+            }
+        },
+        firstPhase() {
+            const firstPhase = this.phases[0];
+            if (firstPhase.isFirstPhase) {
+                return firstPhase;
+            }
+        },
+        lastPhase() {
+            const lastPhase = this.phases[this.phases.length - 1];
+            if (lastPhase.isLastPhase) {
+                return lastPhase;
+            }
+        },
+        isPublished() {
+            return this.firstPhase.status > 0;
+        },
+    },
+
+    methods: {
+        publishRegistration () {
+            this.phase.POST('publishRegistrations', this.phase).then(item => {
+                this.phase.populate(item);
+            });
+        },
+        unpublishRegistration () {
+            this.phase.POST('unpublishRegistrations', this.phase).then(item => {
+                this.phase.populate(item);
+            });
+        }
+    }
+});
---
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/template.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    entity-field
+    mc-confirm-button
+');
+?>
+<div class="col-12">
+    <div class="grid-12 opportunity-phase-publish-config-registration">
+        <div v-if="phase.publishedRegistrations" class="published">
+            <div class="col-4">
+                <mc-confirm-button :message="text('despublicar')" @confirm="unpublishRegistration()">
+                    <template #button="modal">
+                        <button class="button button--text button--text-danger" @click="modal.open()">
+                            <?= i::__("Despublicar") ?>
+                        </button>
+                    </template>
+                </mc-confirm-button>
+            </div>
+            <div v-if="!!phase.publishTimestamp" class="col-4">
+                <h5>{{ msgPublishDate }}</h5>
+            </div>
+        </div>
+
+        <div v-if="!phase.publishedRegistrations" class="grid-12 col-12 notPublished">
+            <entity-field v-if="!hideDatepicker" :entity="phase" prop="publishTimestamp" :autosave="300" :min="minDate" :max="maxDate" classes="col-4"></entity-field>
+            <div class="opportunity-phase-publish-config-registration__left col-12">
+
+                <div v-if="hideDatepicker && phase.publishTimestamp" class="col-4 msgpub-date">
+                    <h5 v-if="phase.autoPublish && hideCheckbox">
+                        <?= sprintf(
+                            i::__("O resultado será publicado automaticamente no dia %s às %s"),
+                            "{{phase.publishTimestamp.date('2-digit year')}}",
+                            "{{phase.publishTimestamp.time('2-digit')}}"
+                        ) ?>
+                    </h5>
+                    <h5 v-else>
+                        <?= sprintf(
+                            i::__("O resultado será publicado no dia %s às %s"),
+                            "{{phase.publishTimestamp.date('2-digit year')}}",
+                            "{{phase.publishTimestamp.time('2-digit')}}"
+                        ) ?>
+                    </h5>
+                </div>
+                <div v-else-if="hideCheckbox && phase.autoPublish" class="msg-auto-pub col-4">
+                    <h5><?= i::__('O resultado será publicado automaticamente') ?></h5>
+                </div>
+                <div v-else class="col-4">
+                    <h5><?= i::__("A publicação do resultado é opcional.") ?></h5>
+                </div>
+            </div>
+            <div class="col-12 grid-12">
+
+                <entity-field v-if="!hideCheckbox" :entity="phase" prop="autoPublish" type="checkbox" :autosave="300" :disabled="!phase.publishTimestamp" hideRequired classes="col-4"></entity-field>
+
+                <div v-if="!hideButton && firstPhase.status > 0" class="col-4 opportunity-phase-publish-config-registration__button">
+                    <mc-confirm-button :message="text('confirmar_publicacao')" @confirm="publishRegistration()">
+                        <template #button="modal">
+                            <button class="button button--primary button--large" @click="modal.open()">
+                                <?= i::__("Publicar Resultados") ?>
+                            </button>
+                        </template>
+                    </mc-confirm-button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
