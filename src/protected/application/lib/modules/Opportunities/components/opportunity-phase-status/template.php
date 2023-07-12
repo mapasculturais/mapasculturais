5685e9cab07d032eb595140dd5e5f8ccc150f580
robedj <ed.wassouf@gmail.com>
2023-07-12 17:39:37 -0400

    Renomeia componente opportunity-phase-list-data-collection para opportynity-phase-status Ref.: #2874

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
@@ -11,7 +11,7 @@ $this->import('
 ')
 ?>
 <mc-card>
-    <div class="grid-12 opportunity-phase-list-data-collection">
+    <div class="grid-12 opportunity-phase-status">
         <div v-if="entity.summary?.registrations" class="col-12">
             <h3><?php i::_e("Status das inscrições") ?></h3>
             <p v-if="entity.summary.registrations"><?= i::__("Quantidade de inscrições:") ?> <strong>{{entity.summary.registrations}}</strong><strong> <?= i::__('inscrições') ?></strong></p>
@@ -19,16 +19,17 @@ $this->import('
             <p v-if="entity.summary?.Pending"><?= i::__("Quantidade de inscrições <strong>pendentes</strong>:") ?> <strong>{{entity.summary.Pending}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
             <p v-if="entity.summary?.Draft"><?= i::__("Quantidade de inscrições <strong>rascunho</strong>:") ?> <strong>{{entity.summary.Draft}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
         </div>
-        <div class="col-12 opportunity-phase-list-data-collection_action--center">
-            <mc-link :entity="entity" class="opportunity-phase-list-data-collection_action--button" icon="external" route="registrations" right-icon>
-              <?= i::__("Lista de inscrições da fase") ?>
-              <!-- Refatorar  -->
+        <div class="col-12 opportunity-phase-status_action--center">
+        <opportunity-phase-publish-config-registration v-if="entity.isLastPhase" :phase="entity" :phases="phases" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
+            
+            <mc-link :entity="entity" class="opportunity-phase-status_action--button" route="registrations" right-icon>
+              <?= i::__("Acessar lista de pessoas inscritas") ?>
             </mc-link>
         </div>
 
         <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
             <div class="config-phase__line col-12"></div>
-            <opportunity-phase-publish-config-registration :phase="entity" :phases="phases" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
+            <opportunity-phase-publish-config-registration v-if="!entity.isLastPhase" :phase="entity" :phases="phases" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
         </template>
     </div>
 </mc-card>
