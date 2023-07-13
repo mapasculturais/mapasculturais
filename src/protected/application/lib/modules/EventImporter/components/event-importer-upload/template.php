e85ec341830ad48bcfdd131293afc27b30755f69
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-07-13 00:42:40 -0300

    Ajusta e estiliza o componente <event-importer-upload> (Ref.: #2901)

--- src/protected/application/lib/modules/EventImporter/components/event-importer-upload/template.php
+++ src/protected/application/lib/modules/EventImporter/components/event-importer-upload/template.php
@@ -12,21 +12,56 @@ $this->import('
 ');
 ?>
 
-<mc-modal :title="modalTitle" button-label="<?php i::_e('Importar Evento') ?>">
-    <template #default>
-        <p><?php i::_e('Para importar eventos, faça upload da planilha de modelo preechida') ?></p>
+<div :class="['event-importer-upload', classes]">
+    <div class="grid-12 v-center">
+        <div class="col-3 sm:col-12">    
+            <mc-modal :title="modalTitle">
+                <template v-if="!loading" #default>
+                    <p><?php i::_e('Para importar eventos, faça upload da planilha de modelo preechida') ?></p>
+                    
+                    <div class="event-importer-upload__field">
+                        <p id="fileName">{{fileName}}</p>
+                        <label for="fileUpload">
+                            <small class="input-label">
+                                <?= i::__("Selecionar arquivo") ?> 
+                                <mc-icon name="add"></mc-icon>
+                            </small>
+                        </label>
+                        <input type="file" name="file" id="fileUpload" @change="setFile" ref="file">
+                    </div>
+                </template>
 
-        <form>
-            <input type="file" name="file" @change="setFile" ref="file">
-        </form>
-
-    </template>
-
-    <template #actions="modal">
-        <button><?php i::_e('Cancelar') ?></button>
-        <button @click="upload(modal)"><?php i::_e('Enviar') ?></button>
-    </template>
-</mc-modal>
-
-<a :href="csvUrl" class="button button--icon button--sm event__color"> <?= i::__('Baixar modelo CSV') ?></a>
-<a :href="xlsUrl" class="button button--icon button--sm event__color"> <?= i::__('Baixar modelo XLS') ?></a>
+                <template v-if="loading" #default>
+                    <p class="event-importer-upload__loading semibold">
+                        <?= i::__("Carregando") ?> <mc-icon name="loading"></mc-icon>
+                    </p>
+                </template>
+        
+                <template #button="modal">
+                    <button class="button button--primary-outline button--large" @click="modal.open()"><?php i::_e('Importar Evento') ?></button>
+                </template>
+        
+                <template #actions="modal">
+                    <div class="grid-12">
+                        <div class="col-6">
+                            <button class="button button--text button--large button--md" @click="cancel(modal)"><?php i::_e('Cancelar') ?></button>
+                        </div>
+                        <div class="col-6">
+                            <button class="button button--primary button--large button--md" @click="upload(modal)"><?php i::_e('Enviar') ?></button>
+                        </div>
+                    </div>
+                </template>
+            </mc-modal>
+        </div>
+        <div class="col-3 sm:col-12">    
+            <a :href="csvUrl" class="button button--icon button--large primary__color"> 
+                <?= i::__('Baixar modelo CSV') ?> <mc-icon name="download"></mc-icon>
+            </a>
+        </div>
+        <div class="col-3 sm:col-12">    
+            <a :href="xlsUrl" class="button button--icon button--large primary__color"> 
+                <?= i::__('Baixar modelo XLS') ?> <mc-icon name="download"></mc-icon>
+            </a>
+        </div>
+    </div>
+</div>
