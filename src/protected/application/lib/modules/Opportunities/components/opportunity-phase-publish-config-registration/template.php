dc1434b34f179b56e77f8a8bf0114c7d023ec2a2
robedj <ed.wassouf@gmail.com>
2023-07-14 16:09:58 -0400

    Ajustes no template do opportunity-phase-publish-config-registration Ref: #2784

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/template.php
@@ -13,8 +13,9 @@ $this->import('
     mc-link
 ');
 ?>
-<div class="col-12">
-    <div :class="[{'grid-12': !tab=='registration' && phase.isLastPhase}, 'opportunity-phase-publish-config-registration' ]">
+<div :class="[{'col-12': !phase.isLastPhase}, {'opportunity-phase-publish-config-registration--published': phase.publishedRegistrations},'opportunity-phase-publish-config-registration']">
+    
+    <div :class="[{'grid-12': !tab=='registration' && phase.isLastPhase}, 'opportunity-phase-publish-config-registration__content' ]">
         <div v-if="phase.publishedRegistrations" class="published">
             <div class="col-4">
                 <mc-confirm-button :message="text('despublicar')" @confirm="unpublishRegistration()">
@@ -30,31 +31,31 @@ $this->import('
         <div v-if="!phase.publishedRegistrations" :class="[{'opportunity-phase-publish-config-registration__lastphase': phase.isLastPhase}, {'col-12 grid-12': !phase.isLastPhase && tab!='registrations'}, 'notPublished']">
             <entity-field v-if="!hideDatepicker" :entity="phase" prop="publishTimestamp" :autosave="300" :min="minDate" :max="maxDate" classes="col-4"></entity-field>
             <div :class="[{'col-12 opportunity-phase-publish-config-registration__left': !phase.isLastPhase}, ]">
-
-                <div v-if="hideDatepicker && phase.publishTimestamp" class="msgpub-date" :class="[{'col-4': !phase.isLastPhase}]">
-                    <h5 v-if="phase.autoPublish && hideCheckbox">
-                        <?= sprintf(
-                            i::__("O resultado será publicado automaticamente no dia %s às %s"),
-                            "{{phase.publishTimestamp.date('2-digit year')}}",
-                            "{{phase.publishTimestamp.time('2-digit')}}"
-                        ) ?>
-                    </h5>
-                    <h5 v-else>
-                        <?= sprintf(
-                            i::__("O resultado será publicado no dia %s às %s"),
-                            "{{phase.publishTimestamp.date('2-digit year')}}",
-                            "{{phase.publishTimestamp.time('2-digit')}}"
-                        ) ?>
-                    </h5>
-                </div>
-                <div v-else-if="hideCheckbox && phase.autoPublish" class="msg-auto-pub col-4">
-                    <h5 class="semibold"><?= i::__('O resultado será publicado automaticamente') ?></h5>
-                </div>
-                <div v-else class="col-4">
-                    <h5 class="semibold"><?= i::__("A publicação do resultado é opcional.") ?></h5>
-                </div>
+                    <div v-if="hideDatepicker && phase.publishTimestamp" class="msgpub-date" :class="[{'col-4': !phase.isLastPhase}]">
+                        <p class="bold" v-if="phase.autoPublish && hideCheckbox">
+                            <?= sprintf(
+                                i::__("O resultado será publicado automaticamente no dia %s às %s"),
+                                "{{phase.publishTimestamp.date('2-digit year')}}",
+                                "{{phase.publishTimestamp.time('2-digit')}}"
+                            ) ?>
+                        </p>
+                        <p class="bold" v-else>
+                            <?= sprintf(
+                                i::__("O resultado será publicado no dia %s às %s"),
+                                "{{phase.publishTimestamp.date('2-digit year')}}",
+                                "{{phase.publishTimestamp.time('2-digit')}}"
+                            ) ?>
+                        </p>
+                    </div>
+                    <div v-else-if="hideCheckbox && phase.autoPublish" class="msg-auto-pub col-4">
+                        <p class="bold"><?= i::__('O resultado será publicado automaticamente') ?></p>
+                    </div>
+                    <div v-else class="col-4">
+                        <p class="bold"><?= i::__("A publicação do resultado é opcional.") ?></p>
+                    </div>
             </div>
-            <div class="opportunity-phase-publish-config-registration__unpublishedlast">
+         
+            <div class="opportunity-phase-publish-config-registration__unpublishedlast" :class="[{'col-12':!phase.isLastPhase}]">
 
                 <mc-link v-if="tab=='registrations'" :entity="phase" class="opportunity-phase-status_action--button" route="registrations" right-icon>
                     <?= i::__("Acessar lista de pessoas inscritas") ?>
