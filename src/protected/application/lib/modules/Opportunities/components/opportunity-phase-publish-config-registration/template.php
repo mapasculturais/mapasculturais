9825207bd027194348f57f23787cc43515711687
robedj <ed.wassouf@gmail.com>
2023-07-14 12:27:23 -0400

    Ajusta o componente opportunity-phase-status e opportunity-phase-publish-config-registration adicionando classes e verificações para exibição da publicação de resultados finais ref.: #2874

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-publish-config-registration/template.php
@@ -14,7 +14,7 @@ $this->import('
 ');
 ?>
 <div class="col-12">
-    <div class="grid-12 opportunity-phase-publish-config-registration">
+    <div :class="[{'grid-12': !tab=='registration' && phase.isLastPhase}, 'opportunity-phase-publish-config-registration' ]">
         <div v-if="phase.publishedRegistrations" class="published">
             <div class="col-4">
                 <mc-confirm-button :message="text('despublicar')" @confirm="unpublishRegistration()">
@@ -27,9 +27,9 @@ $this->import('
             </div>
         </div>
       
-        <div v-if="!phase.publishedRegistrations" :class="[{'opportunity-phase-publish-config-registration__lastphase': phase.isLastPhase}, {'grid-12': !phase.isLastPhase}, 'notPublished', 'col-12']">
+        <div v-if="!phase.publishedRegistrations" :class="[{'opportunity-phase-publish-config-registration__lastphase': phase.isLastPhase}, {'col-12 grid-12': !phase.isLastPhase && tab!='registrations'}, 'notPublished']">
             <entity-field v-if="!hideDatepicker" :entity="phase" prop="publishTimestamp" :autosave="300" :min="minDate" :max="maxDate" classes="col-4"></entity-field>
-            <div :class="[{'col-12 opportunity-phase-publish-config-registration__left': !phase.isLastPhase}]">
+            <div :class="[{'col-12 opportunity-phase-publish-config-registration__left': !phase.isLastPhase}, ]">
 
                 <div v-if="hideDatepicker && phase.publishTimestamp" class="msgpub-date" :class="[{'col-4': !phase.isLastPhase}]">
                     <h5 v-if="phase.autoPublish && hideCheckbox">
@@ -54,23 +54,24 @@ $this->import('
                     <h5 class="semibold"><?= i::__("A publicação do resultado é opcional.") ?></h5>
                 </div>
             </div>
-            <mc-link v-if="tab=='registrations'" :entity="phase" class="opportunity-phase-status_action--button" route="registrations" right-icon>
-                <?= i::__("Acessar lista de pessoas inscritas") ?>
-            </mc-link>
-            <div :class="[{'col-12 grid-12': !phase.isLastPhase}]">
-                            
-                <entity-field v-if="!hideCheckbox && firstPhase.status > 0" :entity="phase" prop="autoPublish" type="checkbox" :autosave="300" :disabled="!phase.publishTimestamp" hideRequired classes="col-4"></entity-field>
-                <div v-if="!hideButton" class="opportunity-phase-publish-config-registration__button" :class="{'col-4': !phase.isLastPhase}">
-                    <mc-confirm-button :message="text('confirmar_publicacao')" @confirm="publishRegistration()">
-                        <template #button="modal">
-                            <button :class="['button', 'button--primary', {'button-large': !phase.isLastPhase}, {'disabled': !firstPhase.status >0}, {'button--bg': phase.isLastPhase}]" @click="modal.open()">
-                                <?= i::__("Publicar Resultados") ?>
-                            </button>
-                        </template>
-                    </mc-confirm-button>
+            <div class="opportunity-phase-publish-config-registration__unpublishedlast">
+
+                <mc-link v-if="tab=='registrations'" :entity="phase" class="opportunity-phase-status_action--button" route="registrations" right-icon>
+                    <?= i::__("Acessar lista de pessoas inscritas") ?>
+                </mc-link>
+                <div :class="[{'col-12 grid-12': !phase.isLastPhase}, {'opportunity-phase-publish-config-registration__unpublishlist': phase.isLastPhase}]">
+                    <entity-field v-if="!hideCheckbox && firstPhase.status > 0" :entity="phase" prop="autoPublish" type="checkbox" :autosave="300" :disabled="!phase.publishTimestamp" hideRequired classes="col-4"></entity-field>
+                    <div v-if="!hideButton" class="opportunity-phase-publish-config-registration__button" :class="{'col-4': !phase.isLastPhase}">
+                        <mc-confirm-button :message="text('confirmar_publicacao')" @confirm="publishRegistration()">
+                            <template #button="modal">
+                                <button :class="['button', 'button--primary', {'button-large': !phase.isLastPhase}, {'disabled': !firstPhase.status >0}, {'button--bg': phase.isLastPhase}]" @click="modal.open()">
+                                    <?= i::__("Publicar Resultados") ?>
+                                </button>
+                            </template>
+                        </mc-confirm-button>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
