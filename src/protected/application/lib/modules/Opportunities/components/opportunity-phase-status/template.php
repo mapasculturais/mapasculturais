5467b09286d68fdbb35c9ea1f75e498e0ba0d3e4
robedj <ed.wassouf@gmail.com>
2023-07-25 09:55:32 -0400

    Oculta o checkbox na aba registrations Ref.: #2874

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
@@ -19,10 +19,9 @@ $this->import('
             <p v-if="entity.summary?.Pending"><?= i::__("Quantidade de inscrições <strong>pendentes</strong>:") ?> <strong>{{entity.summary.Pending}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
             <p v-if="entity.summary?.Draft"><?= i::__("Quantidade de inscrições <strong>rascunho</strong>:") ?> <strong>{{entity.summary.Draft}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
         </div>
-        <div :class="[{'col-12':  tab!='registrations' && phase.isLastPhase}, 'opportunity-phase-status_action--center']">
-             <opportunity-phase-publish-config-registration v-if="entity.isLastPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
-      
-            <mc-link v-if="tab!='registrations'" :entity="entity" class="opportunity-phase-status_action--button" route="registrations" right-icon>
+        <div :class="[{'col-12':  tab!='registrations' && entity.isFirstPhase}, 'opportunity-phase-status_action--center']">
+            <opportunity-phase-publish-config-registration v-if="entity.isLastPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
+            <mc-link v-if="tab=='registrations' && entity.isFirstPhase" :entity="entity" class="opportunity-phase-status_action--button" icon="external" route="registrations" right-icon>
                 <?= i::__("Acessar lista de pessoas inscritas") ?>
             </mc-link>
         </div>
