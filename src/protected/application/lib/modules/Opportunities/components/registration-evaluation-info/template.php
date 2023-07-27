ae551af2175b9975c234050f8546f7a9b6a3e211
LimaSystem <wolegario49@gmail.com>
2023-07-27 18:44:04 -0300

    Ajusta os textos referentes as avaliações Ref.:#2916

--- src/protected/application/lib/modules/Opportunities/components/registration-evaluation-info/template.php
+++ src/protected/application/lib/modules/Opportunities/components/registration-evaluation-info/template.php
@@ -10,17 +10,18 @@ $this->import('
     mc-icon
 ')
 ?>
-
 <div class="registration-evaluation-info">
     <section v-for="(info, index) in infos" class="registration-evaluation-info__section">
-        <div class="registration-evaluation-info__header">
-            <p v-if="index == 'general'" class="registration-evaluation-info__title semibold"><?= i::__('Informações gerais')?></p>
-            <p v-if="!index == 'general'" class="registration-evaluation-info__title semibold">{{index}}</p>
-            <h5 v-if="!activeItems[index]" class="registration-evaluation-info__toggle" @click="toggle(index)"><?= i::__('Exibir');?> <mc-icon name="arrowPoint-down"></mc-icon> </h5>
-            <h5 v-if="activeItems[index]" class="registration-evaluation-info__toggle" @click="toggle(index)"><?= i::__('Ocultar');?> <mc-icon name="arrowPoint-up"></mc-icon> </h5>
-        </div>
-        <div v-if="activeItems[index]" class="registration-evaluation-info__content">
-            <h6>{{info}}</h6>
+        <div v-if=" showInfo(index)">
+            <div class="registration-evaluation-info__header">
+                <p v-if="index == 'general'" class="registration-evaluation-info__title semibold"><?= i::__('Informações gerais')?></p>
+                <p v-if="index != 'general'" class="registration-evaluation-info__title semibold">{{index}}</p>
+                <h5 v-if="!activeItems[index]" class="registration-evaluation-info__toggle" @click="toggle(index)"><?= i::__('Exibir');?> <mc-icon name="arrowPoint-down"></mc-icon> </h5>
+                <h5 v-if="activeItems[index]" class="registration-evaluation-info__toggle" @click="toggle(index)"><?= i::__('Ocultar');?> <mc-icon name="arrowPoint-up"></mc-icon> </h5>
+            </div>
+            <div v-if="activeItems[index]" class="registration-evaluation-info__content">
+                <h6>{{info}}</h6>
+            </div>
         </div>
     </section>
 </div>
