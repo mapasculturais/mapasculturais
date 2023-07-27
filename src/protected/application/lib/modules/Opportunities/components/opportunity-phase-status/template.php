4658208b3f98f28dae99ad95db26de38bbddd428
robedj <ed.wassouf@gmail.com>
2023-07-27 16:06:46 -0400

    Ajustes no template do componente opportunity-phase-status para novo estilo do figma para a tela Ref.: #2918

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
@@ -12,23 +12,31 @@ $this->import('
 ?>
 <mc-card>
     <div :class="[{'grid-12': tab!='registrations'}, 'opportunity-phase-status']">
-        <div v-if="entity.summary?.registrations && !entity.isLastPhase" class="col-12">
-            <h4 class="bold"><?php i::_e("Status das inscrições") ?></h4>
-            <p v-if="entity.summary.registrations"><?= i::__("Quantidade de inscrições:") ?> <strong>{{entity.summary.registrations}}</strong><strong> <?= i::__('inscrições') ?></strong></p>
-            <p v-if="entity.summary?.sent"><?= i::__("Quantidade de inscrições <strong>enviadas</strong>:") ?> <strong>{{entity.summary.sent}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
-            <p v-if="entity.summary?.Pending"><?= i::__("Quantidade de inscrições <strong>pendentes</strong>:") ?> <strong>{{entity.summary.Pending}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
-            <p v-if="entity.summary?.Draft"><?= i::__("Quantidade de inscrições <strong>rascunho</strong>:") ?> <strong>{{entity.summary.Draft}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
+        <div v-if="entity.summary?.registrations && !entity.isLastPhase" class="col-12 grid-12">
+            <div class="opportunity-phase-status__box col-6">
+                <div class="opportunity-phase-status__status col-6">
+                    <h4 class="bold"><?php i::_e("Status das inscrições") ?></h4>
+                    <p v-if="entity.summary.registrations"><?= i::__("Quantidade de inscrições:") ?> <strong>{{entity.summary.registrations}}</strong><strong> <?= i::__('inscrições') ?></strong></p>
+                    <p v-if="entity.summary?.sent"><?= i::__("Quantidade de inscrições <strong>enviadas</strong>:") ?> <strong>{{entity.summary.sent}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
+                    <p v-if="entity.summary?.Pending"><?= i::__("Quantidade de inscrições <strong>pendentes</strong>:") ?> <strong>{{entity.summary.Pending}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
+                    <p v-if="entity.summary?.Draft"><?= i::__("Quantidade de inscrições <strong>rascunho</strong>:") ?> <strong>{{entity.summary.Draft}}</strong> <strong><?= i::__('inscrições') ?></strong></p>
+                </div>
+                <div class=" col-6 opportunity-phase-status__endbox">
+                  <h5 class="bold"><?= i::__("A lista de inscrições pode ser acessada utilizando o botão abaixo")?></h5>
+                    <mc-link v-if="tab=='registrations' && entity.isFirstPhase" :entity="entity" class="button button--primary button--icon" icon="external" route="registrations" right-icon >
+                        <h4 class="semibold"><?= i::__("Conferir lista de inscrições") ?></h4>
+                    </mc-link>
+                </div>
+       
+            </div>
         </div>
-        <div v-if="tab=='registrations' && entity.isFirstPhase" class="opportunity-phase-status__line col-12"></div>
         <div :class="[{'col-12':  tab!='registrations' && entity.isFirstPhase}, 'opportunity-phase-status_action--center col-12 grid-12']">
             <opportunity-phase-publish-config-registration v-if="entity.isLastPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox :class="col-12"></opportunity-phase-publish-config-registration>
-            <mc-link v-if="tab=='registrations' && entity.isFirstPhase" :entity="entity" class="opportunity-phase-status_action--button col-12" icon="external" route="registrations" right-icon >
-                <h3><?= i::__("Acessar lista de pessoas inscritas") ?></h3>
-            </mc-link>
+            
         </div>
 
         <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
-            <div class="config-phase__line col-12"></div>
+            <div class="opportunity-phase-status__line col-12"></div>
             <opportunity-phase-publish-config-registration v-if="!entity.isLastPhase && !entity.isFirstPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
         </template>
     </div>
