18bfeef76b0c7180ff0494cecbc64e6744f6307f
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-08-01 13:02:51 -0300

    Resolve erro ao alterar entidade vinculada na criação de oportunidades (Ref.: #2923)

--- src/protected/application/lib/modules/Entities/components/create-opportunity/script.js
+++ src/protected/application/lib/modules/Entities/components/create-opportunity/script.js
@@ -42,7 +42,43 @@ app.component('create-opportunity', {
 
             }
 
-        }
+        },
+        entityType(){
+            switch(this.entity.ownerEntity.__objectType) {
+                case 'project':
+                    return __('projeto', 'create-opportunity');
+                case 'event':
+                    return __('evento', 'create-opportunity');
+                case 'space':
+                    return __('espaço', 'create-opportunity');
+                case 'agent':
+                    return __('agente', 'create-opportunity');
+            }
+        },
+        entityColorClass() {
+            switch(this.entity.ownerEntity.__objectType) {
+                case 'project':
+                    return 'project__color';
+                case 'event':
+                    return 'event__color';
+                case 'space':
+                    return 'space__color';
+                case 'agent':
+                    return 'agent__color--dark';
+            }
+        },
+        entityColorBorder() {
+            switch(this.entity.ownerEntity.__objectType) {
+                case 'project':
+                    return 'project__border';
+                case 'event':
+                    return 'event__border';
+                case 'space':
+                    return 'space__border';
+                case 'agent':
+                    return 'agent__border--dark';
+            }
+        },
     },
 
     methods: {
@@ -72,6 +108,10 @@ app.component('create-opportunity', {
     },
     setEntity(Entity) {
         this.entity.ownerEntity = Entity;
+    },
+    resetEntity() {
+        this.entity.ownerEntity = null;
+        this.entityTypeSelected = null;
     },
 
     destroyEntity() {
