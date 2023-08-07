cb0b2bc45c714d16cf70b5d10f125d8116571274
robedj <ed.wassouf@gmail.com>
2023-08-07 15:36:22 -0400

    Ajustes no opportunity-subscribe-results para exibir as datas referente a fases na aba inscrições e resultados Ref.: #2919

--- src/protected/application/lib/modules/Opportunities/components/opportunity-subscribe-results/script.js
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-subscribe-results/script.js
@@ -37,5 +37,37 @@ app.component('opportunity-subscribe-results', {
     },
     
     methods: {
+        showPublishTimestamp(phase) {
+            const previousPhase = this.getPreviousPhase(phase);
+            const nextPhase = this.getNextPhase(phase);
+
+            if (phase.isLastPhase) {
+                return true;
+            } else if (phase.__objectType == 'opportunity' && nextPhase.__objectType != 'evaluationmethodconfiguration' && phase.publishTimestamp) {
+                return true;
+            } else if (phase.__objectType == 'evaluationmethodconfiguration' && previousPhase.__objectType == 'opportunity' && previousPhase.publishTimestamp) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        publishTimestamp(phase) {
+            if (phase.__objectType == 'opportunity') {
+                return phase.publishTimestamp;
+            } 
+            
+            if (phase.__objectType == 'evaluationmethodconfiguration') {
+                return phase.opportunity.publishTimestamp;
+            }
+        },
+        getPreviousPhase(phase) {
+            const index = this.phases.indexOf(phase);
+            return this.phases[index - 1];
+        },
+
+        getNextPhase(phase) {
+            const index = this.phases.indexOf(phase);
+            return this.phases[index + 1];
+        },
     },
 });
