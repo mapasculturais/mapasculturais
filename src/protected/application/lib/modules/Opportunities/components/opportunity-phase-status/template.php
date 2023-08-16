f6ce0f1e9eb4d80285433961ff99c35fb7e7996a
robedj <ed.wassouf@gmail.com>
2023-08-16 11:09:00 -0400

    Reftoração no componente opportunity-phase-status Ref.: #2951

--- src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
+++ src/protected/application/lib/modules/Opportunities/components/opportunity-phase-status/template.php
@@ -15,17 +15,15 @@ $this->import('
     <div :class="[{'grid-12': tab!='registrations'}, {'grid-12':entity.isLastPhase}, 'opportunity-phase-status']">
         <div v-if="entity.summary?.registrations && !entity.isLastPhase" class="col-12 grid-12">
             <opportunity-phase-list-registrations :entity="entity" :tab="tab"></opportunity-phase-list-registrations>
-            <opportunity-phase-publish-config-registration v-if="entity.isFirstPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox ></opportunity-phase-publish-config-registration>
-
         </div>
-        <div :class="[{'col-12':  tab!='registrations' && entity.isFirstPhase}, 'opportunity-phase-status_action--center col-12']">
-            <opportunity-phase-publish-config-registration v-if="entity.isLastPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox ></opportunity-phase-publish-config-registration>
-            
-        </div>
-
-        <div v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'" class="col-12">
-            <div v-if="!entity.isLastPhase" class="opportunity-phase-status__line col-12"></div>
-            <opportunity-phase-publish-config-registration v-if="!entity.isLastPhase && !entity.isFirstPhase" :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
-        </div>
+        <template v-if="nextPhase?.__objectType != 'evaluationmethodconfiguration'">
+            <div v-if="entity.isLastPhase" :class="['opportunity-phase-status_action--center col-12']">
+                <opportunity-phase-publish-config-registration  :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox ></opportunity-phase-publish-config-registration>
+            </div>
+            <div v-if="!entity.isLastPhase" class="col-12">
+                <div class="opportunity-phase-status__line col-12"></div>
+                <opportunity-phase-publish-config-registration  :phase="entity" :phases="phases" :tab="tab" hide-datepicker hide-checkbox></opportunity-phase-publish-config-registration>
+            </div>
+        </template>
     </div>
 </mc-card>
