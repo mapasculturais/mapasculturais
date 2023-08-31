c22c4350cca41342706851e0f485c72fc8c77347
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-08-31 16:41:31 -0300

    refatora middleware que faz log do tempo de execução das requisições

--- src/core/Middlewares/ExecutionTime.php
+++ src/core/Middlewares/ExecutionTime.php
@@ -1,93 +1,35 @@
 <?php
 namespace MapasCulturais\Middlewares;
-use \MapasCulturais\App;
 
-/**
- * Calculates the request execution time and outputs it to application log.
- *
- * If on the construction of this object you pass true to $print param the request execution time will be
- * appended to the end of the page as a html comment.
- *
- * If you want tha the execution time be visible in the page, pass false to the $html_comment constrictor param.
- *
- */
-class ExecutionTime extends \Slim\Middleware{
-    protected $time_start;
-    protected $html_comment;
-    protected $print;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
+use MapasCulturais\App;
 
-    /**
-     * Creates the middleware.
-     *
-     * @param boolean $print print the execution time to the html body
-     * @param boolean $html_comment print the execution time as a html comment
-     */
-    public function __construct($print = true, $html_comment = true) {
-        $this->time_start = microtime(true);
-        $this->html_comment = $html_comment;
-        $this->print = $print;
-    }
-
-    /**
-     * Executes the middleware.
-     */
-    public function call()
-    {
+class ExecutionTime {
+    public function __invoke(Request $request, RequestHandler $handler) {
         $app = App::i();
-        $METHOD = $app->request->getMethod();
-        $URI = $app->request->getResourceUri();
-        $udate = $this->udate('Y-m-d H:i:s.u');
-        // Get reference to application
-        $app->log->info('');
+
         $app->log->info('=========================================================================');
-        $app->log->info("{$METHOD} {$URI} - timestamp: {$udate}");
+        
+        $response = $handler->handle($request);
 
-        // Run inner middleware and application
-        $this->next->call();
-
-        // Capitalize response body
-        $res = $app->response;
-        $body = $res->body();
-
-        $time_end = microtime(true);
-
-        $execution_time = number_format($time_end - $this->time_start, 2);
+        $endTime = microtime(true);
+        $execution_time = number_format($endTime - $app->startTime, 3);
         $mem = memory_get_usage(true) / 1024 / 1024;
 
-        $log_string = "{$METHOD} {$URI} - executed in <strong>{$execution_time}</strong> seconds. (MEM: <strong>{$mem}MB</strong>)";
-        
-        if($this->print){
-            $append = $this->html_comment ?
-                    "<!-- $log_string -->":
-                    "<div class=\"log-execution-time\"> $log_string </div>";
+        $route = $app->request->route;
 
-            $body = preg_replace('#(</[ ]*html[ ]*>)#i', $append.'$1', $body);
+        $log = "{$route} - executed in {$execution_time} seconds. (MEM: {$mem}MB)";
 
-            $res->body($body);
-        }
-
-        $app->log->info(strip_tags($log_string));
+        $app->log->info($log);
         $app->log->info('=========================================================================');
-
+        
+        return $response;
     }
 
-    /**
-     *
-     * @param type $format
-     * @param type $utimestamp
-     *
-     * @see http://stackoverflow.com/questions/169428/php-datetime-microseconds-always-returns-0
-     *
-     * @return type
-     */
-    function udate($format, $utimestamp = null){
-        if (is_null($utimestamp))
-            $utimestamp = microtime(true);
-
-        $timestamp = floor($utimestamp);
-        $milliseconds = round(($utimestamp - $timestamp) * 1000000);
-
-        return date(preg_replace('`(?<!\\\\)u`', $milliseconds, $format), $timestamp);
+    function rutime($ru, $rus, $index) {
+        return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000))
+         -  ($rus["ru_$index.tv_sec"]*1000 + intval($rus["ru_$index.tv_usec"]/1000));
     }
 }
