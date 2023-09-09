394658ff0fe57f00db3d096ce577e4002c95b47e
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-09-08 21:06:05 -0300

    corrige doctine-cli

--- src/tools/cli-config.php
+++ src/tools/cli-config.php
@@ -1,8 +1,8 @@
 <?php
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
 require __DIR__ . '/../../public/bootstrap.php';
+
 $app = MapasCulturais\App::i();
 
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet([
-    'db' => $app->em->getConnection(),
-    'em' => $app->em
-]);
+return ConsoleRunner::createHelperSet($app->em);
--- src/tools/doctrine.php
+++ src/tools/doctrine.php
@@ -26,27 +26,6 @@ $_SERVER['SERVER_NAME'] = 'localhost';
 
 include_once __DIR__ . '/../../vendor/autoload.php';
 
-$configFile = getcwd() . DIRECTORY_SEPARATOR . 'cli-config.php';
+$helper_set =  require __DIR__ . '/cli-config.php';
 
-$helperSet = null;
-if (file_exists($configFile)) {
-    if ( ! is_readable($configFile)) {
-        trigger_error(
-            'Configuration file [' . $configFile . '] does not have read permission.', E_ERROR
-        );
-    }
-
-    require $configFile;
-
-    foreach ($GLOBALS as $helperSetCandidate) {
-        if ($helperSetCandidate instanceof \Symfony\Component\Console\Helper\HelperSet) {
-            $helperSet = $helperSetCandidate;
-            break;
-        }
-    }
-}
-
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet();
-
-
-\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helper_set);
