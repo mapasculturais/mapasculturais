d02601844d0e79c713408a419f33f89e82027d27
robedj <ed.wassouf@gmail.com>
2023-09-11 09:41:05 -0400

    Adiciona quebra de linga na single do projeto na descrição longa Ref.: #2976

--- src/protected/application/lib/modules/Entities/views/project/single.php
+++ src/protected/application/lib/modules/Entities/views/project/single.php
@@ -69,7 +69,8 @@ $this->breadcrumb = [
                             </div>
                             <div v-if="entity.longDescription!=null" class="col-12">
                                 <h2><?php i::_e('Descrição Detalhada'); ?></h2>
-                                <p>{{entity.longDescription}}</p>
+                                <p v-html="entity.longDescription" class="single-project__longdescription"></p>
+
                             </div>
                             <entity-files-list v-if="entity.files.downloads!= null" :entity="entity" classes="col-12" group="downloads" title="<?php i::esc_attr_e('Arquivos para download'); ?>"></entity-files-list>
                             <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Links'); ?>"></entity-links>
