5fc1604d074b678c31a5b55bdfedf532945e08ee
erleibiazzio <erleibiazzio@gmail.com>
2023-09-14 10:57:32 -0300

    Faz com que os usuarios que tenham alguma Role definida entre no processamento de cache

--- src/protected/application/lib/MapasCulturais/Traits/EntityPermissionCache.php
+++ src/protected/application/lib/MapasCulturais/Traits/EntityPermissionCache.php
@@ -108,6 +108,12 @@ trait EntityPermissionCache {
             if(method_exists($this, 'getExtraPermissionCacheUsers')){
                 $users = array_merge($users, $this->getExtraPermissionCacheUsers());
             }
+
+            if($roles = $app->repo("Role")->findAll()){
+                foreach($roles as $role){
+                    $users[] = $role->user;
+                }
+            }
         }
         $app->applyHookBoundTo($this, "{$this->hookPrefix}.permissionCacheUsers", [&$users]);
 
