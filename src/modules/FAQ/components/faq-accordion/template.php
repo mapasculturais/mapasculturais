f1f7360c1e98b443cc6c343baa57edf80f2fe156
robedj <ed.wassouf@gmail.com>
2023-09-20 16:13:16 -0400

    Adiciona dados na tela do FAQ Ref.: #2986

--- src/modules/FAQ/components/faq-accordion/template.php
+++ src/modules/FAQ/components/faq-accordion/template.php
@@ -8,17 +8,27 @@
 use MapasCulturais\i;
 ?>
 <div class="faq__accordion" id="accordionRegister">
-    <div class="faq-accordion__item">
-        <header @click="toggle()" class="faq-accordion__header">
-            <h5 class="bold">
-                <?= i::__('Como me inscrever em um edital ?') ?>
-            </h5>
-            <mc-icon :name="status ? 'arrowPoint-up':'arrowPoint-down'" class="primary__color"></mc-icon>
-        </header>
-        <div v-if="status" class="faq-accordion__content">
-            <div class="accordion-body">
-                <?= i::__('qualquer conteudo') ?>
+    <div v-for="section in data" class="faq-accordion__item">
+        <template v-if="section.contexts.length>0" v-for="context in section.contexts">
+            {{context.title}}
+            {{context.description}}
+            <div v-for="question in context.questions" class="faq-accordion__items">
+
+                <header @click="toggle()" class="faq-accordion__header">
+                    <h5 class="bold">
+                        {{question.question}}
+                    </h5>
+                    <mc-icon :name="status ? 'arrowPoint-up':'arrowPoint-down'" class="primary__color"></mc-icon>
+                </header>
+                <div v-if="status" class="faq-accordion__content">
+                    <div class="accordion-body">
+                        {{question.answer}}
+                        <br>
+                        {{question.tags.join(', ')}}
+                    </div>
+                </div>
             </div>
-        </div>
+        </template>
+
     </div>
 </div>
