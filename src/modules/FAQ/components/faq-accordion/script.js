fcb2d217865efd37de4abc5ca2b9d49fe9d13717
robedj <ed.wassouf@gmail.com>
2023-09-20 16:13:53 -0400

    Adiciona o faq no js do componente accordion Ref.: #2986

--- src/modules/FAQ/components/faq-accordion/script.js
+++ src/modules/FAQ/components/faq-accordion/script.js
@@ -1,23 +1,27 @@
 
 app.component('faq-accordion', {
     template: $TEMPLATES['faq-accordion'],
-
+    
+    
 
     props: {
         name: {
             type: String,
             default: '',
         },
+        
     },
 
     data() {
         return {
             status: false,
+            data: $MAPAS.faq,
         }
     },
     methods: {
         toggle() {
             this.status = this.status ? false : true;
-        }
+        },
+
     },
 });
