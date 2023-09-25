1cf5bd9138adb220ec86e83198549b186fa7d5f3
robedj <ed.wassouf@gmail.com>
2023-09-25 10:00:01 -0400

    Ajuste na section da pagina do faq Ref.: #2986

--- src/modules/FAQ/views/faq/section.php
+++ src/modules/FAQ/views/faq/section.php
@@ -12,14 +12,18 @@ $this->import('
 <a href="<?= $this->controller->createUrl('index'); ?>" class="primary__color faq__back"><mc-icon name="arrow-left-ios"></mc-icon><mc-title tag="p" class="bold"><?= i::__('Voltar') ?></mc-title></a>
 <div class="faq__content">
     <aside class="faq__aside">
-        <?php foreach ($faq as $section) : ?>
-            <a href="<?= $this->controller->createUrl('index', [$section->slug]) ?>" class="faq__btn <?= $section->slug == $active_section ? 'faq__btn--selected' : '' ?>">
-                <?= $section->title ?> <mc-icon name="arrow-right-ios"></mc-icon>
-            </a>
-        <?php endforeach; ?>
+        <div class="faq__btn-aside">
+            <?php foreach ($faq as $section) : ?>
+                <a href="<?= $this->controller->createUrl('index', [$section->slug]) ?>" class="faq__btn <?= $section->slug == $active_section ? 'faq__btn--selected' : '' ?>">
+                    <?= $section->title ?> <mc-icon name="arrow-right-ios"></mc-icon>
+                </a>
+            <?php endforeach; ?>
+        </div>
         <div class="field faq__sections">
             <select name="faq-sections">
-                <option data-default disabled selected hidden><p class="semibold"><?= i::__("NAVEGUE ENTRE AS CATEGORIAS"); ?></p></option>
+                <option data-default disabled selected hidden>
+                    <p class="semibold"><?= i::__("NAVEGUE ENTRE AS CATEGORIAS"); ?></p>
+                </option>
                 <?php foreach ($faq as $section) : ?>
                     <option value="<?= $this->controller->createUrl('index', [$section->slug]) ?>" class="faq__btn <?= $section->slug == $active_section ? 'faq__btn--selected' : '' ?>">
                         <?= $section->title ?> <mc-icon name="arrow-right-ios"></mc-icon>
