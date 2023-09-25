d72f954e0c948c0718ba1ad82a4f0523cfa5ffaa
robedj <ed.wassouf@gmail.com>
2023-09-25 09:46:44 -0400

    Refatoração para uso do mc-accordion no faq-accordion Ref.: #2986

--- src/modules/FAQ/components/faq-accordion/script.js
+++ src/modules/FAQ/components/faq-accordion/script.js
@@ -1,26 +1,31 @@
 
 app.component('faq-accordion', {
     template: $TEMPLATES['faq-accordion'],
-    
-    
+
+
 
     props: {
         name: {
             type: String,
             default: '',
         },
-        
+
     },
 
     data() {
         return {
-            status: false,
+            arrow: null,
+            status: [],
             data: $MAPAS.faq,
         }
     },
     methods: {
-        toggle() {
-            this.status = this.status ? false : true;
+        toggle(index) {
+            return this.status[index] = !this.status[index];
+        },
+        isOpen(index) {
+
+            return this.status[index];
         },
 
     },
--- src/modules/FAQ/components/faq-accordion/template.php
+++ src/modules/FAQ/components/faq-accordion/template.php
@@ -8,31 +8,31 @@
 use MapasCulturais\i;
 
 $this->import('
+    mc-accordion    
     mc-tag-list
     mc-title
 ');
 ?>
-<div class="faq-accordion" id="accordionRegister">
-    <div v-for="section in data" class="faq-accordion__item">
-        <template v-if="section.contexts.length>0" v-for="context in section.contexts">
-           <div class="faq-accordion__contexts">
-               <mc-title tag="h2" v-html="context.title" class="bold primary__color"></mc-title>
-               <div v-html="context.description" class="faq-accordion__context semibold"></div>
-            </div>
-    
-            <div v-for="question in context.questions" class="faq-accordion__items">
-                <header  @click="toggle()" class="faq-accordion__header">
-                    <mc-title tag="h3" class="bold faq-accordion__subtitle">{{question.question}}</mc-title>
-                    <mc-icon :name="status ? 'arrowPoint-up':'arrowPoint-down'" class="primary__color"></mc-icon>
-                </header>
-                <div  v-if="status" class="faq-accordion__content">
-                    <div class="far-accordion__list">
-                        <div v-html="question.answer" class="faq-accordion__response"></div>
-                        <mc-tag-list :tags="question.tags" classes="faq-accordion__tags"></mc-tag-list>
-                    </div>
-                </div>
-            </div>
-    </div>
+<template v-for="section in data" class="faq-accordion">
+    <template v-if="section.contexts.length > 0" v-for="context in section.contexts">
+        <div class="faq-accordion__contexts">
+            <mc-title tag="h2" v-html="context.title" class="bold primary__color"></mc-title>
+            <div v-html="context.description" class="faq-accordion__context semibold"></div>
+        </div>
+        <div class="faq-accordion__items">
+            <template v-for="question in context.questions">
+                <mc-accordion>
+                    <template #title>
+                        {{question.question}}
+                    </template>
+                    <template #content>
+                        <div class="far-accordion__list">
+                            <div v-html="question.answer" class="faq-accordion__response"></div>
+                            <mc-tag-list :tags="question.tags" classes="faq-accordion__tags"></mc-tag-list>
+                        </div>
+                    </template>
+                </mc-accordion>
+            </template>
+        </div>
     </template>
-
-</div>
+</template>
