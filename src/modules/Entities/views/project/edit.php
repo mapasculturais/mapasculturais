6a43a894f248271c7bbcf7e7572cf94c91f1c1cc
wolegario40@gmail.com <wolegario49@gmail.com>
2023-09-26 15:50:21 -0300

    Adiciona componentes mc-tabs na edição do projeto  Ref.:#2995

--- src/modules/Entities/views/project/edit.php
+++ src/modules/Entities/views/project/edit.php
@@ -27,6 +27,8 @@ $this->import('
     mc-breadcrumb
     mc-card
     mc-container
+    mc-tabs
+    mc-tab
 ');
 
 $label = $this->isRequestedEntityMine() ? i::__('Meus projetos') : i::__('Projetos');
@@ -40,93 +42,135 @@ $this->breadcrumb = [
 <div class="main-app">
     <mc-breadcrumb></mc-breadcrumb>
     <entity-header :entity="entity" editable></entity-header>
-    <mc-container>
-        <entity-status :entity="entity"></entity-status>
-        <mc-card class="feature">
-            <template #title>
-                <label><?php i::_e("Informações de Apresentação") ?></label>
-                <p><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários") ?></p>
-            </template>
-            <template #content>
-                <div class="left">
-                    <div class="grid-12 v-center">
-                        <entity-cover :entity="entity" classes="col-12"></entity-cover>
-                        <div class="col-3 sm:col-12">
-                            <entity-profile :entity="entity"></entity-profile>
-                        </div>
-                        <div class="col-9 sm:col-12">
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Nome do projeto") ?>" prop="name"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Tipo do projeto") ?>" prop="type"></entity-field>
+
+    <mc-tabs class="tabs">
+        <mc-tab label="<?= i::_e('Informações') ?>" slug="info">
+            <mc-container>
+                <entity-status :entity="entity"></entity-status>
+                <mc-card class="feature">
+                    <template #title>
+                        <label><?php i::_e("Informações de Apresentação") ?></label>
+                        <p><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários") ?></p>
+                    </template>
+                    <template #content>
+                        <div class="left">
+                            <div class="grid-12 v-center">
+                                <entity-cover :entity="entity" classes="col-12"></entity-cover>
+                                <div class="col-3 sm:col-12">
+                                    <entity-profile :entity="entity"></entity-profile>
+                                </div>
+                                <div class="col-9 sm:col-12">
+                                    <div class="grid-12">
+                                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Nome do projeto") ?>" prop="name"></entity-field>
+                                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Tipo do projeto") ?>" prop="type"></entity-field>
+                                    </div>
+                                </div>
+                                <entity-field :entity="entity" classes="col-12" prop="shortDescription"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Link para página ou site do projeto") ?>" prop="site"></entity-field>
                             </div>
                         </div>
-                        <entity-field :entity="entity" classes="col-12" prop="shortDescription"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e("Link para página ou site do projeto") ?>" prop="site"></entity-field>
-                    </div>
-                </div>
-                <div class="divider"></div>
-                <div class="right">
-                    <entity-parent-edit :entity="entity" classes="col-12" type="project" label="<?php i::esc_attr_e('Adicione a um projeto principal')?>"></entity-parent-edit>
-                    <entity-social-media :entity="entity" classes="col-12" editable></entity-social-media>
-                </div>
-            </template>
-        </mc-card>
-        <main>
-            <mc-card>
-                <template #title>
-                    <label><?php i::_e("Período de execução do projeto"); ?></label>
-                </template>
-                <template #content>
-                    <div class="grid-12">
-                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Data inicial') ?>" prop="startsOn"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Data Final') ?>" prop="endsOn"></entity-field>
-                    </div>
-                </template>
-            </mc-card>
-            <mc-card>
-                <template #title>
-                    <label><?php i::_e("Contatos do projeto"); ?></label>
-                </template>
-                <template #content>
-                    <div class="grid-12">
-                        <entity-field :entity="entity" classes="col-12" prop="emailPublico"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" prop="emailPrivado"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" prop="telefonePublico"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" label="Telefone privado 1" prop="telefone1"></entity-field>
-                        <entity-field :entity="entity" classes="col-12" label="Telefone privado 2" prop="telefone2"></entity-field>
-                    </div>
-                </template>
-            </mc-card>
-            <mc-card>
-                <template #title>
-                    <label><?php i::_e("Mais informações públicas"); ?></label>
-                    <p><?php i::_e("Os dados inseridos abaixo assim como as informações de apresentação também são exibidos publicamente"); ?></p>
-                </template>
-                <template #content>
-                    <div class="grid-12">
-                        <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Descrição') ?>" prop="longDescription"></entity-field>
-                        <entity-files-list :entity="entity" classes="col-12" group="downloads" title="<?= i::_e('Adicionar arquivos para download') ?>" editable></entity-files-list>
-                        <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>
-                        <entity-gallery-video :entity="entity" classes="col-12" title="<?php i::_e('Adicionar vídeos') ?>" editable></entity-gallery-video>
-                        <entity-gallery :entity="entity" classes="col-12" title="<?php i::_e('Adicionar fotos na galeria') ?>" editable></entity-gallery>
-                    </div>
-                </template>
-            </mc-card>
-        </main>
-        <aside>
-            <mc-card>
-                <template #content>
-                    <div class="grid-12">
-                        <entity-admins :entity="entity" classes="col-12" editable></entity-admins>
-                        <entity-terms :entity="entity" editable classes="col-12" taxonomy="tag" title="<?php i::_e('Tags') ?>" ></entity-terms>
-                        <entity-related-agents :entity="entity" classes="col-12" editable></entity-related-agents>
-                        <entity-owner :entity="entity" classes="col-12" title="<?php i::_e('Publicado por') ?>" editable></entity-owner>
-                    </div>
-                </template>
-            </mc-card>
-        </aside>
-    </mc-container>
-    <entity-actions :entity="entity" editable></entity-actions>
+                        <div class="divider"></div>
+                        <div class="right">
+                            <entity-parent-edit :entity="entity" classes="col-12" type="project" label="<?php i::esc_attr_e('Adicione a um projeto principal') ?>"></entity-parent-edit>
+                            <entity-social-media :entity="entity" classes="col-12" editable></entity-social-media>
+                        </div>
+                    </template>
+                </mc-card>
+                <main>
+                    <mc-card>
+                        <template #title>
+                            <label><?php i::_e("Período de execução do projeto"); ?></label>
+                        </template>
+                        <template #content>
+                            <div class="grid-12">
+                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Data inicial') ?>" prop="startsOn"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Data Final') ?>" prop="endsOn"></entity-field>
+                            </div>
+                        </template>
+                    </mc-card>
+                    <mc-card>
+                        <template #title>
+                            <label><?php i::_e("Contatos do projeto"); ?></label>
+                        </template>
+                        <template #content>
+                            <div class="grid-12">
+                                <entity-field :entity="entity" classes="col-12" prop="emailPublico"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" prop="emailPrivado"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" prop="telefonePublico"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" label="Telefone privado 1" prop="telefone1"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" label="Telefone privado 2" prop="telefone2"></entity-field>
+                            </div>
+                        </template>
+                    </mc-card>
+                    <mc-card>
+                        <template #title>
+                            <label><?php i::_e("Mais informações públicas"); ?></label>
+                            <p><?php i::_e("Os dados inseridos abaixo assim como as informações de apresentação também são exibidos publicamente"); ?></p>
+                        </template>
+                        <template #content>
+                            <div class="grid-12">
+                                <entity-field :entity="entity" classes="col-12" label="<?php i::_e('Descrição') ?>" prop="longDescription"></entity-field>
+                                <entity-files-list :entity="entity" classes="col-12" group="downloads" title="<?= i::_e('Adicionar arquivos para download') ?>" editable></entity-files-list>
+                                <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>
+                                <entity-gallery-video :entity="entity" classes="col-12" title="<?php i::_e('Adicionar vídeos') ?>" editable></entity-gallery-video>
+                                <entity-gallery :entity="entity" classes="col-12" title="<?php i::_e('Adicionar fotos na galeria') ?>" editable></entity-gallery>
+                            </div>
+                        </template>
+                    </mc-card>
+                </main>
+                <aside>
+                    <mc-card>
+                        <template #content>
+                            <div class="grid-12">
+                                <entity-admins :entity="entity" classes="col-12" editable></entity-admins>
+                                <entity-terms :entity="entity" editable classes="col-12" taxonomy="tag" title="<?php i::_e('Tags') ?>"></entity-terms>
+                                <entity-related-agents :entity="entity" classes="col-12" editable></entity-related-agents>
+                                <entity-owner :entity="entity" classes="col-12" title="<?php i::_e('Publicado por') ?>" editable></entity-owner>
+                            </div>
+                        </template>
+                    </mc-card>
+                </aside>
+            </mc-container>
+
+            <entity-actions :entity="entity" editable></entity-actions>
+        </mc-tab>
+
+        <mc-tab label="<?= i::_e('Visitação') ?>" slug="visit">
+            <mc-container>
+                <main>
+
+                </main>
+                <aside>
+
+                </aside>
+            </mc-container>
+            <entity-actions :entity="entity" editable></entity-actions>
+        </mc-tab>
+
+        <mc-tab label="<?= i::_e('Gestão') ?>" slug="maneger">
+            <mc-container>
+                <main>
+
+                </main>
+                <aside>
+
+                </aside>
+            </mc-container>
+            <entity-actions :entity="entity" editable></entity-actions>
+        </mc-tab>
+
+        <mc-tab label="<?= i::_e('Acervo') ?>" slug="collection">
+            <mc-container>
+                <main>
+
+                </main>
+                <aside>
+
+                </aside>
+            </mc-container>
+            <entity-actions :entity="entity" editable></entity-actions>
+        </mc-tab>
+    </mc-tabs>
+    
 </div>
 <confirm-before-exit :entity="entity"></confirm-before-exit>
-
