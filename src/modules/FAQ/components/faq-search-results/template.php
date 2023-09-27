34f58bb8a3b8b11f2326b7f5cde25bea3b551723
robedj <ed.wassouf@gmail.com>
2023-09-27 16:18:36 -0400

    Resolve exibição da imagem no faq Ref.: #2986

--- src/modules/FAQ/components/faq-search-results/template.php
+++ src/modules/FAQ/components/faq-search-results/template.php
@@ -12,12 +12,14 @@ $this->import('
     mc-title
 ');
 ?>
-<template v-for="section in results" class="faq-accordion">
+
+<h3 class="bold faq-accordion__results"><?= i::__('Resultados da pesquisa') ?> '{{global.faqSearch}}'</h3>
+<template v-if="results" v-for="section in results" class="faq-accordion">
     <div v-if="index" class="faq-accordion__contexts">
         <mc-title tag="h2" v-html="section.title" class="bold primary__color"></mc-title>
         <div v-html="section.description" class="faq-accordion__context semibold"></div>
-    </div> 
-   <template v-if="section.contexts.length > 0" v-for="context in section.contexts">
+    </div>
+    <template v-if="section.contexts.length > 0" v-for="context in section.contexts">
         <div class="faq-accordion__contexts">
             <mc-title tag="h2" v-html="context.title" class="bold primary__color"></mc-title>
             <div v-html="context.description" class="faq-accordion__context semibold"></div>
@@ -39,3 +41,8 @@ $this->import('
         </div>
     </template>
 </template>
+<div v-if="results==0" class="faq-accordion__noresults">
+    <img class="faq-accordion__image" src="<?php $this->asset('/img/unknown.png', true, true) ?>">
+    <label class="faq-accordion__msg bold"><?= i::__('Ops! nenhum resultado foi encontrado para sua busca') ?></label>
+    <a class="nounderline" href="<?= $this->controller->createUrl('index'); ?>"><label class="bold primary__color"><?= i::__('VOLTAR À AJUDA') ?></label></a>
+</div>
