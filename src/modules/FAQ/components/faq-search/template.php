bc15956556888ad0e55faf692c0b1f042ebf70fd
robedj <ed.wassouf@gmail.com>
2023-09-28 15:57:40 -0400

    ajusta busca do FAQ Ref.: #2986

--- src/modules/FAQ/components/faq-search/template.php
+++ src/modules/FAQ/components/faq-search/template.php
@@ -7,7 +7,7 @@
 use MapasCulturais\i;
 ?>
 
-<form class="faq-search" @submit.prevent>
+<form :class="['faq-search', {'faq-search--section' : section}]" @submit.prevent>
     <input v-model="global.faqSearch" @input="search()" type="text" placeholder="<?= i::__('Pesquise por palavra-chave') ?>" class="faq-search__input" />
     <button @click="search()" class="faq-search__button">
         <mc-icon name="search"></mc-icon>
