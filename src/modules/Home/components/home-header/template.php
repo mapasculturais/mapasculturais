c3e5a883901261b5fc4be6f1f1bbaff92b6ad851
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-09-29 14:50:17 -0300

    Adaptação do componente home-header para aceitar as novas configurações (Ref.: 2996)

--- src/modules/Home/components/home-header/template.php
+++ src/modules/Home/components/home-header/template.php
@@ -4,30 +4,34 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
-use MapasCulturais\i;
 $this->import('
     home-search
 ');
 
-$this->tex
 ?>
-<div class="home-header">
+<div :class="['home-header', {'home-header--withBanner' : banner}] ">
     <div class="home-header__content">
-        <div class="home-header__content--left">
-            <div class="content">
-                <label class="content__title">
-                    <?= $this->text('title', i::__('Bem-vinde ao Mapas Culturais')) ?>
-                </label>
-                <p class="content__description">
-                    <?= $this->text('description', i::__('O Mapas Culturais é uma ferramenta de gestão cultural, que garante a estruturação de Sistemas de Informações e Indicadores. A plataforma oferece soluções para o mapeamento colaborativo de agentes culturais, realização de todas as etapas de editais e fomentos, organização de uma agenda cultural e divulgação espaços culturais dos territórios.')) ?>
-                </p>
-            </div>
+
+        <div v-if="title || description" class="home-header__main">
+            <label v-if="title" class="home-header__title">
+                {{title}}
+            </label>
+            <p v-if="description" class="home-header__description">
+                {{description}}
+            </p>
         </div>
 
-        <div class="home-header__content--right">
-            <div class="img">
-                <img src="<?php $this->asset($app->config['module.home']['home-header']) ?>" />
-            </div>
+        <div v-if="banner" class="home-header__banner">
+            <a v-if="bannerLink" :href="bannerLink" :download="downloadableLink ? 'download' : null" :target="!downloadableLink ? '_blank' : null">
+                <img :src="banner" />
+            </a>
+            <img v-if="!bannerLink" :src="banner" />
+        </div>
+  
+    </div>
+    <div class="home-header__background">
+        <div class="img">
+            <img :src="background" />
         </div>
     </div>
     <!-- <home-search></home-search> -->
