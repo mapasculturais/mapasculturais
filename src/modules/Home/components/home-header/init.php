c7df97f344280890487a057f302f3ffa80f0215e
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-09-29 15:48:08 -0300

    Atualiza o init.php do home-header (Ref.: #2996)

--- src/modules/Home/components/home-header/init.php
+++ src/modules/Home/components/home-header/init.php
@@ -4,8 +4,10 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
+$background_asset = $app->config['homeHeader.background'] ? $app->config['homeHeader.background'] : $app->config['module.home']['home-header'];
+
 $this->jsObject['config']['homeHeader'] = [
-    'background' => $app->config['homeHeader.background'] ? $this->asset($app->config['homeHeader.background'], false) : $this->asset($app->config['module.home']['home-header']),
+    'background' => $this->asset($background_asset),
     'banner' => $app->config['homeHeader.banner'] ? $this->asset($app->config['homeHeader.banner'], false) : '',
     'bannerLink' => $app->config['homeHeader.bannerLink'],
     'downloadableLink' => $app->config['homeHeader.downloadableLink'],
