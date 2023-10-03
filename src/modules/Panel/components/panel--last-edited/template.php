d8f64859239825c179c920b43e128fba74b1ca99
robedj <ed.wassouf@gmail.com>
2023-10-03 15:55:16 -0400

    Ajusta panel--last-edited para uso do mc-title Ref.: #2935

--- src/modules/Panel/components/panel--last-edited/template.php
+++ src/modules/Panel/components/panel--last-edited/template.php
@@ -22,7 +22,7 @@ $this->import('
                 <slide v-for="entity in entities" :key="entity.id">
                     <panel--entity-card :key="entity.id" :entity="entity" class="card">
                         <template #title="{entity}">
-                            <p class="bold">{{entity.name}}</p>
+                            <mc-title size="small" tag="h4" :shortLength="0" :longLength="1000" class="bold">{{entity.name}}</mc-title>
                         </template>
                         <template #header-actions="{entity}">
                             <div :class="[entity.__objectType+'__background', 'card-actions--tag']">
