e2f052988f60a4ef2b9a81ca844f225732ccd59c
robedj <ed.wassouf@gmail.com>
2023-10-04 20:23:01 -0400

    Ajusta entity-owner para uso do mc-avatar Ref.: #2735

--- src/modules/Entities/components/entity-owner/template.php
+++ src/modules/Entities/components/entity-owner/template.php
@@ -8,32 +8,30 @@
 use MapasCulturais\i;
 
 $this->import('
+    mc-avatar
     mc-icon
     select-entity
 ');
 ?>
 <div v-if="entity != owner" class="entity-owner" :class="classes">
-        <h4 class="bold">{{title}}</h4>
-        <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
-            <div class="entity-owner__owner--img">
-                <img v-if="owner.files.avatar" class="profile" :src="owner.files?.avatar?.url">
-                <div v-else class="placeholder">
-                    <mc-icon name="agent-1"></mc-icon>
-                </div>
-            </div>
-            <div class="entity-owner__owner--name">
-                {{owner.name}}
-            </div>
-        </a>
-
-        <div v-if="editable" class="entity-owner__edit">
-            <select-entity :query="query" type="agent" @select="changeOwner($event)" permissions="" openside="up-right">
-                <template #button="{ toggle }">
-                    <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()">
-                        <mc-icon name="exchange"></mc-icon>
-                        <h4><?php i::_e('Alterar Propriedade') ?></h4>
-                    </a>
-                </template>
-            </select-entity>
+    <h4 class="bold">{{title}}</h4>
+    <a class="entity-owner__owner" :href="owner.singleUrl" :title="owner.shortDescription">
+        <div class="entity-owner__owner--img">
+            <mc-avatar :entity="owner" size="xsmall"></mc-avatar>
         </div>
+        <div class="entity-owner__owner--name">
+            {{owner.name}}
+        </div>
+    </a>
+
+    <div v-if="editable" class="entity-owner__edit">
+        <select-entity :query="query" type="agent" @select="changeOwner($event)" permissions="" openside="up-right">
+            <template #button="{ toggle }">
+                <a class="entity-owner__edit--btn" :class="this.entity.__objectType + '__color'" @click="toggle()">
+                    <mc-icon name="exchange"></mc-icon>
+                    <h4><?php i::_e('Alterar Propriedade') ?></h4>
+                </a>
+            </template>
+        </select-entity>
+    </div>
 </div>
