700d8de446b0f42f33c9a1f817e7d650914d4060
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-10-06 17:00:28 -0300

    Estilização do componente entity-date (Ref.: #3002)

--- src/modules/Entities/components/entity-data/template.php
+++ src/modules/Entities/components/entity-data/template.php
@@ -9,12 +9,23 @@ use MapasCulturais\i;
 ?>
 <div class="entity-data">
     <label class="entity-data__label">{{propertyLabel}}</label>
+
     <div v-if="propertyType == 'date'" class="entity-data__data">
         {{propertyData.date('2-digit year')}}
     </div>
+
     <div v-else-if="propertyType == 'datetime'" class="entity-data__data">
         {{propertyData.date('2-digit year')}} <?= i::__('às') ?> {{propertyData.time('numeric')}}
     </div>
+
+    <div v-else-if="propertyType == 'multiselect'">
+        <mc-tag-list classes="space__background" :tags="propertyData"></mc-tag-list> <!-- classes="primary__background" -->
+    </div>
+
+    <div v-else-if="propertyType == 'radio'" class="entity-data__data">
+        {{description.options[propertyData]}}
+    </div>
+
     <div v-else class="entity-data__data">
         {{propertyData}}
     </div>
