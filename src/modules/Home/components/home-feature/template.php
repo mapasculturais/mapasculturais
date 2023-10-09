0ee496d8467c374699477c712d6c102e7a2d35f8
robedj <ed.wassouf@gmail.com>
2023-10-09 16:04:43 -0400

    Ajusta estilo do componente home-feature Ref.: #2752

--- src/modules/Home/components/home-feature/template.php
+++ src/modules/Home/components/home-feature/template.php
@@ -11,6 +11,7 @@ $this->import('
     mc-loading
     mc-tab
 	mc-tabs
+    mc-title
 ');
 ?>
 <div v-if="enabledEntities()" class="home-feature">
@@ -24,12 +25,15 @@ $this->import('
     </div>
     <div class="home-feature__content">
         <div class="home-feature__content cards">
-            <mc-loading :condition="entities.length"></mc-loading>
+            <mc-loading :condition="loading"></mc-loading>
             <mc-tabs v-if="entities.length > 0">
                 <mc-tab label="<?= i::esc_attr__('Todos') ?>" slug="all">
                     <carousel :settings="settings" :breakpoints="breakpoints">
                         <slide v-for="entity in entities" :key="entity.__objectId">
                             <entity-card :entity="entity" tag="h4" portrait slice-description>
+                                <template #title>
+                                    <mc-title tag="h2" :shortLength="80">{{entity.name}}</mc-title>
+                                </template>
                                 <template #labels>
                                     <div :class="['entityType',  entity.__objectType+'__background']">
                                         <mc-icon :entity="entity"></mc-icon>
@@ -49,7 +53,10 @@ $this->import('
                 <mc-tab v-if="agents.length > 0" label="<?= i::esc_attr__('Agentes') ?>" slug="agents">
                     <carousel :settings="settings" :breakpoints="breakpoints">
                         <slide v-for="entity in agents" :key="entity.__objectId">
-                            <entity-card :entity="entity" tag="h4" slice-description>
+                            <entity-card :entity="entity" tag="h4" portrait slice-description>
+                                <template #title>
+                                    <mc-title tag="h2" :shortLength="80">{{entity.name}}</mc-title>
+                                </template>
                                 <template #labels>
                                     <div :class="['entityType',  entity.__objectType+'__background']">
                                         <mc-icon :entity="entity"></mc-icon>
@@ -69,7 +76,10 @@ $this->import('
                 <mc-tab v-if="spaces.length > 0" label="<?= i::esc_attr__('Espaços') ?>" slug="spaces">
                     <carousel :settings="settings" :breakpoints="breakpoints">
                         <slide v-for="entity in spaces" :key="entity.__objectId">
-                            <entity-card :entity="entity" tag="h4" slice-description>
+                            <entity-card :entity="entity" tag="h4" portrait slice-description>
+                                <template #title>
+                                    <mc-title tag="h2" :shortLength="80">{{entity.name}}</mc-title>
+                                </template>
                                 <template #labels>
                                     <div :class="['entityType',  entity.__objectType+'__background']">
                                         <mc-icon :entity="entity"></mc-icon>
@@ -89,7 +99,10 @@ $this->import('
                 <mc-tab v-if="projects.length > 0" label="<?= i::esc_attr__('Projetos') ?>" slug="projects">
                     <carousel :settings="settings" :breakpoints="breakpoints">
                         <slide v-for="entity in projects" :key="entity.__objectId">
-                            <entity-card :entity="entity" tag="h4" slice-description>
+                            <entity-card :entity="entity" tag="h4" portrait slice-description>
+                                <template #title>
+                                    <mc-title tag="h2" :shortLength="80">{{entity.name}}</mc-title>
+                                </template>
                                 <template #labels>
                                     <div :class="['entityType',  entity.__objectType+'__background']">
                                         <mc-icon :entity="entity"></mc-icon>
