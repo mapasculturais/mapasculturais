6e3d6446c5750f6f1edc415c407c0880868e113c
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-10-10 19:34:47 -0300

    Avisa que dado não foi informado quando estiver vazio (Ref.: #3002)

--- src/modules/Entities/components/entity-data/template.php
+++ src/modules/Entities/components/entity-data/template.php
@@ -14,26 +14,32 @@ $this->import('
 <div class="entity-data">
     <label class="entity-data__label">{{propertyLabel}}</label>
 
-    <div v-if="propertyType == 'date'" class="entity-data__data">
-        {{propertyData.date('2-digit year')}}
-    </div>
+    <template v-if="propertyData">        
+        <div v-if="propertyType == 'date'" class="entity-data__data">
+            {{propertyData.date('2-digit year')}}
+        </div>
+        
+        <div v-else-if="propertyType == 'datetime'" class="entity-data__data">
+            {{propertyData.date('2-digit year')}} <?= i::__('às') ?> {{propertyData.time('numeric')}}
+        </div>
+        
+        <div v-else-if="propertyType == 'multiselect'" :class="{'entity-data__data' : !propertyData}">
+            <mc-tag-list classes="space__background" :tags="propertyData"></mc-tag-list>
+        </div>
+        
+        <div v-else-if="propertyType == 'radio' || propertyType == 'select'" class="entity-data__data">
+            {{description.options[propertyData]}}
+        </div>
+        
+        <div v-else class="entity-data__data">
+            {{propertyData}}
+        </div>
+    </template>
 
-    <div v-else-if="propertyType == 'datetime'" class="entity-data__data">
-        {{propertyData.date('2-digit year')}} <?= i::__('às') ?> {{propertyData.time('numeric')}}
-    </div>
 
-    <div v-else-if="propertyType == 'multiselect'" :class="{'entity-data__data' : !propertyData}">
-        <mc-tag-list v-if="propertyData && propertyData.length > 0" classes="space__background" :tags="propertyData"></mc-tag-list> 
-        <small v-if="!propertyData">
+    <div v-if="!propertyData" class="entity-data__data">
+        <small v-if="!propertyData" class="bold">
             <?= i::__('Não informado') ?>
         </small>
-    </div>
-
-    <div v-else-if="propertyType == 'radio' || propertyType == 'select'" class="entity-data__data">
-        {{description.options[propertyData]}}
-    </div>
-
-    <div v-else class="entity-data__data">
-        {{propertyData}}
     </div>
 </div>
