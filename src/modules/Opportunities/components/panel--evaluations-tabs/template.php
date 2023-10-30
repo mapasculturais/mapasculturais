351ca3329a5124ca60500b2524e8426a9dcf5be6
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-10-30 12:45:38 -0300

    Ajustes no layout da tela Minhas Avaliações (Ref.: #2876)

--- src/modules/Opportunities/components/panel--evaluations-tabs/template.php
+++ src/modules/Opportunities/components/panel--evaluations-tabs/template.php
@@ -8,6 +8,7 @@ use MapasCulturais\i;
 
 $this->import('
     evaluation-card
+    mc-entities
     mc-tab
     mc-tabs
     mc-title
@@ -16,89 +17,66 @@ $this->import('
 
 <mc-tabs> <!-- :class="{'hasDrafts': this.totalDrafts>0}" @changed="changed($event)" -->
 
-    <mc-tab label="<?= i::_e('Avaliações') ?>" slug="evaluations">
-        <div class="panel-evaluations">
-            <div class="panel-evaluations__filter">
-                <form class="form" @submit="$event.preventDefault();"> <!-- @submit="entities.refresh(); $event.preventDefault();" -->
-                    <div class="search">
-                        <input type="text" class="input" /> <!-- v-model="entities.query['@keyword']" @input="entities.refresh();" -->
-                        <button class="button button--icon">
-                            <mc-icon name="search"></mc-icon>
-                        </button>
-                    </div>
+    <?php $this->applyComponentHook('tabs', 'begin'); ?>
+    <mc-tab label="<?= i::_e('Avaliações abertas e disponíveis') ?>" slug="openEvaluations" class="panel-evaluations">
+        <?php $this->applyComponentHook('tabs-openEvaluations', 'begin'); ?>
 
-                    <select class="order primary__border--solid"> <!-- v-model="query['@order']" @change="entities.refresh();" -->
-                        <option value="owner.name ASC"><?= i::__('ordem alfabética') ?></option>
-                        <option value="createTimestamp DESC"><?= i::__('mais recentes primeiro') ?></option>
-                        <option value="createTimestamp ASC"><?= i::__('mais antigas primeiro') ?></option>
-                    </select>
-                </form>
+        <form @submit="$event.preventDefault();" class="panel-evaluations__filter form"> <!-- @submit="entities.refresh(); $event.preventDefault();" -->
+            <?php $this->applyComponentHook('tabs-openEvaluations-filter', 'begin'); ?>
+            
+            <div class="search">
+                <input type="text" class="input" /> <!-- v-model="entities.query['@keyword']" @input="entities.refresh();" -->
+                <button class="button button--icon">
+                    <mc-icon name="search"></mc-icon>
+                </button>
             </div>
 
-            <div class="panel-evaluations__evaluations">
-                <mc-title tag="h4" :short-length="0" :long-length="100" size="medium" class="panel-evaluations__label bold">
-                    <?= i::__('Avaliações abertas e disponíveis') ?>
-                </mc-title>
-                                 
-                <div class="panel-evaluations__evaluations-cards">
-                    <!-- <evaluation-card></evaluation-card> -->
-                    <?= i::__('Você não tem avaliações disponíveis.') ?>
-                </div>
-            </div>   
+            <select class="order primary__border--solid"> <!-- v-model="query['@order']" @change="entities.refresh();" -->
+                <option value="owner.name ASC"><?= i::__('ordem alfabética') ?></option>
+                <option value="createTimestamp DESC"><?= i::__('mais recentes primeiro') ?></option>
+                <option value="createTimestamp ASC"><?= i::__('mais antigas primeiro') ?></option>
+            </select>
 
-            <div class="panel-evaluations__evaluations">
-                <mc-title tag="h4" :short-length="0" :long-length="100" size="medium" class="panel-evaluations__label bold">
-                    <?= i::__('Avaliações encerradas') ?>
-                </mc-title>
-                                 
-                <div class="panel-evaluations__evaluations-cards">
-                    <evaluation-card></evaluation-card>
-                    <!-- <?= i::__('Você ainda não tem avaliações encerradas.') ?> -->
-                </div>
-            </div>  
-        </div>
+            <?php $this->applyComponentHook('tabs-openEvaluations-filter', 'end'); ?>
+        </form>
+
+        <div class="panel-evaluations__cards">                   
+            <?php $this->applyComponentHook('tabs-openEvaluations-cards', 'begin'); ?>
+            <evaluation-card></evaluation-card>
+            <?php $this->applyComponentHook('tabs-openEvaluations-cards', 'end'); ?>
+        </div>  
+
+        <?php $this->applyComponentHook('tabs-openEvaluations', 'end'); ?>
     </mc-tab>
     
-    <mc-tab label="<?= i::_e('Pareceres') ?>" slug="opinions">
-        <div class="panel-evaluations">
-            <div class="panel-evaluations__filter">
-                <form class="form" @submit="$event.preventDefault();"> <!-- @submit="entities.refresh(); $event.preventDefault();" -->
-                    <div class="search">
-                        <input type="text" class="input" /> <!-- v-model="entities.query['@keyword']" @input="entities.refresh();" -->
-                        <button class="button button--icon"> 
-                            <mc-icon name="search"></mc-icon>
-                        </button>
-                    </div>
+    <mc-tab label="<?= i::_e('Avaliações encerradas') ?>" slug="closedEvaluations" class="panel-evaluations">
+        <form @submit="$event.preventDefault();" class="panel-evaluations__filter"> <!-- @submit="entities.refresh(); $event.preventDefault();" -->
+            <?php $this->applyComponentHook('tabs-closedEvaluations-filter', 'begin'); ?>
 
-                    <select class="order primary__border--solid"> <!-- v-model="query['@order']" @change="entities.refresh();" -->
-                        <option value="owner.name ASC"><?= i::__('ordem alfabética') ?></option>
-                        <option value="createTimestamp DESC"><?= i::__('mais recentes primeiro') ?></option>
-                        <option value="createTimestamp ASC"><?= i::__('mais antigas primeiro') ?></option>
-                    </select>
-                </form>
+            <div class="search">
+                <input type="text" class="input" /> <!-- v-model="entities.query['@keyword']" @input="entities.refresh();" -->
+                <button class="button button--icon"> 
+                    <mc-icon name="search"></mc-icon>
+                </button>
             </div>
 
-            <div class="panel-evaluations__evaluations">
-                <mc-title tag="h4" :short-length="0" :long-length="100" size="medium" class="panel-evaluations__label bold">
-                    <?= i::__('Pareceres abertas e disponíveis') ?>
-                </mc-title>
-                                 
-                <div class="panel-evaluations__evaluations-cards">
-                    <evaluation-card></evaluation-card>
-                    <!-- <?= i::__('Você não tem pareceres disponíveis.') ?> -->
-                </div>
-            </div>   
+            <select class="order primary__border--solid"> <!-- v-model="query['@order']" @change="entities.refresh();" -->
+                <option value="owner.name ASC"><?= i::__('ordem alfabética') ?></option>
+                <option value="createTimestamp DESC"><?= i::__('mais recentes primeiro') ?></option>
+                <option value="createTimestamp ASC"><?= i::__('mais antigas primeiro') ?></option>
+            </select>
 
-            <div class="panel-evaluations__evaluations">
-                <mc-title tag="h4" :short-length="0" :long-length="100" size="medium" class="panel-evaluations__label bold">
-                    <?= i::__('Pareceres encerradas') ?>
-                </mc-title>
-                                 
-                <div class="panel-evaluations__evaluations-cards">
-                    <!-- <evaluation-card></evaluation-card> -->
-                    <?= i::__('Você ainda não tem pareceres encerrados.') ?>
-                </div>
-            </div>
+            <?php $this->applyComponentHook('tabs-closedEvaluations-filter', 'end'); ?>
+        </form>
+
+        <!-- Reduzir divs -->
+        <div class="panel-evaluations__cards">                 
+            <?php $this->applyComponentHook('tabs-closedEvaluations-cards', 'begin'); ?>
+            <evaluation-card></evaluation-card>
+            <?php $this->applyComponentHook('tabs-closedEvaluations-cards', 'end'); ?>
+            <!-- <?= i::__('Você ainda não tem avaliações encerradas.') ?> -->
         </div>
     </mc-tab>
+    <?php $this->applyComponentHook('tabs', 'end'); ?>
+
 </mc-tabs>
