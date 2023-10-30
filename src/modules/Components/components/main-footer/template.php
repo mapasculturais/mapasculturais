90581184891e5ae94ab18417436726756e3b8a55
erleibiazzio <erleibiazzio@gmail.com>
2023-10-30 13:28:18 -0300

    Corrige fechamento da condicional que verifica se existe configuração definida para o módulo LGPD no main-footer

--- src/modules/Components/components/main-footer/template.php
+++ src/modules/Components/components/main-footer/template.php
@@ -106,8 +106,8 @@ $config = $app->config['social-media'];
                             <a href="<?= $app->createUrl('lgpd', 'view', [$slug]) ?>"><?= $cfg['title'] ?></a>
                         </li>
                     <?php endforeach ?>
+                <?php endif; ?>
                 </ul>
-            <?php endif; ?>
             <?php $this->applyTemplateHook("main-footer-links", "end")?>
         </div>
         <?php $this->applyTemplateHook("main-footer-links", "after")?>      
