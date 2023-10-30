369b96a5d83304846c4b4bdd03953953f5142b52
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-10-30 12:46:39 -0300

    W.I.P. => query para busca das avaliações abertas e encerradas (Ref.: #2876)

--- src/modules/Opportunities/components/panel--evaluations-tabs/init.php
+++ src/modules/Opportunities/components/panel--evaluations-tabs/init.php
@@ -1,1 +1,19 @@
 <?php
+
+$queryParams =  [
+    '@permissions' => 'evaluateRegistrations',
+    '@select' => 'name,parent.name,status,evaluationMethodConfiguration.{evaluationFrom,evaluationTo}', 
+    'status' => 'IN(1,-1)',
+];
+
+$queryOpportunities = new MapasCulturais\ApiQuery(MapasCulturais\Entities\Opportunity::class, $queryParams);
+eval(\psy\sh());
+
+$this->jsObject['panelEvaluationsTabs']['evaluations'] = $queryOpportunities->getFindResult();
+
+/* 
+    http://localhost/api/opportunity/find?
+    @select=name,parent.name,evaluationMethodConfiguration.{evaluationFrom,evaluationTo}&
+    @permissions=evaluateRegistrations&
+    status=IN(1,-1) 
+*/
