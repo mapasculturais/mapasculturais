536f22bfb4e35492356bccfff5e6cc94d8a503da
robedj <ed.wassouf@gmail.com>
2023-11-07 15:59:02 -0400

    Adicona slots para exibição do botão e select no entity-table Ref.: #3018 #3019

--- src/modules/Entities/components/entity-table/template.php
+++ src/modules/Entities/components/entity-table/template.php
@@ -19,5 +19,15 @@ $this->import('
         </li>
     </template>
 </ul>
-<EasyDataTable :headers="activeHeaders" table-class-name="entity-table__table" :body-row-class-name="customRowClassName"   :items="items" rows-per-page-message="<?= i::esc_attr__('linhas por página')?>">
+<EasyDataTable :headers="activeHeaders" table-class-name="entity-table__table" :body-row-class-name="customRowClassName"  :items="items" rows-per-page-message="<?= i::esc_attr__('linhas por página')?>">
+    <template #item-open="{id}">
+        <mc-link class="button button--primary" :params="id" route="registration/single"><?= i::esc_attr__('Conferir inscrição')?></mc-link>
+    </template>
+
+    <template #item-option="{option}">
+        <select v-model="option">
+            <option v-for="options in option" :value="options">{{options}}</option> 
+        </select>
+    </template>
+
 </EasyDataTable>
