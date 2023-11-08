a52cf5e16847ecb65c3c8f3ef3d8c83ff7ce286a
robedj <ed.wassouf@gmail.com>
2023-11-08 16:00:16 -0400

    Componente entity-table ajustes para exibição dos dados e checkbox ref.: #3018 #3019

--- src/modules/Entities/components/entity-table/template.php
+++ src/modules/Entities/components/entity-table/template.php
@@ -3,6 +3,7 @@
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
+
 use MapasCulturais\i;
 
 $this->import('
@@ -10,27 +11,67 @@ $this->import('
     mc-link
 ');
 ?>
-<ul>
-    <template  v-for="header in headers" :key="header.value">
-        <li v-if="!header.required">
-            <button type="button" @click="toggleColumn(header)">
-                {{header.text}}
-                
+<div class="entity-table">
+
+    <div class="opportunity-registration-table__filter">
+        <div class="opportunity-registration-table__search-key">
+            <input type="text" placeholder="<?= i::__('Busque pelo número de inscrição, status, parecer técnico?') ?>" class="opportunity-registration-table__search-input" />
+            <button @click="search()" class="opportunity-registration-table__search-button">
+                <mc-icon name="search"></mc-icon>
             </button>
-        </li>
-    </template>
-</ul>
-<EasyDataTable :headers="activeHeaders" table-class-name="entity-table__table" :body-row-class-name="customRowClassName"  :items="items" rows-per-page-message="<?= i::esc_attr__('linhas por página')?>">
-    <template #item-open="{id}">
-        <mc-link class="button button--primary" :params="id" route="registration/single"><?= i::esc_attr__('Conferir inscrição')?></mc-link>
-    </template>
+        </div>
+        <div class="opportunity-registration-table__search-fields">
+            <h4 class="bold"><?= i::__('Filtrar:') ?></h4>
+            <div class="field"><input type="number" /></div>
+            <div class="field">
+                <select>
+                    <option value=""><span><?= i::__('Operador:') ?></span></option>
+                </select>
+            </div>
+            <div class="field"><input type="number"></div>
+            <div class="field">
+                <select>
+                    <option value=""><span><?= i::__('Status de inscrição:') ?></span></option>
+                </select>
+            </div>
+            <div class="field">
+                <select>
+                    <option value=""><span><?= i::__('Exequibilidade (R$)') ?></span></option>
+                </select>
+            </div>
+        </div>
+        <!-- <mc-tag-list class="opportunity-registration-table__taglists"></mc-tag-list> -->
+        <div class="field opportunity-registration-table__select-tag">
+            <select>
+                <option value=""><span><?= i::__('Colunas habilitadas na tabela:') ?></span></option>
+            </select>
+        </div>
+    </div>
+    <ul>
+        <template v-for="header in headers" :key="header.value">
+            <li v-if="!header.required">
+                <button type="button" @click="toggleColumn(header)">
+                    {{header.text}}
 
-    <template #item-option="{option}">
-        <select v-model="option">
-            <option v-for="options in option" :value="options">{{options}}</option> 
-        </select>
-    </template>
-<slot name="buton">
+                </button>
+            </li>
+        </template>
+    </ul>
+    <!-- :body-row-class-name="customRowClassName" -->
+    {{itemsSelected}}
+    <EasyDataTable :headers="activeHeaders" table-class-name="entity-table__table" v-model:items-selected="itemsSelected" :items="items" rows-per-page-message="<?= i::esc_attr__('linhas por página') ?>">
+        <template v-for="slot in activeSlots" #[slot]="item">
+            <slot :name="slot" v-bind="item" ></slot>
+        </template>
 
-</slot>
-</EasyDataTable>
+        <template #item-open="{id}">
+            <mc-link class="button button--primary" :params="[id]" route="registration/single"><?= i::esc_attr__('Conferir inscrição') ?></mc-link>
+        </template>
+
+        <template #item-option="{option}">
+            <select v-model="option">
+                <option v-for="options in option" :value="options">{{options}}</option>
+            </select>
+        </template>
+    </EasyDataTable>
+</div>
