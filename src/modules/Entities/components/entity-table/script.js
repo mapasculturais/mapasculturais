4a93ebe9f2ee7b00eb89b5c932fa437d34b272cc
robedj <ed.wassouf@gmail.com>
2023-11-09 15:52:04 -0400

    Construção dos filtros no entity-table ref.:  #3018 #3019

--- src/modules/Entities/components/entity-table/script.js
+++ src/modules/Entities/components/entity-table/script.js
@@ -7,61 +7,108 @@ app.component('entity-table', {
         const activeHeaders = Vue.ref(headers.filter(
             header => header.required
         ));
-        return { activeHeaders, hasSlot, activeSlots };
+        return { activeHeaders, optionalHeadersSelected: [], hasSlot, activeSlots };
     },
     data() {
+
         return {
             itemsSelected: Vue.ref([]),
-
         }
     },
-    // Consulta em qualquer API
-    // Passar a rota do endpoint?
-    // find - row processer? pega o retorno e transforma num entity.
-    // No caso da listagem de avaliação não poderá ser um entity (Exceção)
-    // passar pro componente 
-    // Inscrições e todos os outros usos pode
-    // se nao der certo usando entities, deve-se usar uma query ..via ajax.
-    // carregar mais ao invés da paginaçao na tabela.
+
     props: {
         headers: {
             type: Array,
             required: true
         },
+        // requiredColumns: {
+        //     type: String,
+        //     default: ''
+        // },
+        // optionalColumns: {
+        //     type: String,
+        //     default: ''
+        // },
+        // visibleColumns: {
+        //     type: String,
+        //     default: ''
+        // },
         items: {
             type: Array,
             required: true
         },
 
-        // statusClasses: {
-        //     type: Object,
-        //     default: {
-        //         '-10': 'row--trash',
-        //         '-2': 'row--archived',
-        //         '-9': 'row--disabled',
-        //         '0': 'row--draft',
-        //         '1': 'row--enabled row--sent',
-        //         '2': 'row--invalid',
-        //         '3': 'row--notapproved',
-        //         '8': 'row--waitlist',
-        //         '10': 'row--approved',
-        //     }
-        // },
-
+        statusClasses: {
+            type: Object,
+            default: () => ({
+                '-10': 'row--trash',
+                '-2': 'row--archived',
+                '-9': 'row--disabled',
+                0: 'row--draft',
+                1: 'row--enabled row--sent',
+                2: 'row--invalid',
+                3: 'row--notapproved',
+                8: 'row--waitlist',
+                10: 'row--approved',
+            })
+        },
     },
+
     computed: {
         activeColumns() {
             return this.activeHeaders.map(header => (header.value));
         },
+
+        visibleColumns() {
+            return this.activeHeaders.reduce((columns, header) => {
+                if (!header.required) {
+                    columns.push(header);
+                }
+                return columns;
+            }, []);
+        },
+        selectedColumns() {
+            return this.activeHeaders.reduce((columns, header) => {
+                if (!header.required) {
+                    columns.push(header.text);
+                }
+                return columns;
+            }, []);
+        },
+
+        optionalHeaders() {
+            return this.headers.reduce((columns, header) => {
+                if (!header.required) {
+                    columns.push(header);
+                }
+                return columns;
+            }, []);
+            // return this.headers.filter(header => !header.required);
+        },
     },
 
     methods: {
-        // customRowClassName() {
-        //     return this.statusClasses[items.status];
-        // },
+        removeFromColumns(tag) {
+            if (this.activeColumns.includes(tag)) {
+                this.activeHeaders = this.activeHeaders.filter(header => header.value != tag);
+            }
+        },
+        addInColumns(tag) {
+            console.log(this.optionalHeaders.includes(tag));
+            console.log(this.optionalHeaders.find(header =>header.value == tag));
+            if (!this.activeColumns.includes(tag)) {
+                this.activeHeaders.push(this.optionalHeaders.find(header =>header.value == tag));
+            }
+        },
+        customRowClassName(item) {
+            return this.statusClasses[item.status];
+
+        },
+
         isActive(column) {
             return this.activeColumns.includes(column.value);
         },
+
         toggleColumn(column) {
             if (this.isActive(column)) {
                 this.activeHeaders = this.activeHeaders.filter(header => header.value != column.value)
@@ -69,5 +116,14 @@ app.component('entity-table', {
                 this.activeHeaders.push(column)
             }
         },
+        // toggleSelection(items) {
+        //     console.log(itemsSelected);
+        //     const index = this.itemsSelected.findIndex(itemsSelected=> itemsSelectedid === item.id);
+        //     if (index !== -1) {
+        //         this.itemsSelected.push(item);
+        //     } else {
+        //         this.itemsSelected.splice(index, 1);
+        //     }
+        // }
     },
 });
