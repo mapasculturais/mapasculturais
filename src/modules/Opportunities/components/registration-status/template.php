372e5b0dad3d511000255fc472354cca0fb01106
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-11-14 23:47:31 -0300

    adiciona botão para exibir detalhamento da avaliação técnica (refs #2941)

--- src/modules/Opportunities/components/registration-status/template.php
+++ src/modules/Opportunities/components/registration-status/template.php
@@ -5,9 +5,27 @@
  */
 
 use MapasCulturais\i;
+
+$this->import('
+    mc-icon
+    registration-results
+');
 ?>
 
-<div class="registration-status registration-status--selected">
-    <mc-icon name="dot"></mc-icon>
-    <h5 class="bold">Selecionado</h5>
+<div class="opportunity-phases-timeline__box">
+    <label class="semibold opportunity-phases-timeline__label"><?= i::__('RESULTADO DA FASE')?></label>
+    <div class="opportunity-phases-timeline__status">
+        <mc-icon name="circle" :class="verifyState(registration)"></mc-icon>
+        <p v-if="registration.status == 10"><?= i::__('Inscrição selecionada') ?></p>
+        <p v-if="registration.status == 8"><?= i::__('Inscrição suplente') ?></p>
+        <p v-if="registration.status == 3"><?= i::__('Inscrição não selecionada') ?></p>
+        <p v-if="registration.status == 2"><?= i::__('Inscrição inválida') ?></p>
+        <p v-if="registration.status == 0"><?= i::__('Inscrição não enviada') ?></p>
+    </div>
+
+    <template v-if="phase.type == 'technical'">
+        <div><?= i::__('Pontuação:') ?> <strong>{{formatNote(registration.consolidatedResult)}}</strong></div>
+    
+        <registration-results v-if="phase.publishEvaluationDetails" :registration="registration" :phase="phase"></registration-results>
+    </template>
 </div>
