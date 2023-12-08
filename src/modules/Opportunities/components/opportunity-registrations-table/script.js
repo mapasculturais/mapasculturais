b4c0840809f3a453b856110deafacb5754b872d1
erleibiazzio <erleibiazzio@gmail.com>
2023-12-08 12:28:03 -0300

    Criam método que passa o status no formato inteiro para a string que representa o mesmo Ref.: #3018

--- src/modules/Opportunities/components/opportunity-registrations-table/script.js
+++ src/modules/Opportunities/components/opportunity-registrations-table/script.js
@@ -41,6 +41,9 @@ app.component('opportunity-registrations-table', {
     },
 
     methods: {
+        statusToString (status) {
+            return this.text(status)
+        },
         isFuture() {
             const phase = this.phase;
             if (phase.isLastPhase) {
