59c04081fc7e2b89074b283914fda58511b78b69
erleibiazzio <erleibiazzio@gmail.com>
2023-12-11 16:42:25 -0300

    Volta o componente v1-embed-tool que carrega as inscrições

--- src/modules/Opportunities/components/opportunity-registrations-table/template.php
+++ src/modules/Opportunities/components/opportunity-registrations-table/template.php
@@ -45,15 +45,7 @@ $entity = $this->controller->requestedEntity;
                 </h5>
             </div>
         <div class="col-12"> 
-            <entity-table :select="select" :query="query" type="registration" limit="50" :headers="headers" required="number" visible="number,status,agent" >
-                <template #filters="{entities, filters}">
-                   
-                </template>
-
-                <template #status="item">
-                   {{statusToString(item.status)}}
-                </template>
-            </entity-table>
+            <v1-embed-tool route="registrationmanager" :id="phase.id" min-height="600px"></v1-embed-tool>
         </div>
     </template>
 </div>
