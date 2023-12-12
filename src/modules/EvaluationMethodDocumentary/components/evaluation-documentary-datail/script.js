4ee6adc5f0182a918854225d3cf919ef9ae1f883
erleibiazzio <erleibiazzio@gmail.com>
2023-12-12 12:26:51 -0300

    Cria componente evaluation-documentary-datail que exibe o parecer da avaliação técnica

---
+++ src/modules/EvaluationMethodDocumentary/components/evaluation-documentary-datail/script.js
@@ -0,0 +1,20 @@
+app.component('evaluation-documentary-datail', {
+    template: $TEMPLATES['evaluation-documentary-datail'],
+
+    props: {
+        registration: {
+            type: Entity,
+            required: true
+        }
+    },
+
+    data() {
+        return {}
+    },
+
+    computed: {},
+
+    methods: {
+      
+    },
+});
