31fff22edec6103e74ed5b0b1f4eeff3c133c3cb
erleibiazzio <erleibiazzio@gmail.com>
2024-01-08 18:56:58 -0300

    Remove comentários e debugs desnecessários do componente de criação de usuário fake

--- src/modules/FakeAuthentication/components/fake-user-create/script.js
+++ src/modules/FakeAuthentication/components/fake-user-create/script.js
@@ -1,44 +1,10 @@
-/**
- * Vue Lifecycle
- * 1. setup
- * 2. beforeCreate
- * 3. created
- * 4. beforeMount
- * 5. mounted
- *
- * // sempre que há modificação nos dados
- *  - beforeUpdate
- *  - updated
- *
- * 6. beforeUnmount
- * 7. unmounted
- */
-
 app.component("fake-user-create", {
   template: $TEMPLATES["fake-user-create"],
 
   // define os eventos que este componente emite
   emits: ["create"],
 
-  props: {
-    /* 
-    textoLocalizado: {
-      type: String,
-
-      // nas propriedades não é possível utilizar o this.text
-      default: __("texto localizado", "fake-user-create"),
-    },
-
-  
-
-    lastname: {
-      type: String,
-    },
-
-    nickname: {
-      type: String,
-    }, */
-  },
+  props: { },
 
   setup(props, { slots }) {},
 
@@ -71,15 +37,7 @@ app.component("fake-user-create", {
     };
   },
 
-  computed: {
-    /*    compareDisplayName() {
-      return this.entity.name == this.displayName;
-    },
-
-    compareFullname() {
-      return this.entity.nomeCompleto == this.fullname;
-    }, */
-  },
+  computed: { },
 
   methods: {
     createEntity() {
@@ -97,13 +55,9 @@ app.component("fake-user-create", {
           .then((res) => res.json())
           .then((data) => {
             if (data?.error) {
-              console.log("setStatusError");
               this.error = JSON.stringify(data);
               this.messages.error(this.text("error"));
             } else {
-              console.log(data);
-              console.log("setStatusSuccess");
-              console.log(window);
               window.location = Utils.createUrl("painel", "index");
             }
           });
@@ -116,16 +70,10 @@ app.component("fake-user-create", {
       }
     },
     destroyEntity() {
-      // para o conteúdo da modal não sumir antes dela fechar
       setTimeout(() => (this.entity = null), 200);
     },
     iterationFields() {
       let skip = ["name", "email"];
-      /* Object.keys($DESCRIPTIONS.project).forEach((item) => {
-        if (!skip.includes(item) && $DESCRIPTIONS.project[item].required) {
-          this.fields.push(item);
-        }
-      }); */
     },
   },
 });
