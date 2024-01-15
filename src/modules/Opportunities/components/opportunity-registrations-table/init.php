64e39aaaf3911c4461b478df7350d2f221c65ec3
wolegario40@gmail.com <wolegario49@gmail.com>
2024-01-15 11:32:42 -0300

    envia o status para o javascript, para poder mostrar dentro do template Ref.: #3055

--- src/modules/Opportunities/components/opportunity-registrations-table/init.php
+++ src/modules/Opportunities/components/opportunity-registrations-table/init.php
@@ -1,1 +1,13 @@
 <?php
+
+use MapasCulturais\Entities\Registration;
+
+$registrations = Registration::getStatusesNames();
+
+foreach($registrations as $status => $status_name){
+    if(in_array($status,[0,1,2,3,8,10])){
+        $data[] = ["label" => $status_name, "value" => $status];
+    }
+}
+
+$this->jsObject['config']['opportunityRegistrationTable'] = $data;
