c0a6fb822da731d79ce8d0e03e213b7590939d6f
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-01-22 07:13:24 -0300

    Criação do componente mc-status (Ref.: mapasculturais/plugin-RegistrationPayments:#25)

---
+++ src/modules/Components/components/mc-status/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    mc-icon
+');
+?>
+
+<div :class="statusClass">
+    <mc-icon name="dot"></mc-icon>
+    {{statusName}}
+</div>
