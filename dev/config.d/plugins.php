3230055a8d0fed99b70930d469c7b7c4ffcfe9ce
Pedro Piva <pedro.piva@iterpec.com>
2024-01-24 07:37:07 -0300

    fix: melhorias e correcoes da tabela enetity-table

--- dev/config.d/plugins.php
+++ dev/config.d/plugins.php
@@ -1,5 +1,61 @@
 <?php
 
 return [
-    'plugins' => []
+    'plugins' => [
+        "RegistrationPayments" => [
+            "namespace" => "RegistrationPayments",
+            "config" => [
+                "opportunitys_cnab_active" => [5],
+                'cnab240_company_data' => [
+                    'nome_empresa' => 'SECRETARIA DE CULTURA PE',
+                    'tipo_inscricao' => '2',
+                    'numero_inscricao' => '13.270.478/0001-83',
+                    'agencia' => '697',
+                    'agencia_dv' => '1',
+                    'conta' => '79849',
+                    'conta_dv' => '5',
+                    'numero_sequencial_arquivo' => 1,
+                    'convenio' => '000264470',
+                    'carteira' => '',
+                    'situacao_arquivo' => " ",
+                    'uso_bb1' => '000264470',
+                    'operacao' => 'C',
+                ],
+                "opportunitysCnab" => [ // Configurações de oportunidades
+                    "5" => [
+                        "canab_bb_default_value" => 1, // Define qual valor padão representa o Banco do Brasil
+                        "settings" => [ // Configurações padrões
+                            "social_type" => [ // Tipo de proponente (Pessoa Fisica ou Pessoa Jurídica) Pessoa Fisica = 1 Pessoa Jurídica = 2
+                                "Pessoa física" => "1", // Não utilizado neste edital, por isso id repetido
+                                "Pessoa juridica" => "2",
+                            ],
+                            "release_type" => [
+                                1 => "01", // Corrente BB
+                                2 => "05", // Poupança BB
+                                3 => "03", // Outros bancos
+                            ],
+                        ],
+                        "social_type" => 1, // ID campo que define o tipo de ptoponente, (Pessoa Fisica ou Pessoa Jurídica)
+                        "proponent_name" => [ // Chave 1 Pessoa física Chave 2 Pessoa Jurídica
+                            "dependence" => "social_type",
+                            1 => 5, // Não utilizado neste edital, por isso id repetido
+                            2 => 3,
+                        ],
+                        "proponent_document" => [ // Chave 1 Pessoa física Chave 2 Pessoa Jurídica
+                            "dependence" => "social_type",
+                            1 => 4, // Não utilizado neste edital, por isso id repetido
+                            2 => 2,
+                        ],
+                        "account_type" => 6, // ID campo que define o tipo de conta bancária do proponente
+                        "bank" => 11, // ID campo que define a o banco do proponente
+                        "branch" => 7, // ID campo que define a agência bancária do proponente
+                        "branch_dv" => 10, // ID campo que define o DV da agência bancária do proponente
+                        "account" => 9, // ID campo que define a conta bancária do proponente
+                        "account_dv" => 8, // ID campo que define o DV da conta bancária do proponente
+                    ],
+
+                ],
+            ]
+        ]
+    ]
 ];
--- src/modules/Entities/components/entity-table/template.php
+++ src/modules/Entities/components/entity-table/template.php
@@ -20,65 +20,72 @@ $this->import('
     <mc-entities :select="select" :type="type" :query="query" :limit="limit" :endpoint="endpoint">
 
         <template #header="{entities, filters}">
-            <div class="entity-table__header">
+            <div class="entity-table__header ">
                 <!-- título - opcional -->
                 <div v-if="hasSlot('title')" class="entity-table__title">
                     <slot name="title"></slot>
                 </div>
+                <div className='mc-collapse'>
 
-                <!-- ações - opcional -->
-                <mc-collapse v-if="hasSlot('actions')">
-                    <template #header>
-                        <slot name="actions" :entities="entities" :filters="filters"></slot>
-                    </template>
 
-                    <template v-if="hasSlot('advanced-actions')" #content>
-                        <slot name="advanced-actions" :entities="entities" :filters="filters"></slot>
-                    </template>
-                </mc-collapse>
-
-                <!-- filtros - pré-definido -->
-                <mc-collapse>
-                    <template #header>
-                        <div class="entity-table__main-filter">
-                            <div class="entity-table__search">
-                                <div class="entity-table__search-title">
-                                    <h4 class="bold"><?= i::__('Filtrar:') ?></h4>
-                                </div>
-                                <div class="entity-table__search-field">
-                                    <input v-model="searchText" @keyup="keyword(entities)" type="text" placeholder="<?= i::__('Pesquise por palavra-chave') ?>" class="entity-table__search-input" />
-                                    <button @click="keyword(entities)" class="entity-table__search-button">
-                                        <mc-icon name="search"></mc-icon>
-                                    </button>
-                                </div>
+                    <!-- filtros - pré-definido -->
+                    <mc-collapse className='none'>
+                        <template #header>
+                            <div class="entity-table__clear-filters">
+                                <button class="button button--text button--icon" @click="clearFilters(entities)"> <?= i::__("Limpar filtros") ?> <mc-icon name="trash"></mc-icon> </button>
                             </div>
-                            
-                            <slot name="filters" :entities="entities" :filters="filters"></slot>
-                        </div>
-                    </template>
-
-                    <template #content>
-                        <div class="entity-table__advanced-filters">
-                            <div class="field">
-                                <label><?= i::__('Exibir colunas')?></label>
-
-                                <div class="field__group">
-                                    <label class="field__checkbox">
-                                        <input ref="allHeaders" type="checkbox" @click="showAllHeaders()"> <?= i::__('Todas as colunas') ?>
-                                    </label>
-                                    <label v-for="column in columns" class="field__checkbox">
-                                        <input :checked="column.visible" type="checkbox" :value="column.slug" @click="toggleHeaders($event)"> {{column.text}} 
-                                    </label>
+                            <div class="entity-table__main-filter">
+                                <div class="entity-table__search">
+                                    <div class="entity-table__search-title">
+                                        <h4 class="bold"><?= i::__('Filtrar:') ?></h4>
+                                    </div>
+                                    <div class="entity-table__search-field">
+                                        <input v-model="searchText" @keyup="keyword(entities)" type="text" placeholder="<?= i::__('Pesquise por palavra-chave') ?>" class="entity-table__search-input" />
+                                        <button @click="keyword(entities)" class="entity-table__search-button">
+                                            <mc-icon name="search"></mc-icon>
+                                        </button>
+                                    </div>
                                 </div>
+
+                                <slot name="filters" :entities="entities" :filters="filters"></slot>
                             </div>
 
-                            <slot name="advanced-filters" :entities="entities" :filters="filters"></slot>
-                        </div>
-                    </template>
-                </mc-collapse>
+                        </template>
 
-                <div class="entity-table__clear-filters">
-                    <button class="button button--text button--icon" @click="clearFilters(entities)"> <?= i::__("Limpar filtros") ?> <mc-icon name="trash"></mc-icon> </button>
+                        <template #content>
+                            <div class="entity-table__advanced-filters">
+                                <div class="field">
+                                    <label><?= i::__('Exibir colunas') ?></label>
+
+                                    <div class="field__group">
+                                        <label class="field__checkbox">
+                                            <input ref="allHeaders" type="checkbox" @click="showAllHeaders()"> <?= i::__('Todas as colunas') ?>
+                                        </label>
+                                        <label v-for="column in columns" class="field__checkbox">
+                                            <input :checked="column.visible" type="checkbox" :value="column.slug" @click="toggleHeaders($event)"> {{column.text}}
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <slot name="advanced-filters" :entities="entities" :filters="filters"></slot>
+                            </div>
+                        </template>
+                    </mc-collapse>
+
+                    <!-- ações - opcional -->
+                    <mc-collapse v-if="hasSlot('actions')" className='none'>
+                        <template #header>
+                            <slot name="actions" :entities="entities" :filters="filters"></slot>
+                        </template>
+
+                        <template v-if="hasSlot('advanced-actions')" #content>
+                            <slot name="advanced-actions" :entities="entities" :filters="filters"></slot>
+                        </template>
+                    </mc-collapse>
+
+
+
+
                 </div>
             </div>
         </template>
