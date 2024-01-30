7ca7018492a303de171fe2e163ea01899baa128c
wolegario40@gmail.com <wolegario49@gmail.com>
2024-01-30 16:15:48 -0300

    Cria o dql para retorna os resultados das avaliações Ref.: #3064

--- src/modules/EvaluationMethodTechnical/components/evaluation-method-technical--apply/init.php
+++ src/modules/EvaluationMethodTechnical/components/evaluation-method-technical--apply/init.php
@@ -5,9 +5,14 @@
  */
 
 use MapasCulturais\Entities\Registration;
+$entity = $this->controller->requestedEntity;
+$em = $app->em;
+$conn = $em->getConnection();
+$dql = "SELECT MAX(e.consolidatedResult) FROM MapasCulturais\\Entities\\Registration e WHERE e.opportunity = {$entity->id}";
+$query = $app->em->createQuery($dql);
+$data['max_result'] = $query->getSingleScalarResult();
 
 $registrations = Registration::getStatusesNames();
-
 foreach ($registrations as $status => $status_name) {
     if (in_array($status, [0, 1, 2, 3, 8, 10])) {
         $data["registrationStatusDict"][] = ["label" => $status_name, "value" => $status];
