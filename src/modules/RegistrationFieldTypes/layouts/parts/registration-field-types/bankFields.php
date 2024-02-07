920d99fcbdd4db3dd9ffb0bcd8dcf9fc93521c26
erleibiazzio <erleibiazzio@gmail.com>
2024-02-07 13:51:22 -0300

    Coloca título do campo e exibe texto Obeigatorio quando o mesmo é required #3077

--- src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/bankFields.php
+++ src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/bankFields.php
@@ -1,3 +1,12 @@
+<?php
+
+use MapasCulturais\i; 
+?>
+
 <div ng-if="field.fieldType == 'bankFields'">
+    <span class="label">
+        {{field.title}}
+        <div ng-if="requiredField(field)" class="field-required"><span class="description"><?php i::_e('obrigatório') ?></span><span class="icon-required">*</span></div>
+    </span>
     <?php $this->part('registration-field-types/fields/bankFields') ?>
 </div>
