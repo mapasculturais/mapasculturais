dd86fd4096ec57a69b3c2183bc8144e0d7fb5f9b
erleibiazzio <erleibiazzio@gmail.com>
2024-02-10 12:40:05 -0300

    Corrige serialize e unserialize dos campos @ no modulo RegistrationFieldTypes para evitar o json_encode duplicados

--- src/modules/RegistrationFieldTypes/Module.php
+++ src/modules/RegistrationFieldTypes/Module.php
@@ -436,7 +436,12 @@ class Module extends \MapasCulturais\Module
                 'requireValuesConfiguration' => true,
                 'serialize' => function($value, Registration $registration = null, $metadata_definition = null) use ($module) {
                     $module->saveToEntity($registration->owner, $value, $registration, $metadata_definition);
-                    return json_encode($value);
+
+                    if(is_object($value) || is_array($value)) {
+                        return json_encode($value);
+                    }else {
+                        return $value;
+                    }
                 },
                 'unserialize' => function($value, $registration = null, $metadata_definition = null) use ($module, $app) {
 
@@ -445,7 +450,15 @@ class Module extends \MapasCulturais\Module
                     }
                     
                     if(is_null($registration) || $registration->status > 0){
-                        $result = json_decode($value ?: "");
+                        $value = $value ?: "";
+
+                        $first_char = strlen($value ?? '') > 0 ? $value[0] : "" ;
+                        if(in_array($first_char, ['"', "[", "{"]) || in_array($value, ["null", "false", "true"])) {
+                            $result = json_decode($value ?: "");
+                        }else {
+                            $result = $value;
+                        }
+
                     }else{
                         $disable_access_control = false;
 
@@ -476,7 +489,11 @@ class Module extends \MapasCulturais\Module
                     if($agent){
                         $module->saveToEntity($agent[0], $value, $registration, $metadata_definition);
                     }
-                    return json_encode($value);
+                    if(is_object($value) || is_array($value)) {
+                        return json_encode($value);
+                    }else {
+                        return $value;
+                    }
                 },
                 'unserialize' => function($value, $registration = null, $metadata_definition = null) use ($module, $app) {
                     if(!$registration instanceof \MapasCulturais\Entities\Registration){
@@ -484,7 +501,14 @@ class Module extends \MapasCulturais\Module
                     }
 
                     if(is_null($registration) || $registration->status > 0){
-                        $result = json_decode($value ?: "");
+                            
+                        $first_char = strlen($value ?? '') > 0 ? $value[0] : "" ;
+                        if(in_array($first_char, ['"', "[", "{"]) || in_array($value, ["null", "false", "true"])) {
+                            $result = json_decode($value ?: "");
+                        }else {
+                            $result = $value;
+                        }
+
                     } else {
                         $disable_access_control = false;
 
@@ -523,7 +547,11 @@ class Module extends \MapasCulturais\Module
                     if($space_relation){
                         $module->saveToEntity($space_relation->space, $value, $registration, $metadata_definition);
                     }
-                    return json_encode($value);
+                    if(is_object($value) || is_array($value)) {
+                        return json_encode($value);
+                    }else {
+                        return $value;
+                    }
                 },
                 'unserialize' => function($value, $registration = null, Metadata $metadata_definition = null) use ($module, $app) {
                     if(!$registration instanceof \MapasCulturais\Entities\Registration){
@@ -531,7 +559,12 @@ class Module extends \MapasCulturais\Module
                     }
                     
                     if(is_null($registration) || $registration->status > 0){
-                        $result = json_decode($value ?: "");
+                        $first_char = strlen($value ?? '') > 0 ? $value[0] : "" ;
+                        if(in_array($first_char, ['"', "[", "{"]) || in_array($value, ["null", "false", "true"])) {
+                            $result = json_decode($value ?: "");
+                        }else {
+                            $result = $value;
+                        }
                     } else {
                         $disable_access_control = false;
                     
