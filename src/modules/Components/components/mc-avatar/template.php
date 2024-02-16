9155c87b15138e0df6358cc44bb4b7e4bb0618de
Pedro Piva <pedro.piva@iterpec.com>
2024-02-16 00:12:48 -0300

    fix: ajustando crop de imagem

--- src/modules/Components/components/mc-avatar/template.php
+++ src/modules/Components/components/mc-avatar/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
@@ -8,7 +9,7 @@ $this->import('
     mc-icon
 ');
 ?>
-<div :class="classes" class="mc-avatar"> 
+<div :class="classes" class="mc-avatar">
     <img v-if="image" :src="image" alt="">
     <mc-icon v-if="!image" :entity="entity"></mc-icon>
 </div>
