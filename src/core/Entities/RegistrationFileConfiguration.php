f140b937b129eaf98a26a9d2d068b2130902a8b2
wolegario49@gmail.com <wolegario49@gmail.com>
2024-02-20 16:00:57 -0300

    Passa no jsonSerialize os valores da nova estrutura de condicionalidade Ref.: #3090

--- src/core/Entities/RegistrationFileConfiguration.php
+++ src/core/Entities/RegistrationFileConfiguration.php
@@ -149,7 +149,9 @@ class RegistrationFileConfiguration extends \MapasCulturais\Entity {
             'displayOrder' => $this->displayOrder,
             'conditional' => $this->conditional ? true : false,
             'conditionalField' => $this->conditionalField,
-            'conditionalValue' => $this->conditionalValue
+            'conditionalValue' => $this->conditionalValue,
+            'registrationRanges' => $this->registrationRanges,
+            'proponentTypes' => $this->proponentTypes,
         ];
 
         $app = App::i();
