3f90ebdc4cb96e6e0ba211e8b6c4c61355786e63
erleibiazzio <erleibiazzio@gmail.com>
2024-02-28 16:43:45 -0300

    Adiciona internacionalização do método sendDefinition Ref.: #3059

--- src/modules/Opportunities/components/opportunity-evaluation-committee/texts.php
+++ src/modules/Opportunities/components/opportunity-evaluation-committee/texts.php
@@ -3,5 +3,9 @@ use MapasCulturais\i;
 
 return [
     'enable' => i::__('Habilitar'),
-    'disable' => i::__('Desabilitar')
+    'disable' => i::__('Desabilitar'),
+    'reopenEvaluationsSuccess' => i::__('Avaliações reabertas com sucesso'),
+    'addDistribution' => i::__('A distribuição de avaliações foi atualizada com sucesso.'),
+    'addCategory' => i::__('Categoria adicionada com sucesso.'),
+    'removeCategory' => i::__('Categoria removida com sucesso.'),
 ];
