717dd2c4a27fa7ea96c88f72af49e332e3b7a0d6
Pedro Piva <pedro.piva@iterpec.com>
2024-03-04 13:53:21 -0300

    fix: ajustando classes

--- src/modules/EvaluationMethodTechnical/components/affirmative-policy--bonus-config/template.php
+++ src/modules/EvaluationMethodTechnical/components/affirmative-policy--bonus-config/template.php
@@ -13,73 +13,84 @@ $this->import('
 ');
 ?>
 
-<div class="technical-assessment-section">
-    <div v-if="entity.sections && entity.sections.length > 0">
-        <label>
-            <?php i::_e("Nota máxima:") ?>
-            {{ maxScore }}
-        </label>
 
-        <label>
-            <?php i::_e("Nota de corte:") ?>
-            <input type="number" v-model="entity.cutoffScore" min="0" @blur="autoSave()" placeholder="<?= i::esc_attr__('Nota de corte') ?>">
-        </label>
+<div class="affirmative-policies--quota-configuration">
+    <h4 class="bold"><?= i::__('Configuração das Cotas por Categoria') ?></h4>
 
-        <div v-for="(section, index) in entity.sections" :key="index">
-            <input v-if="editingSections[section.id]" type="text" v-model="section.name" @blur="sendConfigs();editSections(section.id);" placeholder="<?= i::esc_attr__('Nome sessão') ?>">
-            <span v-else class="bold">{{section.name}}</span>
+    <div class="affirmative-policies--quota-configuration__content" v-if="entity.affirmative-policy--bonus-config">
+        <div class="fields">
+            <label class="field__title">
+                <?= i::__('Percentual total de indução:') ?>
+                {{totalPercentage}} %
+            </label>
 
-            <button @click="editSections(section.id)">
-                <mc-icon name="edit"></mc-icon>
-                <?php i::_e("Editar") ?>
-            </button>
 
-            <mc-confirm-button @confirm="delSection(section.id)">
-                <template #button="{open}">
-                    <div class="field__trash">
-                        <mc-icon class="danger__color" name="trash" @click="open()"></mc-icon>
-                        <label class="bold"><?php i::_e("Excluir") ?></label>
+        </div>
+
+        <div class="affirmative-policies--quota-configuration__quota" v-for="(quota, index) in entity.quotaConfiguration.rules" :key="index">
+            <div class="affirmative-policies--quota-configuration__column">
+                <h5 class="field__title--semibold"><?= i::__('Cota') ?> {{index+1}}</h5>
+                <select v-model="quota.fieldName">
+                    <option class="select__selected-option" v-for="(item, index) in entity.opportunity.affirmativePoliciesEligibleFields" :value="item.fieldName">{{ '#' + item.id + ' ' + item.title }}</option>
+                </select>
+
+                <div class="field">
+                    <div class="field__column" v-if="getFieldType(quota) === 'select' || getFieldType(quota) === 'multiselect'">
+                        <label v-for="option in getFieldOptions(quota)">
+                            <input class="input" type="checkbox" :value="option" v-model="quota.eligibleValues" @change="autoSave()">
+                            {{option}}
+                        </label>
                     </div>
-                </template>
-                <template #message="message">
-                    <?= i::__('Deseja deletar a sessão?') ?>
-                </template>
-            </mc-confirm-button>
 
-            <div v-if="entity.criteria && entity.criteria.length > 0">
-                <div v-for="(criteria, index) in entity.criteria" :key="index">
-                    <div v-if="criteria.sid == section.id">
-                        <input type="text" v-model="criteria.title" @blur="sendConfigs" placeholder="<?= i::esc_attr__('Nome do critério') ?>">
-                        <input type="number" v-model="criteria.max" @blur="sendConfigs" placeholder="<?= i::esc_attr__('Pontuação máxima') ?>">
-                        <input type="number" v-model="criteria.weight" @blur="sendConfigs" placeholder="<?= i::esc_attr__('Peso') ?>">
-
-                        <mc-confirm-button @confirm="delCriteria(criteria.id)">
-                            <template #button="{open}">
-                                <div class="field__trash">
-                                    <mc-icon class="danger__color" name="trash" @click="open()"></mc-icon>
-                                </div>
-                            </template>
-                            <template #message="message">
-                                <?= i::__('Deseja deletar o critério?') ?>
-                            </template>
-                        </mc-confirm-button>
+                    <div class="field__column" v-if="getFieldType(quota) === 'checkbox' || getFieldType(quota) === 'boolean'">
+                        <label>
+                            <input class="input" type="radio" :name="quota.fieldName + ':' + index" :value="true" v-model="quota.eligibleValues" @change="autoSave()">
+                            <?= i::__('Sim / Marcado') ?>
+                        </label>
+                        <label>
+                            <input class="input" type="radio" :name="quota.fieldName + ':' + index" :value="false" v-model="quota.eligibleValues" @change="autoSave()">
+                            <?= i::__('Não / Desmarcado') ?>
+                        </label>
                     </div>
                 </div>
             </div>
 
-            <button @click="addCriteria(section.id)" class="button button--primary button--icon">
-                <mc-icon name="add"></mc-icon>
-                <label>
-                    <?php i::_e("Adicionar critério") ?>
+            <div class="quota__fields">
+                <label class="field__title"><?= i::__('Porcentagem') ?>
+                    <div>
+                        <input type="number" v-model="quota.percentage" @change="updateRuleQuotas(quota)" min="0" max="100"> %
+                    </div>
                 </label>
-            </button>
+            </div>
+
+            <div class="quota__fields">
+                <label class="field__title"><?= i::__('Número de Vagas') ?>
+                    <input type="number" v-model="quota.vacancies" @change="updateRuleQuotaPercentage(quota)" min="0" :max="totalVacancies">
+                </label>
+            </div>
+            <div class="quota__trash">
+                <mc-confirm-button @confirm="removeConfig(index)">
+                    <template #button="{open}">
+                        <div class="field__trash button button--md button--text-danger button-icon">
+                            <mc-icon class="danger__color" name="trash" @click="open()"></mc-icon>
+                        </div>
+                    </template>
+                    <template #message="message">
+                        <?= i::__('Deseja deletar a cota?') ?>
+                    </template>
+                </mc-confirm-button>
+            </div>
         </div>
     </div>
-
-    <button @click="addSection" class="button button--primary button--icon">
-        <mc-icon name="add"></mc-icon>
-        <label>
-            <?php i::_e("Adicionar nova sessão de Critérios") ?>
-        </label>
-    </button>
+    <div class="affirmative-policies--quota-configuration__footer">
+        <button @click="addConfig();" class="button button--primary button--icon">
+            <mc-icon name="add"></mc-icon>
+            <label v-if="entity.quotaConfiguration && entity.quotaConfiguration.rules.length > 0">
+                <?php i::_e("Adicionar categoria") ?>
+            </label>
+            <label v-else>
+                <?php i::_e("Configurar Cotas por Categoria") ?>
+            </label>
+        </button>
+    </div>
 </div>
