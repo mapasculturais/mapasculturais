50cca12e82b71f303754d29bbea115cb1f9c12a9
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2024-03-05 16:18:32 -0300

    Add php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->exclude(['logs', 'var', 'vendor'])
+    ->in(__DIR__)
+;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'psr_autoloading' => true,
+        '@PSR12' => true,
+        '@Symfony' => true,
+        '@PHP82Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'void_return' => true,
+        'yoda_style' => false,
+        'increment_style' => ['style' => 'post'],
+    ])
+    ->setFinder($finder)
+;
