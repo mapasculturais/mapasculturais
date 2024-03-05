d7472e435b6df48145d9f7705258a6c6e8dacb35
erleibiazzio <erleibiazzio@gmail.com>
2024-03-05 11:24:42 -0300

    Cria db-update para ajustar o campo de distribuiçõa de avaliação por categoria Ref.: #3091

--- src/modules/Opportunities/components/opportunity-evaluation-committee/script.js
+++ src/modules/Opportunities/components/opportunity-evaluation-committee/script.js
@@ -192,16 +192,5 @@ app.component('opportunity-evaluation-committee', {
                 });
             }
         },
-        getFetchCategories(agentUserId) {
-            let values = [];
-            if( typeof this.entity.fetchCategories[agentUserId] == "array") {
-                return this.entity.fetchCategories[agentUserId];
-            }else if(typeof this.entity.fetchCategories[agentUserId] == "string"){
-                values.push(this.entity.fetchCategories[agentUserId]);
-                return values
-            }
-
-            return values;
-        }
     },
 });
