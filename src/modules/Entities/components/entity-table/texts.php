01ac4343524ff4793191e1e9f4c85e157154abd3
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2024-03-08 00:30:06 -0300

    inicio da refatoração do componente entity-table (Ref.: #3135)
    - possibilidade de configuração de campo de seleção para ordenação
    - reposicionamento do local de habilitar colunas

--- src/modules/Entities/components/entity-table/texts.php
+++ src/modules/Entities/components/entity-table/texts.php
@@ -2,5 +2,11 @@
 use MapasCulturais\i;
 
 return [
-    'item obrigatório' => i::__('Não é permitido desabilitar a coluna')
+    'item obrigatório' => i::__('Não é permitido desabilitar a coluna'),
+
+    // opções de ordenação
+    'mais recentes primeiro' => i::__('mais recentes primeiro'),
+    'mais antidas primeiro' => i::__('mais antigas primeiro'),
+    'modificadas recentemente' => i::__('modificadas recentemente'),
+    'modificadas há mais tempo' => i::__('modificadas há mais tempo'),
 ];
