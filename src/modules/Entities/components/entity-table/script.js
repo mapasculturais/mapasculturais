17d55ddedad65fcf47016553ffae36ef98c769e3
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-03-12 11:19:23 -0300

    Adiciona filtro por campos select/checkboxes (Ref.: #3135)

--- src/modules/Entities/components/entity-table/script.js
+++ src/modules/Entities/components/entity-table/script.js
@@ -69,6 +69,8 @@ app.component('entity-table', {
         const visible = this.visible instanceof Array ? this.visible : this.visible.split(",");
         const required = this.required instanceof Array ? this.required : this.required.split(",");
 
+        this.originalQuery = JSON.parse(JSON.stringify(this.query));
+
         for(let header of this.columns) {
             header.slug = this.parseSlug(header);
             header.visible = visible.includes(header.slug) || required.includes(header.slug);
@@ -101,10 +103,27 @@ app.component('entity-table', {
         $description() {
             return $DESCRIPTIONS[this.type];
         },
+        advancedFilters() {
+            let filters = {};
+
+            for (let visibleColumn of this.visibleColumns) {
+                const description = this.$description[visibleColumn.slug];
+
+                if (description && description.hasOwnProperty('options')) {
+                    if (visibleColumn.slug !== 'status' && Object.keys(description.options).length > 0) {
+                        filters[visibleColumn.slug] = {
+                            label: description.label,
+                            options: description.options, 
+                        }
+                    }
+                }
+            }
+            
+            return filters;
+        },
+
         appliedFilters() {
-            const type = this.type;
             const query = JSON.parse(JSON.stringify(this.query));
-            const $description = this.$description;
             
             delete query['@limit'];
             delete query['@opportunity'];
@@ -112,82 +131,10 @@ app.component('entity-table', {
             delete query['@select'];
             delete query['@page'];
 
-            function getFilterLabels(prop, value) {
-                // Exemplo: 
-                //      key = status  value = EQ(1)
-
-                if (prop == '@keyword') {
-                    return [{prop, value, label: __('palavras-chave', 'entity-table')}]
-                }
-                
-                let values = getFilterValues(value);
-                if (values) {
-                    if (prop == 'status') {
-                        let statusDict = {
-                            '0': __('rascunhos', 'entity-table'),
-                            '1': __('publicadas', 'entity-table'),
-                            '-10': __('lixeira', 'entity-table'),
-                            '-1': __('arquivadas', 'entity-table'),
-                        }
-
-                        if(type == 'registration') {
-                            statusDict = {
-                                '0': __('rascunhos', 'entity-table'),
-                                '1': __('pendentes', 'entity-table'),
-                                '2': __('invalidas', 'entity-table'),
-                                '3': __('nao selecionadas', 'entity-table'),
-                                '8': __('suplentes', 'entity-table'),
-                                '10': __('selecionadas', 'entity-table'),
-                            }
-                        }
-
-                        return values.map((value) => { 
-                            return {prop, value, label: statusDict[value]} 
-                        });
-                    }
-
-                    const fieldDescription = $description[prop];
-                    if (fieldDescription.field_type == 'select') {
-                        return values.map((value) => { 
-                            return {prop, value, label: fieldDescription.options[value]}
-                        });
-                    } else {
-                        return values.map((value) => { 
-                            return {prop, value, label: value.replace(/(\\)/g, '')}
-                        });
-                    }
-                } else {
-                    return null;
-                }
-            }
-
-            function getFilterValues(value) {
-                // Exemplos: 
-                //      EQ(10), EQ(preto), IN(8, 10), IN(preto, pardo)                
-                let values = /(EQ|IN|GT|GTE|LT|LTE)\(([^\)]+,?)+\)/.exec(value); 
-
-                if (values) {
-                    const operator = values[1];
-                    const _values = values[2];
-                    
-                    if (_values) {
-                        if(operator == 'IN') {
-                            values = _values.replace(/([^\\]),/g, '$1%break%');
-                        } else {
-                            values = _values;
-                        }
-                        
-                        return values.split('%break%').filter(value => value.trim());
-                    } else {
-                        return null;
-                    }
-                }
-            }
-
             let result = [];
             for (let key of Object.keys(query)) {
-                if (getFilterLabels(key, query[key])) {
-                    result = result.concat(getFilterLabels(key, query[key]));
+                if (this.getFilterLabels(key, query[key])) {
+                    result = result.concat(this.getFilterLabels(key, query[key]));
                 } 
             }
             return result;
@@ -195,6 +142,79 @@ app.component('entity-table', {
     },
 
     methods: {
+        getFilterLabels(prop, value) {
+            // Exemplo: 
+            //      key = status  value = EQ(1)
+
+            if (prop == '@keyword') {
+                return [{prop, value, label: __('palavras-chave', 'entity-table')}]
+            }
+            
+            let values = this.getFilterValues(value);
+            if (values) {
+                if (prop == 'status') {
+                    let statusDict = {
+                        '0': __('rascunhos', 'entity-table'),
+                        '1': __('publicadas', 'entity-table'),
+                        '-10': __('lixeira', 'entity-table'),
+                        '-1': __('arquivadas', 'entity-table'),
+                    }
+
+                    if(this.type == 'registration') {
+                        statusDict = {
+                            '0': __('rascunhos', 'entity-table'),
+                            '1': __('pendentes', 'entity-table'),
+                            '2': __('invalidas', 'entity-table'),
+                            '3': __('nao selecionadas', 'entity-table'),
+                            '8': __('suplentes', 'entity-table'),
+                            '10': __('selecionadas', 'entity-table'),
+                        }
+                    }
+
+                    return values.map((value) => { 
+                        return {prop, value, label: statusDict[value]} 
+                    });
+                }
+
+                const fieldDescription = this.$description[prop];
+                if (fieldDescription.field_type == 'select') {
+                    return values.map((value) => { 
+                        return {prop, value, label: fieldDescription.options[value]}
+                    });
+                } else {
+                    return values.map((value) => { 
+                        return {prop, value, label: value.replace(/(\\)/g, '')}
+                    });
+                }
+            } else {
+                return null;
+            }
+        },
+
+        getFilterValues(value) {
+            // Exemplos: 
+            //      EQ(10), EQ(preto), IN(8, 10), IN(preto, pardo)                
+            let values = /(EQ|IN|GT|GTE|LT|LTE)\(([^\)]+,?)+\)/.exec(value); 
+
+            if (values) {
+                const operator = values[1];
+                const _values = values[2];
+                
+                if (_values) {
+                    if(operator == 'IN') {
+                        values = _values.replace(/([^\\]),/g, '$1%break%');
+                    } else {
+                        values = _values;
+                    }
+                    
+                    return values.split('%break%').filter(value => value.trim());
+                } else {
+                    return null;
+                }
+            }
+            return null;
+        },
+
         parseSlug(header) {
             if (header.slug) {
                 return header.slug;
@@ -212,11 +232,11 @@ app.component('entity-table', {
             return val;
         },
 
-        keyword(entities) {
-            window.dispatchEvent(new CustomEvent('entityTableSearchText', { detail: {searchText: this.searchText} }));
-            this.query['@keyword'] = this.searchText;
-            entities.refresh(this.watchDebounce);
-        },
+        // keyword(entities) {
+        //     window.dispatchEvent(new CustomEvent('entityTableSearchText', { detail: {searchText: this.searchText} }));
+        //     this.query['@keyword'] = this.searchText;
+        //     entities.refresh(this.watchDebounce);
+        // },
 
         resetHeaders() {
             const visible = this.visible instanceof Array ? this.visible : this.visible.split(",");
@@ -232,8 +252,12 @@ app.component('entity-table', {
         },
 
         toggleHeaders(event) {
+            const field = event.target.value;
+            let column = null;
+
             for (let header of this.columns) {
-                if (header.slug == event.target.value) {
+                if (header.slug == field) {
+                    column = header;
                     if (header.required) {
                         event.preventDefault();
                         event.stopPropagation();
@@ -242,6 +266,10 @@ app.component('entity-table', {
                         header.visible = !header.visible;
                     }
                 }
+            }
+
+            if (column && !column.visible) {
+                delete this.query[field];
             }
         },
 
@@ -255,17 +283,13 @@ app.component('entity-table', {
             }
         },
 
-        clearFilters(entities) {
-            this.resetHeaders();
-
-            if (this.$refs.allHeaders?.checked) {
-                this.$refs.allHeaders.checked = false;
-            }
-
-            if(this.searchText != '') {
-                this.searchText = '';
-                delete this.query['@keyword'];
-                entities.refresh(this.watchDebounce);
+        clearFilters(entities) {            
+            for (let prop in this.query) {
+                if (this.originalQuery[prop]) {
+                    this.query[prop] = this.originalQuery[prop];
+                } else {
+                    delete this.query[prop];
+                }
             }
 
             this.$emit('clear-filters', entities);
@@ -302,9 +326,31 @@ app.component('entity-table', {
                     }
                 }
             }
-            
+
             entities.refresh();
             this.$emit('remove-filter', filter);
+        },
+
+        toggleAdvancedFilter(fieldName, option) {
+            const currentValues = this.getFilterValues(this.query[fieldName] ?? '') || [];
+
+            if (currentValues.includes(option)) {
+                currentValues.splice(currentValues.indexOf(option), 1);    
+            } else {
+                currentValues.push(option);
+            }
+            
+            if (currentValues.length > 0) {
+                this.query[fieldName] = `IN(${currentValues.toString()})`;
+            } else {
+                delete this.query[fieldName];
+            }
+        },
+
+        advancedFilterChecked(fieldName, option) { 
+            const currentValues = this.getFilterValues(this.query[fieldName] ?? '') || [];
+
+            return currentValues.includes(option);
         },
     },
 });
