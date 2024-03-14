e56ed400f01a86d73a511390bf60efb41f4ab16a
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2024-03-14 09:54:34 -0300

    altera comando do docker-compose para docker compose nos scripts de dev

--- dev/dump-db-updates.sh
+++ dev/dump-db-updates.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR
 
-docker-compose exec mapas bash -c "cd /var/www/src/tools/ && ./doctrine orm:schema-tool:update --dump-sql"
+docker compose exec mapas bash -c "cd /var/www/src/tools/ && ./doctrine orm:schema-tool:update --dump-sql"
 
 cd $CDIR
--- dev/shell.sh
+++ dev/shell.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR
 
-docker-compose exec mapas sh /var/www/scripts/shell.sh
+docker compose exec mapas sh /var/www/scripts/shell.sh
 
 cd $CDIR
