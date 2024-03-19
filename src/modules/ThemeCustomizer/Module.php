a4c873ed9bb453060f5214f32bcb282b3f33a0de
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-03-19 12:08:25 -0300

    Adiciona metadados no subsite para personalização da logo (Ref.: #3141)

--- src/modules/ThemeCustomizer/Module.php
+++ src/modules/ThemeCustomizer/Module.php
@@ -7,6 +7,8 @@ use MapasCulturais\i;
 
 class Module extends \MapasCulturais\Module
 {
+    static public $originalColors;
+
     function __construct(array $config = [])
     {
         $app = App::i();
@@ -29,6 +31,48 @@ class Module extends \MapasCulturais\Module
                 }
             ];
         });
+
+        $app->hook('mapas.printJsObject:before', function () use($app) {
+            $this->jsObject['subsite'] = $app->subsite;
+        });
+
+        $app->hook('view.render(<<*>>):before', function () use($app) {
+            $app->view->enqueueScript('components', 'subsite-init', 'js/subsite-init.js', ['components-utils']);
+        });
+
+        $app->hook('app.register:after', function () use($app) {
+            if ($subsite = $app->subsite) {
+                
+                Module::$originalColors = $app->config['logo.colors'];
+
+                if ($subsite->custom_colors) {
+                    if ($color1 = $subsite->logo_color1) {
+                        $app->config['logo.colors'][0] = $color1;
+                    }
+
+                    if ($color2 = $subsite->logo_color2) {
+                        $app->config['logo.colors'][1] = $color2;
+                    }
+
+                    if ($color3 = $subsite->logo_color3) {
+                        $app->config['logo.colors'][2] = $color3;
+                    }
+
+                    if ($color4 = $subsite->logo_color4) {
+                        $app->config['logo.colors'][3] = $color4;
+                    }
+                }
+
+                if ($title = $subsite->logo_title) {
+                    $app->config['logo.title'] = $title;
+                }
+
+                if ($subtitle = $subsite->logo_subtitle) {
+                    $app->config['logo.subtitle'] = $subtitle;
+                }
+            }
+        });
+
     }
 
     function register()
@@ -38,5 +82,47 @@ class Module extends \MapasCulturais\Module
         if (!isset($controllers['theme-customizer'])) {
             $app->registerController('theme-customizer', Controller::class);
         }
+
+        $app->hook('app.register', function(){
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'custom_colors', [
+                'label' => i::__('Cores'),
+                'type' => 'radio',
+                'options' => [
+                    '1' => i::__('Utilizar cores customizadas'),
+                    '' => i::__('Utilizar cores do tema'),
+                ],
+                'default' => '',
+            ]);
+
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_color1', [
+                'label' => i::__("Cor #1"),
+                'type' => 'color',
+            ]);
+
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_color2', [
+                'label' => i::__("Cor #2"),
+                'type' => 'color',
+            ]);
+
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_color3', [
+                'label' => i::__("Cor #3"),
+                'type' => 'color',
+            ]);
+
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_color4', [
+                'label' => i::__("Cor #4"),
+                'type' => 'color',
+            ]);
+            
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_title', [
+                'label' => i::__("Título da logo do Mapas Culturais"),
+                'type' => 'string',
+            ]);
+
+            $this->view->registerMetadata(\MapasCulturais\Entities\Subsite::class, 'logo_subtitle', [
+                'label' => i::__("Subtítulo da logo do Mapas Culturais"),
+                'type' => 'string',
+            ]);
+        });
     }
 }
