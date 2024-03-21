db7dfb75c814ec86b235b9e8052a0d7d624837b6
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-03-21 13:03:24 -0300

    feat: ajusta espaçamentos e tamanho do textarea

--- src/modules/ThemeCustomizer/components/home-texts/template.php
+++ src/modules/ThemeCustomizer/components/home-texts/template.php
@@ -1,9 +1,20 @@
-<div>
-    <div v-for="(section, index) in homeTexts" :key="index">
-        <h2>{{ section.sectionName }}</h2>
-        <div class="field" v-for="(text, textIndex) in section.texts" :key="textIndex">
-            <label :for="text.slug">{{ text.description }}</label>
-            <input :id="text.slug" v-model="subsite.homeTexts[text.slug]" @change="subsite.save()" />
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+?>
+
+<div class="home-texts">
+    <div class="home-texts__wrapper" v-for="(section, index) in homeTexts" :key="index">
+        <h3 class="home-texts__title">{{ section.sectionName }}</h3>
+        <div class="home-texts__content" ref="homeTextsContent">
+            <div class="field" v-for="(text, textIndex) in section.texts" :key="textIndex">
+                <label :for="text.slug">{{ text.description }}</label>
+                <textarea :id="text.slug" v-model="subsite.homeTexts[text.slug]" @change="subsite.save()"></textarea>
+            </div>
         </div>
     </div>
 </div>
