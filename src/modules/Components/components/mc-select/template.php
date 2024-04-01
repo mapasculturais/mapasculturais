fbd92812a3b07fc83ead63ddad69c3a53f9cdef7
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-04-01 16:18:41 -0300

    Aplica campo de filtro no mc-select e ajusta estilização no mobile (Ref.: #3146 #3133)

--- src/modules/Components/components/mc-select/template.php
+++ src/modules/Components/components/mc-select/template.php
@@ -6,18 +6,30 @@
  */
 
 use MapasCulturais\i;
+$this->import('
+    mc-icon
+');
 ?>
 
 <div :class="['mc-select', {'mc-select--sm' : small }]" :id="uniqueID">
-    <VDropdown :triggers="[]" :shown="open" :autoHide="false" popperClass="mc-select__popper" ref="dropdown" eager-mount :positioning-disabled="$media('max-width: 500px')">
+    <VDropdown :triggers="[]" :shown="open" :autoHide="false" @apply-show="focus()" popperClass="mc-select__popper" ref="dropdown" eager-mount :positioning-disabled="$media('max-width: 500px')">
         <div ref="selected" :class="['mc-select__selected-option', {'mc-select__selected-option--open' : open }]" @click="toggleSelect();">
         </div>
 
         <template #popper ref="popperr">
-            <div ref="options" class="mc-select__options" :class="[{'mc-select__options--groups' : hasGroups}]" @click="selectOption($event)" :id="uniqueID">
-                <slot>
-                    <div v-for="option in selectOptions" :class="option.classes" :value="option.value"> {{option.label}} </div>
-                </slot>
+            <div class="mc-select__dropdown">
+                <div class="mc-select__filter" ref="filter">
+                    <input v-if="!hideFilter" class="mc-select__filter-input" v-model="filter" type="text" placeholder="Filtro" @input="filterOptions()" />
+                    <span class="mc-select__close" @click="closeSelect()">
+                        <mc-icon name="close"></mc-icon>
+                    </span>
+                </div>
+
+                <div ref="options" class="mc-select__options" :class="[{'mc-select__options--groups' : hasGroups}]" @click="selectOption($event)" :id="uniqueID">
+                    <slot>
+                        <div v-for="option in selectOptions" :class="option.classes" :value="option.value"> {{option.label}} </div>
+                    </slot>
+                </div>
             </div>
         </template>
     </VDropdown>
