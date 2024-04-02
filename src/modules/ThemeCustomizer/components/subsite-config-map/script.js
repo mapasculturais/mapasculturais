770b1484b84b59747776a51bab7fc0c47430902f
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-04-02 17:14:34 -0300

    Adiciona campo de busca no componente de configuração do mapa (Ref.: #3142)

--- src/modules/ThemeCustomizer/components/subsite-config-map/script.js
+++ src/modules/ThemeCustomizer/components/subsite-config-map/script.js
@@ -11,6 +11,8 @@ app.component('subsite-config-map', {
         return {
             subsite,
             tileServer: $MAPAS.config.map.tileServer,
+            filter: '',
+            filterResult: [],
         }
     },
 
@@ -18,6 +20,19 @@ app.component('subsite-config-map', {
         zoom() {
             return this.$refs.map.leafletObject._zoom;
         },
+
+        searchResult() {
+            return this.filterResult;
+        },
+
+        centerMap() {
+            return {
+                latitude: this.subsite.latitude ?? 0,
+                longitude: this.subsite.longitude ?? 0,
+                lat: this.subsite.latitude ?? 0,
+                lng: this.subsite.longitude ?? 0,
+            }
+        }
     },
 
     methods: {
@@ -35,6 +50,36 @@ app.component('subsite-config-map', {
         setMinZoom() {
             this.subsite.zoom_min = this.zoom;
             this.subsite.save();
+        },
+
+        setLocation(location) {
+            console.log(location);
+            this.subsite.latitude = location.lat;
+            this.subsite.longitude = location.lon;
+            this.subsite.save();
+        },
+
+        search() {
+            let params = {
+                q: this.filter,
+                format: "json",
+                countrycodes: "br",
+                addressdetails: 1,
+            };
+            
+            const url = 'https://nominatim.openstreetmap.org/search' + this.formatParams(params);
+
+            fetch(url)
+                .then( response => response.json() )
+                .then( response => {
+                    this.filterResult = response.filter((location) => location.addresstype == 'state' || location.addresstype == 'municipality');
+                });
+        },
+
+        formatParams( params ){
+            return "?" + Object.keys(params).map(function(key){
+                            return key+"="+encodeURIComponent(params[key])
+                        }).join("&");
         },
     },
 });
