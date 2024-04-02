f3859526f1cd36274167502cb7dff44a50c6119c
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-04-02 17:14:34 -0300

    Adiciona campo de busca no componente de configuração do mapa (Ref.: #3142)

--- src/modules/ThemeCustomizer/components/subsite-config-map/template.php
+++ src/modules/ThemeCustomizer/components/subsite-config-map/template.php
@@ -19,14 +19,27 @@ $this->import('
 
     <div class="subsite-config-map__content">
         <ul class="subsite-config-map__instructions">
-            <li> <?= i::__('centralize o mapa no centro do ceu município / estado') ?> </li>
-            <li> <?= i::__('utilizando os botões de zoom ( + - ), deixe o mapas exibindo o município/estado inteiro e clique no botão "definir zoom padrão"') ?> </li>
-            <li> <?= i::__('utilizando os botões de zoom ( + - ) aproxime o zoom até onde der pra ver o maior nível de detalhes desejado (por exemplo ler os nomes das ruas) e clique no botão "definir zoom máximo"') ?> </li>
-            <li> <?= i::__('utilizando os botões de zoom ( + - ) afaste o zoom até onde der de ver a região em torno do município / estado e clique no botão "definir zoom mínimo"') ?> </li>
+            <li> <?= i::__('Centralize o mapa no seu município/estado (você pode pesquisar no campo de busca abaixo)') ?> </li>
+            <li> <?= i::__('Utilizando os botões de zoom [+] [-], deixe o mapa exibindo o município/estado inteiro e clique no botão "definir zoom padrão"') ?> </li>
+            <li> <?= i::__('Utilizando os botões de zoom [+] [-], aproxime o zoom até onde der pra ver o maior nível de detalhes desejado (por exemplo, ler os nomes das ruas) e clique no botão "definir zoom máximo"') ?> </li>
+            <li> <?= i::__('Utilizando os botões de zoom [+] [-], afaste o zoom até onde der de ver a região em torno do município/estado e clique no botão "definir zoom mínimo"') ?> </li>
         </ul>
+        
+        <form class="subsite-config-map__filter" @submit="$event.preventDefault(); search();">
+            <div class="field">
+                <label><?= i::__('Pesquisar por Estado ou Município') ?></label>
+                <input type="text" v-model="filter" />
+            </div>
 
-        <div class="subsite-config-map__map" style="width: 100%;">
-            <l-map ref="map" :zoom="5" use-global-leaflet>
+            <button class="button button--primary" type="submit" @click="search()"> <?= i::__('Buscar') ?> </button>
+        </form>
+
+        <div v-if="searchResult.length > 0" class="subsite-config-map__filter-results">
+            <p class="subsite-config-map__filter-result" v-for="result in searchResult" @click="setLocation(result)"> {{result.name}} {{result.addresstype != 'state' ? '- '+result.address.state : ''}} </p>
+        </div>
+
+        <div class="mc-map">
+            <l-map ref="map" :zoom="5" :center="centerMap" use-global-leaflet>
                 <l-tile-layer :url="tileServer"></l-tile-layer>
             </l-map>
         </div>
