2ad31999b89a658301a12a9d3e79b06f8dc5dca2
Welington De Lima Olegario <wolegario49@gmail.com>
2024-04-04 08:43:06 -0300

    Ajusta a maneira de validar se o avatar foi enviado Ref.: #3149

--- src/core/Entities/Registration.php
+++ src/core/Entities/Registration.php
@@ -999,6 +999,10 @@ class Registration extends \MapasCulturais\Entity
             $errorsResult['category'] = [\MapasCulturais\i::__('Categoria é um campo obrigatório.')];
         }
 
+        if($this->opportunity->requestAgentAvatar && !array_key_exists("avatar", $this->owner->files)){
+            $errorsResult['avatar'] = [sprintf(\MapasCulturais\i::__('A imagem avatar do agente "%s" é obrigatório.'),$this->owner->name)];
+        }
+
         $definitionsWithAgents = $this->_getDefinitionsWithAgents();
         
         // validate agents
@@ -1014,10 +1018,6 @@ class Registration extends \MapasCulturais\Entity
             }
 
             if($def->agent){
-                
-                if($opportunity->requestAgentAvatar && !array_key_exists("avatar", $def->agent->files)){
-                    $errors[] = sprintf(\MapasCulturais\i::__('O avatar do agente é obrigatório.'));
-                }
 
                 if($def->relationStatus < 0){
                     $errors[] = sprintf(\MapasCulturais\i::__('O agente %s ainda não confirmou sua participação neste projeto.'), $def->agent->name);
