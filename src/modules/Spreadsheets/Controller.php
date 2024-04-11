2a64960d0db0560e7889edf63d5f04fa3c7649ce
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-04-11 11:25:14 -0300

    Adiciona verificações para o entityClassName (Ref.: #2979)

--- src/modules/Spreadsheets/Controller.php
+++ src/modules/Spreadsheets/Controller.php
@@ -4,25 +4,55 @@ namespace Spreadsheets;
 
 use MapasCulturais\App;
 use MapasCulturais\Entities\Agent;
+use MapasCulturais\Entities\Event;
+use MapasCulturais\Entities\Opportunity;
+use MapasCulturais\Entities\Project;
+use MapasCulturais\Entities\Space;
+use MapasCulturais\Entities\User;
 use MapasCulturais\i;
 
 class Controller extends \MapasCulturais\Controller
-{
-    function GET_exportSpreadsheets()
+{   
+    function getExtension() {
+        /**
+         * @todo fazer a verificação das extensões (csv, xlsx, ods)
+         */
+        return $this->data['extension'] ?? 'csv';
+    }
+
+    function POST_entities()
     {
         $app = App::i();
+        $extension = $this->getExtension();
+        $entity_class_names = [
+            'agent' => Agent::class,
+            'space' => Space::class,
+            'event' => Event::class,
+            'project' => Project::class,
+            'opportunity' => Opportunity::class,
+        ];
 
-        //$extension = $this->data['extension'] ?? 'xlsx';
+        $entity_class_name = $entity_class_names[$this->data['entityType'] ?? 'invalida'] ?? false;
+
+        if (!$entity_class_name) {
+            $this->errorJson(i::__('Tipo de entidade inválida'));
+        }
                 
-        $app->enqueueOrReplaceJob('entities-spreadsheets', [
-            'owner' => $app->user,
-            'authenticatedUser' => $app->user,
-            'extension' => 'xlsx',
-            'entityClassName' => Agent::class,
-            'query' => [
-                '@select' => $this->data['@select'] ?? 'id,name',
-                '@order' => $this->data['@order'] ?? 'id ASC'
-            ] 
-        ]);
+        try {
+            $app->enqueueOrReplaceJob('entities-spreadsheets', [
+                'owner' => $app->user,
+                'authenticatedUser' => $app->user,
+                'extension' => $extension,
+                'entityClassName' => $entity_class_name,
+                'query' => [
+                    '@select' => $this->data['@select'] ?? 'id,name',
+                    '@order' => $this->data['@order'] ?? 'id ASC'
+                ] 
+            ]);
+
+            $this->json(true);
+        } catch(\Throwable $e) {
+            $this->errorJson($e, 500);
+        }
     }
 }
