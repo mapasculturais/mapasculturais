825e89d50a86fbffb8b1e4f9f7935c611a090fbd
Welington De Lima Olegario <wolegario49@gmail.com>
2024-04-26 10:25:19 -0300

    Faz a criação do componente para a configuração do suporte Ref.: #3168

---
+++ src/modules/Support/components/opportunity-support-config/script.js
@@ -0,0 +1,18 @@
+app.component('opportunity-support-config', {
+    template: $TEMPLATES['opportunity-support-config'],
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+    },
+
+    data() {
+        return {}
+    },
+    
+    methods: {
+
+    },
+});
