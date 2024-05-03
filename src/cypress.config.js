c9ce7aed5b3c5fd80cc5ac12235c8c37f17455f3
Lucas Pirola <lucaspirola@gmail.com>
2024-05-03 07:54:16 -0300

    Atualiza configurações do cypress para utilizar versão 13

---
+++ src/cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  e2e: {
+
+    baseUrl: 'https://mapas.tec.br',
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
