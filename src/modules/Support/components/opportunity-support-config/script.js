336618341ff6128d85811e38062a549a9c5852bc
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-03 15:21:07 -0300

    Define com quais os tipos de permissões os agentes pode ter Ref.: #3168

--- src/modules/Support/components/opportunity-support-config/script.js
+++ src/modules/Support/components/opportunity-support-config/script.js
@@ -15,47 +15,113 @@ app.component('opportunity-support-config', {
     },
 
     data() {
+        const fields = $MAPAS.config.opportunitySupportConfig;
         return {
+            allPermissions: null,
             selectAll: false,
-            fields: [
-                { selected: false },
-                { selected: false },
-            ]
+            selectedFields: {},
+            permissionsFields: {},
+            fields,
         };
     },
 
-    watch: {
-        selectAll: function (value) {
-            this.fields.forEach(function (field) {
-                field.selected = value;
-            });
-        },
-
-        fields: {
-            deep: true,
-            handler: function () {
-                var allSelected = this.fields.every(function (field) {
-                    return field.selected;
-                });
-                this.selectAll = allSelected;
-            }
-        },
+    mounted() {
+        this.fields.forEach(field => {
+            this.selectedFields[field.ref] = false;
+        });
     },
 
     computed: {
-        relations(){
-           return this.entity.agentRelations["@support"];
+        relations() {
+            return this.entity.agentRelations["@support"];
+        },
+        
+        query() {
+            let ids = [];
+            let query = {};
+
+            for (let relation of this.relations) {
+                ids.push(relation.agent.id);
+            }
+            if (ids.length > 0) {
+                query['id'] = `!IN(${ids})`;
+            }
+            return query;
+        },
+
+        permissions() {
+            return [
+                {
+                    value: '',
+                    label: this.text('Sem permissão'),
+                },
+                {
+                    value: 'ro',
+                    label: this.text('Visualizar'),
+                },
+                {
+                    value: 'rw',
+                    label: this.text('Modificar'),
+                }
+            ]
         }
     },
-    
+
     methods: {
         addAgent(agent) {
-            this.entity.addRelatedAgent('@support',agent);
+            this.entity.addRelatedAgent('@support', agent);
         },
+
         removeAgent(agent) {
-            this.entity.removeAgentRelation('@support',agent);
+            this.entity.removeAgentRelation('@support', agent);
         },
-        send(){
+
+        setPerssion(event, field) {
+            this.permissionsFields[field.ref] = event.value;
+        },
+
+        setAllPerssions(event) {
+            for (let field in this.selectedFields) {
+                if (this.selectedFields[field]) {
+                    this.permissionsFields[field] = event.value;
+                }
+            }
+        },
+
+        toggleSelectAll(event) {
+            if (event.target.checked) {
+                this.fields.forEach(field => {
+                    this.selectedFields[field.ref] = true;
+                });
+            } else {
+                this.clearSelectedFields();
+            }
+        },
+
+        clearSelectedFields() {
+            this.fields.forEach(field => {
+                this.selectedFields[field.ref] = false;
+            });
+        },
+
+        send(modal, relation) {
+            let api = new API();
+            let url = Utils.createUrl('suporte', 'opportunityPermissions', { agentId: relation.agent.id, opportunityId: this.entity.id });
+
+            api.PUT(url, this.permissionsFields)
+                .then(response => response.json())
+                .then(data => {
+                    for (let relation of this.entity.agentRelations["@support"]) {
+                        if (relation.id == data.id) {
+                            relation.metadata = data.metadata;
+                        }
+                    }
+                    this.clearSelectedFields();
+                    this.messages.success(this.text('Permisão enviada com sucesso'));
+                    this.selectAll = false;
+                    this.allPermissions = null;
+                    modal.close();
+                })
         },
     },
 });
