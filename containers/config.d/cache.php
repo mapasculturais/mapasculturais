dd0835355d1ff8c66f4e0edc57bce464332d617a
Lucas Pirola <lucaspirola@gmail.com>
2024-05-06 17:33:22 -0300

    Adiciona RoadRunner como alternativa de web server

---
+++ containers/config.d/cache.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    // 'app.useRegisteredAutoloadCache' => false,
+    'app.useAssetsUrlCache'          => false,
+    // 'app.useFileUrlCache'            => false,
+    // 'app.useEventsCache'             => false,
+    // 'app.useSubsiteIdsCache'         => false,
+    // 'app.usePermissionsCache'        => false,
+    // 'app.useRegisterCache'           => false,
+    // 'app.useApiCache'                => false,
+];
---
+++ containers/config.d/middlewares.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'middlewares' => [
+        MapasCulturais\Middlewares\ExecutionTime::class
+    ]
+];
