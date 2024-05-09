13599e06700ca2de0ad49b4ae08e5002eb16276a
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-05-09 15:09:23 -0300

    Criação do componente opportunity-evaluations-table (Ref.: #3057)

--- src/modules/Opportunities/components/opportunity-evaluations-table/script.js
+++ src/modules/Opportunities/components/opportunity-evaluations-table/script.js
@@ -1,35 +1,58 @@
-app.component('opportunity-evaluations-table' , {
+app.component('opportunity-evaluations-table', {
     template: $TEMPLATES['opportunity-evaluations-table'],
+
     props: {
         phase: {
             type: Entity,
             required: true
         },
+        
         classes: {
             type: [String, Array, Object],
             required: false
         },
+
         user: {
             type: [String, Number],
             required: true
         }
     },
+
     setup() {
         // os textos estão localizados no arquivo texts.php deste componente
         const text = Utils.getTexts('opportunity-evaluations-table');
         return { text }
     },
-    data () {
-      return {
-      }
+
+    data() {
+        return {
+            query: {
+                '@opportunity': this.phase.opportunity.id,
+            },
+        }
     },
+
+    computed: {
+        headers () {
+            let itens = [
+                { text: __('inscrição', 'opportunity-evaluations-table'), value: "registration.number", slug: "number", sticky: true, width: '160px' },
+                { text: __('agente', 'opportunity-evaluations-table'), value: "registration.owner.name", slug: "agent"},
+                { text: __('resultado final', 'opportunity-evaluations-table'), value: "evaluation.resultString", slug: "result"},
+                { text: __('estado', 'opportunity-evaluations-table'), value: "evaluation.status", slug: "status"},
+            ];
+
+            return itens;
+        },
+    },
+    
     methods: {
-        canSee(action){
-            if(this.phase.opportunity.currentUserPermissions[action]){
+        canSee(action) {
+            if (this.phase.opportunity.currentUserPermissions[action]) {
                 return true;
             }
             return false
         },
+        
         isFuture() {
             return this.phase.evaluationFrom?.isFuture();
         },
@@ -40,6 +63,37 @@ app.component('opportunity-evaluations-table' , {
 
         isPast() {
             return this.phase.evaluationTo?.isPast();
+        },
+
+        rawProcessor(rawData) {
+            const registrationApi = new API('registration');
+            const registration = registrationApi.getEntityInstance(rawData.registration.id);
+
+            registration.populate(rawData.registration, true);
+            registration.evaluation = rawData.evaluation;
+
+            console.log('----------------------------');
+            console.log(rawData);
+            console.log(registration);
+
+            return registration;
+        },
+
+        getStatus(status) {
+            switch(status) {
+                case 0:
+                    return  __('iniciada', 'opportunity-evaluations-table');
+                case 1:
+                    return  __('avaliada', 'opportunity-evaluations-table');
+                case 2:
+                    return  __('enviada', 'opportunity-evaluations-table');
+                default:
+                    return  __('pendente', 'opportunity-evaluations-table');
+            }
+        },
+
+        getResultString(result) {
+            return result ?? __('não avaliado', 'opportunity-evaluations-table');
         }
     }
 });
--- src/modules/Opportunities/components/opportunity-evaluations-table/template.php
+++ src/modules/Opportunities/components/opportunity-evaluations-table/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
@@ -9,26 +10,44 @@ use MapasCulturais\i;
 $this->layout = 'entity';
 
 $this->import('
-    v1-embed-tool
+    mc-status
+    entity-table
 ')
 ?>
 <div :class="['grid-12', classes]">
-    <div class="col-6">
-        <h2 v-if="isPast()"><?= i::__("As avaliações já estão encerradas") ?></h2>
-        <h2 v-if="isHappening()"><?= i::__("As avaliações estão em andamento") ?></h2>
-        <h2 v-if="isFuture()"><?= i::__("As avaliações ainda não iniciaram") ?></h2>
-    </div>
 
     <template v-if="!isFuture()">
-        <div class="col-3" v-if="canSee('sendUserEvaluations') && user == global.auth.user.id">
-            <mc-link :entity="phase.opportunity" route="sendEvaluations" class="button button--primary-outline" :param="phase.opportunity.id"><?= i::__("Enviar avaliações") ?></mc-link>
-        </div>
-        <div class="col-3" v-if="user == 'all'">
-            <mc-link :entity="phase.opportunity" route="reportEvaluations" class="button button--secondarylight" :param="phase.opportunity.id"><?= i::__("Baixar lista de avaliações") ?></mc-link>
-        </div>
+        <div class="col-12">
+            <entity-table controller="opportunity" :raw-processor="rawProcessor" endpoint="findEvaluations" type="registration" :headers="headers" :phase="phase" :visible="['agent', 'number', 'result', 'status']" :query="query" :limit="100" hide-filters> 
+                <template #title>
+                    <h2 v-if="isPast()"><?= i::__("As avaliações já estão encerradas") ?></h2>
+                    <h2 v-if="isHappening()"><?= i::__("As avaliações estão em andamento") ?></h2>
+                    <h2 v-if="isFuture()"><?= i::__("As avaliações ainda não iniciaram") ?></h2>
+                </template>
 
-        <div class="col-12" >
-            <v1-embed-tool route="evaluationlist" :id="phase.opportunity.id" :params="{user}" min-height="600px"></v1-embed-tool>
+                <template #actions="{entities,filters}">
+                    <div class="opportunity-evaluations-table__actions">
+                        <h4 class="bold"><?= i::__('Ações:') ?></h4>
+                        
+                        <div class="opportunity-evaluations-table__actions">
+                            <div v-if="canSee('sendUserEvaluations') && user == global.auth.user.id">
+                                <mc-link :entity="phase.opportunity" route="sendEvaluations" class="button button--primary-outline" :param="phase.opportunity.id"><?= i::__("Enviar avaliações") ?></mc-link>
+                            </div>
+                            <div v-if="user == 'all'">
+                                <mc-link :entity="phase.opportunity" route="reportEvaluations" class="button button--secondarylight" :param="phase.opportunity.id"><?= i::__("Baixar lista de avaliações") ?></mc-link>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+
+                <template #result="{entity}">
+                    {{getResultString(entity.evaluation?.resultString)}}
+                </template>
+
+                <template #status="{entity}">
+                    <mc-status :status-name="getStatus(entity.evaluation?.status)"></mc-status>
+                </template>
+            </entity-table>
         </div>
     </template>
 </div>
