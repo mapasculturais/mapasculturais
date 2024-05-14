436c4902606121e192bc0533f8f749a9a1dcbf15
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-05-14 13:42:45 -0300

    Ajustes no componente de configurações do suporte (Ref.: #3168)

--- src/modules/Support/components/opportunity-support-config/init.php
+++ src/modules/Support/components/opportunity-support-config/init.php
@@ -7,12 +7,14 @@ $files = $entity->registrationFileConfigurations;
 
 $fields_list = array_merge($fields, $files);
 
+
 foreach ($fields_list as $field) {
     $result[] = [
         "id" => $field->id,
         "title" => $field->title,
         "ref" => $field->fieldType == "text" ? $field->fieldName : $field->fileGroupName,
-        "typeDescription" => $field->fieldType == "text" ? $field->fieldType : "Arquivo",
+        "typeDescription" => $field->fieldType == "text" ? "Texto" : "Arquivo",
+        "type" => $field->fieldType == "text" ? "text" : "file",
     ];
 }
 $this->jsObject['config']['opportunitySupportConfig'] = $result;
