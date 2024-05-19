7fd30a0e2648f82497b9d27a5b2d57e78a508b46
fns21 <fabinhosilva7@outlook.com>
2024-05-18 23:44:19 -0300

    fixing e2e register page bugs

--- src/cypress/e2e/registerPage/index.cy.js
+++ src/cypress/e2e/registerPage/index.cy.js
@@ -1,5 +1,8 @@
 const { confirmRecaptcha } = require("../../commands/recaptcha");
 const { loginWith } = require("../../commands/login");
+const { generateCPF } = require("../../commands/genCPF");
+const { generateString} = require("../../commands/genString")
+
 describe("LoginPage", () => {
     beforeEach(() => {
         cy.visit("/");
@@ -12,17 +15,13 @@ describe("LoginPage", () => {
 });
 
 describe("RegisterPage", () => {
-    beforeEach(() => {
-        cy.visit("/autenticacao/");
-    });
-
     it("click no botão \"Fazer cadastro\"", () => {
+        cy.visit("/autenticacao/");
         cy.contains("Fazer cadastro").click();
         cy.url().should("include", "/autenticacao/register/");
     });
 
-    // Ao gerar fazer um novo teste de cadastro, é necessário alterar o email e cpf, pois já foram cadastrados.
-    it("click nos campos de cadastro, captcha e no botão \"Continuar\" para continuar cadastro", () => {
+    it("Garante que não haja contas com o mesmo cpf ou email", () => {
         cy.visit("/autenticacao/register/");
         cy.get("#email").type("fake3@email.com");
         cy.get("#email").should("have.value", "fake3@email.com");
@@ -37,9 +36,56 @@ describe("RegisterPage", () => {
         cy.wait(2000);
         cy.contains("Continuar").click();
 
+        cy.wait(1000);
+
+        cy.contains("Este CPF já esta em uso. Tente recuperar a sua senha.");
+        cy.contains("Este endereço de email já está em uso. Tente recuperar a sua senha.");
+    });
+
+    it("Garante que cpf seja valido", () => {
+        cy.visit("/autenticacao/register/");
+        cy.get("#email").type("fake4@email.com");
+        cy.get("#email").should("have.value", "fake4@email.com");
+        cy.get("#cpf").type("68861193541");
+        cy.get("#pwd").type("Fakepassword@10");
+        cy.get("#pwd").should("have.attr", "type", "password");
+        cy.get(".seePassword").click({ multiple: true });
+        cy.get("#pwd").should("have.value", "Fakepassword@10");
+        cy.get("#pwd-check").type("Fakepassword@10");
+        cy.wait(1000);
+        confirmRecaptcha();
         cy.wait(2000);
+        cy.contains("Continuar").click();
+
+        cy.wait(1000);
+
+        cy.contains("Por favor, informe um cpf válido.");
+    });
+
+    
+    it("Continuar cadastro", () => {
+        cy.visit("/autenticacao/register/");  
+
+        let email = generateString(5) + "@email.com";  
+        cy.get("#email").type(email);
+        cy.get("#email").should("have.value", email);
+
+        let cpf = generateCPF();
+        cy.get("#cpf").type(cpf);
+
+        cy.get("#pwd").type("Fakepassword@10");
+        cy.get("#pwd").should("have.attr", "type", "password");
+        cy.get(".seePassword").click({ multiple: true });
+        cy.get("#pwd").should("have.value", "Fakepassword@10");
+        cy.get("#pwd-check").type("Fakepassword@10");
+        cy.wait(1000);
+        confirmRecaptcha();
         cy.wait(2000);
-        cy.get(".field__title ~ input").type("Maria");
+        cy.contains("Continuar").click();
+
+        cy.wait(4000);
+
+        cy.get(".field__title ~ input").type("Joao");
         cy.get(".field__shortdescription textarea").type("Test description");
         cy.get(".v-popper > .button").click();
         cy.contains("Arte Digital").click();
@@ -51,6 +97,6 @@ describe("RegisterPage", () => {
         cy.wait(2000);
         cy.contains("Acessar meu cadastro").click();
 
-        loginWith("fake3@email.com", "Fakepassword@10");
+        loginWith(email, "Fakepassword@10");
     });
 });
