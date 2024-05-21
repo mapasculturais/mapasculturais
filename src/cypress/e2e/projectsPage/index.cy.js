1ffd78e16cbddf46d3d0c0c63d2ad7d1ddece5a6
Caio <caioshadow2014@gmail.com>
2024-05-20 21:06:39 -0300

    Remove 'beforeEach' em 'mapas/src/cypress/e2e/projectsPage/index.cy.js'

--- src/cypress/e2e/projectsPage/index.cy.js
+++ src/cypress/e2e/projectsPage/index.cy.js
@@ -1,6 +1,7 @@
 describe("Projeto", () => {
     it("Garante que o projeto seja clicável", () => {
         cy.visit("/");
+        
         cy.get(".mc-header-menu__btn-mobile").click();
         cy.contains(".mc-header-menu__itens a", "Projetos").click();
         cy.url().should("include", "projetos");
