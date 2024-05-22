25cfe99a8396c3e512b115791dfc91cac89ffcf3
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-05-22 15:21:03 -0300

    feat: add configurações paineis agentes e espaços dashboard e home

---
+++ dev/config.d/Metabase.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+     'Metabase' => [
+          'config' => [
+              'links' => [
+                  'painel-agentes' => [
+                      'link' => 'https://metabase.mapas.tec.br/public/dashboard/898173be-47ae-4987-894c-b7e54914539c',
+                      'text' => 'Saiba os números de agentes individuais cadastrados, quantos são criados mensalmente, por onde estão distribuídos no território e outras informações.',
+                      'title' => 'Painel sobre agentes individuais'
+                  ],
+                  'painel-espacos' => [
+                    'link' => 'https://metabase.mapas.tec.br/public/dashboard/ea36c1b3-c3f3-4435-afc6-9faeea4c7c8f',
+                    'text' => 'Saiba os números de espaços cadastrados, quantos são criados mensalmente, por onde estão distribuídos no território e outras informações.',
+                    'title' => 'Painel sobre espaços'
+                ],
+            ],
+          ]
+      ]
+];
