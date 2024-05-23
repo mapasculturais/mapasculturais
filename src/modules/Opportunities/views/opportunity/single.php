ebe33db2c27a29209802050fb2c982bf8c2d52a5
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-05-23 12:34:33 -0300

    Corrige quebra de linha na descrição detalhada das entidades (Ref.: #2644)

--- src/modules/Opportunities/views/opportunity/single.php
+++ src/modules/Opportunities/views/opportunity/single.php
@@ -89,9 +89,9 @@ $this->breadcrumb = [
             <mc-container>
                 <main>
                     <div class="grid-12">
-                        <div class="col-12">
+                        <div v-if="entity.longDescription" class="col-12">
                             <h3><?= i::__("Apresentação") ?></h3>
-                            <p v-html="entity.longDescription"></p>
+                            <p class="description" v-html="entity.longDescription"></p>
                         </div>
                         <opportunity-rules :entity="entity" classes="col-12" title="<?php i::esc_attr_e('Regulamento'); ?>"></opportunity-rules>
                         <entity-files-list :entity="entity" classes="col-12" group="downloads" title="<?php i::esc_attr_e('Arquivos para download');?>"></entity-files-list>
