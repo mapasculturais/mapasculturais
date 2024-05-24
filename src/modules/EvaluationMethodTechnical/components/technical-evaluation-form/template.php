9358e5333216d6fc93dbffb5ffe2d987f04e0731
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-24 10:46:08 -0300

    Remove botão desnecessário Ref.: #3184

--- src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/template.php
+++ src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/template.php
@@ -11,7 +11,7 @@ use MapasCulturais\i;
         <h3>{{ section.name }}</h3>
         <div v-for="criterion in section.criteria" :key="criterion.id">
             <label>{{ criterion.title }}
-                <input v-model.number="formData.data[criterion.id]" min="0" type="number" @input="handleInput(sectionIndex, criterion.id)">
+                <input v-model.number="formData.data[criterion.id]" min="0" step="0.1" type="number" @input="handleInput(sectionIndex, criterion.id)">
             </label>
         </div>
         <div>
@@ -24,19 +24,17 @@ use MapasCulturais\i;
         <textarea v-model="formData.data.obs" rows="10" cols="30"></textarea>
     </div>
     <h4><strong><?php i::_e('Exequibilidade orçamentária') ?></strong></h4>
-    <p><?php i::_e('Esta proposta esta adequada ao orçamento apresentado? Os custos orçametários estão compatíveis com os praticados no mercado?') ?></p>
+    <p><?php i::_e('Esta proposta está adequada ao orçamento apresentado? Os custos orçamentários estão compatíveis com os praticados no mercado?') ?></p>
     <div>
         <input v-model="formData.data.viability" type="radio" name="confirmation" value="valid" /> <?php i::_e('Sim') ?>
         <input v-model="formData.data.viability" type="radio" name="confirmation" value="invalid" /> <?php i::_e('Não') ?>
     </div>
     <div>
         <div>
-            <label><?php i::_e('Pontuação total:') ?> {{notesResult}}</label>
-
+            <label><?php i::_e('Pontuação total:') ?> {{ notesResult }}</label>
         </div>
         <div>
-            <label><?php i::_e('Pontuação máxima:') ?> {{totalMaxScore}}</label>
+            <label><?php i::_e('Pontuação máxima:') ?> {{ totalMaxScore }}</label>
         </div>
-        <button @click="saveEvaluation()">Enviar</button>
     </div>
 </div>
