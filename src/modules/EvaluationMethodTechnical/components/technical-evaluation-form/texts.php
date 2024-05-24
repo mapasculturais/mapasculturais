d1fa50318195a4ff5c2c74121c0bf233d28281c9
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-24 16:22:23 -0300

    Adiciona as novas mensagem de erros Ref.: #3184

--- src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/texts.php
+++ src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/texts.php
@@ -6,4 +6,6 @@ return [
     'evaluation-successfully' => i::__('Avaliação enviada com sucesso'),
     'note-higher-configured' => i::__('Nota maior que a configurada para avaliação'),
     'technical-mandatory' => i::__('O campo Parecer técnico é obrigatório.'),
+    'technical-checkViability' => i::__('O campo Exequibilidade orçamentária tem que ser selecionado.'),
+    'mandatory-note' => i::__('nota obrigatoria'),
 ];
