0cd561ebfeb2177f3750e169e4ceb22bb6a7a2cf
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-24 10:44:09 -0300

    Adiciona nova mensagem de erro para a validação Ref.: #3184

--- src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/texts.php
+++ src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/texts.php
@@ -5,4 +5,5 @@ use MapasCulturais\i;
 return [
     'evaluation-successfully' => i::__('Avaliação enviada com sucesso'),
     'note-higher-configured' => i::__('Nota maior que a configurada para avaliação'),
+    'technical-mandatory' => i::__('O campo Parecer técnico é obrigatório.'),
 ];
