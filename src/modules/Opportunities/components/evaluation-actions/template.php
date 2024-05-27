5631a513ebf8b663dfbb2a4ce1508ca2e8abcd0b
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-05-27 09:48:48 -0300

    feat: implementa botões de ação dos formulários de avaliação. Ref: #3189

---
+++ src/modules/Opportunities/components/evaluation-actions/template.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    v1-embed-tool
+    mc-icon
+    mc-modal
+');
+?>
+
+<div class="evaluation-actions" v-if="evaluationRegistrationList">
+    <div class="grid-12">
+        <div class="col-12" v-if="showActions(entity, 'finishEvaluation')">
+            <button class="button button--primary button--large" @click="finishEvaluation(entity)">
+                <?= i::__('Finalizar avaliação') ?>
+            </button>
+        </div>
+        <div class="col-12" v-if="showActions(entity, 'finishEvaluation')">
+            <mc-modal button-label="Finalizar e Avançar" title="<?= i::__('Avaliação feita!') ?>">
+                <template #default>
+                    <div class="finish-send-evaluation__text">
+                        <span class="finish-send-evaluation__span"><?= i::__('Agora é necessário enviar essa avaliação para a pessoa gestora. Você pode enviar uma por uma ou todas de uma só vez.') ?></span>
+                    </div>
+                </template>
+
+                <template #actions="modal">
+                    <button class="button button--text button--text-del" @click="finishEvaluationSendLater(entity); modal.close()"><?= i::__('Enviar Depois') ?></button>
+                    <button class="button button--primary" @click="finishEvaluationSend(entity); modal.close()"><?= i::__('Enviar agora') ?></button>
+                </template>
+                <template #button="modal">
+                    <button class="button button--primary button--icon button--large" @click="modal.open()">
+                        <span v-if="lastRegistration?.registrationid != entity.id"><?= i::__('Finalizar e avançar') ?></span>
+                        <span v-if="lastRegistration?.registrationid == entity.id"><?= i::__('Finalizar e enviar') ?></span>
+                        <mc-icon name="arrow-right-ios"></mc-icon>
+                    </button>
+                </template>
+            </mc-modal>
+        </div>
+
+        <div class="col-12" v-if="showActions(entity, 'reopen')">
+            <button class="button button--primary button--large button--large" @click="reopen(entity)"> <?= i::__('Reabrir avaliação') ?> </button>
+        </div>
+        <div class="col-12" v-if="showActions(entity, 'send')">
+            <button class="button button--primary button--icon button--large evaluation-actions__buttons__send" @click="finishEvaluationSend(entity)">
+                <?= i::__('Enviar avaliação') ?>
+                <mc-icon name="send"></mc-icon>
+            </button>
+        </div>
+        
+        <div class="col-12" v-if="showActions(entity, 'save')">
+            <button class="button button--primary button--large evaluation-actions__buttons__saveAfter" @click="saveEvaluation()">
+                <?php i::_e('Salvar e continuar depois') ?>
+            </button>
+        </div>
+
+        <div class="col-6">
+            <button v-if="firstRegistration?.registrationid != entity.id" class="button button--primary-outline button--icon button--large" @click="previous()">
+                <mc-icon name="arrow-left-ios"></mc-icon>
+                <?= i::__('Anterior') ?>
+            </button>
+        </div>
+        <div class="col-6">
+            <button v-if="lastRegistration?.registrationid != entity.id" class="button button--primary-outline button--icon button--large" @click="next()">
+                <?= i::__('Próximo') ?>
+                <mc-icon name="arrow-right-ios"></mc-icon>
+            </button>
+        </div>
+    </div>
+</div>
