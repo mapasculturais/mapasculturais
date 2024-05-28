662ae893edabb6e068e737177b5f55a91a0e9065
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-28 19:32:25 -0300

    Adiciona mensagem de erros Ref.: #3184

---
+++ src/modules/EvaluationMethodTechnical/components/technical-assessment-section/texts.php
@@ -0,0 +1,8 @@
+<?php
+
+use MapasCulturais\i;
+
+return [
+'criterion-field' => i::__('O Campo criterio ou nota precisar estar preenchido'),
+'name-section' => i::__('O campo nome da sessão precisa ser preenchido'),
+];
