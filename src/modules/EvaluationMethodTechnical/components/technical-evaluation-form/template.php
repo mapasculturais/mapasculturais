0724fdf00b704083962e961bcd8ce1796a42a20e
Welington De Lima Olegario <wolegario49@gmail.com>
2024-05-28 17:49:27 -0300

    Estiliza componente do furmulario de avaliação tecninca Ref.: #3184

--- src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/template.php
+++ src/modules/EvaluationMethodTechnical/components/technical-evaluation-form/template.php
@@ -7,38 +7,42 @@ $this->import('
 ')
 ?>
 
-<div>
+<div class="tecnical-evaluation-form">
     <div>
         <h4><?php i::_e('Insira as notas nos campos abaixo') ?></h4>
     </div>
-    <div v-for="(section, sectionIndex) in sections" :key="section.name">
-        <h3>{{ section.name }}</h3>
-        <div v-for="criterion in section.criteria" :key="criterion.id">
-            <label>{{ criterion.title }}
-                <input v-model="formData.data[criterion.id]" min="0" step="0.1" type="number" @input="handleInput(sectionIndex, criterion.id)">
-            </label>
+    <div class="tecnical-evaluation-form__fields" v-for="(section, sectionIndex) in sections" :key="section.name">
+        <h3><strong>{{ section.name }}</strong></h3>
+        <div class="field" v-for="criterion in section.criteria" :key="criterion.id">
+            <label>{{ criterion.title }}</label>
+            <input v-model="formData.data[criterion.id]" min="0" step="0.1" type="number" @input="handleInput(sectionIndex, criterion.id)">
         </div>
         <div>
-            <label>SubTotal: {{ subtotal(sectionIndex) }}</label>
+            <label><strong>{{ subtotal (sectionIndex) }}</strong></label>
         </div>
     </div>
-    <hr>
-    <div>
-        <h3><?php i::_e('Informe o Parecer técnico') ?></h3>
-        <textarea v-model="formData.data.obs" rows="10" cols="30"></textarea>
+
+    <div class="tecnical-evaluation-form__textarea-adjusted">
+        <p><strong><?php i::_e('Informe o Parecer técnico') ?></strong></p>
+        <textarea v-model="formData.data.obs" rows="10" cols="36"></textarea>
     </div>
-    <div v-if="enableViability">
+    
+    <div class="tecnical-evaluation-form__viability-radio-group" v-if="enableViability">
         <h4><strong><?php i::_e('Exequibilidade orçamentária') ?></strong></h4>
         <p><?php i::_e('Esta proposta está adequada ao orçamento apresentado? Os custos orçamentários estão compatíveis com os praticados no mercado?') ?></p>
-        <input v-model="formData.data.viability" type="radio" name="confirmation" value="valid" /> <?php i::_e('Sim') ?>
-        <input v-model="formData.data.viability" type="radio" name="confirmation" value="invalid" /> <?php i::_e('Não') ?>
+        <label>
+            <input v-model="formData.data.viability" type="radio" name="confirmation" value="valid" /> <?php i::_e('Sim') ?>
+        </label>
+        <label>
+            <input v-model="formData.data.viability" type="radio" name="confirmation" value="invalid" /> <?php i::_e('Não') ?>
+        </label>
     </div>
-    <div>
+    <div class="tecnical-evaluation-form__label-nolts-result">
         <div>
-            <label><?php i::_e('Pontuação total:') ?> {{ notesResult }}</label>
+            <label><?php i::_e('Pontuação total:') ?> <strong>{{ notesResult }}</strong></label>
         </div>
         <div>
-            <label><?php i::_e('Pontuação máxima:') ?> {{ totalMaxScore }}</label>
+            <label><?php i::_e('Pontuação máxima:') ?> <strong>{{ totalMaxScore }}</strong></label>
         </div>
     </div>
 
