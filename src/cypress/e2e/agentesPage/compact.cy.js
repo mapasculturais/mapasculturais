100934a62e057a508f89e4796e9e7a029c8d72a8
fns21 <fabinhosilva7@outlook.com>
2024-05-31 02:20:32 -0300

    teste screenshot

--- src/cypress/e2e/agentesPage/compact.cy.js
+++ src/cypress/e2e/agentesPage/compact.cy.js
@@ -17,6 +17,6 @@ describe("Pagina de Agentes", () => {
 
     it("clica em \"Acessar\" e entra na pagina no agente selecionado", () => {
         cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
-        cy.contains("Informações");
+        cy.contains("Informaçõeuhyg");
     });
 });
