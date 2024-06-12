82e77d69f61343c87e433f6df9ae8224c4c8ae8d
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-06-12 15:53:41 -0300

    feat: testes tratamento de registros por páginas nas oportunidades

--- src/cypress/commands/checkFilterCountOf.js
+++ src/cypress/commands/checkFilterCountOf.js
@@ -1,26 +1,28 @@
 /* Checa a contagem de um elemento específico na página de filtros. */
 function checkFilterCountOf(element) {
         cy.get('.foundResults').then(($foundResults) => {
+                const countPerPage = 20;
                 var resultsTextArray = $foundResults.text().split(" ");
                 var resultsCount = Number(resultsTextArray[0]);
+                const resultsCountPerPage = resultsCount < countPerPage ? resultsCount : countPerPage;
 
                 switch (element) {
                         case "opportunity":
-                                cy.get(".upper." + element + "__color").should("have.length", resultsCount);
+                                cy.get("span.upper." + element + "__color").should("have.length", resultsCountPerPage);
                                 cy.wait(1000);
                                 cy.contains(resultsCount + " Oportunidades encontradas");
                                 
                                 break;
                         
                         case "project":
-                                cy.get(".upper." + element + "__color").should("have.length", resultsCount);
+                                cy.get("span.upper." + element + "__color").should("have.length", resultsCountPerPage);
                                 cy.wait(1000);
                                 cy.contains(resultsCount + " Projetos encontrados");
         
                                 break;
                         
                         case "space":
-                                cy.get(".upper." + element + "__color").should("have.length", resultsCount);
+                                cy.get("span.upper." + element + "__color").should("have.length", resultsCountPerPage);
                                 cy.wait(1000);
                                 cy.contains(resultsCount + " Espaços encontrados");
                 
