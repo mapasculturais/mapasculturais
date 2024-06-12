a5e23cbd48f89c52bfba8a394ce6a4c6674e6993
erleibiazzio <erleibiazzio@gmail.com>
2024-06-12 12:17:28 -0300

    Evita que os botões de baixar rascunhos e baixar lista de avaliações sejam exibidos com usuario deslogado

--- src/modules/Opportunities/components/opportunity-results-table/template.php
+++ src/modules/Opportunities/components/opportunity-results-table/template.php
@@ -11,7 +11,7 @@ $this->import('
 ');
 ?>
 
-<opportunity-registrations-table :phase="lastPhase" :visible-columns="visibleColumns" :avaliable-columns="visibleColumns" hide-filters hide-sort status-not-editable>
+<opportunity-registrations-table :phase="lastPhase" :visible-columns="visibleColumns" :avaliable-columns="visibleColumns" hide-filters hide-sort status-not-editable hide-actions>
     <template #title>
         <b><?= i::__("Clique no número de uma inscrição para conferir todas as avaliações realizadas.") ?></b>
     </template>
