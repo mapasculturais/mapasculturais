3c3c20724eba30ff24f6f89702564b43344bcc15
Welington De Lima Olegario <wolegario49@gmail.com>
2024-06-27 15:10:33 -0300

    Adiciona a rede social tiktok no project-types Ref.: #3202

--- src/conf/project-types.php
+++ src/conf/project-types.php
@@ -157,6 +157,18 @@ return array(
             'placeholder' => "nomedousuario",
             'available_for_opportunities' => true
         ),
+        'tiktok' => array(
+            'type' => "socialMedia",
+            'label' => \MapasCulturais\i::__('Tiktok'),
+            'serialize' =>function($value){
+                return Utils::parseSocialMediaUser('tiktok.com', $value);
+            },
+            'validations' => array(
+                "v::oneOf(v::urlDomain('tiktok.com'), v::regex('/^@?([-\w\d\.]+)$/i'))" => \MapasCulturais\i::__("O valor deve ser uma URL ou usuário válido.")
+            ),
+            'placeholder' => "nomedousuario",
+            'available_for_opportunities' => true
+        ),
 
         'emailPublico' => array(
             'label' => \MapasCulturais\i::__('Email Público'),
