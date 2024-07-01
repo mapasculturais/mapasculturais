0c3024dab222a7b735204e3c1e06e8270848bb3c
israelmelo <israelg.8@gmail.com>
2024-07-01 09:25:15 -0300

    Criação do componente entity-renew-lock para renovar o token a cada 45 segundos Ref.: #3216

---
+++ src/modules/Entities/components/entity-renew-lock/script.js
@@ -0,0 +1,42 @@
+app.component('entity-renew-lock', {
+    template: $TEMPLATES['entity-renew-lock'],
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true,
+        }
+    },
+
+    data() {
+        return {
+            token: $MAPAS.lockToken ?? null
+        }
+    },
+
+    methods: {
+        renewLock() {
+            const messages = useMessages();
+
+            this.entity.POST('renew', {
+                data: {token: this.token}, callback: data => {}
+            }).catch((data) => {
+                messages.error(data.data);
+
+                setTimeout(function() {
+                    window.location.href = document.location.origin;
+                }, 3000);
+            });
+        }
+    },
+
+    mounted() {
+        setInterval(() => {
+            this.renewLock();
+        }, 45000);
+
+        const url = this.entity.getUrl($MAPAS.request.action, {token: this.token}).toString();
+        window.history.replaceState({}, '',  url + document.location.hash);
+    }
+    
+});
