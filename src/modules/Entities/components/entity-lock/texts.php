a6c627c1bbcd38ee6836de01c0a443b1a83b2949
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-07-10 14:54:51 -0300

    Aplica estilização no frontend do bloqueio de edição multipla de entidades (Ref.: #3216)

--- src/modules/Entities/components/entity-lock/texts.php
+++ src/modules/Entities/components/entity-lock/texts.php
@@ -2,5 +2,10 @@
 use MapasCulturais\i;
 
 return [
-    'as' => i::__('às')
+    'as' => i::__('às'),
+    'opportunity' => i::__('Esta oportunidade'),
+    'agent' => i::__('Este agente'),
+    'event' => i::__('Este evento'),
+    'space' => i::__('Este espaço'),
+    'project' => i::__('Este projeto'),
 ];
