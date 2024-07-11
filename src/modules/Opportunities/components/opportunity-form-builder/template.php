7f36eca24d3968891963a55dde006fed056a2561
erleibiazzio <erleibiazzio@gmail.com>
2024-07-11 16:04:08 -0300

    Insere autosave no requestAgentAvatar

--- src/modules/Opportunities/components/opportunity-form-builder/template.php
+++ src/modules/Opportunities/components/opportunity-form-builder/template.php
@@ -98,7 +98,7 @@ $this->import('
                         <h4 class="input-group__title"><?= i::__("Solicitar avatar no agente responsável") ?></h4>
                         <h6 class="input-group__subtitle"><?= i::__("Solicita ao usuário que insira um avatar") ?></h6>
                         <div class="input-group__inputs">
-                            <entity-field class="input-box" :entity="entity" hide-required  :editable="true" prop="requestAgentAvatar"></entity-field>
+                            <entity-field class="input-box" :entity="entity" hide-required  :editable="true" prop="requestAgentAvatar" autosave="3000"></entity-field>
                         </div>
                     </div>
                 </template>
