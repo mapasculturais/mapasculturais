668f36405c34ef411d192477b5754e7df1a9db3b
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-19 18:01:28 -0300

    ajusta template para poder receber os dados de forma correta Ref.: #3226

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/template.php
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/template.php
@@ -3,15 +3,18 @@
     <div v-for="section in sections" :key="section.id">
         <h3>{{ section.name }}</h3>
         <div v-for="crit in section.criteria" :key="crit.id">
-            <label>
-                {{ crit.name }}
-            </label>
-            <div>
-                <select>
-                    <option>selecione</option>
-                    <option v-for="option in crit.options" :key="option">{{ option }}</option>
+            <label>{{ crit.name }}</label>
+            <div>  
+                <select v-model="formData.data[crit.id]" @change="handleChange(section.id)">
+                    <option value="">selecione</option>
+                    <option v-for="option in crit.options" :key="option" :value="option">{{ option }}</option>
                 </select>
             </div>
         </div>
+        <label>Resultado da seção: {{ formData.sectionStatus[section.id] || section.status }}</label>
+    </div>
+    <div>
+        <p>Observações</p>
+        <textarea v-model="formData.data.obs"></textarea>
     </div>
 </div>
