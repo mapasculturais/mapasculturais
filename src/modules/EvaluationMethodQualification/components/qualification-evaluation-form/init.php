54abdb9683c173dffb96299061f3b09f5864923a
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-19 17:54:41 -0300

    Ajuste o init.php para receber o status das avaliações, a fim de possibilitar a avaliação. Ref.: #3226

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/init.php
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/init.php
@@ -5,33 +5,55 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
+use MapasCulturais\i;
+
 $entity = $this->controller->requestedEntity;
 
 $sections = $entity->opportunity->evaluationMethodConfiguration->sections;
 $criteria = $entity->opportunity->evaluationMethodConfiguration->criteria;
-
 $data = [];
+
+$statusList = [
+    ['value' => '2', 'label' => i::__('Inválida')],
+    ['value' => '3', 'label' => i::__('Não selecionada')],
+    ['value' => '8', 'label' => i::__('Suplente')],
+    ['value' => '10', 'label' => i::__('Selecionada')],
+];
 
 foreach ($sections as $section) {
     $sectionData = [
         'id' => $section->id,
         'name' => $section->name,
-        'criteria' => []
+        'criteria' => [],
+        'status' => 'não avalidada'
     ];
-
+    
     foreach ($criteria as $crit) {
         if ($crit->sid === $section->id) {
+            
+            $critStatus = isset($crit->status) ? $crit->status : 'não avalidada';
+            
             $sectionData['criteria'][] = [
                 'id' => $crit->id,
                 'sid' => $crit->sid,
                 'name' => $crit->name,
-                'description' => $crit->description,
-                'options' => $crit->options,
+                'description' => isset($crit->description) ? $crit->description : '',
+                'options' => isset($crit->options) ? $crit->options : [],
+                'status' => $critStatus
             ];
+            
+            if ($critStatus === 'avalidada') {
+                $sectionData['status'] = 'avalidada';
+            } elseif ($critStatus === 'suplente' && $sectionData['status'] !== 'avalidada') {
+                $sectionData['status'] = 'suplente';
+            }
         }
     }
-
+    
     $data[] = $sectionData;
 }
 
-$this->jsObject['config']['qualificationEvaluationForm'] = ['sections' => $data];
+$this->jsObject['config']['qualificationEvaluationForm'] = [
+    'sections' => $data,
+    'statusList' => $statusList,
+];
