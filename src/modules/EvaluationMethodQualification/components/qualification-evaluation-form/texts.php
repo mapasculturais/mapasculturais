398f50be8b2e9cd845b396554b13b0e872882d4f
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-23 17:25:47 -0300

    Criação de mensagem de validação Ref.: #3226

---
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/texts.php
@@ -0,0 +1,10 @@
+<?php
+
+use MapasCulturais\i;
+
+return [
+    'technical-mandatory' => i::__('O campo Observação é obrigatório.'),
+    'the_field' => i::__('O campo'),
+    'is_required' => i::__('é obrigatório'),
+    'on_section' => i::__('Na sessão'),
+];
