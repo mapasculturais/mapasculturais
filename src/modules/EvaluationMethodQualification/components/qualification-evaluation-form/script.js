8d5560bf0f9f2ba2c5bc5e37120c354fc82c0003
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-23 17:39:42 -0300

    Faz com que retorna os dados vindo do banco de dados Ref.: #3226

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/script.js
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/script.js
@@ -6,6 +6,10 @@ app.component('qualification-evaluation-form', {
         const text = Utils.getTexts('qualification-evaluation-form');
         return { text, messages };
     },
+    created() {
+        this.formData['data'] = this.evaluationData || this.skeleton();
+        this.formData.uid = this.userId;
+    },
 
     props: {
         entity: {
@@ -20,13 +24,31 @@ app.component('qualification-evaluation-form', {
                 sectionStatus: {},
                 obs: '',
                 data: {},
-
             },
+            evaluationId: null,
         };
     },
+    
     computed: {
         sections() {
             return $MAPAS.config.qualificationEvaluationForm.sections || [];
+
+        },
+        status() {
+            return $MAPAS.config.qualificationEvaluationForm.evaluationData || [];
+        },
+        statusText() {
+            const statusMap = {
+                0: 'Não Enviada',
+                1: 'Em andamento',
+                2: 'Enviada',
+            };
+
+            return statusMap[this.status];
+        },
+        evaluationData() {
+            const evaluation = $MAPAS.config.qualificationEvaluationForm.evaluationData;
+            return evaluation && evaluation.evaluationData ? evaluation.evaluationData : {};
         },
         userId() {
             return $MAPAS.userId;
