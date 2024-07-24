d318d582b8f28770778a4f66b72585f2590e1846
erleibiazzio <erleibiazzio@gmail.com>
2024-07-24 10:55:28 -0300

    Corrige erro no endpoint sendEvaluations() ao finalizar processo

--- src/core/Controllers/Opportunity.php
+++ src/core/Controllers/Opportunity.php
@@ -104,7 +104,8 @@ class Opportunity extends EntityController {
         if($this->isAjax()){
             $this->json($opportunity);
         }else{
-            $app->redirect($app->request->getReferer());
+            $referer = $app->request->getReferer();
+            $app->redirect($referer[0]);
         }
     }
 
