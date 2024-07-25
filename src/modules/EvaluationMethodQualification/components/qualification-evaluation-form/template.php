5ca2f46578f33d9de8089891b111003ba7936158
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-25 18:01:42 -0300

    Ajusta o template para poder desabilitar e habilitar campos apos o envio da avaliação Ref.: #3226

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/template.php
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/template.php
@@ -4,6 +4,7 @@ use MapasCulturais\i;
 
 $this->import('
     evaluation-actions
+    mc-select
 ')
 ?>
 <div>
@@ -13,23 +14,24 @@ $this->import('
         <div v-for="crit in section.criteria" :key="crit.id">
             <label>{{ crit.name }}</label>
             <div>
-                <select v-model="formData.data[crit.id]" @change="handleChange(section.id)">
+                <mc-select v-if="isEditable" v-model="formData.data[crit.id]" @change-option="handleChange(section.id, crit.id, $event)" :disabled="!isEditable">
                     <option value=""><?php i::_e('Selecione') ?></option>
                     <option v-if="crit.notApplyOption == 'true'" value="Não se aplica"><?php i::_e('Não se aplica') ?></option>
                     <option value="Habilitado"><?php i::_e('Habilitado') ?></option>
                     <option value="Inabilitado"><?php i::_e('Inabilitado') ?></option>
                     <option v-for="option in crit.options" :key="option" :value="option">{{ option }}</option>
-                </select>
+                </mc-select>
+                <input v-if="!isEditable" type="text" :value="formData.data[crit.id]" disabled>
             </div>
         </div>
-        <label><?php i::_e('Resultado da seção:') ?> {{ formData.sectionStatus[section.id] || section.status }}</label>
+        <label><?php i::_e('Resultado da seção:') ?> </label>
     </div>
     <div v-if="statusText">
         <label><?php i::_e('Resultado da seção:') ?> {{statusText}}</label>
     </div>
     <div>
         <p><?php i::_e('Observações') ?></p>
-        <textarea v-model="formData.data.obs"></textarea>
+        <textarea v-model="formData.data.obs" :disabled="!isEditable"></textarea>
     </div>
     <evaluation-actions :formData="formData" :entity="entity" :validateErrors='validateErrors'></evaluation-actions>
 </div>
