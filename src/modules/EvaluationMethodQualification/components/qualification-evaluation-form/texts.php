4960a2f717be410dc55207176f8e2f64380475bf
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-25 17:50:58 -0300

    Criação de mensagem de validação Ref.: #3226

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/texts.php
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/texts.php
@@ -7,4 +7,8 @@ return [
     'the_field' => i::__('O campo'),
     'is_required' => i::__('é obrigatório'),
     'on_section' => i::__('Na sessão'),
+    'Not_sent'=> i::__('Não Enviada'),
+    'In_progress'=> i::__('Em andamento'),
+    'Sent'=> i::__('Enviada'),
+    'Disabled'=>i::__('Inabilitado')
 ];
