491a24fa9ffdceb999e262aea9c7c6e482f5cdf2
erleibiazzio <erleibiazzio@gmail.com>
2024-08-05 17:02:10 -0300

    WIP - Implementação de estrutura serializada para salvar resumo das avaliações

--- src/core/Entities/EvaluationMethodConfigurationAgentRelation.php
+++ src/core/Entities/EvaluationMethodConfigurationAgentRelation.php
@@ -3,6 +3,8 @@ namespace MapasCulturais\Entities;
 
 use MapasCulturais\App;
 use Doctrine\ORM\Mapping as ORM;
+use MapasCulturais\Exceptions\PermissionDenied;
+use MapasCulturais\Exceptions\WorkflowRequest;
 use MapasCulturais\GuestUser;
 use MapasCulturais\JobTypes\ReopenEvaluations;
 
@@ -28,6 +30,20 @@ class EvaluationMethodConfigurationAgentRelation extends AgentRelation {
      * })
      */
     protected $owner;
+
+
+    public function __construct()
+    {
+        $this->metadata =  [
+            "summary" => [
+                "pending" => 0, 
+                "started" => 0, 
+                "completed" => 0,
+                "sent" => 0
+            ]
+        ];
+    }
+    
     
     function delete($flush = false) {
         $app = App::i();
@@ -87,6 +103,81 @@ class EvaluationMethodConfigurationAgentRelation extends AgentRelation {
         $app->enableAccessControl();
         
         $app->applyHookBoundTo($this,"{$this->hookPrefix}.enable:after");
+    }
+
+
+    /**
+     * Atualiza o resumo de avaliações do avaliador
+     * Este método é chamado nas seguintes situações
+     * 1) Sempre que uma avaliação é iniciada entity(RegistrationEvaluation)
+     * 2) Sempre que o status de uma avaliação é alterado entity(RegistrationEvaluation).setStatus(<<*>>)
+     * 3) Sempre que uma avaliação é removida entity(RegistrationEvaluation).remove:after
+     * 4) Sempre que uma inscrição for atribuida para o usuario avaliar entity(recreatePermissionCache:after) Verificando a permissão evaluateOnTime e o usuario for o mesmo da relação
+     * @param bool $flush 
+     * @return void 
+     * @throws PermissionDenied 
+     * @throws WorkflowRequest 
+     */
+    function updateSummary(bool $flush = false, $pending = true, $started = true, $completed =  true, $sent = true): void
+    {
+        $entity = $this->owner;
+        $app = App::i();
+        $conn = $app->em->getConnection();
+
+        $user = $this->agent->user;
+        $data = $this->metadata["summary"] ?? [];
+
+        $_buildQuery = function($status = null) use ($user, $entity, $conn) {
+
+            
+            if(is_null($status)) {
+                $complement = " e.status IS NULL ";
+            } else {
+                $complement = " e.status = {$status} AND e.registration_id IN (SELECT r.id FROM registration r WHERE r.opportunity_id ={$entity->opportunity->id})";
+            }
+
+
+            $query = "SELECT DISTINCT count(e.registration_id) as qtd
+            FROM registration_evaluation e 
+            WHERE 
+                {$complement} AND 
+                user_id = {$user->id}";
+
+
+// eval(\psy\sh());
+            return  $conn->fetchAssoc($query);
+        };
+
+        $buildQuery = function($colluns = "*", $params = "", $type = "fetchAll") use ($conn, $entity){
+            return $conn->$type("SELECT {$colluns} FROM evaluations e WHERE opportunity_id = {$entity->opportunity->id} {$params}");
+        };
+
+        // Retorna as pendentes
+        if($pending = $_buildQuery()) {
+
+            $data['pending'] = $pending['qtd'];
+        }
+
+        // Retorna as iniciadas
+        if($started = $_buildQuery('0')) {
+            $data['started'] = $started['qtd'];
+        }
+
+        // Retorna as concluidas
+        if ($completed = $_buildQuery(1)) {
+            $data['completed'] = $completed['qtd'];
+        }
+
+         // Retorna as enviadas
+         if ($sent = $_buildQuery(2)) {
+            $data['sent'] = $sent['qtd'];
+        }
+
+        $this->metadata = ['summary' => $data];
+
+        $app->disableAccessControl();
+        $this->save($flush);
+        $app->enableAccessControl();
     }
 
     protected function canUserRemove($user): bool
