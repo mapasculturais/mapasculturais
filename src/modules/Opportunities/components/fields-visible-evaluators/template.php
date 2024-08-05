a9e87a07389419c28196ed7eb5995c2bc8648680
Welington De Lima Olegario <wolegario49@gmail.com>
2024-08-05 10:05:54 -0300

    Adiciona o metodo para fazer os filtros dos campos Ref.: #3062

--- src/modules/Opportunities/components/fields-visible-evaluators/template.php
+++ src/modules/Opportunities/components/fields-visible-evaluators/template.php
@@ -22,26 +22,24 @@ $this->import('
     <mc-modal title="<?= i::esc_attr__('Configurar campos visíveis para os avaliadores') ?>" classes="modalEmbedTools">
         <template #default="modal">
             <div>
-                <label>Filtrar campo</label>
+                <label><?= i::__('Filtrar campo') ?></label>
             </div>
             <div>
-                <small>Pesquise pelo título ou pelo ID</small>
+                <input type="text" v-model="searchQuery"><small><?= i::__('Pesquise pelo título ou pelo ID') ?></small>
             </div>
-            <input type="text">
             <div class="fields-visible-evaluators__content">
                 <div>
-                <input type="checkbox" v-model="selectAll" @change="toggleSelectAll()">
+                    <input type="checkbox" v-model="selectAll" @change="toggleSelectAll()">
                     <label>Selecionar todos os campos</label>
                 </div>
                 <div class="fields-visible-evaluators__fields">
-                    <div v-for="field in fields" :class="['fields-visible-evaluators__field' , {'disabled':field.disabled}]">
+                    <div v-for="field in filteredFields" :class="['fields-visible-evaluators__field' , {'disabled':field.disabled}]">
                         <label>
                             <input type="checkbox" :disabled="field.disabled" v-model="avaliableEvaluationFields[field.fieldName]" @change="toggleSelect(field.fieldName)" /> <span v-if="field.id">#{{field.id}}</span> {{field.title}}
                         </label>
                     </div>
                 </div>
             </div>
-
         </template>
         <template #button="modal">
             <button class="button button--bg button--primarylight" @click="modal.open"><?= i::__('Abrir lista de campos') ?></button>
