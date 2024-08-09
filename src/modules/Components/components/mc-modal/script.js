6ba6557a0de68c6cb6d3a5db4e16e2189a006531
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-08-09 17:50:31 -0300

    adiciona opções para desativar o fechamento de modais com a tecla ESC e com o com o click fora da modal

--- src/modules/Components/components/mc-modal/script.js
+++ src/modules/Components/components/mc-modal/script.js
@@ -2,12 +2,6 @@ app.component('mc-modal', {
     template: $TEMPLATES['mc-modal'],
     emits: ['open', 'close'],
 
-    data() {
-        return {
-            modalOpen: false,
-            processing: false,
-        }
-    },
     props: {
         title: {
             type: String,
@@ -29,10 +23,20 @@ app.component('mc-modal', {
             type: Boolean,
             default: true
         },
-        teleport: {
-            type: null,
-            default: false
+        escToClose: {
+            type: Boolean,
+            default: true
         },
+        clickToClose: {
+            type: Boolean,
+            default: true
+        },
+    },
+    data() {
+        return {
+            processing: false,
+            modalOpen: false
+        }
     },
     methods: {
         open () {
