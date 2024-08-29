602ad910fac6c24391d60a25173f4ab24475a0a8
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-08-29 15:34:27 -0300

    ajusta responsividade e estilo. Ref.: #3062

--- src/modules/Opportunities/components/fields-visible-evaluators/template.php
+++ src/modules/Opportunities/components/fields-visible-evaluators/template.php
@@ -30,17 +30,20 @@ $this->import('
                 </div>
             </div>
             <div class="fields-visible-evaluators__content">
-                <div class="fields-visible-evaluators__fields">
+                <div class="fields-visible-evaluators__fields field">
                     <div class="fields-visible-evaluators__field__select-all field">
                         <input type="checkbox" v-model="selectAll" @change="toggleSelectAll()">
                         <label><?= i::__('Selecionar todos os campos') ?></label>
                     </div>
                     <div v-for="field in fieldsResult()" :class="['fields-visible-evaluators__field' , 'field', {'disabled':field.disabled}]">
-                        <mc-icon :name="fieldType(field)"></mc-icon>
                         <label>
-                            <input type="checkbox" :disabled="field.disabled" v-model="avaliableEvaluationFields[field.fieldName]" @change="toggleSelect(field.fieldName)" /> 
-                            <span v-if="field.id">#{{field.id}}</span>  {{field.title}}
-                            <small v-if="field.titleDisabled">{{field.titleDisabled}}</small>
+                            <mc-icon :name="fieldType(field)"></mc-icon>
+                            <input type="checkbox" :disabled="field.disabled" v-model="avaliableEvaluationFields[field.fieldName]" @change="toggleSelect(field.fieldName)" />
+                            <span v-if="field.id">#{{field.id}}</span>
+                            <div class="fields-visible-evaluators__field__title">
+                                <span>{{field.title}}</span>
+                                <small v-if="field.titleDisabled">{{field.titleDisabled}}</small>
+                            </div>
                         </label>
                     </div>
                 </div>
