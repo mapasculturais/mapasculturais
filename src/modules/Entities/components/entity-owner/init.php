63ba788882ac45b4153f0ed01e738752dbe06151
erleibiazzio <erleibiazzio@gmail.com>
2024-09-12 13:20:22 -0300

    Melhora reatividade do componente entity-owner

--- src/modules/Entities/components/entity-owner/init.php
+++ src/modules/Entities/components/entity-owner/init.php
@@ -1,60 +1,30 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
- $entity = $this->controller->requestedEntity;
+$entity = $this->controller->requestedEntity;
 
- $class = $entity->getClassName();
+$class = $entity->getClassName();
 
+$has_request =  false;
+$request_data =  [];
+$destinationName = "";
+if ($request = $app->repo("Request")->findBy(["originType" => $class, "originId" => $entity->id])) {
+    $destination = $app->repo('Agent')->find($request[0]->destinationId);
+    $has_request = true;
+    $destinationName = $destination->name;
 
- $dict_entity = function($class) {
-    switch ($class) {
-        case 'MapasCulturais\Entities\Agent':
-            $result = "agent";
-            break;
-        case 'MapasCulturais\Entities\Opprotunity':
-            $result = "opportunity";
-            break;
-        case 'MapasCulturais\Entities\Project':
-            $result = "project";
-            break;
-        case 'MapasCulturais\Entities\Event':
-            $result = "event";
-            break;
-        case 'MapasCulturais\Entities\Space':
-            $result = "space";
-            break;
-    
-        default:
-            $result = null;
-            break;
-    }
-    return $result;
- };
+    $request_data = [
+        'id' => $request->id,
+        'status' => $request->status,
+    ];
+}
 
- $has_request =  false;
- $request_data =  [];
- if($requests = $app->repo("Request")->findBy(["originType" => $class, "originId" => $entity->id])) {
-     $has_request = true;
-     $count = count($requests);
-    foreach($requests as $request) {
-        
-        $request_data = [
-            'status' => $request->status,
-            'originEntity' => $dict_entity($request->originType),
-            'originType' => $request->originType,
-            'originId' => $request->originId,
-            'destinationEntity' => $dict_entity($request->destinationType),
-            'destinationType' => $request->destinationType,
-            'destinationId' => $request->destinationId,
-            'count' => $count
-        ];
-    }
- }
-
- $this->jsObject['config']['entityOwner'] = [
+$this->jsObject['config']['entityOwner'] = [
     'hasRequest' => $has_request,
-    'requestData' => $request_data
- ];
+    'requestData' => $request_data,
+    'destinationName' => $destinationName
+];
