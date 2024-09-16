8993f274a7d38900c3c289b32ea0538b1b70b7e4
israelmelo <israelg.8@gmail.com>
2024-09-16 07:22:25 -0300

    Criação do watch para manipular os checkbox Ref.: #3258

--- src/modules/Opportunities/components/opportunity-basic-info/script.js
+++ src/modules/Opportunities/components/opportunity-basic-info/script.js
@@ -29,5 +29,30 @@ app.component('opportunity-basic-info' , {
             const phase = this.phases.find(item => item.isLastPhase);
             return phase;
         }
+    },
+
+    watch: {
+        'entity.isContinuousFlow'(newVal) {
+            if (!newVal) {
+                this.entity.hasEndDate = false;
+                this.lastPhase.name = "Publicação final do resultado";
+                this.lastPhase.save();
+            } else {
+                this.lastPhase.name = "Resultado";
+                this.lastPhase.save();
+            }
+        },
+
+        'entity.hasEndDate'(newVal) {
+            if (!newVal) {
+                const myDate = new McDate(new Date(`2111-01-01 00:00`));
+                
+                this.entity.continuousFlow = myDate.sql('full');
+                this.entity.registrationTo = myDate.sql('full');
+            } else {
+                this.entity.continuousFlow = null;
+                this.entity.registrationTo = null;
+            }
+        },
     }
 });
