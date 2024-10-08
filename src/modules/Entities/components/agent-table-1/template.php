b0c6c6d00cf09c5c86bb3671be9e283482c228e4
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-10-08 11:53:27 -0300

    Resolve responsividade nas tabelas da tela de busca dos agentes (Ref.: #3211)

--- src/modules/Entities/components/agent-table-1/template.php
+++ src/modules/Entities/components/agent-table-1/template.php
@@ -17,7 +17,7 @@ $this->import('
 <div class="agent-table-1">
     <agent-table :agentType=1 :additionalHeaders="headers" :extra-query="extraQuery" :visible-columns="visibleColumns" @clear-filters="clearFilters" @remove-filter="removeFilter($event)" :hide-filters="hideFilters">
         <template #filters="{entities}">
-            <mc-multiselect class="col-2" :model="selectedSexualOrientation" :items="sexualOrientation" title="<?= i::esc_attr__('Selecione a orientação sexual: ') ?>" @selected="filterByOrientacaoSexual(entities)" @removed="filterByOrientacaoSexual(entities)" :hide-filter="hideFilters" hide-button>
+            <mc-multiselect class="col-3 sm:col-4" :model="selectedSexualOrientation" :items="sexualOrientation" title="<?= i::esc_attr__('Selecione a orientação sexual: ') ?>" @selected="filterByOrientacaoSexual(entities)" @removed="filterByOrientacaoSexual(entities)" :hide-filter="hideFilters" hide-button>
                 <template #default="{popover, setFilter, filter}">
                     <div class="field">
                         <input class="mc-multiselect--input" @keyup="setFilter($event.target.value)" @focus="popover.open()" placeholder="<?= i::esc_attr__('Selecione a orientação sexual: ') ?>">
@@ -25,7 +25,7 @@ $this->import('
                 </template>
             </mc-multiselect>
 
-            <mc-multiselect class="col-2" :model="selectedGender" :items="gender" title="<?= i::esc_attr__('Selecione o gênero: ') ?>" @selected="filterByGender(entities)" @removed="filterByGender(entities)" :hide-filter="hideFilters" hide-button>
+            <mc-multiselect class="col-3 sm:col-4" :model="selectedGender" :items="gender" title="<?= i::esc_attr__('Selecione o gênero: ') ?>" @selected="filterByGender(entities)" @removed="filterByGender(entities)" :hide-filter="hideFilters" hide-button>
                 <template #default="{popover, setFilter, filter}">
                     <div class="field">
                         <input class="mc-multiselect--input" @keyup="setFilter($event.target.value)" @focus="popover.open()" placeholder="<?= i::esc_attr__('Selecione o gênero: ') ?>">
@@ -33,7 +33,7 @@ $this->import('
                 </template>
             </mc-multiselect>
 
-            <mc-multiselect class="col-2" :model="selectedRace" :items="race" title="<?= i::esc_attr__('Selecione raça/cor: ') ?>" @selected="filterByRace(entities)" @removed="filterByRace(entities)" :hide-filter="hideFilters" hide-button>
+            <mc-multiselect class="col-3 sm:col-4" :model="selectedRace" :items="race" title="<?= i::esc_attr__('Selecione raça/cor: ') ?>" @selected="filterByRace(entities)" @removed="filterByRace(entities)" :hide-filter="hideFilters" hide-button>
                 <template #default="{popover, setFilter, filter}">
                     <div class="field">
                         <input class="mc-multiselect--input" @keyup="setFilter($event.target.value)" @focus="popover.open()" placeholder="<?= i::esc_attr__('Selecione raça/cor: ') ?>">
