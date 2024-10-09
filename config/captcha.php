30edda0a020ed3fe63830b14bfc52a6723833924
Lucas Oliveira <lucas.santana@basisti.com.br>
2024-10-08 21:45:29 -0300

    feat: fazendo com que a área de denúncias passe a utilizar o novo componente de captcha

--- config/captcha.php
+++ config/captcha.php
@@ -1,5 +1,23 @@
 <?php
-return [    
+return [
     'app.recaptcha.key' => env("GOOGLE_RECAPTCHA_SITEKEY", null),
     'app.recaptcha.secret' => env("GOOGLE_RECAPTCHA_SECRET", null),
+
+    /**
+     * Configuração para implementação de captcha seguindo um novo padrão de configuração
+     * 
+     * - O exemplo abaixo segue o padrão de configuração para o Google Recaptcha atualmente utilizado
+     * - Esse padrão de configuração permite a implementação de outros provedores de captcha, como por exemplo o Cloudflare Turnstile
+     */
+    'captcha' => [
+        'provider' => 'google',
+        'providers' => [
+            'google' => [
+                'url' => 'https://www.google.com/recaptcha/api.js',
+                'verify' => 'https://www.google.com/recaptcha/api/siteverify',
+                'key' => env('GOOGLE_RECAPTCHA_SITEKEY', null),
+                'secret' => env('GOOGLE_RECAPTCHA_SECRET', null)
+            ]
+        ]
+    ]
 ];
--- src/modules/Components/components/mc-captcha/init.php
+++ src/modules/Components/components/mc-captcha/init.php
@@ -1,5 +1,25 @@
 <?php
 
+// Se a configuração de captcha não existir, significa que o captcha não foi implementado
+if (!isset($app->config['captcha']) && !isset($app->_config['app.recaptcha.key'])) {
+    return;
+}
+
+// Se não houver configuração nova de captcha, mas houver configuração antiga, vamos utilizar a configuração antiga
+if (!isset($app->_config['captcha']) && isset($app->_config['app.recaptcha.key']) && isset($app->_config['app.recaptcha.secret'])) {
+    return $this->jsObject['mcCaptchaConfig'] = [
+        'captcha' => [
+            'provider' => 'google',
+            'key' => $app->_config['app.recaptcha.key'],
+            'secret' => $app->_config['app.recaptcha.secret']
+        ]
+    ];
+}
+
+if (!isset($app->_config['captcha']['providers'])) {
+    throw new \Exception('Configuração de captcha inválida');
+}
+
 $provider = $app->_config['captcha']['provider'];
 $captcha = $app->_config['captcha']['providers'][$provider];
 
