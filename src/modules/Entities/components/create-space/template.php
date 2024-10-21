d73f1835aead50f5d3e6263925a0873177a70e8f
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-10-21 16:22:18 -0300

    refatoração do campo textarea do componente entity-field e utilização da nova forma para os campos shortDescription

--- src/modules/Entities/components/create-space/template.php
+++ src/modules/Entities/components/create-space/template.php
@@ -20,7 +20,7 @@ $this->import('
             <entity-field :entity="entity" hide-required label=<?php i::esc_attr_e("Nome ou título")?>  prop="name"></entity-field>
             <entity-field :entity="entity" hide-required  :editable="true" label="<?php i::esc_attr_e("Selecione o tipo do espaço")?>" prop="type"></entity-field>
             <entity-terms :entity="entity" hide-required :editable="true" :classes="areaClasses" taxonomy='area' title="<?php i::esc_attr_e("Área de Atuação") ?>"></entity-terms>
-            <entity-field :entity="entity" hide-required prop="shortDescription" label="<?php i::esc_attr_e("Adicione uma Descrição curta para o Espaço")?>"></entity-field>
+            <entity-field :entity="entity" hide-required prop="shortDescription" :max-length="400" label="<?php i::esc_attr_e("Adicione uma Descrição curta para o Espaço")?>"></entity-field>
             {{entity.id}}
         </div>
     </template>
