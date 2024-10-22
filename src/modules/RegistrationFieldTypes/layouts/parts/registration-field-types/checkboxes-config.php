71e1dc7d6b3101ab4ea265e6d9dab45e765735cd
israelmelo <israelg.8@gmail.com>
2024-10-22 11:45:19 -0300

    Adicionado novo campo maxOptions na configuração de formulário Ref.: #3279

--- src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkboxes-config.php
+++ src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/checkboxes-config.php
@@ -1,7 +1,9 @@
+<?php use MapasCulturais\i; ?>
 <div ng-if="field.fieldType === 'checkboxes'">
     <div class="options-selector">
         <label><?php \MapasCulturais\i::_e('Opções do campo') ?></label><br>
         <textarea ng-model="field.fieldOptions" placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>" style="min-height: 75px"/></textarea>
+        <label><?php i::_e('Super teste') ?><input type="number" ng-model="field.config.maxOptions"> </label><br>
         <p class="registration-help"><?php \MapasCulturais\i::_e("Informe uma opção por linha.");?></p>
     </div>
 </div>
