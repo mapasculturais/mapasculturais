f7a4dc46a796b76a99668d67239bde5a8ae405c1
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-10-29 13:10:25 -0300

    adiciona novos hooks na ApiQuery para manipulação da DQL da consulta

--- src/core/ApiQuery.php
+++ src/core/ApiQuery.php
@@ -1030,17 +1030,23 @@ class ApiQuery {
         if(!$this->_subsiteId){
             if($subsite = $app->getCurrentSubsite()){
                 $subsite_query = $subsite->getApiQueryFilter($this->entityClassName);
+
+                $app->applyHookBoundTo($this, "{$this->hookPrefix}.subsiteFilters", [&$subsite_query]);
+
                 if($subsite_query){
                     $filters[] = ['subquery' => $subsite_query, 'subquery_property' => $this->pk, 'property' => $this->pk];
                 }
             }
         }
         
+        $app->applyHookBoundTo($this, "{$this->hookPrefix}.subqueryFilters", [&$filters]);
+
         return $filters;
     }
 
     protected function generateWhere() {
-        
+        $app = App::i();
+
         $where = $this->where;
         $where_dqls = implode(" $this->_op \n\t", $this->_whereDqls);
         
@@ -1090,6 +1096,8 @@ class ApiQuery {
             }
         }
 
+        $app->applyHookBoundTo($this, "{$this->hookPrefix}.where", [&$where]);
+
         return $where;
     }
 
@@ -1109,14 +1117,18 @@ class ApiQuery {
             $joins .= " JOIN e.__sealRelations {$sl} WITH {$sl}.seal IN ($slv)";
         }
 
+        $app->applyHookBoundTo($this, "{$this->hookPrefix}.joins", [&$joins]);
+
         return $joins;
     }
     
     protected $_removeFromResult = [];
 
     protected function generateSelect() {
+        $app = App::i();
+
         $select = $this->select;
-        $class = $this->entityClassName;
+
         if(!in_array($this->pk, $this->_selectingProperties)){
             $this->_selectingProperties = array_merge([$this->pk], $this->_selectingProperties);
         }
@@ -1172,6 +1184,9 @@ class ApiQuery {
         foreach($this->orderCasts as $order_cast) {
             $select .= ", $order_cast";
         }
+
+        $app->applyHookBoundTo($this, "{$this->hookPrefix}.select", [&$select]);
+
         return $select;
     }
 
