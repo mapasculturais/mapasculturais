6677d3267713f60c9d1a51ee6c88d42794d0d606
erleibiazzio <erleibiazzio@gmail.com>
2024-10-30 18:12:20 -0300

    Corrige erro na listagem de campo no componente fields-visible-evaluators

--- src/modules/Opportunities/components/fields-visible-evaluators/init.php
+++ src/modules/Opportunities/components/fields-visible-evaluators/init.php
@@ -1,20 +1,31 @@
 <?php
-$entity = $this->controller->requestedEntity;
-$previous_phases = $entity->previousPhases;
+$phase =  $this->controller->requestedEntity;
 
-if ($entity->firstPhase->id != $entity->id) {
-    $previous_phases[] = $entity;
-}
-$_fields = [];
+$get_fields = function ($opportunity) {
+    $previous_phases = $opportunity->previousPhases;
 
-foreach ($previous_phases as $phase) {
-    foreach ($phase->registrationFieldConfigurations as $field) {
-        $_fields[] = $field;
+    if ($opportunity->firstPhase->id != $opportunity->id) {
+        $previous_phases[] = $opportunity;
     }
-    
-    foreach ($phase->registrationFileConfigurations as $file) {
-        $_fields[] = $file;
-    }
-}
 
-$this->jsObject['config']['fieldsToEvaluate'] = $_fields;
+    $_fields = [];
+    foreach ($previous_phases as $phase) {
+        foreach ($phase->registrationFieldConfigurations as $field) {
+            $_fields[] = $field;
+        }
+        
+        foreach ($phase->registrationFileConfigurations as $file) {
+            $_fields[] = $file;
+        }
+    }
+
+    return $_fields;
+};
+
+$phases_fields = [];
+
+do {
+    $phases_fields[$phase->id] = $get_fields($phase);
+} while ($phase = $phase->nextPhase);
+
+$this->jsObject['config']['fieldsVisibleEvaluators'] = $phases_fields;
