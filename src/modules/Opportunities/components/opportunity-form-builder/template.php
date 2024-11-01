f896d14d5e93defd792c40916456bd332ca99e35
leopiccionia <leopiccionia@gmail.com>
2024-11-01 09:35:12 -0300

    Remove espaços extras no nome da etapa

--- src/modules/Opportunities/components/opportunity-form-builder/template.php
+++ src/modules/Opportunities/components/opportunity-form-builder/template.php
@@ -132,7 +132,7 @@ $this->import('
                     <template #default>
                         <div class="field">
                             <label for="step-name"><?php i::_e('Nome') ?></label>
-                            <input id="step-name" type="text" v-model="newStep.name">
+                            <input id="step-name" type="text" v-model.trim="newStep.name">
                         </div>
                     </template>
 
