c533bdf87c3d94a7dae50b4f68c02200b68ec40a
leopiccionia <leopiccionia@gmail.com>
2024-11-06 15:25:27 -0300

    Corrige sincronização de abas após remoção de etapa. Ref.: #3291

--- src/modules/Opportunities/components/opportunity-form-builder/template.php
+++ src/modules/Opportunities/components/opportunity-form-builder/template.php
@@ -96,7 +96,7 @@ $this->import('
     </div>
 
     <div class="col-12">
-        <mc-tabs v-model:draggable="stepsWithSlugs">
+        <mc-tabs ref="tabs" v-model:draggable="stepsWithSlugs">
             <template #default>
                 <mc-tab v-for="({ step, slug }, index) of stepsWithSlugs" :label="`${index + 1}. ${step.name ?? ''}`" :key="step.id" :slug="slug" cache>
                     <div class="form-builder__step-config">
