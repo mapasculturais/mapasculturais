c7b7007b3b17fe2685c806b762a984ac453b0733
israelmelo <israelg.8@gmail.com>
2024-11-07 10:20:09 -0300

    Ajuste no init do evaluation-simple-detail Ref.: #3309

--- src/modules/EvaluationMethodSimple/components/evaluation-simple-detail/init.php
+++ src/modules/EvaluationMethodSimple/components/evaluation-simple-detail/init.php
@@ -4,45 +4,53 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
 */
 
-$registration = $this->controller->requestedEntity;
-$needs_tiebreaker = $registration->needsTieBreaker();
+use MapasCulturais\Entities\Registration;
 
-$opportunity = $registration->opportunity;
-$evaluation_configuration = $opportunity->evaluationMethodConfiguration;
-$enable_external_reviews = $evaluation_configuration->showExternalReviews;
+$entity = $this->controller->requestedEntity;
 
-$related_agents = $evaluation_configuration->relatedAgents;
-$is_minerva_group = false;
+// SOLUÇÃO TEMPORÁRIA
+$class = $entity->getClassName();
 
-foreach($related_agents as $group => $agents) {
-    if($group == '@tiebreaker') {
-        foreach($agents as $agent) {
-            if($agent->id == $app->user->profile->id) {
-                $is_minerva_group = true;
+if($class == Registration::class) {
+    $needs_tiebreaker = $entity->needsTieBreaker();
+    
+    $opportunity = $entity->opportunity;
+    $evaluation_configuration = $opportunity->evaluationMethodConfiguration;
+    $enable_external_reviews = $evaluation_configuration->showExternalReviews;
+    
+    $related_agents = $evaluation_configuration->relatedAgents;
+    $is_minerva_group = false;
+    
+    foreach($related_agents as $group => $agents) {
+        if($group == '@tiebreaker') {
+            foreach($agents as $agent) {
+                if($agent->id == $app->user->profile->id) {
+                    $is_minerva_group = true;
+                }
             }
         }
     }
-}
-
-$data = [];
-if ($needs_tiebreaker && $is_minerva_group && $enable_external_reviews) {
-    if ($evaluation_configuration->publishEvaluationDetails){
-        $em = $evaluation_configuration->evaluationMethod;
-        $data['consolidatedDetails'] = $em->getConsolidatedDetails($registration);
-        $data['evaluationsDetails'] = [];
-
-        $evaluations = $registration->sentEvaluations;
-
-        foreach($evaluations as $eval) {
-            $detail = $em->getEvaluationDetails($eval);
-            if ($evaluation_configuration->publishValuerNames){
-                $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
+    
+    $data = [];
+    if ($needs_tiebreaker && $is_minerva_group && $enable_external_reviews) {
+        if ($evaluation_configuration->publishEvaluationDetails){
+            $em = $evaluation_configuration->evaluationMethod;
+            $data['consolidatedDetails'] = $em->getConsolidatedDetails($entity);
+            $data['evaluationsDetails'] = [];
+    
+            $evaluations = $entity->sentEvaluations;
+    
+            foreach($evaluations as $eval) {
+                $detail = $em->getEvaluationDetails($eval);
+                if ($evaluation_configuration->publishValuerNames){
+                    $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
+                }
+                $data['evaluationsDetails'][] = $detail;
             }
-            $data['evaluationsDetails'][] = $detail;
         }
     }
+    
+    $this->jsObject['config']['simpleEvaluationDetail'] = [
+        'data' => $data,
+    ];
 }
-
-$this->jsObject['config']['simpleEvaluationDetail'] = [
-    'data' => $data,
-];
