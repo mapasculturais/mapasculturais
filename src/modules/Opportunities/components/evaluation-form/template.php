9464bc1c73811a580625a83513b3d371be09e5c4
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-11-08 11:43:09 -0300

    ajusta visualização de componente registration-evaluation-info. Ref.: #3313

--- src/modules/Opportunities/components/evaluation-form/template.php
+++ src/modules/Opportunities/components/evaluation-form/template.php
@@ -16,14 +16,18 @@ $entity = $this->controller->requestedEntity;
 
 $opportunity = $entity->opportunity;
 $evaluation_method_config_name = $opportunity->evaluationMethodConfiguration->name;
+$infos = (array) $opportunity->evaluationMethodConfiguration->infos;
+
 ?>
 
 <div class="registration__actions">
     <div ref=header>
         <h2 class="regular primary__color"><?= i::__("Formulário de") ?> <strong><?= $evaluation_method_config_name ?></strong></h2>
-        <registration-evaluation-info :entity="entity"></registration-evaluation-info>
+        <?php if (!empty($infos["general"])): ?>
+            <registration-evaluation-info :entity="entity"></registration-evaluation-info>
+        <?php endif; ?>
     </div>
-    <div ref="form" style="overflow-y: auto;">
+    <div class="evaluation-form scrollbar" ref="form">
         <?php $this->part("{$entity->opportunity->evaluationMethod->slug}/evaluation-form"); ?>
     </div>
 
