c432e2f18cbd7b7750f0c84cae5b7a6e8f45a1a0
leopiccionia <leopiccionia@gmail.com>
2024-11-08 10:30:28 -0300

    Exibe campo no iframe da etapa nova. Ref.: #3312

--- src/modules/Opportunities/components/opportunity-form-builder/template.php
+++ src/modules/Opportunities/components/opportunity-form-builder/template.php
@@ -98,7 +98,7 @@ $this->import('
     <div class="col-12">
         <mc-tabs ref="tabs" v-model:draggable="stepsWithSlugs">
             <template #default>
-                <mc-tab v-for="({ step, slug }, index) of stepsWithSlugs" :label="`${index + 1}. ${step.name ?? ''}`" :key="step.id" :slug="slug" cache>
+                <mc-tab v-for="({ step, slug }, index) of stepsWithSlugs" :label="`${index + 1}. ${step.name ?? ''}`" :key="step.id" :slug="slug" :cache="false">
                     <div class="form-builder__step-config">
                         <entity-field :entity="step" prop="name" :autosave="1000" hide-required></entity-field>
 
