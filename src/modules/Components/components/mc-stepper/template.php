ba38d3410126098ad95cf03314c1df1d17d3e74b
leopiccionia <leopiccionia@gmail.com>
2024-11-14 12:48:28 -0300

    Tornar etapas do stepper equidistantes

--- src/modules/Components/components/mc-stepper/template.php
+++ src/modules/Components/components/mc-stepper/template.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="stepper" :class="{'noNavigation' : disableNavigation}" id="stepper">
-    <div v-for="(stepped, n) in steps" class="step" :class="{'passedby' : step>=n, 'active' : step==n}" :id="'step'+(n+1)" @click="goToStep(n)">
+    <div v-for="(stepped, n) in steps" class="step" :class="{'passedby' : step>n, 'active' : step==n}" :id="'step'+(n+1)" @click="goToStep(n)">
         <div class="count"></div>
         <span v-show="!noLabels" class="label active" v-if="step == n">{{n + 1}}. {{stepped}}</span>
         <span v-show="!noLabels" class="label" v-else>{{n + 1}}</span>
