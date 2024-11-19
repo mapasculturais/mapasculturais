78e1a47b76aa7d33d865c643a7e9dd0743ad3c00
leopiccionia <leopiccionia@gmail.com>
2024-11-19 15:43:02 -0300

    Melhora alguns formulários de configurações de campo

--- src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/select-config.php
+++ src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/select-config.php
@@ -1,13 +1,17 @@
 <?php use MapasCulturais\i; ?>
 <div ng-if="field.fieldType === 'select'">
-    <div class="options-selector">
-        <label><?php i::_e('Opções do campo') ?></label><br>
-        <textarea ng-model="field.fieldOptions" placeholder="<?php i::esc_attr_e("Opções de seleção");?>" style="min-height: 75px"/></textarea>
-        <p class="registration-help"><?php i::_e("Informe uma opção por linha.");?></p>
+    <div class="mc-content options-selector">
+        <label>
+            <?php i::_e('Opções do campo') ?><br>
+            <textarea ng-model="field.fieldOptions" placeholder="<?php i::esc_attr_e("Opções de seleção");?>" style="min-height: 75px"/></textarea>
+            <small class="registration-help"><?php i::_e("Informe uma opção por linha.");?></small>
+        </label>
+        <label>
+            <?php i::_e('Modo de visualização') ?><br>
+            <select ng-model="field.config.viewMode">
+                <option value="select"><?php i::_e('Select') ?></option>
+                <option value="radio"><?php i::_e('Radio') ?></option>
+            </select>
+        </label>
     </div>
-    <label><?php i::_e('Modo de visualização') ?></label>
-    <select ng-model="field.config.viewMode">
-        <option value="select"><?php i::_e('Select') ?></option>
-        <option value="radio"><?php i::_e('Radio') ?></option>
-    </select>
 </div>
