eb0cbdb14afdf321e0980210ba9f85440f23d11c
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-11-21 16:37:40 -0300

    padronização da configuração dos campos arroba de agente responsável e agente coletivo

--- src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/agent-collective-field-config.php
+++ src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/agent-collective-field-config.php
@@ -7,15 +7,36 @@ $agent_fields = $app->modules['RegistrationFieldTypes']->getAgentFields();
 
 $fields_options = [];
 $fields_labels = [
-    '@location' => i::__(' Campos de endereço'),
-    '@terms:area' => i::__(' Área de atuação'),
-    '@links' => i::__(' Links'),
-
+    '@location' => " " . i::__('Campos de endereço'),
+    '@terms:area' => " " . i::__('Área de atuação'),
+    '@links' => " " . i::__(' Links'),
 ];
 
 $app->applyHookBoundTo($this, "registrationFieldTypes--agent-collective-field-config-fields_labels", [&$fields_labels]);
 
+$select_fields = [];
+$area_options = [];
+$area = $app->getRegisteredTaxonomyBySlug('area');
 foreach ($agent_fields as $field) {
+
+    if($field == "@terms:area"){
+        $area_options[] = $field;
+    }
+    
+    if(in_array($definitions[$field]['type'] ?? [], ['select', 'multiselect','checkboxes'])){
+        $options_list = $definitions[$field]['options'] ?? [];
+        $options = [];
+        foreach($options_list as $key => $value){
+            if($key != $value && is_string($key)){
+                $options[] = "{$key}:{$value}";
+            }else{
+                $options[] = $value;
+            }
+        }
+        
+        $select_fields[$field] = implode("\n", $options);
+    }
+
     if (isset($definitions[$field])) {
         $def = $definitions[$field];
         $fields_options[$field] = $def['label'] ?: $field;
@@ -25,34 +46,49 @@ foreach ($agent_fields as $field) {
         $fields_options[$field] = $field;
     }
 }
+
 ?>
 <div ng-if="field.fieldType === 'agent-collective-field'" >
-    <?php i::_e('Campo do agente coletivo:') ?>
+    <?php i::_e('Campo do agente responsável:') ?>
 
-    <select ng-model="field.config.entityField">
+    <select ng-model="field.config.entityField" ng-change="field.fieldOptions = null">
     <?php foreach ($fields_options as $key => $label) : ?>
         <option value="<?= $key ?>"><?= $label ?></option>
     <?php endforeach; ?>
     </select>
-
+    
     <?php $this->applyTemplateHook('registrationFieldTypes--agent-collective-field-config','before', ['agent_fields' => $agent_fields]); ?>
-
     <div ng-if="field.config.entityField == '@location'">
         <label><input type="checkbox" ng-model="field.config.setLatLon" ng-true-value="'true'" ng-false-value=""> <?php i::_e('Definir a latitude e longitude baseado no CEP?') ?></label><br>
         <label><input type="checkbox" ng-model="field.config.setPrivacy" ng-true-value="'true'" ng-false-value=""> <?php i::_e('Fornecer opção para mudar a privacidade da localização?') ?></label>
     </div>
     <div ng-if="field.config.entityField == '@terms:area'">
-        <?php i::_e("Informe os termos que estarão disponíveis para seleção. <br>Para fazer um mapeamento de valores utilize <strong>valor salvo:valor exibido</strong>. Exemplo: <strong>Dança:Artes da Dança</strong>") ?>
-        
-        <textarea ng-model="field.fieldOptions" placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>" style="min-height: 75px"/></textarea>
+        <?php foreach($area_options as $field_name):?>
+            <div ng-if='field.config.entityField === "<?=$field_name?>"'>
+                <?php i::_e("Informe os termos que estarão disponíveis para seleção.") ?>
+                <textarea ng-model="field.fieldOptions" ng-init='field.fieldOptions = field.fieldOptions || data.taxonomies.area.terms.join("\n")' placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>"></textarea>
+                <div>
+                    <label><?php i::_e('Limite de Opções') ?><input type="number" ng-model="field.config.maxOptions"></label><br>
+                    <small class="registration-help"><?php i::_e('Digite o limite de opções. Deixe em branco ou coloque 0 para selecionar ilimitadas.'); ?></small>
+                </div>
+                
+            </div>
+        <?php endforeach?>
     </div>
-    <div ng-if="field.config.entityField == 'genero' || field.config.entityField == 'raca' || field.config.entityField == 'orientacaoSexual'">
-        <?php i::_e("Informe os termos que estarão disponíveis para seleção.") ?>
-        <textarea ng-model="field.fieldOptions" placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>" style="min-height: 75px"/></textarea>
-    </div>
+    <?php foreach($select_fields as $field_name => $options):?>
+        <div ng-if='field.config.entityField === "<?=$field_name?>"'>
+            <?php i::_e("Informe os termos que estarão disponíveis para seleção.") ?>
+            <textarea ng-model="field.fieldOptions" ng-init="field.fieldOptions = field.fieldOptions || '<?=htmlentities($options)?>'" placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>"></textarea>
+            <div>
+                <label><?php i::_e('Limite de Opções') ?><input type="number" ng-model="field.config.maxOptions"></label><br>
+                <small class="registration-help"><?php i::_e('Digite o limite de opções. Deixe em branco ou coloque 0 para selecionar ilimitadas.'); ?></small>
+            </div>
+        </div>
+    <?php endforeach?>
     <div ng-if="field.config.entityField == '@links'">
         <label><input type="checkbox" ng-model="field.config.title" ng-true-value="'true'" ng-false-value=""> <?php i::_e('Pedir de título') ?></label><br>
     </div>
-    <?php $this->applyTemplateHook('registrationFieldTypes--agent-collective-field-config','after', ['agent_fields' => $agent_fields]); ?>
 
+    
+    <?php $this->applyTemplateHook('registrationFieldTypes--agent-collective-field-config','after', ['agent_fields' => $agent_fields]); ?>
 </div>
--- src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/agent-owner-field-config.php
+++ src/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/agent-owner-field-config.php
@@ -7,7 +7,6 @@ $agent_fields = $app->modules['RegistrationFieldTypes']->getAgentFields();
 
 $fields_options = [];
 $fields_labels = [
-    '@bankFields' => " " . i::__('Campos de dados bancários'),
     '@location' => " " . i::__('Campos de endereço'),
     '@terms:area' => " " . i::__('Área de atuação'),
     '@links' => " " . i::__(' Links'),
@@ -68,7 +67,7 @@ foreach ($agent_fields as $field) {
             <div ng-if='field.config.entityField === "<?=$field_name?>"'>
                 <?php i::_e("Informe os termos que estarão disponíveis para seleção.") ?>
                 <textarea ng-model="field.fieldOptions" ng-init='field.fieldOptions = field.fieldOptions || data.taxonomies.area.terms.join("\n")' placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>"></textarea>
-                <div ng-if="countWords(field.fieldOptions) >= 10">
+                <div>
                     <label><?php i::_e('Limite de Opções') ?><input type="number" ng-model="field.config.maxOptions"></label><br>
                     <small class="registration-help"><?php i::_e('Digite o limite de opções. Deixe em branco ou coloque 0 para selecionar ilimitadas.'); ?></small>
                 </div>
@@ -80,7 +79,7 @@ foreach ($agent_fields as $field) {
         <div ng-if='field.config.entityField === "<?=$field_name?>"'>
             <?php i::_e("Informe os termos que estarão disponíveis para seleção.") ?>
             <textarea ng-model="field.fieldOptions" ng-init="field.fieldOptions = field.fieldOptions || '<?=htmlentities($options)?>'" placeholder="<?php \MapasCulturais\i::esc_attr_e("Opções de seleção");?>"></textarea>
-            <div ng-if="countWords(field.fieldOptions) >= 10">
+            <div>
                 <label><?php i::_e('Limite de Opções') ?><input type="number" ng-model="field.config.maxOptions"></label><br>
                 <small class="registration-help"><?php i::_e('Digite o limite de opções. Deixe em branco ou coloque 0 para selecionar ilimitadas.'); ?></small>
             </div>
