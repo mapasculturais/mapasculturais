66f70a4772cf22848fd9a7a5bec4897de62b9b78
Lucas Oliveira <lucas.santana@basisti.com.br>
2024-11-22 09:38:05 -0300

    fix: correção ortográfica

--- src/modules/Components/components/complaint-suggestion/texts.php
+++ src/modules/Components/components/complaint-suggestion/texts.php
@@ -4,6 +4,6 @@ use MapasCulturais\i;
 
 return [
     'Recaptcha inválida' => i::__('Recaptcha inválida'),
-    'Todos os campos são obrigatorio' => i::__('Todos os campos são obrigatório'),
+    'Todos os campos são obrigatórios' => i::__('Todos os campos são obrigatórios'),
     'Dados enviados com sucesso' => i::__('Dados enviados com sucesso'),
 ];
