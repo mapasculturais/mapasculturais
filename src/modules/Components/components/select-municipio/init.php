97c58c413c73a9bf495147dd494d0be02e13972f
israelmelo <israelg.8@gmail.com>
2024-11-25 13:57:26 -0300

    Cria componente select-municipio

---
+++ src/modules/Components/components/select-municipio/init.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* @var MapasCulturais\App $app
+* @var MapasCulturais\Themes\BaseV2\Theme $this
+*/
+
+$app->view->enqueueScript('components', 'ibge', 'js/ibge.js', ['components-init']);
