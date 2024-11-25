39be130a7762ea96c31c3d3ec9634ba59960aca9
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-11-25 15:51:28 -0300

    ajusta campo de privacidade de endereço. Ref.: (no Issue)

--- src/modules/Entities/components/entity-field-location/texts.php
+++ src/modules/Entities/components/entity-field-location/texts.php
@@ -3,5 +3,6 @@ use MapasCulturais\i;
 
 return [
     'texto localizado' => i::__('Aqui o texto localizado que será exibido'),
-    'outro texto localizado' => i::__('Outro texto localizado')
+    'outro texto localizado' => i::__('Outro texto localizado'),
+    'privacy-label' => i::__('Este endereço pode ficar público na plataforma?'),
 ];
