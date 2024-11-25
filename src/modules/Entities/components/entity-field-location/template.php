9993d869e511ac3ec36a3706a22758cba0bd9ce7
erleibiazzio <erleibiazzio@gmail.com>
2024-11-25 14:35:41 -0300

    Corrige campo entity-field-location para que salve os dados no momento correto

--- src/modules/Entities/components/entity-field-location/template.php
+++ src/modules/Entities/components/entity-field-location/template.php
@@ -21,49 +21,49 @@ $this->import('
                 <label for="cep">
                     <?= i::__('CEP') ?>
                 </label>
-                <input @change="pesquisacep(entity[fieldName].En_CEP, true);" id="cep" type="text" v-maska data-maska="#####-###" v-model="entity[fieldName].En_CEP" />
+                <input @change="pesquisacep(addressData.En_CEP, true);" id="cep" type="text" v-maska data-maska="#####-###" v-model="addressData.En_CEP" />
             </div>
 
             <div class="field col-4">
                 <label for="logradouro">
                     <?= i::__('Logradouro') ?>
                 </label>
-                <input id="logradouro" type="text" v-model="entity[fieldName].En_Nome_Logradouro" @change="save" />
+                <input id="logradouro" type="text" v-model="addressData.En_Nome_Logradouro" @change="save" />
             </div>
 
             <div class="field col-4">
                 <label for="num">
                     <?= i::__('Número') ?>
                 </label>
-                <input id="num" type="number" v-model="entity[fieldName].En_Num" @change="save" />
+                <input id="num" type="number" v-model="addressData.En_Num" @change="save" />
             </div>
 
             <div class="field col-4">
                 <label for="bairro">
                     <?= i::__('Bairro') ?>
                 </label>
-                <input id="bairro" type="text" v-model="entity[fieldName].En_Bairro" @change="save" />
+                <input id="bairro" type="text" v-model="addressData.En_Bairro" @change="save" />
             </div>
 
             <div class="field col-12">
                 <label for="complemento">
                     <?= i::__('Complemento') ?>
                 </label>
-                <input id="complemento" type="text" v-model="entity[fieldName].En_Complemento" @change="save" />
+                <input id="complemento" type="text" v-model="addressData.En_Complemento" @change="save" />
             </div>
 
             <div v-if="statesAndCitiesCountryCode != 'BR'" class="field">
                 <label for="country">
                     <?= i::__('País') ?>
                 </label>
-                <input id="country" type="text" v-model="entity[fieldName].En_Pais" @change="save" />
+                <input id="country" type="text" v-model="addressData.En_Pais" @change="save" />
             </div>
 
             <div class="field col-6">
                 <label for="field__title">
                     <?= i::__('Estado') ?>
                 </label>
-                <mc-select placeholder="<?= i::esc_attr_e("Selecione"); ?>"  @change="citiesList(); address()" v-model:default-value="entity[fieldName].En_Estado" show-filter>
+                <mc-select placeholder="<?= i::esc_attr_e("Selecione"); ?>"  @change="citiesList(); address()" v-model:default-value="addressData.En_Estado" show-filter>
                     <option v-for="state in states" :value="state.value">{{state.label}}</option>
                 </mc-select>
             </div>
@@ -72,14 +72,14 @@ $this->import('
                 <label for="field__title">
                     <?= i::__('Cidade') ?>
                 </label>
-                <mc-select placeholder="<?= i::esc_attr_e("Selecione"); ?>"  @change="address()" v-model:default-value="entity[fieldName].En_Municipio" show-filter>
+                <mc-select placeholder="<?= i::esc_attr_e("Selecione"); ?>"  @change="address()" v-model:default-value="addressData.En_Municipio" show-filter>
                     <option v-for="city in cities" :value="city">{{city}}</option>
                 </mc-select>
             </div>
 
             <div v-if="configs?.setPrivacy" class="field col-12">
                 <label>
-                    <input type="checkbox" v-model="entity[fieldName].publicLocation" />
+                    <input type="checkbox" v-model="addressData.publicLocation" />
                     <?= i::__('Marque para deixar sua localização pública.') ?>
                 </label>
             </div>
