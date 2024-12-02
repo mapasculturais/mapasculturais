4f1075a590d622d748e57250cde5691a2ac3e81a
leopiccionia <leopiccionia@gmail.com>
2024-12-02 17:21:13 -0300

    Refatora suporte de inscrição para usar registration-form

--- src/modules/Components/components/mc-stepper/template.php
+++ src/modules/Components/components/mc-stepper/template.php
@@ -5,9 +5,9 @@
  */
 ?>
 <div id="stepper" class="stepper" :class="{'noNavigation' : disableNavigation}" :style="{ '--steps': steps.length }">
-    <div v-for="(stepped, n) in steps" class="step" :class="{'passedby' : step > n, 'active' : step == n}" :id="'step'+(n+1)" :title="stepped" @click="goToStep(n)">
+    <button v-for="(stepped, n) in steps" type="button" class="step" :class="{'passedby' : step > n, 'active' : step == n}" :id="'step'+(n+1)" :title="stepped" :disabled="disabledSteps.includes(n)" @click="goToStep(n)">
         <div class="count"></div>
         <span v-show="!noLabels" class="label active" v-if="step == n">{{n + 1}}. {{stepped}}</span>
         <span v-show="!noLabels" class="label" v-else>{{n + 1}}</span>
-    </div>
+    </button>
 </div>
