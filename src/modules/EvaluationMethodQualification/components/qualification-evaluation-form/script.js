5f9fd54ba485e4383c6da1925ecb1ada6596e71d
israelmelo <israelg.8@gmail.com>
2024-12-04 12:24:14 -0300

    Revert "Ajusta exibição do resultado da seção com a limitação dos critérios eliminatórios"
    This reverts commit 7ab65ad1b0f5df423e7e34176ede1a53581c9909.

--- src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/script.js
+++ src/modules/EvaluationMethodQualification/components/qualification-evaluation-form/script.js
@@ -200,35 +200,25 @@ app.component('qualification-evaluation-form', {
 
             let nonEliminatoryCount = 0;
 
-            const allCriteriaFilled = section.criteria.filter(crit => {
+            for (const crit of section.criteria) {
                 const critValue = this.formData.data[crit.id];
-                return Array.isArray(critValue) && critValue.length > 0;
-            }).length === section.criteria.length;
+                if (!Array.isArray(critValue)) continue;
+                if (!this.showSectionAndCriterion(crit)) continue;
 
-            if(allCriteriaFilled) {
-                for (const crit of section.criteria) {
-                    const critValue = this.formData.data[crit.id];
-                    if (!Array.isArray(critValue)) continue;
-                    if (!this.showSectionAndCriterion(crit)) continue;
-    
-                    if (crit.nonEliminatory === 'false' && critValue.includes('invalid')) {
-                        return this.text('Não atende');
-                    } else if (critValue.length === 0) {
-                        return this.text('Avaliação incompleta');
-                    } else if (crit.nonEliminatory === 'true' && critValue.includes('invalid')) {
-                        nonEliminatoryCount++;
-                    }
-                }
-
-                if (nonEliminatoryCount > section.numberMaxNonEliminatory && section.numberMaxNonEliminatory > 0) {
+                if (crit.nonEliminatory === 'false' && critValue.includes('invalid')) {
                     return this.text('Não atende');
-                } else {
-                    return this.text('Atende');
+                } else if (critValue.length === 0) {
+                    return this.text('Avaliação incompleta');
+                } else if (crit.nonEliminatory === 'true' && critValue.includes('invalid')) {
+                    nonEliminatoryCount++;
                 }
-            } else {
-                return this.text('Avaliação incompleta');
             }
-            
+
+            if (nonEliminatoryCount > section.numberMaxNonEliminatory && section.numberMaxNonEliminatory > 0) {
+                return this.text('Não atende');
+            } else {
+                return this.text('Atende');
+            }
         },
         updateSectionStatusByFromData() {
             const updatedSectionStatus = {};
