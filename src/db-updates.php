c4c70339ca0f78975ea1eed99d4156bf7d17cbc0
Fernando Lordão <fernandolordao@gmail.com>
2024-12-09 01:31:44 -0300

    Elimina trechos repetidos pelo primeiro merge de 'upstream/develop-7.6'

--- src/db-updates.php
+++ src/db-updates.php
@@ -1652,40 +1652,6 @@ $$
         
     },
 
-    'create object_type enum type' => function () {
-        $object_types = implode(',', array_map(function($el) {
-            return "'$el'";
-        }, DoctrineEnumTypes\ObjectType::values()));
-
-        __exec("CREATE TYPE object_type AS ENUM($object_types)");
-    }, 
-
-    'create permission_action enum type' => function () {
-        $permission_actions = implode(',', array_map(function($el) {
-            return "'$el'";
-        }, DoctrineEnumTypes\PermissionAction::values()));
-
-        __exec("CREATE TYPE permission_action AS ENUM($permission_actions)");
-    }, 
-
-    'alter tables to use enum types' => function() {
-        __exec("ALTER TABLE pcache ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-        __exec("ALTER TABLE pcache ALTER COLUMN action TYPE permission_action USING action::permission_action");
-
-        __exec("ALTER TABLE file ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-        __exec("ALTER TABLE agent_relation ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-        __exec("ALTER TABLE term_relation ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-        __exec("ALTER TABLE entity_revision ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-        __exec("ALTER TABLE metadata ALTER COLUMN object_type TYPE object_type USING object_type::object_type");
-
-    },
-
-    'alter table permission_cache_pending add column status' => function() use($conn) {
-        if (!__column_exists('permission_cache_pending', 'status')) {
-            $conn->executeQuery("ALTER TABLE permission_cache_pending ADD status smallint DEFAULT 0");
-        }
-    },
-
     'RECREATE VIEW evaluations AGAIN!!!!!' => function() use($conn) {
         __try("DROP VIEW evaluations");
 
