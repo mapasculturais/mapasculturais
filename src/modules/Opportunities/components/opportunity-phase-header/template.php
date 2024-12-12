823f560e08b3b728cc515ce39f937a295d6e9477
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-12-12 17:17:22 -0300

    Adição de classe no opportunity-phase-header para ocultar as datas no tema rcv (Ref.: #160)

--- src/modules/Opportunities/components/opportunity-phase-header/template.php
+++ src/modules/Opportunities/components/opportunity-phase-header/template.php
@@ -11,15 +11,15 @@ use MapasCulturais\i;
     <div class="sm:col-12 opportunity-phase-header__title" :class="titleColClass">
         <h3 class="opportunity__color">{{ title }}</h3>
     </div>
-    <div v-if="dateFrom" class="col-2 sm:col-4">
+    <div v-if="dateFrom" class="opportunity-phase-header__date col-2 sm:col-4">
         <h6 class="bold"><?= i::__("Data de início") ?></h6>
         <h4 class="opportunity__color bold">{{ dateFrom }}</h4>
     </div>
-    <div v-if="dateTo" class="col-2 sm:col-4">
+    <div v-if="dateTo" class="opportunity-phase-header__date col-2 sm:col-4">
         <h6 class="bold"><?= i::__("Data final") ?></h6>
         <h4 class="opportunity__color bold">{{ dateTo }}</h4>
     </div>
-    <div v-if="publishDate" class="col-2 sm:col-4">
+    <div v-if="publishDate" class="opportunity-phase-header__date col-2 sm:col-4">
         <h6 class="bold"><?= i::__("Data de publicação") ?></h6>
         <h4 class="opportunity__color bold">{{ publishDate }}</h4>
     </div>
