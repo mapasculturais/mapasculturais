212f2488b67090f069faebee62b705907842d53b
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2024-12-16 21:31:59 -0300

    corrige exibição do embedtools de ficha de inscrião para editais sem step definido (editais legados)

--- src/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
+++ src/themes/BaseV1/layouts/parts/singles/registration-single--fields.php
@@ -1,6 +1,6 @@
 <div ng-if="data.fields.length > 0" id="registration-attachments" class="registration-fieldset">
     <div class="registration-step" ng-controller="RegistrationFieldsController" ng-repeat="(stepName, fields) in data.fieldsByStep">
-        <div class="registration-step__title" ng-if="fields.filter(showField).length > 0">{{ $index + 1 }}. {{stepName}} </div>
+        <div class="registration-step__title" ng-if="fields.filter(showField).length > 0 && stepName">{{ $index + 1 }}. {{stepName}} </div>
 
         <?php $this->applyTemplateHook('registration-field-list', 'before') ?>
         <ul class="attachment-list">
