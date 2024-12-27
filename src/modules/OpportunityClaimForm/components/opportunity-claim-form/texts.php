269f83ef263d13fdcdf895a509b9fd7071a61d59
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-12-27 10:13:56 -0300

    remove antigo módulo de recurso (OpportunityClaimForm)

--- src/modules/OpportunityClaimForm/components/opportunity-claim-form/texts.php
+++
@@ -1,6 +0,0 @@
-<?php
-use MapasCulturais\i;
-
-return [
-    'Solicitação de recurso enviada' => i::__('Solicitação de recurso enviada'),
-];
--- src/modules/OpportunityClaimForm/layouts/parts/opportunity-claim-form-component.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$this->import('opportunity-claim-form');
-?>
-<opportunity-claim-form v-if="shouldShowResults(item)" :registration="registration"></opportunity-claim-form>
