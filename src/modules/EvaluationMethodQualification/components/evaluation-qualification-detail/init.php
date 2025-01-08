8ce3dbf44ecfdd4e5ed2e779613fc0f9df5db701
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-01-08 12:21:52 -0300

    correções no método de avaliação habilitação documental para avaliações legadas de versões anteriores à 7.6

--- src/modules/EvaluationMethodQualification/components/evaluation-qualification-detail/init.php
+++ src/modules/EvaluationMethodQualification/components/evaluation-qualification-detail/init.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
-*/
+ */
 
 use MapasCulturais\Entities\Registration;
 
@@ -11,31 +12,31 @@ $entity = $this->controller->requestedEntity;
 // SOLUÇÃO TEMPORÁRIA
 $class = $entity->getClassName();
 
-if($class == Registration::class) {
+if ($class == Registration::class) {
     $needs_tiebreaker = $entity->needsTieBreaker();
-    
+
     $opportunity = $entity->opportunity;
     $evaluation_configuration = $opportunity->evaluationMethodConfiguration;
     
     if(!$evaluation_configuration) {
         return;
     }
-
-    $enable_external_reviews = $evaluation_configuration->showExternalReviews;
     
+    $enable_external_reviews = $evaluation_configuration->showExternalReviews;
+
     $related_agents = $evaluation_configuration->relatedAgents;
     $is_minerva_group = false;
-    
-    foreach($related_agents as $group => $agents) {
-        if($group == '@tiebreaker') {
-            foreach($agents as $agent) {
-                if($agent->id == $app->user->profile->id && $agent->status == 1) {
+
+    foreach ($related_agents as $group => $agents) {
+        if ($group == '@tiebreaker') {
+            foreach ($agents as $agent) {
+                if ($agent->id == $app->user->profile->id && $agent->status == 1) {
                     $is_minerva_group = true;
                 }
             }
         }
     }
-    
+
     $data = [];
     if ($needs_tiebreaker && $is_minerva_group && $enable_external_reviews) {
         $em = $evaluation_configuration->evaluationMethod;
