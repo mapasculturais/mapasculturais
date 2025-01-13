d4267e48533a34f6e918dc1fcf7cbed9b2ade7ce
israelmelo <israelg.8@gmail.com>
2025-01-13 15:42:35 -0300

    Renomeia css e classes css do method AppealPhase

--- src/modules/EvaluationMethodAppealPhase/components/appeal-phase-evaluation-form/template.php
+++ src/modules/EvaluationMethodAppealPhase/components/appeal-phase-evaluation-form/template.php
@@ -11,30 +11,30 @@ $this->import('
     mc-select
     mc-modal
     evaluation-actions
-    evaluation-simple-detail
+    evaluation-appeal-phase-detail
 ');
 ?>
 
-<div class="simple-evaluation-form">
+<div class="appeal-phase-evaluation-form">
     <h2 v-if="needsTiebreaker && isMinervaGroup && enableExternalReviews" class="needs-tiebreaker danger__background"><?= i::_e('Voto de minerva') ?></h2>
     <mc-modal v-if="needsTiebreaker && isMinervaGroup && enableExternalReviews" :title="`${evaluationName} - ${entity.number}`" classes="registration-results__modal">
         <template #default>
-            <evaluation-simple-detail :registration="entity"></evaluation-simple-detail>
+            <evaluation-appeal-phase-detail :registration="entity"></evaluation-appeal-phase-detail>
         </template>
     
         <template #button="modal">
             <button class="button button--primary button--sm button--large" @click="modal.open()"><?php i::_e('Ver pareceres dos demais avaliadores') ?></button>
         </template>
     </mc-modal>
-    <div class="simple-evaluation-form__form grid-12">
-        <div class="simple-evaluation-form__header field col-12">
+    <div class="appeal-phase-evaluation-form__form grid-12">
+        <div class="appeal-phase-evaluation-form__header field col-12">
             <label class="field__label">
                 <?php i::_e('Selecione o status dessa inscrição') ?>
             </label>
             <mc-select v-if="isEditable" :default-value="formData.data.status" @change-option="handleOptionChange" :options="statusList"></mc-select>
             <input v-if="!isEditable" type="text" :value="statusToString(formData.data.status)" disabled>
         </div>
-        <div class="simple-evaluation-form__content field col-12">
+        <div class="appeal-phase-evaluation-form__content field col-12">
             <label class="field__label">
                 <?php i::_e('Insira suas Justificativas ou observações') ?>
             </label>
