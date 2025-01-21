c2ac97e198330043f850d6cd37b2af975e5cc33e
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-01-21 12:09:14 -0300

    ajusta componente appeal-phase-chat para funcionar na pagina do avaliador. Ref.: #3364

--- src/modules/OpportunityAppealPhase/components/appeal-phase-chat/template.php
+++ src/modules/OpportunityAppealPhase/components/appeal-phase-chat/template.php
@@ -12,4 +12,61 @@ $this->import('
 
 ?>
 
-<mc-chat v-if="thread" :thread="thread" anonymous-sender="<?= i::__('Avaliador') ?>"></mc-chat>
+<mc-chat v-if="thread" :thread="thread" anonymous-sender="<?= i::__('Avaliador') ?>">
+    <template #message-payload="{ message }">
+        <div class="mc-chat__reviewer-message field" :let="initMessage(message)">
+            <label for="status"><?= i::__('Resultado da validação:') ?></label>
+            <mc-select :default-value="message.payload.status" @change-option="message.payload.status = $event.value" id="status">
+                <div v-for="(label, value) in statusList" :key="value" :value="value">
+                    <mc-icon name="circle" :class="verifyState(value)"></mc-icon>
+                    {{ label }}
+                </div>
+            </mc-select>
+            
+            <label for="agent-response"><?= i::__('Justificativa:') ?></label>
+            <textarea 
+                v-model="message.payload.message" 
+                ref="textarea" 
+                placeholder="<?= i::__('Digite sua mensagem') ?>" 
+                id="agent-response" 
+                class="mc-chat__textarea">
+            </textarea>
+
+            <div class="mc-chat__endChat field">
+                <input type="checkbox" v-model="message.payload.endChat" id="endChat"></input>
+                <label for="endChat"><?= i::__('Encerrar processo') ?></label>
+            </div>
+    
+            <textarea 
+                v-if="message.payload.endChat"
+                v-model="message.payload.justification" 
+                ref="textarea" 
+                placeholder="<?= i::__('Digite sua justificativa para encerrar o processo') ?>" 
+                id="endChat-response" 
+                class="mc-chat__textarea">
+            </textarea>
+        </div>
+    </template>
+    <template #default="{ message }">
+        <div v-if="typeof message.payload === 'string'">
+            <p>{{ message.payload }}</p>
+        </div>
+
+        <div v-if="typeof message.payload === 'object'">
+            <div class="mc-chat__evaluation">
+                <div class="mc-chat__evaluation-status">
+                    <h4 class="semibold"><?= i::__('Resultado da validação:') ?></h4>
+                    <p>{{message.payload.status}}</p>
+                </div>
+                <div class="mc-chat__text">
+                    <h4 class="semibold"><?= i::__('Justificativa ou observações:') ?></h4>
+                    <p>{{message.payload.message}}</p>
+                </div>
+                <div class="mc-chat__evaluation-closed" v-if="message.payload.endChat">
+                    <h4 class="semibold"><?= i::__('Justificativa para encerramento:') ?></h4>
+                    <p>{{message.payload.justification}}</p>
+                </div>
+            </div>
+        </div>
+</template>
+</mc-chat>
