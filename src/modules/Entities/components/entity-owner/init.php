da63ffb23424de6d4131eaf9564e9760a9a6cca5
israelmelo <israelg.8@gmail.com>
2025-01-28 15:10:37 -0300

    Correção do init do componente entity-owner para tratar o name do destination

--- src/modules/Entities/components/entity-owner/init.php
+++ src/modules/Entities/components/entity-owner/init.php
@@ -12,10 +12,16 @@ $class = $entity->getClassName();
 $has_request =  false;
 $request_data =  [];
 $destinationName = "";
-if ($request = $app->repo("RequestChangeOwnership")->findBy(["originType" => $class, "originId" => $entity->id])) {
-    $destination = $app->repo('Agent')->find($request[0]->destinationId);
+if ($request = $app->repo("RequestChangeOwnership")->findOneBy(["originType" => $class, "originId" => $entity->id])) {
+    $destination = $app->repo('Agent')->find($request->destinationId);
     $has_request = true;
-    $destinationName = $destination->name;
+    if($entity->ownerUser->id == $request->requesterUser->id) {
+        // ceder propriedade
+        $destinationName =  $destination->ownerUser->profile->name;
+    } else {
+        // reinvindicar propriedade
+        $destinationName = $entity->ownerUser->profile->name;
+    }
 
     $request_data = [
         'id' => $request->id,
