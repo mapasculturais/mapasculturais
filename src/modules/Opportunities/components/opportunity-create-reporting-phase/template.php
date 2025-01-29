bb634c62e326d353caf69a05873d1b63c8863dc0
leopiccionia <leopiccionia@gmail.com>
2025-01-29 18:07:05 -0300

    Cria modal de criação de fase de prestação de informações (ref.: #3372) (WIP)

---
+++ src/modules/Opportunities/components/opportunity-create-reporting-phase/template.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+use MapasCulturais\i;
+
+$this->import('
+    entity-field
+    mc-modal
+');
+?>
+
+<mc-modal title="<?= i::__("Adicionar etapa de prestação de informações") ?>" @open="createEntities()" @close="destroyEntities()" classes="-with-datepicker">
+    <template #default="modal">
+        <div class="grid-12">
+            <p class="col-12"><?= i::__("Para criar a etapa de prestação de informações, preencha os campos abaixo.") ?></p>
+
+            <div class="col-12 modal__title"><?= i::__("Prestação de informações") ?></div>
+            <div class="col-12">
+                <entity-field :entity="collectionPhase" prop="name" label="<?= i::__("Defina um título") ?>" hideRequired></entity-field>
+            </div>
+            <div class="col-6">
+                <entity-field :entity="collectionPhase" prop="evaluationFrom" :min="minDate" :max="maxDate" label="<?= i::__("Data de início") ?>" hideRequired></entity-field>
+            </div>
+            <div class="col-6">
+                <entity-field :entity="collectionPhase" prop="evaluationTo" :min="minDate" :max="maxDate" label="<?= i::__("Data final") ?>" hideRequired></entity-field>
+            </div>
+
+            <div class="col-12 modal__title"><?= i::__("Avaliação contínua") ?></div>
+            <div class="col-12">
+                <entity-field :entity="evaluationPhase" prop="name" label="<?= i::__("Defina um título") ?>" hideRequired></entity-field>
+            </div>
+            <div class="col-6">
+                <entity-field :entity="evaluationPhase" prop="evaluationFrom" :min="minDate" :max="maxDate" label="<?= i::__("Data de início") ?>" hideRequired></entity-field>
+            </div>
+            <div class="col-6">
+                <entity-field :entity="evaluationPhase" prop="evaluationTo" :min="minDate" :max="maxDate" label="<?= i::__("Data final") ?>" hideRequired></entity-field>
+            </div>
+        </div>
+    </template>
+
+    <template #actions="modal">
+        <button class="button button--text" @click="modal.close()"><?= i::__("Cancelar") ?></button>
+        <button class="button button--primary" @click="save(modal)"><?= i::__("Adicionar") ?></button>
+    </template>
+
+    <template #button="modal">
+        <button class="button button--primary w-100" @click="modal.open()">
+            <?= i::__("Adicionar fase de avaliação") ?>
+        </button>
+    </template>
+</mc-modal>
--- src/modules/ProjectMonitoring/Controller.php
+++ src/modules/ProjectMonitoring/Controller.php
@@ -2,6 +2,16 @@
 
 namespace ProjectMonitoring;
 
+use \MapasCulturais\App;
+use \MapasCulturais\Entities;
+
 class Controller extends \MapasCulturais\Controller {
     
+    public function POST_reportingPhase() {
+        $this->requireAuthentication();
+
+        $app = App::i();
+
+        return null;
+    }
 }
