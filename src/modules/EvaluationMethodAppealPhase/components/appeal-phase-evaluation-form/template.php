6c75206ffb2a359fe0eaeeb3526d2a6c4570bb45
Everaldo Matias <matiaseveraldo@gmail.com>
2025-01-29 15:42:33 -0300

    Nao permite edicao do arquivo do parecer quando para status maior que 0. Ref.: #3395

--- src/modules/EvaluationMethodAppealPhase/components/appeal-phase-evaluation-form/template.php
+++ src/modules/EvaluationMethodAppealPhase/components/appeal-phase-evaluation-form/template.php
@@ -39,7 +39,7 @@ $this->import('
             title-modal="<?php i::_e('Anexar parecer') ?>"
             classes="col-12"
             title="<?php i::_e('Anexar parecer') ?>"
-            editable
+            :editable="!(currentEvaluation.status > 0)"
             ></entity-file>
 
         <mc-modal v-if="!isEditable":title="`${evaluationName} - ${entity.number}`" classes="registration-results__modal">
