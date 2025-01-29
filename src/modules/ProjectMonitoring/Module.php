2835a3ec1dd70c3460c6018b8abda3387a40b36d
leopiccionia <leopiccionia@gmail.com>
2025-01-29 13:53:53 -0300

    Registra os metadados `isReportingPhase` e `isFinalReportingPhase` (ref.: #3397)

--- src/modules/ProjectMonitoring/Module.php
+++ src/modules/ProjectMonitoring/Module.php
@@ -2,11 +2,27 @@
 
 namespace ProjectMonitoring;
 
+use \MapasCulturais\i;
+
 class Module extends \MapasCulturais\Module {
 
     public function _init() {
     }
     
-    public function register() {}
+    public function register() {
+        $this->registerOpportunityMetadata('isReportingPhase', [
+            'label' => i::__('É fase de prestação de informações?'),
+            'type' => 'boolean',
+            'default' => false,
+            'private' => false,
+        ]);
+
+        $this->registerOpportunityMetadata('isFinalReportingPhase', [
+            'label' => i::__('É fase final de prestação de informações?'),
+            'type' => 'boolean',
+            'default' => false,
+            'private' => false,
+        ]);
+    }
 
 }
