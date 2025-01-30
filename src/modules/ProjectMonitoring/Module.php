b94d0c72b6103bd9c9bdcd1844ec3d1aa062adba
leopiccionia <leopiccionia@gmail.com>
2025-01-30 18:21:09 -0300

    Cria metadados `includesWorkPlan`  e `allowsMultipleReplies` (ref.: #3373)

--- src/modules/ProjectMonitoring/Module.php
+++ src/modules/ProjectMonitoring/Module.php
@@ -15,7 +15,7 @@ class Module extends \MapasCulturais\Module {
 
         $this->registerOpportunityMetadata('isReportingPhase', [
             'label' => i::__('É fase de prestação de informações?'),
-            'type' => 'boolean',
+            'type' => 'checkbox',
             'default' => false,
             'private' => false,
             'available_for_opportunities' => true,
@@ -23,10 +23,25 @@ class Module extends \MapasCulturais\Module {
 
         $this->registerOpportunityMetadata('isFinalReportingPhase', [
             'label' => i::__('É fase final de prestação de informações?'),
-            'type' => 'boolean',
+            'type' => 'checkbox',
             'default' => false,
             'private' => false,
             'available_for_opportunities' => true,
+        ]);
+
+        $this->registerOpportunityMetadata('includesWorkPlan', [
+            'label' => i::__('Incluir plano de trabalho na prestação de informações?'),
+            'type' => 'checkbox',
+            'default' => false,
+            'private' => false,
+            'available_for_opportunities' => true,
+        ]);
+
+        $this->registerEvauationMethodConfigurationMetadata('allowsMultipleReplies', [
+            'label' => i::__('Possibilitar mais de uma resposta do proponente'),
+            'type' => 'checkbox',
+            'default' => false,
+            'private' => false,
         ]);
 
         $app->registerController('projectReporting', Controller::class);
