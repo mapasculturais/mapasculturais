652cbe198fea80dc9384567bbe002b9cf2ed63c5
leopiccionia <leopiccionia@gmail.com>
2025-01-31 18:01:45 -0300

    Atualiza texto do botão de exclusão de fase

--- src/modules/Opportunities/components/opportunity-phase-config-data-collection/texts.php
+++ src/modules/Opportunities/components/opportunity-phase-config-data-collection/texts.php
@@ -3,5 +3,4 @@ use MapasCulturais\i;
 
 return [
     'nao foi possivel remover fase' => i::__('Não foi possível remover a fase'),
-    'excluir_fase_coleta_dados' => i::__("Excluir fase de coleta de dados")
 ];
