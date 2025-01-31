09518c7d664de3c6a0de7d51c4877bcb209bdbbe
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-01-31 13:22:18 -0300

    ajusta exibição de status da inscrição e do botão de exibir detalhamento para o proponente. Ref.: #3396

--- src/modules/Opportunities/components/opportunity-phase-publish-date-config/template.php
+++ src/modules/Opportunities/components/opportunity-phase-publish-date-config/template.php
@@ -77,7 +77,7 @@ $this->import('
         </div>
         
         <div class="col-12 grid-12" v-if="true">
-            <div class="col-12" v-if="phase.evaluationMethodConfiguration">
+            <div class="col-12" v-if="phase.evaluationMethodConfiguration && !phase.isAppealPhase">
                 <entity-field :entity="phase.evaluationMethodConfiguration" prop="publishEvaluationDetails" type="checkbox" :autosave="300" ></entity-field>
             </div>
             <div class="col-12" v-if="phase.evaluationMethodConfiguration">
