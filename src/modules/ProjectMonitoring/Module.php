f9b73032a5bbba36e486e8ae4862cdde3440c11d
leopiccionia <leopiccionia@gmail.com>
2025-02-04 16:32:52 -0300

    Notifica proponentes quando fase de prestação de informações começar (ref.: #3390)

--- src/modules/ProjectMonitoring/Module.php
+++ src/modules/ProjectMonitoring/Module.php
@@ -2,11 +2,36 @@
 
 namespace ProjectMonitoring;
 
+use \MapasCulturais\App;
+use \MapasCulturais\Entities;
 use \MapasCulturais\i;
 
 class Module extends \MapasCulturais\Module {
 
     public function _init() {
+        $app = App::i();
+
+        $app->hook('entity(Registration).insert:after', function () use ($app) {
+            /** @var Entities\Registration $this */
+            if ($this->opportunity->isReportingPhase) {
+                $notification_message = sprintf(
+                    i::__('A fase de prestação de informações da inscrição %s se iniciou'),
+                    "<a href='{$this->singleUrl}'>{$this->number}</a>",
+                );
+                
+                $notification = new Entities\Notification();
+                $notification->user = $this->owner;
+                $notification->message = $notification_message;
+                $notification->save(true);
+
+                $app->createAndSendMailMessage([
+                    'from' => $app->config['mailer.from'],
+                    'to' => $this->owner->emailPrivado,
+                    'subject' => i::__('Início da fase de prestação de informações'),
+                    'body' => $notification_message,
+                ]);
+            }
+        });
     }
     
     public function register() {
