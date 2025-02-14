e801c6d4ff5943c10fb3a5b6c9b224d95e331773
Fernando Lordão <fernandolordao@gmail.com>
2025-02-14 10:19:18 -0300

    Habilita exibição dos termos para aceitação

--- config/lgpd.php
+++ config/lgpd.php
@@ -2,23 +2,20 @@
 use MapasCulturais\i;
 return [
     'module.LGPD' => [
-        // 'termsOfUsage'=>[
-        //     'title'=> 'Termos de Uso', 
-        //     'text'=> file_get_contents(__DIR__ . '/lgpd-terms/terms-of-usage.html'),
-        //     'buttonText' => i::__('Aceito os termos de uso')
-        // ],
-        // 'privacyPolicy' => [
-        //     'title'=>  'Política de Privacidade do Mapa da Cultura',
-        //     'text'=> file_get_contents(__DIR__ . '/lgpd-terms/privacy-policy.html'),
-        //     'buttonText' => i::__('Aceito as políticas de privacidade')
-        // ],
-        // 'termsUse' => [
-        //     'title'=>  'Autorização de uso de imagem',
-        //     'text'=> file_get_contents(__DIR__ . '/lgpd-terms/images-use.html'),
-        //     'buttonText' => i::__('Autorizo o uso de imagem')
-        // ],
+        'termsOfUsage'=>[
+            'title'=> 'Termos de Uso', 
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/terms-of-usage.html'),
+            'buttonText' => i::__('Aceito os termos de uso')
+        ],
+        'privacyPolicy' => [
+            'title'=>  'Política de Privacidade do Mapa da Cultura',
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/privacy-policy.html'),
+            'buttonText' => i::__('Aceito as políticas de privacidade')
+        ],
+        'termsUse' => [
+            'title'=>  'Autorização de uso de imagem',
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/images-use.html'),
+            'buttonText' => i::__('Autorizo o uso de imagem')
+        ],
     ]
 ];
-
-
-
