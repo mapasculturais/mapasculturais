87295e09ddd7e0817c77fbb2b3adc5b0c6890198
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-02-19 15:54:17 -0300

    adiciona metadado de detalhamento da execução da meta (Goal). Ref.: mapasculturais#3406

--- src/modules/ProjectMonitoring/Module.php
+++ src/modules/ProjectMonitoring/Module.php
@@ -2,7 +2,12 @@
 
 namespace ProjectMonitoring;
 
+use MapasCulturais\App;
+use MapasCulturais\Controllers\Registration;
+use MapasCulturais\Definitions\Metadata;
 use \MapasCulturais\i;
+use OpportunityWorkplan\Entities\Goal;
+use OpportunityWorkplan\Entities\Delivery;
 
 class Module extends \MapasCulturais\Module {
 
@@ -10,6 +15,8 @@ class Module extends \MapasCulturais\Module {
     }
     
     public function register() {
+        $app = App::i();
+
         $this->registerOpportunityMetadata('isReportingPhase', [
             'label' => i::__('É fase de prestação de informações?'),
             'type' => 'boolean',
@@ -23,6 +30,13 @@ class Module extends \MapasCulturais\Module {
             'default' => false,
             'private' => false,
         ]);
+
+        // Metadados para Goal (Meta)
+        $executionDetail = new Metadata('executionDetail', [
+            'label' => \MapasCulturais\i::__('Detalhamento da execução da meta')
+        ]);
+        $app->registerMetadata($executionDetail, Goal::class);
+
     }
 
 }
