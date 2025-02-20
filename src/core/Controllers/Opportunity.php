24f02987107d699b96242878c88b7899a886d281
erleibiazzio <erleibiazzio@gmail.com>
2025-02-20 18:11:32 -0300

    Implementa possibilidade de desativar as cotas nas requisições via API no endpoint findRegistrations

--- src/core/Controllers/Opportunity.php
+++ src/core/Controllers/Opportunity.php
@@ -368,7 +368,7 @@ class Opportunity extends EntityController {
         $this->apiResponse($fields);
     }
 
-    function apiFindRegistrations($opportunity, $query_data) {
+    function apiFindRegistrations($opportunity, $query_data, $enalble_quota = false) {
         $app = App::i();
         $app->registerFileGroup('registration', new \MapasCulturais\Definitions\FileGroup('zipArchive',[], '', true, null, true));
         $data = $query_data;
@@ -398,7 +398,8 @@ class Opportunity extends EntityController {
             }
 
             $current_phase_query_params = [
-                'opportunity' => API::EQ($phase->id)
+                'opportunity' => API::EQ($phase->id),
+                '__enableQuota' => $enalble_quota
             ];
             
             // $phase é a fase que foi informada no parâmetro @opportunity
@@ -532,7 +533,7 @@ class Opportunity extends EntityController {
             $query_data['status'] = API::GT(0);
         }
 
-        $result = $this->apiFindRegistrations($opportunity, $query_data);
+        $result = $this->apiFindRegistrations($opportunity, $query_data, enalble_quota: true);
         
         $this->apiAddHeaderMetadata($query_data, $result->registrations, $result->count);
         $this->apiResponse($result->registrations);
