c0c4f726ada513b58c217c36d1318cf5547cb180
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2025-03-02 23:13:28 -0300

    feat: adicionando, no db-updates, os scripts de exclusao dos campos e anexos de formulario que foram duplicados com erro

--- src/db-updates.php
+++ src/db-updates.php
@@ -2610,5 +2610,16 @@ $$
                FROM etnias;
         ");
     },
+    "Removendo os campos e anexos de formulário erroneamente duplicados pela funcionalidade 'Duplicar Oportunidade'" => function() {
+        __try("DELETE FROM registration_field_configuration rfc
+                     USING registration_step rs
+                     WHERE rs.id = rfc.step_id
+                       AND rs.opportunity_id != rfc.opportunity_id;");
+
+        __try("DELETE FROM registration_file_configuration rfc
+                     USING registration_step rs
+                     WHERE rs.id = rfc.step_id
+                       AND rs.opportunity_id != rfc.opportunity_id;");
+    }
   
 ] + $updates ;   
