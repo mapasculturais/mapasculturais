a485d2a7c7a52eeddf71495b2727d49ed68eb8fa
israelmelo <israelg.8@gmail.com>
2025-03-11 17:49:52 -0300

    Adiciona alerta para habilitar criação da fase de monitoramento apenas se tiver data de publicação final configurada

--- src/modules/Opportunities/components/opportunity-phases-config/template.php
+++ src/modules/Opportunities/components/opportunity-phases-config/template.php
@@ -99,7 +99,11 @@ $this->import('
         <template v-else-if="index === phases.length - 1">
             <div class="add-phase grid-12">
                 <div class="col-12" v-if="!finalReportingPhase">
-                    <opportunity-create-reporting-phase :opportunity="entity" @create="addReportingPhases"></opportunity-create-reporting-phase>
+                    <mc-alert v-if="!lastPhase?.publishTimestamp" type="warning">
+                        <p><small class="required"><?= i::__("A data e hora da 'Publicação final' precisa estar preenchida para adicionar novas fases de prestação de informações.") ?></small></p>
+                    </mc-alert>
+
+                    <opportunity-create-reporting-phase v-if="lastPhase?.publishTimestamp" :opportunity="entity" @create="addReportingPhases"></opportunity-create-reporting-phase>
                 </div>
             </div>
         </template>
