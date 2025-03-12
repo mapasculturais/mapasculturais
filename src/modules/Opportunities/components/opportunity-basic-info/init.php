b4c272277890005d74f763b0c503f44df275060a
Welington De Lima Olegario <wolegario49@gmail.com>
2025-03-12 11:31:03 -0300

    Melhora logica que define a data final dos editais de fluxo continuo

--- src/modules/Opportunities/components/opportunity-basic-info/init.php
+++ src/modules/Opportunities/components/opportunity-basic-info/init.php
@@ -1,2 +1,7 @@
 <?php
 $this->useOpportunityAPI();
+$entity = $this->controller->requestedEntity;
+
+$this->jsObject['config']['opportunityBasicInfo'] = [
+    'date' => $entity::CONTINUOUS_FLOW_DATE,
+];
