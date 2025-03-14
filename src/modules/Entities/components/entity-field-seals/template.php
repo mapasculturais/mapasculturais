a4b20c992bce431ce45bb2ed0f98f7e9f64967d6
leopiccionia <leopiccionia@gmail.com>
2025-03-14 15:51:18 -0300

    Sempre exibe um texto de validação (Ref. #3430)
    Esse novo comportamento evita 'content jumps'

--- src/modules/Entities/components/entity-field-seals/template.php
+++ src/modules/Entities/components/entity-field-seals/template.php
@@ -12,9 +12,9 @@ $this->import('
 
 <div class="entity-field-seals" v-if="seals.length > 0">
     <div class="entity-field-seal" v-for="seal of seals" :key="seal.sealRelationId" :title="seal.name">
-        <div class="entity-field-seal__image" @click="setSealTouch(seal)" @mouseenter="setSeal(seal)" @mouseleave="setSeal(null)">
+        <div class="entity-field-seal__image" @click="setSealTouch(seal)" @mouseenter="setSeal(seal)">
             <mc-avatar v-if="seal.files?.avatar" :entity="seal" size="small" square></mc-avatar>
             <mc-icon v-else name="seal"></mc-icon>
-    </div>
+        </div>
     </div>
 </div>
