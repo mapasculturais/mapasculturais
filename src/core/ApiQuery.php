58bc3bc9e424fcd1aa9355492035452a671d8e40
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-03-25 12:23:17 -0300

    se o controle de acesso estiver desligado, não usa o filtro por permissão na api

--- src/core/ApiQuery.php
+++ src/core/ApiQuery.php
@@ -3156,6 +3156,11 @@ class ApiQuery {
 
     protected function _addFilterByPermissions($value) {
         $app = App::i();
+
+        if(!$app->isAccessControlEnabled()) {
+            return;
+        }
+
         $user = $this->_permissionsUser ?
             $app->repo('User')->find($this->_permissionsUser) :
             $app->user;
