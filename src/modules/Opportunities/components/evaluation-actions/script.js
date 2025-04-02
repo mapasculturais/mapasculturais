907ba9909c13fa8ab4f3bf07689cbe650f058b63
leopiccionia <leopiccionia@gmail.com>
2025-04-02 16:46:12 -0300

    Corrige ordenamento de requisições na página de avaliação

--- src/modules/Opportunities/components/evaluation-actions/script.js
+++ src/modules/Opportunities/components/evaluation-actions/script.js
@@ -25,7 +25,7 @@ app.component('evaluation-actions', {
     },
 
     data() {
-        return {            
+        return {
             evaluationRegistrationList: null,
             currentEvaluation: $MAPAS.config.evaluationActions?.currentEvaluation || null,
             oldEvaluation: null
@@ -44,51 +44,50 @@ app.component('evaluation-actions', {
 
     methods: {
         buttonActionsActive(action){
-            let reg = eval(`this.${action}?.registrationId`);
+            const reg = this[action]?.registrationId;
             return reg != this.entity.id;
         },
+
         getEvaluationList(data){
             if (data.detail.evaluationRegistrationList){
                 this.evaluationRegistrationList = data.detail.evaluationRegistrationList;
             }
         },
-        
-        requestEvaluation(action, data = {}, args = {}, controller = 'registration') {
-            return new Promise((resolve, reject) => {
+
+        async requestEvaluation(action, data = {}, args = {}, controller = 'registration') {
+            return new Promise(async (resolve) => {
                 if (action == 'reopenEvaluation' || !this.globalState.validateEvaluationErrors) {
                     const api = new API(controller);
-                    let url = api.createUrl(action, args);
-                    let result = api.POST(url, data);
-                    resolve(result);
-                } 
+                    const url = api.createUrl(action, args);
+                    const res = await api.POST(url, data);
+                    resolve(await res.json());
+                }
             });
         },
 
         dispatchResponse(type, response) {
             this.oldEvaluation = this.currentEvaluation;
             this.currentEvaluation = response;
-            window.dispatchEvent(new CustomEvent('responseEvaluation', {detail:{response: response, type: type}}));
+            window.dispatchEvent(new CustomEvent('responseEvaluation', { detail: { response, type } }));
         },
 
         dispatchErrors() {
-            window.dispatchEvent(new CustomEvent('processErrors', {detail:{}}));
+            window.dispatchEvent(new CustomEvent('processErrors', { detail: {} }));
         },
 
-        saveEvaluation(finish = false) {
+        async saveEvaluation(finish = false) {
             const messages = useMessages();
-            let args = {id: this.entity.id};
+            const args = { id: this.entity.id };
 
             if (finish) {
-                args['status'] = 'evaluated';
+                args.status = 'evaluated';
             }
 
-            this.requestEvaluation('saveEvaluation', this.formData, args).then(res => res.json()).then(response => {
+            return this.requestEvaluation('saveEvaluation', this.formData, args).then(response => {
                 if (response.error) {
                     messages.error(response.data);
-                    
                 } else {
                     this.dispatchResponse('saveEvaluation', response);
-
                     if (finish) {
                         messages.success(this.text('finish'));
                         this.updateSummaryEvaluations('completed');
@@ -100,12 +99,13 @@ app.component('evaluation-actions', {
             });
         },
 
-        sendEvaluation(){
-            this.saveEvaluation(true);
+        async sendEvaluation(){
             const messages = useMessages();
-            let args = {id: this.entity.id};
+            const args = { id: this.entity.id };
 
-            this.requestEvaluation('sendEvaluation', {data: this.formData}, args).then(res => res.json()).then(response => {
+            await this.saveEvaluation(true);
+
+            return this.requestEvaluation('sendEvaluation', { data: this.formData }, args).then(response => {
                 if (response.error) {
                     messages.error(response.data);
                 } else {
@@ -116,33 +116,33 @@ app.component('evaluation-actions', {
             });
         },
 
-        finishEvaluation() {
+        async finishEvaluation() {
             this.dispatchErrors();
-            this.saveEvaluation(true);
+            await this.saveEvaluation(true);
             this.updateSummaryEvaluations('completed');
         },
 
-        finishEvaluationSend() {
+        async finishEvaluationSend() {
             this.dispatchErrors();
-            this.sendEvaluation();
+            await this.sendEvaluation();
             if (this.lastRegistration?.registrationid != this.entity.id && !this.globalState.validateEvaluationErrors){
                 this.next();
             } 
         },
 
-        finishEvaluationSendLater(){
+        async finishEvaluationSendLater(){
             this.dispatchErrors();
-            this.saveEvaluation(true);
+            await this.saveEvaluation(true);
             if (this.lastRegistration?.registrationid != this.entity.id && !this.globalState.validateEvaluationErrors){
                 this.next();
             } 
         },
 
-        reopen(){
+        async reopen(){
             const messages = useMessages();
-            let args = {id: this.entity.id};
+            const args = { id: this.entity.id };
 
-            this.requestEvaluation('reopenEvaluation', {data: this.formData}, args).then(res => res.json()).then(response => {
+            return this.requestEvaluation('reopenEvaluation', { data: this.formData }, args).then(response => {
                 if (response.error) {
                     messages.error(response.data);
                 } else {
@@ -154,52 +154,37 @@ app.component('evaluation-actions', {
         },
 
         previous() {
-            window.dispatchEvent(new CustomEvent('previousEvaluation', {detail:{registrationId:this.entity.id}}));
+            window.dispatchEvent(new CustomEvent('previousEvaluation', { detail: { registrationId: this.entity.id } }));
         },
 
         next() {
-            window.dispatchEvent(new CustomEvent('nextEvaluation', {detail:{registrationId:this.entity.id}}));
+            window.dispatchEvent(new CustomEvent('nextEvaluation', { detail: { registrationId: this.entity.id } }));
         },
 
         showActions(action) {
             let result = false;
-            switch (action) {
-                case 'finishEvaluation':
-                case 'save':
-                    if (!this.currentEvaluation) {
-                        result = true;
-                    }
+            if (action === 'finishEvaluation' || action === 'save') {
+                if (!this.currentEvaluation) {
+                    return true;
+                }
 
-                    if (this.currentEvaluation?.status < 1) {
-                        result = true;
-                    }
-
-                    if (this.currentEvaluation?.status === undefined) {
-                        result = true;
-                    }
-
-                    if (this.currentEvaluation?.status == "") {
-                        result = true;
-                    }
-                    break;
-                case 'send':
-                case 'reopen':
-                    result = this.currentEvaluation?.status == 1;
-                    break;
-                default:
-                    result = false;
-                    break;
+                const status = this.currentEvaluation.status;
+                if (status < 1 || status === undefined || status == '') {
+                    return true;
+                }
+            } else if (action === 'send' || action === 'reopen') {
+                return this.currentEvaluation?.status == 1;
             }
-            return result;
+            return false;
         },
 
-        updateSummaryEvaluations(newStatus) {     
+        updateSummaryEvaluations(newStatus) {
             // remove status anterior
-            if(!this.oldEvaluation) {
-                this.oldEvaluation = {status: null}
+            if (!this.oldEvaluation) {
+                this.oldEvaluation = { status: null };
             }
-            
-            switch(this.oldEvaluation.status) {
+
+            switch (this.oldEvaluation.status) {
                 case 0:
                     this.global.summaryEvaluations.started -= 1;
                     break;
@@ -213,9 +198,9 @@ app.component('evaluation-actions', {
                     this.global.summaryEvaluations.pending -= 1;
                     break;
             }
-            
+
             // adiciona novo status
-            switch(newStatus) {
+            switch (newStatus) {
                 case 'pending':
                     this.global.summaryEvaluations.pending += 1;
                     break;
@@ -227,7 +212,7 @@ app.component('evaluation-actions', {
                     break;
                 case 'sent':
                     this.global.summaryEvaluations.sent += 1;
-                    break;    
+                    break;
             }
         }
     }
