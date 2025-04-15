49c6b285e959b9b92d49b3ec828de6d1381aec68
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2025-04-15 12:54:16 -0300

    remove console log

--- src/modules/Opportunities/components/registration-card/template.php
+++ src/modules/Opportunities/components/registration-card/template.php
@@ -22,7 +22,6 @@ $this->import('
 
         <div class="right">            
             <div class="header">
-                {{console.log(entity.opportunity)}}
                 <div v-if="pictureCard" class="title"> <strong>{{entity.opportunity?.name}}</strong> </div>
                 <?php if($app->config['app.registrationCardFields']['number']): ?>
                     <div v-if="!pictureCard" class="title"><?= $this->text('my-records-numbers', i::__('Número de inscrição:')) ?> <strong>{{entity.number}}</strong> </div>
