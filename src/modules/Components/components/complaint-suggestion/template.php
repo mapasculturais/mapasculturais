3944fa1d9b3a77c8ae9c9f2a7bb35358cfd08009
Victor Ferreira <vitfera@hotmail.com>
2025-04-30 12:59:48 -0300

    fix: complaint-suggestion

--- src/modules/Components/components/complaint-suggestion/template.php
+++ src/modules/Components/components/complaint-suggestion/template.php
@@ -63,9 +63,6 @@ $this->import("
                     <label><?php i::_e('Sua mensagem foi enviada para a equipe responsável. Agradecemos a contribuição.'); ?> </label>
                 </div>
             </template>
-            <template v-if="!sendSuccess"  #actions="modal">
-                <!-- Componente responsável por renderizar o CAPTCHA -->
-                <mc-captcha @captcha-verified="verifyCaptcha" @captcha-expired="expiredCaptcha" :error="error" class="complaint-suggestion__recaptcha"></mc-captcha>
 
             <template v-if="!sendSuccess"  #actions="modal">
                 <mc-captcha @captcha-verified="verifyCaptcha" @captcha-expired="expiredCaptcha" class="col-12"></mc-captcha>
@@ -141,9 +138,9 @@ $this->import("
                 </div>
             </template>
 
-            <template v-if="!sendSuccess" #actions="modal">
+            <template #actions="modal">
                 <!-- Componente responsável por renderizar o CAPTCHA -->
-                <mc-captcha @captcha-verified="verifyCaptcha" @captcha-expired="expiredCaptcha" :error="error" class="complaint-suggestion__recaptcha"></mc-captcha>
+                <mc-captcha class="complaint-suggestion__recaptcha" @captcha-verified="verifyCaptcha" @captcha-expired="expiredCaptcha"></mc-captcha>
 
                 <button class="button button--primary" @click="send(modal)"><?= i::__('Enviar Mensagem') ?></button>
                 <button class="button button--text button--text-del" @click="modal.close()"><?= i::__('Cancelar') ?></button>
