af8b2f7ec93823a14f555feb2dc3064fc71082cb
erleibiazzio <erleibiazzio@gmail.com>
2025-05-20 11:42:32 -0300

    Melhora a interface da timeline de acompanhamento da inscrição para exibir a data de encerramento do prazo de edição, quando houver campos liberados para edição para o usuário.

--- src/modules/Opportunities/components/registration-form-timeline/template.php
+++ src/modules/Opportunities/components/registration-form-timeline/template.php
@@ -5,4 +5,8 @@
 
  use MapasCulturais\i;
 ?>
-<button v-if="showButton()" @click="editForm()" class="button button--primary"><?= i::__('Editar informações enviadas') ?></button>
+<div v-if="showButton()" class="editable-fields">
+    <button  @click="editForm()" class="button button--primary"><?= i::__('Editar informações enviadas') ?></button>
+    <small><i><?= i::__('O prazo para editar as informações termina em') ?> {{formatEditableUntil}}</i></small>
+    
+</div>
