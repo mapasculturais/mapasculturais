9d7761f3ef26fd9ba115b123f7e450c331927c74
kterva <leotrujillo@vera.com.uy>
2025-05-29 15:12:00 -0300

    Internacionalização: substituição de textos estáticos por chamadas à função de tradução i::_e() em múltiplos arquivos de tema e componentes

--- src/modules/Entities/components/opportunity-create-model/template.php
+++ src/modules/Entities/components/opportunity-create-model/template.php
@@ -22,7 +22,7 @@ $this->import("
 
                 <div class="field">
                     <label><?= i::__('Breve descrição do modelo') ?><span class="required">*</span></label>
-                    <textarea placeholder="Breve descrição" v-model="formData.description"></textarea>
+                    <textarea placeholder="<?= i::__('Breve descrição') ?>" v-model="formData.description"></textarea>
                 </div>
             </div>
         </template>
