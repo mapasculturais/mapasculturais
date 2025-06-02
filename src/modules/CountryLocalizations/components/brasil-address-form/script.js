039feecd0b2f97339329806430c9713258037cb1
israelmelo <israelg.8@gmail.com>
2025-06-02 09:29:09 -0300

    Cria componente brasil-address-form Ref.: #3497

---
+++ src/modules/CountryLocalizations/components/brasil-address-form/script.js
@@ -0,0 +1,288 @@
+
+app.component('brasil-address-form', {
+    template: $TEMPLATES['brasil-address-form'],
+    emits: [],
+
+    setup(props, { slots }) {
+        const hasSlot = name => !!slots[name]
+        return { hasSlot }
+    },
+    data(){
+        let cities = {};
+        return {cities};
+    },
+
+    computed: {
+        hasPublicLocation() {
+            return !!this.entity.$PROPERTIES.publicLocation;
+        },
+        statesAndCities(){
+            return $MAPAS.config.brasilAddressForm.statesAndCities;
+        },
+        statesAndCitiesEnable(){
+            return true;
+        },
+        statesAndCitiesCountryCode() {
+            return $MAPAS.config.brasilAddressForm.statesAndCitiesCountryCode;
+        },
+        states(){
+            let states = [];
+
+            const statesArray = this.statesAndCities[1];
+
+            Object.keys(statesArray).forEach((uf) => {
+                const stateData = statesArray[uf];
+                states.push({
+                    value: uf,
+                    label: stateData[0]
+                });
+            });
+
+            if (this.entity.En_Estado) {
+                this.citiesList();
+            }
+
+            return states.sort((a, b) => a.label.localeCompare(b.label));
+        }
+    },
+
+    props: {
+        entity: {
+            type: Entity,
+            required: true
+        },
+        editable: {
+            type: Boolean,
+            default: false,
+        },
+        hideLabel: {
+            type: Boolean,
+            default: false,
+        },
+        classes: {
+            type: [String, Array, Object],
+            required: false
+        },
+    },
+
+    watch: {
+        'entity.En_Pais'(_new, _old){
+            if(_new != _old && this.statesAndCitiesCountryCode != 'BR') {
+                this.entity.En_Nome_Logradouro = "";
+                this.entity.En_Num             = "";
+                this.entity.En_Complemento     = "";
+                this.entity.En_Bairro          = "";
+                this.entity.En_Municipio       = "";
+                this.entity.En_Estado          = "";
+                this.entity.En_CEP             = "";
+            }
+        }
+    },
+
+    methods: {
+        propId(prop) {
+            let uid = Math.random().toString(36).slice(2);
+            return`${this.entity.__objectId}--${prop}--${uid}`;
+        },
+        verifiedAdress() {
+            if(this.entity.currentUserPermissions['@control']){
+                return true;
+            };
+            const fields = ['En_Nome_Logradouro', 'En_Num', 'En_Bairro', 'En_Municipio', 'En_Estado', 'En_CEP'];
+            let result = !this.hasPublicLocation || this.entity.publicLocation;
+            fields.forEach((element)=> {
+                if(this.entity[element]==null) {
+                    result = false;
+                    return;
+                }
+            });
+               
+            return result;
+        },
+        address() {
+            this.entity.En_Pais = this.statesAndCitiesCountryCode == 'BR' ? this.statesAndCitiesCountryCode : this.entity.En_Pais;
+            let rua         = this.entity.En_Nome_Logradouro == null ? '' : this.entity.En_Nome_Logradouro;
+            let numero      = this.entity.En_Num             == null ? '' : this.entity.En_Num;
+            let complemento = this.entity.En_Complemento     == null ? '' : this.entity.En_Complemento;
+            let bairro      = this.entity.En_Bairro          == null ? '' : this.entity.En_Bairro;
+            let cidade      = this.entity.En_Municipio       == null ? '' : this.entity.En_Municipio;
+            let estado      = this.entity.En_Estado          == null ? '' : this.entity.En_Estado;
+            let cep         = this.entity.En_CEP             == null ? '' : this.entity.En_CEP;
+
+            // rua, num, complemento - bairro - cidade/uf - CEP: 00000000
+            var address = '';
+
+            if(rua) {
+                address += rua;
+            }
+
+            if(numero) {
+                if (address) {
+                    address += ', ' + numero;
+                } else {
+                    address += numero;
+                }
+            }
+
+            if(complemento) {
+                if (address) {
+                    address += ', ' + complemento;
+                } else {
+                    address += complemento;
+                }
+            }
+
+            if(bairro) {
+                if (address) {
+                    address += ' - ' + bairro;
+                } else {
+                    address += bairro;
+                }
+            }
+
+            if (cidade && estado) {
+                if (address) {
+                    address += ' - ' + cidade + '/' + estado;
+                } else {
+                    address += cidade + '/' + estado;
+                }                
+            } else if (cidade) {
+                if (address) {
+                    address += ' - ' + cidade;
+                } else {
+                    address += cidade;
+                }  
+            } else if (estado) {
+                if (address) {
+                    address += ' - ' + estado;
+                } else {
+                    address += estado;
+                }
+            }
+
+            if(cep) {
+                if (address) {
+                    address += ' - CEP: ' + cep;
+                } else {
+                    address += 'CEP: ' + cep;
+                }
+            }
+           
+            this.entity.endereco = address;
+            this.geolocation();
+        },
+
+        pesquisacep(valor) {
+            //Nova variável "cep" somente com dígitos.
+            var cep = valor.replace(/\D/g, '');                
+            if (cep != "") {
+                var validacep = /^[0-9]{8}$/;   
+                if(validacep.test(cep)) {     
+                    fetch('//viacep.com.br/ws/'+ cep +'/json/')
+                        .then((response) => response.json())
+                        .then((data) => {
+                            this.entity.En_Nome_Logradouro = data.logradouro;
+                            this.entity.En_Bairro = data.bairro;
+                            this.entity.En_Municipio = data.localidade;
+                            this.entity.En_Estado = data.uf;
+                            this.address();
+                        });    
+                } 
+            } 
+        },
+
+        formatParams( params ){
+            return "?" + Object.keys(params).map(function(key){
+                            return key+"="+encodeURIComponent(params[key])
+                        }).join("&");
+        },
+
+        geolocation() {
+            let rua         = this.entity.En_Nome_Logradouro == null ? '' : this.entity.En_Nome_Logradouro;
+            let numero      = this.entity.En_Num             == null ? '' : this.entity.En_Num;
+            let bairro      = this.entity.En_Bairro          == null ? '' : this.entity.En_Bairro;
+            let cidade      = this.entity.En_Municipio       == null ? '' : this.entity.En_Municipio;
+            let estado      = this.entity.En_Estado          == null ? '' : this.entity.En_Estado;
+            let cep         = this.entity.En_CEP             == null ? '' : this.entity.En_CEP;
+
+            if (estado && cidade) {
+                var address = bairro ?
+                    rua + " " + numero + ", " + bairro + ", " + cidade + ", " + estado :
+                    rua + " " + numero + ", " + cidade + ", " + estado;
+
+                var addressElements = {
+                    fullAddress: address,
+                    streetName: rua,
+                    city: cidade,
+                    state: estado,
+                };
+
+                if (numero)
+                    addressElements["number"] = numero;
+
+                if (bairro)
+                    addressElements["neighborhood"] = bairro;
+
+                if (cep)
+                    addressElements["postalCode"] = cep;
+
+                var params = {
+                    format: "json",
+                    countrycodes: "br"
+                };
+                var structured = false;
+
+                if (addressElements.streetName) {
+                    params.street = (addressElements.number ? addressElements.number + " " : "") + addressElements.streetName;
+                    structured = true;
+                }
+                if (addressElements.city) {
+                    params.city = addressElements.city;
+                    structured = true;
+                }
+                if (addressElements.state) {
+                    params.state = addressElements.state;
+                    structured = true;
+                }
+                if (addressElements.country) {
+                    params.country = addressElements.country;
+                    structured = true;
+                }
+                // Parece que o nominatim não se dá bem com nosso CEP
+                // if (addressElements.postalCode) {
+                //     params.postalcode = addressElements.postalCode;
+                //     structured = true;
+                // }
+                if (!structured && addressElements.fullAddress) {
+                    params.q = addressElements.fullAddress;
+                }
+
+                let url = 'https://nominatim.openstreetmap.org/search' + this.formatParams(params);
+                fetch(url)
+                    .then( response => response.json() )
+                    .then( r => {
+                        // Consideramos o primeiro resultado
+                        if (r[0] && r[0].lat && r[0].lon) {
+                            this.entity.location = {lat: r[0].lat, lng: r[0].lon};
+                        }
+                    } );
+            }            
+        },
+        citiesList(){
+            const uf = this.entity.En_Estado;
+            const statesArray = this.statesAndCities[1];
+
+            if (!uf || !statesArray[uf]) {
+                this.cities = [];
+                return;
+            }
+
+            this.cities = statesArray[uf][1]
+                .map(c => c[0])
+                .sort((a, b) => a.localeCompare(b));
+        },
+        isRequired(field){
+            return $DESCRIPTIONS[this.entity.__objectType][field].required;
+        }
+    }
+});
---
+++ src/modules/CountryLocalizations/components/brasil-address-form/template.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    entity-field
+    entity-map
+');
+?>
+
+<?php $this->applyTemplateHook('entity-location','before'); ?>
+<div :class="classes" class="entity-location grid-12">
+    <?php $this->applyTemplateHook('entity-location','begin'); ?>
+    <div v-if="!hideLabel" class="entity-location__title col-12">
+        <label v-if="verifiedAdress()"><?= i::__('Endereço')?></label>
+        <?php if($this->isEditable()): ?>
+            <?php $this->info('cadastro -> configuracoes-entidades -> endereco') ?>
+        <?php endif; ?>
+    </div>
+    
+    <div class="col-12" v-if="editable">
+        <div class="grid-12">
+            <entity-field @change="address(); pesquisacep(entity.En_CEP);" classes="col-4 sm:col-12" :entity="entity" prop="En_CEP"></entity-field>
+            <entity-field @change="address()" classes="col-8 sm:col-12" :entity="entity" prop="En_Nome_Logradouro"></entity-field>
+            <entity-field @change="address()" classes="col-2 sm:col-4" :entity="entity" prop="En_Num"></entity-field>
+            <entity-field @change="address()" classes="col-10 sm:col-8" :entity="entity" prop="En_Bairro"></entity-field>
+            <entity-field @change="address()" classes="col-12" :entity="entity" prop="En_Complemento" label="<?php i::_e('Complemento ou ponto de referência')?>"></entity-field>
+            <entity-field v-if="statesAndCitiesCountryCode != 'BR'" @change="address()" classes="col-12" :entity="entity" prop="En_Pais" label="<?php i::_e('País') ?>"></entity-field>
+        </div>
+    </div>
+
+    <div class="col-12" v-if="editable && !statesAndCitiesEnable">
+        <div class="grid-12" v-if="!entity.En_Pais || entity.En_Pais == statesAndCitiesCountryCode">
+            <entity-field @change="address()" classes="col-6 sm:col-12" :entity="entity" prop="En_Estado" label="<?php i::_e('Estado')?>"></entity-field>
+            <entity-field @change="address()" classes="col-6 sm:col-12" :entity="entity" prop="En_Municipio" label="<?php i::_e('Município')?>"></entity-field>
+        </div>            
+    </div>
+ 
+    <div class="col-12" v-if="editable && statesAndCitiesEnable">
+        <div class="grid-12" v-if="!entity.En_Pais || entity.En_Pais == statesAndCitiesCountryCode">
+            <div class="field col-6">
+                <label class="field__title">
+                    <?php i::_e('Estado')?>
+                    <span v-if="isRequired('En_Estado')" class="required">*<?php i::_e('obrigatório') ?></span>
+                </label>
+                <select @change="citiesList(); address()" v-model="entity.En_Estado">
+                    <option v-for="state in states" :value="state.value">{{state.label}}</option>
+                </select>
+            </div>
+            <div class="field col-6">
+                <label class="field__title">
+                    <?php i::_e('Município')?>
+                    <span v-if="isRequired('En_Municipio')" class="required">*<?php i::_e('obrigatório') ?></span>
+                </label>
+                <select @change="address()" v-model="entity.En_Municipio">
+                    <option v-for="city in cities" :value="city">{{city}}</option>
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <div v-if="entity.En_Pais && entity.En_Pais != statesAndCitiesCountryCode" class="col-12">
+        <div class="grid-12">
+            <div class="field col-6">
+                    <label class="field__title">
+                    <?php i::_e('Estado')?>
+                    <span v-if="isRequired('En_Estado')" class="required">*<?php i::_e('obrigatório') ?></span>
+                </label>
+                <input :id="propId('En_Estado')" v-model="entity.En_Estado" type="text" @change="address()" autocomplete="off">
+            </div>
+
+            <div class="field col-6">
+                <label class="field__title">
+                    <?php i::_e('Município')?>
+                    <span v-if="isRequired('En_Municipio')" class="required">*<?php i::_e('obrigatório') ?></span>
+                </label>
+                <input v-model="entity.En_Municipio" :id="propId('En_Municipio')"  type="text" @change="address()" autocomplete="off">
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12" v-if="editable && hasPublicLocation">
+        <div class="col-6 sm:col-12 public-location">
+            <entity-field  @change="address()" type="checkbox" classes="public-location__field col-6" :entity="entity" prop="publicLocation" label="<?php i::esc_attr_e('Localização pública')?>">
+                <?php if($this->isEditable()): ?>
+                    <template #info>
+                        <?php $this->info('cadastro -> configuracoes-entidades -> localizacao-publica') ?>
+                    </template>
+                <?php endif; ?>
+            </entity-field>
+            <label class="public-location__label col-12"><?php i::_e('Marque o campo acima para tornar o endereço público ou deixe desmarcado para manter o endereço privado.')?></label>
+        </div>
+    </div>
+    <div v-if="verifiedAdress()" class="col-12">
+        <p class="entity-location__address">
+            <span v-if="entity.endereco">{{entity.endereco}}</span>
+            <span v-if="!entity.endereco"><?= i::_e("Sem Endereço"); ?></span>
+        </p>
+        <entity-map  :entity="entity" :editable="editable"></entity-map>
+    </div>
+    <?php $this->applyTemplateHook('entity-location','end'); ?>
+</div>
+<?php $this->applyTemplateHook('entity-location','after'); ?>
