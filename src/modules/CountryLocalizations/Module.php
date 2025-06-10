b4d7852bf015f451e0c6bbdee4e4106bb0a171bc
israelmelo <israelg.8@gmail.com>
2025-06-10 12:41:17 -0300

    Cria metadado de código postal e endereços de linha 1 e 2 Ref.: #3495

--- src/modules/CountryLocalizations/Module.php
+++ src/modules/CountryLocalizations/Module.php
@@ -25,16 +25,19 @@ class Module extends \MapasCulturais\Module {
             $app->registerController('country-localization', Controller::class);
         }
 
-        $levels = [
-            'address_level_1' => i::__('Campo de endereço de nível 1 (Região)'),
-            'address_level_2' => i::__('Campo de endereço de nível 2 (Estado/Província)'),
-            'address_level_3' => i::__('Campo de endereço de nível 3 (Departamento)'),
-            'address_level_4' => i::__('Campo de endereço de nível 4 (Cidade/Município/Comune)'),
-            'address_level_5' => i::__('Campo de endereço de nível 5 (Subprefeitura/Distrito)'),
-            'address_level_6' => i::__('Campo de endereço de nível 6 (Bairro)'),
+        $address_metadata = [
+            'address_postal_code' => i::__('Código postal'),
+            'address_level_1'     => i::__('Campo de endereço de nível 1 (Região)'),
+            'address_level_2'     => i::__('Campo de endereço de nível 2 (Estado/Província)'),
+            'address_level_3'     => i::__('Campo de endereço de nível 3 (Departamento)'),
+            'address_level_4'     => i::__('Campo de endereço de nível 4 (Cidade/Município/Comune)'),
+            'address_level_5'     => i::__('Campo de endereço de nível 5 (Subprefeitura/Distrito)'),
+            'address_level_6'     => i::__('Campo de endereço de nível 6 (Bairro)'),
+            'address_line_1'      => i::__('Endereço linha 1'),
+            'address_line_2'      => i::__('Endereço linha 2')
         ];
 
-        foreach ($levels as $slug => $label) {
+        foreach ($address_metadata as $slug => $label) {
             $this->registerAgentMetadata($slug, ['label' => $label]);
             $this->registerSpaceMetadata($slug, ['label' => $label]);
         }
