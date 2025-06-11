32e1e248fa4a2443c2f0d38256931ce796b9c899
israelmelo <israelg.8@gmail.com>
2025-06-11 14:36:56 -0300

    Ajusta o countryLocalizationDefinition Ref.: #3491

--- src/modules/CountryLocalizations/CountryLocalizationDefinition.php
+++ src/modules/CountryLocalizations/CountryLocalizationDefinition.php
@@ -8,16 +8,6 @@ use MapasCulturais\Traits;
  * @property-read string $countryCode
  * @property-read string $countryName
  * @property-read array[] $activeLevels
- * @property-read string $postalCode
- * @property-read string $level1
- * @property-read string $level2
- * @property-read string $level3
- * @property-read string $level4
- * @property-read string $level5
- * @property-read string $level6
- * @property-read string $line1
- * @property-read string $line2
- * @property-read string $fullAddress
  * @property-read array[] $levelHierarchy
  */
 abstract class CountryLocalizationDefinition
@@ -33,85 +23,91 @@ abstract class CountryLocalizationDefinition
      * 
      * @return string 
      */
-    abstract protected function getCountryCode(): ?string;
+    abstract public function getCountryCode(): ?string;
     
     /**
      * 
      * @return string 
      */
-    abstract protected function getCountryName(): ?string;
+    abstract public function getCountryName(): ?string;
 
     /**
      * 
      * @return array[]
      */
-    abstract protected function getActiveLevels(): ?array;
+    abstract public function getActiveLevels(): ?array;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getPostalCode(Entity $entity): ?string;
+    abstract public function getPostalCode(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string 
      */
-    abstract protected function getLevel1(Entity $entity): ?string;
+    abstract public function getLevel0(Entity $entity): ?string;
+
+    /**
+     * @param Entity $entity
+     * @return string 
+     */
+    abstract public function getLevel1(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLevel2(Entity $entity): ?string;
+    abstract public function getLevel2(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLevel3(Entity $entity): ?string;
+    abstract public function getLevel3(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLevel4(Entity $entity): ?string;
+    abstract public function getLevel4(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLevel5(Entity $entity): ?string;
+    abstract public function getLevel5(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLevel6(Entity $entity): ?string;
+    abstract public function getLevel6(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLine1(Entity $entity): ?string;
+    abstract public function getLine1(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getLine2(Entity $entity): ?string;
+    abstract public function getLine2(Entity $entity): ?string;
 
     /**
      * @param Entity $entity
      * @return string
      */
-    abstract protected function getFullAddress(Entity $entity): ?string;
+    abstract public function getFullAddress(Entity $entity): ?string;
 
     /**
      * 
      * @return array[]
      */
-    abstract protected function getLevelHierarchy(): ?array;
+    abstract public function getLevelHierarchy(): ?array;
 
     // =================== SETTERS ===================== //
 
@@ -120,62 +116,76 @@ abstract class CountryLocalizationDefinition
      * @param string $value
      * @return void
      */
-    abstract protected function setPostalCode(Entity $entity, string $value): void;
+    abstract public function setPostalCode(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel1(Entity $entity, string $value): void;
+    abstract public function setLevel0(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel2(Entity $entity, string $value): void;
+    abstract public function setLevel1(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel3(Entity $entity, string $value): void;
+    abstract public function setLevel2(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel4(Entity $entity, string $value): void;
+    abstract public function setLevel3(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel5(Entity $entity, string $value): void;
+    abstract public function setLevel4(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLevel6(Entity $entity, string $value): void;
+    abstract public function setLevel5(Entity $entity, string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLine1(Entity $entity, string $value): void;
+    abstract public function setLevel6(Entity $entity, ?string $value): void;
 
     /**
      * @param Entity $entity
      * @param string $value
      * @return void
      */
-    abstract protected function setLine2(Entity $entity, string $value): void;
+    abstract public function setLine1(Entity $entity, ?string $value): void;
+
+    /**
+     * @param Entity $entity
+     * @param string $value
+     * @return void
+     */
+    abstract public function setLine2(Entity $entity, ?string $value): void;
+
+     /**
+     * @param Entity $entity
+     * @param string $value
+     * @return void
+     */
+    abstract public function setFullAddress(Entity $entity, ?string $value): void;
 
 }
