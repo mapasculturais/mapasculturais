dcb07e1271c8b5425a040d24a84575191fcf32ae
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2025-06-12 12:05:26 -0300

    Torna título e subtitulo configuraveis e adiciona flags para ocultar partes do header do componente opportunity-subscription-list

--- src/modules/Opportunities/components/opportunity-subscription-list/template.php
+++ src/modules/Opportunities/components/opportunity-subscription-list/template.php
@@ -14,16 +14,16 @@ $this->import('
 
 <div v-if="global.auth.isLoggedIn && registrations.length > 0" class="grid-12 opportunity-subscription-list">	
 	<div class="opportunity-subscription-list__header col-12">
-		<p class="title">
-			<?= i::__("Você tem inscrições neste edital") ?>
+		<p v-if="!hideTitle" class="title">
+			<?= $this->text('subscription-list-title', i::__('Você tem inscrições neste edital'))?>
 		</p>	
-		<p class="description">
-			<?= i::__("Acompanhe suas inscrições e saiba o andamento da Oportunidade.") ?>
+		<p v-if="!hideSubtitle" class="description">
+			<?= $this->text('subscription-list-subtitle', i::__('Acompanhe suas inscrições e saiba o andamento da Oportunidade.'))?>
 		</p>
-		<mc-alert v-if="registrationStatus == 'closed'" type="warning">
+		<mc-alert v-if="registrationStatus == 'closed' && !hideInfos" type="warning">
 			<strong><?= i::__('O prazo de inscrição se encerrou.') ?></strong> <?= i::__('Não é mais possível enviar uma inscrição') ?> <strong><?= i::__('“Não enviada”.') ?></strong>
 		</mc-alert>
-		<mc-alert v-if="registrationStatus == 'open' && registrationsOpen" type="warning">
+		<mc-alert v-if="registrationStatus == 'open' && registrationsOpen && !hideInfos" type="warning">
 			<strong><?= i::__('Você possui inscrições não enviadas.') ?></strong> <?= i::__('Fique atento ao período das inscrições para enviá-las dentro do prazo. ') ?>
 		</mc-alert>
 	</div>
