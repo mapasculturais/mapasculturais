de9229f70ff18e9e427b419d0f9daa5d4b8fef6e
israelmelo <israelg.8@gmail.com>
2025-06-12 09:30:17 -0300

    Ajusta init do componente international-address-form para mandar o active_levels corretamente Ref.: #3495

--- src/modules/CountryLocalizations/components/international-address-form/init.php
+++ src/modules/CountryLocalizations/components/international-address-form/init.php
@@ -8,5 +8,15 @@
 $country_code = $app->config['address.defaultCountryCode'] != 'BR' ? $app->config['address.defaultCountryCode'] : null;
 $international_localization = $country_code ? $app->getRegisteredCountryLocalizationByCountryCode($country_code) : null;
 
-$app->view->jsObject['config']['internationalAddressForm']['activeLevels'] = $international_localization ? $international_localization->activeLevels : $app->config['address.defaultActiveLevels'];
+$default_active_levels = $app->config['address.defaultActiveLevels'];
+$default_level_labels = $app->config['address.defaultLevelsLabels'];
+
+$active_levels = [];
+foreach ($default_active_levels as $level) {
+    if (isset($default_level_labels[$level])) {
+        $active_levels[$level] = $default_level_labels[$level];
+    }
+}
+
+$app->view->jsObject['config']['internationalAddressForm']['activeLevels'] = $international_localization ? $international_localization->activeLevels : $active_levels;
 $app->view->jsObject['config']['internationalAddressForm']['levelHierarchy'] = $international_localization ? $international_localization->levelHierarchy : null;
