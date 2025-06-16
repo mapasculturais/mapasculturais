19e0f4ce77eec011d7d0ec14d7d37c59c981d38d
Welington De Lima Olegario <wolegario49@gmail.com>
2025-06-16 10:24:35 -0300

    Corrige a duplicação da exibição do 'RESULTADO DA FASE

--- src/modules/Opportunities/components/registration-status/template.php
+++ src/modules/Opportunities/components/registration-status/template.php
@@ -11,8 +11,7 @@ $this->import('
     registration-results
 ');
 ?>
-
-<div class="opportunity-phases-timeline__box">
+<div v-if="!phase.isLastPhase" class="opportunity-phases-timeline__box">
     <label class="semibold opportunity-phases-timeline__label"><?= i::__('RESULTADO DA FASE')?></label>
     <div class="opportunity-phases-timeline__status">
         <mc-icon name="circle" :class="verifyState(registration)"></mc-icon>
