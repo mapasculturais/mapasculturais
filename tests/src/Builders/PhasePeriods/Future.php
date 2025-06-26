d1736d8c899c2db0b4d20d491d9264546cb9ceeb
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-06-26 19:01:20 -0300

    corrige PhasePeriods/Future

--- tests/src/Builders/PhasePeriods/Future.php
+++ tests/src/Builders/PhasePeriods/Future.php
@@ -5,21 +5,20 @@ namespace Tests\Builders\PhasePeriods;
 use DateTime;
 use MapasCulturais\Entities\Opportunity;
 use Tests\Interfaces\DataCollectionPeriodInterface;
-use Tests\Interfaces\EvaluationPeriodInterface;
 
-class Past implements DataCollectionPeriodInterface
+class Future implements DataCollectionPeriodInterface
 {
     public function getRegistrationFrom(?Opportunity $reference_phase = null): DateTime
     {
         $reference_date = $reference_phase ? clone $reference_phase->registrationTo : new DateTime;
 
-        return new DateTime($reference_date->format('Y-m-d 8:00') . ' - 1 week');
+        return new DateTime($reference_date->format('Y-m-d 8:00') . ' + 1 day');
     }
 
     public function getRegistrationTo(?Opportunity $reference_phase = null): DateTime
     {
         $reference_date = $reference_phase ? clone $reference_phase->registrationFrom : new DateTime;
 
-        return new DateTime($reference_date->format('Y-m-d 8:00') . ' - 1 day');
+        return new DateTime($reference_date->format('Y-m-d 18:00') . ' + 1 week');
     }
 }
