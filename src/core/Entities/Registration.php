99077fa6509b219780a705dd8a56fcdc547520fc
israelmelo <israelg.8@gmail.com>
2025-08-13 09:57:33 -0300

    Adiciona verificação no getExtraEntitiesToRecreatePermissionCache verificando se não tem id

--- src/core/Entities/Registration.php
+++ src/core/Entities/Registration.php
@@ -1959,6 +1959,10 @@ class Registration extends \MapasCulturais\Entity
     }
     
     function getExtraEntitiesToRecreatePermissionCache(): array {
+        if(!$this->id) {
+            return [];
+        }
+
         $result = [];
 
         if ($previous_phase = $this->previousPhase) {
