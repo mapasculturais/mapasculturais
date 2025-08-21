a01732ae8d3a30001c4717b28f1a3c604ffb980d
Welington De Lima Olegario <wolegario49@gmail.com>
2025-08-21 18:12:37 -0300

    Adiciona configuração de text() para atualizar o texto de Localização Pública

--- src/modules/CountryLocalizations/components/international-address-form/template.php
+++ src/modules/CountryLocalizations/components/international-address-form/template.php
@@ -37,7 +37,7 @@ $this->import('
 
         <div class="col-12" v-if="hasPublicLocation">
             <div class="col-6 sm:col-12 field public-location">
-                <entity-field  @change="address()" type="checkbox" classes="public-location__field col-6" :entity="entity" prop="publicLocation" label="<?php i::esc_attr_e('Localização pública')?>">
+                <entity-field  @change="address()" type="checkbox" classes="public-location__field col-6" :entity="entity" prop="publicLocation" label="<?= $this->text('international-address-forms', i::__('Localização pública')) ?>">
                     <?php if($this->isEditable()): ?>
                         <template #info>
                             <?php $this->info('cadastro -> configuracoes-entidades -> localizacao-publica') ?>
