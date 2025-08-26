6b27fc17f04d3c8bc80650fe296e74d79547ec65
Welington De Lima Olegario <wolegario49@gmail.com>
2025-08-26 12:18:43 -0300

    Mostra 'Sim' quando entity.idoso é 1, caso contrário 'Não'

--- src/modules/Entities/components/agent-data-1/template.php
+++ src/modules/Entities/components/agent-data-1/template.php
@@ -45,7 +45,7 @@ $this->import('
             <div v-if="entity.dataDeNascimento" class="agent-data__fields--field">
                 <label class="title"><?php i::_e("Pessoa idosa") ?></label>
                 <div class="box">
-                    <label class="box__content">{{entity.idoso ? getTextI18n('yes') : getTextI18n('no')}}</label>
+                   <label class="box__content">{{ entity.idoso == 1 ? getTextI18n('yes') : getTextI18n('no') }}</label>
                 </div>
             </div>
             <entity-data v-if="entity.genero" class="agent-data__fields--field" :entity="entity" prop="genero" label="<?php i::_e("Gênero")?>"></entity-data>
@@ -54,7 +54,7 @@ $this->import('
             <entity-data v-if="entity.raca" class="agent-data__fields--field" :entity="entity" prop="agenteItinerante" label="<?php i::_e("Raça/Cor") ?>"></entity-data>
             <entity-data v-if="entity.escolaridade" class="agent-data__fields--field" :entity="entity" prop="escolaridade" label="<?php i::_e("Escolaridade") ?>"></entity-data>
             <entity-data v-if="entity.pessoaDeficiente" class="agent-data__fields--field" :entity="entity" prop="pessoaDeficiente" label="<?php i::_e("Pessoa portadora de deficiência") ?>"></entity-data>
-            <entity-data v-if="entity.pessoaDeficiente.length==1 && entity.pessoaDeficiente[0]==''" class="agent-data__fields--field" :entity="entity" prop="pessoaDeficiente" label="<?php i::_e("Não sou") ?>"></entity-data>
+            <entity-data v-if="entity.pessoaDeficiente && entity.pessoaDeficiente.length === 1 && entity.pessoaDeficiente[0] === ''" class="agent-data__fields--field" :entity="entity" prop="pessoaDeficiente" label="<?php i::_e('Não sou') ?>"> </entity-data>
             <entity-data v-if="entity.comunidadesTradicional" class="agent-data__fields--field" :entity="entity" prop="comunidadesTradicional" label="<?php i::_e("Comunidades Tradicionais") ?>"></entity-data>
             <entity-data v-if="entity.comunidadesTradicionalOutros" class="agent-data__fields--field" :entity="entity" prop="comunidadesTradicionalOutros" label="<?php i::_e("Outras Comunidades Tradicionais") ?>"></entity-data>
         </div>
