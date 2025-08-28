73389a05d339673256d4e0d39203b42b124c7364
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-08-28 01:36:17 -0300

    Novo teste de campos condicionados obrigatórios

---
+++ tests/src/OpportunityRegistrationsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Test;
+
+use MapasCulturais\App;
+use MapasCulturais\Connection;
+use MapasCulturais\Entities\Opportunity;
+use Tests\Abstract\TestCase;
+use Tests\Builders\PhasePeriods\After;
+use Tests\Builders\PhasePeriods\ConcurrentEndingAfter;
+use Tests\Builders\PhasePeriods\Open;
+use Tests\Builders\PhasePeriods\Past;
+use Tests\Traits\OpportunityBuilder;
+use Tests\Traits\RegistrationDirector;
+use Tests\Traits\UserDirector;
+
+class OpportunityRegistrationsTest extends TestCase
+{
+    use OpportunityBuilder,
+        RegistrationDirector,
+        UserDirector;
+
+
+    function testRequiredOneLevelConditionalField() {
+        $admin = $this->userDirector->createUser('admin');
+        $this->login($admin);
+
+        $builder = $this->opportunityBuilder;
+        
+        /** @var Opportunity */
+        $opportunity = $this->opportunityBuilder
+                                ->reset(owner: $admin->profile, owner_entity: $admin->profile)
+                                ->fillRequiredProperties()
+                                ->save()
+                                ->firstPhase()
+                                    ->setRegistrationPeriod(new Open)
+                                    ->createStep('etapa')
+                                    ->createField('cor', 'select', required:true, options:['Azul', 'Vermelho', 'Amarelo'])
+                                    ->createField('tom-de-azul', 'text', required:true, field_condition:'cor:Azul')
+                                    ->createField('tom-de-vermelho', 'text', required:true, field_condition:'cor:Vermelho')
+                                    ->done()
+                                ->save()
+                                ->refresh()
+                                ->getInstance();
+        
+        $field_cor = $this->opportunityBuilder->getFieldName('cor');
+        $field_tom_de_azul = $this->opportunityBuilder->getFieldName('tom-de-azul');
+        $field_tom_de_vermelho = $this->opportunityBuilder->getFieldName('tom-de-vermelho');
+
+        $registrations = $this->registrationDirector->createDraftRegistrations(
+            $opportunity,
+            number_of_registrations: 3
+        );
+
+        
+        list($azul, $vermelho, $amarelo) = $registrations;
+        
+        $amarelo->$field_cor = 'Amarelo';
+        $this->assertEmpty($amarelo->validationErrors, 'Certificando que um CAMPO NÃO PREENCHIDO, obrigatório e condicionado a outro campo NÃO causa erro de validação quando a condição para sua exibição NÃO foi atendida');
+        
+        $azul->$field_cor = 'Azul';
+        $this->assertArrayHasKey($field_tom_de_azul, $azul->validationErrors, 'Certificando que um CAMPO NÃO PREENCHIDO, obrigatório e condicionado a outro campo CAUSA erro de validação quando a condição para sua exibição FOI atendida');
+        $this->assertCount(1, $azul->validationErrors, 'Certificando que há o número certo de campos com erro de validação quando um CAMPO NÃO PREENCHIDO, obrigatório e condicionado a outro campo CAUSA erro de validação quando a condição para sua exibição FOI atendida');
+
+        $vermelho->$field_cor = 'Vermelho';
+        $vermelho->$field_tom_de_vermelho = 'Escuro';
+        $this->assertEmpty($amarelo->validationErrors, 'Certificando que um PREENCHIDO, obrigatório e condicionado a outro campo NÃO causa erro de validação quando a condição para sua exibição FOI atendida');
+
+    }
+}
