840f7094fbc91b8de89352d0abd2f1940c8d06b2
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2025-09-01 10:22:37 -0300

    feat: adicionando configurações para que o plugin-Metabase continue respeitando as variáveis de ambiente já definidas

--- config/plugins.php
+++ config/plugins.php
@@ -2,11 +2,28 @@
 
 return [
     'plugins' => [
-         'MultipleLocalAuth',
-         'AdminLoginAsUser',
-         'RecreatePCacheOnLogin',
-         'SpamDetector',
-         'Metabase',
-         'MapasBlame',
+        'MultipleLocalAuth',
+        'AdminLoginAsUser',
+        'RecreatePCacheOnLogin',
+        'SpamDetector',
+        'MapasBlame',
+        'Metabase' => [
+            'namespace' => 'Metabase',
+            'config' => [
+                'enabled' => function () {
+                    $app = \MapasCulturais\App::i();
+
+                    // Gestão de metabase por tema
+                    $themes = array_keys(array_filter([
+                        'MapaMinC\Theme' => env('METABASE_ENABLED', false),
+                        'Funarte\Theme' => env('METABASE_FUNARTE_ENABLED', false),
+                        'CulturaViva\Theme' => env('METABASE_CULTURAVIVA_ENABLED', false),
+                    ], fn($enabled) => $enabled));
+
+                    // Verifica se o tema atual está na lista de temas habilitados
+                    return in_array(get_class($app->view), $themes);
+                },
+            ]
+        ],
     ]
 ];
