f5dd0240b2c979b8e27518ebd105d604c3d48289
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-09-08 15:38:21 -0300

    altera o cache adapter dos testes para ser o NullAdapter

--- tests/config.d/cache.php
+++ tests/config.d/cache.php
@@ -1,9 +1,9 @@
 <?php 
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
-
+use Symfony\Component\Cache\Adapter\NullAdapter;
 return [
-    'app.cache' => new ArrayAdapter(),
-    'app.mscache' => new ArrayAdapter(),
+    'app.cache' => new NullAdapter(),
+    'app.mscache' => new NullAdapter(),
     'app.useMagicGetterCache'        => false,
     'app.useRegisteredAutoloadCache' => false,
     'app.useAssetsUrlCache'          => false,
