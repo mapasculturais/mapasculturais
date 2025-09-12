294c0b4beec70800cb53facc89a04e783ed484fa
erleibiazzio <erleibiazzio@gmail.com>
2025-09-12 18:59:23 -0300

    Impede que os pareceres sejam carregados no front-end quando não estiver permitido exibi-los.

--- src/modules/EvaluationMethodContinuous/components/evaluation-continuous-detail/init.php
+++ src/modules/EvaluationMethodContinuous/components/evaluation-continuous-detail/init.php
@@ -29,22 +29,25 @@ if($class == Registration::class) {
     foreach($registrations as $reg) {
         $em = $reg->evaluationMethod;
         $data = [
-            'consolidatedDetails' => $em->getConsolidatedDetails($reg),
+            'consolidatedDetails' => $em->shouldDisplayEvaluationResults($reg) ? $em->getConsolidatedDetails($reg) : [],
             'evaluationsDetails' => []
         ];
 
         $evaluations = $reg->sentEvaluations;
     
         foreach ($evaluations as $eval) {
-            $detail = $em->getEvaluationDetails($eval);
-            $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
+            $detail = $em->shouldDisplayEvaluationResults($reg) ? $em->getEvaluationDetails($eval) : [];
+            $emc = $reg->opportunity->evaluationMethodConfiguration;
+            if ($emc->publishValuerNames){
+                $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
+            }
             $data['evaluationsDetails'][] = $detail;
         }
 
+        $data['shouldDisplayEvaluationResults'] = $em->shouldDisplayEvaluationResults($reg);
         $result[$reg->id] = $data;
         
     }
-    
     
     $this->jsObject['config']['continuousEvaluationDetail'] = (object) $result;
 }
