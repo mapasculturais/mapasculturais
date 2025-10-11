414140ca9bef817f1e9c0a6c0b8618d450194b18
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-10-10 22:27:25 -0300

    wip. exportador/importador de oportunidades

--- src/modules/OpportunityExporter/Module.php
+++ src/modules/OpportunityExporter/Module.php
@@ -4,6 +4,7 @@ namespace OpportunityExporter;
 use MapasCulturais\Module as MapasCulturaisModule;
 use MapasCulturais\App;
 use MapasCulturais\Controllers\Opportunity as OpportunityController;
+use MapasCulturais\Entities;
 use MapasCulturais\Entities\Opportunity;
 use MapasCulturais\i;
 
@@ -64,8 +65,137 @@ class Module extends MapasCulturaisModule {
             
             $app->halt(200, $file_content);
         });
+
+        $app->hook('POST(opportunity.import)', function () use ($app) {
+            /** @var OpportunityController $this */
+            $this->requireAuthentication();
+
+            $data = $this->data['opportunity'] ?? null;
+
+            if(!$data) {
+                $this->errorJson(['opportunity' => [i::__('Os dados para importação não foram enviados')]], 400);
+            }
+
+            $filters = $this->data['filters'] ?? null;
+
+            if(!$filters) {
+                $this->errorJson(['opportunity' => [i::__('As definição do que deve ser importado não foi enviada')]], 400);
+            }
+            
+            $owner_entity = $this->data['opportunity']['ownerEntity'] ?? null;
+
+            if (!$owner_entity) {
+                $this->errorJson(['ownerEntity' => [i::__('A entidade vinculada é obrigatória')]], 400);
+            }
+
+            $opportunity_classes = [
+                'agent' => Entities\AgentOpportunity::class,
+                'space' => Entities\SpaceOpportunity::class,
+                'event' => Entities\EventOpportunity::class,
+                'project' => Entities\ProjectOpportunity::class,
+            ];
+
+            $opportunity_class = $opportunity_classes[$owner_entity['__objectType']] ?? null;
+
+            if (!$opportunity_class) {
+                $this->errorJson(['ownerEntity' => [i::__('Tipo inválido para a entidade vinculada')]], 400);
+            }
+
+            $owner_entity = $app->repo($owner_entity['__objectType'])->find($owner_entity['_id']);
+
+            if (!$owner_entity) {
+                $this->errorJson(['ownerEntity' => [i::__('Entidade vinculada não encontrada')]], 400);
+            }
+
+            $importer = new Importer(
+                onwerEntity: $owner_entity, 
+                data: $data,
+                files: $filters['files'] ?? false,
+                images: $filters['images'] ?? false,
+
+                dates: $filters['dates'] ?? false,
+
+                vacancyLimits: $filters['vacancyLimits'] ?? false,
+
+                statusLabels: $filters['statusLabels'] ?? false,
+                appealPhases: $filters['appealPhases'] ?? false,
+                monitoringPhases: $filters['monitoringPhases'] ?? false,
+            );
+
+            $app->conn->beginTransaction();
+            try {
+                $opportunity = $importer->import();                
+            } catch(\Throwable $e) {
+                $app->conn->rollBack();
+                throw $e;
+            }
+            
+            $app->conn->commit();
+            
+            $this->json($opportunity);
+        });
     }
 
     public function register() { }
 
+    public static function getInfoProperties(): array
+    {
+        $app = App::i();
+
+        $properties = [
+            'name',
+            'type',
+            'shortDescription',
+            'longDescription',
+            'terms',
+
+            'site',
+
+            'facebook',
+            'twitter',
+            'instagram',
+            'linkedin',
+            'vimeo',
+            'spotify',
+            'youtube',
+            'pinterest',
+            'tiktok',
+
+        ];
+
+        $app->applyHook('opportunityExport.infoProperties', [&$properties]);
+
+        return $properties;
+    }
+
+    public static function getWorkplanProperties(): array
+    {
+        $app = App::i();
+
+        $properties = [
+            'workplanLabelDefault',
+            'goalLabelDefault',
+            'deliveryLabelDefault',
+            'workplan_dataProjectlimitMaximumDurationOfProjects',
+            'workplan_dataProjectmaximumDurationInMonths',
+            'workplan_metaInformTheStageOfCulturalMaking',
+            'workplan_metaLimitNumberOfGoals',
+            'workplan_metaMaximumNumberOfGoals',
+            'workplan_deliveryReportTheDeliveriesLinkedToTheGoals',
+            'workplan_deliveryLimitNumberOfDeliveries',
+            'workplan_deliveryMaximumNumberOfDeliveries',
+            'workplan_registrationReportTheNumberOfParticipants',
+            'workplan_registrationInformCulturalArtisticSegment',
+            'workplan_registrationReportExpectedRenevue',
+            'workplan_monitoringInformTheFormOfAvailability',
+            'workplan_monitoringInformAccessibilityMeasures',
+            'workplan_monitoringInformThePriorityAudience',
+            'workplan_monitoringProvideTheProfileOfParticipants',
+            'workplan_monitoringReportExecutedRevenue',
+        ];
+
+        $app->applyHook('opportunityExport.workplanProperties', [&$properties]);
+
+        return $properties;
+    }
 }
