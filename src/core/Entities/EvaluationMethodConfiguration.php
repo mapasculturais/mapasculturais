71f0679e2c4721830b98c9279d1ee8609d10ddc5
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-10-17 17:06:01 -0300

    PHPDoc das classes RegistrationFileConfiguration e EvaluationMethodConfiguration

--- src/core/Entities/EvaluationMethodConfiguration.php
+++ src/core/Entities/EvaluationMethodConfiguration.php
@@ -15,6 +15,8 @@ use Opportunities\Jobs\UpdateSummaryCaches;
  * @property \MapasCulturais\Entities\Opportunity $opportunity Opportunity
  * @property \DateTime $evaluationFrom
  * @property \DateTime $evaluationTo
+ * @property string $name
+ * @property \MapasCulturais\Definitions\EntityType $type
  * 
  * @property-read \MapasCulturais\Definitions\EvaluationMethod $definition The evaluation method definition object
  * @property-read \MapasCulturais\EvaluationMethod $evaluationMethod The evaluation method module object
