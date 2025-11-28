0cd72e52f8fe5e6cff96b8368143ab62229d4035
Welington De Lima Olegario <wolegario49@gmail.com>
2025-11-28 17:20:15 -0300

    ajusta botão de Áreas de atuação segundo Figma Ref.: #3562

--- src/modules/Entities/components/entity-terms/template.php
+++ src/modules/Entities/components/entity-terms/template.php
@@ -19,42 +19,45 @@ $this->import('
         <span v-if="required && !hideRequired" class="entity-terms__required">*<?=i::__('obrigatório');?></span>
     </div>
  
-    <mc-popover v-if="allowInsert && editable" openside="down-right"  @open="loadTerms()" :title="popoverTitle">
-        <template #button="popover">
-            <button @click="popover.toggle()" class="button button--rounded button--sm button--icon button--primary" v-if="editable">
-                <?php i::_e("Adicionar nova") ?>
-                <mc-icon name="add"></mc-icon>
-            </button>
-        </template>
+    <div class="entity-terms__content">
+        <mc-tag-list v-if="selectedTerms.length > 0" :editable="editable" :classes="entity.__objectType+'__background'" :tags="selectedTerms"></mc-tag-list>
 
-        <!-- Modo Tags -->
-        <template #default="{toggle}">
-            <div class="entity-terms__tags">
-                <form class="entity-terms__tags--form" @submit.prevent="insertTag(toggle)">
-                    <input type="text"  class="input" placeholder="<?= i::__('Adicione uma nova tag') ?>" v-model="filter">
-                    <button class="button button--primary button--icon entity-terms__tags--form-addBtn" type="submit">
-                        <mc-icon name="add"></mc-icon>
-                    </button>
-                </form>
-                <ul class="entity-terms__tags--list" v-if="filteredTerms.length > 0">
-                    <li class="entity-terms__tags--list-item" @click="addTerm(term)" v-for="term in filteredTerms">
-                        <span v-html="highlightedTerm(term)"></span>
-                    </li>
-                </ul>
-            </div>
-        </template>
-    </mc-popover>
+        <mc-popover v-if="allowInsert && editable" openside="down-right"  @open="loadTerms()" :title="popoverTitle">
+            <template #button="popover">
+                <button @click="popover.toggle()" class="button button--rounded button--sm button--icon button--primary" v-if="editable">
+                    <?php i::_e("Adicionar nova") ?>
+                    <mc-icon name="add"></mc-icon>
+                </button>
+            </template>
 
-    <mc-multiselect v-if="!allowInsert && editable" :model="entity.terms[this.taxonomy]" :title="title" :items="terms" @open="loadTerms()">
-        <template #default="{ toggleMultiselect }">    
-            <button class="button button--rounded button--sm button--icon button--primary" @click="toggleMultiselect()" >
-                <?php i::_e("Adicionar nova") ?>
-                <mc-icon name="add"></mc-icon>
-            </button>
-        </template>
-    </mc-multiselect>
+            <!-- Modo Tags -->
+            <template #default="{toggle}">
+                <div class="entity-terms__tags">
+                    <form class="entity-terms__tags--form" @submit.prevent="insertTag(toggle)">
+                        <input type="text"  class="input" placeholder="<?= i::__('Adicione uma nova tag') ?>" v-model="filter">
+                        <button class="button button--primary button--icon entity-terms__tags--form-addBtn" type="submit">
+                            <mc-icon name="add"></mc-icon>
+                        </button>
+                    </form>
+                    <ul class="entity-terms__tags--list" v-if="filteredTerms.length > 0">
+                        <li class="entity-terms__tags--list-item" @click="addTerm(term)" v-for="term in filteredTerms">
+                            <span v-html="highlightedTerm(term)"></span>
+                        </li>
+                    </ul>
+                </div>
+            </template>
+        </mc-popover>
+
+        <mc-multiselect v-if="!allowInsert && editable" :model="entity.terms[this.taxonomy]" :title="title" :items="terms" @open="loadTerms()">
+            <template #default="{ toggleMultiselect }">    
+                <button class="button button--rounded button--sm button--icon button--primary" @click="toggleMultiselect()" >
+                    <?php i::_e("Adicionar nova") ?>
+                    <mc-icon name="add"></mc-icon>
+                </button>
+            </template>
+        </mc-multiselect>
+    </div>
     <small class="field__error" v-if="hasErrors">        
         {{errors.join('; ')}}
     </small>
-    <mc-tag-list v-if="selectedTerms.length > 0" :editable="editable" :classes="entity.__objectType+'__background'" :tags="selectedTerms"></mc-tag-list>
 </div>
