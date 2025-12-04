621192a9a8833ea6f86fe8879e0f6b7b3c9d9e79
erleibiazzio <erleibiazzio@gmail.com>
2025-12-04 14:05:02 -0300

    Implementa os métodos responsáveis pela criação de avaliações no ValuerBuilder nos testes automátizados

--- tests/src/Builders/ValuerBuilder.php
+++ tests/src/Builders/ValuerBuilder.php
@@ -3,15 +3,18 @@
 namespace Tests\Builders;
 
 use Exception;
-use MapasCulturais\Entities\Agent;
-use MapasCulturais\Entities\EvaluationMethodConfigurationAgentRelation;
 use Tests\Abstract\Builder;
-use Tests\Traits\AgentDirector;
 use Tests\Traits\UserDirector;
+use Tests\Traits\AgentDirector;
+use MapasCulturais\Entities\Agent;
+use Tests\Traits\EvaluationBuilder;
+use MapasCulturais\Entities\Registration;
+use MapasCulturais\Entities\EvaluationMethodConfigurationAgentRelation;
 
 class ValuerBuilder extends Builder
 {
     use AgentDirector,
+        EvaluationBuilder,
         UserDirector;
 
     protected EvaluationMethodConfigurationAgentRelation $instance;
@@ -70,6 +73,90 @@ class ValuerBuilder extends Builder
     public function maxRegistrations(?int $max_registrations): static
     {
         $this->instance->maxRegistrations = $max_registrations;
+
+        return $this;
+    }
+
+    public function createDraftEvaluation(?Registration $registration = null): static
+    {
+        if ($registration) {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                registration: $registration
+            );
+        } else {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                opportunity: $this->instance->owner->opportunity
+            );
+        }
+
+        $this->evaluationBuilder->save();
+
+        return $this;
+    }
+
+    public function createConcludedEvaluation(?Registration $registration = null): static
+    {
+        if ($registration) {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                registration: $registration
+            );
+        } else {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                opportunity: $this->instance->owner->opportunity
+            );
+        }
+
+        $this->evaluationBuilder->conclude();
+
+        return $this;
+    }
+
+    public function createSentEvaluation(?Registration $registration = null): static
+    {
+        if ($registration) {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                registration: $registration
+            );
+        } else {
+            $this->evaluationBuilder->reset(
+                user: $this->instance->agent->user,
+                opportunity: $this->instance->owner->opportunity
+            );
+        }
+
+        $this->evaluationBuilder->send();
+
+        return $this;
+    }
+
+    public function createDraftRegistrations(int $number_of_registrations): static
+    {
+        for ($i = 0; $i < $number_of_registrations; $i++) {
+            $this->createDraftEvaluation();
+        }
+
+        return $this;
+    }
+
+    public function createConcludedRegistrations(int $number_of_registrations): static
+    {
+        for ($i = 0; $i < $number_of_registrations; $i++) {
+            $this->createConcludedEvaluation();
+        }
+
+        return $this;
+    }
+
+    public function createSentRegistrations(int $number_of_registrations): static
+    {
+        for ($i = 0; $i < $number_of_registrations; $i++) {
+            $this->createSentEvaluation();
+        }
 
         return $this;
     }
