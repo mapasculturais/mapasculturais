6f91e17ebf1235d05fdb78b9df89a654abbca768
Welington De Lima Olegario <wolegario49@gmail.com>
2025-12-10 17:18:45 -0300

    Ajusta o template para melhorar a exibição dos dados da capa e das informações do agente, garantindo fidelidade ao design definido no Figma Ref.: #3562

--- src/modules/Entities/components/entity-header/template.php
+++ src/modules/Entities/components/entity-header/template.php
@@ -17,50 +17,8 @@ $this->import('
     <div class="entity-header__single--content">
         <div class="leftSide">
             <div class="avatar">
-
                 <mc-avatar :entity="entity" size="big"></mc-avatar>
             </div>
-            <!-- <mc-icon  v-if="!entity.files.avatar" :entity="entity"></mc-icon> -->
-            <nav v-if="hasSocialNetworks" class="share" aria-label="<?= i::__('Compartilhar') ?>">
-                <a v-if="entity.twitter"  class="button button--text button--icon" aria-label="Twitter" target="_blank" :href="buildSocialMediaLink('twitter')">
-                    <mc-icon name="twitter"></mc-icon>
-                </a>
-                <a v-if="entity.linkedin"  class="button button--text button--icon" aria-label="Linkedin" target="_blank" :href="buildSocialMediaLink('linkedin')">
-                    <mc-icon name="linkedin"></mc-icon>
-                </a>
-                <a v-if="entity.youtube"  class="button button--text button--icon" aria-label="Youtube" target="_blank" :href="buildSocialMediaLink('youtube')">
-                    <mc-icon name="youtube"></mc-icon>
-                </a>
-                <a v-if="entity.vimeo" class="button button--text button--icon" aria-label="Vimeo" target="_blank" :href="buildSocialMediaLink('vimeo')">
-                    <mc-icon name="vimeo"></mc-icon>
-                </a>
-                <a v-if="entity.spotify" class="button button--text button--icon" aria-label="Spotify" target="_blank" :href="buildSocialMediaLink('spotify')">
-                    <mc-icon name="spotify"></mc-icon>
-                </a>
-                <a v-if="entity.facebook" class="button button--text button--icon" aria-label="Facebook" target="_blank" :href="buildSocialMediaLink('facebook')">
-                    <mc-icon name="facebook"></mc-icon>
-                </a>
-                <a v-if="entity.instagram" class="button button--text button--icon" aria-label="Instagram" target="_blank" :href="buildSocialMediaLink('instagram')">
-                    <mc-icon name="instagram"></mc-icon>
-                </a>
-                <a v-if="entity.telegram" class="button button--text button--icon" aria-label="Telegram" target="_blank" :href="buildSocialMediaLink('telegram')">
-                    <mc-icon name="telegram"></mc-icon>
-                </a>
-                <a v-if="entity.pinterest" class="button button--text button--icon" aria-label="Pinterest" target="_blank" :href="buildSocialMediaLink('pinterest')">
-                    <mc-icon name="pinterest"></mc-icon>
-                </a>
-                <a v-if="entity.whatsapp" class="button button--text button--icon" aria-label="Whatsapp" target="_blank" :href="buildSocialMediaLink('whatsapp')">
-                    <mc-icon name="whatsapp"></mc-icon>
-                </a>
-                <a v-if="entity.tiktok" class="button button--text button--icon" aria-label="tiktok" target="_blank" :href="buildSocialMediaLink('tiktok')">
-                    <mc-icon name="tiktok"></mc-icon>
-                </a>
-                <a v-if="entity.fediverso" class="button button--text button--icon" aria-label="fediverso" target="_blank" :href="buildSocialMediaLink('fediverso')">
-                    <mc-icon name="fediverso"></mc-icon>
-                </a>
-            </nav>
-        </div>
-        <div class="rightSide">
             <div class="data">
                 <mc-title tag="h1" size="big" class="entity-header__title"> {{entity.name}} </mc-title>
                 <div class="metadata">
@@ -76,31 +34,13 @@ $this->import('
                     </slot>
                 </div>
             </div>
-            <nav v-if="hasSocialNetworks" class="share share-mobile" aria-label="<?= i::__('Compartilhar') ?>">
-                <a v-if="entity.twitter" :href="entity.twitter" class="button button--text button--icon" aria-label="Twitter" target="_blank">
-                    <mc-icon name="twitter"></mc-icon>
-                </a>
-                <a v-if="entity.facebook" :href="entity.facebook" class="button button--text button--icon" aria-label="Facebook" target="_blank">
-                    <mc-icon name="facebook"></mc-icon>
-                </a>
-                <a v-if="entity.instagram" :href="entity.instagram" class="button button--text button--icon" aria-label="Instagram" target="_blank">
-                    <mc-icon name="instagram"></mc-icon>
-                </a>
-                <a v-if="entity.telegram" :href="entity.telegram" class="button button--text button--icon" aria-label="Telegram" target="_blank">
-                    <mc-icon name="telegram"></mc-icon>
-                </a>
-                <a v-if="entity.pinterest" :href="entity.pinterest" class="button button--text button--icon" aria-label="Pinterest" target="_blank">
-                    <mc-icon name="pinterest"></mc-icon>
-                </a>
-                <a v-if="entity.whatsapp" :href="entity.whatsapp" class="button button--text button--icon" aria-label="WhatsApp" target="_blank">
-                    <mc-icon name="whatsapp"></mc-icon>
-                </a>
-            </nav>
             <div :class="['description', {'description--event':entity.__objectType=='event'}]">
                 <slot name="description">
                     <p class="description" v-html="entity.shortDescription" ></p>
                 </slot>
             </div>
+        </div>
+        <div class="rightSide">
             <div v-if="entity.site" class="site">
                 <a :href="entity.site" target="_blank"><mc-icon :class="entity.__objectType+'__color'" name="link"></mc-icon>{{entity.site}}</a>
             </div>
