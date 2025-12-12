5680c7022467e18d57e023213a2aca7add772870
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2025-12-11 22:23:39 -0300

    novos testes de rotas de avaliação, garantindo que o acesso à página de avaliação não cria a avaliação e que a avaliação é criada após o salvamento pela rota saveEvaluation (Ref.: #3567)

--- tests/src/EvaluationRoutesTest.php
+++ tests/src/EvaluationRoutesTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use MapasCulturais\App;
+use MapasCulturais\Entities\Registration;
 use Tests\Traits\Faker;
 use Tests\Traits\UserDirector;
 use Tests\Traits\RequestFactory;
@@ -20,7 +21,7 @@ class EvaluationRoutesTest extends Abstract\TestCase
         OpportunityBuilder,
         RegistrationDirector;
 
-    function testNewEvaluationRoute()
+    function testFirstAccessToEvaluationRoute()
     {
         $admin = $this->userDirector->createUser('admin');
         $this->login($admin);
@@ -57,13 +58,17 @@ class EvaluationRoutesTest extends Abstract\TestCase
         foreach($committee as $valuer) {
             if($valuer->user->id == $valuer_user_id) {
                 $registration_valuer = $valuer->user->refreshed();
-            } else {
-                $another_valuer = $valuer->user->refreshed();
-            }
+            } 
         }
 
-
         $this->login($registration_valuer);
+
+        $evaluation_id = $this->app->conn->fetchScalar("SELECT evaluation_id FROM evaluations WHERE valuer_user_id = :uid AND registration_id = :rid", [
+            'uid' => $registration_valuer->id,
+            'rid' => $registration->id
+        ]);
+
+        $this->assertNull($evaluation_id, 'Garantindo que ANTES de acessar a rota da avaliação o id da avaliação é NULL');
 
         $request = $this->requestFactory->GET('registration', 'evaluation', [$registration->id]);
         $this->assertStatus200($request, 'Garantindo status 200 no primeiro acesso à rota "registration.evaluation" para um avaliador da inscrição');
@@ -71,11 +76,125 @@ class EvaluationRoutesTest extends Abstract\TestCase
         $request = $this->requestFactory->GET('registration', 'evaluation', [$registration->id]);
         $this->assertStatus200($request, 'Garantindo status 200 no segundo acesso à rota "registration.evaluation" para um avaliador da inscrição');
 
+        $evaluation_id = $this->app->conn->fetchScalar("SELECT evaluation_id FROM evaluations WHERE valuer_user_id = :uid AND registration_id = :rid", [
+            'uid' => $registration_valuer->id,
+            'rid' => $registration->id
+        ]);
+
+        $this->assertNull($evaluation_id, 'Garantindo que DEPOIS de acessar a rota da avaliação o id da avaliação é NULL');
+    }
+
+    function testEvaluationCreationOnSaveRoute()
+    {
+        $admin = $this->userDirector->createUser('admin');
+        $this->login($admin);
+
+        $opportunity = $this->opportunityBuilder
+            ->reset(owner: $admin->profile, owner_entity: $admin->profile)
+            ->fillRequiredProperties()
+            ->firstPhase()
+                ->setRegistrationPeriod(new Open)
+                ->done()
+            ->save()
+            ->addEvaluationPhase(EvaluationMethods::simple)
+                ->setEvaluationPeriod(new ConcurrentEndingAfter)
+                ->setCommitteeValuersPerRegistration('committee 1', 1)
+                ->save()
+                ->addValuers(2, 'committee 1')
+                ->done()
+            ->getInstance();
+            
+        $registration = $this->registrationDirector->createSentRegistration(
+            $opportunity,
+            data: []
+        );
+
+        $committee = $opportunity->evaluationMethodConfiguration->relatedAgents['committee 1'];
+
+        $opportunity->evaluationMethodConfiguration->redistributeCommitteeRegistrations();
+
+        $registration = $registration->refreshed();
+
+        $valuer_user_id = array_keys($registration->valuers)[0];
+
+        foreach ($committee as $valuer) {
+            if ($valuer->user->id == $valuer_user_id) {
+                $registration_valuer = $valuer->user->refreshed();
+            }
+        }
+
+        $this->login($registration_valuer);
+
+        $evaluation_id = $this->app->conn->fetchScalar("SELECT evaluation_id FROM evaluations WHERE valuer_user_id = :uid AND registration_id = :rid", [
+            'uid' => $registration_valuer->id,
+            'rid' => $registration->id
+        ]);
+
+        $request = $this->requestFactory->POST(
+            controller_id: 'registration',
+            action: 'saveEvaluation',
+            url_params: [$registration->id],
+            payload: [
+                'data' => [
+                    'status' => Registration::STATUS_WAITLIST,
+                    'obs' => $this->faker->text()
+                ]
+            ]
+        );
+
+        $this->assertStatus200($request, 'Garantindo status 200 na rota "registration.saveEvaluation" para criação e salvamento dos dados da avaliação');
+
+        $evaluation_id = $this->app->conn->fetchScalar("SELECT evaluation_id FROM evaluations WHERE valuer_user_id = :uid AND registration_id = :rid", [
+            'uid' => $registration_valuer->id,
+            'rid' => $registration->id
+        ]);
+
+        $this->assertNotNull($evaluation_id, 'Garantindo que DEPOIS de salvar a avaliação pela rota /registration/saveEvaluation a avaliação foi criada e o id da avaliação NÃO é mais NULL');
+    }
+
+    function testValuerAccessToAnotherValuerRegistration()
+    {
+        $admin = $this->userDirector->createUser('admin');
+        $this->login($admin);
+
+        $opportunity = $this->opportunityBuilder
+            ->reset(owner: $admin->profile, owner_entity: $admin->profile)
+            ->fillRequiredProperties()
+            ->firstPhase()
+                ->setRegistrationPeriod(new Open)
+                ->done()
+            ->save()
+            ->addEvaluationPhase(EvaluationMethods::simple)
+                ->setEvaluationPeriod(new ConcurrentEndingAfter)
+                ->setCommitteeValuersPerRegistration('committee 1', 1)
+                ->save()
+                ->addValuers(2, 'committee 1')
+                ->done()
+            ->getInstance();
+            
+        $registration = $this->registrationDirector->createSentRegistration(
+            $opportunity,
+            data: []
+        );
+
+        $committee = $opportunity->evaluationMethodConfiguration->relatedAgents['committee 1'];
+
+        $opportunity->evaluationMethodConfiguration->redistributeCommitteeRegistrations();
+        
+        $registration = $registration->refreshed();
+
+        $valuer_user_id = array_keys($registration->valuers)[0];
+        
+        foreach($committee as $valuer) {
+            if($valuer->user->id != $valuer_user_id) {
+                $another_valuer = $valuer->user->refreshed();
+            }
+        }
+
         $this->login($another_valuer);
 
         $request = $this->requestFactory->GET('registration', 'evaluation', [$registration->id]);
         $this->assertStatus403($request, 'Garantindo status 403 no primeiro acesso à rota "registration.evaluation" para um avaliador que não é avaliador da inscrição');
-
     }
 
     public function testReplaceValuerRoute()
