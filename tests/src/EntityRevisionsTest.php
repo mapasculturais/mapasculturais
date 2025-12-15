1d5e894785e45d2c916ad7baa2cc4f7d05dfc7ec
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2025-12-15 09:34:49 -0300

    WIP testes das revisões

---
+++ tests/src/EntityRevisionsTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Tests;
+
+use DateTime;
+use Exception;
+use MapasCulturais\Entities\Agent;
+use MapasCulturais\Entities\EntityRevision;
+use MapasCulturais\Entities\EvaluationMethodConfiguration;
+use MapasCulturais\Entities\Event;
+use MapasCulturais\Entities\Opportunity;
+use MapasCulturais\Entities\Project;
+use MapasCulturais\Entities\Seal;
+use MapasCulturais\Entities\Space;
+use MapasCulturais\Entities\User;
+use MapasCulturais\Entity;
+use Tests\Abstract\TestCase;
+use Tests\Enums\EvaluationMethods;
+use Tests\Traits\AgentDirector;
+use Tests\Traits\OpportunityBuilder;
+use Tests\Traits\ProjectDirector;
+use Tests\Traits\EventDirector;
+use Tests\Traits\Faker;
+use Tests\Traits\SpaceDirector;
+use Tests\Traits\UserDirector;
+use UserManagement\Entities\SystemRole;
+
+class EntityRevisionsTest extends TestCase
+{
+    use UserDirector,
+        AgentDirector,
+        SpaceDirector,
+        ProjectDirector,
+        EventDirector,
+        OpportunityBuilder,
+        Faker;
+
+    protected $entityClasses = [
+        Agent::class,
+        Space::class,
+        Project::class,
+        Event::class,
+        Opportunity::class,
+        // EvaluationMethodConfiguration::class,
+        // User::class,
+        // Seal::class,
+        // SystemRole::class,
+    ];
+
+    function createEntity(string $class, ?User $user = null): Entity
+    {
+
+        if (!$user) {
+            if ($this->app->auth->isUserAuthenticated()) {
+                $user = $this->app->user;
+            } else {
+                $user = $this->userDirector->createUser();
+            }
+        }
+
+        switch ($class) {
+            case Agent::class:
+                $entity = $this->agentDirector->createAgent($user);
+                break;
+            case Space::class:
+                $entity = $this->spaceDirector->createSpace($user->profile);
+                break;
+            case Project::class:
+                $entity = $this->projectDirector->createProject($user->profile);
+                break;
+            case Event::class:
+                $entity = $this->eventDirector->createEvent($user->profile);
+                break;
+            case Opportunity::class:
+                $entity = $this->opportunityBuilder
+                    ->reset($user->profile, $user->profile)
+                    ->fillRequiredProperties()
+                    ->save()
+                    ->getInstance();
+                break;
+            case EvaluationMethodConfiguration::class:
+                $entity = $this->opportunityBuilder
+                    ->reset($user->profile, $user->profile)
+                    ->fillRequiredProperties()
+                    ->save()
+                    ->addEvaluationPhase(EvaluationMethods::simple)
+                        ->save()
+                        ->getInstance();
+                break;
+            case User::class:
+                $entity = $this->userDirector->createUser();
+                break;
+            // case Seal::class:
+            //     break;
+            // case SystemRole::class:
+            //     break;
+
+            default:
+                throw new Exception('classe não implementada');
+        }
+
+
+        return $entity;
+    }
+
+    function changeEntity(Entity $entity): object
+    {
+        $result = (object)[
+            'key' => null,
+            'value' => null
+        ];
+
+        switch ($entity->className) {
+            case User::class:
+                $result->key = 'email';
+                $result->value = $this->faker->email;
+                break;
+                
+            default:
+                $result->key = 'name';
+                $result->value = $this->faker->name;
+                break;
+            }
+            
+        $entity->{$result->key} = $result->value;
+
+        $entity->save(true);
+
+        return $result;
+    }
+
+    function testCreatedRevision()
+    {
+        $user = $this->userDirector->createUser();
+
+        $this->login($user);
+
+        foreach ($this->entityClasses as $class) {
+            $entity = $this->createEntity($class, $user);
+
+            /** @var EntityRevision */
+            $last_revision = $entity->getLastRevision();
+
+            $this->assertEquals(EntityRevision::ACTION_CREATED, $last_revision->action ?? null, "Garantindo que a revisão da criação da entidade {$class} foi criada");
+
+        }
+    }
+
+    function testModifiedRevision()
+    {
+        $user = $this->userDirector->createUser();
+
+        $this->login($user);
+
+        foreach ($this->entityClasses as $class) {
+            $entity = $this->createEntity($class, $user);
+
+            $modification = $this->changeEntity($entity);
+            
+            /** @var EntityRevision */
+            $last_revision = $entity->getLastRevision();
+
+            $this->assertEquals(EntityRevision::ACTION_MODIFIED, $last_revision->action ?? null, "Garantindo que a revisão da criação da entidade {$class} foi criada");
+
+            $this->assertEquals($modification->value, $last_revision->revisionData[$modification->key]->value, "Garantindo que o valor modificado foi salvo na última revisão");
+
+        }
+    }
+}
