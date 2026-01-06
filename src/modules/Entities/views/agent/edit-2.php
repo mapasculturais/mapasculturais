e5363751a773c30b5a4eec047cc22760e2135ea3
Welington De Lima Olegario <wolegario49@gmail.com>
2026-01-06 08:28:58 -0300

    Criação do template com separação das abas Ref.: #3562

--- src/modules/Entities/views/agent/edit-2.php
+++ src/modules/Entities/views/agent/edit-2.php
@@ -45,104 +45,123 @@ $this->breadcrumb = [
     <entity-header :entity="entity" editable></entity-header>
 
     <mc-tabs class="tabs" sync-hash>
-        <?php $this->applyTemplateHook('tabs','begin') ?>
-        <mc-tab label="<?= i::_e('Informações') ?>" slug="info">
-            <mc-container>
+        <?php $this->applyTemplateHook('tabs', 'begin') ?>
+
+        <mc-tab label="<?= i::_e('Perfil') ?>" slug="info">
+            <mc-container class="single-1">
                 <entity-status :entity="entity"></entity-status>
                 <mc-card class="feature">
+                    <template #content>
+                        <mc-tabs class="tabs" sync-hash>
+                            <mc-tab label="<?= i::esc_attr_e('Público') ?>" slug="publico">
+                                <mc-card>
+                                    <div class="grid-12 v-bottom">
+                                        <entity-cover :entity="entity" classes="col-12"></entity-cover>
+
+                                        <div class="col-12 grid-12">
+                                            <?php $this->applyTemplateHook('entity-info', 'begin') ?>
+                                            <div class="col-3 sm:col-12">
+                                                <entity-profile :entity="entity"></entity-profile>
+                                            </div>
+                                            <div class="col-9 sm:col-12 grid-12 v-bottom">
+                                                <entity-field :entity="entity" classes="col-12" prop="name" label="<?php i::_e('Nome do Agente') ?>"></entity-field>
+                                            </div>
+                                            <?php $this->applyTemplateHook('entity-info', 'end') ?>
+                                        </div>
+                                        <?php $this->applyTemplateHook('edit2-entity-info-taxonomie-area', 'before') ?>
+                                        <entity-terms :entity="entity" taxonomy="area" editable classes="col-12" title="<?php i::_e('Área de atuação'); ?>"></entity-terms>
+                                        <?php $this->applyTemplateHook('edit2-entity-info-taxonomie-area', 'after') ?>
+
+                                        <entity-terms :entity="entity" taxonomy="tag" classes="col-12" title="Tags" editable></entity-terms>
+
+                                        <entity-field :entity="entity" classes="col-12" prop="shortDescription" :max-length="400"></entity-field>
+                                        <entity-field :entity="entity" classes="col-6" prop="site" label="<?php i::_e('Link (URL)') ?>"></entity-field>
+                                        <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone1" label="<?= i::__('Telefone privado 1 com DDD') ?>"></entity-field>
+                                    </div>
+
+                                </mc-card>
+                                 <div class="card-separator"></div>
+                                <mc-card class="social-media-card">
+                                    <template #content>
+                                        <entity-social-media :entity="entity" editable classes="col-12"></entity-social-media>
+                                    </template>
+                                </mc-card>
+                            </mc-tab>
+
+                            <mc-tab label="<?= i::esc_attr_e('Dados organizacionais') ?>" slug="org">
+                                <mc-card>
+                                    <template #title>
+                                        <label><?php i::_e("Dados do Agente Coletivo"); ?></label>
+                                        <p class="data-subtitle"><?php i::_e("Os dados inseridos abaixo serão registrados apenas no sistemas e não serão exibidos publicamente") ?></p>
+                                    </template>
+                                    <template #content>
+                                        <div class="grid-12">
+                                            <entity-field :entity="entity" classes="col-9 sm:col-12" prop="nomeSocial" label="<?php i::_e('Nome Fantasia') ?>"></entity-field>
+                                            <entity-field :entity="entity" classes="col-9 sm:col-12" prop="nomeCompleto" label="<?php i::_e('Razão Social') ?>"></entity-field>
+                                            <entity-field v-if="global.auth.is('admin')" :entity="entity" prop="type" @change="entity.save(true).then(() => global.reload())" classes="col-12"></entity-field>
+                                            <entity-field :entity="entity" classes="col-12" prop="cnpj" label="CNPJ"></entity-field>
+                                            <entity-field :disabled="!(entity?.cnpj?.length == 18)" :entity="entity" classes="col-12" prop="cnpjAnexo" title-modal="<?php i::_e('Anexar CNPJ - Formatos: (png, jpeg, pdf)') ?>" group-name="docs-cnpj" :hide-label="true"></entity-field>
+                                            <entity-field :entity="entity" classes="col-12" prop="dataDeNascimento" label="<?= i::__('Data de fundação') ?>"></entity-field>
+                                            <entity-field :entity="entity" classes="col-12" prop="emailPrivado" label="<?= i::__('E-mail privado ') ?>"></entity-field>
+                                            <entity-field :entity="entity" classes="col-12" prop="telefonePublico" label="<?= i::__('Telefone público com DDD') ?>"></entity-field>
+                                            <entity-field :entity="entity" classes="col-12" prop="emailPublico" label="<?= i::__('E-mail público') ?>"></entity-field>
+                                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone2" label="<?= i::__('Telefone privado 2 com DDD') ?>"></entity-field>
+                                            <div class="col-12 divider"></div>
+
+                                        </div>
+                                    </template>
+                                </mc-card>
+                            </mc-tab>
+
+                            <mc-tab label="<?= i::esc_attr_e('Endereço') ?>" slug="endereco">
+                                <mc-card>
+                                    <country-address-form :entity="entity" class="col-12"></country-address-form>
+                                </mc-card>
+                            </mc-tab>
+
+
+                        </mc-tabs>
+                        <div class="divider"></div>
+                    </template>
+                </mc-card>
+
+            </mc-container>
+
+            <!--  <main>
+                INFORMAÇÕES PUBLICAS
+                <mc-card>
                     <template #title>
-                        <label><?php i::_e("Informações de Apresentação") ?></label>
-                        <p><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários") ?></p>
+                        <label><?php i::_e("informações públicas"); ?></label>
+                        <p><?php i::_e("Os dados inseridos abaixo assim como as informações de apresentação também são exibidos publicamente"); ?></p>
                     </template>
                     <template #content>
-                        <div class="left">
-                            <div class="grid-12 v-bottom">
-                                <entity-cover :entity="entity" classes="col-12"></entity-cover>
-                                
-                                <div class="col-12 grid-12">
-                                    <?php $this->applyTemplateHook('entity-info','begin') ?>
-                                    <div class="col-3 sm:col-12">
-                                        <entity-profile :entity="entity"></entity-profile>
-                                    </div>
-                                    <div class="col-9 sm:col-12 grid-12 v-bottom">
-                                        <entity-field :entity="entity" classes="col-12" prop="name" label="<?php i::_e('Nome do Agente') ?>"></entity-field>
-                                    </div>
-                                    <?php $this->applyTemplateHook('entity-info','end') ?>
-                                </div>
-
-                                <entity-field :entity="entity" classes="col-12" prop="shortDescription" :max-length="400"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="site"></entity-field>
-                            </div>
-                        </div>
-                        <div class="divider"></div>
-                        <div class="right">
-                            <div class="grid-12">
-                                <?php $this->applyTemplateHook('edit2-entity-info-taxonomie-area','before') ?>
-                                <entity-terms :entity="entity" taxonomy="area" editable classes="col-12" title="<?php i::_e('Área de atuação'); ?>"></entity-terms>
-                                <?php $this->applyTemplateHook('edit2-entity-info-taxonomie-area','after') ?>
-
-                                <entity-social-media :entity="entity" classes="col-12" editable></entity-social-media>
-                            </div>
+                        <div class="grid-12">
+                            <entity-field :entity="entity" classes="col-12" prop="longDescription" editable></entity-field>
+                            <entity-files-list :entity="entity" classes="col-12" group="downloads" title="Adicionar arquivos para download" editable></entity-files-list>
+                            <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>
+                            <entity-gallery-video title="<?php i::_e('Adicionar vídeos') ?>" :entity="entity" classes="col-12" editable></entity-gallery-video>
+                            <entity-gallery title="<?php i::_e('Adicionar fotos na galeria') ?>" :entity="entity" classes="col-12" editable></entity-gallery>
                         </div>
                     </template>
                 </mc-card>
-                <main>
-                    <mc-card>
-                        <template #title>
-                            <label><?php i::_e("Dados do Agente Coletivo"); ?></label>
-                            <p class="data-subtitle"><?php i::_e("Os dados inseridos abaixo serão registrados apenas no sistemas e não serão exibidos publicamente") ?></p>
-                        </template>
-                        <template #content>
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-9 sm:col-12" prop="nomeSocial" label="<?php i::_e('Nome Fantasia') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-9 sm:col-12" prop="nomeCompleto" label="<?php i::_e('Razão Social') ?>"></entity-field>
-                                <entity-field v-if="global.auth.is('admin')" :entity="entity" prop="type" @change="entity.save(true).then(() => global.reload())" classes="col-12"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="cnpj" label="CNPJ"></entity-field>
-                                <entity-field :disabled="!(entity?.cnpj?.length == 18)" :entity="entity" classes="col-12" prop="cnpjAnexo" title-modal="<?php i::_e('Anexar CNPJ - Formatos: (png, jpeg, pdf)') ?>" group-name="docs-cnpj" :hide-label="true"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="dataDeNascimento" label="<?= i::__('Data de fundação') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="emailPrivado" label="<?= i::__('E-mail privado ') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="telefonePublico" label="<?= i::__('Telefone público com DDD') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="emailPublico" label="<?= i::__('E-mail público') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone1" label="<?= i::__('Telefone privado 1 com DDD') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone2" label="<?= i::__('Telefone privado 2 com DDD') ?>"></entity-field>
-                                <div class="col-12 divider"></div>
-                                <country-address-form :entity="entity" class="col-12"></country-address-form>
-                            </div>
-                        </template>
-                    </mc-card>
-                    <mc-card>
-                        <template #title>
-                            <label><?php i::_e("informações públicas"); ?></label>
-                            <p><?php i::_e("Os dados inseridos abaixo assim como as informações de apresentação também são exibidos publicamente"); ?></p>
-                        </template>
-                        <template #content>
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-12" prop="longDescription" editable></entity-field>
-                                <entity-files-list :entity="entity" classes="col-12" group="downloads" title="Adicionar arquivos para download" editable></entity-files-list>
-                                <entity-links :entity="entity" classes="col-12" title="<?php i::_e('Adicionar links'); ?>" editable></entity-links>
-                                <entity-gallery-video title="<?php i::_e('Adicionar vídeos') ?>" :entity="entity" classes="col-12" editable></entity-gallery-video>
-                                <entity-gallery title="<?php i::_e('Adicionar fotos na galeria') ?>" :entity="entity" classes="col-12" editable></entity-gallery>
-                            </div>
-                        </template>
-                    </mc-card>
-                </main>
-                <aside>
-                    <mc-card>
-                        <template #content>
-                            <div class="grid-12">
-                                <entity-admins :entity="entity" classes="col-12" editable></entity-admins>
-                                <entity-terms :entity="entity" taxonomy="tag" classes="col-12" title="Tags" editable></entity-terms>
-                                <entity-related-agents :entity="entity" classes="col-12" editable></entity-related-agents>
-                                <entity-owner :entity="entity" title="Publicado por" classes="col-12" editable></entity-owner>
-                            </div>
-                        </template>
-                    </mc-card>
-                </aside>
+            </main> -->
+            <aside>
+                <mc-card>
+                    <template #content>
+                        <div class="grid-12">
+                            <!--  <entity-admins :entity="entity" classes="col-12" editable></entity-admins> -->
+
+                            <!--   <entity-related-agents :entity="entity" classes="col-12" editable></entity-related-agents> -->
+                            <!--  <entity-owner :entity="entity" title="Publicado por" classes="col-12" editable></entity-owner>
+                        </div> -->
+                    </template>
+                </mc-card>
+            </aside>
             </mc-container>
         </mc-tab>
-        <?php $this->applyTemplateHook('tabs','end') ?>
+        <?php $this->applyTemplateHook('tabs', 'end') ?>
     </mc-tabs>
-    
+
     <entity-actions :entity="entity" editable></entity-actions>
 </div>
 <confirm-before-exit :entity="entity"></confirm-before-exit>
