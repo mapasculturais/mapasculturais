53319bbbf8e93171b8b6119cd3564e074c9d207e
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2026-01-20 11:36:37 -0300

    classe MapasCulturais\DateTime

---
+++ src/core/DateTime.php
@@ -0,0 +1,23 @@
+<?php
+namespace MapasCulturais;
+
+use DateTimeZone;
+
+class DateTime extends \DateTime
+{
+    static public string $datetime = 'now';
+
+    public function __construct(string $datetime = 'now', DateTimeZone|null $timezone = null)
+    {
+        if ($datetime == 'now') {
+            $datetime = self::$datetime;
+        }
+
+        return parent::__construct($datetime, $timezone);
+    }
+
+    static function date(string $format, ?int $timestamp) {
+        $timestamp = $timestamp ?: strtotime(self::$datetime);
+        return date($format, $timestamp);
+    }
+}
