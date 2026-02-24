9a259dee660ad1a6967373c0f4ca85b7fc1b2998
israelmelo <israelg.8@gmail.com>
2026-02-24 10:23:10 -0300

    Corrige aplicação de resultado das avaliações por classificação

--- src/modules/EvaluationMethodTechnical/Module.php
+++ src/modules/EvaluationMethodTechnical/Module.php
@@ -623,8 +623,8 @@ class Module extends \MapasCulturais\EvaluationMethod {
                 $wait_list = $this->data['waitList'];
                 $invalidate_registrations = $this->data['invalidateRegistrations'];
 
-                $cutoff_score = $this->data['cutoffScore'];
-                $quantity_vacancies = $this->data['quantityVacancies'];
+                $cutoff_score = (float) $this->data['cutoffScore'];
+                $quantity_vacancies = (int) $this->data['quantityVacancies'];
                 $consider_quotas = $this->data['considerQuotas'];
 
                 $query_params['status'] = $statusIn;
@@ -632,6 +632,7 @@ class Module extends \MapasCulturais\EvaluationMethod {
                 // considerar cotas
                 if($consider_quotas) {
                     $query_params['@order'] = '@quota';
+                    $query_params['__enableQuota'] = true;
                 }
 
                 $query = new ApiQuery(Registration::class, $query_params);
@@ -685,7 +686,7 @@ class Module extends \MapasCulturais\EvaluationMethod {
                             $registration->skipSync = true;
                             $registration->__skipQueuingPCacheRecreation = true;
 
-                            $app->log->debug("{$count}/{$total} Alterando status da inscrição {$registration->number} para INVÁLIDO");
+                            $app->log->debug("{$count}/{$total} Alterando status da inscrição {$registration->number} para NÃO SELECIONADO");
                             $registration->setStatusToNotApproved();
                             $app->em->clear();
                         }
