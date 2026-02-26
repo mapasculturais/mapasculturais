9f4451604cf1084a2395496fb37246907b26a939
Lucas Pirola <lucaspirola@gmail.com>
2026-02-26 18:33:46 -0300

    fix(workplan): adota mc-tag-list em mais campos

--- src/modules/OpportunityWorkplan/components/registration-workplan-form-delivery/template.php
+++ src/modules/OpportunityWorkplan/components/registration-workplan-form-delivery/template.php
@@ -90,8 +90,8 @@ $this->import('
 
         <div class="field" v-if="opportunity.workplan_monitoringInformAccessibilityMeasures && (editable || accessibilityMeasures.length > 0)">
             <label :for="`${vid}__accessibilityMeasures`"><?= i::__('Medidas de acessibilidade') ?></label>
-            <mc-multiselect v-if="editable" :id="`${vid}__accessibilityMeasures`" :model="accessibilityMeasures" :items="accessibilityOptions"></mc-multiselect>
-            <mc-tag-list classes="primary__background" :tags="accessibilityMeasures"></mc-tag-list>
+            <mc-multiselect v-if="editable" :id="`${vid}__accessibilityMeasures`" :model="accessibilityMeasures" :items="accessibilityOptions" hide-filter hide-button></mc-multiselect>
+            <mc-tag-list classes="primary__background" :tags="accessibilityMeasures" :labels="accessibilityOptions" editable></mc-tag-list>
             <small class="field__error" v-if="validationErrors.accessibilityMeasures">{{ validationErrors.accessibilityMeasures.join('; ') }}</small>
         </div>
 
@@ -104,8 +104,8 @@ $this->import('
 
         <div class="field" v-if="opportunity.workplan_monitoringInformThePriorityAudience && (editable || priorityAudience.length > 0)">
             <label :for="`${vid}__priorityAudience`"><?= i::__('Territórios prioritários') ?></label>
-            <mc-multiselect v-if="editable" :id="`${vid}__priorityAudience`" :model="priorityAudience" :items="audienceOptions"></mc-multiselect>
-            <mc-tag-list classes="primary__background" :tags="priorityAudience"></mc-tag-list>
+            <mc-multiselect v-if="editable" :id="`${vid}__priorityAudience`" :model="priorityAudience" :items="audienceOptions" hide-filter hide-button></mc-multiselect>
+            <mc-tag-list classes="primary__background" :tags="priorityAudience" :labels="audienceOptions" editable></mc-tag-list>
             <small class="field__error" v-if="validationErrors.priorityAudience">{{ validationErrors.priorityAudience.join('; ') }}</small>
         </div>
 
