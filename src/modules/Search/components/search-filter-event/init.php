b01fdb858706e8ec227498ea284984b4b0878317
Victor Ferreira <vitfera@hotmail.com>
2026-03-02 00:24:21 -0300

    fix: corrige filtro de selos e exibição de estado/cidade na busca de eventos

--- src/modules/Search/components/search-filter-event/init.php
+++ src/modules/Search/components/search-filter-event/init.php
@@ -4,7 +4,20 @@
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
+$states_cities_filter_enabled = $app->config['events.filter.statesAndCities'] ?? false;
 $seals_filter_enabled = $app->config['events.filter.seals'] ?? false;
+
+// Carrega os dados de estado/cidade quando o filtro está habilitado e estamos numa instalação BR
+if ($states_cities_filter_enabled && ($app->config['statesAndCities.enable'] ?? false)) {
+    if (empty($app->view->jsObject['config']['statesAndCities'])) {
+        $file_name = $app->config['statesAndCities.file'];
+        $content = $this->resolveFilename('states-and-cities', $file_name);
+        include $content;
+        $app->view->jsObject['config']['statesAndCities'] = $data;
+        $app->view->jsObject['config']['statesAndCitiesEnable'] = true;
+        $app->view->jsObject['config']['statesAndCitiesCountryCode'] = $app->config['statesAndCities.countryCode'];
+    }
+}
 
 $seals = [];
 if ($seals_filter_enabled) {
@@ -16,7 +29,7 @@ if ($seals_filter_enabled) {
 }
 
 $this->jsObject['config']['searchFilterEvent'] = [
-    'statesAndCitiesFilterEnabled' => $app->config['events.filter.statesAndCities'] ?? false,
+    'statesAndCitiesFilterEnabled' => $states_cities_filter_enabled,
     'sealsFilterEnabled'           => $seals_filter_enabled,
     'seals'                        => $seals,
 ];
