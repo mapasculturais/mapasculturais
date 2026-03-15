eaa13b8c33dcf1068edc41b851197f2f464d8efa
Victor Ferreira <vitfera@hotmail.com>
2026-03-15 18:02:12 -0300

    fix: completa strings i18n do componente opportunity-execution-requests

--- src/modules/OpportunityExecution/components/opportunity-execution-requests/texts.php
+++ src/modules/OpportunityExecution/components/opportunity-execution-requests/texts.php
@@ -4,6 +4,10 @@ use MapasCulturais\i;
 return [
     'Pedido criado com sucesso' => i::__('Pedido criado com sucesso'),
     'Status desconhecido'       => i::__('Status desconhecido'),
-    'Não enviada'               => i::__('Não enviada'),
-    'Enviada'                   => i::__('Enviada'),
+    'Em preenchimento'          => i::__('Em preenchimento'),
+    'Pendente'                  => i::__('Pendente'),
+    'Inválida'                  => i::__('Inválida'),
+    'Não selecionada'           => i::__('Não selecionada'),
+    'Suplente'                  => i::__('Suplente'),
+    'Selecionada'               => i::__('Selecionada'),
 ];
