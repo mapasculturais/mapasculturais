cb0b285af59d0db5a3f4c4d685646d31b55411a0
erleibiazzio <erleibiazzio@gmail.com>
2026-05-13 10:30:01 -0300

    feat: exibe numero sequencial nos templates de detalhamento de avaliacao
    Atualiza os 5 templates de metodos de avaliacao para exibir o numero
    sequencial da comissao. Admin ve #numero + dados do avaliador,
    proponente ve apenas o numero quando nomes estao ocultos.

--- src/modules/EvaluationMethodContinuous/components/evaluation-continuous-detail/init.php
+++ src/modules/EvaluationMethodContinuous/components/evaluation-continuous-detail/init.php
@@ -38,7 +38,18 @@ if($class == Registration::class) {
         foreach ($evaluations as $eval) {
             $detail = $em->shouldDisplayEvaluationResults($reg) ? $em->getEvaluationDetails($eval) : [];
             $emc = $reg->opportunity->evaluationMethodConfiguration;
-            if ($emc->publishValuerNames){
+            $user = $app->user;
+            
+            // Busca número sequencial do avaliador (sempre que disponível)
+            $valuerRelation = $emc->getUserRelation($eval->user);
+            if ($valuerRelation) {
+                $detail['committeeSequentialNumber'] = $valuerRelation->getCommitteeSequentialNumber();
+            }
+            
+            // Administradores/gestores sempre veem nome e ID do avaliador
+            if ($reg->opportunity->canUser('@control', $user)) {
+                $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
+            } elseif ($emc->publishValuerNames) {
                 $detail['valuer'] = $eval->user->profile->simplify('id,name,singleUrl');
             }
             $data['evaluationsDetails'][] = $detail;
--- src/modules/EvaluationMethodTechnical/layouts/parts/technical/evaluations-details.php
+++ src/modules/EvaluationMethodTechnical/layouts/parts/technical/evaluations-details.php
@@ -51,8 +51,14 @@
 <div v-if="registration.evaluationsDetails.length" v-for="(evaluation, index) in registration.evaluationsDetails" class="registration-results__card">
     <div class="registration-results__card-header">
         <div class="registration-results__card-title">
-            <h4 v-if="evaluation.valuer" class="registration-results__opinion-title bold">
-                <?= i::__('Parecerista: ') ?> {{evaluation.valuer.name}}
+            <h4 v-if="evaluation.valuer && evaluation.committeeSequentialNumber" class="registration-results__opinion-title bold">
+                <?= i::__('Parecerista: ') ?> #{{evaluation.committeeSequentialNumber}} - ({{evaluation.valuer.id}} - {{evaluation.valuer.name}})
+            </h4>
+            <h4 v-else-if="evaluation.valuer" class="registration-results__opinion-title bold">
+                <?= i::__('Parecerista: ') ?> #{{index+1}} - ({{evaluation.valuer.id}} - {{evaluation.valuer.name}})
+            </h4>
+            <h4 v-else-if="evaluation.committeeSequentialNumber" class="registration-results__opinion-title bold">
+                <?= i::__('Parecerista: ') ?> #{{evaluation.committeeSequentialNumber}}
             </h4>
             <h4 v-else class="registration-results__opinion-title bold">
                 <?= i::__('Parecerista: ') ?> #{{index+1}}
