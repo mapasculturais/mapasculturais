c498b5c54b76615281ce6e672a30748724d9fb08
erleibiazzio <erleibiazzio@gmail.com>
2026-05-21 13:07:06 -0300

    vita reentrar em jsonSerialize de entidades Doctrine no printJsObject

--- src/modules/Opportunities/Module.php
+++ src/modules/Opportunities/Module.php
@@ -661,7 +661,10 @@ class Module extends \MapasCulturais\Module{
                 return $a->displayOrder <=> $b->displayOrder;
             });
 
-            $this->jsObject['registrationFields'] = $fields;
+            $this->jsObject['registrationFields'] = array_map(
+                fn ($field) => $field->jsonSerialize(),
+                $fields
+            );
         });
 
         $app->hook('mapas.printJsObject:before', function() use($app) {
