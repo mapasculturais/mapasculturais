04d726b4ab295c0ece0b687628a3bb7f373797c9
israelmelo <israelg.8@gmail.com>
2026-07-24 05:29:09 -0300

    Ajusta header da entidade na visão single
    Ref: #3562

--- src/modules/Entities/components/entity-header/template.php
+++ src/modules/Entities/components/entity-header/template.php
@@ -7,11 +7,12 @@
 use MapasCulturais\i;
 $this->import('
     mc-avatar
+    mc-icon
     mc-title
 ');
 ?>
 <?php $this->applyTemplateHook('entity-header', 'before'); ?>
-<header v-if="!editable" class="entity-header" :class="{ 'entity-header--no-image': !entity.files.header }">
+<header v-if="!editable" class="entity-header" :class="[{ 'entity-header--no-image': !entity.files.header }, 'entity-header--' + entity.__objectType]">
     <?php $this->applyTemplateHook('entity-header', 'begin'); ?>
     <div class="entity-header__single--cover" :style="{ '--url': url(entity.files.header?.url) }"></div>
     <div class="entity-header__single--content">
@@ -20,30 +21,42 @@ $this->import('
                 <mc-avatar :entity="entity" size="big"></mc-avatar>
             </div>
             <div class="data">
-                <mc-title tag="h1" size="big" class="entity-header__title"> {{entity.name}} </mc-title>
+                <div class="entity-header__identity">
+                    <mc-title tag="h1" size="big" class="entity-header__title"> {{entity.name}} </mc-title>
+                    <span v-if="entity.type && entity.__objectType === 'agent'" :class="['entity-header__type-badge', entity.__objectType + '__background']">
+                        <mc-icon :name="entity.type?.id == 1 ? 'agent-1' : 'agent-2'"></mc-icon>
+                        {{entity.type.name}}
+                    </span>
+                    <?php $this->applyTemplateHook('entity-header-seals', 'before'); ?>
+                    <slot name="seals"></slot>
+                    <?php $this->applyTemplateHook('entity-header-seals', 'after'); ?>
+                </div>
                 <div class="metadata">
                     <slot name="metadata">
                         <dl v-if="entity.id && global.showIds[entity.__objectType]" class="metadata__id">
                             <dt class="metadata__id--id"><?= i::__('ID') ?></dt>
                             <dd><strong>{{entity.id}}</strong></dd>
                         </dl>
-                        <dl v-if="entity.type">
+                        <dl v-if="entity.type && entity.__objectType !== 'agent'">
                             <dt><?= i::__('Tipo')?></dt>
                             <dd :class="[entity.__objectType+'__color', 'type']">{{entity.type.name}} </dd>
                         </dl>
                     </slot>
                 </div>
             </div>
-            <div :class="['description', {'description--event':entity.__objectType=='event'}]">
-                <slot name="description">
-                    <p class="description" v-html="entity.shortDescription" ></p>
-                </slot>
-            </div>
         </div>
         <div class="rightSide">
+            <div class="entity-header__actions">
+                <slot name="actions"></slot>
+            </div>
             <div v-if="entity.site" class="site">
                 <a :href="entity.site" target="_blank"><mc-icon :class="entity.__objectType+'__color'" name="link"></mc-icon>{{entity.site}}</a>
             </div>
+        </div>
+        <div v-if="entity.shortDescription" :class="['description', 'entity-header__short-description', {'description--event':entity.__objectType=='event'}]">
+            <slot name="description">
+                <p class="description" v-html="entity.shortDescription"></p>
+            </slot>
         </div>
     </div>
 
