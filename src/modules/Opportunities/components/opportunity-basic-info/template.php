2cb8920ca34f5580ce5ba28da95d1cbe5bf4e12e
israelmelo <israelg.8@gmail.com>
2026-08-03 09:38:21 -0300

    Estiliza aba Informações da edição de oportunidade Ref.: #3562

--- src/modules/Opportunities/components/opportunity-basic-info/template.php
+++ src/modules/Opportunities/components/opportunity-basic-info/template.php
@@ -23,51 +23,103 @@ $this->import('
     entity-social-media
     entity-status
     entity-terms
-    entity-terms
     link-opportunity
+    mc-collapsible
     mc-container
 ');
 ?>
-<div class="opportunity-basic-info__container">
-    <entity-status v-if="!entity.isModel" :entity="entity"></entity-status>
-</div>
+<div class="opportunity-basic-info edit-1">
+    <mc-container>
+        <entity-status v-if="!entity.isModel" :entity="entity"></entity-status>
+        <main class="edit-1__perfil-main">
+            <div class="stack--sm">
+                <div class="edit-1__section">
+                    <mc-collapsible :open="true">
+                        <template #header>
+                            <div class="edit-1__section-heading">
+                                <h3 class="edit-1__section-title"><?php i::_e('Informações de apresentação') ?></h3>
+                                <p class="edit-1__section-subtitle"><?php i::_e('Os dados inseridos abaixo serão exibidos para todos os usuários da plataforma.') ?></p>
+                            </div>
+                        </template>
+                        <template #body>
+                            <div class="grid-12 v-bottom">
+                                <entity-cover :entity="entity" classes="col-12"></entity-cover>
 
-<mc-container>
-    <main>
-        <mc-card>
-            <template #content>
-                <div class="header-opp grid-12 v-bottom">
-                    <entity-cover :entity="entity" classes=" header-opp__cover col-12"></entity-cover>
-                    <div class="header-opp__profile col-3 sm:col-12">
-                        <entity-profile :entity="entity"></entity-profile>
-                    </div>
-                    <div class="header-opp__field grid-12 col-9 sm:col-12">
-                        <entity-field :entity="entity" prop="name" classes="header-opp__field--name col-12"></entity-field>
-                        <entity-field :entity="entity" label="<?php i::esc_attr_e("Selecione o tipo da oportunidade") ?>" prop="type" classes="header-opp__field--name col-12"></entity-field>
-                    </div>
-                    <entity-field :entity="entity" classes="header-opp__field--name col-12" prop="shortDescription" :max-length="400"></entity-field>
-                    <entity-field :entity="entity" classes="header-opp__field--name col-12" prop="longDescription"></entity-field>
-                    <entity-files-list :entity="entity" classes="content-fileList col-12" group="downloads" title="<?php i::esc_attr_e('Adicionar arquivos'); ?>" editable></entity-files-list>
-                    <entity-links :entity="entity" classes="col-12" title="<?php i::esc_attr_e('Adicionar links'); ?>" editable></entity-links>
-                    <entity-gallery-video :entity="entity" classes="col-12" editable></entity-gallery-video>
-                    <entity-gallery :entity="entity" classes="col-12" editable></entity-gallery>
+                                <div class="col-12 grid-12">
+                                    <div class="col-3 sm:col-12">
+                                        <entity-profile :entity="entity"></entity-profile>
+                                    </div>
+                                    <div class="col-12 sm:col-12 grid-12 v-bottom">
+                                        <entity-field :entity="entity" prop="name" classes="col-12"></entity-field>
+                                        <entity-field :entity="entity" label="<?php i::esc_attr_e('Selecione o tipo da oportunidade') ?>" prop="type" classes="col-12"></entity-field>
+                                    </div>
+                                </div>
+
+                                <entity-field :entity="entity" classes="col-12" prop="shortDescription" :max-length="400"></entity-field>
+                                <entity-field :entity="entity" classes="col-12" prop="longDescription"></entity-field>
+                            </div>
+                        </template>
+                    </mc-collapsible>
                 </div>
-            </template>
-        </mc-card>
-    </main>
-    <aside>
-        <mc-card>
-            <div class="grid-12">
-                <link-opportunity :entity="entity" editable class="col-12"></link-opportunity>
-                <entity-file :entity="entity" titleModal="<?php i::_e('Adicionar regulamento') ?>" groupName="rules" classes="col-12" title="<?php i::esc_attr_e('Adicionar regulamento'); ?>" editable></entity-file>
-                <entity-admins :entity="entity" classes="col-12" editable></entity-admins>
-                <entity-related-agents :entity="entity" classes="col-12" title="<?php i::esc_attr_e('Agentes Relacionados'); ?>" editable></entity-related-agents>
-                <entity-social-media :entity="entity" classes="col-12" editable></entity-social-media>
-                <entity-seals :entity="entity" :editable="entity.currentUserPermissions?.createSealRelation" classes="col-12" title="<?php i::esc_attr_e('Verificações'); ?>"></entity-seals>
-                <entity-terms :entity="entity" classes="col-12" taxonomy="tag" title="<?php i::_e('Tags') ?>" editable></entity-terms>
-                <entity-owner :entity="entity" classes="col-12" title="Publicado por" editable></entity-owner>
+
+                <div class="edit-1__section">
+                    <mc-collapsible :open="true">
+                        <template #header>
+                            <div class="edit-1__section-heading">
+                                <h3 class="edit-1__section-title"><?php i::_e('Arquivos, links e galerias') ?></h3>
+                            </div>
+                        </template>
+                        <template #body>
+                            <div class="grid-12">
+                                <entity-files-list :entity="entity" classes="content-fileList col-12" group="downloads" title="<?php i::esc_attr_e('Adicionar arquivos'); ?>" editable></entity-files-list>
+                                <entity-links :entity="entity" classes="col-12" title="<?php i::esc_attr_e('Adicionar links'); ?>" editable></entity-links>
+                                <entity-gallery-video :entity="entity" classes="col-12" editable></entity-gallery-video>
+                                <entity-gallery :entity="entity" classes="col-12" editable></entity-gallery>
+                            </div>
+                        </template>
+                    </mc-collapsible>
+                </div>
+
+                <div class="edit-1__section">
+                    <mc-collapsible :open="true">
+                        <template #header>
+                            <div class="edit-1__section-heading">
+                                <h3 class="edit-1__section-title"><?php i::_e('Vínculo e regulamento') ?></h3>
+                            </div>
+                        </template>
+                        <template #body>
+                            <div class="grid-12">
+                                <link-opportunity :entity="entity" editable class="col-12"></link-opportunity>
+                                <entity-file :entity="entity" titleModal="<?php i::_e('Adicionar regulamento') ?>" groupName="rules" classes="col-12" title="<?php i::esc_attr_e('Adicionar regulamento'); ?>" editable></entity-file>
+                            </div>
+                        </template>
+                    </mc-collapsible>
+                </div>
+
+                <div class="edit-1__section">
+                    <mc-collapsible :open="true">
+                        <template #header>
+                            <div class="edit-1__section-heading">
+                                <h3 class="edit-1__section-title"><?php i::_e('Administração e relações') ?></h3>
+                            </div>
+                        </template>
+                        <template #body>
+                            <div class="grid-12">
+                                <entity-admins :entity="entity" classes="col-12" editable></entity-admins>
+                                <entity-related-agents :entity="entity" classes="col-12" title="<?php i::esc_attr_e('Agentes Relacionados'); ?>" editable></entity-related-agents>
+                                <entity-seals :entity="entity" :editable="entity.currentUserPermissions?.createSealRelation" classes="col-12" title="<?php i::esc_attr_e('Verificações'); ?>"></entity-seals>
+                                <entity-terms :entity="entity" classes="col-12" taxonomy="tag" title="<?php i::_e('Tags') ?>" editable></entity-terms>
+                                <entity-owner :entity="entity" classes="col-12" title="Publicado por" editable></entity-owner>
+                            </div>
+                        </template>
+                    </mc-collapsible>
+                </div>
+
+                <div class="edit-1__section edit-1__section--social">
+                    <entity-social-media :entity="entity" classes="col-12" editable></entity-social-media>
+                </div>
             </div>
-        </mc-card>
-    </aside>
-</mc-container>
+        </main>
+    </mc-container>
+</div>
 <confirm-before-exit :entity="entity"></confirm-before-exit>
