68c8613abed047ef9cee9aa0a675853c65d76a5b
Victor Ferreira <vitfera@hotmail.com>
2026-08-18 23:36:11 -0300

    Corrige persistência dos filtros individuais da comissão

--- src/core/Entities/EvaluationMethodConfigurationAgentRelation.php
+++ src/core/Entities/EvaluationMethodConfigurationAgentRelation.php
@@ -138,6 +138,7 @@ class EvaluationMethodConfigurationAgentRelation extends AgentRelation {
         $this->metadata->ranges = $this->metadata->ranges ?? null;
         $this->metadata->distribution = $this->metadata->distribution ?? null;
         $this->metadata->selectionFields = $this->metadata->selectionFields ?? null;
+        $this->metadata->filtersStoredOnRelation = $this->metadata->filtersStoredOnRelation ?? false;
         $this->metadata->committeeSequentialNumber = $this->metadata->committeeSequentialNumber ?? null;
 
         return $this->metadata;
@@ -282,6 +283,19 @@ class EvaluationMethodConfigurationAgentRelation extends AgentRelation {
     {
         $this->initializeMetadata();
         $this->metadata->selectionFields = $selection_fields ? (object) $selection_fields : null;
+        $this->metadata = (object) (array) $this->metadata;
+    }
+
+    public function getFiltersStoredOnRelation(): bool
+    {
+        $this->initializeMetadata();
+        return (bool) $this->metadata->filtersStoredOnRelation;
+    }
+
+    public function setFiltersStoredOnRelation(bool $stored): void
+    {
+        $this->initializeMetadata();
+        $this->metadata->filtersStoredOnRelation = $stored;
         $this->metadata = (object) (array) $this->metadata;
     }
 
